bbfe87c103cff01c4558ca3b9d54175c7a087a7c
goffle <se.legoff@gmail.com>
2021-01-14 09:50:37 +0100

    put back error file corrupted

--- app/src/scenes/inscription/Create/stepConsentements.js
+++ app/src/scenes/inscription/Create/stepConsentements.js
@@ -87,7 +87,16 @@ export default ({ setStep }) => {
                   name="parentConsentmentFiles"
                   value={values.parentConsentmentFiles}
                   onChange={async (e) => {
-                    let { data: files, ok } = await api.uploadFile("/young/file/parentConsentmentFiles", e.target.files);
+                    let { data: files, ok, code } = await api.uploadFile("/young/file/parentConsentmentFiles", e.target.files);
+
+                    if (code === "FILE_CORRUPTED") {
+                      return toastr.error(
+                        "Le fichier semble corrompu",
+                        "Pouvez vous changer le format ou regénérer votre fichier ? Si vous rencontrez toujours le problème, contactez le support inscription@snu.gouv.fr",
+                        { timeOut: 0 }
+                      );
+                    }
+
                     if (!ok) return toastr.error("Une erreur s'est produite lors du téléversement de votre fichier");
                     handleChange({ target: { value: files, name: "parentConsentmentFiles" } });
                     toastr.success("Fichier téléversé");
