d6bd24bee07ca88bf7fa5184530a8d8b1544c870
Tangi Mendes <tangimds@gmail.com>
2021-01-18 17:26:24 +0100

    fix + script : referents email (#27)
    * fix + script
    
    * script
    
    * fix: improve script
    
    * hop
    
    Co-authored-by: Raphaël Huchet <raph@selego.co>

--- api/src/controllers/referent.js
+++ api/src/controllers/referent.js
@@ -51,10 +51,11 @@ router.post("/reset_password", passport.authenticate("referent", { session: fals
 router.post("/signup_invite/:role", passport.authenticate("referent", { session: false }), async (req, res) => {
   try {
     const obj = {};
-    if (req.body.hasOwnProperty(`email`)) obj.email = req.body.email;
+    if (req.body.hasOwnProperty(`email`)) obj.email = req.body.email.trim().toLowerCase();
     if (req.body.hasOwnProperty(`firstName`)) obj.firstName = req.body.firstName;
     if (req.body.hasOwnProperty(`lastName`)) obj.lastName = req.body.lastName;
     if (req.body.hasOwnProperty(`role`)) obj.role = req.body.role;
+
     if (req.body.hasOwnProperty(`region`)) obj.region = req.body.region; //TODO
     if (req.body.hasOwnProperty(`department`)) obj.department = req.body.department;
 
