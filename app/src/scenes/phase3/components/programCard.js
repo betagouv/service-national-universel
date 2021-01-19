d1272c260a9af3c17802caf6f85ed19d6243c358
tangimds <tangimds@gmail.com>
2021-01-19 14:07:40 +0100

    add screen refused

--- app/src/scenes/phase3/components/programCard.js
+++ app/src/scenes/phase3/components/programCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-export default ({ title, image, details }) => {
+export default ({ title, image, details, href }) => {
   return (
     <Card>
       <div className="thumb">
@@ -9,6 +9,9 @@ export default ({ title, image, details }) => {
       </div>
       <h4>{title}</h4>
       <p>{details}</p>
+      <SeeMore href={href} target="_blank">
+        DÉCOUVRIR
+      </SeeMore>
     </Card>
   );
 };
@@ -19,8 +22,8 @@ const Card = styled.div`
     margin-bottom: 20px;
     img {
       border-radius: 6px;
-      max-width: 100%;
-      height: 240px;
+      width: 100%;
+      height: 290px;
       object-fit: cover;
       box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
     }
@@ -32,6 +35,12 @@ const Card = styled.div`
   }
   p {
     color: #6b7280;
-    font-weight: 700;
+    font-weight: 400;
   }
+`;
+
+const SeeMore = styled.a`
+  color: #42389d;
+  font-size: 16px;
+  font-weight: 600;
 `;
