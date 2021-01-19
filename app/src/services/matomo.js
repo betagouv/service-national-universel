d45038903f9f2fd1caa98272f1ef42cc63a9b707
goffle <se.legoff@gmail.com>
2021-01-19 09:49:40 +0100

    add stats (#54)

--- app/src/services/matomo.js
+++ app/src/services/matomo.js
@@ -2,11 +2,11 @@
 // https://developer.matomo.org/guides/tracking-javascript-guide
 // https://matomo.org/docs/event-tracking/#what-is-event-tracking
 
-
 class Api {
   constructor() {
     this.userProperties = {};
-    window._paq &&  window._paq.push(["enableHeartBeatTimer", 30]);
+    if (!window._paq) return console.log("No matomo detected");
+    window._paq.push(["enableHeartBeatTimer", 30]);
   }
 
   /*
@@ -16,28 +16,14 @@ class Api {
     Value (Optional) – This is a numeric value and is often added dynamically. It could be the cost of a product that is added to a cart, or the completion percentage of a video.
   */
   logEvent(categorie, action, name, value) {
-    if (!window._paq) return console.log("No matomo detected");
-    console.log("LOG EVENT CALLED")
-    window._paq && window._paq.push(["trackEvent", categorie, action, name, value]);
+    if (!window._paq) return;
+    window._paq.push(["trackEvent", categorie, action, name, value]);
   }
 
-  // init({ baseUrl, idsite, userId, _idvc }) {
-  //   this.baseUrl = baseUrl;
-  //   this.idsite = idsite;
-  //   this.userId = userId;
-  //   this._idvc = _idvc;
-  //   this.initDone = true;
-  //   this.userProperties = {};
-  // }
-  // makeid(length = 16) {
-  //   var result = "";
-  //   var characters = "01234567890abcdefABCDEF";
-  //   var charactersLength = characters.length;
-  //   for (var i = 0; i < length; i++) {
-  //     result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  //   }
-  //   return result;
-  // }
+  setUserId(userId) {
+    if (!window._paq) return;
+    window._paq.push(["setUserId", userId]);
+  }
 
   setUserProperties(newProperties) {
     //   _paq.push(['setCustomVariable',
@@ -53,82 +39,6 @@ class Api {
 
     this.userProperties = { ...this.userProperties, ...newProperties };
   }
-
-
-  // computeCvar(cvarObject) {
-  //   const _cvar = {};
-  //   for (let [index, key] of Object.keys(cvarObject).entries()) {
-  //     _cvar[`${index}`] = [key, cvarObject[key]];
-  //   }
-  //   return JSON.stringify(_cvar);
-  // }
-  // computeParams(params, idsite) {
-  //   params = {
-  //     _cvar: this.computeCvar(this.userProperties),
-  //     idsite,
-  //     _id: this.userId,
-  //     uid: this.userId,
-  //     rec: 1,
-  //     rand: Date.now(),
-  //     _idvc: this._idvc,
-  //     ...params,
-  //   };
-  //   return Object.keys(params).reduce((paramString, key, index) => {
-  //     const computedParam = `${key}=${params[key]}`;
-  //     if (index === 0) {
-  //       return computedParam;
-  //     }
-  //     return `${paramString}&${computedParam}`;
-  //   }, "");
-  // }
-  // async logEvent({ category, action, name = "", value = null }) {
-  //   // e_c — The event category. Must not be empty. (eg. Videos, Music, Games...)
-  //   // e_a — The event action. Must not be empty. (eg. Play, Pause, Duration, Add Playlist, Downloaded, Clicked...)
-  //   // e_n — The event name. (eg. a Movie name, or Song name, or File name...)
-  //   // e_v — The event value. Must be a float or integer value (numeric), not a string.
-  //   const params = { e_c: category, e_a: action };
-  //   if (name !== "") params.e_n = name;
-  //   if (value !== null && !isNaN(Number(value))) params.e_v = Number(value);
-  //   await this.execute(params);
-  // }
-  // async execute(params) {
-  //   try {
-  //     if (!this.initDone) {
-  //       throw new Error("matomo not initialized yet");
-  //     }
-  //     if (__DEV__) {
-  //       console.log(`${this.baseUrl}?${this.computeParams(params, this.idsite)}`);
-  //       return;
-  //     }
-  //     const url = `${this.baseUrl}?${this.computeParams(params, this.idsite)}`;
-  //     const res = await fetch(encodeURI(url));
-  //     // if (__DEV__) {
-  //     //   console.log(url);
-  //     //   console.log(res.status);
-  //     // }
-  //     if (__DEV__ && res.status !== 200) {
-  //       console.log(res);
-  //       throw new Error("error fetching matomo");
-  //     }
-  //     if (!this.baseUrl2 || !this.idsite2) {
-  //       return;
-  //     }
-  //     const url2 = `${this.baseUrl2}?${this.computeParams(params, this.idsite2)}`;
-  //     const res2 = await fetch(encodeURI(url2));
-  //     if (__DEV__) {
-  //       console.log(url2);
-  //       console.log(res2.status);
-  //     }
-  //     if (__DEV__ && res2.status !== 200) {
-  //       console.log(res);
-  //       throw new Error("error fetching matomo");
-  //     }
-  //   } catch (e) {
-  //     if (__DEV__) {
-  //       console.log("matomo error", e);
-  //     }
-  //   }
-  // }
 }
 
 const matomo = new Api();
