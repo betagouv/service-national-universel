f7a4321d48fed76da32b94ba77b2f661c41b4151
Raphaël Huchet <raph@selego.co>
2021-02-02 17:35:59 +0100

    hotfix

--- admin/src/components/DocumentInModal.js
+++ admin/src/components/DocumentInModal.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Modal } from "reactstrap";
 import { Page, Document, pdfjs } from "react-pdf";
-import { pdfjsworker } from "pdfjs-dist/es5/build/pdf.worker.entry";
-pdfjs.GlobalWorkerOptions.workerSrc = pdfjsworker;
+// import { pdfjsworker } from "pdfjs-dist/es5/build/pdf.worker.entry";
+// pdfjs.GlobalWorkerOptions.workerSrc = pdfjsworker;
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 // pdfjs.GlobalWorkerOptions.workerSrc = `https://cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjs.version}/es5/build/pdf.worker.min.js`;
 
 export default function DocumentInModal({ value, onChange }) {
