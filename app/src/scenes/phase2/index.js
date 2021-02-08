8cc40c1b1a452647c6f9b8d570ca1ea30edf600a
Tangi Mendès <tangimds@gmail.com>
2021-02-08 14:50:28 +0100

    Feat : homes en fonction des cohortes (#111)

--- app/src/scenes/phase2/index.js
+++ app/src/scenes/phase2/index.js
@@ -1,29 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import NextStep from "./nextStep";
 
 export default () => {
   return (
-    <Hero>
-      <div className="content">
-        <h1>
-          <strong>Phase 2</strong>
-        </h1>
-        <p>
-          Culpa voluptate deserunt proident anim ex sunt ad adipisicing enim et pariatur anim. Lorem sint ipsum cupidatat sit laborum Lorem dolor id laborum adipisicing non et.
-          Aliquip aute voluptate amet irure cillum esse mollit veniam reprehenderit.
-        </p>
-        <Link to="/phase3/une-mission">
-          <BackButton>Trouver une mission</BackButton>
-        </Link>
-      </div>
-      <div className="thumb" />
-    </Hero>
+    <>
+      <Hero>
+        <div className="content">
+          <h1>
+            Réalisez votre <strong>mission d'intérêt général</strong>
+          </h1>
+          <p>
+            Culpa voluptate deserunt proident anim ex sunt ad adipisicing enim et pariatur anim. Lorem sint ipsum cupidatat sit laborum Lorem dolor id laborum adipisicing non et.
+            Aliquip aute voluptate amet irure cillum esse mollit veniam reprehenderit.
+          </p>
+        </div>
+        <div className="thumb" />
+      </Hero>
+      <NextStep />
+    </>
   );
 };
 
 const Hero = styled.div`
-  border-radius: 6px;
+  border-radius: 0.5rem;
+  margin: 0 auto;
+  max-width: 80rem;
   box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
   position: relative;
   overflow: hidden;
@@ -31,6 +34,9 @@ const Hero = styled.div`
   justify-content: space-between;
   .content {
     width: 65%;
+    @media (max-width: 768px) {
+      width: 100%;
+    }
     padding: 60px 30px 60px 50px;
     position: relative;
     background-color: #fff;
@@ -40,15 +46,21 @@ const Hero = styled.div`
     }
   }
   h1 {
-    font-size: 50px;
+    font-size: 3rem;
+    @media (max-width: 768px) {
+      font-size: 1.8rem;
+    }
     color: #161e2e;
     margin-bottom: 20px;
-    font-weight: 400;
+    font-weight: 500;
     line-height: 1;
   }
   p {
     color: #6b7280;
-    font-size: 16px;
+    font-size: 1.25rem;
+    @media (max-width: 768px) {
+      font-size: 1rem;
+    }
     font-weight: 400;
     display: -webkit-box;
     -webkit-line-clamp: 5;
@@ -60,25 +72,5 @@ const Hero = styled.div`
     background: url(${require("../../assets/phase3.jpg")}) no-repeat center;
     background-size: cover;
     flex: 1;
-  }
-`;
-
-const BackButton = styled.button`
-  color: #fff;
-  background-color: #5145cd;
-  padding: 9px 20px;
-  border: 0;
-  outline: 0;
-  border-radius: 6px;
-  font-weight: 400;
-  font-size: 16px;
-  margin: 40px auto 0;
-  display: block;
-  outline: 0;
-  box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
-  position: relative;
-  z-index: 2;
-  :hover {
-    opacity: 0.9;
   }
 `;
