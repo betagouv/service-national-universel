476e42952d12f2272346942b1a5d33bc37a82b5c
tangimds <tangimds@gmail.com>
2021-02-10 18:39:46 +0100

    Update sync.js

--- api/scripts/sync.js
+++ api/scripts/sync.js
@@ -1,39 +1,33 @@
-require("dotenv").config({ path: "../.env-prod" });
+require("dotenv").config({ path: "../.env-staging" });
 
 require("../src/mongo");
-const YoungModel = require("../src/models/young");
-const StructureModel = require("../src/models/structure");
+
+const esclient = require("../src/es");
+const ApplicationModel = require("../src/models/application");
 const MissionModel = require("../src/models/mission");
 const ReferentModel = require("../src/models/referent");
-
-const { getQPV } = require("../src/qpv");
+const StructureModel = require("../src/models/structure");
+const YoungModel = require("../src/models/young");
 
 (async function fetch() {
-  await run(YoungModel);
+  await run(ApplicationModel, "application");
+  await run(MissionModel, "mission");
+  await run(ReferentModel, "referent");
+  await run(StructureModel, "structure");
+  await run(YoungModel, "young");
+  process.exit(1);
 })();
 
-let count = 0;
-async function run(MyModel) {
-  const cursor = MyModel.find({ cohort: "2021" }).cursor();
+async function run(MyModel, index) {
+  console.log("START", MyModel);
+  await esclient.indices.delete({ index });
+  let count = 0;
+  const cursor = MyModel.find({}).cursor();
   await cursor.eachAsync(async function (doc) {
     count++;
     if (count % 10 === 0) console.log(count);
-
     try {
-      if (!doc.zip || !doc.city || !doc.address) return;
-
-      const qpv = await getQPV(doc.zip, doc.city, doc.address);
-      if (qpv === true) {
-        doc.set({ qpv: "true" });
-      } else if (qpv === false) {
-        doc.set({ qpv: "false" });
-      } else {
-        doc.set({ qpv: "false" });
-      }
-      await doc.save();
-
-      // await doc.save();
-      // await doc.index();
+      await doc.index();
     } catch (e) {
       console.log("e", e);
     }
