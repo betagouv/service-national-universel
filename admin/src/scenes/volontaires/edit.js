52e2d9c63a5b3a789205cad6723a6fc5add15f62
tangimds <tangimds@gmail.com>
2021-02-10 15:40:46 +0100

    fix

--- admin/src/scenes/volontaires/edit.js
+++ admin/src/scenes/volontaires/edit.js
@@ -664,22 +664,6 @@ const BadgeStyle = styled.span`
   `}
 `;
 
-  const InfoBtn = styled(LoadingButton)`
-  color: #555;
-  background: url(${require("../../assets/eye.svg")}) left 15px center no-repeat;
-  box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.16);
-  border: 0;
-  outline: 0;
-  border-radius: 5px;
-  padding: 8px 25px 8px 40px;
-  font-size: 14px;
-  font-weight: 500;
-  cursor: pointer;
-  margin-top: 1rem;
-  margin-left: 1rem;
-  width: fit-content;
-`;
-
 const Documents = styled.div`
   margin-left: 1rem;
   margin-right: 1rem;
