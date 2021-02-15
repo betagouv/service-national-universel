98522c20bc4618241f49b8422d607651bc5759ee
tangimds <tangimds@gmail.com>
2021-02-15 12:04:51 +0100

    drag and drop

--- app/src/scenes/preferences/rankingPeriod.js
+++ app/src/scenes/preferences/rankingPeriod.js
@@ -1,16 +1,35 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
 import { PERIOD_DURING_HOLIDAYS, PERIOD_DURING_SCHOOL, PERIOD, translate } from "../../utils";
 
 export default ({ title, period, handleChange, name, values }) => {
   const [items, setItems] = useState(values[name]);
 
+  const updateList = (value) => {
+    handleChange({ target: { name, value } });
+    setItems(value);
+  };
+
+  const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+    return result;
+  };
+
+  const onDragEnd = (result) => {
+    if (!result.destination || result.destination.index === result.source.index) return;
+    const res = reorder(items, result.source.index, result.destination.index);
+    updateList(res);
+  };
+
   useEffect(() => {
     let defaultRanking = items;
     if (period === PERIOD.DURING_HOLIDAYS && !Object.keys(PERIOD_DURING_HOLIDAYS).includes(items[0])) defaultRanking = Object.keys(PERIOD_DURING_HOLIDAYS);
     if (period === PERIOD.DURING_SCHOOL && !Object.keys(PERIOD_DURING_SCHOOL).includes(items[0])) defaultRanking = Object.keys(PERIOD_DURING_SCHOOL);
-    handleChange({ target: { name, value: defaultRanking } });
-    setItems(defaultRanking);
+    updateList(defaultRanking);
   }, [period]);
 
   if (!items) return <div />;
@@ -18,9 +37,18 @@ export default ({ title, period, handleChange, name, values }) => {
   return (
     <Container>
       <Title>{title}</Title>
-      {items.map((e, i) => (
-        <Item key={i} index={i} value={e} values={values} handleChange={handleChange} name={name} />
-      ))}
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="list">
+          {(provided) => (
+            <div ref={provided.innerRef} {...provided.droppableProps}>
+              {items.map((e, i) => (
+                <Item key={e} index={i} value={e} values={values} updateList={updateList} name={name} />
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </Container>
   );
 };
@@ -31,29 +59,31 @@ Array.prototype.toogleValue = function (i, j) {
   this[i] = temp;
 };
 
-const Item = ({ value, values, index, handleChange, name }) => {
+const Item = ({ value, values, index, updateList, name }) => {
   const handleMove = (index, delta) => {
     const nextIndex = index + delta;
     if (nextIndex >= 0 && nextIndex < values[name].length) {
       const d = values[name];
       d.toogleValue(nextIndex, index);
-      handleChange({ target: { name: "periodRanking", value: d } });
+      updateList(d);
     }
   };
 
   return (
-    <>
-      <ItemContainer>
-        <div style={{ display: "flex", flex: 1 }}>
-          <RoundItem value={index + 1} />
-          <Label>{translate(value)}</Label>
-        </div>
-        <div style={{ display: "flex" }}>
-          <RoundItem plus onClick={() => handleMove(index, -1)} />
-          <RoundItem minus onClick={() => handleMove(index, 1)} />
-        </div>
-      </ItemContainer>
-    </>
+    <Draggable draggableId={value} index={index}>
+      {(provided) => (
+        <ItemContainer ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+          <div style={{ display: "flex", flex: 1 }}>
+            <RoundItem value={index + 1} />
+            <Label>{translate(value)}</Label>
+          </div>
+          <div style={{ display: "flex" }}>
+            <RoundItem plus onClick={() => handleMove(index, -1)} />
+            <RoundItem minus onClick={() => handleMove(index, 1)} />
+          </div>
+        </ItemContainer>
+      )}
+    </Draggable>
   );
 };
 
