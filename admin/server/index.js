431ea12294b359b618bf6567d365a22f5a5f2032
Raphaël Huchet <raph@selego.co>
2021-02-22 17:04:22 +0100

    chore: test host

--- admin/server/index.js
+++ admin/server/index.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const path = require("path");
 const hsts = require("hsts");
+const { forceDomain } = require("forcedomain");
 
 const app = express();
 const port = 8080;
 
 app.use(hsts({ maxAge: 31536000, includeSubDomains: true, preload: true }));
 app.use(express.static(path.join(__dirname, "../build")));
+app.use(
+  forceDomain({
+    hostname: "candidature.snu.gouv.fr",
+    protocol: "https",
+  })
+);
 
 app.route("*").all((req, res) => {
+  console.log(req.headers.host);
   res.status(200).sendFile(path.join(__dirname, "/../build/index.html"));
 });
 
