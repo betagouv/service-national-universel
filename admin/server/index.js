a60a41436e29f39f9c7614c6dd5764e9e6a02e35
Raphaël Huchet <raph@selego.co>
2021-02-22 17:13:08 +0100

    fix: fix

--- admin/server/index.js
+++ admin/server/index.js
@@ -6,17 +6,18 @@ const { forceDomain } = require("forcedomain");
 const app = express();
 const port = 8080;
 
-app.use(hsts({ maxAge: 31536000, includeSubDomains: true, preload: true }));
-app.use(express.static(path.join(__dirname, "../build")));
 app.use(
   forceDomain({
     hostname: "candidature.snu.gouv.fr",
     protocol: "https",
+    excludeRule: /[a-zA-Z0-9-]+\.cleverapps\.io/,
   })
 );
 
+app.use(hsts({ maxAge: 31536000, includeSubDomains: true, preload: true }));
+app.use(express.static(path.join(__dirname, "../build")));
+
 app.route("*").all((req, res) => {
-  console.log(req.headers.host);
   res.status(200).sendFile(path.join(__dirname, "/../build/index.html"));
 });
 
