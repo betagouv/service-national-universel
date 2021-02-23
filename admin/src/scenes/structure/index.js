8e16803c2bbcf62c0ffa7fd08cb84bce2d8bec4b
tangimds <tangimds@gmail.com>
2021-02-23 17:07:16 +0100

    up

--- admin/src/scenes/structure/index.js
+++ admin/src/scenes/structure/index.js
@@ -6,11 +6,12 @@ import Missions from "./view/missions";
 import Historic from "./view/historic";
 import Edit from "./edit";
 import List from "./list";
+import Create from "./create";
 
 export default () => {
   return (
     <Switch>
-      <Route path="/structure/create" component={Edit} />
+      <Route path="/structure/create" component={Create} />
       <Route path="/structure/:id/edit" component={Edit} />
       <Route path="/structure/:id" component={View} />
       <Route path="/structure" component={List} />
