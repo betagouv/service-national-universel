70ff0be501d1e6910e69336202a09af5372c9030
tangimds <tangimds@gmail.com>
2021-02-26 18:04:32 +0100

    Update application.js

--- api/src/controllers/application.js
+++ api/src/controllers/application.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
+const fs = require("fs");
+const path = require("path");
 
 const { capture } = require("../sentry");
-
 const ApplicationObject = require("../models/application");
 const MissionObject = require("../models/mission");
 const ReferentObject = require("../models/referent");
+const { sendEmail } = require("../sendinblue");
 
 const SERVER_ERROR = "SERVER_ERROR";
 const NOT_FOUND = "NOT_FOUND";
@@ -90,6 +92,82 @@ router.delete("/:id", passport.authenticate("referent", { session: false }), asy
   } catch (error) {
     capture(error);
     res.status(500).send({ ok: false, error, code: SERVER_ERROR });
+  }
+});
+
+router.post("/:id/notify/:template", passport.authenticate(["referent", "young"], { session: false }), async (req, res) => {
+  try {
+    const { id, template } = req.params;
+
+    const application = await ApplicationObject.findById(id);
+    if (!application) return res.status(404).send({ ok: false, code: NOT_FOUND + "_app" });
+    const mission = await MissionObject.findById(application.missionId);
+    if (!mission) return res.status(404).send({ ok: false, code: NOT_FOUND + "_mission" });
+    const referent = await ReferentObject.findById(mission.tutorId);
+    if (!referent) return res.status(404).send({ ok: false, code: NOT_FOUND + "_referent" });
+
+    let htmlContent = "";
+    let subject = "";
+    let to = {};
+
+    if (template === "waiting_validation") {
+      htmlContent = fs.readFileSync(path.resolve(__dirname, "../templates/application/waitingValidation.html")).toString();
+      htmlContent = htmlContent.replace(/{{firstName}}/g, referent.firstName);
+      htmlContent = htmlContent.replace(/{{lastName}}/g, referent.lastName);
+      htmlContent = htmlContent.replace(/{{youngFirstName}}/g, application.youngFirstName);
+      htmlContent = htmlContent.replace(/{{youngLastName}}/g, application.youngLastName);
+      htmlContent = htmlContent.replace(/{{missionName}}/g, mission.name);
+      htmlContent = htmlContent.replace(/{{cta}}/g, "https://admin.snu.gouv.fr/volontaire");
+      subject = `${application.youngFirstName} ${application.youngLastName} a candidaté sur votre mission d'intérêt général ${mission.name}`;
+      to = { name: `${referent.firstName} ${referent.lastName}`, email: referent.email };
+    } else if (template === "validated_responsible") {
+      htmlContent = fs.readFileSync(path.resolve(__dirname, "../templates/application/validatedResponsible.html")).toString();
+      htmlContent = htmlContent.replace(/{{firstName}}/g, referent.firstName);
+      htmlContent = htmlContent.replace(/{{lastName}}/g, referent.lastName);
+      htmlContent = htmlContent.replace(/{{youngFirstName}}/g, application.youngFirstName);
+      htmlContent = htmlContent.replace(/{{youngLastName}}/g, application.youngLastName);
+      htmlContent = htmlContent.replace(/{{missionName}}/g, mission.name);
+      htmlContent = htmlContent.replace(/{{cta}}/g, "https://admin.snu.gouv.fr/volontaire");
+      subject = `${application.youngFirstName} ${application.youngLastName} est validée sur votre mission d'intérêt général ${mission.name}`;
+      to = { name: `${referent.firstName} ${referent.lastName}`, email: referent.email };
+    } else if (template === "validated_young") {
+      htmlContent = fs.readFileSync(path.resolve(__dirname, "../templates/application/validatedYoung.html")).toString();
+      htmlContent = htmlContent.replace(/{{firstName}}/g, application.youngFirstName);
+      htmlContent = htmlContent.replace(/{{lastName}}/g, application.youngLastName);
+      htmlContent = htmlContent.replace(/{{structureName}}/g, mission.structureName);
+      htmlContent = htmlContent.replace(/{{youngFirstName}}/g, application.youngFirstName);
+      htmlContent = htmlContent.replace(/{{youngLastName}}/g, application.youngLastName);
+      htmlContent = htmlContent.replace(/{{missionName}}/g, mission.name);
+      htmlContent = htmlContent.replace(/{{cta}}/g, "https://inscription.snu.gouv.fr");
+      subject = `Votre candidature sur la mission d'intérêt général ${mission.name} a été validée`;
+      to = { name: `${application.youngFirstName} ${application.youngLastName}`, email: application.youngEmail };
+    } else if (template === "cancel") {
+      htmlContent = fs.readFileSync(path.resolve(__dirname, "../templates/application/canceled.html")).toString();
+      htmlContent = htmlContent.replace(/{{firstName}}/g, referent.firstName);
+      htmlContent = htmlContent.replace(/{{lastName}}/g, referent.lastName);
+      htmlContent = htmlContent.replace(/{{youngFirstName}}/g, application.youngFirstName);
+      htmlContent = htmlContent.replace(/{{youngLastName}}/g, application.youngLastName);
+      htmlContent = htmlContent.replace(/{{missionName}}/g, mission.name);
+      subject = `${application.youngFirstName} ${application.youngLastName} a annulé sa candidature sur votre mission d'intérêt général ${mission.name}`;
+      to = { name: `${referent.firstName} ${referent.lastName}`, email: referent.email };
+    } else if (template === "refused") {
+      htmlContent = fs.readFileSync(path.resolve(__dirname, "../templates/application/refused.html")).toString();
+      htmlContent = htmlContent.replace(/{{firstName}}/g, application.youngFirstName);
+      htmlContent = htmlContent.replace(/{{lastName}}/g, application.youngLastName);
+      htmlContent = htmlContent.replace(/{{structureName}}/g, mission.structureName);
+      htmlContent = htmlContent.replace(/{{missionName}}/g, mission.name);
+      htmlContent = htmlContent.replace(/{{cta}}/g, "https://inscription.snu.gouv.fr/mission");
+      subject = `Votre candidature sur la mission d'intérêt général ${mission.name} a été refusée.`;
+      to = { name: `${application.youngFirstName} ${application.youngLastName}`, email: application.youngEmail };
+    } else {
+      throw new Error("Template de mail introuvable");
+    }
+
+    await sendEmail(to, subject, htmlContent);
+    return res.status(200).send({ ok: true }); //todo
+  } catch (error) {
+    capture(error);
+    res.status(500).send({ ok: false, code: SERVER_ERROR, error });
   }
 });
 
