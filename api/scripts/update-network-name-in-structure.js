bbd40f5e915f3d8d1613b13654f08306f7352246
Raphaël Huchet <raph@selego.co>
2021-03-02 15:43:00 +0100

    feat: add network name in structure

--- api/scripts/update-network-name-in-structure.js
+++ api/scripts/update-network-name-in-structure.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: "../.env-prod" });
+require("dotenv").config({ path: "../.env-staging" });
 
 require("../src/mongo");
 const StructureModel = require("../src/models/structure");
