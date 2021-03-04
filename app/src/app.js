da4504bdac393f5a18c293a5056c41513c458496
goffle <se.legoff@gmail.com>
2021-03-04 16:24:04 +0100

    tracker

--- app/src/app.js
+++ app/src/app.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { BrowserRouter as Router, Route, Switch, Redirect, useLocation } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as Sentry from "@sentry/browser";
-import { Helmet } from "react-helmet";
+
 import styled from "styled-components";
 
 import { setYoung } from "./redux/auth/actions";
@@ -24,7 +24,6 @@ import Loader from "./components/Loader";
 
 import Header from "./components/header";
 import Drawer from "./components/drawer";
-
 import Footer from "./components/footer";
 
 import api from "./services/api";
@@ -62,14 +61,6 @@ export default () => {
 
   return (
     <Router>
-      <Helmet>
-        <script>{`
-            gtag('event', 'conversion', {
-              'allow_custom_scripts': true,
-              'send_to': 'DC-2971054/snuiz0/snu21lp+unique'
-            });`}</script>
-        <noscript>{`<img src="https://ad.doubleclick.net/ddm/activity/src=2971054;type=snuiz0;cat=snu21lp;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=\${GDPR};gdpr_consent=\${GDPR_CONSENT_755};ord=1;num=1?" width="1" height="1" alt=""/>`}</noscript>
-      </Helmet>
       <ScrollToTop />
       <div className="main">
         <Switch>
