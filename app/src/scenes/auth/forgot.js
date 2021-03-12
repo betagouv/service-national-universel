cf784a15fa5a8b619b16a5374304a5538bbc71b6
tangimds <tangimds@gmail.com>
2021-03-12 12:21:07 +0100

    fix : forgot and reset screen

--- app/src/scenes/auth/forgot.js
+++ app/src/scenes/auth/forgot.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import { FormGroup } from "reactstrap";
 import { Formik, Field } from "formik";
+import styled from "styled-components";
 import classnames from "classnames";
 import validator from "validator";
 import { toastr } from "react-redux-toastr";
-import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-import LoadingButton from "../../components/loadingButton";
 import api from "../../services/api";
+import LoadingButton from "../../components/loadingButton";
+import Header from "./components/header";
 import { translate } from "../../utils";
 
 export default () => {
@@ -16,168 +17,175 @@ export default () => {
   const [mail, setMail] = useState();
 
   return (
-    <AuthWrapper>
-      <ForgotBox>
-        <Logos>
-          <Link to="/">
-            <img src={require("../../assets/small-logo.svg")} height={22} />
-          </Link>
-          <Link to="/">
-            <img src={require("../../assets/logo-snu.png")} height={62} />
-          </Link>
-        </Logos>
-        {done ? (
-          <>
-            <Title>
-              E-mail <span>envoyé</span>
-            </Title>
-            <Subtitle>Un email de réinitialisation de mot de passe vous a été envoyé !</Subtitle>
-            <Subtitle>
-              <span>{mail}</span>
-            </Subtitle>
-            <Description>Cet e-mail contient un lien permettant de réinitialiser ton mot de passe.</Description>
-            <Link to="/auth">
-              <BackButton>Retourner à la connexion</BackButton>
-            </Link>
-          </>
-        ) : (
-          <>
-            <Title>
-              Mon <span>Espace volontaire</span>
-            </Title>
-            <Subtitle>Récupérer mon mot de passe</Subtitle>
-            <Formik
-              initialValues={{ email: "" }}
-              onSubmit={async (values, actions) => {
-                try {
-                  await api.post("/young/forgot_password", values);
-                  toastr.success("E-mail envoyé !");
-                  setMail(values.email);
-                  setDone(true);
-                } catch (e) {
-                  toastr.error("Erreur !", translate(e.code));
-                }
-                actions.setSubmitting(false);
-              }}
-            >
-              {({ values, errors, isSubmitting, handleChange, handleSubmit }) => {
-                return (
-                  <form onSubmit={handleSubmit}>
-                    <StyledFormGroup>
-                      <div>
-                        <InputField
-                          validate={(v) => !validator.isEmail(v) && "Invalid email address"}
-                          className={classnames({ "has-error": errors.email })}
-                          name="email"
-                          type="email"
-                          id="email"
-                          value={values.email}
-                          onChange={handleChange}
-                        />
-                        <label htmlFor="email">Ton e-mail</label>
-                      </div>
-                      <p style={{ fontSize: 12, color: "rgb(253, 49, 49)" }}>{errors.email}</p>
-                    </StyledFormGroup>
-                    <Submit type="submit" color="success" loading={isSubmitting}>
-                      Recevoir mon mot de passe
-                    </Submit>
-                  </form>
-                );
-              }}
-            </Formik>
-          </>
-        )}
-      </ForgotBox>
-      <ThumbBox />
-    </AuthWrapper>
+    <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+      <Header />
+      <AuthWrapper>
+        <LoginBox>
+          <Title>
+            <span>Mon espace volontaire</span>
+          </Title>
+
+          {done ? (
+            <>
+              <Subtitle>Un email de réinitialisation de mot de passe vous a été envoyé !</Subtitle>
+              <Subtitle>
+                <span>{mail}</span>
+              </Subtitle>
+              <Description>Cet e-mail contient un lien permettant de réinitialiser ton mot de passe.</Description>
+            </>
+          ) : (
+            <>
+              <Subtitle>Récupérer mon mot de passe</Subtitle>
+
+              <Formik
+                initialValues={{ email: "" }}
+                onSubmit={async (values, actions) => {
+                  try {
+                    await api.post("/young/forgot_password", values);
+                    toastr.success("E-mail envoyé !");
+                    setMail(values.email);
+                    setDone(true);
+                  } catch (e) {
+                    toastr.error("Erreur !", translate(e.code));
+                  }
+                  actions.setSubmitting(false);
+                }}
+              >
+                {({ values, errors, isSubmitting, handleChange, handleSubmit }) => {
+                  return (
+                    <form onSubmit={handleSubmit}>
+                      <StyledFormGroup>
+                        <div>
+                          <InputField
+                            validate={(v) => !validator.isEmail(v) && "format incorrect"}
+                            className={classnames({ "has-error": errors.email })}
+                            name="email"
+                            type="email"
+                            id="email"
+                            value={values.email}
+                            onChange={handleChange}
+                          />
+                          <label htmlFor="email">Ton e-mail</label>
+                        </div>
+                        <p style={{ fontSize: 12, color: "rgb(253, 49, 49)" }}>{errors.email}</p>
+                      </StyledFormGroup>
+                      <Submit type="submit" color="success" loading={isSubmitting}>
+                        Recevoir mon mot de passe
+                      </Submit>
+                    </form>
+                  );
+                }}
+              </Formik>
+            </>
+          )}
+          <Title>
+            <span>Retourner à la connexion</span>
+          </Title>
+          <Register to="/auth/signin">Se connecter</Register>
+        </LoginBox>
+      </AuthWrapper>
+    </div>
   );
 };
 
 const AuthWrapper = styled.div`
+  display: flex;
   width: 100%;
-  height: 100vh;
-  background-color: #fff;
-  overflow: hidden;
-  display: flex;
-  justify-content: space-between;
-  color: #1a202c;
-`;
-const Logos = styled.div`
-  display: flex;
-  align-items: center;
-  margin-bottom: 60px;
-  img {
-    vertical-align: top;
-    margin-right: 20px;
+  height: 100%;
+  flex-grow: 2;
+  position: relative;
+  background: url(${require("../../assets/login.jpg")}) center no-repeat;
+  background-size: cover;
+  ::after {
+    content: "";
+    display: block;
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: rgba(63, 54, 148, 0.95);
+    z-index: 1;
+  }
+  > * {
+    position: relative;
+    z-index: 2;
   }
 `;
 
-const ForgotBox = styled.div`
-  font-family: Arial, Helvetica, sans-serif;
-  max-width: 600px;
-  padding: 15px 40px;
-`;
-
-const ThumbBox = styled.div`
-  flex: 0 0 50%;
-  max-width: 50%;
-  background: url(${require("../../assets/login.jpg")}) center no-repeat;
-  background-size: cover;
-`;
-
-const Title = styled.h1`
-  font-size: 46px;
+const Title = styled.h2`
+  position: relative;
+  text-align: center;
+  font-size: 1rem;
+  @media (max-width: 768px) {
+    font-size: 0.8rem;
+  }
   font-weight: 700;
-  margin-bottom: 10px;
+  margin-bottom: 20px;
+  ::after {
+    content: "";
+    display: block;
+    height: 1px;
+    width: 100%;
+    background-color: #d2d6dc;
+    position: absolute;
+    left: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    z-index: -1;
+  }
   span {
-    color: #434190;
+    padding: 0 10px;
+    background-color: #fff;
+    color: rgb(22, 30, 46);
   }
 `;
 
 const Subtitle = styled.h2`
-  font-size: 36px;
-  font-weight: 700;
-  line-height: 1;
-  margin-bottom: 20px;
-  span {
-    color: #434190;
+  text-align: center;
+  font-size: 1.2rem;
+  @media (max-width: 768px) {
+    font-size: 1rem;
   }
-`;
-
-const Description = styled.p`
-  font-size: 24px;
-  font-weight: 400;
-  line-height: 1;
+  font-weight: 500;
   margin-bottom: 20px;
 `;
 
-const Submit = styled(LoadingButton)`
+const LoginBox = styled.div`
+  max-width: 450px;
   width: 100%;
-  display: block;
-  font-size: 18px;
-  border-radius: 0;
-  padding: 15px;
-  border: 1px solid #434190;
-  background-color: #434190;
-  margin-top: 30px;
-  :hover {
-    background-color: #5a67d8;
-  }
-  :focus {
-    border: 1px solid rgba(66, 153, 225, 0.5);
-    box-shadow: 0 0 0 3px rgba(66, 153, 225, 0.5);
+  padding: 40px 30px 20px;
+  border-radius: 8px;
+  margin: auto;
+  background-color: #fff;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 3px 0px, rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;
+  @media (max-width: 768px) {
+    border-radius: 0;
+    margin: 0;
   }
 `;
 
+const StyledFormGroup = styled(FormGroup)`
+  margin-bottom: 20px;
+  div {
+    display: flex;
+    flex-direction: column-reverse;
+  }
+  label {
+    color: #37415b;
+    font-size: 14px;
+    margin-bottom: 5px;
+    cursor: pointer;
+  }
+`;
 const InputField = styled(Field)`
   background-color: transparent;
   outline: 0;
   display: block;
   width: 100%;
-  padding: 15px;
-  margin-top: 3px;
-  margin-bottom: 0;
+  padding: 12px 20px;
   border: 1px solid #e2e8f0;
+  border-radius: 5px;
   color: #798fb0;
   -webkit-transition: border 0.2s ease;
   transition: border 0.2s ease;
@@ -188,7 +196,6 @@ const InputField = styled(Field)`
   &:focus {
     outline: none;
     border: 1px solid rgba(66, 153, 225, 0.5);
-    box-shadow: 0 0 0 3px rgba(66, 153, 225, 0.5);
     & + label {
       color: #434190;
     }
@@ -198,28 +205,50 @@ const InputField = styled(Field)`
   }
 `;
 
-const StyledFormGroup = styled(FormGroup)`
-  margin-bottom: 20px;
-  div {
-    display: flex;
-    flex-direction: column-reverse;
+const Submit = styled(LoadingButton)`
+  width: 100%;
+  display: block;
+  font-size: 18px;
+  font-weight: 700;
+  border-radius: 0;
+  padding: 12px;
+  border: 0;
+  background-color: #5145cd;
+  margin-top: 30px;
+  margin-bottom: 30px;
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
+  cursor: pointer;
+  :hover {
+    background-color: #42389d;
+  }
+  :focus {
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
   }
 `;
-
-const BackButton = styled.button`
-  color: #fff;
-  background-color: #5145cd;
-  padding: 9px 20px;
-  border: 0;
-  outline: 0;
-  border-radius: 6px;
-  font-weight: 700;
-  font-size: 20px;
-  margin: 40px auto 0;
+const Description = styled.p`
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1;
+  margin-bottom: 20px;
+`;
+const Register = styled(Link)`
+  width: 100%;
   display: block;
-  outline: 0;
-  box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
+  font-size: 18px;
+  font-weight: 700;
+  border-radius: 0;
+  padding: 12px;
+  border: 0;
+  background-color: #fff;
+  margin-top: 20px;
+  border-radius: 10px;
+  border: 1px solid #e5e7eb;
+  color: #000;
+  text-align: center;
+  cursor: pointer;
   :hover {
-    opacity: 0.9;
+    background-color: #f4f5f7;
+    color: #000;
   }
 `;
--- app/src/scenes/auth/reset.js
+++ app/src/scenes/auth/reset.js
@@ -1,144 +1,152 @@
 import React, { useState } from "react";
 import { FormGroup } from "reactstrap";
 import { Formik, Field } from "formik";
-import { toastr } from "react-redux-toastr";
 import styled from "styled-components";
+import { toastr } from "react-redux-toastr";
 import { Link, Redirect } from "react-router-dom";
-import queryString from "query-string";
 import PasswordEye from "../../components/PasswordEye";
+import queryString from "query-string";
 
-import { translate } from "../../utils";
-import LoadingButton from "../../components/loadingButton";
 import api from "../../services/api";
+import LoadingButton from "../../components/loadingButton";
+import Header from "./components/header";
+import { translate } from "../../utils";
 
 export default () => {
   const [redirect, setRedirect] = useState(false);
 
   return (
-    <AuthWrapper>
+    <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
       {redirect && <Redirect to="/auth" />}
-      <ForgotBox>
-        <Logos>
-          <Link to="/">
-            <img src={require("../../assets/small-logo.svg")} height={22} />
-          </Link>
-          <Link to="/">
-            <img src={require("../../assets/logo-snu.png")} height={62} />
-          </Link>
-        </Logos>
+      <Header />
+      <AuthWrapper>
+        <LoginBox>
+          <Title>
+            <span>Mon espace volontaire</span>
+          </Title>
 
-        <Title>
-          Mon <span>Espace volontaire</span>
-        </Title>
-        <Subtitle>Récupérer mon mot de passe</Subtitle>
-        <Formik
-          initialValues={{ password: "" }}
-          validateOnChange={false}
-          validateOnBlur={false}
-          onSubmit={async (values, actions) => {
-            try {
-              const { token } = queryString.parse(location.search);
-              const res = await api.post("/young/forgot_password_reset", { ...values, token });
-              if (!res.ok) throw res;
-              toastr.success("Mot de passe changé avec succès");
-              setRedirect(true);
-            } catch (e) {
-              return toastr.error("Une erreur s'est produite :", translate(e && e.code));
-            }
-            actions.setSubmitting(false);
-          }}
-        >
-          {({ values, errors, isSubmitting, handleChange, handleSubmit }) => {
-            return (
-              <form onSubmit={handleSubmit}>
-                {redirect && <Redirect to="/" />}
-                <StyledFormGroup>
-                  <label>Mot de passe</label>
-                  <PasswordEye value={values.password} onChange={handleChange} />
-                  <p style={{ fontSize: 12, color: "rgb(253, 49, 49)" }}>{errors.password}</p>
-                  <p>👉 Il doit contenir au moins 8 caractères, dont une majuscule, une minuscule, un chiffre et un symbole</p>
-                </StyledFormGroup>
-                <div className="button">
-                  <Submit loading={isSubmitting} type="submit" color="primary" disabled={isSubmitting}>
-                    Confirmer
-                  </Submit>
-                </div>
-              </form>
-            );
-          }}
-        </Formik>
-      </ForgotBox>
-      <ThumbBox />
-    </AuthWrapper>
+          <Subtitle>Récupérer mon mot de passe</Subtitle>
+
+          <Formik
+            initialValues={{ password: "" }}
+            validateOnChange={false}
+            validateOnBlur={false}
+            onSubmit={async (values, actions) => {
+              try {
+                const { token } = queryString.parse(location.search);
+                const res = await api.post("/young/forgot_password_reset", { ...values, token });
+                if (!res.ok) throw res;
+                toastr.success("Mot de passe changé avec succès");
+                setRedirect(true);
+              } catch (e) {
+                return toastr.error("Une erreur s'est produite :", translate(e && e.code));
+              }
+              actions.setSubmitting(false);
+            }}
+          >
+            {({ values, errors, isSubmitting, handleChange, handleSubmit }) => {
+              return (
+                <form onSubmit={handleSubmit}>
+                  {redirect && <Redirect to="/" />}
+                  <StyledFormGroup>
+                    <label>Mot de passe</label>
+                    <PasswordEye value={values.password} onChange={handleChange} />
+                    <p style={{ fontSize: 12, color: "rgb(253, 49, 49)" }}>{errors.password}</p>
+                    <p>👉 Il doit contenir au moins 8 caractères, dont une majuscule, une minuscule, un chiffre et un symbole</p>
+                  </StyledFormGroup>
+                  <div className="button">
+                    <Submit loading={isSubmitting} type="submit" color="primary" disabled={isSubmitting}>
+                      Confirmer
+                    </Submit>
+                  </div>
+                </form>
+              );
+            }}
+          </Formik>
+          <Title>
+            <span>Retourner à la connexion</span>
+          </Title>
+          <Register to="/auth/signin">Se connecter</Register>
+        </LoginBox>
+      </AuthWrapper>
+    </div>
   );
 };
 
 const AuthWrapper = styled.div`
+  display: flex;
   width: 100%;
-  height: 100vh;
-  background-color: #fff;
-  overflow: hidden;
-  display: flex;
-  justify-content: space-between;
-  color: #1a202c;
-`;
-const Logos = styled.div`
-  display: flex;
-  align-items: center;
-  margin-bottom: 60px;
-  img {
-    vertical-align: top;
-    margin-right: 20px;
+  height: 100%;
+  flex-grow: 2;
+  position: relative;
+  background: url(${require("../../assets/login.jpg")}) center no-repeat;
+  background-size: cover;
+  ::after {
+    content: "";
+    display: block;
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: rgba(63, 54, 148, 0.95);
+    z-index: 1;
+  }
+  > * {
+    position: relative;
+    z-index: 2;
   }
 `;
 
-const ForgotBox = styled.div`
-  font-family: Arial, Helvetica, sans-serif;
-  max-width: 600px;
-  padding: 15px 40px;
-`;
-
-const ThumbBox = styled.div`
-  flex: 0 0 50%;
-  max-width: 50%;
-  background: url(${require("../../assets/login.jpg")}) center no-repeat;
-  background-size: cover;
-`;
-
-const Title = styled.h1`
-  font-size: 46px;
+const Title = styled.h2`
+  position: relative;
+  text-align: center;
+  font-size: 1rem;
+  @media (max-width: 768px) {
+    font-size: 0.8rem;
+  }
   font-weight: 700;
-  margin-bottom: 10px;
+  margin-bottom: 20px;
+  ::after {
+    content: "";
+    display: block;
+    height: 1px;
+    width: 100%;
+    background-color: #d2d6dc;
+    position: absolute;
+    left: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    z-index: -1;
+  }
   span {
-    color: #434190;
+    padding: 0 10px;
+    background-color: #fff;
+    color: rgb(22, 30, 46);
   }
 `;
 
 const Subtitle = styled.h2`
-  font-size: 36px;
-  font-weight: 700;
-  line-height: 1;
-  margin-bottom: 20px;
-  span {
-    color: #434190;
+  text-align: center;
+  font-size: 1.2rem;
+  @media (max-width: 768px) {
+    font-size: 1rem;
   }
+  font-weight: 500;
+  margin-bottom: 20px;
 `;
 
-const Submit = styled(LoadingButton)`
+const LoginBox = styled.div`
+  max-width: 450px;
   width: 100%;
-  display: block;
-  font-size: 18px;
-  border-radius: 0;
-  padding: 15px;
-  border: 1px solid #434190;
-  background-color: #434190;
-  margin-top: 30px;
-  :hover {
-    background-color: #5a67d8;
-  }
-  :focus {
-    border: 1px solid rgba(66, 153, 225, 0.5);
-    box-shadow: 0 0 0 3px rgba(66, 153, 225, 0.5);
+  padding: 40px 30px 20px;
+  border-radius: 8px;
+  margin: auto;
+  background-color: #fff;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 3px 0px, rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;
+  @media (max-width: 768px) {
+    border-radius: 0;
+    margin: 0;
   }
 `;
 
@@ -147,5 +155,54 @@ const StyledFormGroup = styled(FormGroup)`
   div {
     display: flex;
     flex-direction: column-reverse;
+  }
+  label {
+    color: #37415b;
+    font-size: 14px;
+    margin-bottom: 5px;
+    cursor: pointer;
+  }
+`;
+
+const Submit = styled(LoadingButton)`
+  width: 100%;
+  display: block;
+  font-size: 18px;
+  font-weight: 700;
+  border-radius: 0;
+  padding: 12px;
+  border: 0;
+  background-color: #5145cd;
+  margin-top: 30px;
+  margin-bottom: 30px;
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
+  cursor: pointer;
+  :hover {
+    background-color: #42389d;
+  }
+  :focus {
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
+  }
+`;
+
+const Register = styled(Link)`
+  width: 100%;
+  display: block;
+  font-size: 18px;
+  font-weight: 700;
+  border-radius: 0;
+  padding: 12px;
+  border: 0;
+  background-color: #fff;
+  margin-top: 20px;
+  border-radius: 10px;
+  border: 1px solid #e5e7eb;
+  color: #000;
+  text-align: center;
+  cursor: pointer;
+  :hover {
+    background-color: #f4f5f7;
+    color: #000;
   }
 `;
