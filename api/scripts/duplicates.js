52d7eb32f642362fbaab88a6328961259a64d43d
Raphaël Huchet <raph@selego.co>
2021-03-15 11:43:36 +0100

    feat: add duplicate script

---
+++ api/scripts/duplicates.js
@@ -0,0 +1,28 @@
+require("dotenv").config({ path: "../.env-prod" });
+
+require("../src/mongo");
+const YoungModel = require("../src/models/young");
+
+(async function run() {
+  const buffer = [];
+  console.log("lets go");
+  for await (const doc of YoungModel.find({}).cursor()) {
+    buffer.push({
+      name: `${doc.firstName} ${doc.lastName}`,
+      email: doc.email,
+      birthdate: doc.birthdateAt && doc.birthdateAt.toISOString(),
+      status: doc.status,
+    });
+  }
+  console.log("step done");
+  const similarFound = [];
+  buffer.forEach((b) => {
+    const similar = buffer.filter((a) => a.name === b.name && a.birthdate === b.birthdate);
+    const alreadyFound = similarFound.find((a) => a.name === b.name && a.birthdate === b.birthdate);
+    if (!alreadyFound && similar.length > 1) {
+      similarFound.push(b);
+      console.log(`Similar found for ${b.name} (${similar.length}), emails: ${similar.map((e) => `${e.email} (${e.status})`).join()}`);
+    }
+  });
+  process.exit(0);
+})();
