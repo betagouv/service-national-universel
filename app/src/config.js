7745bf539206a26f22048387c7b1da30ca5018f5
tangimds <tangimds@gmail.com>
2021-03-22 11:19:11 +0100

    hotfix

--- app/src/config.js
+++ app/src/config.js
@@ -24,6 +24,4 @@ function getEnvironment() {
   return "production";
 }
 
-const LUMIERE_APP_ID = "app_Pemqr1RIvcJ9lVNp_zeVF";
-
 export { apiURL, S3PREFIX, SENTRY_URL, environment, franceConnectUrl, LUMIERE_APP_ID };
