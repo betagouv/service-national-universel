e89f5873aa26f211b1a95c82199ac0203c813bc2
Raphaël Huchet <raph@selego.co>
2021-03-31 16:23:06 +0200

    chore: change password policy

--- api/src/utils.js
+++ api/src/utils.js
@@ -54,17 +54,7 @@ function fileExist(url) {
 
 function validatePassword(password) {
   const schema = new passwordValidator();
-  schema
-    .is()
-    .min(8) // Minimum length 8
-    .has()
-    .uppercase() // Must have uppercase letters
-    .has()
-    .lowercase() // Must have lowercase letters
-    .has()
-    .digits() // Must have digits
-    .has()
-    .symbols(); // Must have symbols
+  schema.is().min(8); // Minimum length 8
 
   return schema.validate(password);
 }
--- app/src/utils/index.js
+++ app/src/utils/index.js
@@ -9,21 +9,11 @@ export * from "./colors";
 export function getPasswordErrorMessage(v, matomo) {
   if (!v) return "Ce champ est obligatoire";
   const schema = new passwordValidator();
-  schema
-    .is()
-    .min(8) // Minimum length 8
-    .has()
-    .uppercase() // Must have uppercase letters
-    .has()
-    .lowercase() // Must have lowercase letters
-    .has()
-    .digits() // Must have digits
-    .has()
-    .symbols(); // Must have symbols
+  schema.is().min(8); // Must have symbols
 
   if (!schema.validate(v)) {
     matomo.logEvent("inscription", "password_failed");
     window.lumiere("sendEvent", "inscription", "password_failed");
-    return "Votre mot de passe doit contenir au moins 8 caractères, dont une majuscule, une minuscule, un chiffre et un symbole";
+    return "Votre mot de passe doit contenir au moins 8 caractères";
   }
 }
