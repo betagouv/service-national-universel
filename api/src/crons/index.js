1a489e472139dcb011a9b650cc369241fb6cd9bd
tangimds <tangimds@gmail.com>
2021-04-06 14:15:46 +0200

    fix

--- api/src/crons/index.js
+++ api/src/crons/index.js
@@ -29,7 +29,7 @@ if (ENVIRONMENT === "production") {
   });
 
   // everyday at 0200
-  cron.schedule("10 12 * * *", () => {
+  cron.schedule("0 13 * * *", () => {
     apiEngagement.handler();
   });
 }
