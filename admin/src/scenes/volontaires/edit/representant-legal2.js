c8e8c7219f159c4f97accea9b50a2e94c140ed39
tangimds <tangimds@gmail.com>
2021-04-13 09:51:17 +0200

    representant 2

--- admin/src/scenes/volontaires/edit/representant-legal2.js
+++ admin/src/scenes/volontaires/edit/representant-legal2.js
@@ -13,58 +13,50 @@ export default ({ values, handleChange }) => (
     <Box>
       <BoxTitle>Représentant Légal n°2</BoxTitle>
       <BoxContent direction="column">
-        {values.parent2Status ? (
-          <>
-            <Select
-              name="parent2Status"
-              values={values}
-              handleChange={handleChange}
-              title="Statut"
-              options={[
-                { value: "mother", label: "Mère" },
-                { value: "father", label: "Père" },
-                { value: "representant", label: "Représentant légal" },
-              ]}
-            />
-            <Item title="Prénom" values={values} name="parent2FirstName" handleChange={handleChange} />
-            <Item title="Nom" values={values} name="parent2LastName" handleChange={handleChange} />
-            <Item title="E-mail" values={values} name="parent2Email" handleChange={handleChange} />
-            <Item title="Tél." values={values} name="parent2Phone" handleChange={handleChange} />
-            <Select
-              name="parent2OwnAddress"
-              values={values}
-              handleChange={handleChange}
-              title="Adresse differente de celle du volontaire"
-              options={[
-                { value: "true", label: "Oui" },
-                { value: "false", label: "Non" },
-              ]}
-            />
-            <Item disabled={values.parent2OwnAddress !== "true"} title="Adresse" values={values} name="parent2Address" handleChange={handleChange} />
-            <Item disabled={values.parent2OwnAddress !== "true"} title="Ville" values={values} name="parent2City" handleChange={handleChange} />
-            <Item disabled={values.parent2OwnAddress !== "true"} title="Code Postal" values={values} name="parent2Zip" handleChange={handleChange} />
-            <Select
-              disabled={values.parent2OwnAddress !== "true"}
-              name="parent2Department"
-              values={values}
-              handleChange={handleChange}
-              title="Département"
-              options={departmentList.map((d) => ({ value: d, label: d }))}
-            />
-            <Select
-              disabled={values.parent2OwnAddress !== "true"}
-              name="parent2Region"
-              values={values}
-              handleChange={handleChange}
-              title="Région"
-              options={regionList.map((r) => ({ value: r, label: r }))}
-            />
-          </>
-        ) : (
-          <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-            <div className="quote">Non renseigné</div>
-          </div>
-        )}
+        <Select
+          name="parent2Status"
+          values={values}
+          handleChange={handleChange}
+          title="Statut"
+          options={[
+            { value: "mother", label: "Mère" },
+            { value: "father", label: "Père" },
+            { value: "representant", label: "Représentant légal" },
+          ]}
+        />
+        <Item title="Prénom" values={values} name="parent2FirstName" handleChange={handleChange} />
+        <Item title="Nom" values={values} name="parent2LastName" handleChange={handleChange} />
+        <Item title="E-mail" values={values} name="parent2Email" handleChange={handleChange} />
+        <Item title="Tél." values={values} name="parent2Phone" handleChange={handleChange} />
+        <Select
+          name="parent2OwnAddress"
+          values={values}
+          handleChange={handleChange}
+          title="Adresse differente de celle du volontaire"
+          options={[
+            { value: "true", label: "Oui" },
+            { value: "false", label: "Non" },
+          ]}
+        />
+        <Item disabled={values.parent2OwnAddress !== "true"} title="Adresse" values={values} name="parent2Address" handleChange={handleChange} />
+        <Item disabled={values.parent2OwnAddress !== "true"} title="Ville" values={values} name="parent2City" handleChange={handleChange} />
+        <Item disabled={values.parent2OwnAddress !== "true"} title="Code Postal" values={values} name="parent2Zip" handleChange={handleChange} />
+        <Select
+          disabled={values.parent2OwnAddress !== "true"}
+          name="parent2Department"
+          values={values}
+          handleChange={handleChange}
+          title="Département"
+          options={departmentList.map((d) => ({ value: d, label: d }))}
+        />
+        <Select
+          disabled={values.parent2OwnAddress !== "true"}
+          name="parent2Region"
+          values={values}
+          handleChange={handleChange}
+          title="Région"
+          options={regionList.map((r) => ({ value: r, label: r }))}
+        />
       </BoxContent>
     </Box>
   </Col>
