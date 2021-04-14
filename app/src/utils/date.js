d58ce1893d811b4aedf2409bd16aa7add8b70ffc
Raphaël Huchet <raph@selego.co>
2021-04-14 16:44:19 +0200

    fix: remove unused files

--- app/src/utils/date.js
+++
@@ -1,5 +0,0 @@
-export const formatStringDate = (date) => {
-  if (!date) return "-";
-  const d = new Date(date);
-  return d.toLocaleDateString("fr-FR", { year: "numeric", month: "long", day: "numeric" });
-};
