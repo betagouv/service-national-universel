0eec8f845b278d836f7adc0edd5f5664c474f99d
Raphaël Huchet <raph@selego.co>
2021-04-20 17:57:44 +0200

    chore: try to deploy

--- admin/src/scenes/dashboard/inscription/gender.js
+++ admin/src/scenes/dashboard/inscription/gender.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { Col, Row } from "reactstrap";
 
 import CircularProgress from "../components/CircularProgress";
 
