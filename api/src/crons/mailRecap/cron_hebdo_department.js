e2157a9edcf30ee18dd3f5448f2b692a04fc25bd
Raphaël Huchet <raph@selego.co>
2021-04-20 18:27:01 +0200

    refactor: code complexity in cron hebdo

--- api/src/crons/mailRecap/cron_hebdo_department.js
+++ api/src/crons/mailRecap/cron_hebdo_department.js
@@ -4,7 +4,7 @@ const path = require("path");
 require("../../mongo");
 const { sendEmail } = require("../../sendinblue");
 const ReferentModel = require("../../models/referent");
-const { departmentListTest, departmentList, getDataInscriptions, getDataStructure } = require("../utils");
+const { departmentList, getDataInscriptions, getDataStructure } = require("../utils");
 const { capture } = require("../../sentry");
 
 const arr = departmentList;
@@ -18,31 +18,30 @@ async function sendRecapDepartmentTuesday() {
     const department = arr[i];
     console.log("> start department :", department);
     const referents = await ReferentModel.find({ role: "referent_department", department });
-    if (referents && referents.length) {
-      console.log(referents.length, "referent(s) found");
-      const dataInscriptions = await getDataInscriptions({ department, days: 5 });
-      console.log(dataInscriptions);
-      const dataStructure = await getDataStructure({ department, days: 5 });
-      console.log(dataStructure);
+    if (!(referents && referents.length)) continue;
+    console.log(referents.length, "referent(s) found");
+    const dataInscriptions = await getDataInscriptions({ department, days: 5 });
+    console.log(dataInscriptions);
+    const dataStructure = await getDataStructure({ department, days: 5 });
+    console.log(dataStructure);
 
-      for (let j = 0; j < referents.length; j++) {
-        const ref = referents[j];
-        let htmlContent = htmlContentDefault;
-        let toName = `${ref.firstName} ${ref.lastName}`;
-        let email = ref.email;
-        htmlContent = htmlContent.replace(/{{sectionTitle}}/g, "Depuis jeudi dernier, il y a...");
-        htmlContent = htmlContent.replace(/{{cta}}/g, "https://admin.snu.gouv.fr");
-        htmlContent = htmlContent.replace(/{{toName}}/g, toName);
-        htmlContent = htmlContent.replace(/{{department}}/g, department);
-        htmlContent = htmlContent.replace(/{{new_inscription_waiting_validation}}/g, dataInscriptions.new_inscription_waiting_validation);
-        htmlContent = htmlContent.replace(/{{new_inscription_validated}}/g, dataInscriptions.new_inscription_validated);
-        htmlContent = htmlContent.replace(/{{new_structure}}/g, dataStructure.new_structure);
-        htmlContent = htmlContent.replace(/{{inscription_waiting_validation}}/g, dataInscriptions.inscription_waiting_validation);
-        htmlContent = htmlContent.replace(/{{inscription_validated}}/g, dataInscriptions.inscription_validated);
-        await sendEmail({ name: toName, email: email }, subject, htmlContent);
-        count++;
-        console.log("recap hebdo", department, "sent to :", email);
-      }
+    for (let j = 0; j < referents.length; j++) {
+      const ref = referents[j];
+      let toName = `${ref.firstName} ${ref.lastName}`;
+      let email = ref.email;
+      const htmlContent = htmlContentDefault
+        .replace(/{{sectionTitle}}/g, "Depuis jeudi dernier, il y a...")
+        .replace(/{{cta}}/g, "https://admin.snu.gouv.fr")
+        .replace(/{{toName}}/g, toName)
+        .replace(/{{department}}/g, department)
+        .replace(/{{new_inscription_waiting_validation}}/g, dataInscriptions.new_inscription_waiting_validation)
+        .replace(/{{new_inscription_validated}}/g, dataInscriptions.new_inscription_validated)
+        .replace(/{{new_structure}}/g, dataStructure.new_structure)
+        .replace(/{{inscription_waiting_validation}}/g, dataInscriptions.inscription_waiting_validation)
+        .replace(/{{inscription_validated}}/g, dataInscriptions.inscription_validated);
+      await sendEmail({ name: toName, email: email }, subject, htmlContent);
+      count++;
+      console.log("recap hebdo", department, "sent to :", email);
     }
   }
   capture(`RECAP DEPARTEMENT SENT : ${count} mails`);
@@ -56,31 +55,30 @@ async function sendRecapDepartmentThursday() {
     const department = arr[i];
     console.log("> start department :", department);
     const referents = await ReferentModel.find({ role: "referent_department", department });
-    if (referents && referents.length) {
-      console.log(referents.length, "referent(s) found");
-      const dataInscriptions = await getDataInscriptions({ department, days: 2 });
-      console.log(dataInscriptions);
-      const dataStructure = await getDataStructure({ department, days: 2 });
-      console.log(dataStructure);
+    if (!(referents && referents.length)) continue;
+    console.log(referents.length, "referent(s) found");
+    const dataInscriptions = await getDataInscriptions({ department, days: 2 });
+    console.log(dataInscriptions);
+    const dataStructure = await getDataStructure({ department, days: 2 });
+    console.log(dataStructure);
 
-      for (let j = 0; j < referents.length; j++) {
-        const ref = referents[j];
-        let htmlContent = htmlContentDefault;
-        let toName = `${ref.firstName} ${ref.lastName}`;
-        let email = ref.email;
-        htmlContent = htmlContent.replace(/{{sectionTitle}}/g, "Depuis mardi, il y a...");
-        htmlContent = htmlContent.replace(/{{cta}}/g, "https://admin.snu.gouv.fr");
-        htmlContent = htmlContent.replace(/{{toName}}/g, toName);
-        htmlContent = htmlContent.replace(/{{department}}/g, department);
-        htmlContent = htmlContent.replace(/{{new_inscription_waiting_validation}}/g, dataInscriptions.new_inscription_waiting_validation);
-        htmlContent = htmlContent.replace(/{{new_inscription_validated}}/g, dataInscriptions.new_inscription_validated);
-        htmlContent = htmlContent.replace(/{{new_structure}}/g, dataStructure.new_structure);
-        htmlContent = htmlContent.replace(/{{inscription_waiting_validation}}/g, dataInscriptions.inscription_waiting_validation);
-        htmlContent = htmlContent.replace(/{{inscription_validated}}/g, dataInscriptions.inscription_validated);
-        await sendEmail({ name: toName, email }, subject, htmlContent);
-        count++;
-        console.log("recap hebdo", department, "sent to :", email);
-      }
+    for (let j = 0; j < referents.length; j++) {
+      const ref = referents[j];
+      let toName = `${ref.firstName} ${ref.lastName}`;
+      let email = ref.email;
+      const htmlContent = htmlContentDefault
+        .replace(/{{sectionTitle}}/g, "Depuis mardi, il y a...")
+        .replace(/{{cta}}/g, "https://admin.snu.gouv.fr")
+        .replace(/{{toName}}/g, toName)
+        .replace(/{{department}}/g, department)
+        .replace(/{{new_inscription_waiting_validation}}/g, dataInscriptions.new_inscription_waiting_validation)
+        .replace(/{{new_inscription_validated}}/g, dataInscriptions.new_inscription_validated)
+        .replace(/{{new_structure}}/g, dataStructure.new_structure)
+        .replace(/{{inscription_waiting_validation}}/g, dataInscriptions.inscription_waiting_validation)
+        .replace(/{{inscription_validated}}/g, dataInscriptions.inscription_validated);
+      await sendEmail({ name: toName, email }, subject, htmlContent);
+      count++;
+      console.log("recap hebdo", department, "sent to :", email);
     }
   }
   capture(`RECAP DEPARTEMENT SENT : ${count} mails`);
