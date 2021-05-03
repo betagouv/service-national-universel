9960fb6edcc71ab41af0ffd94308601c1259663c
Raphaël Huchet <raph@selego.co>
2021-05-03 16:48:49 +0200

    chore: remove useless files

--- app/src/scenes/home/index.js
+++ app/src/scenes/home/index.js
@@ -4,14 +4,9 @@ import { useSelector } from "react-redux";
 import { YOUNG_STATUS } from "../../utils";
 import WaitingValidation from "./waitingValidation";
 import WaitingCorrection from "./waitingCorrection";
-import Validated from "./validated";
 import Refused from "./refused";
-import WaitingList from "./waitingList";
 import Default from "./default";
 import Withdrawn from "./withdrawn";
-
-import Cohort2019 from "./2019";
-import Cohort2020 from "./2020";
 
 export default () => {
   const young = useSelector((state) => state.Auth.young) || {};
@@ -23,10 +18,6 @@ export default () => {
     if (young.status === YOUNG_STATUS.WAITING_LIST) return <WaitingValidation />;
     if (young.status === YOUNG_STATUS.REFUSED) return <Refused />;
     return <Default />;
-    if (young.cohort === "2019") return <Cohort2019 />;
-    if (young.cohort === "2020" && young.cohesion2020Step !== "DONE") return <Cohort2020 />;
-    if (young.status === YOUNG_STATUS.VALIDATED) return <Validated />;
-    return <div />;
   };
 
   return renderStep();
