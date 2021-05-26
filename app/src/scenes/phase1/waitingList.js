5caac9f0d926a5effa1a8f3de2c167d773ee3c00
tangimds <tangimds@gmail.com>
2021-05-26 17:29:41 +0200

    Update waitingList.js

--- app/src/scenes/phase1/waitingList.js
+++ app/src/scenes/phase1/waitingList.js
@@ -1,25 +1,28 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+
 import { HeroContainer, Hero } from "../../components/Content";
 import NextStep from "./nextStep";
 
 export default () => {
+  const young = useSelector((state) => state.Auth.young);
+
   return (
     <>
       <HeroContainer>
         <Hero>
           <div className="content">
             <h1>
-              <strong>Mon séjour de cohésion</strong>
+              <strong>{young.firstName}</strong>, bienvenue dans votre espace personnel.
             </h1>
             <p>
-              Le SNU vous donne l'opportunité de découvrir la vie collective au sein d'un centre accueillant environ 200 jeunes de votre région pour créer ainsi des liens nouveaux
-              et développer votre culture de l’engagement et ainsi affirmer votre place dans la société.
+              <b>Les inscriptions au SNU édition 2021 sont victimes de leur succès et nous tentons de trouver des places pour tous les candidats.</b>
             </p>
+            <p>L’administration du SNU vous recontactera au plus vite pour vous informer de votre participation au séjour du 21 juin au 2 juillet 2021 .</p>
           </div>
           <div className="thumb" />
         </Hero>
       </HeroContainer>
-      <NextStep />
     </>
   );
 };
