0c9835776bfc8e08c04d674b6aaa28a94d6a25e8
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2021-06-07 14:30:23 +0200

    refactor(app): webpack 5 (#206)

--- app/src/index.js
+++ app/src/index.js
@@ -1,3 +1,5 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
@@ -13,7 +15,6 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
