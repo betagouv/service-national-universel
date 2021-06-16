d5f4c3c83ff4f6f7096f22b685136c15046f5575
hugo3m <58819009+hugo3m@users.noreply.github.com>
2021-06-16 17:40:13 +0200

    up

--- api/src/templates/contractPhase2/index.js
+++ api/src/templates/contractPhase2/index.js
@@ -14,56 +14,11 @@ const render = async (contract) => {
     html = html.replace(/{{CONTRACT_ID}}/g, contract._id).replace(/{{BASE_URL}}/g, getBaseUrl());
     html = addParents(html, contract, "isYoungAdult");
     html = addSignature(html, contract, "status");
-    html = replaceField(html, contract, "projectManagerFirstName");
-    html = replaceField(html, contract, "projectManagerLastName");
-    html = replaceField(html, contract, "projectManagerRole");
-    html = replaceField(html, contract, "projectManagerEmail");
-    html = replaceField(html, contract, "structureManagerFirstName");
-    html = replaceField(html, contract, "structureManagerLastName");
-    html = replaceField(html, contract, "structureManagerRole");
-    html = replaceField(html, contract, "structureManagerEmail");
-    html = replaceField(html, contract, "structureSiret");
-    html = replaceField(html, contract, "youngFirstName");
-    html = replaceField(html, contract, "youngLastName");
+    html = replaceDate(html, contract, "date");
     html = replaceDate(html, contract, "youngBirthdate");
-    html = replaceField(html, contract, "youngAddress");
-    html = replaceField(html, contract, "youngCity");
-    html = replaceField(html, contract, "youngDepartment");
-    html = replaceField(html, contract, "youngEmail");
-    html = replaceField(html, contract, "youngPhone");
-    html = replaceField(html, contract, "missionName");
-    html = replaceField(html, contract, "missionObjective");
-    html = replaceField(html, contract, "missionAction");
     html = replaceDate(html, contract, "missionStartAt");
     html = replaceDate(html, contract, "missionEndAt");
-    html = replaceField(html, contract, "missionDuration");
-    html = replaceField(html, contract, "missionAddress");
-    html = replaceField(html, contract, "missionCity");
-    html = replaceField(html, contract, "missionZip");
-    html = replaceField(html, contract, "missionFrequence");
-    html = replaceField(html, contract, "structureName");
-    html = replaceField(html, contract, "structureManagerFirstName");
-    html = replaceField(html, contract, "structureManagerLastName");
-    html = replaceDate(html, contract, "date");
-    html = replaceField(html, contract, "parent1FirstName");
-    html = replaceField(html, contract, "parent1LastName");
-    html = replaceField(html, contract, "parent1Address");
-    html = replaceField(html, contract, "parent1City");
-    html = replaceField(html, contract, "parent1Department");
-    html = replaceField(html, contract, "parent1Email");
-    html = replaceField(html, contract, "parent1Phone");
-    html = replaceField(html, contract, "parent2FirstName");
-    html = replaceField(html, contract, "parent2LastName");
-    html = replaceField(html, contract, "parent2Address");
-    html = replaceField(html, contract, "parent2City");
-    html = replaceField(html, contract, "parent2Department");
-    html = replaceField(html, contract, "parent2Email");
-    html = replaceField(html, contract, "parent2Phone");
-    html = replaceField(html, contract, "projectManagerStatus");
-    html = replaceField(html, contract, "structureManagerStatus");
-    html = replaceField(html, contract, "youngContractStatus");
-    html = replaceField(html, contract, "parent1Status");
-    html = replaceField(html, contract, "parent2Status");
+    html = replaceField(html, contract._doc);
     return html;
   } catch (e) {
     throw e;
@@ -73,25 +28,25 @@ const render = async (contract) => {
 const addParents = (str, context, field) => {
   const regex = new RegExp("{{" + field + "}}", "g");
   let content = "";
-  if (context.isYoungAdult == "false") {
+  if (context.isYoungAdult === "false") {
     content = `
       <h2>Représenté par ses représentant légaux</h2>
       <div>
         1) Le représentant légal du volontaire n°1 disposant de l’autorité parentale :
         <div class="field" name="parent1Name" placeholder="Prénom" context="{context}">
-          <p class="attributes">{{parent1LastName}} {{parent1FirstName}}</p>
+          <p class="attributes">${context.parent1LastName} ${context.parent1FirstName}</p>
         </div>
         <div class="field" name="parent1Address" placeholder="Adresse" className="md" context="{context}">
           demeurant :
-          <p class="attributes">{{parent1Address}} {{parent1City}} {{parent1Department}}</p>
+          <p class="attributes">{{parent1Address}} ${context.parent1City} ${context.parent1Department}</p>
         </div>
         <div class="field" name="parent1Email" placeholder="Email" className="md" type="email" context="{context}">
           Email :
-          <p class="attributes">{{parent1Email}}</p>
+          <p class="attributes">${context.parent1Email}</p>
         </div>
         <div class="field" name="parent1Phone" placeholder="0123456789" className="md" context="{context}">
           Téléphone :
-          <p class="attributes">{{parent1Phone}}</p>
+          <p class="attributes">${context.parent1Phone}</p>
         </div>
       </div>
     `;
@@ -100,19 +55,19 @@ const addParents = (str, context, field) => {
       <div>
         1) Le représentant légal du volontaire n°2 disposant de l’autorité parentale :
         <div class="field" name="parent2Name" placeholder="Prénom" context="{context}">
-          <p class="attributes">{{parent2LastName}} {{parent2FirstName}}</p>
+          <p class="attributes">${context.parent2LastName} ${context.parent2FirstName}</p>
         </div>
         <div class="field" name="parent2Address" placeholder="Adresse" className="md" context="{context}">
           demeurant :
-          <p class="attributes">{{parent2Address}} {{parent2City}} {{parent2Department}}</p>
+          <p class="attributes">${context.parent2Address} ${context.parent2City} ${context.parent2Department}</p>
         </div>
         <div class="field" name="parent2Email" placeholder="Email" className="md" type="email" context="{context}">
           Email :
-          <p class="attributes">{{parent2Email}}</p>
+          <p class="attributes">${context.parent2Email}</p>
         </div>
         <div class="field" name="parent2Phone" placeholder="0123456789" className="md" context="{context}">
           Téléphone :
-          <p class="attributes">{{parent2Phone}}</p>
+          <p class="attributes">${context.parent2Phone}</p>
         </div>
       </div>
       `;
@@ -120,9 +75,13 @@ const addParents = (str, context, field) => {
   return str.replace(regex, content);
 };
 
-const replaceField = (str, context, field) => {
-  const regex = new RegExp("{{" + field + "}}", "g");
-  return str.replace(regex, context[field]);
+const replaceField = (str, context) => {
+  Object.keys(context).forEach((key) => {
+    console.log(key);
+    const regex = new RegExp("{{" + key + "}}", "g");
+    str = str.replace(regex, context[key]);
+  });
+  return str;
 };
 
 const badgeSignature = (status) => {
