7c4af7c13dc46b654e0e4db9380e09eb9776c9c9
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2021-06-29 09:30:53 +0200

    refactor: downloads (#306)
    * wip
    
    * fix
    
    * hop

--- admin/src/components/buttons/DownloadAttestationButton.js
+++ admin/src/components/buttons/DownloadAttestationButton.js
@@ -1,28 +1,16 @@
-import React, { useEffect, useState } from "react";
-import { Spinner } from "reactstrap";
-import styled from "styled-components";
-import api from "../../services/api";
-import { environment } from "../../config";
+import React, { useState } from "react";
 import LoadingButton from "./LoadingButton";
+import downloadPDF from "../../utils/download-pdf";
 
 export default ({ young, children, disabled, uri, ...rest }) => {
   const [loading, setLoading] = useState();
 
   const viewAttestation = async (a) => {
     setLoading(true);
-    const file = await api.openpdf(`/young/${young._id}/certificate/${a}`, { options: { landscape: true } });
-    const fileName = `${young.firstName} ${young.lastName} - attestation ${a}.pdf`;
-    if (window.navigator.msSaveOrOpenBlob) {
-      //IE11 & Edge
-      window.navigator.msSaveOrOpenBlob(file, fileName);
-    } else {
-      //Other browsers
-      const a = document.createElement("a");
-      document.body.appendChild(a);
-      a.href = URL.createObjectURL(file);
-      a.download = fileName;
-      a.click();
-    }
+    await downloadPDF({
+      url: `/young/${young._id}/certificate/${a}`,
+      fileName: `${young.firstName} ${young.lastName} - attestation ${a}.pdf`,
+    });
     setLoading(false);
   };
   return (
--- app/src/components/buttons/DownloadFormButton.js
+++ app/src/components/buttons/DownloadFormButton.js
@@ -1,38 +1,18 @@
 import React, { useState } from "react";
 import { Spinner } from "reactstrap";
-import { toastr } from "react-redux-toastr";
-import * as Sentry from "@sentry/browser";
-
-import api from "../../services/api";
+import downloadPDF from "../../utils/download-pdf";
 
 export default ({ young, children, disabled, uri, ...rest }) => {
   const [loading, setLoading] = useState();
 
   const viewFile = async (a) => {
-    try {
-      setLoading(true);
-      const file = await api.openpdf(`/young/${young._id}/form/${a}`, { young });
-      const fileName = `${young.firstName} ${young.lastName} - formulaire - ${a}.pdf`;
-      if (window.navigator.msSaveOrOpenBlob) {
-        //IE11 & Edge
-        window.navigator.msSaveOrOpenBlob(file, fileName);
-      } else {
-        //Other browsers
-        const a = document.createElement("a");
-        document.body.appendChild(a);
-        a.href = URL.createObjectURL(file);
-        a.download = fileName;
-        a.click();
-      }
-      setLoading(false);
-    } catch (e) {
-      if (e?.message === "unauthorized") {
-        return (window.location.href = "/auth/login?disconnected=1&redirect=phase1");
-      }
-      Sentry.captureException(e);
-      toastr.error("Une erreur est survenue lors du téléchargement", e?.message, { timeOut: 10000 });
-      setLoading(false);
-    }
+    setLoading(true);
+    await downloadPDF({
+      url: `/young/${young._id}/form/${a}`,
+      body: { young },
+      fileName: `${young.firstName} ${young.lastName} - formulaire - ${a}.pdf`,
+    });
+    setLoading(false);
   };
   return (
     <div {...rest} onClick={() => viewFile(uri)}>
