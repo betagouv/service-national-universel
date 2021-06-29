01615b73a8a7d70130ff4a3ddefc28e3bcc42a1d
Raphaël Huchet <raph@selego.co>
2021-06-29 10:38:50 +0200

    refactor: remove download dup on new component

--- admin/src/components/buttons/DownloadAllAttestation.js
+++ admin/src/components/buttons/DownloadAllAttestation.js
@@ -1,28 +1,17 @@
-import React, { useEffect, useState } from "react";
-import { Spinner } from "reactstrap";
-import styled from "styled-components";
-import api from "../../services/api";
-import { environment } from "../../config";
+import React, { useState } from "react";
 import LoadingButton from "./LoadingButton";
+import downloadPDF from "../../utils/download-pdf";
 
 export default ({ cohesionCenterId, children, disabled, uri }) => {
   const [loading, setLoading] = useState();
 
   const viewAttestation = async () => {
     setLoading(true);
-    const file = await api.openpdf(`/cohesion-center/${cohesionCenterId}/certificate`, { options: { landscape: true } });
-    const fileName = `attestations.pdf`;
-    if (window.navigator.msSaveOrOpenBlob) {
-      //IE11 & Edge
-      window.navigator.msSaveOrOpenBlob(file, fileName);
-    } else {
-      //Other browsers
-      const a = document.createElement("a");
-      document.body.appendChild(a);
-      a.href = URL.createObjectURL(file);
-      a.download = fileName;
-      a.click();
-    }
+    await downloadPDF({
+      url: `/cohesion-center/${cohesionCenterId}/certificate`,
+      body: { options: { landscape: true } },
+      fileName: `attestations.pdf`,
+    });
     setLoading(false);
   };
   return (
