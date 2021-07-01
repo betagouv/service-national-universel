ed01f9fef39df1d6333fd390e3a6966139aa4727
SpookyUmi <chloe.danisevicius@gmail.com>
2021-07-01 16:15:55 +0200

    add helmet to app server

--- app/server/index.js
+++ app/server/index.js
@@ -2,9 +2,12 @@ const express = require("express");
 const path = require("path");
 const hsts = require("hsts");
 const { forceDomain } = require("forcedomain");
+const helmet = require("helmet");
 
 const app = express();
 const port = 8080;
+
+app.use(helmet());
 
 app.use(function (req, res, next) {
   if (req.headers.host === "moncompte.snu.gouv.fr") {
