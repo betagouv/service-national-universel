7da126c8542e869c2f4e622462ce697079957f09
Chloé Danisevicius <chloe.danisevicius@gmail.com>
2021-07-02 11:35:58 +0200

    refacto : create 2 components Title and Subtitle

---
+++ admin/src/scenes/auth/components/title.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  position: relative;
+  font-size: 2rem;
+  @media (max-width: 768px) {
+    font-size: 1.2rem;
+  }
+  font-weight: 700;
+  margin-bottom: 14px;
+`;
+
+export default Title;
