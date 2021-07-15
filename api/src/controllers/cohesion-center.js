8d6456e577511617fa35e2520f41b93b952aa987
Raphaël Huchet <raph@selego.co>
2021-07-15 17:26:13 +0200

    fix: not found cohesion center

--- api/src/controllers/cohesion-center.js
+++ api/src/controllers/cohesion-center.js
@@ -140,6 +140,8 @@ router.post("/:centerId/assign-young-waiting-list/:youngId", passport.authentica
 router.get("/:id", passport.authenticate("referent", { session: false }), async (req, res) => {
   try {
     const data = await CohesionCenterModel.findById(req.params.id);
+    if (!data) return res.status(404).send({ ok: false, code: ERRORS.NOT_FOUND });
+
     return res.status(200).send({ ok: true, data });
   } catch (error) {
     capture(error);
