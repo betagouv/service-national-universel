5098276ecbf8b7c179361cb2397ac56e58d0df27
Chloé Danisevicius <chloe.danisevicius@gmail.com>
2021-07-22 19:42:20 +0200

    app support page

--- app/src/scenes/support-center/index.js
+++ app/src/scenes/support-center/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { toastr } from "react-redux-toastr";
+import styled from "styled-components";
 
 import api from "../../services/api";
-import { HeroContainer } from "../../components/Content";
+import { Hero, HeroContainer } from "../../components/Content";
 import LoadingButton from "../../components/buttons/LoadingButton";
 
 export default () => {
@@ -25,16 +26,37 @@ export default () => {
       <button
         onClick={createTicket}
       >Ouvrir un ticket</button>
-      <section>
-        <h2>Besoin d'aide ?</h2>
-        <p>Vous avec un problème technique, vous souhaitez en savoir plus sur votre situation, ou souhaitez contacter l’un de vos référents ?</p>
-        <LoadingButton>
-          <NavLink to="/support/ticket" className="">
-            Contacter le support
-          </NavLink>
-        </LoadingButton>
-      </section>
-
+      <Container>
+        <section>
+          <h2>Besoin d'aide ?</h2>
+          <p>Vous avec un problème technique, vous souhaitez en savoir plus sur votre situation, ou souhaitez contacter l’un de vos référents ?</p>
+          <LoadingButton>
+            <Link to="/support/ticket" className="">
+              Contacter le support
+            </Link>
+          </LoadingButton>
+        </section>
+        <Hero>
+          <h3>Mes informations</h3>
+          <p><span>{young.firstName}</span> Né le {young.birthdateAt}</p>
+          <p>Centre de cohésion</p>
+          <p>Phase 1</p>
+          <p>Phase 1</p>
+          <p>Phase 1</p>
+        </Hero>
+      </Container>
     </HeroContainer>
   )
 }
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Link = styled(NavLink)`
+  color: #fff;
+  cursor: pointer;
+  :hover {
+    color: #fff;
+  }
+`;
