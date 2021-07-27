c7f70e4345bdb60d2fb092604f2dad495dac3dee
SpookyUmi <chloe.danisevicius@gmail.com>
2021-07-27 18:47:30 +0200

    implementation : user creation and token creation

--- api/src/controllers/support-center.js
+++ api/src/controllers/support-center.js
@@ -27,15 +27,57 @@ router.get("/", passport.authenticate(["referent", "young"], { session: false })
 
 router.post("/ticket", passport.authenticate(["referent", "young"], { session: false }), async (req, res) => {
   try {
-    let url = `${process.env.ZAMMAD_URL}/api/v1/tickets`;
+    console.log('USER ?', req.user);
+    if (req.user.zammadToken) {
+      const zammadUser = await fetch(`${process.env.ZAMMAD_URL}/api/v1/users/me`, {
+        method: "GET",
+        headers: { "Content-Type": "application/json", "Authorization": `Bearer ${req.user.zammadToken}` }
+      });
+      if (!zammadUser) return console.log("USER DOESN'T EXIST", zammadUser);
+      return console.log("USER EXISTS", zammadUser);
+    }
+    let url = `${process.env.ZAMMAD_URL}/api/v1/users`
     let options = {
       method: "POST",
       headers: { "Content-Type": "application/json", "Authorization": `Bearer ${process.env.ZAMMAD_TOKEN}` },
-      body: JSON.stringify(req.body),
+      body: JSON.stringify(
+        {
+          "firstname": req.user.firstName,
+          "lastname": req.user.lastName,
+          "email": req.user.email,
+          "vip": false,
+          "active": true,
+          "role_ids": [
+            3
+          ],
+        }
+      ),
     };
     const response = await fetch(url, options);
-    //const responseAsJson = await response.json();
-    console.log("---RESPONSE---", response);
+    console.log('---RESPONSE---', response);
+    console.log('---STATUS---', response.status);
+    if (!response.ok) return res.status(400).send({ ok: false });
+    const tokenResponse = await fetch(`${process.env.ZAMMAD_URL}/api/v1/user_access_token`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json", "Authorization": `Bearer ${process.env.ZAMMAD_TOKEN}` },
+      body: JSON.stringify({
+        "label": "Customer Token",
+        "permission": ["ticket.customer", "user_preferences"],
+        "expires_at": null
+      })
+    });
+    console.log('---TOKEN RESPONSE---', tokenResponse, tokenResponse.data);
+    return res.status(200).send({ ok: true, token: tokenResponse.name });
+
+    // let url = `${process.env.ZAMMAD_URL}/api/v1/tickets`;
+    // let options = {
+    //   method: "POST",
+    //   headers: { "Content-Type": "application/json", "Authorization": `Bearer ${process.env.ZAMMAD_TOKEN}` },
+    //   body: JSON.stringify(req.body),
+    // };
+    // const response = await fetch(url, options);
+    // //const responseAsJson = await response.json();
+    // console.log("---RESPONSE---", response);
     //const dataUrl = responseAsJson.data.generateUrl;
 
     return res.status(200).send({ ok: true, data: response });
