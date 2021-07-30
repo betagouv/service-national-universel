584a61a46613e0c658522c3ad6133dba56c379c4
tangimds <tangimds@gmail.com>
2021-07-30 18:38:50 +0200

    add svg

--- app/src/components/DomainThumb.js
+++ app/src/components/DomainThumb.js
@@ -3,32 +3,33 @@ import styled from "styled-components";
 
 export default ({ domain = "default", backgroundColor = "#42389d", size = "3rem", sizeMobile = "3rem", style }) => (
   <Thumb backgroundColor={backgroundColor} size={size} sizeMobile={sizeMobile} style={style}>
-    <img src={require(`../assets/mission-domains/${domain}.svg`)} />
+    <img src={require(`../assets/mission-domains/${domain.toLowerCase()}.svg`)} />
   </Thumb>
 );
 
 const Thumb = styled.div`
   margin-right: 20px;
   background-color: ${({ backgroundColor }) => backgroundColor};
+  min-height: ${({ size }) => size};
+  min-width: ${({ size }) => size};
   height: ${({ size }) => size};
   width: ${({ size }) => size};
   border-radius: 4px;
-  padding: 0.7rem;
   text-align: center;
   display: flex;
   justify-content: center;
   align-items: center;
+  box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
   @media (max-width: 768px) {
+    min-height: ${({ sizeMobile }) => sizeMobile};
+    min-width: ${({ sizeMobile }) => sizeMobile};
     height: ${({ sizeMobile }) => sizeMobile};
     width: ${({ sizeMobile }) => sizeMobile};
     margin-right: 10px;
-    padding: 0.4rem;
   }
   img {
-    border-radius: 6px;
-    height: 100%;
-    width: 100%;
+    height: 60%;
+    width: 60%;
     object-fit: cover;
-    box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
   }
 `;
