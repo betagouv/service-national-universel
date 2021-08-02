d05558ac5633df9ea86ff4a4b63d83955f4f951c
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2021-08-02 11:57:13 +0200

    chore(security): add IP allowlist to email route (#416)
    * chore(security): add IP allowlist to email route
    
    * fix tests

--- api/src/__tests__/email.test.js
+++ api/src/__tests__/email.test.js
@@ -10,8 +10,14 @@ afterAll(dbClose);
 
 describe("Email", () => {
   it("should work", async () => {
-    let res = await request(getAppHelper()).post("/email").send({ subject: "hello", email: "foo@bar.fr" });
+    let res = await request(getAppHelper()).post("/email").set("x-forwarded-for", "185.107.232.1").send({ subject: "hello", email: "foo@bar.fr" });
     expect(res.status).toBe(200);
+
+    res = await request(getAppHelper()).post("/email").set("x-forwarded-for", "1.2.3.4").send({ subject: "hello", email: "foo@bar.fr" });
+    expect(res.status).toBe(401);
+
+    res = await request(getAppHelper()).post("/email").send({ subject: "hello", email: "foo@bar.fr" });
+    expect(res.status).toBe(401);
 
     res = await request(getAppHelper()).get("/email?email=foo@bar.fr");
     expect(res.status).toBe(200);
