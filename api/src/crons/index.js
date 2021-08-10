ed82ae63343f49adf0e32fbbe51c84e3b3dc2c7f
tangimds <tangimds@gmail.com>
2021-08-10 10:03:19 +0200

    fix: stop script

--- api/src/crons/index.js
+++ api/src/crons/index.js
@@ -39,7 +39,7 @@ if (ENVIRONMENT === "production" && process.env.INSTANCE_NUMBER === "0") {
   });
 
   //every hour
-  cron.schedule(EVERY_HOUR, () => {
-    autoAffectationCohesionCenter.handler();
-  });
+  // cron.schedule(EVERY_HOUR, () => {
+  //   autoAffectationCohesionCenter.handler();
+  // });
 }
