a50636e6325ac36a8c2cea89606d06badfb87271
Raphaël Huchet <raph@selego.co>
2021-08-16 16:26:17 +0200

    test: update contract tests

--- api/src/__tests__/contract.test.js
+++ api/src/__tests__/contract.test.js
@@ -9,7 +9,7 @@ const { getNewApplicationFixture } = require("./fixtures/application");
 const getNewYoungFixture = require("./fixtures/young");
 const { createApplication } = require("./helpers/application");
 const { getYoungByIdHelper, createYoungHelper } = require("./helpers/young");
-const { expectContractToEqual, getContractByIdHelper } = require("./helpers/contract");
+const { expectContractToEqual, getContractByIdHelper, createContractHelper } = require("./helpers/contract");
 
 jest.mock("../sendinblue", () => ({
   ...jest.requireActual("../sendinblue"),
@@ -41,62 +41,68 @@ describe("Structure", () => {
         .send({ ...getNewContractFixture(), applicationId: application._id });
       expect(res.status).toBe(404);
     });
-    it("should create contract and sent it", async () => {
-      let young = await createYoungHelper(getNewYoungFixture());
-      const applicationFixture = getNewApplicationFixture();
-      applicationFixture.youngId = young._id;
-      const application = await createApplication(applicationFixture);
-      const contractFixture = getNewContractFixture();
-      contractFixture.youngId = young._id;
-      contractFixture.applicationId = application._id;
-      const res = await request(getAppHelper())
-        .post("/contract")
-        .send({ ...contractFixture, sendMessage: true });
-      expect(res.status).toBe(200);
-      expectContractToEqual(res.body.data, contractFixture);
-      expect(res.body.data.invitationSent).toBe("true");
-      young = await getYoungByIdHelper(young._id);
-      expect(young.statusPhase2Contract[0]).toBe("SENT");
-    });
-    it("should create contract and draft it", async () => {
-      const youngFixture = getNewYoungFixture();
-      let young = await createYoungHelper(youngFixture);
-      const applicationFixture = getNewApplicationFixture();
-      applicationFixture.youngId = young._id;
-      const application = await createApplication(applicationFixture);
-      const contractFixture = getNewContractFixture();
-      contractFixture.youngId = young._id;
-      contractFixture.applicationId = application._id;
-      const res = await request(getAppHelper())
-        .post("/contract")
-        .send({ ...contractFixture, sendMessage: false });
-      expect(res.status).toBe(200);
-      expectContractToEqual(res.body.data, contractFixture);
-      expect(res.body.data.invitationSent).not.toBe("true");
-      young = await getYoungByIdHelper(young._id);
-      expect(young.statusPhase2Contract[0]).toBe("DRAFT");
-    });
-    it("should create contract draft it and then send it", async () => {
-      const youngFixture = getNewYoungFixture();
-      let young = await createYoungHelper(youngFixture);
-      const applicationFixture = getNewApplicationFixture();
-      applicationFixture.youngId = young._id;
-      const application = await createApplication(applicationFixture);
-      const contractFixture = getNewContractFixture();
-      contractFixture.youngId = young._id;
-      contractFixture.applicationId = application._id;
-      const res = await request(getAppHelper())
-        .post("/contract")
-        .send({ ...contractFixture, sendMessage: false });
-      expect(res.status).toBe(200);
-      const resAfter = await request(getAppHelper())
-        .post("/contract")
-        .send({ ...res.body.data, sendMessage: true });
-      expect(resAfter.status).toBe(200);
-      expectContractToEqual(resAfter.body.data, contractFixture);
-      expect(resAfter.body.data.invitationSent).toBe("true");
-      young = await getYoungByIdHelper(young._id);
-      expect(young.statusPhase2Contract[0]).toBe("SENT");
+    describe("when it works", () => {
+      async function createContract(options, initialYoung) {
+        const young = initialYoung || (await createYoungHelper(getNewYoungFixture()));
+        const application = await createApplication({ ...getNewApplicationFixture(), youngId: young._id });
+        const contractFixture = getNewContractFixture();
+        contractFixture.youngId = young._id;
+        contractFixture.applicationId = application._id;
+        const res = await request(getAppHelper())
+          .post("/contract")
+          .send({ ...contractFixture, ...options });
+        return { res, young, application, contractFixture };
+      }
+      it("should create contract and send it when setting sendMessage to true", async () => {
+        const { res, young, contractFixture } = await createContract({ sendMessage: true });
+        expect(res.status).toBe(200);
+        expectContractToEqual(res.body.data, contractFixture);
+        expect(res.body.data.invitationSent).toBe("true");
+        const updatedYoung = await getYoungByIdHelper(young._id);
+        expect(updatedYoung.statusPhase2Contract[0]).toBe("SENT");
+      });
+      it("should create contract and not send it when setting sendMessage to false", async () => {
+        const { res, young, contractFixture } = await createContract({ sendMessage: false });
+        expect(res.status).toBe(200);
+        expectContractToEqual(res.body.data, contractFixture);
+        expect(res.body.data.invitationSent).not.toBe("true");
+        const updatedYoung = await getYoungByIdHelper(young._id);
+        expect(updatedYoung.statusPhase2Contract[0]).toBe("DRAFT");
+      });
+      it("should create contract and draft it then send it", async () => {
+        const { res, young, contractFixture } = await createContract({ sendMessage: false });
+        expect(res.status).toBe(200);
+        expect(res.body.data.invitationSent).not.toBe("true");
+        const resAfter = await request(getAppHelper())
+          .post("/contract")
+          .send({ ...res.body.data, sendMessage: true });
+        expect(resAfter.status).toBe(200);
+        expectContractToEqual(resAfter.body.data, contractFixture);
+        expect(resAfter.body.data.invitationSent).toBe("true");
+
+        const updatedYoung = await getYoungByIdHelper(young._id);
+        expect(updatedYoung.statusPhase2Contract[0]).toBe("SENT");
+      });
+
+      it("should create tokens for young (not adult)", async () => {
+        const { res } = await createContract({ sendMessage: true });
+        expect(res.status).toBe(200);
+        expect(res.body.data.parent1Token).toBeTruthy();
+        expect(res.body.data.projectManagerToken).toBeTruthy();
+        expect(res.body.data.structureManagerToken).toBeTruthy();
+        expect(res.body.data.parent2Token).toBeTruthy();
+        expect(res.body.data.youngContractToken).toBeUndefined();
+      });
+
+      it("should create tokens for young (adult)", async () => {
+        const { res } = await createContract({ sendMessage: true, isYoungAdult: "true" });
+        expect(res.status).toBe(200);
+        expect(res.body.data.parent1Token).toBeUndefined();
+        expect(res.body.data.projectManagerToken).toBeTruthy();
+        expect(res.body.data.structureManagerToken).toBeTruthy();
+        expect(res.body.data.parent2Token).toBeUndefined();
+        expect(res.body.data.youngContractToken).toBeTruthy();
+      });
     });
   });
   describe("GET /contract/:id", () => {
@@ -108,69 +114,58 @@ describe("Structure", () => {
       const res = await request(getAppHelper()).get(`/contract/1`).send();
       expect(res.status).toBe(400);
     });
-    it("should return 200 and tokens for not adult young", async () => {
-      const youngFixture = getNewYoungFixture();
-      let young = await createYoungHelper(youngFixture);
-      const applicationFixture = getNewApplicationFixture();
-      applicationFixture.youngId = young._id;
-      const application = await createApplication(applicationFixture);
-      const contractFixture = getNewContractFixture();
-      contractFixture.youngId = young._id;
-      contractFixture.applicationId = application._id;
-      const resContract = await request(getAppHelper()).post("/contract").send(contractFixture);
-      expect(resContract.status).toBe(200);
-      const res = await request(getAppHelper()).get(`/contract/${resContract.body.data._id}`).send();
+    it("should return 200 when it works", async () => {
+      const young = await createYoungHelper(getNewYoungFixture());
+      const application = await createApplication({ ...getNewApplicationFixture(), youngId: young._id });
+      const contract = await createContractHelper({ ...getNewContractFixture(), youngId: young._id, applicationId: application._id });
+      const res = await request(getAppHelper()).get(`/contract/${contract._id}`).send();
       expect(res.status).toBe(200);
-      expect(res.body.data.parent1Token).not.toBe(undefined);
-      expect(res.body.data.projectManagerToken).not.toBe(undefined);
-      expect(res.body.data.structureManagerToken).not.toBe(undefined);
-      expect(res.body.data.parent2Token).not.toBe(undefined);
-      expect(res.body.data.youngContractToken).toBe(undefined);
     });
-    it("should return 200 and tokens for not adult young", async () => {
-      const youngFixture = getNewYoungFixture();
-      let young = await createYoungHelper(youngFixture);
-      const applicationFixture = getNewApplicationFixture();
-      applicationFixture.youngId = young._id;
-      const application = await createApplication(applicationFixture);
-      const contractFixture = getNewContractFixture();
-      contractFixture.youngId = young._id;
-      contractFixture.applicationId = application._id;
-      contractFixture.isYoungAdult = "true";
-      const resContract = await request(getAppHelper()).post("/contract").send(contractFixture);
-      expect(resContract.status).toBe(200);
-      const res = await request(getAppHelper()).get(`/contract/${resContract.body.data._id}`).send();
-      expect(res.status).toBe(200);
-      expect(res.body.data.parent1Token).toBe(undefined);
-      expect(res.body.data.projectManagerToken).not.toBe(undefined);
-      expect(res.body.data.structureManagerToken).not.toBe(undefined);
-      expect(res.body.data.parent2Token).toBe(undefined);
-      expect(res.body.data.youngContractToken).not.toBe(undefined);
-    });
-    it("should return 200 without token", async () => {
-      const youngPassport = await createYoungHelper(getNewYoungFixture());
+    it("should not return tokens for young", async () => {
+      const young = await createYoungHelper(getNewYoungFixture());
+      const application = await createApplication({ ...getNewApplicationFixture(), youngId: young._id });
+      const contract = await createContractHelper({
+        ...getNewContractFixture(),
+        youngId: young._id,
+        applicationId: application._id,
+        projectManagerToken: "foo",
+        parent1Token: "bar",
+        parent2Token: "baz",
+        structureManagerToken: "qux",
+      });
+
       const passport = require("passport");
       const previous = passport.user;
-      passport.user = youngPassport;
-      let young = await createYoungHelper(getNewYoungFixture());
-      const applicationFixture = getNewApplicationFixture();
-      applicationFixture.youngId = young._id;
-      const application = await createApplication(applicationFixture);
-      const contractFixture = getNewContractFixture();
-      contractFixture.youngId = young._id;
-      contractFixture.applicationId = application._id;
-      const resContract = await request(getAppHelper()).post("/contract").send(contractFixture);
-      expect(resContract.status).toBe(200);
-      const res = await request(getAppHelper()).get(`/contract/${resContract.body.data._id}`).send();
+      passport.user = young;
+
+      const res = await request(getAppHelper()).get(`/contract/${contract._id}`).send();
       expect(res.status).toBe(200);
-      expect(res.body.data.parent1Token).toBe(undefined);
-      expect(res.body.data.projectManagerToken).toBe(undefined);
-      expect(res.body.structureManagerToken).toBe(undefined);
-      expect(res.body.data.parent2Token).toBe(undefined);
-      expect(res.body.data.youngContractToken).toBe(undefined);
+      expect(res.body.data.projectManagerToken).toBeUndefined();
+      expect(res.body.structureManagerToken).toBeUndefined();
+      expect(res.body.data.parent1Token).toBeUndefined();
+      expect(res.body.data.parent2Token).toBeUndefined();
+      expect(res.body.data.youngContractToken).toBeUndefined();
+
+      passport.user = previous;
+    });
+    it("should return 401 when young tries to see a contract from someone else", async () => {
+      const young = await createYoungHelper(getNewYoungFixture());
+      const application = await createApplication({ ...getNewApplicationFixture(), youngId: young._id });
+      const contract = await createContractHelper({ ...getNewContractFixture(), youngId: young._id, applicationId: application._id });
+
+      const secondYoung = await createYoungHelper(getNewYoungFixture());
+
+      const passport = require("passport");
+      const previous = passport.user;
+      passport.user = secondYoung;
+
+      const res = await request(getAppHelper()).get(`/contract/${contract._id}`).send();
+      expect(res.status).toBe(401);
+
       passport.user = previous;
     });
   });
+
   describe("GET /contract/token/:token", () => {
     it("should return 404 not found", async () => {
       const resGet = await request(getAppHelper()).get(`/contract/token/${ObjectId()}`).send();
