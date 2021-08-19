a2325f17be11dec601b88a6237b39cee8f987890
SpookyUmi <chloe.danisevicius@gmail.com>
2021-08-19 14:23:41 +0200

    try out

--- app/src/scenes/support-center/index.js
+++ app/src/scenes/support-center/index.js
@@ -7,6 +7,7 @@ import styled from "styled-components";
 import api from "../../services/api";
 import { Hero, HeroContainer, Separator } from "../../components/Content";
 import LoadingButton from "../../components/buttons/LoadingButton";
+import { translate } from "../../utils/translation";
 
 // This component isn't finished.
 
@@ -21,45 +22,51 @@ export default () => {
       toastr.error("Une erreur est survenue");
     }
   }
+  console.log("YOUNG", JSON.stringify(young));
 
   return (
     <HeroContainer>
-      <pre>{JSON.stringify(young)}</pre>
-      <button
-        onClick={createTicket}
-      >Ouvrir un ticket</button>
       <Container>
-        <section>
+        <section className="help-section">
           <h2>Besoin d'aide ?</h2>
-          <p>Vous avec un problème technique, vous souhaitez en savoir plus sur votre situation, ou souhaitez contacter l’un de vos référents ?</p>
+          <p style={{ color: "#6B7280", }}>Vous avez un problème technique, vous souhaitez en savoir plus sur votre situation, ou souhaitez contacter l’un de vos référents ?</p>
           <LoadingButton>
-            <Link to="/ticket" className="">
+            <Link to="/ticket">
               Contacter le support
             </Link>
           </LoadingButton>
         </section>
-        <Hero>
+        <Card>
           <div className="content">
             <h3>
               Mes informations
             </h3>
-            <p>
-              <span>{young.firstName}</span> Né le {young.birthdateAt}
+            <p style={{ color: "#6B7280" }}>
+              <span style={{ fontWeight: "700" }}>{young.firstName} {young.lastName}</span> • née le 27/11/2020 • Ile-de-France
             </p>
-            <Separator />
-            <p>
-              <span>Centre de cohésion</span>
+            <div className="division" style={{ borderBottom: "1px solid #E5E7EB", borderTop: "1px solid #E5E7EB" }}>
+              <p>Centre de cohésion</p>
               <br />
-              <Link to="/mission">{young.cohesionCenter}</Link>
-            </p>
-            <Separator />
-            <p>
-              Phase 1
-            </p>
-            <Separator />
-            <p>Phase 2</p>
+              <p>{young.cohesionCenterName}</p>
+            </div>
+            <div className="division" style={{ borderBottom: "1px solid #E5E7EB" }}>
+              <p>
+                Phase 1
+              </p>
+              <p>
+                {translate(young.statusPhase1)}
+              </p>
+            </div>
+            <div className="division" style={{ borderBottom: "1px solid #E5E7EB" }}>
+              <p>Phase 2</p>
+              <p>{translate(young.statusPhase2)}</p>
+            </div>
+            <div className="division">
+              <p>Phase 3</p>
+              <p>{translate(young.statusPhase3)}</p>
+            </div>
           </div>
-        </Hero>
+        </Card>
       </Container>
       <List>
         <header className="header">
@@ -70,19 +77,24 @@ export default () => {
           </p>
         </header>
         <section className="section">
-          <p>#4566</p>
-          <p>Lorem ipsum dolor</p>
-          <p className="text">Il y a un jour</p>
+          <p>#4563</p>
+          <p>Problème avec ma mission</p>
+          <p className="text">Il y a une heure</p>
+        </section>
+        <section className="section background">
+          <p>#4562</p>
+          <p>Ma candidature a disparu</p>
+          <p className="text">Il y a 2 jours</p>
         </section>
         <section className="section">
-          <p>#4566</p>
-          <p>Lorem ipsum dolor</p>
-          <p className="text">Il y a un jour</p>
+          <p>#4561</p>
+          <p>Contact chef de centre</p>
+          <p className="text">Il y a 4 jours</p>
         </section>
-        <section className="section">
-          <p>#4566</p>
-          <p>Lorem ipsum dolor</p>
-          <p className="text">Il y a un jour</p>
+        <section className="section background">
+          <p>#4560</p>
+          <p>Informations mission</p>
+          <p className="text">Il y a une semaine</p>
         </section>
       </List>
     </HeroContainer>
@@ -91,22 +103,46 @@ export default () => {
 
 const Container = styled.div`
   display: flex;
+  .help-section {
+    max-width: 450px;
+    margin: 40px 20px;
+  }
 `;
 
 const List = styled.div`
   border-radius: 0.5rem;
   box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
   margin: 20px 0;
-  padding: 10px 20px;
   background-color: #fff;
+  overflow: hidden;
   :nth-child(2n + 1) {
     background-color: #f4f5f7;
   }
   .header {
+    p {
+      margin: 0;
+    }
+    border-bottom: 1px solid #E5E7EB;
     display: flex;
+    justify-content: space-between;
+    padding: 15px;
+    background-color: #F9FAFB;
   }
   .section {
+    p {
+      margin: 0;
+    }
     display: flex;
+    justify-content: space-between;
+    padding: 15px;
+  }
+  .background {
+    background-color: #F9FAFB;
+  }
+  .text {
+    width: 50%;
+    display: flex;
+    justify-content: flex-end;
   }
 `;
 
@@ -117,3 +153,24 @@ const Link = styled(NavLink)`
     color: #fff;
   }
 `;
+
+const Card = styled.div`
+  display: flex;
+  flex: 2;
+  padding: 3rem;
+  border-radius: 0.5rem;
+  box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
+  flex-direction: column;
+  background-color: #fff;
+  .division {
+    display: flex;
+    justify-content: space-between;
+    padding: 10px;
+  }
+  .division p {
+    margin: 0;
+  }
+  .division p:first-child {
+    color: #6B7280;
+  }
+`
