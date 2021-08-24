0b12cf91065212fc89380d8df87bccd8cb2e6a82
hugo3m <58819009+hugo3m@users.noreply.github.com>
2021-08-24 18:34:19 +0200

    Feat: improve edit

--- admin/src/scenes/volontaires/edit/preferences.js
+++ admin/src/scenes/volontaires/edit/preferences.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { Col } from "reactstrap";
+import { Col, Row } from "reactstrap";
 import { useSelector } from "react-redux";
 
-import { MISSION_DOMAINS, PROFESSIONNAL_PROJECT, PROFESSIONNAL_PROJECT_PRECISION, PERIOD, translate } from "../../../utils";
-import { Box, BoxContent, BoxHeadTitle } from "../../../components/box";
+import { MISSION_DOMAINS, PROFESSIONNAL_PROJECT, PROFESSIONNAL_PROJECT_PRECISION, PERIOD, TRANSPORT, translate } from "../../../utils";
+import { Box, BoxContent, BoxHeadTitle, BoxTitleCircular } from "../../../components/box";
 import Item from "../components/Item";
 import MultiSelect from "../../../components/Multiselect";
 import Select from "../components/Select";
@@ -17,7 +17,14 @@ export default ({ values, handleChange, required = {}, errors, touched }) => {
       <Box>
         <BoxHeadTitle>Préférences de missions</BoxHeadTitle>
         <BoxContent direction="column">
-          <MultiSelect value={values.domains} onChange={handleChange} name="domains" options={Object.values(MISSION_DOMAINS)} placeholder="Sélectionnez trois domaines" />
+          <MultiSelectWithTitle
+            title="Domaines"
+            value={values.domains}
+            onChange={handleChange}
+            name="domains"
+            options={Object.values(MISSION_DOMAINS)}
+            placeholder="Sélectionnez trois domaines"
+          />
           <Select
             name="professionnalProject"
             values={values}
@@ -58,7 +65,7 @@ export default ({ values, handleChange, required = {}, errors, touched }) => {
               { value: PERIOD.DURING_SCHOOL, label: translate(PERIOD.DURING_SCHOOL) },
             ]}
           />
-          <p>Mission à proximité de</p>
+          <BoxTitleCircular>Mission à proximité de</BoxTitleCircular>
           <Select
             name="mobilityNearSchool"
             values={values}
@@ -89,9 +96,60 @@ export default ({ values, handleChange, required = {}, errors, touched }) => {
               { value: "false", label: "Non" },
             ]}
           />
-          <Item title="Nom" values={values} name="schoolName" handleChange={handleChange} />
+          {values.mobilityNearRelative && values.mobilityNearRelative === "true" && (
+            <>
+              <BoxTitleCircular>Coordonnées du proche</BoxTitleCircular>
+              <Item title="Nom" values={values} name="mobilityNearRelativeName" handleChange={handleChange} />
+              <Item title="Adresse" values={values} name="mobilityNearRelativeAddress" handleChange={handleChange} />
+              <Item title="Code postal" values={values} name="mobilityNearRelativeZip" handleChange={handleChange} />
+              <Item title="Ville" values={values} name="mobilityNearRelativeCity" handleChange={handleChange} />
+            </>
+          )}
+          <MultiSelectWithTitle
+            title="Moyen de transport"
+            value={values.mobilityTransport}
+            onChange={handleChange}
+            name="mobilityTransport"
+            options={Object.values(TRANSPORT)}
+            placeholder="Moyen de transport"
+          />
+          <Select
+            name="missionFormat"
+            values={values}
+            handleChange={handleChange}
+            title="Format de mission"
+            options={[
+              { value: "CONTINUOUS", label: translate("CONTINUOUS") },
+              { value: "DISCONTINUOUS", label: translate("DISCONTINUOUS") },
+            ]}
+          />
+          <Select
+            name="engaged"
+            values={values}
+            handleChange={handleChange}
+            title="Bénévole en parallèle"
+            options={[
+              { value: "true", label: "Oui" },
+              { value: "false", label: "Non" },
+            ]}
+          />
+          {values.engaged && values.engaged === "true" && <Item title="Description de l'activité" values={values} name="engagedDescription" handleChange={handleChange} />}
+          <Item title="Endroit où souhaite effectuer la mission" values={values} name="desiredLocation" handleChange={handleChange} />
         </BoxContent>
       </Box>
     </Col>
+  );
+};
+
+const MultiSelectWithTitle = ({ title, value, onChange, name, options, placeholder }) => {
+  return (
+    <Row className="detail">
+      <Col md={4}>
+        <label>{title}</label>
+      </Col>
+      <Col md={8}>
+        <MultiSelect value={value} onChange={onChange} name={name} options={options} placeholder={placeholder} />
+      </Col>
+    </Row>
   );
 };
