371882ffaddcf23d4094ca37f626049c4a0cc96d
Raphaël Huchet <raph@selego.co>
2021-09-17 11:17:05 +0200

    feat: add chat in app

--- app/src/redux/auth/actions.js
+++ app/src/redux/auth/actions.js
@@ -3,5 +3,11 @@ export const authActions = {
 };
 
 export function setYoung(young) {
+  if (window && young)
+    window.zammadUser = {
+      email: young.email,
+      name: young.firstName + " " + young.lastName,
+      _id: young._id,
+    };
   return { type: authActions.SETYOUNG, young };
 }
