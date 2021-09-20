ecdbc7c8c544aae142d0583c09110bdbfa8a98cf
tangimds <tangimds@gmail.com>
2021-09-20 13:43:06 +0200

    fix: redirect

--- app/src/scenes/support-center/index.js
+++ app/src/scenes/support-center/index.js
@@ -14,18 +14,18 @@ const articles = [
   {
     title: "Je cherche une mission",
     body: `Depuis l'onglet "Mission d'intérêt général", cliquez sur la rubrique...`,
-    url: "https://support.selego.co/help/fr-fr/13-phase-2-mission-d-interet-general/33-je-cherche-une-mission-mig"
+    url: "https://support.selego.co/help/fr-fr/13-phase-2-mission-d-interet-general/33-je-cherche-une-mission-mig",
   },
   {
     title: "Je modifie mon identifiant e-mail",
     body: `Connectez vous à votre espace volontaire, accédez à "Mon profil"...`,
-    url: "https://support.selego.co/help/fr-fr/10-mon-compte/51-je-modifie-mon-identifiant-email"
+    url: "https://support.selego.co/help/fr-fr/10-mon-compte/51-je-modifie-mon-identifiant-email",
   },
   {
     title: "Je consulte mes missions réalisées",
     body: `Les missions réalisées correspondent aux missions que vous avez déjà...`,
-    url: "https://support.selego.co/help/fr-fr/13-phase-2-mission-d-interet-general/38-je-consulte-mes-missions-realisees"
-  }
+    url: "https://support.selego.co/help/fr-fr/13-phase-2-mission-d-interet-general/38-je-consulte-mes-missions-realisees",
+  },
 ];
 
 export default () => {
@@ -40,7 +40,7 @@ export default () => {
       } catch (error) {
         console.log(error);
       }
-    }
+    };
     fetchTickets();
   }, []);
 
@@ -49,25 +49,29 @@ export default () => {
       <Container>
         <section className="help-section">
           <h2>Besoin d'aide ?</h2>
-          <p style={{ color: "#6B7280", }}>Vous souhaitez en savoir plus sur les phases de votre parcours volontaire ou sur le fonctionnement de votre espace ? N'hésitez pas à consulter notre <strong>base de connaissance</strong> !<br /> Vous avez un problème technique ou souhaitez contacter un référent ? Contactez notre service de support.</p>
+          <p style={{ color: "#6B7280" }}>
+            Vous souhaitez en savoir plus sur les phases de votre parcours volontaire ou sur le fonctionnement de votre espace ? N'hésitez pas à consulter notre{" "}
+            <strong>base de connaissance</strong> !<br /> Vous avez un problème technique ou souhaitez contacter un référent ? Contactez notre service de support.
+          </p>
           <div className="buttons">
             <LinkButton href="https://support.selego.co/help/fr-fr" target="_blank">
               Base de connaissance
             </LinkButton>
-            <InternalLink to="/ticket">
-              Contacter quelqu'un
-            </InternalLink>
+            <InternalLink to="/ticket">Contacter quelqu'un</InternalLink>
           </div>
         </section>
         <Card>
-          <h4 style={{ marginLeft: "0.5rem" }}>
-            Quelques articles pour vous aider :
-          </h4>
+          <h4 style={{ marginLeft: "0.5rem" }}>Quelques articles pour vous aider :</h4>
           <div className="division">
             {articles.map((article) => (
               <div className="block" key={article.url}>
                 <h6>{article.title}</h6>
-                <p>{article.body} <a className="block-link" href={article.url} target="_blank">Lire la suite</a></p>
+                <p>
+                  {article.body}{" "}
+                  <a className="block-link" href={article.url} target="_blank">
+                    Lire la suite
+                  </a>
+                </p>
               </div>
             ))}
           </div>
@@ -80,16 +84,16 @@ export default () => {
           <p className="ticket-date">Dernière mise à jour</p>
         </section>
         {userTickets?.reverse().map((ticket) => (
-          <NavLink to="/ticket-detail" key={ticket.id} className="ticket">
-            <p>{ticket.number}</p>
+          <NavLink to={`/ticket/${ticket.id}`} key={ticket.id} className="ticket">
+            <p>{ticket.id}</p>
             <p>{ticket.title}</p>
             <p className="ticket-date">il y a {formatDistanceToNow(new Date(ticket.last_contact_at), { locale: fr })}</p>
           </NavLink>
         ))}
       </List>
     </HeroContainer>
-  )
-}
+  );
+};
 
 const Container = styled.div`
   display: flex;
@@ -172,14 +176,15 @@ const Card = styled.div`
     box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
     border-radius: 0.5rem;
   }
-  .block p, .block a {
+  .block p,
+  .block a {
     margin: 0;
     font-size: 0.9rem;
   }
   @media (min-width: 1024px) {
     margin-top: 0;
   }
-`
+`;
 
 const List = styled.div`
   background-color: #fff;
@@ -206,4 +211,4 @@ const List = styled.div`
   .titles {
     font-weight: bold;
   }
-`
+`;
