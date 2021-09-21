5d31c720a17ce06da2d300e48dbe744efadc4fe1
Tangi Mendès <tangimds@gmail.com>
2021-09-21 14:19:14 +0200

    fix: mail new application pm (#452)

--- api/src/controllers/application.js
+++ api/src/controllers/application.js
@@ -54,6 +54,32 @@ const updatePlacesMission = async (app) => {
   }
 };
 
+const getReferentManagerPhase2 = async (department) => {
+  // get the referent_department manager_phase2
+  let toReferent = await ReferentObject.findOne({
+    subRole: SUB_ROLES.manager_phase2,
+    role: ROLES.REFERENT_DEPARTMENT,
+    department,
+  });
+  // if not found, get the referent_region manager_phase2
+  if (!toReferent) {
+    toReferent = await ReferentObject.findOne({
+      subRole: SUB_ROLES.manager_phase2,
+      role: ROLES.REFERENT_REGION,
+      region: department2region[department],
+    });
+  }
+  // if not found, get the manager_department
+  if (!toReferent) {
+    toReferent = await ReferentObject.findOne({
+      subRole: SUB_ROLES.manager_department,
+      role: ROLES.REFERENT_DEPARTMENT,
+      department,
+    });
+  }
+  return toReferent;
+};
+
 router.post("/", passport.authenticate(["young", "referent"], { session: false }), async (req, res) => {
   try {
     const { value, error } = validateNewApplication(req.body, req.user);
@@ -126,29 +152,10 @@ router.get("/:id", passport.authenticate("referent", { session: false }), async 
 router.post("/notify/docs-military-preparation/:template", passport.authenticate("young", { session: false }), async (req, res) => {
   const { error, value: template } = Joi.string().required().validate(req.params.template);
   if (error) return res.status(400).send({ ok: false, code: ERRORS.INVALID_PARAMS, error: error.message });
-  // get the referent_department manager_phase2
-  let toReferent = await ReferentObject.findOne({
-    subRole: SUB_ROLES.manager_phase2,
-    role: ROLES.REFERENT_DEPARTMENT,
-    department: req.user.department,
-  });
-  // if not found, get the referent_region
-  if (!toReferent) {
-    toReferent = await ReferentObject.findOne({
-      subRole: SUB_ROLES.manager_phase2,
-      role: ROLES.REFERENT_REGION,
-      region: department2region[req.user.department],
-    });
-  }
-  // if not found, get the manager_department
-  if (!toReferent) {
-    toReferent = await ReferentObject.findOne({
-      subRole: SUB_ROLES.manager_department,
-      role: ROLES.REFERENT_DEPARTMENT,
-      department: req.user.department,
-    });
-  }
+
+  const toReferent = await getReferentManagerPhase2(req.user.department);
   if (!toReferent) return res.status(404).send({ ok: false, code: ERRORS.NOT_FOUND });
+
   const mail = await sendTemplate(parseInt(template), {
     emailTo: [{ name: `${toReferent.firstName} ${toReferent.lastName}`, email: toReferent.email }],
     params: { cta: `${ADMIN_URL}/volontaire/${req.user._id}/phase2`, youngFirstName: req.user.firstName, youngLastName: req.user.lastName },
@@ -167,7 +174,7 @@ router.post("/:id/notify/:template", passport.authenticate(["referent", "young"]
       .validate({ ...req.params, ...req.body }, { stripUnknown: true });
     if (error) return res.status(400).send({ ok: false, code: ERRORS.INVALID_PARAMS });
 
-    const { id, template, message } = value;
+    const { id, template: defaultTemplate, message } = value;
 
     const application = await ApplicationObject.findById(id);
     if (!application) return res.status(404).send({ ok: false, code: ERRORS.NOT_FOUND });
@@ -180,15 +187,13 @@ router.post("/:id/notify/:template", passport.authenticate(["referent", "young"]
       return res.status(401).send({ ok: false, code: ERRORS.OPERATION_UNAUTHORIZED });
     }
 
+    let template = defaultTemplate;
     let emailTo;
     // build default values for params
     // => young name, and mission name
     let params = { youngFirstName: application.youngFirstName, youngLastName: application.youngLastName, missionName: mission.name };
 
-    if (template === SENDINBLUE_TEMPLATES.referent.NEW_APPLICATION) {
-      emailTo = [{ name: `${referent.firstName} ${referent.lastName}`, email: referent.email }];
-      params = { ...params, cta: `${ADMIN_URL}/volontaire` };
-    } else if (template === SENDINBLUE_TEMPLATES.referent.YOUNG_VALIDATED) {
+    if (template === SENDINBLUE_TEMPLATES.referent.YOUNG_VALIDATED) {
       emailTo = [{ name: `${referent.firstName} ${referent.lastName}`, email: referent.email }];
       params = { ...params, cta: `${ADMIN_URL}/volontaire` };
     } else if (template === SENDINBLUE_TEMPLATES.young.VALIDATE_APPLICATION) {
@@ -199,6 +204,18 @@ router.post("/:id/notify/:template", passport.authenticate(["referent", "young"]
     } else if (template === SENDINBLUE_TEMPLATES.young.REFUSE_APPLICATION) {
       emailTo = [{ name: `${application.youngFirstName} ${application.youngLastName}`, email: application.youngEmail }];
       params = { ...params, message, cta: `${APP_URL}/mission` };
+    } else if (template === SENDINBLUE_TEMPLATES.referent.NEW_APPLICATION) {
+      // when it is a new application, there are 2 possibilities
+      if (mission.isMilitaryPreparation === "true") {
+        const referentManagerPhase2 = await getReferentManagerPhase2(application.youngDepartment);
+        emailTo = [{ name: `${referentManagerPhase2.firstName} ${referentManagerPhase2.lastName}`, email: referentManagerPhase2.email }];
+        template = SENDINBLUE_TEMPLATES.referent.NEW_MILITARY_PREPARATION_APPLICATION;
+        params = { ...params, cta: `${ADMIN_URL}/volontaire/${application.youngId}` };
+      } else {
+        emailTo = [{ name: `${referent.firstName} ${referent.lastName}`, email: referent.email }];
+        template = SENDINBLUE_TEMPLATES.referent.NEW_APPLICATION_MIG;
+        params = { ...params, cta: `${ADMIN_URL}/volontaire` };
+      }
     } else {
       return res.status(404).send({ ok: false, code: ERRORS.NOT_FOUND });
     }
