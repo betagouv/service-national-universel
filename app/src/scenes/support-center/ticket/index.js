2d74be9e84c2baefbc938f773ac227b0b1760675
tangimds <tangimds@gmail.com>
2021-09-22 10:13:29 +0200

    fix: route support
    * app

--- app/src/scenes/support-center/ticket/index.js
+++ app/src/scenes/support-center/ticket/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-import List from "./list";
+import Create from "./create";
 import View from "./view";
 
 export default () => {
   return (
     <Switch>
-      <Route path="/ticket/:id" component={View} />
-      <Route path="/ticket" component={List} />
+      <Route path="/support/ticket/:id" component={View} />
+      <Route path="/support/ticket" component={Create} />
     </Switch>
   );
 };
