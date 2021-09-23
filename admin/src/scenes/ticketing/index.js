0d19d63492b95b8da8795aced93aecd43b6949cb
SpookyUmi <chloe.danisevicius@gmail.com>
2021-09-23 16:39:46 +0200

    remove console.log

--- admin/src/scenes/ticketing/index.js
+++ admin/src/scenes/ticketing/index.js
@@ -9,15 +9,13 @@ import Infos from "./components/infos";
 export default () => {
   const [ticket, setTicket] = useState(null);
 
-  console.log("ticket",ticket.articles[0].created_by)
-
   return (
     <HeroContainer>
       <Header />
       <section>
         <TicketTabs setTicket={setTicket} />
         <TicketMessage ticketId={ticket?.id} />
-        <Infos ticket={ticket}/>
+        <Infos ticket={ticket} />
       </section>
     </HeroContainer>
   );
