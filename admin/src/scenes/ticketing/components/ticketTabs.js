112428cc2ce0f4b6e4323a8d15b36e972e516be3
goffle <se.legoff@gmail.com>
2021-09-23 16:11:50 +0200

    up

--- admin/src/scenes/ticketing/components/ticketTabs.js
+++ admin/src/scenes/ticketing/components/ticketTabs.js
@@ -21,23 +21,15 @@ export default ({ setSelectedTicket }) => {
     })();
   }, []);
 
-  // useEffect(() => {
-  //   const fetchTickets = async () => {
-  //     try {
-  //       const response = await api.get("/support-center/ticket");
-  //       if (!response.ok) {
-  //         setUserTickets([]);
-  //         return console.log(response);
-  //       }
-  //       setUserTickets(response.data);
-  //       console.log(response.data);
-  //     } catch (error) {
-  //       setUserTickets([]);
-  //       console.log(error);
-  //     }
-  //   };
-  //   fetchTickets();
-  // }, []);
+  const getFrom = (ticket) => {
+    if (!ticket.articles.length) return "";
+    return ticket.articles[1].from;
+  };
+
+  const getDate = (ticket) => {
+    console.log("ticket", ticket);
+    return (ticket.created_at || "").slice(0, 10);
+  };
 
   return (
     <HeroContainer>
@@ -79,10 +71,10 @@ export default ({ setSelectedTicket }) => {
         {tickets?.map((ticket) => (
           <Link key={ticket.id} className="ticket">
             <div className="ticket-subject">
-              <p>{ticket.name}</p>
-              <p>{ticket.id}</p>
+              <p>{getFrom(ticket)}</p>
+              <p>{getDate(ticket)}</p>
             </div>
-            <p className="ticket-text">{ticket.text}</p>
+            <p className="ticket-text">{ticket.title}</p>
           </Link>
         ))}
       </List>
@@ -112,6 +104,7 @@ const List = styled.div`
     color: blue;
   }
   .ticket {
+    cursor: pointer;
     border-bottom: 1px solid #f1f1f1;
     color: black;
     padding: 1rem 1.5rem;
