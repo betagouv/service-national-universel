031b75d7c42f8a1bc443ad522c8484dc6dd9ed46
Raphaël Huchet <raph@selego.co>
2021-09-24 14:54:42 +0200

    chore: clean redux

--- app/src/redux/auth/actions.js
+++ app/src/redux/auth/actions.js
@@ -4,16 +4,5 @@ export const authActions = {
 };
 
 export function setYoung(young) {
-  if (window && young) {
-    window.zammadUser = {
-      email: young.email,
-      name: young.firstName + " " + young.lastName,
-      _id: young._id,
-      status: translate(young.status),
-      statusPhase1: translate(young.statusPhase1),
-      statusPhase2: translate(young.statusPhase2),
-      statusPhase3: translate(young.statusPhase3),
-    };
-  }
   return { type: authActions.SETYOUNG, young };
 }
