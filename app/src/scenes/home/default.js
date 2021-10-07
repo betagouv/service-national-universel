d707a8d63f871cd9f193c361997cadadf61a9a02
tangimds <tangimds@gmail.com>
2021-10-07 15:05:57 +0200

    Update default.js

--- app/src/scenes/home/default.js
+++ app/src/scenes/home/default.js
@@ -30,18 +30,20 @@ export default () => {
           <p style={{ color: "#161e2e", fontSize: "1.5rem", fontWeight: 700 }}>Votre parcours</p>
           <WrapperItem to="/phase1">
             <div className="title">
-              <span className="link">1. Un séjour de cohésion</span> <Badge text={translate(young.statusPhase1)} color={PHASE_STATUS_COLOR[young.statusPhase1]} />
+              <span className="link">1. Un séjour de cohésion</span>{" "}
+              <Badge style={{ margin: 0 }} text={translate(young.statusPhase1)} color={PHASE_STATUS_COLOR[young.statusPhase1]} />
             </div>
           </WrapperItem>
           <WrapperItem to="/phase2">
             <div className="title">
-              <span className="link">2. Une première mission d'intérêt général</span> <Badge text={translate(young.statusPhase2)} color={PHASE_STATUS_COLOR[young.statusPhase2]} />
+              <span className="link">2. Une première mission d'intérêt général</span>{" "}
+              <Badge style={{ margin: 0 }} text={translate(young.statusPhase2)} color={PHASE_STATUS_COLOR[young.statusPhase2]} />
             </div>
           </WrapperItem>
           <WrapperItem to="/phase3">
             <div className="title">
               <span className="link">3. Un engagement vers une société plus solidaire</span>
-              <Badge text={translate(young.statusPhase3)} color={PHASE_STATUS_COLOR[young.statusPhase3]} />
+              <Badge style={{ margin: 0 }} text={translate(young.statusPhase3)} color={PHASE_STATUS_COLOR[young.statusPhase3]} />
             </div>
           </WrapperItem>
         </Content>
@@ -53,22 +55,23 @@ export default () => {
 
 const WrapperItem = styled(Link)`
   .title {
+    font-size: 1.1rem;
     display: flex;
-    flex-direction: column;
+    flex-wrap: wrap;
+    gap: 0.5rem;
     align-items: center;
     color: #161e2e;
-    margin-bottom: 0.5rem;
+    margin-bottom: 0.8rem;
     font-weight: 500;
     .link:hover {
       text-decoration: underline;
     }
   }
-  @media (min-width: 768px) {
+  @media (max-width: 768px) {
     margin-bottom: 1rem;
     .title {
-      flex-direction: row;
-      justify-content: space-between;
-      font-size: 1.25rem;
+      flex-direction: column;
+      font-size: 1rem;
       .link {
         margin-right: 0.5rem;
       }
