4f8cf9bfc1cc34ad72ce7bb20984a90f5dadd52f
Lucien Klein <44434935+lucienklein@users.noreply.github.com>
2021-10-13 15:16:50 +0200

    fix: couleur bouton violet (#540)
    * fix: couleur bouton violet
    Fixes #534
    
    * fix: couleur bouton violet
    Fixes #534
    
    * fix: override child css

--- admin/src/components/buttons/LoadingButton.js
+++ admin/src/components/buttons/LoadingButton.js
@@ -5,45 +5,49 @@ import { colors } from "../../utils";
 
 export default ({ loading, children, disabled, loadingText, ...rest }) => {
   return (
-    <VioletButtonHeader
-      disabled={loading || disabled}
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        opacity: loading ? 0.7 : 1,
-        cursor: loading || disabled ? "not-allowed" : "pointer",
-      }}
-      {...rest}
-    >
-      {loading && loadingText && (
-        <>
-          <Spinner size="sm" key={loadingText} style={{ borderWidth: "0.1em", marginRight: "0.5rem" }} />
-          {loadingText}
-        </>
-      )}
-      {loading && !loadingText && <Spinner size="sm" style={{ borderWidth: "0.1em" }} />}
-      {!loading && children}
+    <VioletButtonHeader>
+      <Button
+        disabled={loading || disabled}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          opacity: loading ? 0.7 : 1,
+          cursor: loading || disabled ? "not-allowed" : "pointer",
+        }}
+        {...rest}
+      >
+        {loading && loadingText && (
+          <>
+            <Spinner size="sm" key={loadingText} style={{ borderWidth: "0.1em", marginRight: "0.5rem" }} />
+            {loadingText}
+          </>
+        )}
+        {loading && !loadingText && <Spinner size="sm" style={{ borderWidth: "0.1em" }} />}
+        {!loading && children}
+      </Button>
     </VioletButtonHeader>
   );
 };
 
-const VioletButtonHeader = styled(Button)`
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  border: none;
-  border-radius: 5px;
-  padding: 7px 30px;
-  margin: 0;
-  margin-left: 1rem;
-  font-size: 14px;
-  font-weight: 700;
-  color: ${({ textColor }) => (textColor ? `${textColor}` : `#fff`)} !important;
-  cursor: pointer;
-  ${({ disabled, color }) => (!disabled && !color ? `:hover {background: ${colors.darkPurple};}` : null)}
-  ${({ color }) => (color ? `background-color: ${color};}` : `background-color: ${colors.purple};`)}
+const VioletButtonHeader = styled.div`
+  .btn-secondary {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    border: none;
+    border-radius: 5px;
+    padding: 7px 30px;
+    margin: 0;
+    margin-left: 1rem;
+    font-size: 14px;
+    font-weight: 700;
+    color: ${({ textColor }) => (textColor ? `${textColor}` : `#fff`)} !important;
+    cursor: pointer;
+    ${({ disabled, color }) => (!disabled && !color ? `:hover {background: ${colors.darkPurple};}` : null)}
+    ${({ color }) => (color ? `background-color: ${color};` : `background-color: ${colors.purple};`)}
   &:hover {
-    box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.5);
+      box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.5);
+    }
   }
 `;
