bb889b04653aeb7aabcead633d9c237dd91f7368
tangimds <tangimds@gmail.com>
2021-10-13 15:26:41 +0200

    Revert "fix: couleur bouton violet (#540)"
    This reverts commit 4f8cf9bfc1cc34ad72ce7bb20984a90f5dadd52f.

--- app/src/scenes/auth/components/Submit.js
+++ app/src/scenes/auth/components/Submit.js
@@ -2,25 +2,23 @@ import styled from "styled-components";
 import LoadingButton from "../../../components/loadingButton";
 
 export default styled(LoadingButton)`
-  && {
-    width: 100%;
-    display: block;
-    font-size: 18px;
-    font-weight: 700;
-    border-radius: 0;
-    padding: 12px;
-    border: 0;
-    background-color: #5145cd;
-    margin-top: 30px;
-    margin-bottom: 30px;
-    border-radius: 10px;
+  width: 100%;
+  display: block;
+  font-size: 18px;
+  font-weight: 700;
+  border-radius: 0;
+  padding: 12px;
+  border: 0;
+  background-color: #5145cd;
+  margin-top: 30px;
+  margin-bottom: 30px;
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
+  cursor: pointer;
+  :hover {
+    background-color: #42389d;
+  }
+  :focus {
     box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
-    cursor: pointer;
-    :hover {
-      background-color: #42389d;
-    }
-    :focus {
-      box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
-    }
   }
 `;
