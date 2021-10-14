1f403273728c6b6a7a9dbebaf2b1dec0747369a9
Tangi Mendès <tangimds@gmail.com>
2021-10-14 14:22:22 +0200

    feat: cron auto archive outdated missions (#456)
    only `console.log` for now
    testing in prod

--- api/src/crons/index.js
+++ api/src/crons/index.js
@@ -6,6 +6,7 @@ const { sendRecapDepartmentTuesday, sendRecapDepartmentThursday } = require("./m
 const apiEngagement = require("./syncApiEngagement");
 const { capture } = require("../sentry");
 const autoAffectationCohesionCenter = require("./autoAffectationCohesionCenter");
+const missionOutdated = require("./missionOutdated");
 
 // dev : */5 * * * * * (every 5 secs)
 // prod : 0 8 * * 1 (every monday at 0800)
@@ -42,4 +43,10 @@ if (ENVIRONMENT === "production" && process.env.INSTANCE_NUMBER === "0") {
   // cron.schedule(EVERY_HOUR, () => {
   //   autoAffectationCohesionCenter.handler();
   // });
+
+  // everyday at 02:00 UTC
+  cron.schedule(EVERY_MINUTE, () => {
+    missionOutdated.handler();
+    missionOutdated.handlerNotice1Week();
+  });
 }
