c0370a7e2eb8eaebe92b24e07e73ed053bc01075
tangimds <tangimds@gmail.com>
2021-10-18 14:27:35 +0200

    Update referent.js

--- api/src/controllers/referent.js
+++ api/src/controllers/referent.js
@@ -411,7 +411,7 @@ router.get("/youngFile/:youngId/:key/:fileName", passport.authenticate("referent
     try {
       const { mime } = await FileType.fromBuffer(decryptedBuffer);
       mimeFromFile = mime;
-    } catch (e) { }
+    } catch (e) {}
 
     return res.status(200).send({
       data: Buffer.from(decryptedBuffer, "base64"),
@@ -582,6 +582,7 @@ router.get("/", passport.authenticate(["referent"], { session: false }), async (
     const { error, value } = Joi.string().required().email().validate(req.query.email);
     if (error) return res.status(400).send({ ok: false, code: ERRORS.INVALID_PARAMS, error: error.message });
     let data = await ReferentModel.findOne({ email: value });
+    if (!data) return res.status(404).send({ ok: false, code: ERRORS.NOT_FOUND });
     return res.status(200).send({ ok: true, data: serializeReferent(data, req.user) });
   } catch (error) {
     capture(error);
