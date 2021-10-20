9dc3ade9b472c49aa7d6e47ca912e36be86dc406
tangimds <tangimds@gmail.com>
2021-10-20 14:32:22 +0200

    Update youngs.js

--- admin/src/scenes/missions/view/youngs.js
+++ admin/src/scenes/missions/view/youngs.js
@@ -241,7 +241,7 @@ const Hit = ({ hit, onClick, onChangeApplication, selected }) => {
             />
           </React.Fragment>
         )}
-        {hit.status === "WAITING_VERIFICATION" && (!young.statusMilitaryPreparationFiles || young.statusMilitaryPreparationFiles === "WAITING_UPLOAD") ? (
+        {hit.status === "WAITING_VERIFICATION" && (
           <React.Fragment>
             <CopyLink
               onClick={async () => {
@@ -274,7 +274,7 @@ const Hit = ({ hit, onClick, onChangeApplication, selected }) => {
               }}
             />
           </React.Fragment>
-        ) : null}
+        )}
       </td>
     </tr>
   );
