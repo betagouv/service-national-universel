86b786148f3c77d8af9433bd660ffa2c54184c6a
tangimds <tangimds@gmail.com>
2021-10-26 13:27:16 +0200

    fix(wording): motif exempted phase 1 in app

--- app/src/scenes/phase1/cancel.js
+++ app/src/scenes/phase1/cancel.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { HeroContainer, Hero, Content } from "../../components/Content";
-
+import { YOUNG_STATUS_PHASE1_MOTIF, translate } from "../../utils";
 export default () => {
   const young = useSelector((state) => state.Auth.young);
   return (
@@ -11,6 +11,9 @@ export default () => {
           <h1>
             <strong>{young.firstName},</strong> vous avez été dispensé(e) de séjour de cohésion
           </h1>
+          {young.statusPhase1Motif ? (
+            <p>Motif : {young.statusPhase1Motif === YOUNG_STATUS_PHASE1_MOTIF.OTHER ? `${young.statusPhase1MotifDetail}` : `${translate(young.statusPhase1Motif)}`}</p>
+          ) : null}
           <div style={{ marginTop: "1rem", fontStyle: "italic" }}>
             Si vous n'avez pas réalisé votre JDC, nous vous invitons à vous inscrire sur{" "}
             <a href="http://majdc.fr" target="_blank">
