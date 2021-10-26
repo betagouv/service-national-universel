3c420efee5c3021ed33393928515afc56551b32f
Lucien Klein <lucien.kln@gmail.com>
2021-10-26 10:44:59 +0200

    fix: feedback PR

--- api/src/controllers/young/index.js
+++ api/src/controllers/young/index.js
@@ -67,8 +67,8 @@ router.post("/signup", async (req, res) => {
     const { password, email, lastName, firstName, birthdateAt } = value;
     if (!validatePassword(password)) return res.status(400).send({ ok: false, user: null, code: ERRORS.PASSWORD_NOT_VALIDATED });
 
-    const userExist = await YoungObject.find({ lastName, firstName, birthdateAt });
-    if (userExist.length > 0) return res.status(409).send({ ok: false, code: ERRORS.USER_ALREADY_REGISTERED });
+    const countDocuments = await YoungObject.countDocuments({ lastName, firstName, birthdateAt });
+    if (userExist > 0) return res.status(409).send({ ok: false, code: ERRORS.USER_ALREADY_REGISTERED });
 
     const user = await YoungObject.create({ password, email, firstName, lastName, birthdateAt });
     const token = jwt.sign({ _id: user._id }, config.secret, { expiresIn: JWT_MAX_AGE });
