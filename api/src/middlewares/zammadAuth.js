a041eb6d67612f425c1f51612bd7bb0af9edeb00
Chloe Danisevicius <62291986+SpookyUmi@users.noreply.github.com>
2021-10-28 11:01:23 +0200

    Update api/src/middlewares/zammadAuth.js
    Co-authored-by: Tangi Mendès <tangimds@gmail.com>

--- api/src/middlewares/zammadAuth.js
+++ api/src/middlewares/zammadAuth.js
@@ -3,7 +3,7 @@ const { ERRORS } = require("../utils");
 
 const admin = (req, res, next) => {
   if (req.headers['x-forwarded-for'] !== ZAMMAD_IP) {
-    res.status(401).send({ ok: false, code: ERRORS.OPERATION_UNAUTHORIZED });
+    res.status(401).send({ ok: false, code: ERRORS.INVALID_IP, message: "Invalid IP" });
     return;
   }
   next();
