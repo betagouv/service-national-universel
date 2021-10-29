1faf9a2d5609d62648052ba5e5342c8a61ebb9e6
Lucien Klein <lucien.kln@gmail.com>
2021-10-29 12:15:57 +0200

    fix(inscription): display only the name of the current step Fixes #631

--- app/src/scenes/inscription/components/Nav.js
+++ app/src/scenes/inscription/components/Nav.js
@@ -12,15 +12,6 @@ export default ({ step }) => {
   const dispatch = useDispatch();
   const young = useSelector((state) => state.Auth.young);
 
-  const currentIndex = Object.keys(STEPS).indexOf(step);
-
-  const getStatus = (s) => {
-    if (s === step) return "inprogress";
-    const stepIndex = Object.keys(STEPS).indexOf(s);
-    if (stepIndex > currentIndex) return "todo";
-    return "done";
-  };
-
   const logout = async () => {
     try {
       await api.post(`/young/logout`);
@@ -41,6 +32,31 @@ export default ({ step }) => {
     return history.push(`/inscription/${s.toLowerCase()}`);
   };
 
+  const Step = ({ stepId, stepName, stepNumber }) => {
+    const currentIndex = Object.keys(STEPS).indexOf(step);
+
+    const status = (() => {
+      if (stepId === step) return "inprogress";
+      const stepIndex = Object.keys(STEPS).indexOf(stepId);
+      if (stepIndex > currentIndex) return "todo";
+      return "done";
+    })();
+
+    return (
+      <>
+        <Element status={status} onClick={() => handleClick(stepId)}>
+          <a>
+            <span className="icon">
+              <span>{stepNumber}</span>
+            </span>
+            {status === "inprogress" && <span className="text">{stepName}</span>}
+          </a>
+        </Element>
+        <Divider status={status} />
+      </>
+    );
+  };
+
   return (
     <>
       <HeaderNav>
@@ -58,54 +74,12 @@ export default ({ step }) => {
         )}
       </HeaderNav>
       <Topbar>
-        <Element status={getStatus(STEPS.PROFIL)} onClick={() => handleClick(STEPS.PROFIL)}>
-          <a>
-            <span className="icon">
-              <span>01</span>
-            </span>
-            <span className="text">Profil</span>
-          </a>
-        </Element>
-        <Element status={getStatus(STEPS.COORDONNEES)} onClick={() => handleClick(STEPS.COORDONNEES)}>
-          <a>
-            <span className="icon">
-              <span>02</span>
-            </span>
-            <span className="text">Coordonnées</span>
-          </a>
-        </Element>
-        <Element status={getStatus(STEPS.PARTICULIERES)} onClick={() => handleClick(STEPS.PARTICULIERES)}>
-          <a>
-            <span className="icon">
-              <span>03</span>
-            </span>
-            <span className="text">Situations particulières</span>
-          </a>
-        </Element>
-        <Element status={getStatus(STEPS.REPRESENTANTS)} onClick={() => handleClick(STEPS.REPRESENTANTS)}>
-          <a>
-            <span className="icon">
-              <span>04</span>
-            </span>
-            <span className="text">Représentants légaux</span>
-          </a>
-        </Element>
-        <Element status={getStatus(STEPS.CONSENTEMENTS)} onClick={() => handleClick(STEPS.CONSENTEMENTS)}>
-          <a>
-            <span className="icon">
-              <span>05</span>
-            </span>
-            <span className="text">Consentements</span>
-          </a>
-        </Element>
-        <Element status={getStatus(STEPS.DOCUMENTS)} onClick={() => handleClick(STEPS.DOCUMENTS)}>
-          <a>
-            <span className="icon">
-              <span>06</span>
-            </span>
-            <span className="text">Pièces justificatives</span>
-          </a>
-        </Element>
+        <Step stepId={STEPS.PROFIL} stepName="Profil" stepNumber="01" />
+        <Step stepId={STEPS.COORDONNEES} stepName="Coordonnées" stepNumber="02" />
+        <Step stepId={STEPS.PARTICULIERES} stepName="Situations particulières" stepNumber="03" />
+        <Step stepId={STEPS.REPRESENTANTS} stepName="Représentants légaux" stepNumber="04" />
+        <Step stepId={STEPS.CONSENTEMENTS} stepName="Consentements" stepNumber="05" />
+        <Step stepId={STEPS.DOCUMENTS} stepName="Pièces justificatives" stepNumber="06" />
         <Element onClick={() => handleClick(STEPS.DONE)} style={{ flexGrow: 0 }}>
           <div className="logo" />
         </Element>
@@ -113,6 +87,17 @@ export default ({ step }) => {
     </>
   );
 };
+
+const Divider = styled.div`
+  flex-grow: 1;
+  border-bottom: 2px solid;
+  border-color: ${(props) => {
+    if (props.status === "done") return "#362F78";
+    return "#7F8591";
+  }};
+  margin: 5px;
+  align-self: center;
+`;
 
 const HeaderNav = styled.div`
   display: flex;
@@ -176,6 +161,10 @@ const Topbar = styled.ul`
 
 const Element = styled.li`
   flex: 1;
+  flex-grow: ${(props) => {
+    if (props.status === "inprogress") return "1";
+    return "0";
+  }};
   position: relative;
   display: flex;
   align-items: center;
