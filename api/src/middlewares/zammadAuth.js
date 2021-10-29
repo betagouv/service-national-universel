1bf95eea9137ef328b764fe6df523f65727bd421
SpookyUmi <chloe.danisevicius@gmail.com>
2021-10-29 14:42:10 +0200

    fix(zammadAuth): add logs and remove tripe equals

--- api/src/middlewares/zammadAuth.js
+++ api/src/middlewares/zammadAuth.js
@@ -2,7 +2,9 @@ const { ZAMMAD_IP } = require("../config");
 const { ERRORS } = require("../utils");
 
 const admin = (req, res, next) => {
-  if (req.headers['x-forwarded-for'] !== ZAMMAD_IP) {
+  console.log("|---- IP ADDRESS ----|", req.headers['x-forwarded-for'], typeof req.headers['x-forwarded-for']);
+  console.log("|---- ZAMMAD IP ADDRESS ----|", ZAMMAD_IP, typeof ZAMMAD_IP);
+  if (req.headers['x-forwarded-for'] != ZAMMAD_IP) {
     res.status(401).send({ ok: false, code: ERRORS.INVALID_IP, message: "Invalid IP" });
     return;
   }
