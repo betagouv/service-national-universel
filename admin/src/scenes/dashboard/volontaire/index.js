f0c1064e705f123fff75adcf3a59901b78f5a2e8
Lucien Klein <lucien.kln@gmail.com>
2021-11-02 15:39:55 +0100

    fix(inscription): préparer dashboard pour inscription 2022 Fixes #642

--- admin/src/scenes/dashboard/volontaire/index.js
+++ admin/src/scenes/dashboard/volontaire/index.js
@@ -33,10 +33,10 @@ export default () => {
   return (
     <>
       <Row style={{}}>
-        <Col md={6}>
+        <Col md={3}>
           <Title>Volontaires</Title>
         </Col>
-        <Col md={6}>
+        <Col md={9}>
           {filter ? (
             <>
               <FiltersList>
@@ -48,6 +48,7 @@ export default () => {
                       { key: "2019", label: "2019" },
                       { key: "2020", label: "2020" },
                       { key: "2021", label: "2021" },
+                      { key: "2022", label: "2022" },
                       { key: "", label: "Toutes" },
                     ]}
                     onChange={(cohort) => updateFilter({ cohort })}
