cbb6d8b92cef7a277e18537d1e10d64d61432c9a
tangimds <tangimds@gmail.com>
2021-11-04 12:24:08 +0100

    up

--- app/src/scenes/home/index.js
+++ app/src/scenes/home/index.js
@@ -15,17 +15,17 @@ export default () => {
   const young = useSelector((state) => state.Auth.young) || {};
 
   const renderStep = () => {
-    if (young.cohort === "2022") {
-      if (young.status === YOUNG_STATUS.WITHDRAWN) return <Withdrawn />;
+    // screens that doesn't depend on the cohort of the young
+    if (young.status === YOUNG_STATUS.WITHDRAWN) return <Withdrawn />;
+    if (young.status === YOUNG_STATUS.WAITING_LIST) return <WaitingList />;
+    if (young.status === YOUNG_STATUS.REFUSED) return <Refused />;
+    if (["2022", "Juillet 2022", "Juin 2022", "Février 2022"].includes(young.cohort)) {
+      // they are in the new cohort, we display the inscription step
       if (young.status === YOUNG_STATUS.WAITING_CORRECTION) return <WaitingCorrection />;
       if (young.status === YOUNG_STATUS.WAITING_VALIDATION) return <WaitingValidation />;
-      if (young.status === YOUNG_STATUS.WAITING_LIST) return <WaitingList />;
-      if (young.status === YOUNG_STATUS.REFUSED) return <Refused />;
       if (young.status === YOUNG_STATUS.VALIDATED) return <Validated />;
-    } else if (young.status === YOUNG_STATUS.REFUSED && young.cohort === "2021") {
-      // screen for re-inscription
-      return <Reinscription />;
-    } else return <Default />;
+    }
+    return <Default />;
   };
 
   return renderStep();
--- app/src/scenes/home/refused.js
+++ app/src/scenes/home/refused.js
@@ -23,11 +23,20 @@ export default () => {
             <strong>{young.firstName},</strong> bienvenue sur votre compte volontaire.
           </h1>
           <IconContainer>
-            <svg width="48" height="48" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="48" height="48" rx="24" fill="#FFEBEB" /><path d="M30.364 30.364L17.636 17.636m12.728 12.728a9 9 0 00-12.728-12.728l12.728 12.728zm0 0a9 9 0 01-12.728-12.728l12.728 12.728z" stroke="#EF4036" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" /></svg>
+            <svg width="48" height="48" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <rect width="48" height="48" rx="24" fill="#FFEBEB" />
+              <path
+                d="M30.364 30.364L17.636 17.636m12.728 12.728a9 9 0 00-12.728-12.728l12.728 12.728zm0 0a9 9 0 01-12.728-12.728l12.728 12.728z"
+                stroke="#EF4036"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
             <p style={{ color: "#000" }}>
               <strong>Votre inscription n’a pas pu être retenue</strong>
               <br />
-              <p>Suite au traitement de votre dossier d’inscription, votre référent SNU de votre département n’a pu retenir votre inscirption au motif de :</p>
+              <p>Suite au traitement de votre dossier d’inscription, votre référent SNU de votre département n’a pu retenir votre inscription.</p>
               {/* //TODO : params.message */}
             </p>
           </IconContainer>
@@ -42,6 +51,6 @@ const IconContainer = styled.div`
   display: flex;
   margin-top: 2.5rem;
   svg {
-    min-width: 4rem
+    min-width: 4rem;
   }
 `;
