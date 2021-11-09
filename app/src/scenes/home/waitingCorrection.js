f073073454a286e7dbc6e2a3d7da45c636750b2d
Chloe Danisevicius <62291986+SpookyUmi@users.noreply.github.com>
2021-11-09 17:20:23 +0100

    Update app/src/scenes/home/waitingCorrection.js
    Co-authored-by: Tangi Mendès <tangimds@gmail.com>

--- app/src/scenes/home/waitingCorrection.js
+++ app/src/scenes/home/waitingCorrection.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
