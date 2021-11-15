bdfa66c93f3fcbea9bec54e1f95121b1945a473f
tangimds <tangimds@gmail.com>
2021-11-15 15:40:10 +0100

    Update stepDone.js

--- app/src/scenes/inscription/Create/stepDone.js
+++ app/src/scenes/inscription/Create/stepDone.js
@@ -43,6 +43,7 @@ export default () => {
               toastr.success("Enregistré");
               dispatch(setYoung(data));
               await api.post(`/young/${young._id}/email/${SENDINBLUE_TEMPLATES.young.INSCRIPTION_WAITING_VALIDATION}`, { cta: `${appURL}/auth` });
+              history.push("/");
             } catch (e) {
               console.log(e);
               toastr.error("Oups, une erreur est survenue pendant le traitement du formulaire :", translate(e.code));
