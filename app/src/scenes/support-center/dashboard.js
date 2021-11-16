2a9f46d0ed9f6c735ebdccf30301ad5a11c839e7
Chloe Danisevicius <62291986+SpookyUmi@users.noreply.github.com>
2021-11-16 11:44:54 +0100

    Update app/src/scenes/support-center/dashboard.js
    Co-authored-by: Tangi Mendès <tangimds@gmail.com>

--- app/src/scenes/support-center/dashboard.js
+++ app/src/scenes/support-center/dashboard.js
@@ -85,7 +85,7 @@ export default () => {
 
   return (
     <HeroContainer>
-      <NavLink className="link" style={{ color: "#32257F", fontWeight: "bold" }} to="/profil"> <svg width="8" height="11" viewBox="0 0 6 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <NavLink style={{ color: "#32257F", fontWeight: "bold" }} to="/"> <svg width="8" height="11" viewBox="0 0 6 9" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M4.42367 0L0.423279 4.00716L4.42367 8.01432L5.41483 7.02148L2.4056 4.00716L5.41483 0.992838L4.42367 0Z" fill="#32257F" />
       </svg> Retour à mon espace</NavLink>
       <Container>
