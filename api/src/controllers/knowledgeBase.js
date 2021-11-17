11211706fb8bd4790b36a7da1b3249572e73b167
Arnaud AMBROSELLI <arnaudambroselli@pc72.home>
2021-11-17 14:23:25 +0100

    setup knowledgeBase controller

--- api/src/controllers/knowledgeBase.js
+++ api/src/controllers/knowledgeBase.js
@@ -7,9 +7,38 @@ const KnowledgeBaseObject = require("../models/knowledgeBase");
 const { ERRORS } = require("../utils/index.js");
 const { validateId } = require("../utils/validator");
 
+const findChildrenRecursive = async (section, allChildren, findAll = false) => {
+  if (section.type !== "section") return;
+  const children = await KnowledgeBaseObject.find({ parentId: section._id });
+  for (const child of children) {
+    allChildren.push(child);
+    if (findAll) await findChildrenRecursive(child, allChildren, findAll);
+  }
+};
+
+const findChildren = async (section, findAll = false) => {
+  const allChildren = [];
+  await findChildrenRecursive(section, allChildren, findAll);
+  return allChildren;
+};
+
 router.post("/", passport.authenticate("referent", { session: false, failWithError: true }), async (req, res) => {
   try {
-    return res.status(200).send({ ok: true });
+    const kb = {};
+
+    if (req.body.hasOwnProperty("type")) kb.type = req.body.type;
+    if (req.body.hasOwnProperty("parentId")) kb.parentId = req.body.parentId;
+    if (req.body.hasOwnProperty("position")) kb.position = req.body.position;
+    if (req.body.hasOwnProperty("content")) kb.content = req.body.content;
+    if (req.body.hasOwnProperty("description")) kb.description = req.body.description;
+    if (req.body.hasOwnProperty("allowedRoles")) kb.allowedRoles = req.body.allowedRoles;
+    if (req.body.hasOwnProperty("status")) kb.status = req.body.status;
+    if (req.body.hasOwnProperty("author")) kb.author = req.body.author;
+    if (req.body.hasOwnProperty("read")) kb.read = req.body.read;
+
+    const newKb = await KnowledgeBaseObject.create(kb);
+
+    return res.status(200).send({ ok: true, data: newKb });
   } catch (error) {
     capture(error);
     res.status(500).send({ ok: false, code: ERRORS.SERVER_ERROR, error });
@@ -18,7 +47,25 @@ router.post("/", passport.authenticate("referent", { session: false, failWithErr
 
 router.put("/:id", passport.authenticate("referent", { session: false, failWithError: true }), async (req, res) => {
   try {
-    return res.status(200).send({ ok: true });
+    const existingKb = await KnowledgeBaseObject.findById(req.params.id);
+    if (!existingKb) return res.status(404).send({ ok: false, code: ERRORS.NOT_FOUND });
+
+    const updateKb = {};
+
+    if (req.body.hasOwnProperty("type")) updateKb.type = req.body.type;
+    if (req.body.hasOwnProperty("parentId")) updateKb.parentId = req.body.parentId;
+    if (req.body.hasOwnProperty("position")) updateKb.position = req.body.position;
+    if (req.body.hasOwnProperty("content")) updateKb.content = req.body.content;
+    if (req.body.hasOwnProperty("description")) updateKb.description = req.body.description;
+    if (req.body.hasOwnProperty("allowedRoles")) updateKb.allowedRoles = req.body.allowedRoles;
+    if (req.body.hasOwnProperty("status")) updateKb.status = req.body.status;
+    if (req.body.hasOwnProperty("author")) updateKb.author = req.body.author;
+    if (req.body.hasOwnProperty("read")) updateKb.read = req.body.read;
+
+    existingKb.set(updateKb);
+    await existingKb.save();
+
+    return res.status(200).send({ ok: true, data: existingKb });
   } catch (error) {
     capture(error);
     res.status(500).send({ ok: false, code: ERRORS.SERVER_ERROR, error });
@@ -27,7 +74,15 @@ router.put("/:id", passport.authenticate("referent", { session: false, failWithE
 
 router.get("/:id", passport.authenticate(["referent", "young"], { session: false, failWithError: true }), async (req, res) => {
   try {
-    return res.status(200).send({ ok: true });
+    const existingKb = await KnowledgeBaseObject.findById(req.params.id);
+    if (!existingKb) return res.status(404).send({ ok: false, code: ERRORS.NOT_FOUND });
+
+    if (req.query.withDirectChildren === "true" || req.query.withAllChildren === "true") {
+      const children = findChildren(existingKb, req.query.withAllChildren === "true");
+      existingKb.children = children;
+    }
+
+    return res.status(200).send({ ok: true, data: existingKb });
   } catch (error) {
     capture(error);
     res.status(500).send({ ok: false, code: ERRORS.SERVER_ERROR, error });
@@ -42,7 +97,10 @@ router.delete("/:id", passport.authenticate("referent", { session: false, failWi
     const kb = await KnowledgeBaseObject.findById(checkedId);
     if (!kb) return res.status(404).send({ ok: false, code: ERRORS.NOT_FOUND });
 
-    // remove children too
+    const childrenToDelete = await findChildren(kb);
+    for (const child of [kb, ...childrenToDelete]) {
+      await KnowledgeBaseObject.findByIdAndDelete(child._id);
+    }
 
     res.status(200).send({ ok: true });
   } catch (error) {
