e125a8394dadcbd5ea7dcbbd84db5205cc184876
tangimds <tangimds@gmail.com>
2021-11-23 16:40:26 +0100

    Update index.js

--- app/src/scenes/home/index.js
+++ app/src/scenes/home/index.js
@@ -36,7 +36,6 @@ export default () => {
           <Refused />
         </>
       );
-    if (young.status === YOUNG_STATUS.REFUSED) return <Refused />;
     if (["2022", "Juillet 2022", "Juin 2022", "Février 2022"].includes(young.cohort)) {
       // they are in the new cohort, we display the inscription step
       if (young.status === YOUNG_STATUS.WAITING_CORRECTION) return <WaitingCorrection />;
