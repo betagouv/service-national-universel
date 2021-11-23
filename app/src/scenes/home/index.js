353cdc5cdd75edf511d0d86fb8d9d802387f7c4e
tangimds <tangimds@gmail.com>
2021-11-23 16:34:28 +0100

    remove console log

--- app/src/scenes/home/index.js
+++ app/src/scenes/home/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 
 import { YOUNG_STATUS } from "../../utils";
@@ -13,32 +13,32 @@ import Banner from "./components/banner";
 
 export default () => {
   const young = useSelector((state) => state.Auth.young) || {};
-  useEffect(() => {
-    console.log("YOUNG", young);
-  }, []);
 
   const renderStep = () => {
     // screens that doesn't depend on the cohort of the young
-    if (young.status === YOUNG_STATUS.WITHDRAWN && young.cohort === "2021") return (
-      <>
-        <Withdrawn />
-        <Banner />
-      </>
-    )
+    if (young.status === YOUNG_STATUS.WITHDRAWN && young.cohort === "2021")
+      return (
+        <>
+          <Withdrawn />
+          <Banner />
+        </>
+      );
     if (young.status === YOUNG_STATUS.WITHDRAWN) return <Withdrawn />;
-    if (young.status === YOUNG_STATUS.WAITING_LIST && young.cohort === "2021") return (
-      <>
-        <WaitingList />
-        <Banner />
-      </>
-    );
+    if (young.status === YOUNG_STATUS.WAITING_LIST && young.cohort === "2021")
+      return (
+        <>
+          <WaitingList />
+          <Banner />
+        </>
+      );
     if (young.status === YOUNG_STATUS.WAITING_LIST) return <WaitingList />;
-    if (young.status === YOUNG_STATUS.REFUSED && young.cohort === "2021") return (
-      <>
-        <Refused />
-        <Banner />
-      </>
-    );
+    if (young.status === YOUNG_STATUS.REFUSED && young.cohort === "2021")
+      return (
+        <>
+          <Refused />
+          <Banner />
+        </>
+      );
     if (young.status === YOUNG_STATUS.REFUSED) return <Refused />;
     if (["2022", "Juillet 2022", "Juin 2022", "Février 2022"].includes(young.cohort)) {
       // they are in the new cohort, we display the inscription step
