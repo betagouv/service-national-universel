9c7cb6f566b0fd243e1187fafca596bced703bfa
tangimds <tangimds@gmail.com>
2021-11-23 16:42:56 +0100

    Update banner.js

--- app/src/scenes/home/components/banner.js
+++ app/src/scenes/home/components/banner.js
@@ -7,17 +7,19 @@ export default () => {
   const young = useSelector((state) => state.Auth.young);
 
   return (
-
     <HeroContainer>
       <Hero>
         <Container>
           <svg width="48" height="48" fill="none" xmlns="http://www.w3.org/2000/svg">
             <rect width="48" height="48" rx="24" fill="#FFFBEB" />
-            <path fill-rule="evenodd" clip-rule="evenodd"
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
               d="M22.503 15.172a2.194 2.194 0 013.883 0L32.6 26.574c.835 1.533-.238 3.426-1.942 3.426H18.23c-1.703 0-2.776-1.893-1.94-3.426l6.213-11.402zm3.055 11.38c0 .635-.499 1.15-1.114 1.15-.615 0-1.113-.515-1.113-1.15 0-.635.498-1.15 1.113-1.15s1.114.515 1.114 1.15zm-1.114-9.195c-.615 0-1.113.515-1.113 1.15v3.447c0 .635.498 1.15 1.113 1.15s1.114-.515 1.114-1.15v-3.448c0-.634-.499-1.15-1.114-1.15z"
-              fill="#FBBF24" />
+              fill="#FBBF24"
+            />
           </svg>
-          <section>
+          <section style={{ flex: 1 }}>
             <p>Vous n'avez pas pu participer au séjour de cohésion en 2021.</p>
             <p className="small">Si vous souhaitez participer à la session 2022 :</p>
             <section className="button_container">
@@ -28,7 +30,6 @@ export default () => {
                 <VioletButton style={{ borderRadius: "8px" }}>Consultez la procédure d'inscription 2022</VioletButton>
               </a>
             </section>
-
           </section>
         </Container>
       </Hero>
