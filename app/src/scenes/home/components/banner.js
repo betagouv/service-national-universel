5a599e41a974f289221ba63ef45b560ed450dcc2
SpookyUmi <chloe.danisevicius@gmail.com>
2021-11-23 10:04:22 +0100

    feat: banner

--- app/src/scenes/home/components/banner.js
+++ app/src/scenes/home/components/banner.js
@@ -1,26 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { Content, Separator, VioletButton } from "../../../components/Content";
+import { HeroContainer, Content, Hero, VioletButton } from "../../../components/Content";
 
 export default () => {
   const young = useSelector((state) => state.Auth.young);
 
   return (
-    <Content>
-      <h1>
-        <strong>{young.firstName},</strong> dommage que vous nous quittiez !
-      </h1>
-      <p>Votre désistement du SNU a bien été pris en compte.</p>
-      <p>Si l'engagement vous donne envie, vous trouverez ci-dessous des dispositifs qui pourront vous intéresser.</p>
-      <p>
-        Bonne continuation, <br />
-        Les équipes du Service National Universel
-      </p>
-      <Separator />
-      <Link to="/les-programmes">
-        <VioletButton>Consulter les autres possibilités d'engagement</VioletButton>
-      </Link>
-    </Content>
+    <HeroContainer>
+      <Hero>
+        <Content>
+          <p>Vous n'avez pas pu participer au séjour de cohésion en 2021.</p>
+          <p>Si vous souhaitez participer à la session 2022 :</p>
+          <a href="https://support.snu.gouv.fr/help/fr-fr/24-questions-frequemment-posees/181-suis-je-eligible-au-sejour-de-cohesion" target="_blank">
+            <VioletButton>Vérifiez votre éligibilité</VioletButton>
+          </a>
+          <a href="https://support.snu.gouv.fr/help/fr-fr/24-questions-frequemment-posees/175-j-etais-inscrit-en-2021-comment-me-reinscrire-en-2022" target="_blank">
+            <VioletButton>Consultez la procédure d'inscription 2022</VioletButton>
+          </a>
+        </Content>
+      </Hero>
+    </HeroContainer>
   );
 };
