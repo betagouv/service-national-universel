a3d6e47167cd5acee58096d3ebb266f0a23def66
tangimds <tangimds@gmail.com>
2021-11-25 15:02:55 +0100

    Update FilterDepartment.js

--- admin/src/scenes/dashboard/components/FilterDepartment.js
+++ admin/src/scenes/dashboard/components/FilterDepartment.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
-import { departmentList, region2department, REFERENT_ROLES } from "../../../utils";
+import { departmentList, region2department, REFERENT_ROLES, academyToDepartments } from "../../../utils";
 import MultiSelect from "./MultiSelect";
 
 export default ({ value = [], onChange, filter }) => {
   const user = useSelector((state) => state.Auth.user);
   const filteredDepartment = () => {
+    if (filter.academy?.length)
+      return filter.academy?.reduce((previous, current) => {
+        return [...previous, ...academyToDepartments[current]?.map((d) => ({ label: d, value: d }))];
+      }, []);
     if (!filter.region?.length) return departmentList?.map((d) => ({ label: d, value: d }));
     return filter.region?.reduce((previous, current) => previous?.concat(region2department[current]?.map((d) => ({ label: d, value: d }))), []);
   };
