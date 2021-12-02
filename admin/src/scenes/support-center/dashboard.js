84455212120df800da581ca8c22e377afbcfa5a0
Raphaël Huchet <raph@selego.co>
2021-12-02 16:09:33 +0100

    fix: fix blank page

--- admin/src/scenes/support-center/dashboard.js
+++ admin/src/scenes/support-center/dashboard.js
@@ -17,7 +17,8 @@ import { referentArticles, adminArticles, structureArticles } from "./articles";
 const Dashboard = () => {
   const [userTickets, setUserTickets] = useState(null);
   const [articles, setArticles] = useState(null);
-  const [setLink] = useState(null);
+  // eslint-disable-next-line no-unused-vars
+  const [link, setLink] = useState(null);
   const user = useSelector((state) => state.Auth.user);
 
   dayjs.extend(relativeTime).locale("fr");
