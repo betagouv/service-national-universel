48c22db6d0b63ac2c0fdfbbc9c691e66ff332224
Arnaud AMBROSELLI <arnaudambroselli@pc72.home>
2021-12-02 07:29:03 +0100

    fix form

--- support/src/pages/admin/auth.js
+++ support/src/pages/admin/auth.js
@@ -1,5 +1,4 @@
 import { useRouter } from "next/router";
-import { useForm } from "react-hook-form";
 import { toast, ToastContainer } from "react-toastify";
 import Loader from "../../components/Loader";
 import useUser from "../../hooks/useUser";
@@ -9,13 +8,13 @@ const Auth = () => {
   const { isLoading, mutate } = useUser({ redirectOnLoggedIn: "/admin" });
   const router = useRouter();
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-
-  const onSubmit = async (body) => {
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const body = {};
+    for (let [key, value] of formData.entries()) {
+      body[key] = value;
+    }
     const response = await API.post({ path: "/referent/signin", body });
     if (!response.ok) {
       if (response.code === "USER_NOT_EXISTS") {
@@ -32,20 +31,14 @@ const Auth = () => {
   };
 
   if (isLoading) return <Loader />;
-  /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+
   return (
     <>
       <div className="flex w-full h-full items-center justify-center">
-        <form onSubmit={handleSubmit(onSubmit)} className="w-500 h-500 border-2 p-20 flex flex-col">
+        <form onSubmit={onSubmit} className="w-500 h-500 border-2 p-20 flex flex-col">
           <h1 className="text-center mb-5 font-bold">Support - Espace Administrateur</h1>
-          {/* register your input into the hook by invoking the "register" function */}
-          <input className="p-2 border-2 mb-5" placeholder="adresse e-mail" {...register("email")} />
-
-          {/* include validation with required or other standard HTML validation rules */}
-          <input className="p-2 border-2 mb-5" type="password" {...register("password", { required: true })} />
-          {/* errors will return when field validation fails  */}
-          {errors.exampleRequired && <span>This field is required</span>}
-
+          <input className="p-2 border-2 mb-5" placeholder="adresse e-mail" name="email" />
+          <input className="p-2 border-2 mb-5" type="password" name="password" />
           <button type="submit">Se connecter</button>
         </form>
       </div>
