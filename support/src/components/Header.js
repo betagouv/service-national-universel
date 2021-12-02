694236d3f421c46c59bd62fa223b37f79bdef3c9
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2021-12-02 14:08:00 +0100

    feat(support): new navbar (#862)
    Co-authored-by: Arnaud AMBROSELLI <arnaudambroselli@pc72.home>

--- support/src/components/Header.js
+++ support/src/components/Header.js
@@ -1,3 +1,3 @@
-const Header = ({ children }) => <h1 className="py-3 pl-12 pr-8 font-bold text-lg flex justify-between bg-snu-purple-900 text-white">{children}</h1>;
+const Header = ({ children }) => <h1 className="py-2 pl-12 pr-8 font-bold text-lg flex flex-col items-start bg-snu-purple-900 text-white">{children}</h1>;
 
 export default Header;
--- support/src/components/knowledge-base/KnowledgeBaseItemMetadata.js
+++ support/src/components/knowledge-base/KnowledgeBaseItemMetadata.js
@@ -6,7 +6,7 @@ import API from "../../services/api";
 import useKnowledgeBaseData from "../../hooks/useKnowledgeBaseData";
 import InputWithEmojiPicker from "../InputWithEmojiPicker";
 
-const KnowledgeBaseItemMetadata = ({ visible, setVisible }) => {
+const KnowledgeBaseItemMetadata = ({ visible }) => {
   const router = useRouter();
 
   const [slug, setSlug] = useState(router.query?.slug || "");
@@ -45,68 +45,57 @@ const KnowledgeBaseItemMetadata = ({ visible, setVisible }) => {
       router.replace(`/admin/knowledge-base/${parent?.slug || ""}`);
     }
   };
-  console.log(item);
 
   return (
-    <div className={`flex-grow-0 flex-shrink-0 border-l-2 shadow-md resize-x dir-rtl overflow-hidden ${visible ? "w-80" : "w-0 hidden"}`}>
-      <aside className="flex flex-col p-2 border-6 overflow-scroll h-full dir-ltr">
-        <svg
-          onClick={() => setVisible(false)}
-          xmlns="http://www.w3.org/2000/svg"
-          className="flex-grow-0 flex-shrink-0 ml-auto m-2 h-6 w-6 cursor-pointer"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-        </svg>
-        <form onSubmit={onSubmit} className="flex flex-col w-full items-start" key={item._id}>
-          <label htmlFor="title">Titre</label>
-          <InputWithEmojiPicker
-            inputClassName="p-2"
-            className="border-2 mb-5 bg-white w-full"
-            placeholder={`Titre ${item.type === "section" ? "de la rubrique" : "de l'article"}`}
-            name="title"
-            defaultValue={item.title}
-          />
-          <label htmlFor="slug">Slug (Url)</label>
-          <input className="p-2 border-2 mb-5 w-full" placeholder={`Slug ${item.type === "section" ? "de la rubrique" : "de l'article"}`} name="slug" defaultValue={item.slug} />
-          <label htmlFor="description">Description</label>
-          <textarea
-            className="p-2 border-2 mb-5 w-full"
-            placeholder={`Description ${item.type === "section" ? "de la rubrique" : "de l'article"}`}
-            name="description"
-            defaultValue={item.description}
-          />
-          <fieldset className="mb-5">
-            <legend className="mb-2">Visible par:</legend>
-            <div className="flex flex-col ml-4">
-              {Object.keys(SUPPORT_ROLES).map((role) => (
-                <div className="flex items-center" key={role}>
-                  <input className="mr-4" id={`allowedRoles.${role}`} type="checkbox" name={`allowedRoles.${role}`} defaultChecked={item.allowedRoles?.includes(role)} />
-                  <label className="mr-2" id={role} htmlFor={`allowedRoles.${role}`}>
-                    {SUPPORT_ROLES[role]}
-                  </label>
-                </div>
-              ))}
-            </div>
-          </fieldset>
-          <div className="flex justify-start items-center">
-            <label htmlFor="status">Visibilité: </label>
-            <select className="border-2 ml-5 p-2" name="status" defaultValue={item.status}>
-              <option value="PUBLISHED">Publié</option>
-              <option value="DRAFT">Brouillon</option>
-            </select>
+    <aside className={`flex-grow-0 flex-shrink-0 border-l-2 shadow-lg z-10 resize-x dir-rtl overflow-hidden ${visible ? "w-80" : "w-0 hidden"}`}>
+      <form onSubmit={onSubmit} className="flex-grow-0 flex-shrink-0  px-4 py-6 flex flex-col w-full overflow-scroll h-full dir-ltr items-start" key={item._id}>
+        <label htmlFor="title">Titre</label>
+        <InputWithEmojiPicker
+          inputClassName="p-2"
+          className="border-2 mb-5 bg-white w-full"
+          placeholder={`Titre ${item.type === "section" ? "de la rubrique" : "de l'article"}`}
+          name="title"
+          defaultValue={item.title}
+        />
+        <label htmlFor="slug">Slug (Url)</label>
+        <input className="p-2 border-2 mb-5 w-full" placeholder={`Slug ${item.type === "section" ? "de la rubrique" : "de l'article"}`} name="slug" defaultValue={item.slug} />
+        <label htmlFor="description">Description</label>
+        <textarea
+          className="p-2 border-2 mb-5 w-full flex-shrink-0"
+          placeholder={`Description ${item.type === "section" ? "de la rubrique" : "de l'article"}`}
+          name="description"
+          defaultValue={item.description}
+        />
+        <fieldset className="mb-5">
+          <legend className="mb-2">Visible par:</legend>
+          <div className="flex flex-col ml-4">
+            {Object.keys(SUPPORT_ROLES).map((role) => (
+              <div className="flex items-center" key={role}>
+                <input className="mr-4" id={`allowedRoles.${role}`} type="checkbox" name={`allowedRoles.${role}`} defaultChecked={item.allowedRoles?.includes(role)} />
+                <label className="mr-2" id={role} htmlFor={`allowedRoles.${role}`}>
+                  {SUPPORT_ROLES[role]}
+                </label>
+              </div>
+            ))}
           </div>
-          <button type="submit" className="mr-auto mt-8 mb-2">
+        </fieldset>
+        <div className="flex justify-start items-center">
+          <label htmlFor="status">Visibilité: </label>
+          <select className="border-2 ml-5 p-2" name="status" defaultValue={item.status}>
+            <option value="PUBLISHED">Publié</option>
+            <option value="DRAFT">Brouillon</option>
+          </select>
+        </div>
+        <div className="flex flex-wrap justify-around items-center mt-8 mb-2 w-full">
+          <button type="submit" className="mb-2">
             Enregistrer
           </button>
-          <button className="bg-white !border-2 border-red-500  text-red-500 mr-auto" onClick={onDelete}>
+          <button className="bg-white mb-2 !border-2 border-red-500  text-red-500" onClick={onDelete}>
             Supprimer
           </button>
-        </form>
-      </aside>
-    </div>
+        </div>
+      </form>
+    </aside>
   );
 };
 
