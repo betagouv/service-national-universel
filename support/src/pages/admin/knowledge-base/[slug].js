895597ec6146e375b6f45962d9c2b34ba80e7023
Arnaud AMBROSELLI <arnaudambroselli@pc72.home>
2021-12-06 07:10:59 +0100

    fix(support): lint

--- support/src/pages/admin/knowledge-base/[slug].js
+++ support/src/pages/admin/knowledge-base/[slug].js
@@ -83,39 +83,4 @@ const Content = ({ item }) => {
   return <KnowledgeBaseCreate position={0} />;
 };
 
-const TreeButton = ({ visible, setVisible }) =>
-  visible ? null : (
-    <svg
-      onClick={() => setVisible(true)}
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-8 w-8 cursor-pointer absolute left-2 top-2 z-10"
-      viewBox="0 0 100 125"
-      fill="currentColor"
-      stroke="currentColor"
-    >
-      <title>Category3</title>
-      <path d="M36,28.5H21a4,4,0,0,1-4-4V9.5a4,4,0,0,1,4-4H36a4,4,0,0,1,4,4v15A4,4,0,0,1,36,28.5ZM21,8.5a1,1,0,0,0-1,1v15a1,1,0,0,0,1,1H36a1,1,0,0,0,1-1V9.5a1,1,0,0,0-1-1Z" />
-      <path d="M79,58.5H64a4,4,0,0,1-4-4v-15a4,4,0,0,1,4-4H79a4,4,0,0,1,4,4v15A4,4,0,0,1,79,58.5Zm-15-20a1,1,0,0,0-1,1v15a1,1,0,0,0,1,1H79a1,1,0,0,0,1-1v-15a1,1,0,0,0-1-1Z" />
-      <path d="M79,94.5H64a4,4,0,0,1-4-4v-15a4,4,0,0,1,4-4H79a4,4,0,0,1,4,4v15A4,4,0,0,1,79,94.5Zm-15-20a1,1,0,0,0-1,1v15a1,1,0,0,0,1,1H79a1,1,0,0,0,1-1v-15a1,1,0,0,0-1-1Z" />
-      <polygon points="61.5 86 25.5 86 25.5 27 31.5 27 31.5 80 61.5 80 61.5 86" />
-      <rect x="28.5" y="44" width="33" height="6" />
-    </svg>
-  );
-
-const MoreButton = ({ visible, setVisible }) =>
-  visible ? null : (
-    <svg onClick={() => setVisible(true)} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 40" className="h-8 w-8 cursor-pointer absolute right-2 top-2 z-10">
-      <title>music, sound, volume, More</title>
-      <g data-name="Layer 15">
-        <path d="M16,2A14,14,0,1,0,30,16,14,14,0,0,0,16,2Zm0,26A12,12,0,1,1,28,16,12,12,0,0,1,16,28ZM12,16a2,2,0,1,1-2-2A2,2,0,0,1,12,16Zm6,0a2,2,0,1,1-2-2A2,2,0,0,1,18,16Zm6,0a2,2,0,1,1-2-2A2,2,0,0,1,24,16Z" />
-      </g>
-      <text x="0" y="47" fill="#000000" fontSize="5px" fontWeight="bold" fontFamily="'Helvetica Neue', Helvetica, Arial-Unicode, Arial, Sans-serif">
-        Created by Caesar Rizky Kurniawan
-      </text>
-      <text x="0" y="52" fill="#000000" fontSize="5px" fontWeight="bold" fontFamily="'Helvetica Neue', Helvetica, Arial-Unicode, Arial, Sans-serif">
-        from the Noun Project
-      </text>
-    </svg>
-  );
-
 export default withAuth(KnowledgeBase);
