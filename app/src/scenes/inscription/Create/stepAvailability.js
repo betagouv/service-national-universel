187d6ca4165b9b27b1e5a3e1afca2217faac6724
Lucien Klein <lucien.kln@gmail.com>
2021-12-13 14:51:32 +0100

    fix: feedback PR

--- app/src/scenes/inscription/Create/stepAvailability.js
+++ app/src/scenes/inscription/Create/stepAvailability.js
@@ -12,6 +12,7 @@ import { setYoung } from "../../../redux/auth/actions";
 import { STEPS } from "../utils";
 import InfoIcon from "../../../components/InfoIcon";
 import BackIcon from "../../../components/BackIcon";
+import ModalConfirm from "../../../components/modals/ModalConfirm";
 
 export default function StepAvailability() {
   const young = useSelector((state) => state.Auth.young);
@@ -19,6 +20,7 @@ export default function StepAvailability() {
   const dispatch = useDispatch();
   const [indexAvailability, setIndexAvailability] = useState(0);
   const [availability, setAvailability] = useState();
+  const [modal, setModal] = useState({ isOpen: false });
 
   if (!young) {
     history.push("/inscription/profil");
@@ -39,7 +41,7 @@ export default function StepAvailability() {
 
   useEffect(() => {
     (async () => {
-      if (availability?.length === 0) await api.put("/young", { status: YOUNG_STATUS.NOT_ELIGIBLE });
+      if (availability?.length === 0 && young.cohort === "2022") await api.put("/young", { status: YOUNG_STATUS.NOT_ELIGIBLE });
     })();
   }, [availability]);
 
@@ -55,6 +57,19 @@ export default function StepAvailability() {
     }
   };
 
+  const resetCohort = async () => {
+    try {
+      const { ok, code, data } = await api.put("/young", { cohort: "2022" });
+      if (!ok || !data?._id) return toastr.error("Une erreur s'est produite :", translate(code));
+      dispatch(setYoung(data));
+    } catch (e) {
+      console.log(e);
+      toastr.error("Erreur !");
+    } finally {
+      setModal({ isOpen: false, onConfirm: null });
+    }
+  };
+
   return (
     <>
       {!availability ? (
@@ -62,77 +77,104 @@ export default function StepAvailability() {
           <Spinner size="xl" style={{ width: "4rem", height: "4rem", color: "#362F78" }} />
         </div>
       ) : (
-        <Container>
-          {availability?.length === 0 ? (
-            <>
-              <Info>
-                <h3>INSCRIPTION NON-RECEVABLE</h3>
-                <h1 style={{ marginBottom: "1rem" }}>Malheureusement votre situation ne vous permet pas de participer à la session 2022 du SNU.</h1>
-                <Infos
-                  href="https://support.snu.gouv.fr/help/fr-fr/24-questions-frequemment-posees/181-suis-je-eligible-a-un-sejour-de-cohesion-en-2022"
-                  target="_blank"
-                  rel="noreferrer">
-                  <InfoIcon color="#32257F" />
-                  <p>
-                    <i>Pourquoi je ne vois aucun séjour ?</i> En savoir plus sur l&apos;éligibilité
-                  </p>
-                </Infos>
-                <div className="btns">
-                  <Button backgroundColor="#4f46e5" dark>
-                    <a
-                      style={{ fontSize: ".9rem", color: "#fff" }}
-                      href="https://support.snu.gouv.fr/help/fr-fr/16-comprendre-le-snu/7-les-autres-formes-d-engagement"
-                      target="_blank"
-                      rel="noreferrer">
-                      Consulter d&apos;autres dispositifs d&apos;engagement
-                    </a>
-                  </Button>
-                </div>
-              </Info>
-              <div className="thumb" />
-            </>
-          ) : (
-            <>
-              <Info>
-                {availability[indexAvailability - 1] && (
-                  <div className="back" onClick={() => setIndexAvailability(indexAvailability - 1)}>
-                    <BackIcon color="#9CA3AF" style={{ marginRight: "7px" }} />
-                    <p>Session de {availability[indexAvailability - 1].month}</p>
-                  </div>
-                )}
-                <h3>Séjour de cohésion à venir</h3>
-                <h1>Etes-vous disponible du {availability[indexAvailability].stringDate} ?</h1>
-                {availability[indexAvailability].info ? <AlerteInfo url={availability[indexAvailability].url}>{availability[indexAvailability].info}</AlerteInfo> : null}
-                <Infos
-                  href="https://support.snu.gouv.fr/help/fr-fr/24-questions-frequemment-posees/181-suis-je-eligible-a-un-sejour-de-cohesion-en-2022"
-                  target="_blank"
-                  rel="noreferrer">
-                  <InfoIcon color="#32257F" />
-                  <p>
-                    Veuillez vous assurer d&apos;être disponible sur l&apos;ensemble de la période.
-                    {availability?.length < 3 ? (
-                      <>
-                        <br />
-                        <i>Pourquoi je ne vois pas tous les séjours ?</i> En savoir plus sur l&apos;éligibilité
-                      </>
-                    ) : null}
-                  </p>
-                </Infos>
-                <div className="btns">
-                  <Button backgroundColor="#4f46e5" dark onClick={() => submit(availability[indexAvailability].id)}>
-                    Je suis disponible pour la session de {availability[indexAvailability].month}
-                  </Button>
-                  {availability[indexAvailability + 1] && (
-                    <Button onClick={() => setIndexAvailability(indexAvailability + 1)} borderColor="#D1D5DB">
-                      Non je ne suis pas disponible
+        <>
+          <ModalConfirm
+            isOpen={modal?.isOpen}
+            title="Attention cette action est irréversible"
+            message="Voulez-vous changer de séjour de cohésion"
+            onCancel={() => setModal({ isOpen: false })}
+            onConfirm={resetCohort}
+          />
+          <Container>
+            {young.cohort !== "2022" && (
+              <>
+                <Info>
+                  <h3>INSCRIPTION</h3>
+                  <h1 style={{ marginBottom: "1rem" }}>Vous êtes inscrits au séjour {young.cohort}</h1>
+                  <div className="btns">
+                    <Button borderColor="#D1D5DB" onClick={() => setModal({ isOpen: true })}>
+                      Changer de séjour
                     </Button>
+                    <Button backgroundColor="#4f46e5" dark onClick={() => history.push("/inscription/particulieres")}>
+                      Étape suivante
+                    </Button>
+                  </div>
+                </Info>
+                <div className="thumb" />
+              </>
+            )}
+            {young.cohort === "2022" && availability?.length === 0 && (
+              <>
+                <Info>
+                  <h3>INSCRIPTION NON-RECEVABLE</h3>
+                  <h1 style={{ marginBottom: "1rem" }}>Malheureusement votre situation ne vous permet pas de participer à la session 2022 du SNU.</h1>
+                  <Infos
+                    href="https://support.snu.gouv.fr/help/fr-fr/24-questions-frequemment-posees/181-suis-je-eligible-a-un-sejour-de-cohesion-en-2022"
+                    target="_blank"
+                    rel="noreferrer">
+                    <InfoIcon color="#32257F" />
+                    <p>
+                      <i>Pourquoi je ne vois aucun séjour ?</i> En savoir plus sur l&apos;éligibilité
+                    </p>
+                  </Infos>
+                  <div className="btns">
+                    <Button backgroundColor="#4f46e5" dark>
+                      <a
+                        style={{ fontSize: ".9rem", color: "#fff" }}
+                        href="https://support.snu.gouv.fr/help/fr-fr/16-comprendre-le-snu/7-les-autres-formes-d-engagement"
+                        target="_blank"
+                        rel="noreferrer">
+                        Consulter d&apos;autres dispositifs d&apos;engagement
+                      </a>
+                    </Button>
+                  </div>
+                </Info>
+                <div className="thumb" />
+              </>
+            )}
+            {young.cohort === "2022" && availability?.length > 0 && (
+              <>
+                <Info>
+                  {availability[indexAvailability - 1] && (
+                    <div className="back" onClick={() => setIndexAvailability(indexAvailability - 1)}>
+                      <BackIcon color="#9CA3AF" style={{ marginRight: "7px" }} />
+                      <p>Session de {availability[indexAvailability - 1].month}</p>
+                    </div>
                   )}
-                </div>
-              </Info>
-              <div className="thumb" />
-            </>
-          )}
-        </Container>
+                  <h3>Séjour de cohésion à venir</h3>
+                  <h1>Etes-vous disponible du {availability[indexAvailability].stringDate} ?</h1>
+                  {availability[indexAvailability].info ? <AlerteInfo url={availability[indexAvailability].url}>{availability[indexAvailability].info}</AlerteInfo> : null}
+                  <Infos
+                    href="https://support.snu.gouv.fr/help/fr-fr/24-questions-frequemment-posees/181-suis-je-eligible-a-un-sejour-de-cohesion-en-2022"
+                    target="_blank"
+                    rel="noreferrer">
+                    <InfoIcon color="#32257F" />
+                    <p>
+                      Veuillez vous assurer d&apos;être disponible sur l&apos;ensemble de la période.
+                      {availability?.length < 3 ? (
+                        <>
+                          <br />
+                          <i>Pourquoi je ne vois pas tous les séjours ?</i> En savoir plus sur l&apos;éligibilité
+                        </>
+                      ) : null}
+                    </p>
+                  </Infos>
+                  <div className="btns">
+                    <Button backgroundColor="#4f46e5" dark onClick={() => submit(availability[indexAvailability].id)}>
+                      Je suis disponible pour la session de {availability[indexAvailability].month}
+                    </Button>
+                    {availability[indexAvailability + 1] && (
+                      <Button onClick={() => setIndexAvailability(indexAvailability + 1)} borderColor="#D1D5DB">
+                        Non je ne suis pas disponible
+                      </Button>
+                    )}
+                  </div>
+                </Info>
+                <div className="thumb" />
+              </>
+            )}
+          </Container>
+        </>
       )}
     </>
   );
