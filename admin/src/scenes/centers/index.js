5123150de15d7a31a78a8785872831b46a77486c
tangimds <tangimds@gmail.com>
2021-12-14 12:03:14 +0100

    add banner

--- admin/src/scenes/centers/index.js
+++ admin/src/scenes/centers/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Switch, Route, useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
+import styled from "styled-components";
 
+import InfoIcon from "../../assets/InfoIcon";
 import List from "./list";
 import Edit from "./edit";
 import View from "./view";
@@ -15,11 +17,38 @@ export default function CenterIndex() {
   }
 
   return (
-    <Switch>
-      <Route path="/centre/create" component={Edit} />
-      <Route path="/centre/:id/edit" component={Edit} />
-      <Route path="/centre/:id" component={View} />
-      <Route path="/centre" component={List} />
-    </Switch>
+    <>
+      <Infos>
+        ⚠️
+        <InfoIcon color="#32257F" />
+        <p> Chantier en cours...</p>⚠️
+      </Infos>
+      <Switch>
+        <Route path="/centre/create" component={Edit} />
+        <Route path="/centre/:id/edit" component={Edit} />
+        <Route path="/centre/:id" component={View} />
+        <Route path="/centre" component={List} />
+      </Switch>
+    </>
   );
 }
+
+const Infos = styled.section`
+  margin-top: 1rem;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.5rem;
+  align-items: center;
+  justify-content: center;
+  background: rgba(79, 70, 229, 0.1);
+  padding: 1rem;
+  color: #32257f;
+  border-radius: 6px;
+  svg {
+    margin-top: 4px;
+  }
+  p {
+    font-size: 1.1rem;
+    margin: 0;
+  }
+`;
