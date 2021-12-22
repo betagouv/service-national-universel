0f5d404e80b16314b7f1877870d1eef65ba69429
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2021-12-22 14:27:38 +0100

    fix(api): base-de-connaissance.snu.gouv.fr

--- api/src/index.js
+++ api/src/index.js
@@ -10,12 +10,13 @@ const logger = require("morgan");
 const passport = require("passport");
 require("./mongo");
 
-const { PORT, APP_URL, ADMIN_URL, SUPPORT_URL, ENVIRONMENT } = require("./config.js");
+const { PORT, APP_URL, ADMIN_URL, SUPPORT_URL, KNOWLEDGEBASE_URL, ENVIRONMENT } = require("./config.js");
 
 if (process.env.NODE_ENV !== "test") {
   console.log("APP_URL", APP_URL);
   console.log("ADMIN_URL", ADMIN_URL);
   console.log("SUPPORT_URL", SUPPORT_URL);
+  console.log("KNOWLEDGEBASE_URL", KNOWLEDGEBASE_URL);
   console.log("ENVIRONMENT: ", ENVIRONMENT);
 }
 
@@ -39,7 +40,7 @@ function handleError(err, req, res, next) {
   res.status(statusCode).json(output);
 }
 
-const origin = [APP_URL, ADMIN_URL, SUPPORT_URL, "https://inscription.snu.gouv.fr"];
+const origin = [APP_URL, ADMIN_URL, SUPPORT_URL, KNOWLEDGEBASE_URL, "https://inscription.snu.gouv.fr"];
 app.use(cors({ credentials: true, origin }));
 app.use(bodyParser.json());
 app.use(bodyParser.text({ type: "application/x-ndjson" }));
