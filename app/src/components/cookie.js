0b69713cbcd51c21d30a975e91d3ff3595322638
Raphaël Huchet <raph@selego.co>
2021-12-23 15:19:30 +0100

    eslint

--- app/src/components/cookie.js
+++ app/src/components/cookie.js
@@ -1,72 +1,81 @@
-import React, {useState} from "react";
-import {Container, Modal} from "reactstrap";
+import React, { useState } from "react";
+import { Container, Modal } from "reactstrap";
 import styled from "styled-components";
-import {useSelector} from "react-redux";
 
 export default function Cookie() {
-  const young = useSelector((state) => state.Auth.young);
   const [open, setOpen] = useState(false);
 
   return (
     <CookieContainer>
-      <Modal size="lg" isOpen={open} scrollable={true} toggle={()=>setOpen(false)}>
+      <Modal size="lg" isOpen={open} scrollable={true} toggle={() => setOpen(false)}>
         <ModalContainer>
-          <h2 style={{textAlign: "center", color: "#1D4094", marginBottom: "30px", fontWeight: "600"}}>Gestion de vos cookies</h2>
-          <p style={{fontWeight: "500"}}>L'Assurance Maladie s'engage, dans le cadre des missions qui lui sont confiées, à assurer la protection, la confidentialité et la sécurité de l'ensemble de vos
-            données personnelles dans
-            le respect de votre vie privée. Lorsque vous naviguez sur notre site, nous pouvons être amenés à déposer différents types de cookies sur votre terminal. Ils ont des finalités différentes
-            décrites ci-dessous. Cette page vous permet de les accepter ou de les refuser de manière globale ou au cas par cas.</p>
-          <div style={{display: "flex", justifyContent: "flex-end", gap: "10px"}}>
+          <h2 style={{ textAlign: "center", color: "#1D4094", marginBottom: "30px", fontWeight: "600" }}>Gestion de vos cookies</h2>
+          <p style={{ fontWeight: "500" }}>
+            L&apos;Assurance Maladie s&apos;engage, dans le cadre des missions qui lui sont confiées, à assurer la protection, la confidentialité et la sécurité de l&apos;ensemble
+            de vos données personnelles dans le respect de votre vie privée. Lorsque vous naviguez sur notre site, nous pouvons être amenés à déposer différents types de cookies
+            sur votre terminal. Ils ont des finalités différentes décrites ci-dessous. Cette page vous permet de les accepter ou de les refuser de manière globale ou au cas par
+            cas.
+          </p>
+          <div style={{ display: "flex", justifyContent: "flex-end", gap: "10px" }}>
             <ButtonModalBlue>TOUT ACCEPTER</ButtonModalBlue>
             <ButtonModalRed>TOUT REFUSER</ButtonModalRed>
           </div>
-          <hr/>
+          <hr />
           <Subtitle>Nécessaires au bon fonctionnement du site</Subtitle>
-          <TextModal>Ces cookies permettent de garantir des fonctionnalités importantes du site comme la sélection de votre caisse à travers la saisie de votre code postal, la mémorisation de l'espace
-            visité ou la personnalisation de l'affichage (version contrastée). Le site web ne pouvant fonctionner correctement sans eux, ils ne peuvent être désactivés.
+          <TextModal>
+            Ces cookies permettent de garantir des fonctionnalités importantes du site comme la sélection de votre caisse à travers la saisie de votre code postal, la mémorisation
+            de l&apos;espace visité ou la personnalisation de l&apos;affichage (version contrastée). Le site web ne pouvant fonctionner correctement sans eux, ils ne peuvent être
+            désactivés.
           </TextModal>
-          <hr/>
+          <hr />
           <Subtitle>Campagnes</Subtitle>
-          <TextModal>Ces cookies sont utilisés pour diffuser des contenus ciblés en fonction de votre navigation. Ils permettent également d'établir des statistiques anonymes de nos campagnes de
-            communication, via Google Tag Manager, pour évaluer leur diffusion et leur efficacité.
+          <TextModal>
+            Ces cookies sont utilisés pour diffuser des contenus ciblés en fonction de votre navigation. Ils permettent également d&apos;établir des statistiques anonymes de nos
+            campagnes de communication, via Google Tag Manager, pour évaluer leur diffusion et leur efficacité.
           </TextModal>
-          <div style={{display: "flex", justifyContent: "flex-end", gap: "10px", padding: "40px 0 10px 0"}}>
+          <div style={{ display: "flex", justifyContent: "flex-end", gap: "10px", padding: "40px 0 10px 0" }}>
             <ButtonModalBlue>ACCEPTER</ButtonModalBlue>
             <ButtonModalRed>REFUSER</ButtonModalRed>
           </div>
-          <hr/>
-          <Subtitle>Mesure d'audience</Subtitle>
-          <TextModal>Les services de mesure d'audience permettent de générer des statistiques de fréquentation utiles à l'amélioration du site.</TextModal>
-          <div style={{display: "flex", justifyContent: "flex-end", gap: "10px", padding: "40px 0 10px 0"}}>
+          <hr />
+          <Subtitle>Mesure d&apos;audience</Subtitle>
+          <TextModal>Les services de mesure d&apos;audience permettent de générer des statistiques de fréquentation utiles à l&apos;amélioration du site.</TextModal>
+          <div style={{ display: "flex", justifyContent: "flex-end", gap: "10px", padding: "40px 0 10px 0" }}>
             <ButtonModalBlue>ACCEPTER</ButtonModalBlue>
             <ButtonModalRed>REFUSER</ButtonModalRed>
           </div>
-          <hr/>
+          <hr />
           <Subtitle>Vidéos</Subtitle>
           <TextModal>Ces cookies déposés via le service de partage de vidéos Vimeo vous permettent de visionner les contenus multimédia.</TextModal>
-          <div style={{display: "flex", justifyContent: "flex-end", gap: "10px", padding: "40px 0 10px 0"}}>
+          <div style={{ display: "flex", justifyContent: "flex-end", gap: "10px", padding: "40px 0 10px 0" }}>
             <ButtonModalBlue>ACCEPTER</ButtonModalBlue>
             <ButtonModalRed>REFUSER</ButtonModalRed>
           </div>
-          <hr/>
+          <hr />
           <Subtitle>Réseaux sociaux</Subtitle>
           <TextModal>Ces cookies déposés via les boutons réseaux sociaux permettent de faciliter le partage des contenus du site sur Facebook, Twitter ou LinkedIn.</TextModal>
-          <div style={{display: "flex", justifyContent: "flex-end", gap: "10px", padding: "40px 0 10px 0"}}>
+          <div style={{ display: "flex", justifyContent: "flex-end", gap: "10px", padding: "40px 0 10px 0" }}>
             <ButtonModalBlue>ACCEPTER</ButtonModalBlue>
             <ButtonModalRed>REFUSER</ButtonModalRed>
           </div>
-          <hr/>
+          <hr />
         </ModalContainer>
       </Modal>
       <Container>
-        <p>Ce site utilise des cookies qui nous permettent de vous proposer une navigation optimale, de mesurer l'audience du site et de nos campagnes de communication, ainsi que de vous proposer
-          des vidéos.</p>
-        <div style={{display: "flex", justifyContent: "center", gap: "15px", padding: "25px 0 15px 0"}}>
+        <p>
+          Ce site utilise des cookies qui nous permettent de vous proposer une navigation optimale, de mesurer l&apos;audience du site et de nos campagnes de communication, ainsi
+          que de vous proposer des vidéos.
+        </p>
+        <div style={{ display: "flex", justifyContent: "center", gap: "15px", padding: "25px 0 15px 0" }}>
           <Button onClick={() => console.log("TEST")}>Tout accepter</Button>
           <Button onClick={() => console.log("TEST")}>Tout refuser</Button>
-          <Button style={{background: "#4766A9", color: "#fff"}} onClick={() => setOpen(true)}>Personnaliser</Button>
+          <Button style={{ background: "#4766A9", color: "#fff" }} onClick={() => setOpen(true)}>
+            Personnaliser
+          </Button>
         </div>
-        <a style={{textDecoration: "underline", fontWeight: "200", cursor: "pointer"}} href={"https://www.snu.gouv.fr/donnees-personnelles-et-cookies-23"}>En savoir plus</a>
+        <a style={{ textDecoration: "underline", fontWeight: "200", cursor: "pointer" }} href={"https://www.snu.gouv.fr/donnees-personnelles-et-cookies-23"}>
+          En savoir plus
+        </a>
       </Container>
     </CookieContainer>
   );
@@ -78,7 +87,7 @@ const ModalContainer = styled.footer`
 `;
 
 const Subtitle = styled.footer`
-  color: #1D4094;
+  color: #1d4094;
   font-weight: 500;
   padding: 3px 0 20px 0;
 `;
@@ -96,10 +105,10 @@ const ButtonModalBlue = styled.footer`
   font-weight: 500;
   font-size: smaller;
   cursor: pointer;
-  color: #1D4094;
+  color: #1d4094;
 
   &:hover {
-    background: #1D4094;
+    background: #1d4094;
     color: #fff;
   }
 `;
@@ -112,10 +121,10 @@ const ButtonModalRed = styled.footer`
   font-weight: 500;
   font-size: smaller;
   cursor: pointer;
-  color: #AC5355;
+  color: #ac5355;
 
   &:hover {
-    background: #AC5355;
+    background: #ac5355;
     color: #fff;
   }
 `;
@@ -123,13 +132,12 @@ const ButtonModalRed = styled.footer`
 const Button = styled.footer`
   background: #fff;
   display: inline-block;
-  color: #142D68;
+  color: #142d68;
   font-weight: 500;
   border-radius: 2px;
   padding: 1px 7px;
   cursor: pointer;
 `;
-
 
 const CookieContainer = styled.footer`
   text-align: center;
@@ -139,7 +147,7 @@ const CookieContainer = styled.footer`
     font-size: 0.8rem;
   }
   padding: 40px 0 15px 0;
-  background: #142D68;
+  background: #142d68;
   position: fixed;
   bottom: 0px;
   width: 100%;
