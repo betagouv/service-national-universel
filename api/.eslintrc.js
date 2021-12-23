29bbab691a8fef01b0d259504111c8d095f209b2
Raphaël Huchet <raph@selego.co>
2021-12-23 14:31:13 +0100

    fix: jest should work locally

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,18 +1,14 @@
 module.exports = {
   root: true,
   env: {
-    browser: true,
+    browser: false,
     commonjs: true,
     node: true,
     es6: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:import/recommended", "plugin:prettier/recommended"],
+  extends: ["eslint:recommended", "plugin:import/recommended", "plugin:prettier/recommended"],
   plugins: ["prettier"],
   parserOptions: {
     ecmaVersion: 2020,
-  },
-  rules: {
-    "react/jsx-no-duplicate-props": 1,
-    "react/prop-types": 1,
   },
 };
