c230a47395715cbf5f5d0e838979de8d646ca863
tangimds <tangimds@gmail.com>
2022-01-03 16:38:04 +0100

    feat(api,patches): add patches on cohesionCenter

--- api/src/models/cohesionCenter.js
+++ api/src/models/cohesionCenter.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const mongooseElastic = require("@selego/mongoose-elastic");
+const patchHistory = require("mongoose-patch-history").default;
 const esClient = require("../es");
-
 const MODELNAME = "cohesioncenter";
 
 const Schema = new mongoose.Schema({
@@ -111,6 +111,28 @@ const Schema = new mongoose.Schema({
 
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
+});
+
+Schema.virtual("fromUser").set(function (fromUser) {
+  if (fromUser) {
+    const { _id, role, department, region, email, firstName, lastName, model } = fromUser;
+    this._user = { _id, role, department, region, email, firstName, lastName, model };
+  }
+});
+
+Schema.pre("save", function (next, params) {
+  this.fromUser = params?.fromUser;
+  next();
+});
+
+Schema.plugin(patchHistory, {
+  mongoose,
+  name: `${MODELNAME}Patches`,
+  trackOriginalValue: true,
+  includes: {
+    modelName: { type: String, required: true, default: MODELNAME },
+    user: { type: Object, required: false, from: "_user" },
+  },
 });
 
 Schema.plugin(mongooseElastic(esClient), MODELNAME);
