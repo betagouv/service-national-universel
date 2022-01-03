c41d706f7d2870d01feb314bfcc4080cc617b154
tangimds <tangimds@gmail.com>
2022-01-03 11:55:15 +0100

    chore: fix wording

--- admin/src/scenes/centers/view/team.js
+++ admin/src/scenes/centers/view/team.js
@@ -6,7 +6,7 @@ export default function Team({ center, focusedCohort }) {
   return (
     <div style={{ display: "flex", alignItems: "flex-start", width: "100%" }}>
       <Box>
-        <div style={{ padding: "2rem" }}>Bientôt Disponible ! On y afficher l&nbsp;équipe de {focusedCohort}</div>
+        <div style={{ padding: "2rem" }}>Bientôt Disponible ! On y affichera l&apos;équipe de {focusedCohort}</div>
       </Box>
     </div>
   );
