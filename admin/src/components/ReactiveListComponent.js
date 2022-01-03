ba395fe190aa5fa0b3dadf186a7dc27b8de15175
tangimds <tangimds@gmail.com>
2022-01-03 13:56:21 +0100

    fix(admin,list): displayed results on incomplete list pages

--- admin/src/components/ReactiveListComponent.js
+++ admin/src/components/ReactiveListComponent.js
@@ -4,6 +4,7 @@ import { TopResultStats, BottomResultStats } from "./list";
 import { getResultLabel } from "../utils";
 
 export default function ReactiveListComponent(props) {
+  const PAGE_SIZE = props.pageSize || 10;
   return (
     <ReactiveList
       // default props
@@ -11,7 +12,7 @@ export default function ReactiveListComponent(props) {
       pagination={true}
       paginationAt="both"
       innerClass={{ pagination: "pagination", sortOptions: "sort-options" }}
-      size={10}
+      size={PAGE_SIZE}
       dataField="createdAt"
       sortBy="desc"
       showLoader={true}
@@ -20,8 +21,8 @@ export default function ReactiveListComponent(props) {
       renderResultStats={(e) => {
         return (
           <>
-            <TopResultStats>{getResultLabel(e)}</TopResultStats>
-            <BottomResultStats>{getResultLabel(e)}</BottomResultStats>
+            <TopResultStats>{getResultLabel(e, PAGE_SIZE)}</TopResultStats>
+            <BottomResultStats>{getResultLabel(e, PAGE_SIZE)}</BottomResultStats>
           </>
         );
       }}
