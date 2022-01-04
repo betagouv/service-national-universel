7645159e02e799a6b7f27f6200dea1cf5925938a
Tangi Mendès <tangimds@gmail.com>
2022-01-04 10:45:57 +0100

    feat: open center tab (#1049)

--- admin/src/components/drawer/index.js
+++ admin/src/components/drawer/index.js
@@ -86,7 +86,7 @@ function admin({ onClick, newTickets, openedTickets, closedTickets, tickets }) {
       <DrawerTab to="/user" title="Utilisateurs" onClick={onClick} />
       <DrawerTab to="/volontaire" title="Volontaires" onClick={onClick} />
       <DrawerTab to="/inscription" title="Inscriptions" onClick={onClick} />
-      <DrawerTab to="/centre" title="Centres" onClick={onClick} beta />
+      <DrawerTab to="/centre" title="Centres" onClick={onClick} />
       <DrawerTab to="/point-de-rassemblement" title="Points de rassemblement" onClick={onClick} />
       <DrawerTab to="/contenu" title="Contenus" onClick={onClick} />
       <DrawerTab to="/objectifs" title="Objectifs" onClick={onClick} />
