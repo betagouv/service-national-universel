39eae8bd6fd0d01f7c7db601f0b00fc17c3e7476
SpookyUmi <chloe.danisevicius@gmail.com>
2022-01-04 15:29:08 +0100

    functional modal

--- app/src/scenes/inscription/components/eligibilityModal.js
+++ app/src/scenes/inscription/components/eligibilityModal.js
@@ -1,19 +1,18 @@
 import React, { useState } from "react";
-import { Modal, Col, Row } from "reactstrap";
+import { Modal, Col } from "reactstrap";
 import styled from "styled-components";
 //import Modal from "./modal";
 import { Formik, Field } from "formik";
 import close from "../../../assets/cancel.png";
 
-import { translate } from "../../../utils";
 import LoadingButton from "../../../components/buttons/LoadingButton";
 import ErrorMessage, { requiredMessage } from "../../inscription/components/errorMessage";
-import { SelectTag, step1, step2TechnicalPublic, step2QuestionPublic } from "../../support-center/ticket/worflow";
-import { ModalContainer, Content, Footer } from "../../../components/modals/Modal";
+import { ModalContainer } from "../../../components/modals/Modal";
 
 export default function EligibilityModal({ onChange }) {
   const [isOpen, setIsOpen] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [isEligible, setIsEligible] = useState(null);
 
   return (
     <>
@@ -32,7 +31,50 @@ export default function EligibilityModal({ onChange }) {
               onSubmit={async (values) => {
                 try {
                   setLoading(true);
-                  const { birthDate, school, schoolLevel, address } = values;
+                  const { birthDate, schoolLevel, address } = values;
+                  let sessions = [
+                    {
+                      month: "Février",
+                      excludedGrade: ["3eme", "1ere", "Terminale", "Terminale CAP"],
+                      // exclude all DOM-TOMs
+                      excludedZip: ["971", "972", "973", "974", "975", "976", "978", "984", "986", "987", "988"],
+                      includedBirthdate: { begin: "2004-02-26", end: "2007-02-12" },
+                      stringDate: "13 au 25 février 2022",
+                      info: "Pour les élèves de 2nde scolarisés dans un établissement relevant du ministère de l’éducation nationale, de la jeunesse et des sports, l’inscription est possible y compris dans le cas où une semaine du séjour de cohésion se déroule sur le temps scolaire. Ils bénéficieront d’une autorisation de participation au séjour de cohésion.",
+                      buffer: 1.15,
+                      id: "Février 2022",
+                    },
+                    {
+                      month: "Juin",
+                      excludedGrade: ["3ème", "1ère", "Terminale", "Terminale CAP"],
+                      excludedZip: [],
+                      includedBirthdate: { begin: "2004-06-25", end: "2007-06-11" },
+                      stringDate: "12 au 24 juin 2022",
+                      buffer: 1.25,
+                      id: "Juin 2022",
+                    },
+                    {
+                      month: "Juillet",
+                      excludedGrade: [],
+                      excludedZip: [],
+                      includedBirthdate: { begin: "2004-07-16", end: "2007-07-02" },
+                      stringDate: "3 au 15 juillet 2022",
+                      buffer: 1.25,
+                      id: "Juillet 2022",
+                    },
+                  ].filter((el) => {
+                    if (el.excludedGrade.includes(schoolLevel)) return false;
+                    else if (el.excludedZip.some((e) => new RegExp(`^${e}`).test(address))) return false;
+                    else if (
+                      new Date(el.includedBirthdate.begin).getTime() <= new Date(birthDate).getTime() &&
+                      new Date(birthDate).getTime() <= new Date(el.includedBirthdate.end).getTime()
+                    ) {
+                      return true;
+                    }
+                    return false;
+                  });
+                  console.log("SESSIONS", sessions.length);
+                  setIsEligible(!!sessions.length);
                   setLoading(false);
                 } catch (e) {
                   console.log(e);
@@ -43,7 +85,8 @@ export default function EligibilityModal({ onChange }) {
                   <Item
                     name="birthDate"
                     title="Date de naissance"
-                    placeholder="Renseignez votre date de naissance"
+                    subTitle="Au format : AAAA/MM/JJ"
+                    placeholder="Par exemple : 2005/05/22"
                     type="input"
                     value={values.birthDate}
                     handleChange={handleChange}
@@ -69,7 +112,7 @@ export default function EligibilityModal({ onChange }) {
                     <Item
                       name="schoolLevel"
                       type="select"
-                      options={["Seconde", "Première", "Terminale"]}
+                      options={["3ème", "Seconde", "1ère", "Terminale", "Terminale CAP"]}
                       title=""
                       selectPlaceholder="Sélectionnez votre classe"
                       handleChange={handleChange}
@@ -91,6 +134,7 @@ export default function EligibilityModal({ onChange }) {
                   <LoadingButton loading={loading} type="submit" style={{ margin: "1.5rem auto 0 auto", width: "93%" }} onClick={handleSubmit} disabled={isSubmitting}>
                     Vérifier
                   </LoadingButton>
+                  {isEligible ? null : <p>NOPE</p>}
                 </>
               )}
             </Formik>
@@ -102,10 +146,12 @@ export default function EligibilityModal({ onChange }) {
   );
 }
 
-const Item = ({ title, name, value, handleChange, errors, touched, validate, type, options, selectPlaceholder, ...props }) => {
+const Item = ({ title, subTitle, name, value, handleChange, errors, touched, validate, type, options, selectPlaceholder, ...props }) => {
   return (
-    <Col style={{ marginTop: 20 }}>
-      <Label>{title}</Label>
+    <Col style={{ marginTop: type !== "select" ? 20 : 0 }}>
+      <Label>
+        {title} <Subtitle>{subTitle}</Subtitle>
+      </Label>
       {type === "select" ? (
         <Field as={type} className="form-control" name={name} value={value} onChange={handleChange} validate={validate} {...props}>
           <option value="" disabled>
@@ -130,6 +176,12 @@ const Label = styled.div`
   font-weight: 600;
   font-size: 14px;
   margin-bottom: 5px;
+`;
+
+const Subtitle = styled.p`
+  color: grey;
+  font-size: 0.7rem;
+  margin-bottom: 0;
 `;
 
 const Form = styled.div`
