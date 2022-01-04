bc4014d9038a703224d1b8eb7be0b88d0fcafab5
tangimds <tangimds@gmail.com>
2022-01-04 10:38:02 +0100

    Update index.js

--- admin/src/scenes/centers/index.js
+++ admin/src/scenes/centers/index.js
@@ -1,19 +1,11 @@
 import React from "react";
-import { Switch, Route, useHistory } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { Switch, Route } from "react-router-dom";
 
 import List from "./list";
 import Edit from "./edit";
 import View from "./view";
 
 export default function CenterIndex() {
-  const user = useSelector((state) => state.Auth.user);
-  const history = useHistory();
-  if (user?.role !== "admin") {
-    history.push("/");
-    return null;
-  }
-
   return (
     <Switch>
       <Route path="/centre/nouveau" component={Edit} />
