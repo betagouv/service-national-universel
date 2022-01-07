93b21dc6a5631aed9c958e30ec08bab7e030c856
SpookyUmi <chloe.danisevicius@gmail.com>
2022-01-07 16:32:18 +0100

    remove status

--- app/src/scenes/phase1/index.js
+++ app/src/scenes/phase1/index.js
@@ -5,7 +5,6 @@ import Done from "./done.js";
 import Affected from "./affected.js";
 import Cancel from "./cancel.js";
 import NotDone from "./notDone.js";
-import WaitingAcceptation from "./waitingAcceptation.js";
 import WaitingAffectation from "./waitingAffectation.js";
 import WaitingList from "./waitingList.js";
 import { YOUNG_STATUS_PHASE1, permissionPhase1 } from "../../utils";
@@ -21,9 +20,8 @@ export default () => {
   const renderStep = () => {
     if (young.statusPhase1 === YOUNG_STATUS_PHASE1.DONE) return <Done />;
     if (young.statusPhase1 === YOUNG_STATUS_PHASE1.AFFECTED) return <Affected />;
-    if ((young.statusPhase1 === YOUNG_STATUS_PHASE1.CANCEL || young.statusPhase1 === YOUNG_STATUS_PHASE1.EXEMPTED) && young.cohesion2020Step !== "DONE") return <Cancel />;
+    if (young.statusPhase1 === YOUNG_STATUS_PHASE1.EXEMPTED && young.cohesion2020Step !== "DONE") return <Cancel />;
     if (young.statusPhase1 === YOUNG_STATUS_PHASE1.NOT_DONE) return <NotDone />;
-    if (young.statusPhase1 === YOUNG_STATUS_PHASE1.WAITING_ACCEPTATION) return <WaitingAcceptation />;
     if (young.statusPhase1 === YOUNG_STATUS_PHASE1.WAITING_AFFECTATION) return <WaitingAffectation young={young} />;
     if (young.statusPhase1 === YOUNG_STATUS_PHASE1.WAITING_LIST) return <WaitingList />;
     return (
