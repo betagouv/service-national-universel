109d66b858a4b5b0706df0c1d53ec123a8c40d26
Raphaël Huchet <raph@selego.co>
2022-01-10 10:26:47 +0100

    chore: eslint

--- api/src/crons/index.js
+++ api/src/crons/index.js
@@ -9,6 +9,7 @@ const noticePushMission = require("./noticePushMission");
 
 // doubt ? -> https://crontab.guru/
 
+/* eslint-disable no-unused-vars */
 // dev : */5 * * * * * (every 5 secs)
 // prod : 0 8 * * 1 (every monday at 0800)
 const EVERY_MINUTE = "* * * * *";
@@ -16,6 +17,7 @@ const EVERY_HOUR = "0 * * * *";
 const everySeconds = (x) => `*/${x} * * * * *`;
 const everyMinutes = (x) => `*/${x} * * * *`;
 const everyHours = (x) => `0 */${x} * * *`;
+/* eslint-enable no-unused-vars */
 
 // See: https://www.clever-cloud.com/doc/administrate/cron/#deduplicating-crons (INSTANCE_NUMBER)
 if (ENVIRONMENT === "production" && process.env.INSTANCE_NUMBER === "0") {
