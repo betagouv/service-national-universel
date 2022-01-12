45240d6e189728ad623f7a4731ba41cd2cebac60
Chloe Danisevicius <62291986+SpookyUmi@users.noreply.github.com>
2022-01-12 11:19:31 +0100

    hotfix(app+admin): public support region and dept tags (#1071)
    * hotfix(app+admin): public support region and dept tags
    
    * Update app/src/scenes/public-support-center/form.js
    
    Co-authored-by: Tangi Mendès <tangimds@gmail.com>
    
    * Update app/src/scenes/public-support-center/form.js
    
    Co-authored-by: Tangi Mendès <tangimds@gmail.com>
    
    Co-authored-by: Tangi Mendès <tangimds@gmail.com>

--- app/src/scenes/public-support-center/form.js
+++ app/src/scenes/public-support-center/form.js
@@ -6,7 +6,7 @@ import { Formik, Field } from "formik";
 import close from "../../assets/cancel.png";
 
 import api from "../../services/api";
-import { translate } from "../../utils";
+import { translate, departmentList, department2region } from "../../utils";
 import LoadingButton from "../../components/buttons/LoadingButton";
 import ErrorMessage, { requiredMessage } from "../inscription/components/errorMessage";
 import { SelectTag, step1, step2TechnicalPublic, step2QuestionPublic } from "../support-center/ticket/worflow";
@@ -18,13 +18,14 @@ export default function FormComponent({ setOpen, setSuccessMessage }) {
     <Form>
       <img src={close} onClick={() => setOpen(false)} />
       <Formik
-        initialValues={{ step1: null, step2: null, message: "", subject: "", email: "", name: "" }}
+        initialValues={{ step1: null, step2: null, message: "", subject: "", email: "", name: "", department: "" }}
         validateOnChange={false}
         validateOnBlur={false}
         onSubmit={async (values) => {
           try {
             setLoading(true);
-            const { message, subject, name, email, step1, step2 } = values;
+            const { message, subject, name, email, step1, step2, department } = values;
+            const regionTags = [`DEPARTEMENT_${department}`, `REGION_${department2region[department]}`];
             const { ok, code } = await api.post("/support-center/public/ticket", {
               title: `${step1?.label} - ${step2?.label} - ${subject}`,
               subject,
@@ -32,7 +33,7 @@ export default function FormComponent({ setOpen, setSuccessMessage }) {
               email,
               message,
               // eslint-disable-next-line no-unsafe-optional-chaining
-              tags: [...new Set([...tags, ...(step1?.tags || []), ...(step2?.tags || [])])], // we use this dirty hack to remove duplicates
+              tags: [...new Set([...tags, ...(step1?.tags || []), ...(step2?.tags || []), ...(regionTags || [])])], // we use this dirty hack to remove duplicates
             });
             setLoading(false);
             setOpen(false);
@@ -49,6 +50,7 @@ export default function FormComponent({ setOpen, setSuccessMessage }) {
             <Item
               name="name"
               title="Nom et prénom"
+              placeholder="Renseignez vos nom et prénom"
               type="input"
               value={values.name}
               handleChange={handleChange}
@@ -60,6 +62,7 @@ export default function FormComponent({ setOpen, setSuccessMessage }) {
             <Item
               name="email"
               title="Email"
+              placeholder="Renseignez votre email"
               type="input"
               value={values.email}
               handleChange={handleChange}
@@ -68,37 +71,63 @@ export default function FormComponent({ setOpen, setSuccessMessage }) {
               touched={touched}
               rows="2"
             />
+            <Item
+              name="department"
+              type="select"
+              placeholder="Sélectionnez votre département"
+              values={values}
+              value={values.department}
+              handleChange={handleChange}
+              title="Département"
+              options={departmentList.map((d) => ({ value: d, label: d }))?.sort((a, b) => a.label.localeCompare(b.label))}
+              validate={(v) => !v && requiredMessage}
+              errors={errors}
+              touched={touched}
+            />
             <SelectTag
               name="step1"
               options={Object.values(step1)}
               title={"Ma demande"}
               selectPlaceholder={"Choisir la catégorie"}
               handleChange={handleChange}
+              values={values}
               value={values?.step1?.id}
+              validate={(v) => !v && requiredMessage}
+              errors={errors}
+              touched={touched}
             />
             {values.step1?.id === "TECHNICAL" ? (
               <SelectTag
                 name="step2"
                 options={Object.values(step2TechnicalPublic)}
-                title={"Sujet"}
-                selectPlaceholder={"Choisir le sujet"}
+                title={"Précision"}
+                selectPlaceholder={"Préciser"}
                 handleChange={handleChange}
+                values={values}
                 value={values.step2?.id}
+                validate={(v) => !v && requiredMessage}
+                errors={errors}
+                touched={touched}
               />
             ) : null}
             {values.step1?.id === "QUESTION" ? (
               <SelectTag
                 name="step2"
                 options={Object.values(step2QuestionPublic)}
-                title={"Sujet"}
-                selectPlaceholder={"Choisir le sujet"}
+                title={"Précision"}
+                selectPlaceholder={"Préciser"}
                 handleChange={handleChange}
+                values={values}
                 value={values.step2?.id}
+                validate={(v) => !v && requiredMessage}
+                errors={errors}
+                touched={touched}
               />
             ) : null}
             <Item
               name="subject"
               title="Le sujet de ma demande"
+              placeholder="Renseignez votre demande"
               type="input"
               value={values.subject}
               handleChange={handleChange}
@@ -110,6 +139,7 @@ export default function FormComponent({ setOpen, setSuccessMessage }) {
             <Item
               name="message"
               title="Mon message"
+              placeholder="Votre message"
               type="textarea"
               value={values.message}
               handleChange={handleChange}
@@ -128,23 +158,23 @@ export default function FormComponent({ setOpen, setSuccessMessage }) {
   );
 }
 
-const Item = ({ title, name, value, handleChange, errors, touched, validate, type, options, ...props }) => {
+const Item = ({ title, name, value, handleChange, errors, touched, validate, type, options, placeholder, ...props }) => {
   return (
     <Col style={{ marginTop: 20 }}>
       <Label>{title}</Label>
       {type === "select" ? (
         <Field as={type} className="form-control" name={name} value={value} onChange={handleChange} validate={validate} {...props}>
-          <option value="" disabled>
-            Catégorie
+          <option disabled value="" selected={!value} label={placeholder}>
+            {placeholder}
           </option>
-          {options?.map((option) => (
-            <option value={option} key={option}>
-              {option}
+          {options?.map((o, i) => (
+            <option key={i} value={o.value} label={o.label}>
+              {o.label}
             </option>
           ))}
         </Field>
       ) : (
-        <Field as={type} className="form-control" name={name} value={value} onChange={handleChange} validate={validate} {...props} />
+        <Field as={type} className="form-control" name={name} value={value} onChange={handleChange} validate={validate} placeholder={placeholder} {...props} />
       )}
       {errors && <ErrorMessage errors={errors} touched={touched} name={name} />}
     </Col>
