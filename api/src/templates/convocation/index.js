feaaff9b6818911f3a280f9f84b23cf46a125330
tangimds <tangimds@gmail.com>
2022-01-19 12:02:04 +0100

    up

--- api/src/templates/convocation/index.js
+++ api/src/templates/convocation/index.js
@@ -1,11 +1,12 @@
+/* eslint-disable no-useless-catch */
 const fs = require("fs");
 const path = require("path");
 const { getSignedUrl, getBaseUrl } = require("../../utils");
 const CohesionCenterModel = require("../../models/cohesionCenter");
+const SessionPhase1 = require("../../models/sessionPhase1");
 const MeetingPointModel = require("../../models/meetingPoint");
 const BusModel = require("../../models/bus");
 const DepartmentServiceModel = require("../../models/departmentService");
-const { capture } = require("../../sentry");
 const { formatStringDate, formatStringDateTimezoneUTC } = require("snu-lib");
 
 const isFromDOMTOM = (young) => {
@@ -16,16 +17,30 @@ const isFromDOMTOM = (young) => {
 
 function getBg(template = "default") {
   if (template === "domtom") return getSignedUrl("convocation/convocationCohesionTemplate-DOMTOM.png");
-  return getSignedUrl("convocation/convocationCohesionTemplate.png");
+  return getSignedUrl("convocation/convocation_phase1_v2.png");
 }
+
+const departureMeetingDate = {
+  2021: "lundi 20 février, 14:00",
+  "Février 2022": "lundi 13 février, 14:00",
+};
+const returnMeetingDate = {
+  2021: "2 juillet, 14:00",
+  "Février 2022": "xxx",
+};
+
+const COHESION_STAY_DATE_STRING = {
+  2021: "20 février 2021",
+  "Février 2022": "13 février 2022",
+};
 
 const render = async (young) => {
   const getDepartureMeetingDate = (meetingPoint) => {
-    if (young.deplacementPhase1Autonomous === "true" || !meetingPoint) return "dimanche 20 juin, 16:30"; //new Date("2021-06-20T14:30:00.000+00:00");
+    if (young.deplacementPhase1Autonomous === "true" || !meetingPoint) return departureMeetingDate[young.cohort]; //new Date("2021-06-20T14:30:00.000+00:00");
     return meetingPoint.departureAtString;
   };
   const getReturnMeetingDate = (meetingPoint) => {
-    if (young.deplacementPhase1Autonomous === "true" || !meetingPoint) return "vendredi 2 juillet, 14:00"; // new Date("2021-07-02T12:00:00.000+00:00");
+    if (young.deplacementPhase1Autonomous === "true" || !meetingPoint) return returnMeetingDate[young.cohort]; // new Date("2021-07-02T12:00:00.000+00:00");
     return meetingPoint.returnAtString;
   };
   const getMeetingAddress = (meetingPoint, center) => {
@@ -33,18 +48,21 @@ const render = async (young) => {
     return meetingPoint.departureAddress;
   };
   try {
-    if (!young.cohesionCenterId && young.deplacementPhase1Autonomous !== "true") throw `young ${young.id} unauthorized`;
-    const center = await CohesionCenterModel.findById(young.cohesionCenterId);
-    if (!center) throw `center ${young.cohesionCenterId} not found for young ${young._id}`;
+    if (young.statusPhase1 === "AFFECTED" && !young.sessionPhase1Id && young.deplacementPhase1Autonomous !== "true") throw `young ${young.id} unauthorized`;
+    const session = await SessionPhase1.findById(young.sessionPhase1Id);
+    if (!session) throw `session ${young.sessionPhase1Id} not found for young ${young._id}`;
+    const center = await CohesionCenterModel.findById(session.cohesionCenterId);
+    if (!center) throw `center ${session.cohesionCenterId} not found for young ${young._id} - session ${session._id}`;
     const meetingPoint = await MeetingPointModel.findById(young.meetingPointId);
     const bus = await BusModel.findById(meetingPoint?.busId);
     const service = await DepartmentServiceModel.findOne({ department: young?.department });
     if (!service) throw `service not found for young ${young._id}, center ${center?._id} in department ${young?.department}`;
+    const contact = service?.contacts.find((c) => c.cohort === young.cohort) || {};
 
     const html = fs.readFileSync(path.resolve(__dirname, "./cohesion.html"), "utf8");
     return html
-      .replace(/{{REFERENT_NAME}}/g, service?.contactName)
-      .replace(/{{REFERENT_PHONE}}/g, service?.contactPhone)
+      .replace(/{{REFERENT_NAME}}/g, contact?.contactName)
+      .replace(/{{REFERENT_PHONE}}/g, contact?.contactPhone)
       .replace(/{{DATE}}/g, formatStringDate(Date.now()))
       .replace(/{{FIRST_NAME}}/g, young.firstName)
       .replace(/{{LAST_NAME}}/g, young.lastName)
@@ -52,6 +70,7 @@ const render = async (young) => {
       .replace(/{{ADDRESS}}/g, young.address)
       .replace(/{{ZIP}}/g, young.zip)
       .replace(/{{CITY}}/g, young.city)
+      .replace(/{{COHESION_STAY_DATE_STRING}}/g, COHESION_STAY_DATE_STRING[young.cohort])
       .replace(/{{COHESION_CENTER_NAME}}/g, center.name)
       .replace(/{{COHESION_CENTER_ADDRESS}}/g, center.address)
       .replace(/{{COHESION_CENTER_ZIP}}/g, center.zip)
@@ -68,8 +87,10 @@ const render = async (young) => {
   }
 };
 
+// todo ⚠️ not updates because no Février 2022 ⚠️
 const renderDOMTOM = async (young) => {
   try {
+    if (young.cohort !== "Février 2022") throw `young ${young.id} unauthorized`;
     if (!young.cohesionCenterId && young.deplacementPhase1Autonomous !== "true") throw `unauthorized`;
     const center = await CohesionCenterModel.findById(young.cohesionCenterId);
     if (!center) throw `center ${young.cohesionCenterId} not found for young ${young._id}`;
