09633c4cf3e8652a83a766a9cea5d8f8b10d01f5
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2022-01-19 10:32:19 +0100

    fix(support): see as with bandeau

--- support/src/components/Wrapper.js
+++ support/src/components/Wrapper.js
@@ -46,43 +46,23 @@ const Wrapper = ({ children }) => {
           </div>
           {isLoggedIn ? (
             <>
-              <Popover className="relative flex justify-end flex-1 order-2 w-auto md:flex-none lg:w-1/3">
+              <Popover className="relative flex justify-end lg:flex-1 order-2 w-auto md:flex-none lg:w-1/3">
                 <Popover.Button className="flex items-start justify-center gap-3 p-0 text-left bg-white border-none rounded-none shadow-none">
                   <div className="rounded-full h-9 w-9 bg-snu-purple-300"></div>
                   <div className="flex flex-col justify-center h-full">
                     <span className="text-sm font-medium text-gray-700">{user.firstName}</span>
-                    {!!user.role && <span className="text-xs font-medium text-gray-500">{SUPPORT_ROLES[seeAs || user.role]}</span>}
+                    {!!user.role && <span className="text-xs font-medium text-gray-500">{user.role}</span>}
                   </div>
                 </Popover.Button>
 
                 <Popover.Panel className="absolute right-0 min-w-[208px] lg:min-w-0 z-10 top-10">
                   <div className="flex flex-col gap-4 px-4 py-3 bg-white border border-gray-300 rounded-md">
                     {user.role === "admin" && (
-                      <>
-                        <Link href="/admin/knowledge-base">
-                          <a href="#" className="text-sm font-medium text-gray-700 cursor-pointer">
-                            Admin
-                          </a>
-                        </Link>
-                        <Popover className="relative flex justify-end order-3 flex-1 w-auto md:flex-none">
-                          <Popover.Button className="flex items-start justify-center gap-3 p-0 text-left bg-white border-none rounded-none shadow-none">
-                            {/* <img src="/assets/change-user.png" className="opacity-30 h-5 w-5 my-auto object-contain"></img> */}
-                            <div className="flex flex-col justify-center h-full">
-                              <span className="text-sm font-medium text-gray-700">Voir en tant que</span>
-                            </div>
-                          </Popover.Button>
-
-                          <Popover.Panel className="absolute right-0 min-w-[208px] lg:min-w-0 z-10 top-10">
-                            <div className="flex flex-col gap-4 px-4 py-3 bg-white border border-gray-300 rounded-md">
-                              {Object.keys(SUPPORT_ROLES).map((role) => (
-                                <a key={role} onClick={() => setSeeAs(role)} className={`text-sm font-${seeAs === role ? "bold" : "medium"} text-gray-700 cursor-pointer`}>
-                                  {SUPPORT_ROLES[role]}
-                                </a>
-                              ))}
-                            </div>
-                          </Popover.Panel>
-                        </Popover>
-                      </>
+                      <Link href="/admin/knowledge-base">
+                        <a href="#" className="text-sm font-medium text-gray-700 cursor-pointer">
+                          Admin
+                        </a>
+                      </Link>
                     )}
                     <a onClick={onLogout} className="text-sm font-medium text-gray-700 cursor-pointer">
                       Déconnexion
@@ -90,6 +70,27 @@ const Wrapper = ({ children }) => {
                   </div>
                 </Popover.Panel>
               </Popover>
+              {user.role === "admin" && (
+                <Popover className="relative flex justify-end order-1 margin md:order-3 mx-auto w-auto md:flex-none /3">
+                  <Popover.Button className="flex items-center justify-center gap-3 p-0 text-left bg-white border-none rounded-none shadow-none">
+                    <img src="/assets/change-user.png" className="h-5 w-5 grayscale" />
+                    <div className="flex flex-col justify-center h-full">
+                      <span className="text-sm font-medium text-gray-700">Voir en tant que</span>
+                      {/* {!!user.role && <span className="text-xs font-medium text-gray-500">{SUPPORT_ROLES[seeAs || user.role]}</span>} */}
+                    </div>
+                  </Popover.Button>
+
+                  <Popover.Panel className="absolute right-0 min-w-[208px] lg:min-w-0 z-10 top-10">
+                    <div className="flex flex-col gap-4 px-4 py-3 bg-white border border-gray-300 rounded-md">
+                      {Object.keys(SUPPORT_ROLES).map((role) => (
+                        <a key={role} onClick={() => setSeeAs(role)} className={`text-sm font-${seeAs === role ? "bold" : "medium"} text-gray-700 cursor-pointer`}>
+                          {SUPPORT_ROLES[role]}
+                        </a>
+                      ))}
+                    </div>
+                  </Popover.Panel>
+                </Popover>
+              )}
             </>
           ) : (
             <div className="flex items-center justify-end flex-1 order-2 w-auto gap-3 md:flex-none md:order-3 md:gap-5 lg:w-1/3 lg:gap-10">
@@ -103,6 +104,11 @@ const Wrapper = ({ children }) => {
           )}
         </div>
       </header>
+      {seeAs !== "admin" && (
+        <button onClick={() => setSeeAs("admin")} className="bg-red-500 border-none rounded-none font-normal">
+          Vous visualisez la base de connaissance en tant que {SUPPORT_ROLES[seeAs]}, pour retourner à votre vue cliquez ici
+        </button>
+      )}
       <main className="flex-1 bg-[#F3F4F6]">{children}</main>
       <footer className="flex flex-col gap-6 wrapper bg-white w-full">
         <div className="flex flex-col gap-6 wrapper w-full">
