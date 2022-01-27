d226c032df2729a904a740da161749201661415a
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2022-01-27 16:54:40 +0100

    fix(app): department for young (#1148)

--- app/src/scenes/inscription/Create/stepCoordonnees.js
+++ app/src/scenes/inscription/Create/stepCoordonnees.js
@@ -508,6 +508,7 @@ export default function StepCoordonnees() {
                               schoolId: "schoolId",
                               schoolCountry: "schoolCountry",
                               schoolCity: "schoolCity",
+                              schoolDepartment: "schoolDepartment",
                             }}
                           />
                         </div>
