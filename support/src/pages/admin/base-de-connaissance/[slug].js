9ec9a1e6dde2f7eedc8873036bbfd17defe5ae43
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2022-02-01 02:20:30 +0100

    feat(support): metadata for knowledge base at the right place

--- support/src/pages/admin/base-de-connaissance/[slug].js
+++ support/src/pages/admin/base-de-connaissance/[slug].js
@@ -13,7 +13,6 @@ import Loader from "../../../components/Loader";
 import ResizablePanel from "../../../components/ResizablePanel";
 import withAuth from "../../../hocs/withAuth";
 import useKnowledgeBaseData from "../../../hooks/useKnowledgeBaseData";
-import Head from "next/head";
 
 const KnowledgeBase = () => {
   const [treeVisible, setTreeVisible] = useState(!localStorage.getItem("snu-support-kb-tree-hidden"));
@@ -53,64 +52,46 @@ const KnowledgeBase = () => {
   }, [isRoot, slug, item]);
 
   return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#32257F" />
-        <meta name="description" content="Service National Universel | Base de connaissance" />
-        <meta property="og:title" key="og:title" content="Service National Universel | Base de connaissance" />
-        <meta property="og:url" key="og:url" content="https://support.snu.gouv.fr/base-de-connaissance/" />
-        <meta rel="canonical" key="canonical" content="https://support.snu.gouv.fr/base-de-connaissance/" />
-        <meta property="og:description" content="Service National Universel | Base de connaissance" />
-
-        <meta property="og:image" key="og:image" content="https://support.snu.gouv.fr/assets/og-image.png" />
-        <meta property="og:image:type" content="image/png" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta property="og:image:alt" key="og:image:alt" content="Service National Universel | Base de connaissance" />
-        <meta property="og:type" content="article" />
-      </Head>
-      <Layout title="Base de connaissance" className="flex flex-col">
-        <Header>
-          <div className="mb-2 w-full flex justify-between items-baseline">
-            <span className="-ml-8">Base de connaissance</span>
-            <Link href={`/base-de-connaissance/${router?.query?.slug}`}>
-              <a className="bg-transparent border-none hover:underline text-xs font-light p-0 m-0 ml-auto" href="#">
-                Aller à la base de connaissance publique
-              </a>
-            </Link>
-          </div>
-          <KnowledgeBaseAdminBreadcrumb parents={item?.parents} />
-          <div id="breadcrumb" className="py-2 -ml-8 -mr-8 -mb-3 flex justify-between items-baseline flex-shrink-0 w-full bg-snu-purple-900">
-            <button onClick={() => setTreeVisible((v) => !v)} className="bg-transparent border-none hover:underline text-xs font-light p-0 m-0">
-              {treeVisible ? "Masquer" : "Afficher"} l'arbre
+    <Layout title="Base de connaissance" className="flex flex-col">
+      <Header>
+        <div className="mb-2 w-full flex justify-between items-baseline">
+          <span className="-ml-8">Base de connaissance</span>
+          <Link href={`/base-de-connaissance/${router?.query?.slug}`}>
+            <a className="bg-transparent border-none hover:underline text-xs font-light p-0 m-0 ml-auto" href="#">
+              Aller à la base de connaissance publique
+            </a>
+          </Link>
+        </div>
+        <KnowledgeBaseAdminBreadcrumb parents={item?.parents} />
+        <div id="breadcrumb" className="py-2 -ml-8 -mr-8 -mb-3 flex justify-between items-baseline flex-shrink-0 w-full bg-snu-purple-900">
+          <button onClick={() => setTreeVisible((v) => !v)} className="bg-transparent border-none hover:underline text-xs font-light p-0 m-0">
+            {treeVisible ? "Masquer" : "Afficher"} l'arbre
+          </button>
+          {!isRoot && (
+            <button onClick={() => setMetadataVisible((v) => !v)} className="bg-transparent border-none hover:underline text-xs font-light p-0 m-0 ml-auto -mr-8">
+              {metadataVisible ? "Masquer les infos" : "Afficher les infos"}
             </button>
-            {!isRoot && (
-              <button onClick={() => setMetadataVisible((v) => !v)} className="bg-transparent border-none hover:underline text-xs font-light p-0 m-0 ml-auto -mr-8">
-                {metadataVisible ? "Masquer les infos" : "Afficher les infos"}
-              </button>
-            )}
-          </div>
-        </Header>
-        {!item ? (
-          <Loader />
-        ) : (
-          <div className="relative bg-coolGray-200 flex max-w-[calc(100vw - 52px)] border-t-2 h-full w-full m-w-full flex-grow flex-shrink overflow-hidden">
-            <ResizablePanel className={`relative flex flex-grow-0 flex-shrink-0 z-10  ${treeVisible ? "w-80" : "w-0 hidden"}`} name="admin-knowledge-base-tree" position="left">
-              <div className="relative flex flex-col pr-2 overflow-hidden">
-                <KnowledgeBaseAdminTree isSortable onClick={(slug) => router.push(`/admin/base-de-connaissance/${slug || ""}`)} />
-              </div>
-            </ResizablePanel>
-            {isLoading ? <Loader /> : <Content key={slug} item={item} />}
-            {!isRoot && (
-              <>
-                <KnowledgeBaseAdminItemMetadata key={item?._id} visible={metadataVisible} />
-              </>
-            )}
-          </div>
-        )}
-      </Layout>
-    </>
+          )}
+        </div>
+      </Header>
+      {!item ? (
+        <Loader />
+      ) : (
+        <div className="relative bg-coolGray-200 flex max-w-[calc(100vw - 52px)] border-t-2 h-full w-full m-w-full flex-grow flex-shrink overflow-hidden">
+          <ResizablePanel className={`relative flex flex-grow-0 flex-shrink-0 z-10  ${treeVisible ? "w-80" : "w-0 hidden"}`} name="admin-knowledge-base-tree" position="left">
+            <div className="relative flex flex-col pr-2 overflow-hidden">
+              <KnowledgeBaseAdminTree isSortable onClick={(slug) => router.push(`/admin/base-de-connaissance/${slug || ""}`)} />
+            </div>
+          </ResizablePanel>
+          {isLoading ? <Loader /> : <Content key={slug} item={item} />}
+          {!isRoot && (
+            <>
+              <KnowledgeBaseAdminItemMetadata key={item?._id} visible={metadataVisible} />
+            </>
+          )}
+        </div>
+      )}
+    </Layout>
   );
 };
 
