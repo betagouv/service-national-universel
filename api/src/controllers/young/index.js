e996ec6a2f1fd892a739aab7c395dad867142d9a
Chloé Danisevicius <chloe.danisevicius@gmail.com>
2022-02-21 16:07:13 +0100

    hotfix(api): comment template while checking

--- api/src/controllers/young/index.js
+++ api/src/controllers/young/index.js
@@ -419,22 +419,22 @@ router.put("/", passport.authenticate("young", { session: false, failWithError: 
 
     // await updateApplicationsWithYoungOrMission({ young, newYoung: value });
 
-    if (req.user.department !== young.department) {
-      const referents = await ReferentModel.find({ department: req.user.department, role: ROLES.REFERENT_DEPARTMENT });
-      for (let referent of referents) {
-        await sendTemplate(SENDINBLUE_TEMPLATES.young.DEPARTMENT_CHANGE, {
-          emailTo: [{ name: `${referent.firstName} ${referent.lastName}`, email: referent.email }],
-          params: {
-            youngFirstName: young.firstName,
-            youngLastName: young.lastName,
-            cta: `${config.ADMIN_URL}/volontaire/${young._id}`,
-          },
-        });
-      }
-    }
+    //! needs further checking
+    // if (req.user.department !== young.department) {
+    //   const referents = await ReferentModel.find({ department: req.user.department, role: ROLES.REFERENT_DEPARTMENT });
+    //   for (let referent of referents) {
+    //     await sendTemplate(SENDINBLUE_TEMPLATES.young.DEPARTMENT_CHANGE, {
+    //       emailTo: [{ name: `${referent.firstName} ${referent.lastName}`, email: referent.email }],
+    //       params: {
+    //         youngFirstName: young.firstName,
+    //         youngLastName: young.lastName,
+    //         cta: `${config.ADMIN_URL}/volontaire/${young._id}`,
+    //       },
+    //     });
+    //   }
+    // }
     young.set(value);
     await young.save({ fromUser: req.user });
-
 
     // Check quartier prioritaires.
     if (value.zip && value.city && value.address) {
