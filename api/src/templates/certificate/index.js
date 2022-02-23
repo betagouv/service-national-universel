26a8029d4358eed171cd5166d93d95b7360e8b18
tangimds <tangimds@gmail.com>
2022-02-23 22:14:23 +0100

    fix: download attestation phase1

--- api/src/templates/certificate/index.js
+++ api/src/templates/certificate/index.js
@@ -2,13 +2,15 @@ const fs = require("fs");
 const path = require("path");
 const { getSignedUrl, getBaseUrl } = require("../../utils");
 const { COHESION_STAY_LIMIT_DATE } = require("snu-lib");
+const SessionPhase1Model = require("../../models/sessionPhase1");
+const CohesionCenterModel = require("../../models/cohesionCenter");
 
-const getLocationCohesionCenter = (y) => {
+const getLocationCohesionCenter = (cohesionCenter) => {
   let t = "";
-  if (y.cohesionCenterCity) {
-    t = `à ${y.cohesionCenterCity}`;
-    if (y.cohesionCenterZip) {
-      t += `, ${y.cohesionCenterZip}`;
+  if (cohesionCenter.city) {
+    t = `à ${cohesionCenter.city}`;
+    if (cohesionCenter.zip) {
+      t += `, ${cohesionCenter.zip}`;
     }
   }
   return t;
@@ -22,21 +24,27 @@ function getBgUrl2019() {
   return getSignedUrl("certificates/certificateTemplate-2019.png");
 }
 
-const phase1 = (young) => {
+const phase1 = async (young) => {
   const d = new Date();
   const html = fs.readFileSync(path.resolve(__dirname, "./phase1.html"), "utf8");
   const template = young.cohort === "2019" ? getBgUrl2019() : getBgUrl();
-  const COHESION_CENTER_LOCATION = getLocationCohesionCenter(young);
+
+  const session = await SessionPhase1Model.findById(young.sessionPhase1Id);
+  if (!session) return;
+  const cohesionCenter = await CohesionCenterModel.findById(session.cohesionCenterId);
+  if (!cohesionCenter) return;
+
+  const COHESION_CENTER_LOCATION = getLocationCohesionCenter(cohesionCenter);
   return html
     .replace(/{{FIRST_NAME}}/g, young.firstName)
     .replace(/{{LAST_NAME}}/g, young.lastName)
     .replace(/{{COHORT}}/g, young.cohort)
     .replace(/{{COHESION_DATE}}/g, COHESION_STAY_LIMIT_DATE[young.cohort].toLowerCase())
-    .replace(/{{COHESION_CENTER_NAME}}/g, young.cohesionCenterName || "")
+    .replace(/{{COHESION_CENTER_NAME}}/g, cohesionCenter.name || "")
     .replace(/{{COHESION_CENTER_LOCATION}}/g, COHESION_CENTER_LOCATION)
     .replace(/{{BASE_URL}}/g, getBaseUrl())
     .replace(/{{GENERAL_BG}}/g, template)
-    .replace(/{{DATE}}/g, "2 juillet 2021");
+    .replace(/{{DATE}}/g, d.toLocaleDateString("fr-FR", { year: "numeric", month: "long", day: "numeric" }));
 };
 
 const phase2 = (young) => {
