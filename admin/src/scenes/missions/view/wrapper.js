35cd62db7b667e85cf426bbddf4fd4f82cfe2ff4
tangimds <tangimds@gmail.com>
2022-02-26 18:32:24 +0100

    Update wrapper.js

--- admin/src/scenes/missions/view/wrapper.js
+++ admin/src/scenes/missions/view/wrapper.js
@@ -138,6 +138,7 @@ const Header = styled.div`
 `;
 
 const Box = styled.div`
+  width: ${(props) => props.width || 100}%;
   height: 100%;
   background-color: #fff;
   filter: drop-shadow(0px 2px 4px rgba(0, 0, 0, 0.05));
