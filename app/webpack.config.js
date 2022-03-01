d2d7e1237f16068667f64065657fab6e178399fa
Tangi Mendès <tangimds@gmail.com>
2022-03-01 09:54:24 +0100

    up (#1295)

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -57,7 +57,7 @@ module.exports = () => {
       rules: [
         {
           test: /\.css$/,
-          use: ["style-loader", "css-loader"],
+          use: ["style-loader", "css-loader", "postcss-loader"],
         },
         {
           test: /\.js$/,
