adba980399124e6a3d42f70eee76c01952125fa9
tangimds <tangimds@gmail.com>
2022-03-04 09:32:58 +0100

    up

--- admin/src/scenes/dashboard/volontaire/index.js
+++ admin/src/scenes/dashboard/volontaire/index.js
@@ -8,30 +8,36 @@ import FilterRegion from "../components/FilterRegion";
 import FilterDepartment from "../components/FilterDepartment";
 import SubTab from "./status";
 
-import { YOUNG_STATUS, ROLES } from "../../../utils";
+import { YOUNG_STATUS, ROLES, translate } from "../../../utils";
 
 export default function Index() {
   const user = useSelector((state) => state.Auth.user);
   const [filter, setFilter] = useState();
 
   function updateFilter(n) {
+    console.log("✍️ ~ n", n);
     setFilter({
-      ...(filter || { status: Object.keys(YOUNG_STATUS), region: [], department: [], cohort: filter?.cohort || ["2021"] }),
+      ...(filter || { status: YOUNG_STATUS.VALIDATED, region: [], department: [], cohort: filter?.cohort || ["2021"] }),
       ...n,
     });
   }
 
   useEffect(() => {
     const cohort = ["2021"];
-    const status = Object.keys(YOUNG_STATUS).filter((e) => !["IN_PROGRESS", "NOT_ELIGIBLE"].includes(e));
     if (user.role === ROLES.REFERENT_DEPARTMENT) {
-      updateFilter({ department: [user.department], status, cohort });
+      updateFilter({ department: [user.department], cohort });
     } else if (user.role === ROLES.REFERENT_REGION) {
-      updateFilter({ region: [user.region], status, cohort });
+      updateFilter({ region: [user.region], cohort });
     } else {
       updateFilter({ cohort });
     }
   }, []);
+
+  const getOptionsStatus = () => {
+    let STATUS = Object.keys(YOUNG_STATUS).map((s) => ({ label: translate(YOUNG_STATUS[s]), value: s }));
+    if (user.role !== ROLES.ADMIN) STATUS = STATUS.filter((e) => !["IN_PROGRESS", "NOT_ELIGIBLE"].includes(e.value));
+    return STATUS;
+  };
 
   return (
     <>
@@ -42,21 +48,20 @@ export default function Index() {
             <FiltersList>
               <FilterRegion onChange={(region) => updateFilter({ region })} value={filter.region} filter={filter} />
               <FilterDepartment onChange={(department) => updateFilter({ department })} value={filter.department} filter={filter} />
-              <FilterWrapper>
-                <MultiSelect
-                  label="Cohorte(s)"
-                  options={[
-                    { value: "2019", label: "2019" },
-                    { value: "2020", label: "2020" },
-                    { value: "2021", label: "2021" },
-                    { value: "Février 2022", label: "Février 2022" },
-                    { value: "Juin 2022", label: "Juin 2022" },
-                    { value: "Juillet 2022", label: "Juillet 2022" },
-                  ]}
-                  onChange={(cohort) => updateFilter({ cohort })}
-                  value={filter.cohort}
-                />
-              </FilterWrapper>
+              <MultiSelect
+                label="Cohorte(s)"
+                options={[
+                  { value: "2019", label: "2019" },
+                  { value: "2020", label: "2020" },
+                  { value: "2021", label: "2021" },
+                  { value: "Février 2022", label: "Février 2022" },
+                  { value: "Juin 2022", label: "Juin 2022" },
+                  { value: "Juillet 2022", label: "Juillet 2022" },
+                ]}
+                onChange={(cohort) => updateFilter({ cohort })}
+                value={filter.cohort}
+              />
+              <MultiSelect label="Statut(s)" options={getOptionsStatus()} value={filter.status} onChange={(status) => updateFilter({ status })} />
             </FiltersList>
           ) : null}
         </Col>
@@ -73,7 +78,4 @@ const FiltersList = styled.div`
   justify-content: flex-end;
   flex-wrap: wrap;
   margin-bottom: 10px;
-`;
-const FilterWrapper = styled.div`
-  margin: 0 5px 10px;
 `;
--- admin/src/scenes/dashboard/volontaire/status/index.js
+++ admin/src/scenes/dashboard/volontaire/status/index.js
@@ -53,6 +53,7 @@ export default function StatusIndex({ filter }) {
       if (filter.cohort?.length) body.query.bool.filter.push({ terms: { "cohort.keyword": filter.cohort } });
       if (filter.region?.length) body.query.bool.filter.push({ terms: { "region.keyword": filter.region } });
       if (filter.department?.length) body.query.bool.filter.push({ terms: { "department.keyword": filter.department } });
+      if (filter.status?.length) body.query.bool.filter.push({ terms: { "status.keyword": filter.status } });
 
       const { responses } = await api.esQuery("young", body);
 
