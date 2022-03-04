241be50acc4f86b0568c02d6565300c6139a53bd
Chloe Danisevicius <62291986+SpookyUmi@users.noreply.github.com>
2022-03-04 15:56:01 +0100

    fix(api/crons): application pending (#1280)
    * fix(api/crons): application pending
    
    * fix and test with slack

--- api/src/crons/applicationPending.js
+++ api/src/crons/applicationPending.js
@@ -1,5 +1,4 @@
 require("../mongo");
-
 const { capture } = require("../sentry");
 const Application = require("../models/application");
 const Referent = require("../models/referent");
@@ -7,6 +6,7 @@ const { sendTemplate } = require("../sendinblue");
 const slack = require("../slack");
 const { SENDINBLUE_TEMPLATES } = require("snu-lib");
 const { ADMIN_URL } = require("../config");
+const { differenceInDays } = require("date-fns");
 
 exports.handler = async () => {
   try {
@@ -14,33 +14,35 @@ exports.handler = async () => {
     const now = Date.now();
     const cursor = await Application.find({
       status: "WAITING_VALIDATION",
-      createdAt: { $lt: addDays(now, 8), $gte: addDays(now, 7) },
     }).cursor();
     await cursor.eachAsync(async function (application) {
       countNotice++;
+      let patches = await application.patches.find({ ref: application._id, date: { $gte: new Date("2021-11-01").toISOString() } }).sort("-date");
+      if (!patches.length) return;
+      patches = patches.filter((patch) => patch.ops.filter((op) => op.path === "/status" && op.value === "WAITING_VALIDATION").length > 0);
+      if (!patches.length) return;
       const tutor = await Referent.findById(application.tutorId);
       if (!tutor) return;
-      // send a mail to the tutor
-      sendTemplate(SENDINBLUE_TEMPLATES.referent.APPLICATION_REMINDER, {
-        emailTo: [{ name: `${tutor.firstName} ${tutor.lastName}`, email: tutor.email }],
-        params: {
-          cta: `${ADMIN_URL}/dashboard`,
-          youngFirstName: application.youngFirstName,
-          youngLastName: application.youngLastName,
-          missionName: application.missionName,
-        },
-      });
+      if (differenceInDays(now, patches[0].date) >= 7) {
+        // send a mail to the tutor
+        slack.success({
+          title: "1 week notice pending application",
+          text: `applicationId: ${application._id} - change status date: ${patches[0].date}, status: WAITING_VALIDATION`,
+        });
+        // sendTemplate(SENDINBLUE_TEMPLATES.referent.APPLICATION_REMINDER, {
+        //   emailTo: [{ name: `${tutor.firstName} ${tutor.lastName}`, email: tutor.email }],
+        //   params: {
+        //     cta: `${ADMIN_URL}/dashboard`,
+        //     youngFirstName: application.youngFirstName,
+        //     youngLastName: application.youngLastName,
+        //     missionName: application.missionName,
+        //   },
+        // });
+      }
     });
-    slack.success({ title: "1 week notice pending application", text: `${countNotice} pending application has been noticed !` });
+    slack.success({ title: "1 week notice pending application 📆", text: `${countNotice} pending application has been noticed !` });
   } catch (e) {
     capture(e);
     slack.error({ title: "applicationPending", text: JSON.stringify(e) });
   }
-};
-
-const addDays = (d, days = 1) => {
-  var date = new Date(d);
-  date.setDate(date.getDate() + days);
-  date.setUTCHours(0, 0, 0, 0);
-  return date;
 };
