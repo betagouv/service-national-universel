43831da679bb53453bc6e824945ab613a2cc74a5
hlecourt <hlecourt99@gmail.com>
2022-03-14 16:42:15 +0100

    Update status.js

--- admin/src/scenes/dashboard/missions/status.js
+++ admin/src/scenes/dashboard/missions/status.js
@@ -9,7 +9,6 @@ export default function Status({ data, filter, getLink }) {
 
   useEffect(() => {
     if (Object.keys(data).length !== 0) {
-      console.log("here", data);
       setTotal(Object.keys(data).reduce((acc, a) => acc + data[a].doc_count, 0));
     }
   }, [data]);
@@ -17,27 +16,26 @@ export default function Status({ data, filter, getLink }) {
   if (Object.keys(data).length === 0) {
     return null;
   }
-
+  console.log(data);
   return (
     <React.Fragment>
       <h3 className="mt-4 mb-2 text-xl">Statut des missions proposées par les structures</h3>
       <Row>
         {Object.values(MISSION_STATUS).map((l, k) => {
-          if (!data[l]) return null;
           return (
             <Col md={6} xl={3} key={k}>
               <Link to={getLink({ base: `/mission`, filter, filtersUrl: [`STATUS=%5B"${l}"%5D`] })}>
                 <Card borderBottomColor={MISSION_STATUS_COLORS[l]}>
-                  <CardTitle>{translate(data[l].name)}</CardTitle>
+                  <CardTitle>{translate(l)}</CardTitle>
                   <CardValueWrapper>
-                    <CardValue>{data[l].doc_count || 0}</CardValue>
+                    <CardValue>{data[l]?.doc_count || 0}</CardValue>
                     <CardPercentage>
-                      {total ? `${(((data[l].doc_count || 0) * 100) / total).toFixed(0)}%` : `0%`}
+                      {total ? `${(((data[l]?.doc_count || 0) * 100) / total).toFixed(0)}%` : `0%`}
                       <CardArrow />
                     </CardPercentage>
                   </CardValueWrapper>
-                  <CardSubtitle className="py-2">Places disponibles: {data[l].placesLeft || 0}</CardSubtitle>
-                  <CardSubtitle>Places totales: {data[l].placesTotal || 0}</CardSubtitle>
+                  <CardSubtitle className="py-2">Places disponibles: {data[l]?.placesLeft || 0}</CardSubtitle>
+                  <CardSubtitle>Places totales: {data[l]?.placesTotal || 0}</CardSubtitle>
                 </Card>
               </Link>
             </Col>
