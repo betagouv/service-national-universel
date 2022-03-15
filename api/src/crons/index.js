66cf3ba0125165a136125aa45cd7be370af246a9
Chloe Danisevicius <62291986+SpookyUmi@users.noreply.github.com>
2022-03-15 15:29:18 +0100

    Fix(api):  application pending script (#1368)
    * fix(api): cron application pending
    
    * change cron

--- api/src/crons/index.js
+++ api/src/crons/index.js
@@ -34,7 +34,7 @@ if (ENVIRONMENT === "production" && process.env.INSTANCE_NUMBER === "0") {
   //   newMissionReminder.handler();
   // });
 
-  cron.schedule("0 9 * * 1", function () {
+  cron.schedule("30 * * * *", function () {
     applicationPending.handler();
   });
 
