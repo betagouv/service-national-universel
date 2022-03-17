4a924d7095edb02c5a20940b71387498518b1071
SpookyUmi <chloe.danisevicius@gmail.com>
2022-03-17 10:27:22 +0100

    feat(api+app): connect public form to new support

--- api/src/controllers/zammood.js
+++ api/src/controllers/zammood.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const slack = require("../slack");
+const Joi = require("joi");
 
 const { capture } = require("../sentry");
 const zammood = require("../zammood");
 const { ERRORS } = require("../utils");
 const { ADMIN_URL } = require("../config.js");
-
 
 router.get("/tickets", passport.authenticate("referent", { session: false, failWithError: true }), async (req, res) => {
   try {
@@ -37,10 +37,29 @@ router.get("/ticket/:id", passport.authenticate("referent", { session: false, fa
 // Create a new ticket while authenticated
 router.post("/ticket", passport.authenticate("referent", { session: false, failWithError: true }), async (req, res) => {
   try {
-    const { subject, message, clientId } = req.body;
+    const obj = {
+      clientId: req.body.clientId,
+      subject: req.body.subject,
+      message: req.body.message,
+    };
+    const { error, value } = Joi.object({
+      clientId: Joi.string().required(),
+      subject: Joi.string().required(),
+      message: Joi.string().required(),
+    })
+      .unknown()
+      .validate(obj);
+    if (error) return res.status(400).send({ ok: false, code: ERRORS.INVALID_PARAMS });
+    const { subject, message, clientId } = value;
     const structureLink = `${ADMIN_URL}/structure/${req.user.structureId}`;
     const missionsLink = `${ADMIN_URL}/structure/${req.user.structureId}/missions`;
-    const userAttributes = [{ name: "date de création", value: req.user.createdAt }, { name: "dernière connexion", value: req.user.lastLoginAt }, { name: "role", value: req.user.role }, { name: "lien vers la fiche structure", value: structureLink }, { name: "lien général vers la page des missions proposées par la structure", value: missionsLink }];
+    const userAttributes = [
+      { name: "date de création", value: req.user.createdAt },
+      { name: "dernière connexion", value: req.user.lastLoginAt },
+      { name: "role", value: req.user.role },
+      { name: "lien vers la fiche structure", value: structureLink },
+      { name: "lien général vers la page des missions proposées par la structure", value: missionsLink },
+    ];
     const response = await zammood.api("/v0/message", {
       method: "POST",
       credentials: "include",
@@ -66,18 +85,47 @@ router.post("/ticket", passport.authenticate("referent", { session: false, failW
 // Create a new ticket for public users => not used for now
 router.post("/ticket/form", async (req, res) => {
   try {
-    const { subject, message, email, firstName, lastName, zammadId } = req.body;
+    //const { subject, message, email, firstName, lastName, clientId, department, region } = req.body;
+    const obj = {
+      email: req.body.email,
+      subject: req.body.subject,
+      message: req.body.message,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      clientId: req.body.clientId,
+      department: req.body.department,
+      region: req.body.region,
+    };
+    const { error, value } = Joi.object({
+      email: Joi.string().email().required(),
+      subject: Joi.string().required(),
+      message: Joi.string().required(),
+      firstName: Joi.string().required(),
+      lastName: Joi.string().required(),
+      clientId: Joi.string().required(),
+      department: Joi.string().required(),
+      region: Joi.string().required(),
+    })
+      .unknown()
+      .validate(obj);
+    if (error) return res.status(400).send({ ok: false, code: ERRORS.INVALID_PARAMS });
+    const { subject, message, firstName, lastName, email, clientId, department, region } = value;
+    const userAttributes = [
+      { name: "département", value: department },
+      { name: "région", value: region },
+    ];
     const response = await zammood.api("/v0/message", {
       method: "POST",
       credentials: "include",
       body: JSON.stringify({
         message,
         email,
-        zammadId,
+        clientId,
         subject,
         firstName,
         lastName,
         source: "FORM",
+        attributes: userAttributes,
       }),
     });
     if (!response.ok) return res.status(400).send({ ok: false, code: response });
