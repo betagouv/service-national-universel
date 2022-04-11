9a31c4c1a1e25439b15eee1883b227e685366782
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2022-04-11 10:08:46 +0200

    chore: dont remove useful files

---
+++ api/src/models/supportTag.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const mongooseElastic = require("@selego/mongoose-elastic");
+
+const esClient = require("../es");
+
+const MODELNAME = "support_tag";
+
+const Schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    // zammad migration
+    zammadId: {
+      type: String,
+    },
+  },
+  { timestamps: true },
+);
+
+Schema.plugin(mongooseElastic(esClient), MODELNAME);
+
+const OBJ = mongoose.model(MODELNAME, Schema);
+module.exports = OBJ;
