2295c68e3cb799f27339a7287cdde617b629fbd1
HugoKessler <94533987+HugoKessler@users.noreply.github.com>
2022-04-19 15:12:15 +0200

    feat(admin) : sync connected young to zammood (#1483)
    * sync done, todo : referent
    
    * clean
    
    * fix profil link
    
    * young attributes
    
    * role fix

--- app/src/scenes/support-center/ticket/create.js
+++ app/src/scenes/support-center/ticket/create.js
@@ -32,14 +32,21 @@ export default function TicketCreate() {
           onSubmit={async (values) => {
             try {
               const { message, step1, step2 } = values;
-              const { ok, code } = await api.post("/zammad-support-center/ticket", {
-                title: `${step1?.label} - ${step2?.label}`,
+              const title = `${step1?.label} - ${step2?.label}`;
+              const { data, ok, code } = await api.post("/zammad-support-center/ticket", {
+                title,
                 message,
                 // eslint-disable-next-line no-unsafe-optional-chaining
                 tags: [...new Set([...tags, ...(step1?.tags || []), ...(step2?.tags || [])])], // we use this dirty hack to remove duplicates
               });
               if (!ok) return toastr.error("Une erreur s'est produite lors de la création de ce ticket :", translate(code));
-              toastr.success("Ticket créé");
+              const response = await api.post("/zammood/ticket", {
+                message,
+                subject: title,
+                clientId: data.id,
+              });
+              if (!response.ok) return toastr.error("Une erreur s'est produite lors de la création de ce ticket :", translate(response.code));
+              toastr.success("Demande envoyée");
               history.push("/besoin-d-aide");
             } catch (e) {
               console.log(e);
