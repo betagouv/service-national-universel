49d8953987b341af83359c4d7d444a5f54193890
tangimds <tangimds@gmail.com>
2022-04-26 10:52:36 +0200

    Update loginAttempts.js

--- api/src/crons/loginAttempts.js
+++ api/src/crons/loginAttempts.js
@@ -1,18 +1,24 @@
 require("../mongo");
 const { capture } = require("../sentry");
 const Referent = require("../models/referent");
+const Young = require("../models/young");
 const slack = require("../slack");
 
 exports.handler = async () => {
+  await clean(Referent);
+  await clean(Young);
+};
+
+const clean = async (model) => {
   try {
     let count = 0;
-    const cursor = await Referent.find({ loginAttempts: { $gt: 0 } }).cursor();
-    await cursor.eachAsync(async function (referent) {
+    const cursor = await model.find({ loginAttempts: { $gt: 0 } }).cursor();
+    await cursor.eachAsync(async function (doc) {
       count++;
-      referent.set({ loginAttempts: 0 });
-      await referent.save();
+      doc.set({ loginAttempts: 0 });
+      await doc.save();
     });
-    count > 0 && slack.success({ title: "loginAttempts", text: `${count} referents ont droits de recommencer à se connecter !` });
+    count > 0 && slack.success({ title: "loginAttempts", text: `${count} ${model.modelName} back again !` });
   } catch (e) {
     capture(e);
     slack.error({ title: "loginAttempts", text: JSON.stringify(e) });
