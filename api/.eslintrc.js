451cded035af661f4f45bb444dbdb8cbefec1e01
Raphaël Huchet <raph@selego.co>
2022-04-29 17:22:45 +0200

    chore: eslint

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
     commonjs: true,
     node: true,
     es6: true,
+    jest: true,
   },
   extends: ["eslint:recommended", "plugin:import/recommended", "plugin:prettier/recommended"],
   plugins: ["prettier", "jest"],
