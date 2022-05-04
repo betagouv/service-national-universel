339a80150c758bf0629ca1ee383942b2737a60e8
tangimds <tangimds@gmail.com>
2022-05-04 15:45:17 +0200

    fix: view center

--- admin/src/scenes/centers/view/index.js
+++ admin/src/scenes/centers/view/index.js
@@ -14,14 +14,16 @@ import { translate, CENTER_ROLES, ROLES } from "../../../utils";
 import { environment } from "../../../config";
 
 export default function Index({ ...props }) {
+  const history = useHistory();
+  const user = useSelector((state) => state.Auth.user);
+
   const [center, setCenter] = useState();
   const [sessions, setSessions] = useState([]);
   const [focusedCohort, setFocusedCohort] = useState();
   const [focusedSession, setFocusedSession] = useState();
   const [focusedTab, setFocusedTab] = useState("equipe");
+  const [occupationPercentage, setOccupationPercentage] = useState();
   const [availableCohorts, setAvailableCohorts] = useState([]);
-  const history = useHistory();
-  const user = useSelector((state) => state.Auth.user);
 
   useEffect(() => {
     (async () => {
@@ -55,6 +57,12 @@ export default function Index({ ...props }) {
   useEffect(() => {
     setFocusedSession(sessions.find((e) => e.cohort === focusedCohort));
   }, [focusedCohort, sessions]);
+
+  useEffect(() => {
+    if (!focusedSession) return;
+    const occupation = focusedSession.placesTotal ? (((focusedSession.placesTotal - focusedSession.placesLeft) * 100) / focusedSession.placesTotal).toFixed(2) : 0;
+    setOccupationPercentage(occupation);
+  }, [focusedSession]);
 
   useEffect(() => {
     (async () => {
@@ -132,6 +140,51 @@ export default function Index({ ...props }) {
     return obj;
   };
 
+  const OccupationCard = ({ occupationPercentage, placesLeft, placesTotal }) => {
+    let height = `h-0`;
+    if (occupationPercentage < 20) height = "h-[20%]";
+    else if (occupationPercentage < 30) height = "h-[30%]";
+    else if (occupationPercentage < 40) height = "h-[40%]";
+    else if (occupationPercentage < 50) height = "h-[50%]";
+    else if (occupationPercentage < 60) height = "h-[60%]";
+    else if (occupationPercentage < 70) height = "h-[70%]";
+    else if (occupationPercentage < 80) height = "h-[80%]";
+    else if (occupationPercentage < 100) height = "h-[90%]";
+    else if (occupationPercentage >= 100) height = "h-[100%]";
+
+    let bgColor = "bg-snu-purple-300";
+    if (occupationPercentage > 100) bgColor = "bg-red-500";
+
+    return occupationPercentage ? (
+      <div className="bg-white rounded-lg shadow-sm py-4 px-8 max-w-xl">
+        <div className="text-lg font-medium mb-1 text-gray-900">Taux d&apos;occupation</div>
+        <div className="flex gap-4">
+          {/* barre */}
+          <div className="flex flex-col justify-end w-9 h-[100px] bg-gray-100 rounded-lg overflow-hidden">
+            <div className={`flex justify-center items-center w-9 ${height} ${bgColor} rounded-lg text-white font-bold text-xs`}>{Math.floor(occupationPercentage)}%</div>
+          </div>
+          {/* nombres */}
+          <div className="flex flex-col justify-around">
+            <div className="flex items-center gap-2">
+              <div className="h-2 w-2 rounded-full bg-snu-purple-300" />
+              <div>
+                <div className="text-xs font-normal">Place occupées</div>
+                <div className="text-base font-bold">{placesTotal - placesLeft}</div>
+              </div>
+            </div>
+            <div className="flex items-center gap-2">
+              <div className="h-2 w-2 rounded-full bg-gray-100" />
+              <div>
+                <div className="text-xs font-normal">Place libres</div>
+                <div className="text-base font-bold">{placesLeft}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    ) : null;
+  };
+
   if (!center) return <div />;
   return (
     <>
@@ -149,12 +202,42 @@ export default function Index({ ...props }) {
           </div>
         ))}
       </div>
-      <div style={{ padding: "0 3rem" }}>
-        <Nav tab={focusedTab} center={center} user={user} cohorts={availableCohorts} onChangeTab={setFocusedTab} focusedSession={focusedSession} />
+      <div className="px-12 my-8">
         {environment !== "production" ? (
-          <>{center?._id && focusedSession?._id ? <NavLink to={`/centre/${center._id}/${focusedSession._id}/volontaires`}>Voir volontaires</NavLink> : null}</>
+          center?._id && focusedSession?._id ? (
+            <div className="flex gap-3">
+              {/* // liste des volontaires */}
+              <div className="bg-white rounded-lg shadow-sm p-4 max-w-xl">
+                <div className="text-lg font-medium mb-1 text-gray-900">Liste des volontaires</div>
+                <div className="text-sm font-normal text-gray-400 mb-3">Accès à la liste des volontaires affectés, au tableau de pointage...</div>
+                <button
+                  className="px-4 py-2 rounded-md bg-snu-purple-300 text-sm text-white hover:shadow-lg"
+                  onClick={() => history.push(`/centre/${center._id}/${focusedSession._id}/volontaires`)}>
+                  Voir les volontaires
+                </button>
+              </div>
+
+              {/* // Taux doccupation */}
+              <OccupationCard occupationPercentage={occupationPercentage} placesTotal={focusedSession.placesTotal} placesLeft={focusedSession.placesLeft} />
+
+              {/* // équipe */}
+              <div className="bg-white rounded-lg shadow-sm p-4 max-w-xl">
+                <div className="flex justify-between">
+                  <div className="text-lg font-medium mb-1 text-gray-900">L&apos;équipe</div>
+                </div>
+                <div className="text-sm font-normal text-gray-400 mb-3">Les coordonnées du chef de centre, du cadre spécialisé, de compagnie...</div>
+                <button
+                  className="px-4 py-2 rounded-md border-[1px] border-snu-purple-300 text-sm text-snu-purple-300 hover:shadow-lg hover:text-white hover:bg-snu-purple-300"
+                  // todo screen equipe
+                  onClick={() => {}}>
+                  Voir l&apos;équipe
+                </button>
+              </div>
+            </div>
+          ) : null
         ) : (
           <>
+            <Nav tab={focusedTab} center={center} user={user} cohorts={availableCohorts} onChangeTab={setFocusedTab} focusedSession={focusedSession} />
             <Switch>
               {/* liste-attente reliquat ? */}
               <Route path="/centre/:id/liste-attente" component={() => <WaitingList center={center} updateCenter={updateCenter} focusedCohort={focusedCohort} />} />
