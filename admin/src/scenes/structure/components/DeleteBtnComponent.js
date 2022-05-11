71b2fec20a57255c8aae4f25668908d3a1199fef
naustra <lebihanp@hotmail.fr>
2022-05-11 18:04:01 +0200

    Better Button

--- admin/src/scenes/structure/components/DeleteBtnComponent.js
+++ admin/src/scenes/structure/components/DeleteBtnComponent.js
@@ -1,14 +1,19 @@
 import * as React from "react";
 import styled from "styled-components";
+import Bin from "../../../assets/Bin";
 
-const DeleteBtnComponent = (props) => <DeleteBtn {...props}>{"🗑"}</DeleteBtn>;
+const DeleteBtnComponent = (props) => (
+  <DeleteBtn>
+    <Bin {...props}></Bin>
+  </DeleteBtn>
+);
 
 export default DeleteBtnComponent;
 
 const DeleteBtn = styled.button`
   border: 4px solid #ce2027;
-  border-radius: 50%;
-  padding: 7px 10px;
+  border-radius: 20%;
+  padding: 2px;
   font-size: 14px;
   font-weight: 700;
   color: #fff;
