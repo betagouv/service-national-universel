82f86a93530ee0e35c21017a0fa69c9ac930ff23
naustra <lebihanp@hotmail.fr>
2022-05-11 18:20:22 +0200

    fix bin

--- admin/src/scenes/structure/components/DeleteBtnComponent.js
+++ admin/src/scenes/structure/components/DeleteBtnComponent.js
@@ -1,24 +1,10 @@
 import * as React from "react";
-import styled from "styled-components";
-import Bin from "../../../assets/Bin";
+import Trash from "../../../assets/icons/Trash";
 
-const DeleteBtnComponent = (props) => (
-  <DeleteBtn>
-    <Bin {...props}></Bin>
-  </DeleteBtn>
+const DeleteBtnComponent = () => (
+  <div className="flex justify-center items-center h-8 w-8 bg-gray-100 cursor-pointer text-gray-600 rounded-full hover:scale-105">
+    <Trash width={16} height={16} />
+  </div>
 );
 
 export default DeleteBtnComponent;
-
-const DeleteBtn = styled.button`
-  border: 4px solid #ce2027;
-  border-radius: 20%;
-  padding: 2px;
-  font-size: 14px;
-  font-weight: 700;
-  color: #fff;
-  cursor: pointer;
-  :hover {
-    background: #ce2027;
-  }
-`;
