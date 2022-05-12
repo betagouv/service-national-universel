c07a4db0172b7f2debcc60344122a146d78e1043
naustra <lebihanp@hotmail.fr>
2022-05-12 10:24:17 +0200

    (educonnect) Clean unused code (#1557)

--- api/src/sso/eduConnect/index.js
+++ api/src/sso/eduConnect/index.js
@@ -1,4 +1,3 @@
-const fs = require("fs");
 const SamlStrategy = require("passport-saml").Strategy;
 const {
   EDUCONNECT_ENTRY_POINT,
@@ -10,26 +9,14 @@ const {
   EDUCONNECT_IDP_SIGN_CERT,
   EDUCONNECT_IDP_ENCR_CERT,
 } = require("../../config");
-const { fetch, toPassportConfig, claimsToCamelCase } = require("passport-saml-metadata");
-const path = require("path");
-
-const MetadataReader = require("../reader");
 
 const STRATEGY_NAME_USER = "educonnect";
 
 function sso(passport) {
   passport.serializeUser(function (user, done) {
-    console.log("-----------------------------");
-    console.log("serialize user");
-    console.log(user);
-    console.log("-----------------------------");
     done(null, user);
   });
   passport.deserializeUser(function (user, done) {
-    console.log("-----------------------------");
-    console.log("deserialize user");
-    console.log(user);
-    console.log("-----------------------------");
     done(null, user);
   });
 
@@ -50,8 +37,6 @@ function sso(passport) {
     },
   );
   passport.use(STRATEGY_NAME_USER, educonnectSamlStrategy);
-
-  // fs.writeFileSync(path.resolve(__dirname, "./SNU_STAGING-metadata.xml"), educonnectSamlStrategy.generateServiceProviderMetadata());
 }
 
 module.exports = sso;
