2cbba5d0211390b1ec6a648fd2b860e3af9bb8d4
naustra <lebihanp@hotmail.fr>
2022-05-17 09:28:00 +0200

    Update routes

--- admin/src/scenes/meetingPoint/index.js
+++ admin/src/scenes/meetingPoint/index.js
@@ -1,173 +1,20 @@
-import React, { useState, useEffect, useRef } from "react";
-import { ReactiveBase, MultiDropdownList, DataSearch } from "@appbaseio/reactivesearch";
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 import useDocumentTitle from "../../hooks/useDocumentTitle";
 
-import api from "../../services/api";
-import { apiURL } from "../../config";
-import { Filter, FilterRow, ResultTable, Table, Header, Title, MultiLine } from "../../components/list";
-import Chevron from "../../components/Chevron";
-import Badge from "../../components/Badge";
-import ReactiveListComponent from "../../components/ReactiveListComponent";
-import DeleteFilters from "../../components/buttons/DeleteFilters";
-
-const FILTERS = ["SEARCH", "CENTER", "DEPARTMENT", "BUS", "COHORT"];
+import List from "./list";
 
 export default function MeetingPoint() {
   useDocumentTitle("Points de rassemblement");
-  const [filterVisible, setFilterVisible] = useState(false);
-  const handleShowFilter = () => setFilterVisible(!filterVisible);
-  const getDefaultQuery = () => {
-    return { query: { match_all: {} }, track_total_hits: true };
-  };
 
   return (
-    <div>
-      <ReactiveBase url={`${apiURL}/es`} app="meetingpoint" headers={{ Authorization: `JWT ${api.getToken()}` }}>
-        <div style={{ display: "flex", alignItems: "flex-start", width: "100%" }}>
-          <div style={{ flex: 2, position: "relative" }}>
-            <Header>
-              <div style={{ flex: 1 }}>
-                <Title>Points de rassemblement</Title>
-              </div>
-            </Header>
-            <Filter>
-              <FilterRow visible>
-                <DataSearch
-                  defaultQuery={getDefaultQuery}
-                  showIcon={false}
-                  placeholder="Rechercher..."
-                  componentId="SEARCH"
-                  dataField={["centerCode", "departureAddress", "busExcelId"]}
-                  react={{ and: FILTERS.filter((e) => e !== "SEARCH") }}
-                  style={{ flex: 1, marginRight: "1rem" }}
-                  innerClass={{ input: "searchbox" }}
-                  URLParams={true}
-                  autosuggest={false}
-                />
-                <MultiDropdownList
-                  defaultQuery={getDefaultQuery}
-                  className="dropdown-filter"
-                  placeholder="Département"
-                  componentId="DEPARTMENT"
-                  dataField="departureDepartment.keyword"
-                  react={{ and: FILTERS.filter((e) => e !== "DEPARTMENT") }}
-                  title=""
-                  URLParams={true}
-                  sortBy="asc"
-                  showSearch={true}
-                  searchPlaceholder="Rechercher..."
-                  size={1000}
-                />
-                <MultiDropdownList
-                  defaultQuery={getDefaultQuery}
-                  className="dropdown-filter"
-                  placeholder="Cohorte"
-                  componentId="COHORT"
-                  dataField="cohort.keyword"
-                  react={{ and: FILTERS.filter((e) => e !== "COHORT") }}
-                  renderItem={(e, count) => {
-                    return `${e} (${count})`;
-                  }}
-                  title=""
-                  URLParams={true}
-                  showSearch={false}
-                />
-                <Chevron color="#444" style={{ cursor: "pointer", transform: filterVisible && "rotate(180deg)" }} onClick={handleShowFilter} />
-              </FilterRow>
-              <FilterRow visible={filterVisible}>
-                <MultiDropdownList
-                  defaultQuery={getDefaultQuery}
-                  className="dropdown-filter"
-                  placeholder="Centre"
-                  componentId="CENTER"
-                  dataField="centerCode.keyword"
-                  react={{ and: FILTERS.filter((e) => e !== "CENTER") }}
-                  title=""
-                  URLParams={true}
-                  sortBy="asc"
-                  showSearch={true}
-                  searchPlaceholder="Rechercher..."
-                  size={1000}
-                />
-                <MultiDropdownList
-                  defaultQuery={getDefaultQuery}
-                  className="dropdown-filter"
-                  placeholder="Transport"
-                  componentId="BUS"
-                  dataField="busExcelId.keyword"
-                  react={{ and: FILTERS.filter((e) => e !== "BUS") }}
-                  title=""
-                  URLParams={true}
-                  sortBy="asc"
-                  showSearch={true}
-                  searchPlaceholder="Rechercher..."
-                  size={1000}
-                />
-                <DeleteFilters />
-              </FilterRow>
-            </Filter>
-            <ResultTable>
-              <ReactiveListComponent
-                defaultQuery={getDefaultQuery}
-                react={{ and: FILTERS }}
-                render={({ data }) => (
-                  <Table>
-                    <thead>
-                      <tr>
-                        <th>Departement départ</th>
-                        <th>Adresse</th>
-                        <th>Centre</th>
-                        <th>Transport</th>
-                        <th>places dispo</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {data.map((hit) => {
-                        return <Hit key={hit._id} hit={hit} />;
-                      })}
-                    </tbody>
-                  </Table>
-                )}
-              />
-            </ResultTable>
-          </div>
-        </div>
-      </ReactiveBase>
-    </div>
+    <Switch>
+      {/* <Route path="/point-de-rassemblement/nouveau" component={Edit} />
+      <Route path="/point-de-rassemblement/:id/edit" component={Edit} />
+      <Route path="/point-de-rassemblement/:id/:sessionId/equipe" component={Team} />
+      <Route path="/point-de-rassemblement/:id/:sessionId/:currentTab" component={Youngs} />
+      <Route path="/point-de-rassemblement/:id" component={View} /> */}
+      <Route path="/point-de-rassemblement" component={List} />
+    </Switch>
   );
 }
-
-const Hit = ({ hit }) => {
-  let mounted = useRef(true);
-  const [bus, setBus] = useState();
-  useEffect(() => {
-    (async () => {
-      const { data, ok } = await api.get(`/bus/${hit.busId}`);
-      if (!ok) return;
-      if (data && mounted) setBus(data);
-    })();
-    return () => (mounted = false);
-  }, [hit]);
-  return (
-    <tr>
-      <td>{hit.departureDepartment}</td>
-      <td>{hit.departureAddress}</td>
-      <td>
-        <a href={`/centre/${hit.centerId}`} target="_blank" rel="noreferrer">
-          {hit.centerCode}
-        </a>
-      </td>
-      <td>{hit.busExcelId}</td>
-      {bus && mounted ? (
-        <td>
-          <MultiLine>
-            {bus.placesLeft === 0 ? <Badge text="Complet" /> : <span className="font-bold text-black">{bus.placesLeft} places disponibles</span>}
-            <p>sur {bus.capacity} places proposées</p>
-          </MultiLine>
-        </td>
-      ) : (
-        <td>chargement...</td>
-      )}
-    </tr>
-  );
-};
