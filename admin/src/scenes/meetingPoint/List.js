f0ec214498cdc8c95bd3658669a921ec637aa722
hlecourt <hlecourt99@gmail.com>
2022-05-18 13:09:50 +0200

    up

--- admin/src/scenes/meetingPoint/List.js
+++ admin/src/scenes/meetingPoint/List.js
@@ -111,6 +111,7 @@ export default function MeetingPoint() {
               </Filter2>
               <ResultTable>
                 <ReactiveListComponent
+                  pageSize={50}
                   defaultQuery={getDefaultQuery}
                   react={{ and: FILTERS }}
                   paginationAt="bottom"
