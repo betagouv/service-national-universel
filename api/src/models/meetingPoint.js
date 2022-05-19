5d12c1ced1649eb7cfaff5bf2d3ff4bbe19d124d
tangimds <tangimds@gmail.com>
2022-05-19 13:43:44 +0200

    fix(api): add patches meetingPoint

--- api/src/models/meetingPoint.js
+++ api/src/models/meetingPoint.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const mongooseElastic = require("@selego/mongoose-elastic");
 const esClient = require("../es");
+const patchHistory = require("mongoose-patch-history").default;
 
 const MODELNAME = "meetingpoint";
 
@@ -73,6 +74,28 @@ const Schema = new mongoose.Schema({
 
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
+});
+
+Schema.virtual("user").set(function (user) {
+  if (user) {
+    const { _id, role, department, region, email, firstName, lastName, model } = user;
+    this._user = { _id, role, department, region, email, firstName, lastName, model };
+  }
+});
+
+Schema.pre("save", function (next, params) {
+  this.user = params?.fromUser;
+  next();
+});
+
+Schema.plugin(patchHistory, {
+  mongoose,
+  name: `${MODELNAME}Patches`,
+  trackOriginalValue: true,
+  includes: {
+    modelName: { type: String, required: true, default: MODELNAME },
+    user: { type: Object, required: false, from: "_user" },
+  },
 });
 
 Schema.plugin(mongooseElastic(esClient), MODELNAME);
