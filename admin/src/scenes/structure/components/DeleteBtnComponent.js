bc6f2d5b3cc71c7e51428af13e161c47d7a697b0
hlecourt <hlecourt99@gmail.com>
2022-05-20 16:57:01 +0200

    fix(admin) : delete button team structure

--- admin/src/scenes/structure/components/DeleteBtnComponent.js
+++ admin/src/scenes/structure/components/DeleteBtnComponent.js
@@ -1,8 +1,8 @@
 import * as React from "react";
 import Trash from "../../../assets/icons/Trash";
 
-const DeleteBtnComponent = () => (
-  <div className="flex justify-center items-center h-8 w-8 bg-gray-100 cursor-pointer text-gray-600 rounded-full hover:scale-105">
+const DeleteBtnComponent = ({ onClick }) => (
+  <div className="flex justify-center items-center h-8 w-8 bg-gray-100 cursor-pointer text-gray-600 rounded-full hover:scale-105" onClick={() => onClick()}>
     <Trash width={16} height={16} />
   </div>
 );
