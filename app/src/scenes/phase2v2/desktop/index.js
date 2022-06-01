d6cbd3fdffacf5124194b829d1f293244c86fca1
tangimds <tangimds@gmail.com>
2022-06-01 14:40:38 +0200

    chore: home phase 2

--- app/src/scenes/phase2v2/desktop/index.js
+++ app/src/scenes/phase2v2/desktop/index.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { BsArrowUpRight } from "react-icons/bs";
 import { HiOutlineAdjustments, HiOutlineSearch } from "react-icons/hi";
-import { IoIosInformationCircleOutline } from "react-icons/io";
 import { MdOutlineContentCopy } from "react-icons/md";
 import { useSelector } from "react-redux";
 import { toastr } from "react-redux-toastr";
 import { Link } from "react-router-dom";
-import api from "../../../services/api";
-import { copyToClipboard } from "../../../utils";
 import ArrowUpRight from "../../../assets/icons/ArrowUpRight";
 import Medaille from "../../../assets/icons/Medaille";
+import api from "../../../services/api";
+import CardMission from "./CardMission";
+import { copyToClipboard } from "../../../utils";
 
 export default function IndexDesktop() {
   const young = useSelector((state) => state.Auth.young);
+  const [applications, setApplications] = React.useState([]);
 
   const [referentManagerPhase2, setReferentManagerPhase2] = React.useState();
   React.useEffect(() => {
@@ -21,9 +21,15 @@ export default function IndexDesktop() {
       if (ok) return setReferentManagerPhase2(data);
     })();
   }, []);
+  React.useEffect(() => {
+    (async () => {
+      const { ok, data } = await api.get(`/young/${young._id.toString()}/application`);
+      if (ok) return setApplications(data);
+    })();
+  }, []);
 
   return (
-    <div className="bg-white mx-4 pb-5 mt-3">
+    <div className="bg-white mx-4 pb-12 my-3 rounded-lg">
       {/* BEGIN HEADER */}
       <div className="bg-gray-700 rounded-t-lg flex">
         <div className="p-14">
@@ -33,15 +39,15 @@ export default function IndexDesktop() {
           </div>
           <div className="flex">
             <Link to="/preferences">
-              <div className="flex gap-1 rounded-[10px] border-[1px] py-2.5 px-3 items-center">
-                <HiOutlineAdjustments className="text-white" />
-                <div className="text-white  text-sm">Renseigner mes préférences </div>
+              <div className="group flex gap-1 rounded-[10px] border-[1px] py-2.5 px-3 items-center">
+                <HiOutlineAdjustments className="text-[#ffffff] group-hover:scale-105" />
+                <div className="text-[#ffffff] group-hover:underline text-sm">Renseigner mes préférences </div>
               </div>
             </Link>
             <Link to="/mission">
-              <div className="flex rounded-[10px] py-2.5 px-4 ml-4 bg-blue-600 items-center border-[1px] border-blue-600">
-                <HiOutlineSearch className="text-white mr-1" />
-                <div className="text-white text-sm">Trouver une mission</div>
+              <div className="group flex rounded-[10px] py-2.5 px-4 ml-4 bg-blue-600 items-center border-[1px] border-blue-600">
+                <HiOutlineSearch className="text-blue-300 mr-1 group-hover:scale-105" />
+                <div className="text-blue-100 text-sm group-hover:underline">Trouver une mission</div>
               </div>
             </Link>
           </div>
@@ -49,15 +55,32 @@ export default function IndexDesktop() {
         <img className="rounded-t-lg" src={require("../../../assets/phase2Header.png")} />
       </div>
       {/* END HEADER */}
-      <div className="mx-10 flex">
-        <div className="border shadow-md rounded-lg w-1/4 -translate-y-4 bg-white p-3">
-          <div className="text-xs bg-indigo-600 text-white px-1 rounded-sm">Mission en cours</div>
-          <div className="text-gray-500 text-xs mt-2">CASC DU SDMIS</div>
-          <div className="font-bold text-sm mt-2">Je participe à l&apos;accueil du public au Musée des sapeurs-pompiers...</div>
-          <div className="text-gray-500 text-xs mt-3">Voir ma candidature</div>
-        </div>
-      </div>
-      <div className="text-gray-700 bg-gray-100 rounded-lg p-2 text-center"> Toutes mes candidatures (5)</div>
+
+      {/* BEGIN CANDIDATURES */}
+      {applications.length > 0 ? (
+        <>
+          <div className="px-14 -translate-y-4">
+            <div className="flex gap-8">
+              {applications
+                .sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt)) // afficher d'abord les candidatures mis a jour récemment
+                .slice(0, 4) // afficher uniquement 4 candidatures
+                .map((application) => (
+                  <CardMission key={application._id} application={application} />
+                ))}
+            </div>
+          </div>
+          {applications.length > 4 ? (
+            <div className="flex justify-center">
+              <Link to="/candidature">
+                <div className="text-gray-700 bg-gray-100 rounded-lg px-4 py-2 text-center hover:underline">Toutes mes candidatures ({applications.length})</div>
+              </Link>
+            </div>
+          ) : null}
+        </>
+      ) : null}
+      {/* END CANDIDATURES */}
+
+      {/* BEGIN LINKS */}
       <div className="mx-10 mt-10">
         <div className="flex gap-2">
           {referentManagerPhase2 ? (
@@ -111,21 +134,25 @@ export default function IndexDesktop() {
             </div>
           </div>
         </Link>
-        <div className="mt-4 mb-2">Vous avez déjà fait preuve de solidarité ?</div>
+        <div className="mt-12 mb-4 text-lg">Vous avez déjà fait preuve de solidarité ?</div>
         <div className="border-0 flex rounded-lg shadow-lg w-1/2 items-center">
           <img src={require("../../../assets/phase2Reconnaissance.png")} className="rounded-lg" />
-          <div className=" pr-2 ml-3">
+          <div className="pr-4 ml-3">
             <div className="flex items-end">
               <div className="font-bold text-lg ">Demandez la reconnaissance d’un engagement déjà réalisé</div>
-              <IoIosInformationCircleOutline className="text-2xl" />
+              {/* TODO que fait le i ? */}
+              {/* <IoIosInformationCircleOutline className="text-2xl" /> */}
             </div>
             <div className="text-gray-600 text-sm mt-2 mb-3">Faîtes reconnaitre comme mission d’intérêt général un engagement déjà réalisé au service de la société</div>
-            <Link to="phase2/equivalence">
-              <div className=" rounded-lg text-blue-700 text-center py-1 mr-4 border-blue-700 border ">Faire ma demande</div>
-            </Link>
+            <div className="flex justify-start">
+              <Link to="phase2/equivalence">
+                <div className="rounded-lg text-blue-700 text-center py-1 px-16 border-blue-700 border-[1px] hover:bg-blue-700 hover:text-white">Faire ma demande</div>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
+      {/* END LINKS */}
     </div>
   );
 }
