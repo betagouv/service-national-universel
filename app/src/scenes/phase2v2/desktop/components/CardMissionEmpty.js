fa962e9fcdca79ee15b50a0ab553d4377acfcf8f
tangimds <tangimds@gmail.com>
2022-06-01 16:08:53 +0200

    chore: fix nina 2

--- app/src/scenes/phase2v2/desktop/components/CardMissionEmpty.js
+++ app/src/scenes/phase2v2/desktop/components/CardMissionEmpty.js
@@ -5,10 +5,10 @@ export default function emptyCard() {
   return (
     <Link
       to="/candidature"
-      className="group flex flex-col basis-1/4 justify-start items-center border shadow-md rounded-lg bg-white p-3 hover:scale-105 transition duration-100 ease-in">
+      className="group flex flex-col basis-1/4 justify-start items-center border shadow-md rounded-lg bg-white p-3 hover:-translate-y-1 transition duration-100 ease-in">
       <div className="flex flex-1 flex-col justify-center items-center">
         <Link to="/candidature">
-          <div className="text-gray-700 bg-gray-100 rounded-lg px-4 py-2 text-center group-hover:underline">Gérer mes candidatures</div>
+          <div className="text-gray-700 bg-gray-100 rounded-lg px-4 py-2 text-center">Gérer mes candidatures</div>
         </Link>
       </div>
     </Link>
