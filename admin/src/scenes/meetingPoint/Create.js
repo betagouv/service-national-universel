2602989ea1fb78a49effa3329b010bb8ec706be9
tangimds <tangimds@gmail.com>
2022-06-03 15:30:22 +0200

    up

--- admin/src/scenes/meetingPoint/Create.js
+++ admin/src/scenes/meetingPoint/Create.js
@@ -6,56 +6,119 @@ import { Link } from "react-router-dom";
 import ArrowCircleRight from "../../assets/icons/ArrowCircleRight";
 import BusSvg from "../../assets/icons/Bus";
 import Plus from "../../assets/icons/Plus";
+import Pencil from "../../assets/icons/Pencil";
 import Breadcrumbs from "../../components/Breadcrumbs";
 import { BottomResultStats, Filter2, ResultTable, Table } from "../../components/list";
 import ReactiveListComponent from "../../components/ReactiveListComponent";
 import { apiURL } from "../../config";
 import api from "../../services/api";
-import { canCreateMeetingPoint, getResultLabel } from "../../utils";
+import { canCreateMeetingPoint, getResultLabel, departmentList, department2region, getDepartmentNumber } from "../../utils";
 import { SelectDate } from "./components/modalEditMeetingPoint";
 
-const FILTERS = ["SEARCH"];
+const FILTERS_BUS = ["SEARCH"];
+const FILTERS_CENTER = ["SEARCH"];
 
-export default function Create(props) {
+export default function Create() {
   const [bus, setBus] = useState();
   const [center, setCenter] = useState();
   const [occupationPercentage, setOccupationPercentage] = React.useState();
   const user = useSelector((state) => state.Auth.user);
-  const [searchedValue, setSearchedValue] = useState("");
+  const [searchedValueCenter, setSearchedValueCenter] = useState("");
+  const [searchedValueBus, setSearchedValueBus] = useState("");
   const [data, setData] = useState({});
-  const [isLoading, setIsLoading] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
   const [year, setYear] = React.useState("2022");
 
   const urlParams = new URLSearchParams(window.location.search);
   const bus_id = urlParams.get("bus_id");
+  const center_id = urlParams.get("center_id");
 
-  const getDefaultQuery = () => ({
+  const getDefaultQueryBus = () => ({
     query: {
       bool: {
-        // filter: [{ term: { "cohort.keyword": young.cohort } }, { term: { "centerCode.keyword": cohesionCenter?.code2022 || "" } }],
+        // filter: [{ term: { "cohorts.keyword": bus.cohort } }],
       },
     },
-    track_total_hits: true,
+  });
+
+  const getDefaultQueryCenter = () => ({
+    query: {
+      bool: {
+        filter: [{ term: { "cohorts.keyword": bus.cohort } }],
+      },
+    },
   });
 
   React.useEffect(() => {
-    console.log("✍️ ~ bus_id", bus_id);
     if (!bus_id) return;
     (async () => {
       const { data, ok } = await api.get(`/bus/${bus_id}`);
       if (!ok) return;
       setBus(data);
+      setData((prev) => ({ ...prev, busId: data._id.toString(), busExcelId: data.idExcel }));
     })();
   }, [bus_id]);
+
+  React.useEffect(() => {
+    if (!center_id) return;
+    (async () => {
+      const { data, ok } = await api.get(`/cohesion-center/${center_id}`);
+      if (!ok) return;
+      setCenter(data);
+      setData((prev) => ({ ...prev, centerId: data._id.toString(), centerCode: data.code2022 }));
+    })();
+  }, [center_id]);
 
   React.useEffect(() => {
     if (!bus) return;
     const occupation = bus.capacity ? (((bus.capacity - bus.placesLeft) * 100) / bus.capacity).toFixed(2) : 0;
     setOccupationPercentage(occupation);
+    if (!center?.cohorts?.includes(bus.cohort)) setCenter(null);
   }, [bus]);
+
+  React.useEffect(() => {
+    if (!data.departureDepartment) return setData((prev) => ({ ...prev, departureRegion: "" }));
+    setData((prev) => ({ ...prev, departureRegion: department2region[data.departureDepartment] }));
+  }, [data.departureDepartment]);
 
   // todo: degage si t'as pas le droit
   if (!canCreateMeetingPoint(user)) return null;
+
+  const handleClickCenter = (hit) => {
+    setCenter(hit);
+    setSearchedValueCenter(null);
+    setData((prev) => ({ ...prev, centerId: hit._id.toString(), centerCode: hit.code2022 }));
+  };
+
+  const handleClickTransport = (hit) => {
+    setBus(hit);
+    setSearchedValueBus(null);
+    setData((prev) => ({ ...prev, busId: hit._id.toString(), busExcelId: hit.idExcel }));
+  };
+
+  const submit = () => {
+    // todo : post meetingPoint
+    setLoading(true);
+    console.log(data);
+    // wait 2 seconds
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  };
+
+  const meetingPointIsComplete = () => {
+    console.log({
+      departureAddress: data.departureAddress,
+      departureDepartment: data.departureDepartment,
+      departureAt: data.departureAt,
+      returnAt: data.returnAt,
+      busId: data.busId,
+      busExcelId: data.busExcelId,
+      centerId: data.centerId,
+      centerCode: data.centerCode,
+    });
+    return data.departureAddress && data.departureDepartment && data.departureAt && data.returnAt && data.busId && data.busExcelId && data.centerId && data.centerCode;
+  };
 
   return (
     <>
@@ -65,6 +128,20 @@ export default function Create(props) {
           <div className="flex flex-row items-center">
             <BusSvg className="h-10 w-10 text-gray-400" />
             <div className="font-bold text-2xl ml-4">Nouveau point de rassemblement</div>
+          </div>
+          <div>
+            {loading ? (
+              <button disabled className="font-bold py-2 px-4 rounded disabled:bg-gray-300 disabled:text-gray-100 disabled:cursor-wait">
+                Chargement...
+              </button>
+            ) : (
+              <button
+                disabled={!meetingPointIsComplete()}
+                className="bg-blue-500 hover:bg-blue-700 text-[#ffffff] font-bold py-2 px-4 rounded disabled:bg-gray-300 disabled:text-gray-100 disabled:cursor-not-allowed"
+                onClick={submit}>
+                Créer le point de rassemblement
+              </button>
+            )}
           </div>
         </div>
         <div className="space-y-8">
@@ -91,19 +168,20 @@ export default function Create(props) {
                           placeholder="Rechercher..."
                           componentId="SEARCH"
                           dataField={["idExcel", "cohort"]}
-                          react={{ and: FILTERS.filter((e) => e !== "SEARCH") }}
+                          react={{ and: FILTERS_BUS.filter((e) => e !== "SEARCH") }}
                           style={{ flex: 2 }}
                           innerClass={{ input: "searchbox" }}
                           autosuggest={false}
                           queryFormat="and"
-                          onValueChange={setSearchedValue}
+                          onValueChange={setSearchedValueBus}
                         />
                       </Filter2>
-                      <ResultTable hide={!searchedValue}>
+                      <ResultTable hide={!searchedValueBus}>
                         <ReactiveListComponent
-                          defaultQuery={getDefaultQuery}
+                          URLParams={false}
+                          defaultQuery={getDefaultQueryBus}
                           scrollOnChange={false}
-                          react={{ and: FILTERS }}
+                          react={{ and: FILTERS_BUS }}
                           paginationAt="bottom"
                           size={10}
                           renderResultStats={(e) => {
@@ -124,14 +202,7 @@ export default function Create(props) {
                               </thead>
                               <tbody>
                                 {data.map((hit, i) => (
-                                  <HitTransport
-                                    key={i}
-                                    hit={hit}
-                                    onSend={() => {
-                                      setBus(hit);
-                                      setSearchedValue(null);
-                                    }}
-                                  />
+                                  <HitTransport key={i} hit={hit} onSend={() => handleClickTransport(hit)} />
                                 ))}
                               </tbody>
                             </Table>
@@ -165,19 +236,10 @@ export default function Create(props) {
                       }}
                     />
                   </div>
-                  {/* todo: select departement and region */}
-                  <div className="flex flex-col justify-center border-[1px] border-gray-300 w-2/3 px-3 py-2 rounded-lg mt-3">
-                    {data?.departureDepartment ? <div className="text-xs leading-4 font-normal text-gray-500">Département</div> : null}
-                    <input
-                      className="w-full text-sm leading-5 font-normal ::placeholder:text-gray-500"
-                      placeholder="Département"
-                      type="text"
-                      onChange={(e) => {
-                        e.persist();
-                        setData((prev) => ({ ...prev, departureDepartment: e.target.value }));
-                      }}
-                    />
-                  </div>
+                  <Select value={data?.departureDepartment} options={departmentList.sort()} name="departureDepartment" onChange={setData} placeholder="Département" />
+                  {/* {data?.departureRegion ? (
+                    <Select value={data?.departureRegion} options={regionList.sort()} name="departureRegion" onChange={setData} placeholder="Région" disabled />
+                  ) : null} */}
                   <div className="flex flex-1 py-1 px-2 justify-start items-center gap-2">
                     <input
                       type="checkbox"
@@ -199,7 +261,7 @@ export default function Create(props) {
                       Merci de rendre l&apos;adresse postale la plus explicite possible.
                     </div>
                   ) : null}
-                  <div className={`flex flex-1 flex-col border-[1px] rounded-lg py-1 px-2 ${isLoading && "bg-gray-200"}`}>
+                  <div className={`flex flex-1 flex-col border-[1px] rounded-lg py-1 px-2 ${loading && "bg-gray-200"}`}>
                     <label htmlFor="departureAt" className="w-full m-0 text-left text-gray-500">
                       Date et heure de rendez-vous aller
                     </label>
@@ -209,7 +271,7 @@ export default function Create(props) {
                       handleChange: (e) => setData({ ...data, departureAt: { ...data.departureAt, [e.target.name]: e.target.value } }),
                     })}
                   </div>
-                  <div className={`flex flex-1 flex-col border-[1px] rounded-lg py-1 px-2 ${isLoading && "bg-gray-200"}`}>
+                  <div className={`flex flex-1 flex-col border-[1px] rounded-lg py-1 px-2 ${loading && "bg-gray-200"}`}>
                     <label htmlFor="returnAt" className="w-full m-0 text-left text-gray-500">
                       Date et heure de rendez-vous retour
                     </label>
@@ -249,13 +311,80 @@ export default function Create(props) {
                   ) : null}
                 </div>
                 <div>
-                  {/* <Donnee title={"Nom du centre"} value={center.name} number={""} />
-              <Donnee title={"Code 2022"} value={center.code2022 || <span className="italic text-gray-500">Non renseigné</span>} number={""} />
-              <Donnee title={"Région"} value={center.region} number={""} />
-              <Donnee title={"Département"} value={center.department} number={`(${getDepartmentNumber(center.department)})`} />
-              <Donnee title={"Ville"} value={center.city} number={`(${center.zip})`} />
-              <Donnee title={"Adresse"} value={center.address} number={""} /> */}
-                  <div>info du centre</div>
+                  {center ? (
+                    <div>
+                      <Donnee title={"Nom du centre"} value={center.name} number={""} />
+                      <Donnee title={"Code 2022"} value={center.code2022 || <span className="italic text-gray-500">Non renseigné</span>} number={""} />
+                      <Donnee title={"Région"} value={center.region} number={""} />
+                      <Donnee title={"Département"} value={center.department} number={`(${getDepartmentNumber(center.department)})`} />
+                      <Donnee title={"Ville"} value={center.city} number={`(${center.zip})`} />
+                      <Donnee title={"Adresse"} value={center.address} number={""} />
+                      <div className="flex flex-col items-center mt-8">
+                        <div
+                          className="group flex gap-1 rounded-[10px] border-[1px] border-blue-600 py-2.5 px-3 items-center hover:bg-blue-600 hover:text-gray-800 cursor-pointer"
+                          onClick={() => setCenter(null)}>
+                          <Pencil className="text-blue-600 group-hover:text-white text-sm" />
+                          <div className="text-blue-600 group-hover:text-white text-sm flex-1 leading-4">Changer de centre</div>
+                        </div>
+                      </div>
+                    </div>
+                  ) : (
+                    <div className="flex mt-4">
+                      <>
+                        <ReactiveBase url={`${apiURL}/es`} app="edit-cohesioncenter" headers={{ Authorization: `JWT ${api.getToken()}` }}>
+                          <div style={{ flex: 2, position: "relative" }}>
+                            <Filter2>
+                              <DataSearch
+                                showIcon={false}
+                                placeholder="Rechercher par nom de centre, code postal, departement..."
+                                componentId="SEARCH"
+                                dataField={["name", "city", "zip", "code", "department"]}
+                                react={{ and: FILTERS_CENTER.filter((e) => e !== "SEARCH") }}
+                                style={{ flex: 2 }}
+                                innerClass={{ input: "searchbox" }}
+                                autosuggest={false}
+                                queryFormat="and"
+                                onValueChange={setSearchedValueCenter}
+                              />
+                            </Filter2>
+                            <ResultTable hide={!searchedValueCenter}>
+                              <ReactiveListComponent
+                                URLParams={false}
+                                defaultQuery={getDefaultQueryCenter}
+                                scrollOnChange={false}
+                                react={{ and: FILTERS_CENTER }}
+                                paginationAt="bottom"
+                                size={10}
+                                renderResultStats={(e) => {
+                                  return (
+                                    <div>
+                                      <BottomResultStats>{getResultLabel(e, 3)}</BottomResultStats>
+                                    </div>
+                                  );
+                                }}
+                                render={({ data }) => (
+                                  <Table>
+                                    <thead>
+                                      <tr>
+                                        <th className="w-[38%]">Centre</th>
+                                        <th className="w-[38%]">Séjour</th>
+                                        <th className="w-1/4">Action</th>
+                                      </tr>
+                                    </thead>
+                                    <tbody>
+                                      {data.map((hit, i) => (
+                                        <HitCentre key={i} hit={hit} cohort={bus.cohort} onClick={() => handleClickCenter(hit)} />
+                                      ))}
+                                    </tbody>
+                                  </Table>
+                                )}
+                              />
+                            </ResultTable>
+                          </div>
+                        </ReactiveBase>
+                      </>
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
@@ -269,6 +398,17 @@ export default function Create(props) {
     </>
   );
 }
+
+const Donnee = ({ title, value, number, showLabelHide }) => {
+  return (
+    <div className="flex pt-4">
+      <div className="w-1/2 text-brand-detail_title "> {title} : </div>
+      <div className="w-1/2 font-medium">
+        {value} {number} {showLabelHide ? <span className="italic text-gray-500 text-xs">masqué</span> : ""}
+      </div>
+    </div>
+  );
+};
 
 const OccupationCard = ({ occupationPercentage, placesLeft, placesTotal }) => {
   let height = `h-0`;
@@ -350,5 +490,68 @@ const HitTransport = ({ hit, onSend }) => {
         </td>
       )}
     </tr>
+  );
+};
+
+const HitCentre = ({ hit, onClick, cohort }) => {
+  return (
+    <tr className={`${hit.placesLeft <= 0 ? "bg-gray-50" : ""}`}>
+      <td>
+        <div className="flex flex-col">
+          <div>{hit.name}</div>
+          <div className="text-sm text-gray-500">{hit.code2022}</div>
+        </div>
+      </td>
+      <td>
+        <div className="flex flex-col">
+          <div>{cohort}</div>
+        </div>
+      </td>
+
+      <td onClick={(e) => e.stopPropagation()}>
+        <div
+          className="flex rounded-[10px] border-[1px] border-blue-600 py-1 px-2 items-center justify-center hover:bg-blue-600 text-blue-600 hover:text-white cursor-pointer"
+          onClick={onClick}>
+          Choisir
+        </div>
+      </td>
+    </tr>
+  );
+};
+
+const Select = ({ value, name, options, onChange, placeholder: defaultPlaceholder, disabled }) => {
+  const [showLabel, setShowLabel] = useState(false);
+  const [placeholder, setPlaceHolder] = useState(defaultPlaceholder);
+
+  return (
+    <div className="flex flex-col justify-center border-[1px] border-gray-300 w-2/3 px-3 py-2 rounded-lg mt-3">
+      {showLabel || value ? <div className="text-xs leading-4 font-normal text-gray-500">{defaultPlaceholder}</div> : null}
+      <select
+        value={value}
+        disabled={disabled}
+        onFocus={() => {
+          setPlaceHolder("");
+          setShowLabel(true);
+        }}
+        onBlur={() => {
+          setPlaceHolder(defaultPlaceholder);
+          setShowLabel(false);
+        }}
+        className="w-full text-sm leading-5 font-normal ::placeholder:text-gray-500 cursor-pointer disabled:cursor-not-allowed"
+        placeholder={placeholder}
+        onChange={(e) => {
+          e.persist();
+          onChange((prev) => ({ ...prev, [name]: e.target.value }));
+        }}>
+        <option disabled selected value={null} label={defaultPlaceholder}>
+          {defaultPlaceholder}
+        </option>
+        {options.map((item) => (
+          <option key={item} value={item} label={item}>
+            {item}
+          </option>
+        ))}
+      </select>
+    </div>
   );
 };
