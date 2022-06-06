1c5d8c4422d33ec60ab82b65768922c46a28d777
tangimds <tangimds@gmail.com>
2022-06-06 16:00:13 +0200

    up

--- app/src/scenes/home/waitingList.js
+++ app/src/scenes/home/waitingList.js
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { HeroContainer, Hero, Content, Alert } from "../../components/Content";
 import styled from "styled-components";
+import { COHESION_STAY_LIMIT_DATE } from "../../utils";
+import { youngCanChangeSession } from "snu-lib";
+import { Link } from "react-router-dom";
+import Pencil from "../../assets/icons/Pencil";
 
 export default function WaitingList() {
   const young = useSelector((state) => state.Auth.young);
@@ -28,7 +32,16 @@ export default function WaitingList() {
               <path d="M24 20v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" stroke="#FBBF24" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
             </svg>
             <p>
-              <strong>Vous êtes inscrit sur liste complémentaire</strong>
+              <strong>Vous êtes inscrit sur liste complémentaire pour le séjour {COHESION_STAY_LIMIT_DATE[young.cohort]}</strong>
+              {youngCanChangeSession({ cohort: young.cohort, status: young.statusPhase1 }) ? (
+                <button className="m-2">
+                  <Link to="/changer-de-sejour">
+                    <div className="flex justify-center items-center h-8 w-8 bg-gray-100 group-hover:bg-white text-gray-600 rounded-full hover:scale-105">
+                      <Pencil className="flex-1 m-0" width={16} height={16} />
+                    </div>
+                  </Link>
+                </button>
+              ) : null}
               <br />
               <p>Votre dossier a été traité.</p>
               <br />
