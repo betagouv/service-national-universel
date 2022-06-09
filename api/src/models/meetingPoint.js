a79029a8a4c2619aab9cd84118fe8cb9fc326935
hlecourt <hlecourt99@gmail.com>
2022-06-09 10:22:16 +0200

    up

--- api/src/models/meetingPoint.js
+++ api/src/models/meetingPoint.js
@@ -77,7 +77,7 @@ const Schema = new mongoose.Schema({
 
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
-  deletedAt: { type: Date, default: null },
+  deletedAt: { type: Date },
 });
 
 Schema.virtual("user").set(function (user) {
