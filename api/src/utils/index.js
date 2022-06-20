9ab55475179c7e86374a13e83921a905d8353f38
hlecourt <hlecourt99@gmail.com>
2022-06-20 18:06:53 +0200

    up

--- api/src/utils/index.js
+++ api/src/utils/index.js
@@ -595,10 +595,10 @@ async function autoValidationSessionPhase1Young({ young, sessionPhase1, req }) {
   if (now > dateDeValidation[sessionPhase1.cohort]) {
     if (young.cohesionStayPresence === "true" && (young.presenceJDM === "true" || young.grade === "Terminale")) {
       if (
-        (now > dateDeValidation[sessionPhase1.cohort] &&
+        (now >= dateDeValidation[sessionPhase1.cohort] &&
           young?.grade !== "Terminale" &&
           (!young?.departSejourAt || young?.departSejourAt > dateDeValidation[sessionPhase1.cohort])) ||
-        (now > dateDeValidationTerminale[sessionPhase1.cohort] &&
+        (now >= dateDeValidationTerminale[sessionPhase1.cohort] &&
           young?.grade === "Terminale" &&
           (!young?.departSejourAt || young?.departSejourAt > dateDeValidationTerminale[sessionPhase1.cohort]))
       ) {
@@ -618,7 +618,6 @@ async function autoValidationSessionPhase1Young({ young, sessionPhase1, req }) {
         }
       }
     } else {
-      //pas sur peut etre ne r faire
       young.set({ statusPhase1: "NOT_DONE" });
     }
   }
