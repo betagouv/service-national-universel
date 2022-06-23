28a6318bfb1fca2bde8a10cf52432097bddbff2b
tangimds <tangimds@gmail.com>
2022-06-23 10:21:26 +0200

    fix(app): raccourci vers mission préparation militaire

--- app/src/scenes/missionsv2/list/desktop/index.js
+++ app/src/scenes/missionsv2/list/desktop/index.js
@@ -1,35 +1,34 @@
 import React from "react";
-import { ReactiveBase, ReactiveList, DataSearch, MultiDropdownList } from "@appbaseio/reactivesearch";
-import styled from "styled-components";
+import { ReactiveBase, ReactiveList } from "@appbaseio/reactivesearch";
 import { useSelector } from "react-redux";
+import styled from "styled-components";
+import { useParams } from "react-router";
 
-import CardMission from "./components/CardMission";
-import { apiURL } from "../../../../config";
-import { translate, getLimitDateForPhase2, getFilterLabel, ENABLE_PM, ES_NO_LIMIT, MISSION_PERIOD_DURING_HOLIDAYS, MISSION_PERIOD_DURING_SCHOOL } from "../../../../utils";
-import api from "../../../../services/api";
-import Loader from "../../../../components/Loader";
-import FilterGeoloc from "../../components/FilterGeoloc";
-import Sante from "../../../../assets/mission-domaines/sante";
-import Solidarite from "../../../../assets/mission-domaines/solidarite";
-import Citoyennete from "../../../../assets/mission-domaines/citoyennete";
-import Education from "../../../../assets/mission-domaines/education";
-import Sport from "../../../../assets/mission-domaines/sport";
-import DefenseEtMemoire from "../../../../assets/mission-domaines/defense-et-memoire";
-import Environment from "../../../../assets/mission-domaines/environment";
-import Securite from "../../../../assets/mission-domaines/securite";
-import Culture from "../../../../assets/mission-domaines/culture";
-import PreparationMilitaire from "../../../../assets/mission-domaines/preparation-militaire";
+import { HiOutlineAdjustments } from "react-icons/hi";
+import { Link } from "react-router-dom";
 import AcademicCap from "../../../../assets/icons/AcademicCap";
-import Sun from "../../../../assets/icons/Sun";
 import Calendar from "../../../../assets/icons/Calendar";
 import Search from "../../../../assets/icons/Search";
-import { Link } from "react-router-dom";
-import { HiOutlineAdjustments } from "react-icons/hi";
+import Sun from "../../../../assets/icons/Sun";
+import Citoyennete from "../../../../assets/mission-domaines/citoyennete";
+import Culture from "../../../../assets/mission-domaines/culture";
+import DefenseEtMemoire from "../../../../assets/mission-domaines/defense-et-memoire";
+import Education from "../../../../assets/mission-domaines/education";
+import Environment from "../../../../assets/mission-domaines/environment";
+import PreparationMilitaire from "../../../../assets/mission-domaines/preparation-militaire";
+import Sante from "../../../../assets/mission-domaines/sante";
+import Securite from "../../../../assets/mission-domaines/securite";
+import Solidarite from "../../../../assets/mission-domaines/solidarite";
+import Sport from "../../../../assets/mission-domaines/sport";
+import { apiURL } from "../../../../config";
+import api from "../../../../services/api";
+import { MISSION_PERIOD_DURING_HOLIDAYS, MISSION_PERIOD_DURING_SCHOOL, translate } from "../../../../utils";
+import FuseeSvg from "../../assets/Fusee";
 import PietonSvg from "../../assets/Pieton";
+import TrainSvg from "../../assets/Train";
 import VeloSvg from "../../assets/Velo";
 import VoitureSvg from "../../assets/Voiture";
-import TrainSvg from "../../assets/Train";
-import FuseeSvg from "../../assets/Fusee";
+import CardMission from "./components/CardMission";
 
 const FILTERS = ["DOMAINS", "SEARCH", "STATUS", "GEOLOC", "DATE", "PERIOD", "RELATIVE", "MILITARY_PREPARATION"];
 
@@ -243,6 +242,12 @@ export default function List() {
       setFilter((prev) => ({ ...prev, LOCATION: location }));
     })();
   }, [focusedAddress]);
+
+  React.useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const MILITARY_PREPARATION = urlParams.get("MILITARY_PREPARATION");
+    if (MILITARY_PREPARATION === "true") setFilter((prev) => ({ ...prev, MILITARY_PREPARATION: "true" }));
+  }, []);
 
   React.useEffect(() => {
     if (!refDropdownControlDistance) return;
