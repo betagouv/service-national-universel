add14037d7f87e62ec93431bd19ff35899274aa7
pierre le bihan <pierre@le-bihan.eu>
2022-06-30 22:19:29 +0200

    fix index

--- api/src/index.js
+++ api/src/index.js
@@ -75,6 +75,7 @@ app.use("/email", require("./controllers/email"));
 app.use("/meeting-point", require("./controllers/meeting-point"));
 app.use("/diagoriente", require("./controllers/diagoriente"));
 app.use("/bus", require("./controllers/bus"));
+app.use("/zammad-support-center", require("./controllers/zammad-support-center"));
 app.use("/zammood", require("./controllers/zammood"));
 app.use("/signin", require("./controllers/signin"));
 app.use("/educonnect", require("./controllers/educonnect"));
@@ -85,13 +86,7 @@ app.use("/jeveuxaider", require("./services/jeveuxaider"));
 app.use(handleError);
 
 app.get("/", async (req, res) => {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
   // ! Memory usage
->>>>>>> 7c62caee (revert shit)
->>>>>>> 4f80b4b7 (revert shit)
   // const formatMemoryUsage = (data) => `${Math.round((data / 1024 / 1024) * 100) / 100} MB`;
 
   // const memoryData = process.memoryUsage();
