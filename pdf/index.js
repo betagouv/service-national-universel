7e27d7482e4b07b95e27a3d54c89950541e56f4f
Raphaël Huchet <raph@selego.co>
2022-06-30 19:44:48 +0200

    chore: up

---
+++ pdf/index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const puppeteer = require("puppeteer");
+const bodyParser = require("body-parser");
+const app = express()
+const port = process.env.PORT || 8087;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const DEFAULT_OPTIONS = {
+  format: "A4",
+  margin: 0,
+};
+
+const renderFromHtml = async (html, options) => {
+  const { browser, page } = await getBrowserAndPage(options);
+  await page.setContent(html, options?.navigation ?? {});
+  const pdfOptions = options ?? {};
+  const buffer = await page.pdf({
+    ...DEFAULT_OPTIONS,
+    ...pdfOptions,
+  });
+
+  await browser.close();
+
+  return buffer;
+};
+
+const getBrowserAndPage = async (options) => {
+  const browser = await puppeteer.launch(options?.launch ?? {});
+  const page = await browser.newPage();
+
+  if (options?.emulateMedia) {
+    await page.emulateMediaType(options.emulateMedia);
+  }
+
+  return { browser, page };
+};
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.post('/render', (req, res) => {
+  const buffer = renderFromHtml(req.body.html, req.body.options || {});
+  res.contentType("application/pdf");
+  res.setHeader("Content-Dispositon", 'inline; filename="test.pdf"');
+  res.set("Cache-Control", "public, max-age=1");
+  res.send(buffer);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
