04b49aff975b6371a6f3250fd0a73ff85b9b7a13
tangimds <tangimds@gmail.com>
2022-06-30 20:52:42 +0200

    fix: envoie par mail

--- api/src/controllers/young/documents.js
+++ api/src/controllers/young/documents.js
@@ -22,7 +22,7 @@ async function getHtmlTemplate(type, template, young, contract) {
   if (type === "certificate" && template === "snu") return certificate.snu(young);
   if (type === "form" && template === "imageRight") return form.imageRight(young);
   if (type === "form" && template === "autotestPCR") return form.autotestPCR(young);
-  // if (type === "convocation" && template === "cohesion") return convocation.cohesion(young);
+  if (type === "convocation" && template === "cohesion") return convocation.cohesion(young);
   if (type === "contract" && template === "2" && contract) return contractPhase2.render(contract);
 }
 
