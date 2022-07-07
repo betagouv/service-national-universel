8df22af33f4e74085ee84a6a7e3049a990c80725
pierre le bihan <pierre@le-bihan.eu>
2022-07-07 09:49:22 +0200

    fix(all) : env.maintenance

--- admin/src/config.js
+++ admin/src/config.js
@@ -12,14 +12,14 @@ if (environment === "staging") {
   appURL = "https://app-66aba4d6-e5fc-4c74-b252-f55fb0e9d37f.cleverapps.io";
   adminURL = "https://app-735c50af-69c1-4a10-ac30-7ba11d1112f7.cleverapps.io";
   supportURL = "https://app-9266b532-ff6e-4a6a-aeeb-e6ff7bb67f60.cleverapps.io";
-  maintenance = process.env["MAINTENANCE"] || false;
+  maintenance = process.env.MAINTENANCE || false;
 }
 if (environment === "production") {
   apiURL = "https://api.snu.gouv.fr";
   appURL = "https://moncompte.snu.gouv.fr";
   adminURL = "https://admin.snu.gouv.fr";
   supportURL = "https://support.snu.gouv.fr";
-  maintenance = process.env["MAINTENANCE"] || false;
+  maintenance = process.env.MAINTENANCE || false;
 }
 const S3PREFIX = "";
 const SENTRY_URL = "https://415a2c2d9246422fa05cd5e96dd39c23@o348403.ingest.sentry.io/5557988";
