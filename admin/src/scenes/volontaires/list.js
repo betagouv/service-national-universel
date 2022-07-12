73ca1afc10129ef03f8cb5d313d6bbdd20f20d3b
pierre le bihan <pierre@le-bihan.eu>
2022-07-12 14:40:14 +0200

    fix(admin) : export Volontaire - Status général

--- admin/src/scenes/volontaires/list.js
+++ admin/src/scenes/volontaires/list.js
@@ -273,7 +273,7 @@ export default function VolontaireList() {
                         "Créé lé": formatLongDateFR(data.createdAt),
                         "Mis à jour le": formatLongDateFR(data.updatedAt),
                         "Dernière connexion le": formatLongDateFR(data.lastLoginAt),
-                        Statut: translate(data.status),
+                        "Statut général": translate(data.status),
                         "Statut Phase 1": translatePhase1(data.statusPhase1),
                         "Statut Phase 2": translatePhase2(data.statusPhase2),
                         "Statut Phase 3": translate(data.statusPhase3),
