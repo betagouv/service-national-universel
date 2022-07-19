08e2dc9da5ba997b5f9699cf97b96291b64b097c
Nicolas Bretécher <bretechern@gmail.com>
2022-07-19 18:39:05 +0200

    revert size change b/c bugs

--- app/src/scenes/phase2/mobile/Validated.js
+++ app/src/scenes/phase2/mobile/Validated.js
@@ -105,7 +105,7 @@ export default function ValidatedMobile() {
           <div className="relative" ref={refAttestationButton}>
             <button
               disabled={loading.attestationPhase2}
-              className="flex justify-between gap-3 items-center rounded-full border-[1px] border-blue-600 bg-blue-600 px-3 py-2 disabled:opacity-50 disabled:cursor-wait w-3/4"
+              className="flex justify-between gap-3 items-center rounded-full border-[1px] border-blue-600 bg-blue-600 px-3 py-2 disabled:opacity-50 disabled:cursor-wait w-full"
               onClick={() => setOpenAttestationButton((e) => !e)}>
               <div className="flex items-center gap-2">
                 <span className="text-white leading-4 text-xs font-medium whitespace-nowrap">Attestation de réalisation phase 2</span>
@@ -147,7 +147,7 @@ export default function ValidatedMobile() {
           <div className="relative" ref={refSNUButton}>
             <button
               disabled={loading.attestationSNU}
-              className="flex justify-between gap-3 items-center rounded-full border-[1px] border-blue-600 bg-blue-600 px-3 py-2 disabled:opacity-50 disabled:cursor-wait w-3/4"
+              className="flex justify-between gap-3 items-center rounded-full border-[1px] border-blue-600 bg-blue-600 px-3 py-2 disabled:opacity-50 disabled:cursor-wait w-full"
               onClick={() => setOpenSNUButton((e) => !e)}>
               <div className="flex items-center gap-2">
                 <span className="text-white leading-4 text-xs font-medium">Attestation de réalisation SNU</span>
