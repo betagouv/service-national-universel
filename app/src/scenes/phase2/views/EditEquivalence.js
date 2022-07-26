49bcbb9a92c37ce524d5228d7d5d66ffdf725740
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2022-07-26 11:17:21 +0200

    Fix (App, Admin): slugify filenames for file upload (#1687)

--- app/src/scenes/phase2/views/EditEquivalence.js
+++ app/src/scenes/phase2/views/EditEquivalence.js
@@ -10,6 +10,7 @@ import InformationCircle from "../../../assets/icons/InformationCircle";
 import PaperClip from "../../../assets/icons/PaperClip";
 import api from "../../../services/api";
 import validator from "validator";
+import { slugifyFileName } from "../../../utils";
 
 export default function EditEquivalence() {
   const young = useSelector((state) => state.Auth.young);
@@ -46,7 +47,7 @@ export default function EditEquivalence() {
       if (!isFileSupported(files[i].name)) return toastr.error(`Le type du fichier ${files[i].name} n'est pas supporté.`);
       if (files[i].size > 5000000) return toastr.error(`Ce fichier ${files[i].name} est trop volumineux.`);
       const fileName = files[i].name.match(/(.*)(\..*)/);
-      const newName = `${fileName[1]}-${filesList.length + index}${fileName[2]}`;
+      const newName = `${slugifyFileName(fileName[1])}-${filesList.length + index}${fileName[2]}`;
       Object.defineProperty(files[i], "name", {
         writable: true,
         value: newName,
