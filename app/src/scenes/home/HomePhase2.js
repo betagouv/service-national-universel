8bdd79906b5dc2ad415d0f2b998bb0dd724220c9
hlecourt <hlecourt99@gmail.com>
2022-07-26 15:15:58 +0200

    up

--- app/src/scenes/home/HomePhase2.js
+++ app/src/scenes/home/HomePhase2.js
@@ -11,7 +11,7 @@ export default function HomePhase2() {
       {/* DESKTOP */}
       <div className="hidden lg:flex">
         <div className="my-12 mx-10 w-full">
-          <div className="flex justify-between rounded-lg bg-white ">
+          <div className="flex justify-between items-center rounded-lg bg-white ">
             <div className="w-1/2 pl-10 py-12">
               <div className="text-[48px] font-medium leading-tight tracking-tight text-gray-800">
                 <strong>{young.firstName},</strong>
