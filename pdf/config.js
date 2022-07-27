6ffa46a48becb6b176368dde6ff63de3d1456885
naustra <lebihanp@hotmail.fr>
2022-07-27 15:53:03 +0200

    feat(pdf): Add a way to generate pdf locally

--- pdf/config.js
+++ pdf/config.js
@@ -3,6 +3,8 @@ const PORT = process.env.PORT || 8087;
 const SENTRY_URL = process.env.SENTRY_URL || "";
 const SENTRY_TRACING_SAMPLE_RATE = process.env.SENTRY_TRACING_SAMPLE_RATE || "";
 
+const GENERATE_LOCALLY = process.env.GENERATE_LOCALLY || "";
+
 module.exports = {
   PORT,
   SENTRY_URL,
