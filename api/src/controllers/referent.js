ae23e0b878b87c9d765f7f0b84c2a627835ba629
naustra <lebihanp@hotmail.fr>
2022-07-28 17:02:18 +0200

    rien a voir

--- api/src/controllers/referent.js
+++ api/src/controllers/referent.js
@@ -564,7 +564,7 @@ router.get("/youngFile/:youngId/:key/:fileName", passport.authenticate("referent
       case ROLES.SUPERVISOR:
       case ROLES.RESPONSIBLE: {
         if (!req.user.structureId) return res.status(404).send({ ok: false, code: ERRORS.NOT_FOUND });
-        const structures = await StructureModel.find({ $or: [{ networkId: String(req.user.structureId) }, { _id: String(req.user.structureId) }] }).cursor();
+        const structures = await StructureModel.find({ $or: [{ networkId: String(req.user.structureId) }, { _id: String(req.user.structureId) }] });
         if (!structures) return res.status(404).send({ ok: false, code: ERRORS.NOT_FOUND });
         if (!structures.reduce((acc, curr) => acc || canViewYoungFile(req.user, young, curr), false))
           return res.status(403).send({ ok: false, code: ERRORS.OPERATION_UNAUTHORIZED });
