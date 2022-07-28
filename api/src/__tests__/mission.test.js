2424af5275ffc77a4510e8f79a84f3cbf1cc4db4
hlecourt <hlecourt99@gmail.com>
2022-07-28 17:24:02 +0200

    up

--- api/src/__tests__/mission.test.js
+++ api/src/__tests__/mission.test.js
@@ -5,16 +5,9 @@ const getAppHelper = require("./helpers/app");
 const getNewMissionFixture = require("./fixtures/mission");
 const getNewStructureFixture = require("./fixtures/structure");
 const { dbConnect, dbClose } = require("./helpers/db");
-const {
-  getMissionsHelper,
-  getMissionByIdHelper,
-  deleteMissionByIdHelper,
-  createMissionHelper,
-  expectMissionToEqual,
-  notExisitingMissionId,
-} = require("./helpers/mission");
+const { getMissionsHelper, getMissionByIdHelper, deleteMissionByIdHelper, createMissionHelper, expectMissionToEqual, notExisitingMissionId } = require("./helpers/mission");
 const { createReferentHelper, getReferentByIdHelper } = require("./helpers/referent");
-const { deleteStructureByIdHelper, createStructureHelper, expectStructureToEqual, notExistingStructureId } = require("./helpers/structure");
+const { deleteStructureByIdHelper, createStructureHelper, , notExistingStructureId } = require("./helpers/structure");
 const getNewReferentFixture = require("./fixtures/referent");
 const { ROLES } = require("snu-lib/roles");
 const { createYoungHelper } = require("./helpers/young");
@@ -57,13 +50,13 @@ describe("Mission", () => {
       expect(res.statusCode).toEqual(404);
     });
     it("should return 403 if user can not update mission", async () => {
-      const mission = await createMissionHelper({ ...getNewMissionFixture(), department: "hop", region: "hop" });
+      const mission = await createMissionHelper({ ...getNewMissionFixture() });
       const passport = require("passport");
-      passport.user.role = ROLES.REFERENT_DEPARTMENT;
+      passport.user.role = ROLES.VISITOR;
       let res = await request(getAppHelper()).put("/mission/" + mission._id);
       expect(res.statusCode).toEqual(403);
 
-      passport.user.role = ROLES.REFERENT_REGION;
+      passport.user.role = ROLES.HEAD_CENTER;
       res = await request(getAppHelper()).put("/mission/" + mission._id);
       expect(res.statusCode).toEqual(403);
 
@@ -216,7 +209,7 @@ describe("Mission", () => {
           expect.objectContaining({
             ops: expect.arrayContaining([expect.objectContaining({ op: "replace", path: "/name", value: "MY NEW NAME" })]),
           }),
-        ])
+        ]),
       );
     });
   });
