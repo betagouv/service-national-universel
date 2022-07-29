598f59eab2c08f55b3f2aeff2a94b4955edafd19
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2022-07-29 16:22:51 +0200

    revert usewindowsize

--- app/src/scenes/missions/list/index.js
+++ app/src/scenes/missions/list/index.js
@@ -1,26 +1,16 @@
 import React from "react";
-import { useWindowSize } from "usehooks-ts";
-import tailwindConfig from "../../../../tailwind.config";
 import DesktopView from "./desktop";
 import MobileView from "./mobile";
 
 export default function View() {
-  console.log("md:", parseInt(tailwindConfig.theme.screens.md));
-  const { width } = useWindowSize();
-  const isDesktop = width > parseInt(tailwindConfig.theme.screens.md);
-  console.log("isDesktop:", isDesktop);
-
   return (
     <>
-      {isDesktop ? (
-        <div className="md:flex flex-1">
-          <DesktopView />
-        </div>
-      ) : (
-        <div className="flex w-screen">
-          <MobileView />
-        </div>
-      )}
+      <div className="hidden md:flex flex-1">
+        <DesktopView />
+      </div>
+      <div className="flex md:hidden w-screen">
+        <MobileView />
+      </div>
     </>
   );
 }
