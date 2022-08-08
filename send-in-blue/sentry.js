21c83139f29066bd670f541084a99c6725f09aa0
naustra <lebihanp@hotmail.fr>
2022-08-08 10:10:19 +0200

    feat(send-in-blue): New app for webhook send in blue (#1723)

---
+++ send-in-blue/sentry.js
@@ -0,0 +1,58 @@
+const { ExtraErrorData, RewriteFrames } = require("@sentry/integrations");
+const {
+  captureException: sentryCaptureException,
+  captureMessage: sentryCaptureMessage,
+  Integrations: NodeIntegrations,
+  init,
+  Handlers,
+} = require("@sentry/node");
+const { Integrations: TracingIntegrations } = require("@sentry/tracing");
+const { SENTRY_URL, SENTRY_TRACING_SAMPLE_RATE } = require("./config");
+
+function initSentry(app) {
+  init({
+    enabled: Boolean(SENTRY_URL),
+    dsn: SENTRY_URL,
+    environment: "email",
+    normalizeDepth: 16,
+    integrations: [
+      new ExtraErrorData({ depth: 16 }),
+      new RewriteFrames({ root: process.cwd() }),
+      new NodeIntegrations.Http({ tracing: true }),
+      new NodeIntegrations.Modules(),
+      new TracingIntegrations.Mongo({ useMongoose: true }),
+      new TracingIntegrations.Express({ app }),
+    ],
+    tracesSampleRate: Number(SENTRY_TRACING_SAMPLE_RATE),
+  });
+
+  // The request handler must be the first middleware on the app
+  app.use(Handlers.requestHandler());
+
+  // TracingHandler creates a trace for every incoming request
+  app.use(Handlers.tracingHandler());
+
+  return () => {
+    // The error handler must be before any other error middleware and after all controllers
+    app.use(Handlers.errorHandler());
+  };
+}
+
+function capture(err) {
+  console.log("capture", err);
+  if (err) {
+    sentryCaptureException(err);
+  }
+}
+function captureMessage(mess) {
+  console.log("captureMessage", mess);
+  if (mess) {
+    sentryCaptureMessage(mess);
+  }
+}
+
+module.exports = {
+  initSentry,
+  capture,
+  captureMessage,
+};
