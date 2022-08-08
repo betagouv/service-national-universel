71bf79b78a9dbb5be1cb4ab20112e8ed7e126b93
naustra <lebihanp@hotmail.fr>
2022-08-08 17:04:03 +0200

    fix(sentry): Group transactions into sentry (#1726)

--- api/src/sentry.js
+++ api/src/sentry.js
@@ -1,7 +1,27 @@
 const { ExtraErrorData, RewriteFrames } = require("@sentry/integrations");
-const { captureException: sentryCaptureException, captureMessage: sentryCaptureMessage, Integrations: NodeIntegrations, init, Handlers } = require("@sentry/node");
+const {
+  addGlobalEventProcessor,
+  CaptureException: sentryCaptureException,
+  captureMessage: sentryCaptureMessage,
+  Integrations: NodeIntegrations,
+  init,
+  Handlers,
+} = require("@sentry/node");
 const { Integrations: TracingIntegrations } = require("@sentry/tracing");
 const { SENTRY_URL, SENTRY_TRACING_SAMPLE_RATE } = require("./config");
+
+const regex = /[0-9a-fA-F]{24}/g;
+
+const sanitizeTransactionName = (name) => {
+  return name.replace(regex, ":id");
+};
+
+addGlobalEventProcessor((event) => {
+  if (event.type === "transaction") {
+    event.transaction = sanitizeTransactionName(event.transaction);
+  }
+  return event;
+});
 
 function initSentry(app) {
   init({
@@ -35,13 +55,13 @@ function initSentry(app) {
 function capture(err) {
   console.log("capture", err);
   if (err) {
-    sentryCaptureException(err);
+    if (!process.env.JEST_WORKER_ID) sentryCaptureException(err);
   }
 }
 function captureMessage(mess) {
   console.log("captureMessage", mess);
   if (mess) {
-    sentryCaptureMessage(mess);
+    if (!process.env.JEST_WORKER_ID) sentryCaptureMessage(mess);
   }
 }
 
