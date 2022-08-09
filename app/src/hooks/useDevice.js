d02fe3f65db1a075860d63687be3707983238d31
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2022-08-09 16:46:53 +0200

    add breakpoint setting

--- app/src/hooks/useDevice.js
+++ app/src/hooks/useDevice.js
@@ -1,6 +1,6 @@
 import tailwind from "../../tailwind.config";
 
-export default function useDevice() {
-  if (window.innerWidth <= parseInt(tailwind.theme.screens.md)) return "mobile";
+export default function useDevice(breakpoint = "md") {
+  if (window.innerWidth <= parseInt(tailwind.theme.screens[breakpoint])) return "mobile";
   else return "desktop";
 }
