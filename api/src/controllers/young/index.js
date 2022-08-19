8a7f35d1d0f74f6600b88c873579d9afbcd55af7
Nicolas Bretécher <bretechern@gmail.com>
2022-08-19 11:57:35 +0200

    chore(api): update young model for new file upload (#1733)

--- api/src/controllers/young/index.js
+++ api/src/controllers/young/index.js
@@ -40,6 +40,7 @@ const {
   getFile,
   notifDepartmentChange,
   autoValidationSessionPhase1Young,
+  deleteFile,
 } = require("../../utils");
 const { sendTemplate } = require("../../sendinblue");
 const { cookieOptions } = require("../../cookie-options");
@@ -821,6 +822,21 @@ router.put("/:id/soft-delete", passport.authenticate("referent", { session: fals
       "zip",
       "city",
     ];
+
+    for (const key in young.files) {
+      if (key.length) {
+        for (const file in key) {
+          try {
+            if (key.includes("military")) await deleteFile(`app/young/${id}/military-preparation/${key}/${file._id}`);
+            else await deleteFile(`app/young/${id}/${key}/${file._id}`);
+            young.set({ files: { [key]: undefined } });
+          } catch (e) {
+            capture(e);
+            console.error(e);
+          }
+        }
+      }
+    }
 
     for (const key in young._doc) {
       if (!fieldToKeep.find((val) => val === key)) {
