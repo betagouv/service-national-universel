571a236f22b88c3c4c78224fb49738352efe4454
naustra <lebihanp@hotmail.fr>
2022-08-22 17:23:00 +0200

    fix(api): Educonnect delete session

--- api/src/index.js
+++ api/src/index.js
@@ -4,7 +4,6 @@ const { initSentry } = require("./sentry");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const express = require("express");
-const session = require("express-session");
 const cookieParser = require("cookie-parser");
 const helmet = require("helmet");
 const logger = require("morgan");
@@ -59,10 +58,7 @@ app.use(cookieParser());
 
 app.use(express.static(__dirname + "/../public"));
 
-app.use(session({ secret: "secret", resave: false, saveUninitialized: true }));
-
 app.use(passport.initialize());
-app.use(passport.session({}));
 
 app.use("/es", require("./controllers/es"));
 app.use("/mission", require("./controllers/mission"));
