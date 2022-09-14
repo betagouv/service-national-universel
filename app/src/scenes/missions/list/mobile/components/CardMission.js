8731dd439d66cf5b64a4de28cba7cad2c9bfb7ec
Mélina Poignet <93988861+Melinapgt@users.noreply.github.com>
2022-09-14 21:32:47 +0200

    import getDistance mobile

--- app/src/scenes/missions/list/mobile/components/CardMission.js
+++ app/src/scenes/missions/list/mobile/components/CardMission.js
@@ -2,28 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import LocationMarker from "../../../../../assets/icons/LocationMarker";
 import IconDomain from "../../../components/IconDomain";
+import { getDistance } from "../../../../../utils";
 
 export default function mission({ mission, youngLocation }) {
-  const getDistance = (lat1, lon1, lat2, lon2) => {
-    if (lat1 == lat2 && lon1 == lon2) {
-      return 0;
-    } else {
-      let radlat1 = (Math.PI * lat1) / 180;
-      let radlat2 = (Math.PI * lat2) / 180;
-      let theta = lon1 - lon2;
-      let radtheta = (Math.PI * theta) / 180;
-      let dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-      if (dist > 1) {
-        dist = 1;
-      }
-      dist = Math.acos(dist);
-      dist = (dist * 180) / Math.PI;
-      dist = dist * 60 * 1.1515;
-
-      return dist;
-    }
-  };
-
   return (
     <Link to={`/mission/${mission._id}`} className="bg-white relative flex  justify-between shadow-nina rounded-xl p-3 border-[1px] border-[#ffffff] mb-4 z-10">
       <div className="flex flex-1">
