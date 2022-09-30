e2770e41cf1291785f09f2d2375e70539a3dcd5b
tangimds <tangimds@gmail.com>
2022-09-30 16:44:58 +0200

    wip file

--- admin/src/scenes/volontaires/view/phase2bis/application.js
+++ admin/src/scenes/volontaires/view/phase2bis/application.js
@@ -13,17 +13,22 @@ import { AiOutlineClockCircle, AiFillClockCircle } from "react-icons/ai";
 import rubberStampValided from "../../../../assets/rubberStampValided.svg";
 import rubberStampNotValided from "../../../../assets/rubberStampNotValided.svg";
 import ReactTooltip from "react-tooltip";
-import Clock from "../../../../assets/Clock.svg";
-import LeftArrow from "../../../../assets/icons/ArrowNarrowLeft";
+import { BsChevronDown } from "react-icons/bs";
+import { HiOutlineMail, HiPlus } from "react-icons/hi";
+import ModalPJ from "./components/ModalPJ";
 
 export default function Phase2Application({ young, onChange }) {
   const [application, setApplication] = React.useState(null);
   const [mission, setMission] = React.useState();
   const [tags, setTags] = React.useState();
   const [contract, setContract] = React.useState(null);
+  const [openAttachments, setOpenAttachments] = React.useState(false);
+  const [modalDocument, setModalDocument] = React.useState({ isOpen: false });
 
   let { applicationId } = useParams();
   const history = useHistory();
+
+  const optionsType = ["contractAvenantFiles", "justificatifsFiles", "feedBackExperienceFiles", "othersFiles"];
 
   const theme = {
     background: {
@@ -66,21 +71,22 @@ export default function Phase2Application({ young, onChange }) {
     getApplication();
   }, []);
 
+  const getMission = async () => {
+    if (!application?.missionId) return;
+    const { ok, data, code } = await api.get(`/mission/${application.missionId}`);
+    if (!ok) {
+      capture(code);
+      return toastr.error("Oups, une erreur est survenue", code);
+    }
+    setMission(data);
+    const t = [];
+    data?.city && t.push(data?.city + (data?.zip ? ` - ${data?.zip}` : ""));
+    (data?.domains || []).forEach((d) => t.push(translate(d)));
+    setTags(t);
+  };
+
   React.useEffect(() => {
     if (!application) return;
-    const getMission = async () => {
-      if (!application.missionId) return;
-      const { ok, data, code } = await api.get(`/mission/${application.missionId}`);
-      if (!ok) {
-        capture(code);
-        return toastr.error("Oups, une erreur est survenue", code);
-      }
-      setMission(data);
-      const t = [];
-      data?.city && t.push(data?.city + (data?.zip ? ` - ${data?.zip}` : ""));
-      (data?.domains || []).forEach((d) => t.push(translate(d)));
-      setTags(t);
-    };
     getMission();
   }, [application]);
 
@@ -238,6 +244,84 @@ export default function Phase2Application({ young, onChange }) {
                 </div>
               )}
             </div>
+          </div>
+          <div className="mx-8 mt-8">
+            <div className="flex justify-between">
+              <div className="text-lg leading-6 font-semibold">Pièces jointes</div>
+              <div className="flex space-x-4 items-center">
+                {optionsType.reduce((nmb, option) => nmb + application[option].length, 0) !== 0 && (
+                  <div
+                    className="group flex items-center rounded-lg text-blue-600 text-center text-sm py-2 px-10 border-blue-600 border-[1px] hover:bg-blue-600 hover:text-white transition duration-100 ease-in-out"
+                    onClick={() => setOpenAttachments(!openAttachments)}>
+                    Voir mes pièces jointes
+                    <BsChevronDown className={`ml-3 text-blue-600 group-hover:text-white h-5 w-5 ${openAttachments ? "rotate-180" : ""}`} />
+                  </div>
+                )}
+                <div
+                  className="text-white bg-blue-600  rounded-full p-2 "
+                  onClick={() => {
+                    setModalDocument({
+                      isOpen: true,
+                      stepOne: true,
+                    });
+                  }}>
+                  <HiPlus />
+                </div>
+              </div>
+            </div>
+
+            {/* {openAttachments && (
+              <div className="flex flex-row overflow-x-auto gap-4 my-4 w-full ">
+                {optionsType.map(
+                  (option, index) =>
+                    mission.application[option].length > 0 && (
+                      <FileCard
+                        key={index}
+                        name={translateAddFilePhase2(option)[3].toUpperCase() + translateAddFilePhase2(option).slice(4)}
+                        icon="reglement"
+                        filled={mission.application[option].length}
+                        color="text-blue-600 bg-white"
+                        status="Modifier"
+                        onClick={() =>
+                          setModalDocument({
+                            isOpen: true,
+                            name: option,
+                            stepOne: false,
+                          })
+                        }
+                      />
+                    ),
+                )}
+              </div>
+            )} */}
+            <ModalPJ
+              isOpen={modalDocument?.isOpen}
+              name={modalDocument?.name}
+              young={young}
+              application={application}
+              optionsType={optionsType}
+              onCancel={async () => {
+                setModalDocument({ isOpen: false });
+                await getMission();
+              }}
+              onSend={async (type, multipleDocument) => {
+                try {
+                  const responseNotification = await api.post(`/application/${application._id}/notify/${SENDINBLUE_TEMPLATES.ATTACHEMENT_PHASE_2_APPLICATION}`, {
+                    type,
+                    multipleDocument,
+                  });
+                  if (!responseNotification?.ok) return toastr.error(translate(responseNotification?.code), "Une erreur s'est produite avec le service de notification.");
+                  toastr.success("L'email a bien été envoyé");
+                } catch (e) {
+                  toastr.error("Une erreur est survenue lors de l'envoi du mail", e.message);
+                }
+              }}
+              onSave={async () => {
+                setModalDocument({ isOpen: false });
+                await getMission();
+              }}
+              typeChose={modalDocument?.stepOne}
+            />
           </div>
         </div>
         {/* {JSON.stringify(application)} */}
