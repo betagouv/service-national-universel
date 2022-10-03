1d8401aeb6e153777ae076a27f356d406d828e77
hlecourt <hlecourt99@gmail.com>
2022-10-03 09:16:15 +0200

    input

---
+++ app/src/scenes/preinscription/components/input.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Input({ value, placeholder = null, onChange }) {
+  return (
+    <input
+      className="flex justify-between items-center gap-3 w-full bg-[#EEEEEE] px-4 py-2 border-b-[2px] border-[#3A3A3A] rounded-t-[4px]"
+      type="text"
+      value={value}
+      placeholder={placeholder}
+      onChange={(e) => onChange(e.target.value)}
+    />
+  );
+}
--- app/src/scenes/preinscription/mobile/stepEligibilite.js
+++ app/src/scenes/preinscription/mobile/stepEligibilite.js
@@ -2,6 +2,7 @@ import React from "react";
 import { PreInscriptionContext } from "../../../context/PreInscriptionContextProvider";
 import { useHistory } from "react-router-dom";
 import Select from "../components/select";
+import Input from "../components/input";
 
 export default function StepEligibilite() {
   const [data, setData] = React.useContext(PreInscriptionContext);
@@ -23,6 +24,12 @@ export default function StepEligibilite() {
       </href>
       <div className="mt-4 p-4 bg-white">
         <Select options={options} value={data.exemple} placeholder="Selectionner une option" onChange={(e) => setData({ ...data, exemple: e })} />
+      </div>
+      <div className="mt-4 p-4 bg-white">
+        <Input value={data.exemple2} placeholder="Entrez une valeur" onChange={(e) => setData({ ...data, exemple2: e })} />
+      </div>
+      <div className="mt-4 p-4 bg-white">
+        <Input value={data.exemple3} onChange={(e) => setData({ ...data, exemple3: e })} />
       </div>
     </div>
   );
