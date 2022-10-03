1089e8afdb9beb5c647e95ccb84c5daaa8de4aa5
hlecourt <hlecourt99@gmail.com>
2022-10-03 17:13:31 +0200

    feat(app): checkbox component + default value context

--- app/src/context/PreInscriptionContextProvider.js
+++ app/src/context/PreInscriptionContextProvider.js
@@ -4,7 +4,14 @@ export const PreInscriptionContext = createContext();
 
 const PreInscriptionContextProvider = ({ children }) => {
   //set default value for uncontrolled input
-  const [value, setValue] = useState({});
+  const [value, setValue] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    emailConfirm: "",
+    password: "",
+    confirmPassword: "",
+  });
   return <PreInscriptionContext.Provider value={[value, setValue]}>{children}</PreInscriptionContext.Provider>;
 };
 
