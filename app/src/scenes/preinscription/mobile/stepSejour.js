a8c7117fd94c10030eb3b9caafc1329239ec4147
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2022-10-03 12:25:41 +0200

    style

--- app/src/scenes/preinscription/mobile/stepSejour.js
+++ app/src/scenes/preinscription/mobile/stepSejour.js
@@ -1,16 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { PreInscriptionContext } from "../../../context/PreInscriptionContextProvider";
+import ArrowRightBlueSquare from "../../../assets/icons/ArrowRightBlueSquare";
 
 export default function StepSejour() {
   const [data, setData] = React.useContext(PreInscriptionContext);
-  let stays = {};
 
   function getAvailableStays() {
     // insert logic to select cohort according to birthdate & location
     return [
       {
-        cohort: "Février 2023 - Zone A",
+        cohort: "Février 2023 - C",
         date: "du 19 février au 3 mars",
+      },
+      {
+        cohort: "Avril 2023 - A",
+        date: "du 9 au 21 avril",
+      },
+      {
+        cohort: "Avril 2023 - B",
+        date: "du 16 au 28 avril",
+      },
+      {
+        cohort: "Juin 2023",
+        date: "du 11 au 23 juin",
+      },
+      {
+        cohort: "Juillet 2023",
+        date: "du 1er au 12 juillet",
       },
     ];
   }
@@ -20,28 +37,42 @@ export default function StepSejour() {
   }, []);
 
   return (
-    <>
-      <div>Etape 2 sur 3</div>
-      <div>Séjour de cohésion</div>
-      <h1>Choisissez la date du séjour</h1>
-      <h2>Séjours de cohésion disponibles</h2>
-      {getAvailableStays().map((e) => StayButton(data, setData))}
-    </>
+    <div className="bg-white p-4">
+      <div className="w-full flex justify-between">
+        <h1 className="text-2xl font-semibold">Choisissez la date du séjour</h1>
+        <div className="">?</div>
+      </div>
+      <hr className="my-4 h-px bg-gray-200 border-0" />
+
+      <div className="font-semibold my-2">Séjours de cohésion disponibles</div>
+
+      <div className="text-gray-500 text-sm">Veuillez vous assurer d’être disponible sur l’ensemble de la période.</div>
+      <div className="my-4">{getAvailableStays().map((e) => StayButton(data, setData, e))}</div>
+      <div className="font-semibold py-2">Pourquoi je ne vois pas tous les séjours ?</div>
+      <div className="text-gray-500 text-sm">
+        La proposition des séjours dépend de vos caractéristiques personnelles (âge, situation scolaire ou professionnelle, localisation). En savoir plus.
+      </div>
+      <div className="text-blue-600 my-4 underline">
+        <Link to="">Consulter d’autres dispositifs d’engagement</Link>
+      </div>
+    </div>
   );
 }
 
 function StayButton(data, setData, stay) {
   return (
     <div
-      className="border"
+      key={stay.name}
+      className="border px-3 py-4 my-3 flex justify-between items-center"
       onClick={() => {
         setData({ ...data, cohort: stay.cohort });
-        // history.push("/preinscription/sejour");
+        // history.push("/preinscription/profil");
         console.log(data);
       }}>
       <div>
-        Séjour du <strong>{stay.sejour}</strong>
+        Séjour du <strong>{stay.date}</strong>
       </div>
+      <ArrowRightBlueSquare />
     </div>
   );
 }
