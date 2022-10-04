432bd845ff2cbbaa79b171ec00a91ead103ac23e
Szilagyi Kinga <szilagyikinga42@gmail.com>
2022-10-04 16:36:50 +0200

    refactor(app): use state object instead of flat data

--- app/src/scenes/inscription2023/mobile/stepCoordonnees.js
+++ app/src/scenes/inscription2023/mobile/stepCoordonnees.js
@@ -9,34 +9,63 @@ import { youngSchooledSituationOptions, youngActiveSituationOptions, countryOpti
 
 const FRANCE = "France";
 
+const defaultState = {
+  isFrench: isFrenchOptions[0].value,
+  gender: genderOptions[0].value,
+  birthCity: "",
+  birthCityZip: "",
+  phone: "",
+  isFrenchResident: isFrenchOptions[0].value,
+  country: FRANCE,
+  address: "",
+  city: "",
+  zip: "",
+  hostFirstName: "",
+  hostLastName: "",
+  hostRelationShip: "",
+  hostCity: "",
+  hostZip: "",
+  hostAddress: "",
+  //@todo: change for only one key after data retrieve
+  schoolSituation: youngSchooledSituationOptions[0].value,
+  activeSitutation: youngActiveSituationOptions[0].value,
+};
+
 export default function StepCoordonnees() {
-  const [isFrench, setFrench] = useState(isFrenchOptions[0].value);
-  const [gender, setGender] = useState(genderOptions[0].value);
-  const [birthCity, setBirthCity] = useState("");
-  const [birthCityZip, setBirthCityZip] = useState("");
-  const [phone, setPhone] = useState("");
-  const [isFrenchResident, _setFrenchResident] = useState(isFrenchOptions[0].value);
-  const [country, setCountry] = useState(FRANCE);
-  const [address, setAddress] = useState("");
-  const [city, setCity] = useState("");
-  const [zip, setZip] = useState("");
-  const [hostFirstName, setHostFirstName] = useState("");
-  const [hostLastName, setHostLastName] = useState("");
-  const [hostRelationShip, setHostRelationShip] = useState("");
-  const [hostCity, setHostCity] = useState("");
-  const [hostZip, setHostZip] = useState("");
-  const [hostAddress, setHostAddress] = useState("");
-  const [schoolSituation, setSchoolSituation] = useState(youngSchooledSituationOptions[0].value);
-  const [activeSitutation, setActiveSituation] = useState(youngActiveSituationOptions[0].value);
+  const [data, setData] = useState(defaultState);
 
   const setFrenchResident = (isFrenchResident) => {
     if (isFrenchResident) {
-      setCountry(FRANCE);
+      setData({ ...data, country: FRANCE, isFrenchResident });
     } else {
-      setCountry("");
+      setData({ ...data, country: "", isFrenchResident });
     }
-    _setFrenchResident(isFrenchResident);
   };
+
+  const updateData = (key) => (value) => {
+    setData({ ...data, [key]: value });
+  };
+
+  const {
+    isFrench,
+    birthCityZip,
+    birthCity,
+    gender,
+    phone,
+    isFrenchResident,
+    country,
+    address,
+    zip,
+    city,
+    hostFirstName,
+    hostLastName,
+    hostCity,
+    hostAddress,
+    hostZip,
+    hostRelationShip,
+    schoolSituation,
+    activeSitutation,
+  } = data;
 
   return (
     <>
@@ -44,18 +73,18 @@ export default function StepCoordonnees() {
       <div className="bg-white p-4 text-[#161616]">
         <h1 className="text-[22px] font-bold">Mon profile volontaire</h1>
         <hr className="my-4 h-px bg-gray-200 border-0" />
-        <RadioButton className="my-4" label="Je suis né(e)..." options={isFrenchOptions} onChange={setFrench} value={isFrench} />
-        <Input className="my-4" value={birthCityZip} label="Code postal de naissance" onChange={setBirthCityZip} />
-        <Input className="my-4" value={birthCity} label="Commune de naissance" onChange={setBirthCity} />
-        <RadioButton className="my-4" label="Sexe" options={genderOptions} onChange={setGender} value={gender} />
-        <Input type="tel" className="my-4" value={phone} label="Votre téléphone" onChange={setPhone} />
+        <RadioButton className="my-4" label="Je suis né(e)..." options={isFrenchOptions} onChange={updateData("isFrench")} value={isFrench} />
+        <Input className="my-4" value={birthCityZip} label="Code postal de naissance" onChange={updateData("birthCityZip")} />
+        <Input className="my-4" value={birthCity} label="Commune de naissance" onChange={updateData("birthCity")} />
+        <RadioButton className="my-4" label="Sexe" options={genderOptions} onChange={updateData("gender")} value={gender} />
+        <Input type="tel" className="my-4" value={phone} label="Votre téléphone" onChange={updateData("phone")} />
         <RadioButton className="my-4" label="Je réside..." options={isFrenchOptions} onChange={setFrenchResident} value={isFrenchResident} />
         {!isFrenchResident && (
-          <Select options={countryOptions} className="my-4" value={country} label="Pays de résidence" placeholder="Sélectionnez un pays" onChange={setCountry} />
+          <Select options={countryOptions} className="my-4" value={country} label="Pays de résidence" placeholder="Sélectionnez un pays" onChange={updateData("country")} />
         )}
-        <Input className="my-4" value={address} label="Adresse de résidence" onChange={setAddress} />
-        <Input className="my-4" value={zip} label="Code postal" onChange={setZip} />
-        <Input className="my-4" value={city} label="Ville" onChange={setCity} />
+        <Input className="my-4" value={address} label="Adresse de résidence" onChange={updateData("address")} />
+        <Input className="my-4" value={zip} label="Code postal" onChange={updateData("zip")} />
+        <Input className="my-4" value={city} label="Ville" onChange={updateData("city")} />
         {isFrenchResident && <GhostButton name="Vérifier mon adresse" onClick={() => {}} />}
         {!isFrenchResident && (
           <>
@@ -71,17 +100,23 @@ export default function StepCoordonnees() {
             <p className="text-[14px] text-[#666666] leading-tight text-justify">
               À noter : l’hébergement chez un proche en France ainsi que le transport entre votre lieu de résidence et celui de votre hébergeur sont à votre charge.
             </p>
-            <Input className="my-4" value={hostFirstName} label="Prénom de l’hébergeur" onChange={setHostFirstName} />
-            <Input className="my-4" value={hostLastName} label="Nom de l’hébergeur" onChange={setHostLastName} />
-            <Select options={hostRelationShipOptions} className="my-4" value={hostRelationShip} label="Précisez votre lien avec l’hébergeur" onChange={setHostRelationShip} />
-            <Input className="my-4" value={hostAddress} label="Son adresse" onChange={setHostCity} />
-            <Input className="my-4" value={hostZip} label="Code postal" onChange={setHostZip} />
-            <Input className="my-4" value={hostCity} label="Ville" onChange={setHostAddress} />
+            <Input className="my-4" value={hostFirstName} label="Prénom de l’hébergeur" onChange={updateData("hostFirstName")} />
+            <Input className="my-4" value={hostLastName} label="Nom de l’hébergeur" onChange={updateData("hostLastName")} />
+            <Select
+              options={hostRelationShipOptions}
+              className="my-4"
+              value={hostRelationShip}
+              label="Précisez votre lien avec l’hébergeur"
+              onChange={updateData("hostRelationShip")}
+            />
+            <Input className="my-4" value={hostAddress} label="Son adresse" onChange={updateData("hostAddress")} />
+            <Input className="my-4" value={hostZip} label="Code postal" onChange={updateData("hostZip")} />
+            <Input className="my-4" value={hostCity} label="Ville" onChange={updateData("hostCity")} />
             <GhostButton name="Vérifier" onClick={() => {}} />
           </>
         )}
-        <Select className="my-4" label="Ma situation scolaire" options={youngSchooledSituationOptions} value={schoolSituation} onChange={setSchoolSituation} />
-        <Select className="my-4" label="Ma situation" options={youngActiveSituationOptions} value={activeSitutation} onChange={setActiveSituation} />
+        <Select className="my-4" label="Ma situation scolaire" options={youngSchooledSituationOptions} value={schoolSituation} onChange={updateData("schoolSituation")} />
+        <Select className="my-4" label="Ma situation" options={youngActiveSituationOptions} value={activeSitutation} onChange={updateData("activeSitutation")} />
       </div>
     </>
   );
