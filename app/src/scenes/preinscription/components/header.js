1a3126c3f442a94f822828c773cb562bff6d2043
Melina <melina.poignet@selego.co>
2022-10-04 15:43:57 +0200

    fix

--- app/src/scenes/preinscription/components/header.js
+++ app/src/scenes/preinscription/components/header.js
@@ -2,7 +2,6 @@ import React from "react";
 import LogoFr from "../../../assets/fr.png";
 import SNU from "../../../assets/logo-snu.png";
 import Menu from "../../../assets/icons/Burger";
-<<<<<<< HEAD
 import useDevice from "../../../hooks/useDevice";
 import Help from "../../../assets/icons/QuestionMarkBlue";
 import File from "../../../assets/file.svg";
@@ -47,19 +46,6 @@ const Header = ({ setIsOpen }) => {
               </Link>
             </div>
           )}
-=======
-
-const Header = () => {
-  return (
-    <div className="flex  px-3  w-full h-[91px] shadow-md ">
-      <div className="flex justify-between w-full h-full border-b border-b-[#E5E5E5] py-3 md:mx-16 ">
-        <div className="flex w-2/3 space-x-6 items-center">
-          <img src={LogoFr} className="w-18 h-16" />
-          <img src={SNU} className="w-14" />
-        </div>
-        <div className="flex items-start">
-          <Menu />
->>>>>>> main
         </div>
       </div>
     </div>
