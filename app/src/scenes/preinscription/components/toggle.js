4dcbca4cdefc207def69e6da37ed2802e41f213f
Pierre LE BIHAN <lebihanp@hotmail.fr>
2022-10-04 11:22:43 +0200

    fix

--- app/src/scenes/preinscription/components/toggle.js
+++ app/src/scenes/preinscription/components/toggle.js
@@ -9,8 +9,11 @@ export default function Toggle({ toggled, onClick }) {
       </div>
     </div>
   ) : (
-    <div onClick={onClick} name="visibility" className={`flex items-center w-10 h-6 rounded-full bg-[#000091] transition duration-100 ease-in cursor-pointer`}>
-      <div className="flex justify-center items-center h-6 w-6 rounded-full border-[1px] border-[#000091] bg-white translate-x-0 transition duration-100 ease-in shadow-nina"></div>
+    <div
+      onClick={onClick}
+      name="visibility"
+      className={`flex items-center w-10 h-6 border-[1px] rounded-full border-[#000091] bg-white transition duration-100 ease-in cursor-pointer`}>
+      <div className="flex justify-center items-center h-6 w-6 rounded-full border-[1px] border-[#000091] bg-white translate-x-[-1px] transition duration-100 ease-in shadow-nina"></div>
     </div>
   );
 }
