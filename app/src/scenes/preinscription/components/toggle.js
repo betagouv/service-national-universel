117cd2c435dbf3be76991f9ae834ec8d460a10d9
Pierre LE BIHAN <lebihanp@hotmail.fr>
2022-10-04 11:09:59 +0200

    First version

---
+++ app/src/scenes/preinscription/components/toggle.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Check from "../../../assets/icons/Check2";
+
+export default function Toggle({ toggled, onClick }) {
+  return toggled ? (
+    <div onClick={onClick} name="visibility" className={`flex items-center w-10 h-6 rounded-full bg-[#000091] transition duration-100 ease-in cursor-pointer`}>
+      <div className="flex justify-center items-center h-6 w-6 rounded-full border-[1px] border-[#000091] bg-white translate-x-[16px] transition duration-100 ease-in shadow-nina">
+        <Check />
+      </div>
+    </div>
+  ) : (
+    <div onClick={onClick} name="visibility" className={`flex items-center w-10 h-6 rounded-full bg-[#000091] transition duration-100 ease-in cursor-pointer`}>
+      <div className="flex justify-center items-center h-6 w-6 rounded-full border-[1px] border-[#000091] bg-white translate-x-0 transition duration-100 ease-in shadow-nina"></div>
+    </div>
+  );
+}
