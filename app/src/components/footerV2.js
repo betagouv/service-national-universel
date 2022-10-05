8692ceb95e9d736d33a19ed18a96eb0f0e4db57d
Mélina Poignet <93988861+Melinapgt@users.noreply.github.com>
2022-10-05 12:09:06 +0200

    fix(app): fix footer preinscription (#1857)

--- app/src/components/footerV2.js
+++ app/src/components/footerV2.js
@@ -7,44 +7,48 @@ import SNU from "../assets/logo-snu.png";
 
 const Footer = () => {
   const { pathname } = useLocation();
+  const desktop = useDevice() === "desktop";
+  const mobile = useDevice() === "mobile";
+  const smallButton = !["/preinscription/sejour"].includes(pathname);
+  const largeButton = ["/preinscription/done"].includes(pathname);
   return (
     <div
       className={`${
-        useDevice() === "desktop" ? " absolute bottom-0 border-t-[#000091] pt-3" : !["/preinscription/sejour"].includes(pathname) && "mb-[12vh]"
-      } w-full bg-white border-t border-t-[#E5E5E5] `}>
-      <div className={`${useDevice() === "desktop" ? "flex justify-between w-full px-24" : "px-3"} text-sm text-[#3A3A3A] py-3`}>
-        <div className={`${useDevice() === "desktop" && "basis-[50%]"} mb-4 flex`}>
+        desktop ? " absolute bottom-0 border-t-[#000091] pt-3 px-3" : largeButton ? "mb-[20vh]" : smallButton && "mb-[12vh]"
+      } w-full bg-white border-t border-t-[#E5E5E5] pb-3 `}>
+      <div className={`${desktop ? "flex justify-between w-full px-[108px]" : "px-3"} text-sm text-[#3A3A3A] py-3`}>
+        <div className={`${desktop && "basis-[50%]"} mb-4 flex`}>
           <img src={LogoFr} className="w-26 h-24" />
-          {useDevice() === "desktop" && <img src={SNU} className="w-[90px] h-[90px] ml-8" />}
+          {desktop && <img src={SNU} className="w-[90px] h-[90px] ml-8" />}
         </div>
-        <div className={`${useDevice() === "desktop" && "basis-[50%]"}`}>
+        <div className={`${desktop && "basis-[50%]"}`}>
           <div className="leading-6 mb-4">
             Le Service national universel s’adresse à tous les jeunes de 15 à 17 ans qui souhaitent vivre une belle expérience collective, se rendre utile aux autres, créer des
             liens forts et se découvrir un talent pour l’engagement !
           </div>
-          <div className={`${useDevice() === "mobile" ? "flex-col" : "items-center space-x-6"} flex font-bold`}>
-            <div className={`${useDevice() === "mobile" ? "w-full mb-4" : "space-x-6"} flex `}>
-              <div className={`${useDevice() === "mobile" && "basis-[50%]"} flex items-center space-x-1  cursor-pointer`}>
+          <div className={`${mobile ? "flex-col" : "items-center space-x-6"} flex font-bold`}>
+            <div className={`${mobile ? "w-full mb-4" : "space-x-6"} flex `}>
+              <div className={`${mobile && "basis-[50%]"} flex items-center space-x-1  cursor-pointer`}>
                 <a href="https://www.legifrance.gouv.fr/" className="hover:text-[#3A3A3A]">
                   legifrance.gouv.fr
                 </a>
                 <LinkTo />
               </div>
-              <div className={`${useDevice() === "mobile" && "basis-[50%]"} flex items-center space-x-1  cursor-pointer`}>
+              <div className={`${mobile && "basis-[50%]"} flex items-center space-x-1  cursor-pointer`}>
                 <a href="https://www.gouvernement.fr/" className="hover:text-[#3A3A3A]">
                   gouvernement.fr
                 </a>
                 <LinkTo />
               </div>
             </div>
-            <div className={`${useDevice() === "mobile" ? "w-full" : "space-x-6"} flex`}>
-              <div className={`${useDevice() === "mobile" && "basis-[50%]"} flex items-center space-x-1  cursor-pointer`}>
+            <div className={`${mobile ? "w-full" : "space-x-6"} flex`}>
+              <div className={`${mobile && "basis-[50%]"} flex items-center space-x-1  cursor-pointer`}>
                 <a href="https://www.service-public.fr/" className="hover:text-[#3A3A3A]">
                   service-public.fr
                 </a>
                 <LinkTo />
               </div>
-              <div className={`${useDevice() === "mobile" && "basis-[50%]"} flex items-center space-x-1  cursor-pointer`}>
+              <div className={`${mobile && "basis-[50%]"} flex items-center space-x-1  cursor-pointer`}>
                 <a href="https://www.data.gouv.fr/fr/" className="hover:text-[#3A3A3A]">
                   data.gouv.fr
                 </a>
