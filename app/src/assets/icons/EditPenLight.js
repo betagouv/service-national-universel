224fd2d1d2f9d2823b248ab63ae9a38b05c63494
hlecourt <hlecourt99@gmail.com>
2022-10-06 12:16:39 +0200

    feat(app): front consentement

---
+++ app/src/assets/icons/EditPenLight.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+const EditPenLight = (props) => (
+  <svg width={13} height={13} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M8.042 2.629 11.813 6.4c.26.26.26.683 0 .943L7.1 12.057l-.943-.943 4.242-4.242-.471-.472-6.6 6.6H.5v-2.829L8.042 2.63ZM10.871.744l1.886 1.885c.26.26.26.682 0 .943l-.944.943-2.828-2.829.943-.942c.26-.26.682-.26.943 0Z"
+      fill="#000091"
+    />
+  </svg>
+);
+
+export default EditPenLight;
