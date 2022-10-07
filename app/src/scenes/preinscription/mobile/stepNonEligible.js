dc773b0834044c3e0c298e4b3f78a8f0b00c29e9
Melina <melina.poignet@selego.co>
2022-10-07 16:46:37 +0200

    add redirections & ellipsis

--- app/src/scenes/preinscription/mobile/stepNonEligible.js
+++ app/src/scenes/preinscription/mobile/stepNonEligible.js
@@ -4,7 +4,6 @@ import serviceCivique from "../../../assets/programmes-engagement/service-civiqu
 import jeVauxAider from "../../../assets/programmes-engagement/je-veux-aider.jpg";
 import reserveGendarmerie from "../../../assets/programmes-engagement/reserve-gendarmerie.jpg";
 import reserveArmee from "../../../assets/programmes-engagement/reserve-armees.jpg";
-import LinesEllipsis from "react-lines-ellipsis";
 import arrowRightBlue from "../../../assets/arrowRightBlue.svg";
 import StickyButton from "../../../components/inscription/stickyButton";
 
@@ -17,24 +16,28 @@ export default function NonEligible() {
       description:
         "Un engagement volontaire au service de l’intérêt général, en France ou à l’étranger, auprès d’organisations à but non lucratif ou publiques, dans 9 domaines d’actions jugés « prioritaires pour la Nation » : solidarité, santé, éducation pour tous, culture et loisirs, sport, environnement, mémoire et citoyenneté, développement international et action humanitaire, intervention d’urgence. Il permet de développer sa citoyenneté comme ses compétences professionnelles.",
       picture: serviceCivique,
+      link: "https://www.service-civique.gouv.fr/",
     },
     {
       title: "JeVeuxAider.gouv.fr par la Réserve Civique",
       description:
         "Un dispositif d’engagement civique accessible à tous, auprès d’organisations publiques ou associatives, dans dix domaines d’action : santé, éducation, protection de l’environnement, culture, sport, protection ... la liste complète est disponible ici.)",
       picture: jeVauxAider,
+      link: "https://www.jeveuxaider.gouv.fr/",
     },
     {
       title: "Réserve la Gendarmerie nationale",
       description:
         "La réserve opérationnelle de la gendarmerie renforce l'action des unités d’active et les structures de commandement. Les réservistes contribuent directement, à la production de sécurité et à la bonne exécution des lois.",
       picture: reserveGendarmerie,
+      link: "https://www.gendarmerie.interieur.gouv.fr/notre-institution/generalites/nos-effectifs/reserve-gendarmerie/devenir-reserviste",
     },
     {
       title: "Réserve des Armées",
       description:
         "Un engagement permettant de contribuer à la sécurité du pays en consacrant une partie de son temps à la défense de la France, notamment en participant à des missions de protection de la population.",
       picture: reserveArmee,
+      link: "https://www.reservistes.defense.gouv.fr/",
     },
   ];
   const onClickButton = () => {
@@ -56,12 +59,17 @@ export default function NonEligible() {
               <div key={index} className="flex w-96">
                 <div className="w-64 h-min-[700px] ">
                   <div className="w-full h-[155px] ">
-                    <img src={program.picture} className="object-cover w-full h-full" />
+                    <a href={program.link} target="_blank" rel="noreferrer">
+                      <img src={program.picture} className="object-cover w-full h-full" />
+                    </a>
                   </div>
                   <div className="min-h-min pl-4 pr-1 pb-2 border border-[#E5E5E5]">
                     <div className="font-semibold my-4 min-h-[40px]">{program.title}</div>
-                    <div className="text-[13px] leading-6 mb-4 ">
-                      {!isOpen ? <LinesEllipsis text={program.description} maxLine="3" ellipsis="..." trimRight basedOn="letters" /> : <>{program.description}</>}
+                    <div className={`text-[13px] leading-6 mb-4 ${!isOpen && "truncate... line-clamp-3 "}`}>
+                      {" "}
+                      <a href={program.link} target="_blank" rel="noreferrer" className="visited:text-[#161616]">
+                        {program.description}
+                      </a>
                     </div>
                     <div
                       className="text-[13px] flex justify-between pr-2"
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -93,5 +93,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/line-clamp")],
 };
