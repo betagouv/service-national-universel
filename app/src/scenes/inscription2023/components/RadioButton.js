dcf27ef9064e13b33f39a302ee9a9144520036fa
szilagyikinga <szilagyikinga42@gmail.com>
2022-10-11 17:05:26 +0200

    feat(api/app): special situations (#1896)

--- app/src/scenes/inscription2023/components/RadioButton.js
+++ app/src/scenes/inscription2023/components/RadioButton.js
@@ -1,14 +1,18 @@
 import React from "react";
+import ErrorMessage from "./ErrorMessage";
 
-const RadioButton = ({ options, label, onChange, value: currentValue }) => {
+const RadioButton = ({ options, label, description = "", onChange, value: currentValue, error }) => {
   return (
     <div className="mt-2 mb-6">
-      <label className="mb-2">{label}</label>
+      <label className="mb-2">
+        {label} <span className="text-[#666666] text-[14px] leading-tight"> {description}</span>
+      </label>
       <div className="flex flex-wrap justify-between max-w-md">
         {options.map(({ label, value }) => (
           <Option key={value} label={label} value={value === currentValue} onChange={() => onChange(value)} />
         ))}
       </div>
+      <ErrorMessage>{error}</ErrorMessage>
     </div>
   );
 };
