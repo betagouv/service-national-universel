350106c9efc9ad6a2cbe295715f98ebc403c8259
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2022-10-18 12:27:15 +0200

    chore(app): change date picker max date

--- app/src/scenes/preinscription/components/DatePickerList.js
+++ app/src/scenes/preinscription/components/DatePickerList.js
@@ -6,6 +6,7 @@ import { FiCalendar } from "react-icons/fi";
 registerLocale("fr", fr);
 
 export default function DatePickerList({ value, onChange, disabled = false }) {
+  const date = new Date();
   return (
     <div className="flex justify-between items-center gap-3 w-full bg-[#EEEEEE] px-4 py-2 border-b-[2px] border-[#3A3A3A] rounded-t-[4px] mt-2">
       <DatePicker
@@ -16,7 +17,7 @@ export default function DatePickerList({ value, onChange, disabled = false }) {
         disabled={disabled}
         className="bg-[#EEEEEE] w-full"
         dateFormat="dd/MM/yyyy"
-        maxDate={new Date("01/01/2030")}
+        maxDate={date.setFullYear(date.getFullYear() + 20)}
       />
       <FiCalendar />
     </div>
