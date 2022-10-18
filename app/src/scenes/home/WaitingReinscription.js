698d3aea660406bbf636478c25a3334692f2a1c1
Pierre LE BIHAN <lebihanp@hotmail.fr>
2022-10-18 10:37:52 +0200

    fix(app): Desktop pages

--- app/src/scenes/home/WaitingReinscription.js
+++ app/src/scenes/home/WaitingReinscription.js
@@ -16,25 +16,52 @@ export default function WaitingReinscription() {
   else return;
 
   return (
-    <div className="flex lg:hidden w-full">
-      <div className="flex flex-col-reverse ">
-        <div className="px-4 pb-4">
-          <div className="text-3xl font-medium leading-tight tracking-tight text-gray-800">
-            <strong>{young.firstName},</strong> vous inscrire sur un séjour en 2023 ?
+    <>
+      {/* DESKTOP */}
+      <div className="hidden lg:flex">
+        <div className="my-12 mx-10 w-full">
+          <div className="flex justify-between items-center rounded-lg bg-white ">
+            <div className="w-1/2 pl-10 py-12">
+              <div className="text-[48px] font-medium leading-tight tracking-tight text-gray-800">
+                <strong>{young.firstName},</strong> vous inscrire sur un séjour en 2023 ?
+              </div>
+              <div className="text-base left-7 text-gray-800 mt-5">{textPrecision}</div>
+              <div className="flex flex-col items-stretch w-fit">
+                <button
+                  className="rounded-[10px] border-[1px] py-2.5 px-3  bg-blue-600 hover:bg-white border-blue-600 mt-5 text-white hover:!text-blue-600 text-sm leading-5 font-medium transition ease-in-out duration-150"
+                  onClick={() => {
+                    plausibleEvent("Phase1/CTA reinscription - home page");
+                    history.push("/reinscription");
+                  }}>
+                  Vérifier mon éligibilité
+                </button>
+              </div>
+            </div>
+            <img className="w-1/2 object-fill" src={require("../../assets/homePhase2Desktop.png")} />
           </div>
-          <div className="left-7 text-[#738297] mt-3">{textPrecision}</div>
-          <div className="left-7 font-bold text-gray-800 mt-4">Vérifiez dès maintenant votre éligibilité</div>
-          <button
-            className="w-full rounded-[10px] border-[1px] py-2.5 px-3  bg-blue-600 hover:bg-white border-blue-600 mt-3 text-white hover:!text-blue-600 text-sm leading-5 transition ease-in-out duration-150"
-            onClick={() => {
-              plausibleEvent("Phase0/CTA reinscription - home page");
-              history.push("/reinscription");
-            }}>
-            Vérifier mon éligibilité
-          </button>
         </div>
-        <img src={require("../../assets/homePhase2Mobile.png")} />
       </div>
-    </div>
+      {/* MOBILE */}
+      <div className="flex lg:hidden w-full">
+        <div className="flex flex-col-reverse ">
+          <div className="px-4 pb-4">
+            <div className="text-3xl font-medium leading-tight tracking-tight text-gray-800">
+              <strong>{young.firstName},</strong> vous inscrire sur un séjour en 2023 ?
+            </div>
+            <div className="left-7 text-[#738297] mt-3">{textPrecision}</div>
+            <div className="left-7 font-bold text-gray-800 mt-4">Vérifiez dès maintenant votre éligibilité</div>
+            <button
+              className="w-full rounded-[10px] border-[1px] py-2.5 px-3  bg-blue-600 hover:bg-white border-blue-600 mt-3 text-white hover:!text-blue-600 text-sm leading-5 transition ease-in-out duration-150"
+              onClick={() => {
+                plausibleEvent("Phase1/CTA reinscription - home page");
+                history.push("/reinscription");
+              }}>
+              Vérifier mon éligibilité
+            </button>
+          </div>
+          <img src={require("../../assets/homePhase2Mobile.png")} />
+        </div>
+      </div>
+    </>
   );
 }
