70fe3fa57a04ba3a7d48d51e635d6293ed79f4c0
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2022-10-18 09:22:08 +0200

    remove exp date comp

--- app/src/scenes/inscription2023/desktop/stepUpload.js
+++ app/src/scenes/inscription2023/desktop/stepUpload.js
@@ -6,10 +6,11 @@ import { capture } from "../../../sentry";
 import api from "../../../services/api";
 import { translate } from "../../../utils";
 import { supportURL } from "../../../config";
+import { formatDateFR, sessions2023 } from "snu-lib";
 
+import DatePickerList from "../../preinscription/components/DatePickerList";
 import DesktopPageContainer from "../components/DesktopPageContainer";
 import Error from "../../../components/error";
-import ExpirationDate from "../components/ExpirationDate";
 import plausibleEvent from "../../../services/plausible";
 
 export default function StepUpload() {
@@ -128,7 +129,23 @@ export default function StepUpload() {
         </a>
         .
       </div>
-      {files.length > 0 && <ExpirationDate ID={ID[category]} date={date} setDate={setDate} young={young} />}
+      {files.length > 0 && (
+        <>
+          <hr className="my-8 h-px bg-gray-200 border-0" />
+          <div className="w-full flex">
+            <div className="w-1/2">
+              <div className="text-xl font-medium">Renseignez la date d’expiration</div>
+              <div className="text-gray-600 leading-loose mt-2 mb-8">
+                Votre pièce d’identité doit être valide à votre départ en séjour de cohésion (le {formatDateFR(sessions2023.filter((e) => e.name === young.cohort)[0].dateStart)}).
+              </div>
+              <DatePickerList value={date} onChange={(data) => setDate({ ...data, date })} />
+            </div>
+            <div className="w-1/2">
+              <img className="h-64 mx-auto" src={require(`../../../assets/IDProof/${ID.imgDate}`)} alt={ID.title} />
+            </div>
+          </div>
+        </>
+      )}
     </DesktopPageContainer>
   );
 }
--- app/src/scenes/inscription2023/mobile/stepUpload.js
+++ app/src/scenes/inscription2023/mobile/stepUpload.js
@@ -7,8 +7,9 @@ import api from "../../../services/api";
 import plausibleEvent from "../../../services/plausible";
 import { translate } from "../../../utils";
 import { supportURL } from "../../../config";
+import { formatDateFR, sessions2023 } from "snu-lib";
 
-import ExpirationDate from "../components/ExpirationDate";
+import DatePickerList from "../../preinscription/components/DatePickerList";
 import Help from "../components/Help";
 import Navbar from "../components/Navbar";
 import QuestionMarkBlueCircle from "../../../assets/icons/QuestionMarkBlueCircle";
@@ -130,7 +131,24 @@ export default function StepUpload() {
             )}
           </div>
         </div>
-        {files.length > 0 && <ExpirationDate ID={ID[category]} date={date} setDate={setDate} />}
+        {files.length > 0 && (
+          <>
+            <hr className="my-8 h-px bg-gray-200 border-0" />
+            <div className="w-full flex">
+              <div className="w-1/2">
+                <div className="text-xl font-medium">Renseignez la date d’expiration</div>
+                <div className="text-gray-600 leading-loose mt-2 mb-8">
+                  Votre pièce d’identité doit être valide à votre départ en séjour de cohésion (le {formatDateFR(sessions2023.filter((e) => e.name === young.cohort)[0].dateStart)}
+                  ).
+                </div>
+                <DatePickerList value={date} onChange={(data) => setDate({ ...data, date })} />
+              </div>
+              <div className="w-1/2">
+                <img className="h-64 mx-auto" src={require(`../../../assets/IDProof/${ID.imgDate}`)} alt={ID.title} />
+              </div>
+            </div>
+          </>
+        )}
       </div>
       <Help />
       <Footer marginBottom="mb-[88px]" />
