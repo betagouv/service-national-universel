5c3a68f29cb65d7c159bdff4dc67aedff1eb67be
Pierre LE BIHAN <lebihanp@hotmail.fr>
2022-10-18 15:25:19 +0200

    up sejour

--- app/src/scenes/reinscription/desktop/stepSejour.js
+++ app/src/scenes/reinscription/desktop/stepSejour.js
@@ -55,23 +55,22 @@ export default function StepSejour() {
   }, [young]);
 
   return (
-    <>
-      <Navbar />{" "}
-      <div className="bg-white p-4">
+    <div className="bg-[#f9f6f2] flex justify-center py-10">
+      <div className="bg-white basis-[70%] mx-auto my-0 px-[102px] py-[60px] drop-shadow-md">
         <div className="w-full flex justify-between items-center">
           <h1 className="text-2xl font-semibold">Choisissez la date du séjour</h1>
           <a href="/public-besoin-d-aide/" target="_blank" rel="noreferrer">
             <QuestionMarkBlueCircle />
           </a>
         </div>
-        <hr className="my-4 h-px bg-gray-200 border-0" />
+        <hr className="my-8 h-px bg-gray-200 border-0" />
         <div className="font-semibold my-2">Séjours de cohésion disponibles</div>
         <div className="text-gray-500 text-sm">Veuillez vous assurer d’être disponible sur l’ensemble de la période.</div>
         <div className="my-4">{sessions?.map((e) => SessionButton(e))}</div>
         {sessions?.length < 3 && (
           <>
-            <div className="font-semibold py-2">Pourquoi je ne vois pas tous les séjours ?</div>
-            <div className="text-gray-500 text-sm">
+            <div className="font-semibold py-2 mt-5">Pourquoi je ne vois pas tous les séjours ?</div>
+            <div className="text-gray-500 text-sm w-2/3">
               La proposition des séjours dépend de vos caractéristiques personnelles (âge, situation scolaire ou professionnelle, localisation).{" "}
               <a href={`${supportURL}/base-de-connaissance/suis-je-eligible-a-un-sejour-de-cohesion`} target="_blank" rel="noreferrer" className="underline underline-offset-4">
                 En savoir plus.
@@ -83,10 +82,8 @@ export default function StepSejour() {
           </>
         )}
       </div>
-      <Footer marginBottom={"88px"} />
-    </>
+    </div>
   );
-
   function SessionButton(session) {
     return (
       <div
