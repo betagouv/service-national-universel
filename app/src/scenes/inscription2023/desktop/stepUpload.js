e7df65e768338ec2a06ed099d969a9a2b0dd4df4
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2022-10-18 11:38:31 +0200

    Fix(app): logic + style steps docs & upload (#1929)

--- app/src/scenes/inscription2023/desktop/stepUpload.js
+++ app/src/scenes/inscription2023/desktop/stepUpload.js
@@ -1,16 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
-import Bin from "../../../assets/icons/Bin";
-import Error from "../../../components/error";
 import { setYoung } from "../../../redux/auth/actions";
 import { capture } from "../../../sentry";
 import api from "../../../services/api";
 import { translate } from "../../../utils";
-import ExpirationDate from "../components/ExpirationDate";
-import DesktopPageContainer from "../components/DesktopPageContainer";
-import plausibleEvent from "../../../services/plausible";
 import { supportURL } from "../../../config";
+import { formatDateFR, sessions2023 } from "snu-lib";
+
+import DatePickerList from "../../preinscription/components/DatePickerList";
+import DesktopPageContainer from "../components/DesktopPageContainer";
+import Error from "../../../components/error";
+import plausibleEvent from "../../../services/plausible";
 
 export default function StepUpload() {
   const { category } = useParams();
@@ -18,71 +19,33 @@ export default function StepUpload() {
   const history = useHistory();
   const dispatch = useDispatch();
   const [error, setError] = useState({});
-  const [loading, setLoading] = useState(false);
-  const [fileError, setFileError] = useState({});
-  const [filesToUpload, setFilesToUpload] = useState();
-  const [filesUploaded, setFilesUploaded] = useState();
+  const [files, setFiles] = useState({});
   const [date, setDate] = useState();
 
-  let disabled = false;
-  if (!date || !error || loading) disabled = true;
-  if (filesUploaded?.length) disabled = false;
-
-  async function upload(files) {
+  async function onSubmit() {
     for (const file of files) {
-      if (file.size > 5000000) {
-        return setFileError({
+      if (file.size > 5000000)
+        return setError({
           text: `Ce fichier ${files.name} est trop volumineux.`,
         });
-      }
     }
-    const res = await api.uploadFile(`/young/${young._id}/documents/cniFiles`, files, ID[category].category, new Date(date));
-    if (res.code === "FILE_CORRUPTED") {
-      setFileError({
+    const res = await api.uploadFile(`/young/${young._id}/documents/cniFiles`, Array.from(files), ID[category].category, new Date(date));
+    if (res.code === "FILE_CORRUPTED")
+      return setError({
         text: "Le fichier semble corrompu. Pouvez-vous changer le format ou regénérer votre fichier ? Si vous rencontrez toujours le problème, contactez le support inscription@snu.gouv.fr",
       });
-    } else if (!res.ok) {
+    if (!res.ok) {
       capture(res.code);
-      setFileError({ text: "Une erreur s'est produite lors du téléversement de votre fichier" });
+      return setError({ text: "Une erreur s'est produite lors du téléversement de votre fichier." });
     }
-  }
-
-  async function deleteFile(fileId) {
-    try {
-      const res = await api.remove(`/young/${young._id}/documents/cniFiles/${fileId}`);
-      if (!res.ok) setError({ text: "Wesh" });
-      setFilesUploaded(res.data.filter((e) => e.category === category));
-    } catch (e) {
-      capture(e);
-      setError({ text: "Impossible de supprimer ce fichier." });
+    const { ok, code, data: responseData } = await api.put("/young/inscription2023/documents/next");
+    if (!ok) {
+      capture(code);
+      return setError({ text: `Une erreur s'est produite`, subText: code ? translate(code) : "" });
     }
-  }
-
-  async function onSubmit() {
-    setLoading(true);
-    try {
-      if (filesToUpload !== undefined) {
-        await upload([...filesToUpload]);
-        if (error.length) return setLoading(false);
-      }
-      const { ok, code, data: responseData } = await api.put("/young/inscription2023/documents/next");
-      if (!ok) {
-        capture(code);
-        setError({ text: `Une erreur s'est produite`, subText: code ? translate(code) : "" });
-        setLoading(false);
-        return;
-      }
-      dispatch(setYoung(responseData));
-      plausibleEvent("Phase0/CTA inscription - CI desktop");
-      history.push("/inscription2023/confirm");
-    } catch (e) {
-      capture(e);
-      setError({
-        text: `Une erreur s'est produite`,
-        subText: e?.code ? translate(e.code) : "",
-      });
-    }
-    setLoading(false);
+    dispatch(setYoung(responseData));
+    plausibleEvent("Phase0/CTA inscription - CI mobile");
+    history.push("/inscription2023/confirm");
   }
 
   const ID = {
@@ -110,79 +73,79 @@ export default function StepUpload() {
     },
   };
 
-  useEffect(() => {
-    setFilesUploaded(young.files.cniFiles.filter((e) => e.category === category));
-  }, [young]);
-
   return (
     <DesktopPageContainer
       title={ID[category].title}
       subTitle={ID[category].subTitle}
       onClickPrevious={() => history.push("/inscription2023/documents")}
-      onSubmit={() => onSubmit(filesToUpload)}
-      disabled={disabled}
+      onSubmit={onSubmit}
+      disabled={!date}
       questionMarckLink={`${supportURL}/base-de-connaissance/je-minscris-et-justifie-mon-identite`}>
-      <div className="bg-white p-4">
-        {Object.keys(error).length > 0 && <Error {...error} onClose={() => setError({})} />}
-        <div className="w-full flex my-4">
-          <div className="flex align-center items-center">
-            <img className={`${ID[category].imgBack && "w-1/2"} px-4`} src={require(`../../../assets/IDProof/${ID[category].imgFront}`)} alt={ID[category].title} />
-            {ID[category].imgBack && <img className="w-1/2 px-4" src={require(`../../../assets/IDProof/${ID[category].imgBack}`)} alt={ID[category].title} />}
-          </div>
+      {Object.keys(error).length > 0 && <Error {...error} onClose={() => setError({})} />}
+      <div className="w-full my-16 flex justify-around">
+        <img className="h-64" src={require(`../../../assets/IDProof/${ID[category].imgFront}`)} alt={ID[category].title} />
+        {ID[category].imgBack && <img className="h-64" src={require(`../../../assets/IDProof/${ID[category].imgBack}`)} alt={ID[category].title} />}
+      </div>
+      <div className="border-l-8 border-l-[#6A6AF4] pl-8 leading-loose">
+        Toutes les informations doivent être <strong>lisibles</strong>, le document doit être visible <strong>entièrement</strong>, la photo doit être <strong>nette</strong>. Le
+        document doit être téléversé en <strong>recto</strong> et <strong>verso</strong>.
+      </div>
+      <hr className="my-8 h-px bg-gray-200 border-0" />
+      <div>Ajouter un fichier</div>
+      <div className="text-gray-500 text-sm my-4">Taille maximale : 5 Mo. Formats supportés : jpg, png, pdf. Plusieurs fichiers possibles.</div>
+      <input
+        type="file"
+        multiple
+        id="file-upload"
+        name="file-upload"
+        accept=".png, .jpg, .jpeg, .pdf"
+        onChange={(e) => {
+          setFiles(e.target.files);
+        }}
+        className="hidden"
+      />
+      <div className="flex w-full my-4">
+        <div>
+          <label htmlFor="file-upload" className="cursor-pointer bg-[#EEEEEE] text-sm py-2 px-3 rounded text-gray-600">
+            Parcourir...
+          </label>
         </div>
-        <div className="my-2 border-l-8 border-l-[#6A6AF4] pl-4">
-          Toutes les informations doivent être <strong>lisibles</strong>, le document doit être visible <strong>entièrement</strong>, la photo doit être <strong>nette</strong>. Le
-          document doit être téléversé en <strong>recto</strong> et <strong>verso</strong>.
-        </div>
-        <hr className="my-4 h-px bg-gray-200 border-0" />
-        <div>Ajouter un fichier</div>
-        <div className="text-gray-500 text-sm mt-4">Taille maximale : 5 Mo. Formats supportés : jpg, png, pdf. Plusieurs fichiers possibles.</div>
-        <input
-          type="file"
-          multiple
-          id="file-upload"
-          name="file-upload"
-          accept=".png, .jpg, .jpeg, .pdf"
-          onChange={(e) => {
-            setFilesToUpload(e.target.files);
-          }}
-          className="hidden"
-        />
-        <div className="flex w-full mt-4">
-          <div>
-            <label htmlFor="file-upload" className="cursor-pointer bg-[#EEEEEE] text-sm py-2 px-3 rounded text-gray-600">
-              Parcourir...
-            </label>
-          </div>
-          <div className="ml-4">
-            {filesToUpload ? (
-              Array.from(filesToUpload).map((e) => (
-                <p className="text-gray-800 text-sm mt-2" key={e.name}>
-                  {e.name}
-                </p>
-              ))
-            ) : (
-              <div className="text-gray-800 text-sm mt-2">Aucun fichier sélectionné.</div>
-            )}
-          </div>
-        </div>
-        {Object.keys(fileError).length > 0 && <Error {...fileError} onClose={() => setError({})} />}
-        {filesToUpload && <ExpirationDate ID={ID[category]} date={date} setDate={setDate} />}
-        <div className="mt-2">
-          {filesUploaded &&
-            filesUploaded.map((e) => (
-              <div key={e._id} className="flex w-full justify-between">
-                <p className=" text-gray-800 text-sm mt-2">{e.name}</p>
-                <div onClick={() => deleteFile(e._id)} className="cursor-pointer text-blue-800 flex mt-2">
-                  <div className="mt-1">
-                    <Bin />
-                  </div>
-                  <p className="text-sm font-medium ml-2">Supprimer</p>
-                </div>
-              </div>
-            ))}
+        <div className="ml-4 mt-2">
+          {files.length ? (
+            Array.from(files).map((e) => (
+              <p className="text-gray-800 text-sm" key={e.name}>
+                {e.name}
+              </p>
+            ))
+          ) : (
+            <div className="text-gray-800 text-sm">Aucun fichier sélectionné.</div>
+          )}
         </div>
       </div>
+      <div className="text-gray-800 text-sm my-4">
+        Vous avez besoin d’aide pour téléverser les documents ?{" "}
+        <a href="https://support.snu.gouv.fr/base-de-connaissance/je-televerse-un-document/" className="underline">
+          Cliquez ici
+        </a>
+        .
+      </div>
+      {files.length > 0 && (
+        <>
+          <hr className="my-8 h-px bg-gray-200 border-0" />
+          <div className="w-full flex">
+            <div className="w-1/2">
+              <div className="text-xl font-medium">Renseignez la date d’expiration</div>
+              <div className="text-gray-600 leading-loose mt-2 mb-8">
+                Votre pièce d’identité doit être valide à votre départ en séjour de cohésion (le {formatDateFR(sessions2023.filter((e) => e.name === young.cohort)[0].dateStart)}).
+              </div>
+              <DatePickerList value={date} onChange={(date) => setDate(date)} />
+            </div>
+            <div className="w-1/2">
+              <img className="h-64 mx-auto" src={require(`../../../assets/IDProof/${ID[category].imgDate}`)} alt={ID.title} />
+            </div>
+          </div>
+        </>
+      )}
     </DesktopPageContainer>
   );
 }
--- app/src/scenes/inscription2023/mobile/stepDocuments.js
+++ app/src/scenes/inscription2023/mobile/stepDocuments.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
-import QuestionMarkBlueCircle from "../../../assets/icons/QuestionMarkBlueCircle";
+import { supportURL } from "../../../config";
+import { useDispatch, useSelector } from "react-redux";
+import { setYoung } from "../../../redux/auth/actions";
+import { translate } from "snu-lib";
+import { capture } from "../../../sentry";
+import api from "../../../services/api";
+
 import ArrowRightBlueSquare from "../../../assets/icons/ArrowRightBlueSquare";
-import Navbar from "../components/Navbar";
-import StickyButton from "../../../components/inscription/stickyButton";
+import Bin from "../../../assets/icons/Bin";
+import Error from "../../../components/error";
 import Footer from "../../../components/footerV2";
 import Help from "../components/Help";
-import { supportURL } from "../../../config";
+import Navbar from "../components/Navbar";
+import QuestionMarkBlueCircle from "../../../assets/icons/QuestionMarkBlueCircle";
+import StickyButton from "../../../components/inscription/stickyButton";
 
 export default function StepDocuments() {
   const history = useHistory();
+  const dispatch = useDispatch();
+  const young = useSelector((state) => state.Auth.young);
+  const [files, setFiles] = useState(young?.files.cniFiles);
+  const [error, setError] = useState({});
 
   const IDs = [
     {
@@ -28,10 +40,33 @@ export default function StepDocuments() {
     },
   ];
 
+  async function deleteFile(fileId) {
+    try {
+      const res = await api.remove(`/young/${young._id}/documents/cniFiles/${fileId}`);
+      if (!res.ok) setError({ text: "Wesh" });
+      setFiles(res.data);
+    } catch (e) {
+      capture(e);
+      setError({ text: "Impossible de supprimer ce fichier." });
+    }
+  }
+
+  async function onSubmit() {
+    const { ok, code, data: responseData } = await api.put("/young/inscription2023/documents/next");
+    if (!ok) {
+      capture(code);
+      setError({ text: `Une erreur s'est produite`, subText: code ? translate(code) : "" });
+      return;
+    }
+    dispatch(setYoung(responseData));
+    history.push("/inscription2023/confirm");
+  }
+
   return (
     <>
       <Navbar />
       <div className="bg-white p-4">
+        {Object.keys(error).length > 0 && <Error {...error} onClose={() => setError({})} />}
         <div className="w-full flex justify-between items-center">
           <h1 className="text-2xl font-semibold">Ma pièce d’identité</h1>
           <a href={`${supportURL}/base-de-connaissance/je-minscris-et-justifie-mon-identite`} target="_blank" rel="noreferrer">
@@ -52,10 +87,33 @@ export default function StepDocuments() {
             </div>
           </Link>
         ))}
+        {files?.length > 0 && (
+          <>
+            <h2 className="text-base text-gray-800 font-semibold my-2">Documents en ligne&nbsp;:</h2>
+            <div className="space-y-2">
+              {files.map((e) => (
+                <div key={e._id} className="flex w-full justify-between">
+                  <div className="w-2/3">
+                    <p className="text-gray-800 text-sm truncate">{e.name}</p>
+                    <p className="text-gray-600 text-xs truncate">{translate(e.category)}</p>
+                  </div>
+                  <div className="text-blue-800 flex">
+                    <div className="mt-1 mr-1">
+                      <Bin />
+                    </div>
+                    <p className="text-sm font-medium" onClick={() => deleteFile(e._id)}>
+                      Supprimer
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </>
+        )}
       </div>
       <Help />
       <Footer marginBottom="mb-[88px]" />
-      <StickyButton text="Continuer" onClickPrevious={() => history.push("/inscription2023/representants")} disabled />
+      <StickyButton text="Continuer" onClickPrevious={() => history.push("/inscription2023/representants")} onClick={onSubmit} disabled={files.length === 0} />
     </>
   );
 }
