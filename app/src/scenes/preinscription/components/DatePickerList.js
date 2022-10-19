8e459bdc38cf1abb264f67a0e894bc1ca08a268a
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2022-10-19 18:29:50 +0200

    up

--- app/src/scenes/preinscription/components/DatePickerList.js
+++ app/src/scenes/preinscription/components/DatePickerList.js
@@ -9,7 +9,8 @@ export default function DatePickerList({ value, onChange, disabled = false }) {
   const date = new Date();
   return (
     <div className="flex justify-between items-center gap-3 w-full bg-[#EEEEEE] px-4 py-2 border-b-[2px] border-[#3A3A3A] rounded-t-[4px] mt-2">
-      <DatePicker
+      <input type="date" value={value} onChange={onChange} className="bg-[#EEEEEE] w-full" disabled={disabled} />
+      {/* <DatePicker
         locale="fr"
         selected={value}
         onChange={onChange}
@@ -19,7 +20,7 @@ export default function DatePickerList({ value, onChange, disabled = false }) {
         dateFormat="dd/MM/yyyy"
         maxDate={date.setFullYear(date.getFullYear() + 20)}
       />
-      <FiCalendar />
+      <FiCalendar /> */}
     </div>
   );
 }
