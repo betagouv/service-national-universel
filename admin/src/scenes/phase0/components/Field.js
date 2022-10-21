e8c8d54a39729a718a953e362866bb5eebf25562
seb <seb@snarkfacgtory.fr>
2022-10-21 11:02:57 +0200

    Finalisation Header de page

--- admin/src/scenes/phase0/components/Field.js
+++ admin/src/scenes/phase0/components/Field.js
@@ -1,7 +1,18 @@
 import React, { useState } from "react";
 import PencilAlt from "../../../assets/icons/PencilAlt";
 
-export default function Field({ name, label, value, onChange, className = "" }) {
+/**
+ *
+ * @param name
+ * @param label
+ * @param value
+ * @param mode  could be "correction|edition|readonly" (par défaut readonly)
+ * @param onChange
+ * @param className
+ * @returns {JSX.Element}
+ * @constructor
+ */
+export default function Field({ name, label, value, mode, onChange, className = "" }) {
   const [mouseIn, setMouseIn] = useState(false);
 
   return (
@@ -12,9 +23,11 @@ export default function Field({ name, label, value, onChange, className = "" }) 
       onMouseLeave={() => setMouseIn(false)}>
       <label className="font-normal text-[12px] leading-[16px] text-[#6B7280]">{label}</label>
       <div className="font-normal text-[14px] leading-[20px] text-[#1F2937]">{value}</div>
-      <div className={(mouseIn ? "block" : "hidden") + " absolute top-[11px] right-[11px] p-[9px] rounded-[100px] bg-[#FFEDD5] cursor-pointer group hover:bg-[#F97316]"}>
-        <PencilAlt className="w-[14px] h-[14px] text-[#F97316] group-hover:text-white" />
-      </div>
+      {mode === "correction" && (
+        <div className={(mouseIn ? "block" : "hidden") + " absolute top-[11px] right-[11px] p-[9px] rounded-[100px] bg-[#FFEDD5] cursor-pointer group hover:bg-[#F97316]"}>
+          <PencilAlt className="w-[14px] h-[14px] text-[#F97316] group-hover:text-white" />
+        </div>
+      )}
     </div>
   );
 }
--- admin/src/scenes/phase0/components/YoungHeader.js
+++ admin/src/scenes/phase0/components/YoungHeader.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Title from "../../../components/views/Title";
-import {canViewEmailHistory, ROLES} from "../../../utils";
+import { canViewEmailHistory, ROLES, translateCohort } from "../../../utils";
 import Badge from "../../../components/Badge";
 import TabList from "../../../components/views/TabList";
 import Tab from "./Tab";
@@ -16,14 +16,15 @@ import { Button } from "./Buttons";
 import Bin from "../../../assets/Bin";
 import TakePlace from "../../../assets/icons/TakePlace";
 import ModalConfirm from "../../../components/modals/ModalConfirm";
-import {toastr} from "react-redux-toastr";
-import {DropdownItem, DropdownMenu, DropdownToggle, UncontrolledDropdown} from "reactstrap";
+import { toastr } from "react-redux-toastr";
+import { DropdownItem, DropdownMenu, DropdownToggle, UncontrolledDropdown } from "reactstrap";
 import IconChangementCohorte from "../../../assets/IconChangementCohorte";
 import Chevron from "../../../components/Chevron";
+import ModalConfirmWithMessage from "../../../components/modals/ModalConfirmWithMessage";
 
 export default function YoungHeader({ young, tab, onChange }) {
   const user = useSelector((state) => state.Auth.user);
-  const [notesCount, setNotesCount] = useState(0);
+  const [notesCount, setNotesCount] = useState(0); // TODO: pour l'instant on ne sait pas ce que c'est ???
   const [confirmModal, setConfirmModal] = useState({ isOpen: false });
 
   function onClickDelete() {
@@ -83,7 +84,7 @@ export default function YoungHeader({ young, tab, onChange }) {
       </div>
       <div className="ml-[30px]">
         <div className="">
-          <Field name="status" label="Inscription" value={translate(young.status)} />
+          <Field mode="readonly" name="status" label="Inscription" value={translate(young.status)} />
           <div className="flex items-center justify-between my-[15px]">
             <Button icon={<Bin fill="red" />} onClick={onClickDelete}>
               Supprimer
@@ -142,12 +143,12 @@ function EditCohortButton({ young, onChange }) {
             );
           })}
       </DropdownMenu>
-      <ChangeCohortModal isOpen={changeCohortModal} young={young} cohort={newCohort} close={() => setChangeCohortModal(false)} />
+      <ChangeCohortModal isOpen={changeCohortModal} young={young} cohort={newCohort} close={() => setChangeCohortModal(false)} onChange={onChange} />
     </UncontrolledDropdown>
   );
 }
 
-function ChangeCohortModal({ isOpen, young, cohort, close }) {
+function ChangeCohortModal({ isOpen, young, cohort, close, onChange }) {
   const [modalConfirmWithMessage, setModalConfirmWithMessage] = useState(false);
   const [newCohort, setNewCohort] = useState(cohort);
   const [motif, setMotif] = useState("");
@@ -160,62 +161,97 @@ function ChangeCohortModal({ isOpen, young, cohort, close }) {
     "Autre",
   ];
 
-  return (
-    <ModalConfirm
-      size="lg"
-      isOpen={isOpen}
-      title="Changement de cohorte"
-      message={
-        <>
-          Vous êtes sur le point de changer la cohorte de {young.firstName} {young.lastName}. <br />
-        </>
-      }
-      onCancel={close}
-      onConfirm={() => {
-        close();
-        setModalConfirmWithMessage(true);
-      }}
-      disableConfirm={!motif}
-      showHeaderIcon={true}
-      showHeaderText={false}>
-      <>
-        <div style={{ display: "grid", marginBlock: "20px", gridTemplateColumns: "1fr 375px", gridGap: "20px", alignItems: "center", justifyItems: "left", minWidth: "75%" }}>
-          <p style={{ margin: 0 }}>Précisez le motif de changement de séjour :</p>
+  async function handleChangeCohort(message) {
+    try {
+      await api.put(`/referent/young/${young._id}/change-cohort`, { cohort: newCohort, message, cohortChangeReason: motif });
+      await onChange();
+      toastr.success("Cohorte modifiée avec succès");
+      setModalConfirmWithMessage(false);
+    } catch (e) {
+      return toastr.error("Oups, une erreur est survenue pendant le changement de cohorte du volontaire :", translate(e.code));
+    }
+  }
 
-          <div className="text-[#9a9a9a] bg-[#ffffff] w-[375px]">
-            <UncontrolledDropdown setActiveFromChild>
-              <DropdownToggle tag="button">
-                {motif || <p></p>}
-                <Chevron color="#9a9a9a" style={{ padding: 0, margin: 0, marginLeft: "15px" }} />
-              </DropdownToggle>
-              <DropdownMenu>
-                {motifs
-                  .filter((e) => e !== motif)
-                  .map((status) => {
-                    return (
-                      <DropdownItem key={status} className="dropdown-item" onClick={() => setMotif(status)}>
-                        {status}
-                      </DropdownItem>
-                    );
-                  })}
-              </DropdownMenu>
-            </UncontrolledDropdown>
+  return (
+    <>
+      <ModalConfirm
+        size="lg"
+        isOpen={isOpen}
+        title="Changement de cohorte"
+        message={
+          <>
+            Vous êtes sur le point de changer la cohorte de {young.firstName} {young.lastName}. <br />
+          </>
+        }
+        onCancel={close}
+        onConfirm={() => {
+          close();
+          setModalConfirmWithMessage(true);
+        }}
+        disableConfirm={!motif}
+        showHeaderIcon={true}
+        showHeaderText={false}>
+        <>
+          <div style={{ display: "grid", marginBlock: "20px", gridTemplateColumns: "1fr 375px", gridGap: "20px", alignItems: "center", justifyItems: "left", minWidth: "75%" }}>
+            <p style={{ margin: 0 }}>Précisez le motif de changement de séjour :</p>
+
+            <div className="text-[#9a9a9a] bg-[#ffffff] w-[375px]">
+              <UncontrolledDropdown setActiveFromChild>
+                <DropdownToggle tag="button">
+                  <div className="border-[#D1D5DB] border-[1px] rounded-[100px] bg-white flex items-center justify-between w-[375px] py-[4px] px-[15px]">
+                    {motif || <p></p>}
+                    <Chevron color="#9a9a9a" style={{ padding: 0, margin: 0, marginLeft: "15px" }} />
+                  </div>
+                </DropdownToggle>
+                <DropdownMenu>
+                  {motifs
+                    .filter((e) => e !== motif)
+                    .map((status) => {
+                      return (
+                        <DropdownItem key={status} className="dropdown-item" onClick={() => setMotif(status)}>
+                          {status}
+                        </DropdownItem>
+                      );
+                    })}
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </div>
+            <p style={{ margin: 0 }}>Choix de la nouvelle cohorte :</p>
+            <CohortDropDown cohort={newCohort} onClick={(value) => setNewCohort(value)} width="375px" />
           </div>
-          <p style={{ margin: 0 }}>Choix de la nouvelle cohorte :</p>
-          <CohortDropDown cohort={newCohort} onClick={(value) => setNewCohort(value)} width="375px" />
-        </div>
-        <p style={{ margin: 0, marginTop: "16px" }}>
-          Veuillez vous assurer de son éligibilité , pour en savoir plus consulter{" "}
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href=" https://support.snu.gouv.fr/base-de-connaissance/suis-je-eligible-a-un-sejour-de-cohesion-en-2022-1"
-            style={{ color: "#5145cc" }}>
-            l’article de la base de connaissance
-          </a>
-        </p>
-      </>
-    </ModalConfirm>
+          <p style={{ margin: 0, marginTop: "16px" }}>
+            Veuillez vous assurer de son éligibilité , pour en savoir plus consulter{" "}
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href=" https://support.snu.gouv.fr/base-de-connaissance/suis-je-eligible-a-un-sejour-de-cohesion-en-2022-1"
+              style={{ color: "#5145cc" }}>
+              l’article de la base de connaissance
+            </a>
+          </p>
+        </>
+      </ModalConfirm>
+      <ModalConfirmWithMessage
+        isOpen={modalConfirmWithMessage}
+        title="Veuillez éditer le message ci-dessous pour préciser le motif de changement de cohorte avant de l’envoyer"
+        message={
+          <>
+            Bonjour {young.firstName} {young.lastName},<br />
+            Votre changement de séjour pour le Service National Universel a été pris en compte. <br />
+            Ancien séjour : <Badge color="#aaaaaa" backgroundColor="#F9FCFF" text={young.cohort} style={{ cursor: "default" }} />
+            <br />
+            Nouveau séjour : <Badge color="#0C7CFF" backgroundColor="#F9FCFF" text={translateCohort(newCohort)} style={{ cursor: "default" }} />
+          </>
+        }
+        onChange={() => setModalConfirmWithMessage(false)}
+        onConfirm={handleChangeCohort}
+        endMessage={
+          <p>
+            Cordialement <br /> Les équipes du Service National Universel
+          </p>
+        }
+      />
+    </>
   );
 }
 
@@ -225,14 +261,14 @@ function CohortDropDown({ originalCohort, cohort, onClick, width }) {
   const disabled = ![ROLES.ADMIN, ROLES.REFERENT_DEPARTMENT, ROLES.REFERENT_REGION].includes(user.role);
 
   return (
-    <div className={`text-[#0C7CFF] bg-[#F9FCFF] -w[${width}]`}>
+    <div className={`text-[#0C7CFF] bg-[#F9FCFF] w-[${width}] border-[#0C7CFF] border-[1px] rounded-[100px]`}>
       <UncontrolledDropdown setActiveFromChild>
         <DropdownToggle tag="button" disabled={disabled}>
-          <div className="flex items-center gap-1">
+          <div className={`flex items-center gap-1 justify-between w-[${width}] py-[4px] px-[15px]`}>
             {originalCohort ? <IconChangementCohorte /> : null}
             {cohort}
+            {!disabled ? <Chevron color="#0C7CFF" style={{ padding: 0, margin: 0, marginLeft: "15px" }} /> : null}
           </div>
-          {!disabled ? <Chevron color="#0C7CFF" style={{ padding: 0, margin: 0, marginLeft: "15px" }} /> : null}
         </DropdownToggle>
         <DropdownMenu>
           {options
