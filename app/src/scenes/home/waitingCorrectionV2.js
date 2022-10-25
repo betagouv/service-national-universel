2c0e7e661d9d1ed6e5c3c52f9d29c3dcb2c38bb2
hlecourt <hlecourt99@gmail.com>
2022-10-25 10:04:27 +0200

    up

--- app/src/scenes/home/waitingCorrectionV2.js
+++ app/src/scenes/home/waitingCorrectionV2.js
@@ -23,26 +23,29 @@ export default function WaitingCorrectionV2() {
               <div className="text-[#738297] text-sm mt-2">Merci d’effectuer les modifications demandées par votre référent :</div>
               <div className="flex flex-col gap-5 mt-3 overflow-auto max-h-[250px]">
                 <hr className="border-gray-200" />
-                {young.correctionRequests.map((correction, i) => (
-                  <>
-                    <div key={i} className="flex items-center justify-between gap-4 pr-2">
-                      <div className="flex flex-col gap-1">
-                        <div className="flex items-center gap-2">
-                          <AiOutlineExclamationCircle className="text-red-500 font-bold" />
-                          <div className="text-red-500 font-bold text-sm">{translateField(correction.field)}</div>
+                {young.correctionRequests.map((correction, i) => {
+                  if (!["SENT", "REMINDED"].includes(correction.status)) return null;
+                  return (
+                    <>
+                      <div key={i} className="flex items-center justify-between gap-4 pr-2">
+                        <div className="flex flex-col gap-1">
+                          <div className="flex items-center gap-2">
+                            <AiOutlineExclamationCircle className="text-red-500 font-bold" />
+                            <div className="text-red-500 font-bold text-sm">{translateField(correction.field)}</div>
+                          </div>
+                          {correction?.reason ? <div className="text-gray-900 text-base font-medium">Motif : {translateCorrectionReason(correction.reason)}</div> : null}
+                          <div className="text-sm text-gray-600 font-normal">{correction.message}</div>
                         </div>
-                        {correction?.reason ? <div className="text-gray-900 text-base font-medium">Motif : {translateCorrectionReason(correction.reason)}</div> : null}
-                        <div className="text-sm text-gray-600 font-normal">{correction.message}</div>
+                        <button
+                          className="text-blue-600 font-medium border-[1px] border-blue-600 px-2 py-2 text-sm hover:text-white hover:bg-blue-600 rounded-lg"
+                          onClick={() => history.push(redirectToCorrection(correction.field))}>
+                          Corriger
+                        </button>
                       </div>
-                      <button
-                        className="text-blue-600 font-medium border-[1px] border-blue-600 px-2 py-2 text-sm hover:text-white hover:bg-blue-600 rounded-lg"
-                        onClick={() => history.push(redirectToCorrection(correction.field))}>
-                        Corriger
-                      </button>
-                    </div>
-                    <hr className="border-gray-200" />
-                  </>
-                ))}
+                      <hr className="border-gray-200" />
+                    </>
+                  );
+                })}
               </div>
             </div>
             <img className="w-1/2 object-fill" src={require("../../assets/homePhase2Desktop.png")} />
@@ -60,26 +63,29 @@ export default function WaitingCorrectionV2() {
             <div className="text-[#738297] text-sm mt-2">Merci d’effectuer les modifications demandées par votre référent :</div>
             <div className="flex flex-col gap-5 mt-3">
               <hr className="border-gray-200" />
-              {young.correctionRequests.map((correction, i) => (
-                <>
-                  <div key={i} className="flex flex-col gap-4 ">
-                    <div className="flex flex-col gap-1">
-                      <div className="flex items-center gap-2">
-                        <AiOutlineExclamationCircle className="text-red-500 font-bold" />
-                        <div className="text-red-500 font-bold text-sm">{translateField(correction.field)}</div>
+              {young.correctionRequests.map((correction, i) => {
+                if (!["SENT", "REMINDED"].includes(correction.status)) return null;
+                return (
+                  <>
+                    <div key={i} className="flex flex-col gap-4 ">
+                      <div className="flex flex-col gap-1">
+                        <div className="flex items-center gap-2">
+                          <AiOutlineExclamationCircle className="text-red-500 font-bold" />
+                          <div className="text-red-500 font-bold text-sm">{translateField(correction.field)}</div>
+                        </div>
+                        {correction?.reason ? <div className="text-gray-900 text-base font-medium">Motif : {translateCorrectionReason(correction.reason)}</div> : null}
+                        <div className="text-sm text-gray-600 font-normal">{correction.message}</div>
                       </div>
-                      {correction?.reason ? <div className="text-gray-900 text-base font-medium">Motif : {translateCorrectionReason(correction.reason)}</div> : null}
-                      <div className="text-sm text-gray-600 font-normal">{correction.message}</div>
+                      <button
+                        className="text-blue-600 font-medium border-[1px] border-blue-600 px-2 py-2 text-sm hover:text-white hover:bg-blue-600 rounded-lg"
+                        onClick={() => history.push(redirectToCorrection(correction.field))}>
+                        Corriger
+                      </button>
                     </div>
-                    <button
-                      className="text-blue-600 font-medium border-[1px] border-blue-600 px-2 py-2 text-sm hover:text-white hover:bg-blue-600 rounded-lg"
-                      onClick={() => history.push(redirectToCorrection(correction.field))}>
-                      Corriger
-                    </button>
-                  </div>
-                  <hr className="border-gray-200" />
-                </>
-              ))}
+                    <hr className="border-gray-200" />
+                  </>
+                );
+              })}
             </div>
           </div>
           <img src={require("../../assets/homePhase2Mobile.png")} />
