9516461e9d9bc495dea6854055a34d132bdea637
hlecourt <hlecourt99@gmail.com>
2022-11-02 12:04:30 +0100

    up

--- admin/src/scenes/tableDeRepartition/Region.js
+++ admin/src/scenes/tableDeRepartition/Region.js
@@ -2,10 +2,10 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { region2department, regionList } from "snu-lib";
 import api from "../../services/api";
+import { FiChevronDown } from "react-icons/fi";
 
 export default function tableDeRepartition() {
   const { region, cohort } = useParams();
-  const [viewDepartement, setViewDepartment] = React.useState({ open: false, region: "" });
   const [selected, setSelected] = React.useState({});
   const getTable = async () => {
     const { ok, data } = await api.get(`/table-de-repartition/from-region/${region}/cohort/${cohort}`);
@@ -74,38 +74,55 @@ export default function tableDeRepartition() {
         {regionList
           .filter((r) => r !== region)
           .map((r, index) => (
-            <div key={index} className="bg-white rounded-xl p-4">
-              <div
-                className="flex items-center justify-between w-full cursor-pointer font-bold"
-                onClick={() => setViewDepartment({ open: r !== viewDepartement.region ? true : !viewDepartement.open, region: r })}>
-                {r}
-                <div onClick={() => onSelectRegion(r)}>
-                  <div className={`relative w-[36px] h-[16px] rounded-[36px] ${Object.keys(selected).includes(r) ? "bg-[#10B981]" : "bg-[#EF4444]"}`}>
-                    <div
-                      className={`w-[20px] h-[20px] bg-[#FFFFFF] rounded-[20px] border-[#E5E7EB] border-[1px] absolute top-[-2px] shadow-[0_1px_3px_rgb(0,0,0,0.1),0_1px_2px_rgb(0,0,0,0.06)] ${
-                        Object.keys(selected).includes(r) ? "right-[-2px]" : "left-[-2px]"
-                      }`}
-                    />
-                  </div>
-                </div>
-              </div>
-              {viewDepartement.open && viewDepartement.region === r ? (
-                <div className="mt-4">
-                  <hr className="my-2" />
-                  {region2department[r].map((d, index) => (
-                    <div key={index}>
-                      <div className="flex items-cneter justify-between w-full  py-2 bg-white rounded-xl cursor-pointer">
-                        {d}
-                        <input type="checkbox" checked={selected[r]?.includes(d) || false} onChange={() => onSelected(r, d)} />
-                      </div>
-                      <hr className="my-2" />
-                    </div>
-                  ))}
-                </div>
-              ) : null}
-            </div>
+            <Region key={index} onSelectRegion={onSelectRegion} onSelected={onSelected} selected={selected} region={r} />
           ))}
       </div>
     </div>
   );
 }
+
+const Region = ({ onSelectRegion, onSelected, selected, region }) => {
+  const [open, setOpen] = React.useState(false);
+  return (
+    <div className="bg-white rounded-xl p-4">
+      <div className="flex items-center justify-between w-full cursor-pointer font-bold" onClick={() => setOpen(!open)}>
+        <div className="flex items-center gap-4">
+          <div onClick={() => onSelectRegion(region)}>
+            <div className={`relative w-[36px] h-[16px] rounded-[36px] ${Object.keys(selected).includes(region) ? "bg-[#10B981]" : "bg-[#EF4444]"}`}>
+              <div
+                className={`w-[20px] h-[20px] bg-[#FFFFFF] rounded-[20px] border-[#E5E7EB] border-[1px] absolute top-[-2px] shadow-[0_1px_3px_rgb(0,0,0,0.1),0_1px_2px_rgb(0,0,0,0.06)] ${
+                  Object.keys(selected).includes(region) ? "right-[-2px]" : "left-[-2px]"
+                }`}
+              />
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <div className="">{region}</div>
+            {selected[region]?.length > 0 && <div className="text-gray-500">{selected[region]?.reduce((prev, d) => `${prev}, ${d}`)}</div>}
+          </div>
+        </div>
+        <FiChevronDown className={`text-gray-500 h-6 w-6 transform ${open ? "rotate-180" : ""}`} />
+      </div>
+      {open ? (
+        <div className="mt-4">
+          <hr className="my-2" />
+          {region2department[region].map((department, index) => (
+            <Department key={index} onSelected={onSelected} selected={selected} region={region} department={department} />
+          ))}
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+const Department = ({ onSelected, selected, region, department }) => {
+  return (
+    <>
+      <div className="flex items-cneter justify-between w-full  py-2 bg-white rounded-xl cursor-pointer">
+        {department}
+        <input type="checkbox" checked={selected[region]?.includes(department) || false} onChange={() => onSelected(region, department)} />
+      </div>
+      <hr className="my-2" />
+    </>
+  );
+};
