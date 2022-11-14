99bd9753f6602ae8be34d9c3c0b9b5521675e247
guiTheret <gtheret@sfr.fr>
2022-11-14 12:44:04 +0100

    invitation mobil/desktop

--- app/src/scenes/authV2/desktop/signupInvite.js
+++ app/src/scenes/authV2/desktop/signupInvite.js
@@ -1,5 +1,101 @@
+import queryString from "query-string";
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toastr } from "react-redux-toastr";
+import { useHistory } from "react-router-dom";
+import { formatToActualTime } from "snu-lib/date";
+import Eye from "../../../assets/icons/Eye";
+import EyeOff from "../../../assets/icons/EyeOff";
+import RightArrow from "../../../assets/icons/RightArrow";
+import Input from "../../../components/inscription/input";
+import { setYoung } from "../../../redux/auth/actions";
+import api from "../../../services/api";
+import Error from "../../../components/error";
 
 export default function Signin() {
-  return <div>Signin</div>;
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [showPassword, setShowPassword] = React.useState(false);
+  const [disabled, setDisabled] = React.useState(true);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState({});
+  const history = useHistory();
+
+  const dispatch = useDispatch();
+  const young = useSelector((state) => state.Auth.young);
+
+  const params = queryString.parse(location.search);
+  const { redirect, disconnected } = params;
+
+  if (disconnected === "1") toastr.error("Votre session a expiré", "Merci de vous reconnecter.", { timeOut: 10000 });
+
+  React.useEffect(() => {
+    if (young) history.push("/" + (redirect || ""));
+  }, [young]);
+
+  const onSubmit = async () => {
+    if (loading || disabled) return;
+    setLoading(true);
+    try {
+      const urlParams = new URLSearchParams(window.location.search);
+      const invitationToken = urlParams.get("token");
+      const { data: young, token } = await api.post(`/young/signup_invite`, { email, password, invitationToken: invitationToken });
+      if (young) {
+        if (redirect?.startsWith("http")) return (window.location.href = redirect);
+        if (token) api.setToken(token);
+        dispatch(setYoung(young));
+      }
+    } catch (e) {
+      console.log(e);
+      if (e.code === "PASSWORD_NOT_VALIDATED") {
+        setError({ text: "Votre mot de passe doit contenir au moins 12 caractères, dont une majuscule, une minuscule, un chiffre et un symbole" });
+      } else if (e.code === "TOO_MANY_REQUESTS") {
+        let date = formatToActualTime(e?.data?.nextLoginAttemptIn);
+        setError({ text: "Vous avez atteint le maximum de tentatives de connexion autorisées.", subText: `Réessayez ${date !== "-" ? `à ${date}.` : "demain."}` });
+      } else if (e.code === "YOUNG_ALREADY_REGISTERED") {
+        setError({ text: "Vous êtes déjà inscrit" });
+      } else if (!e.ok) {
+        setError({ text: "Une erreur est survenue. Essayez de rafraîchir la page" });
+      }
+      setPassword("");
+    }
+    setLoading(false);
+  };
+
+  React.useEffect(() => {
+    if (email && password) setDisabled(false);
+    else setDisabled(true);
+  }, [email, password]);
+
+  return (
+    <div className="bg-[#F9F6F2] py-6 flex">
+      <div className="bg-white basis-[50%] mx-auto my-0 px-[102px] py-[60px]">
+        {Object.keys(error).length > 0 && <Error {...error} onClose={() => setError({})} />}
+        <div className="text-[#161616] text-[32px] font-bold mb-1">Activer mon compte</div>
+        <div className="flex items-center mb-2 gap-4">
+          <RightArrow />
+          <div className="text-[#161616] text-[21px] font-bold">Mon espace volontaire</div>
+        </div>
+        <div className="flex flex-col gap-1 py-1 mb-1">
+          <label className="text-[#161616] text-base">E-mail</label>
+          <Input value={email} onChange={(e) => setEmail(e)} />
+        </div>
+        <div className="flex flex-col gap-1 pb-4">
+          <label className="text-[#161616] text-base">Mot de passe</label>
+          <div className="flex items-center w-full bg-[#EEEEEE] px-4 py-2 border-b-[2px] border-[#3A3A3A] rounded-t-[4px]">
+            <input className="w-full bg-inherit" type={showPassword ? "text" : "password"} value={password} onChange={(e) => setPassword(e.target.value)} />
+            {showPassword ? <EyeOff className="cursor-pointer" onClick={() => setShowPassword(false)} /> : <Eye className="cursor-pointer" onClick={() => setShowPassword(true)} />}
+          </div>
+        </div>
+        <div className="w-full flex justify-end">
+          <button
+            disabled={disabled || loading}
+            className="flex items-center justify-center px-3 py-2 cursor-pointer bg-[#000091] text-white hover:!text-[#000091] hover:bg-white hover:border hover:border-[#000091]  disabled:bg-[#E5E5E5] disabled:!text-[#929292] disabled:border-0 disabled:cursor-default"
+            onClick={onSubmit}>
+            Connexion
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
