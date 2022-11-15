837cdcfd37a187d5da78e20ae79b0861d3a685a4
Seb Doncker <seb@dingorock.fr>
2022-11-15 09:44:25 +0100

    Structure PDT et WIP Schema de répartition (#2045)

--- admin/src/scenes/plan-transport/components/commons.js
+++ admin/src/scenes/plan-transport/components/commons.js
@@ -7,3 +7,9 @@ export function Title({ children, className = "" }) {
 export function SubTitle({ children, className = "" }) {
   return <div className={`text-sm font-normal text-gray-800 leading-[14px] ${className}`}>{children}</div>;
 }
+
+export function Box({ children, className = "" }) {
+  return (
+    <div className={`bg-[#FFFFFF] rounded-[8px] shadow-[0px_2px_4px_rgba(0,0,0,0.05)] text-[14px] leading-[20px] text-[#1F2937] px-[28px] py-[24px] ${className}`}>{children}</div>
+  );
+}
