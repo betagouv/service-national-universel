07b5e9efbdbfa29f1f49bcf2a4a852ded921db10
hlecourt <hlecourt99@gmail.com>
2022-11-15 11:21:49 +0100

    chore(admin): up common plan de transport

--- admin/src/scenes/plan-transport/components/commons.js
+++ admin/src/scenes/plan-transport/components/commons.js
@@ -13,3 +13,39 @@ export function Box({ children, className = "" }) {
     <div className={`bg-[#FFFFFF] rounded-[8px] shadow-[0px_2px_4px_rgba(0,0,0,0.05)] text-[14px] leading-[20px] text-[#1F2937] px-[28px] py-[24px] ${className}`}>{children}</div>
   );
 }
+
+export function Loading({ width }) {
+  return (
+    <div className={`animate-pulse flex space-x-4 ${width}`}>
+      <div className="flex-1 space-y-6">
+        <div className="grid grid-cols-3 gap-4 ">
+          <div className="h-2 bg-gray-300 rounded col-span-2"></div>
+          <div className="h-2 bg-gray-300 rounded col-span-1"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export const regionList = [
+  "Auvergne-Rhône-Alpes",
+  "Bourgogne-Franche-Comté",
+  "Bretagne",
+  "Centre-Val de Loire",
+  "Corse",
+  "Grand Est",
+  "Hauts-de-France",
+  "Île-de-France",
+  "Normandie",
+  "Nouvelle-Aquitaine",
+  "Occitanie",
+  "Pays de la Loire",
+  "Provence-Alpes-Côte d'Azur",
+  "Guadeloupe",
+  "Martinique",
+  "Guyane",
+  "La Réunion",
+  "Mayotte",
+  "Polynésie française",
+  "Nouvelle-Calédonie",
+];
