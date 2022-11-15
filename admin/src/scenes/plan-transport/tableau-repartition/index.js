a5253464bc513936643a2d04d67ac2389c3ac399
hlecourt <hlecourt99@gmail.com>
2022-11-15 14:38:07 +0100

    up

--- admin/src/scenes/plan-transport/tableau-repartition/index.js
+++ admin/src/scenes/plan-transport/tableau-repartition/index.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { toastr } from "react-redux-toastr";
 import { ES_NO_LIMIT } from "snu-lib";
+import Pencil from "../../../assets/icons/Pencil";
 import Breadcrumbs from "../../../components/Breadcrumbs";
+import { capture } from "../../../sentry";
 import API from "../../../services/api";
-import { Loading, SubTitle, Title, regionList } from "../components/commons";
+import { Loading, regionList, SubTitle, Title } from "../components/commons";
 
 export default function TableauRepartition() {
   const [cohort, setCohort] = React.useState("Juillet 2022");
@@ -12,64 +15,111 @@ export default function TableauRepartition() {
   const [loadingQuery, setLoadingQuery] = React.useState(false);
   const [searchRegion, setSearchRegion] = React.useState("");
   const [regions, setRegions] = React.useState(regionList);
+  const [data, setData] = React.useState([]);
+
+  const getRepartitionRegion = async () => {
+    try {
+      const { data, ok } = await API.get(`/table-de-repartition/region/${cohort}`);
+      if (!ok) return toastr.error("Oups, une erreur est survenue lors de la récupération des données");
+      setData(data);
+    } catch (e) {
+      capture(e);
+      toastr.error("Oups, une erreur est survenue lors de la récupération des données");
+    }
+  };
 
   React.useEffect(() => {
     (async () => {
-      setLoadingQuery(true);
-      //get youngs by region
-      const bodyYoung = {
-        query: { bool: { must: { match_all: {} }, filter: [{ term: { "cohort.keyword": cohort } }, { terms: { "status.keyword": ["VALIDATED"] } }] } },
-        aggs: {
-          region: { terms: { field: "region.keyword" } },
-        },
-        track_total_hits: true,
-        size: 0,
-      };
+      try {
+        setLoadingQuery(true);
 
-      const { responses } = await API.esQuery("young", bodyYoung);
-      setYoungsByRegion(responses[0].aggregations.region.buckets);
+        await getRepartitionRegion();
 
-      //get places center by region
-      const bodyCohesionCenter = {
-        query: { bool: { must: { match_all: {} }, filter: [{ terms: { "cohorts.keyword": [cohort] } }] } },
-        track_total_hits: true,
-        size: ES_NO_LIMIT,
-      };
+        //get youngs by region
+        const bodyYoung = {
+          query: { bool: { must: { match_all: {} }, filter: [{ term: { "cohort.keyword": cohort } }, { terms: { "status.keyword": ["VALIDATED"] } }] } },
+          aggs: {
+            region: { terms: { field: "region.keyword" } },
+          },
+          track_total_hits: true,
+          size: 0,
+        };
 
-      const { responses: responsesCenter } = await API.esQuery("cohesioncenter", bodyCohesionCenter);
-      const centerDetail = responsesCenter[0].hits.hits.map((e) => new Object({ ...e._source, _id: e._id }));
+        const { responses } = await API.esQuery("young", bodyYoung);
+        setYoungsByRegion(responses[0].aggregations.region.buckets);
 
-      const bodySession = {
-        query: { bool: { must: { match_all: {} }, filter: [{ terms: { "cohesionCenterId.keyword": centerDetail.map((c) => c._id) } }, { term: { "cohort.keyword": cohort } }] } },
-        track_total_hits: true,
-        size: ES_NO_LIMIT,
-      };
+        //get places center by region
+        const bodyCohesionCenter = {
+          query: { bool: { must: { match_all: {} }, filter: [{ terms: { "cohorts.keyword": [cohort] } }] } },
+          track_total_hits: true,
+          size: ES_NO_LIMIT,
+        };
 
-      const { responses: responsesSession } = await API.esQuery("sessionphase1", bodySession);
-      const sessionsDetail = responsesSession[0].hits.hits.map((e) => e._source);
+        const { responses: responsesCenter } = await API.esQuery("cohesioncenter", bodyCohesionCenter);
+        const centerDetail = responsesCenter[0].hits.hits.map((e) => new Object({ ...e._source, _id: e._id }));
 
-      const sessionPlacesBycohesionCenterRegion = sessionsDetail.reduce((acc, session) => {
-        const region = centerDetail.find((c) => c._id === session.cohesionCenterId).region;
-        if (!acc[region]) acc[region] = 0;
-        acc[region] += session.placesTotal;
-        return acc;
-      }, {});
+        const bodySession = {
+          query: { bool: { must: { match_all: {} }, filter: [{ terms: { "cohesionCenterId.keyword": centerDetail.map((c) => c._id) } }, { term: { "cohort.keyword": cohort } }] } },
+          track_total_hits: true,
+          size: ES_NO_LIMIT,
+        };
 
-      setPlacesCenterByRegion(sessionPlacesBycohesionCenterRegion);
-      setLoadingQuery(false);
+        const { responses: responsesSession } = await API.esQuery("sessionphase1", bodySession);
+        const sessionsDetail = responsesSession[0].hits.hits.map((e) => e._source);
+
+        const sessionPlacesBycohesionCenterRegion = sessionsDetail.reduce((acc, session) => {
+          const region = centerDetail.find((c) => c._id === session.cohesionCenterId).region;
+          if (!acc[region]) acc[region] = 0;
+          acc[region] += session.placesTotal;
+          return acc;
+        }, {});
+
+        setPlacesCenterByRegion(sessionPlacesBycohesionCenterRegion);
+        setLoadingQuery(false);
+      } catch (e) {
+        capture(e);
+        toastr.error("Oups, une erreur est survenue lors de la récupération des données");
+      }
     })();
   }, []);
 
   React.useEffect(() => {
     let regions = regionList.filter((e) => {
       if (searchRegion === "") return true;
-      return e.toLowerCase().includes(searchRegion);
+      return e.toLowerCase().includes(searchRegion.toLowerCase());
     });
     setRegions(regions);
   }, [searchRegion]);
 
-  const onChange = async (fromRegion, toRegion) => {
-    console.log(fromRegion, toRegion);
+  const onCreate = async (fromRegion, toRegion) => {
+    try {
+      //update real time
+      setData([...data, { fromRegion, toRegion }]);
+      const { ok } = await API.post(`/table-de-repartition/region`, { fromRegion, toRegion, cohort });
+      if (!ok) return toastr.error("Oups, une erreur est survenue lors de l'ajout de la region d'accueil");
+      await getRepartitionRegion();
+    } catch (e) {
+      capture(e);
+      console.log(e);
+      toastr.error("Oups, une erreur est survenue lors de l'ajout de la region d'accueil");
+      await getRepartitionRegion();
+    }
+  };
+
+  const onDelete = async (fromRegion, toRegion) => {
+    try {
+      //update real time
+      let update = data.filter((e) => e.fromRegion !== fromRegion || e.toRegion !== toRegion);
+      setData(update);
+      const { ok } = await API.post(`/table-de-repartition/delete/region`, { fromRegion, toRegion, cohort });
+      if (!ok) return toastr.error("Oups, une erreur est survenue lors de la suppression de la region d'accueil");
+      await getRepartitionRegion();
+    } catch (e) {
+      capture(e);
+      console.log(e);
+      toastr.error("Oups, une erreur est survenue lors de la suppression de la region d'accueil");
+      await getRepartitionRegion();
+    }
   };
 
   return (
@@ -98,8 +148,8 @@ export default function TableauRepartition() {
 
           <hr />
           <div className="flex px-4 py-2 items-center">
-            <div className="w-[40%] uppercase text-[#7E858C] text-xs leading-3">Région</div>
-            <div className="w-[50%] uppercase text-[#7E858C] text-xs leading-3">Région d&apos;accueil</div>
+            <div className="w-[30%] uppercase text-[#7E858C] text-xs leading-3">Région</div>
+            <div className="w-[60%] uppercase text-[#7E858C] text-xs leading-3">Région d&apos;accueil</div>
             <div className="w-[10%] uppercase text-[#7E858C] text-xs leading-3">Avancement</div>
           </div>
 
@@ -110,7 +160,10 @@ export default function TableauRepartition() {
                 region={region}
                 youngsInRegion={youngsByRegion.find((r) => r.key === region)?.doc_count || 0}
                 placesCenterByRegion={placesCenterByRegion}
-                onChange={onChange}
+                onCreate={onCreate}
+                onDelete={onDelete}
+                data={data}
+                loadingQuery={loadingQuery}
               />
             ))
           ) : (
@@ -127,26 +180,52 @@ export default function TableauRepartition() {
   );
 }
 
-const Region = ({ region, youngsInRegion, placesCenterByRegion, loadingQuery, onChange }) => {
+const Region = ({ region, youngsInRegion, placesCenterByRegion, loadingQuery, onCreate, data, onDelete }) => {
   const [open, setOpen] = React.useState(false);
+  const [assignRegion, setAssignRegion] = React.useState([]);
+
+  React.useEffect(() => {
+    let assignRegion = data.filter((e) => e.fromRegion === region) || [];
+    setAssignRegion(assignRegion);
+  }, [data]);
 
   return (
     <>
       <hr />
       <div className="flex px-4 py-2 items-center">
-        <div className="w-[40%] flex flex-col gap-1">
+        <div className="w-[30%] flex flex-col gap-1">
           <div className="text-base text-[#242526] font-bold leading-6">{region}</div>
           <div className="flex text-xs text-gray-800 leading-4 items-center">{loadingQuery ? <Loading width="w-1/3" /> : `${youngsInRegion} volontaires`}</div>
         </div>
-        <div className="w-[50%]">
+        <div className="w-[60%]">
           {loadingQuery ? (
             <Loading width="w-1/3" />
           ) : (
             <div className="relative flex flex-row gap-2 items-center">
-              <button className="px-2 py-1 cursor-pointer text-white text-xs leading-5 rounded-full bg-blue-600 hover:scale-105" onClick={() => setOpen(!open)}>
-                À assigner
-              </button>
-              {open ? <SelectHostRegion region={region} setOpen={setOpen} placesCenterByRegion={placesCenterByRegion} onChange={onChange} /> : null}
+              {assignRegion.map((assign) => (
+                <div key={assign._id} className="text-xs text-gray-700 bg-gray-100 rounded-full p-2">
+                  {assign.toRegion}
+                </div>
+              ))}
+              {assignRegion.length === 0 ? (
+                <button className="px-2 py-1 cursor-pointer text-white text-xs leading-5 rounded-full bg-blue-600 hover:scale-105" onClick={() => setOpen(!open)}>
+                  À assigner
+                </button>
+              ) : (
+                <div className="flex items-center p-2 rounded-full bg-blue-600 cursor-pointer hover:scale-105" onClick={() => setOpen(!open)}>
+                  <Pencil className="w-4 h-4 text-white" />
+                </div>
+              )}
+              {open ? (
+                <SelectHostRegion
+                  region={region}
+                  setOpen={setOpen}
+                  placesCenterByRegion={placesCenterByRegion}
+                  onCreate={onCreate}
+                  assignRegion={assignRegion}
+                  onDelete={onDelete}
+                />
+              ) : null}
             </div>
           )}
         </div>
@@ -156,7 +235,7 @@ const Region = ({ region, youngsInRegion, placesCenterByRegion, loadingQuery, on
   );
 };
 
-const SelectHostRegion = ({ region, placesCenterByRegion, setOpen, onChange }) => {
+const SelectHostRegion = ({ region, placesCenterByRegion, setOpen, onCreate, assignRegion, onDelete }) => {
   const ref = React.useRef(null);
 
   React.useEffect(() => {
@@ -171,19 +250,26 @@ const SelectHostRegion = ({ region, placesCenterByRegion, setOpen, onChange }) =
     };
   }, []);
 
+  const onChange = (fromRegion, toRegion) => {
+    if (assignRegion.filter((e) => e.toRegion === toRegion)?.length !== 0) onDelete(fromRegion, toRegion);
+    else if (assignRegion.length > 2) return toastr.info("Vous ne pouvez pas assigner plus de 3 régions d'accueil");
+    else onCreate(fromRegion, toRegion);
+  };
   return (
     <div ref={ref} className="absolute z-50 flex flex-col bg-white top-[110%] left-[0px] shadow-ninaButton rounded-lg w-[90%] py-2">
       {regionList
         .filter((e) => e !== region)
-        .map((r, i) => (
-          <div key={i} className="flex flex-row items-center justify-between px-3 py-2 cursor-pointer hover:bg-gray-100" onClick={() => onChange(region, r)}>
-            <div className="flex items-center gap-2 text-gray-700 text-sm">
-              <input type={"checkbox"} />
-              {r}
+        .map((r, i) => {
+          return (
+            <div key={r} className="flex flex-row items-center justify-between px-3 py-2 cursor-pointer hover:bg-gray-100" onClick={() => onChange(region, r)}>
+              <div className="flex items-center gap-2 text-gray-700 text-sm">
+                <input type={"checkbox"} checked={assignRegion.find((e) => e.toRegion === r) || false} />
+                {r}
+              </div>
+              <div className="text-sm text-gray-500 uppercase">{placesCenterByRegion[r] ? placesCenterByRegion[r] : 0} places</div>
             </div>
-            <div className="text-sm text-gray-500 uppercase">{placesCenterByRegion[r] ? placesCenterByRegion[r] : 0} places</div>
-          </div>
-        ))}
+          );
+        })}
     </div>
   );
 };
