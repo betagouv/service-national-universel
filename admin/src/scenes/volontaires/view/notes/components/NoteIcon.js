74990b7c0668caae824bf1984cdf63a5d7236236
szilagyikinga <szilagyikinga42@gmail.com>
2022-11-21 15:45:24 +0100

    feat(admin/api): Note interne (#2068)

---
+++ admin/src/scenes/volontaires/view/notes/components/NoteIcon.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactTooltip from "react-tooltip";
+
+import note from "../../../../../assets/note.svg";
+
+const NoteIcon = ({ onClick, className, id = "info" }) => {
+  return (
+    <div>
+      <ReactTooltip id={id} className="bg-white shadow-xl" arrowColor="white">
+        <div className="text-[#4B5563] cursor-pointer hover:text-[#2563EB]">Voir la note interne</div>
+      </ReactTooltip>
+      <img
+        onClick={(e) => {
+          e.stopPropagation();
+          onClick();
+        }}
+        data-tip
+        data-for={id}
+        className={`cursor-pointer ${className}`}
+        src={note}
+        alt="icon button"
+      />
+    </div>
+  );
+};
+
+export default NoteIcon;
--- api/src/controllers/young/index.js
+++ api/src/controllers/young/index.js
@@ -1127,5 +1127,6 @@ router.use("/:id/phase1", require("./phase1"));
 router.use("/:id/phase2", require("./phase2"));
 router.use("/reinscription", require("./reinscription"));
 router.use("/inscription2023", require("./inscription2023"));
+router.use("/note", require("./note"));
 
 module.exports = router;
