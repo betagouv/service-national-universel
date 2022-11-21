1ae97d19364815894eb10e8a1b7f307bf848e5ef
Szilagyi Kinga <szilagyikinga42@gmail.com>
2022-11-21 15:38:05 +0100

    short tooltip hide delay

--- admin/src/scenes/volontaires/view/notes/components/NoteIcon.js
+++ admin/src/scenes/volontaires/view/notes/components/NoteIcon.js
@@ -6,7 +6,7 @@ import note from "../../../../../assets/note.svg";
 const NoteIcon = ({ onClick, className, id = "info" }) => {
   return (
     <div>
-      <ReactTooltip delayHide={3000} clickable={true} id={id} className="bg-white shadow-xl" arrowColor="white">
+      <ReactTooltip delayHide={1000} clickable={true} id={id} className="bg-white shadow-xl" arrowColor="white">
         <div
           className="text-[#4B5563] cursor-pointer hover:text-[#2563EB]"
           onClick={(e) => {
