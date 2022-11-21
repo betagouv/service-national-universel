835c2643f6b19fb7d4ede25e8b6ba3dfc9a6616f
Szilagyi Kinga <szilagyikinga42@gmail.com>
2022-11-21 15:44:29 +0100

    clickable note icon

--- admin/src/scenes/volontaires/view/notes/components/NoteIcon.js
+++ admin/src/scenes/volontaires/view/notes/components/NoteIcon.js
@@ -6,17 +6,20 @@ import note from "../../../../../assets/note.svg";
 const NoteIcon = ({ onClick, className, id = "info" }) => {
   return (
     <div>
-      <ReactTooltip delayHide={1000} clickable={true} id={id} className="bg-white shadow-xl" arrowColor="white">
-        <div
-          className="text-[#4B5563] cursor-pointer hover:text-[#2563EB]"
-          onClick={(e) => {
-            e.stopPropagation();
-            onClick();
-          }}>
-          Voir la note interne
-        </div>
+      <ReactTooltip id={id} className="bg-white shadow-xl" arrowColor="white">
+        <div className="text-[#4B5563] cursor-pointer hover:text-[#2563EB]">Voir la note interne</div>
       </ReactTooltip>
-      <img data-tip data-for={id} className={`cursor-pointer ${className}`} src={note} alt="icon button" />
+      <img
+        onClick={(e) => {
+          e.stopPropagation();
+          onClick();
+        }}
+        data-tip
+        data-for={id}
+        className={`cursor-pointer ${className}`}
+        src={note}
+        alt="icon button"
+      />
     </div>
   );
 };
