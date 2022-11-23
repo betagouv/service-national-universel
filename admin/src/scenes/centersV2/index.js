ce9c4257977630334e396e447e74cc1cff1b1d1d
guiTheret <gtheret@sfr.fr>
2022-11-23 12:00:37 +0100

    delete files

--- admin/src/scenes/centersV2/index.js
+++ admin/src/scenes/centersV2/index.js
@@ -4,21 +4,21 @@ import useDocumentTitle from "../../hooks/useDocumentTitle";
 import { SentryRoute } from "../../sentry";
 
 import List from "./list";
-import Edit from "./edit";
-import View from "./view";
-import Youngs from "./youngs";
-import Team from "./view/team";
+//import Edit from "./edit";
+//import View from "./view";
+//import Youngs from "./youngs";
+//import Team from "./view/team";
 
 export default function CenterV2Index() {
   useDocumentTitle("CentresV2");
 
   return (
     <Switch>
-      <SentryRoute path="/centreV2/nouveau" component={Edit} />
-      <SentryRoute path="/centreV2/:id/edit" component={Edit} />
-      <SentryRoute path="/centreV2/:id/:sessionId/equipe" component={Team} />
-      <SentryRoute path="/centreV2/:id/:sessionId/:currentTab" component={Youngs} />
-      <SentryRoute path="/centreV2/:id" component={View} />
+      {/*<SentryRoute path="/centreV2/nouveau" component={Edit} />*/}
+      {/*<SentryRoute path="/centreV2/:id/edit" component={Edit} />*/}
+      {/*<SentryRoute path="/centreV2/:id/cohort/:cohortId" component={View} />*/}
+      {/*<SentryRoute path="/centreV2/:id/:sessionId/equipe" component={Team} />*/}
+      {/*<SentryRoute path="/centreV2/:id/:sessionId/:currentTab" component={Youngs} />*/}
       <SentryRoute path="/centreV2" component={List} />
     </Switch>
   );
