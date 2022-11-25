c8d1e8bce185b8cfd5bd507d6862c28322ff194a
guiTheret <gtheret@sfr.fr>
2022-11-25 12:24:21 +0100

    Update commons.js

--- admin/src/scenes/centersV2/components/commons.js
+++ admin/src/scenes/centersV2/components/commons.js
@@ -43,11 +43,3 @@ export const Badge = ({ cohort, onClick }) => {
     </div>
   );
 };
-
-export function Title({ children, className = "" }) {
-  return <div className={`text-2xl font-bold text-[#242526] leading-7 ${className}`}>{children}</div>;
-}
-
-export function SubTitle({ children, className = "" }) {
-  return <div className={`text-sm font-normal text-gray-800 leading-[14px] ${className}`}>{children}</div>;
-}
