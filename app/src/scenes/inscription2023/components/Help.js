ea8e52df0b8a5617b5409eb5e8438e701d18ce2a
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2022-12-09 03:13:35 +0100

    fix help

--- app/src/scenes/inscription2023/components/Help.js
+++ app/src/scenes/inscription2023/components/Help.js
@@ -4,28 +4,30 @@ import humanCooperation from "../../../assets/humanCooperation.svg";
 import { appURL } from "../../../config";
 
 export default function Help() {
-  <a href={`${appURL}/besoin-d-aide?from=/`} target="_blank" rel="noreferrer" className="hover:text-[#161616]">
-    <div className="bg-[#F9F6F2] px-4 pt-4 pb-12 text-[#161616] md:hidden">
-      <div className="flex justify-end">
-        <img src={humanCooperation} alt="" />
-      </div>
-      <img src={arrowRightBlue} className="mb-2" />
-      <div>
-        <p className="text-lg font-bold my-2">Besoin d&apos;aide ?</p>
-        <p className="text-sm">Consultez notre base de connaissance ou contactez notre équipe support</p>
-      </div>
-    </div>
-
-    <div className="w-[50rem] bg-white mx-auto my-8 p-4 shadow-sm hidden md:flex text-[#161616] justify-between items-center">
-      <img src={arrowRightBlue} className="w-4 h-4 md:hidden" />
-      <div className="flex items-center gap-2">
-        <img src={humanCooperation} alt="" className="w-20 h-20 hidden md:block" />
+  return (
+    <a href={`${appURL}/besoin-d-aide?from=/`} target="_blank" rel="noreferrer" className="hover:text-[#161616]">
+      <div className="bg-[#F9F6F2] px-4 pt-4 pb-12 text-[#161616] md:hidden">
+        <div className="flex justify-end">
+          <img src={humanCooperation} alt="" />
+        </div>
+        <img src={arrowRightBlue} className="mb-2" />
         <div>
-          <p className="text-lg font-bold">Besoin d&apos;aide ?</p>
+          <p className="text-lg font-bold my-2">Besoin d&apos;aide ?</p>
           <p className="text-sm">Consultez notre base de connaissance ou contactez notre équipe support</p>
         </div>
       </div>
-      <img src={arrowRightBlue} className="w-6 h-6 mr-8" />
-    </div>
-  </a>;
+
+      <div className="w-[50rem] bg-white mx-auto my-8 p-4 shadow-sm hidden md:flex text-[#161616] justify-between items-center">
+        <img src={arrowRightBlue} className="w-4 h-4 md:hidden" />
+        <div className="flex items-center gap-2">
+          <img src={humanCooperation} alt="" className="w-20 h-20 hidden md:block" />
+          <div>
+            <p className="text-lg font-bold">Besoin d&apos;aide ?</p>
+            <p className="text-sm">Consultez notre base de connaissance ou contactez notre équipe support</p>
+          </div>
+        </div>
+        <img src={arrowRightBlue} className="w-6 h-6 mr-8" />
+      </div>
+    </a>
+  );
 }
