0a2b2ffd82bf6aea4a8b9d129dada70e884562ca
Theret Guillaume <73123940+guiTheret@users.noreply.github.com>
2023-01-02 16:28:09 +0000

    Update ModalAffectation.js

--- admin/src/scenes/volontaires/components/ModalAffectation.js
+++ admin/src/scenes/volontaires/components/ModalAffectation.js
@@ -14,11 +14,17 @@ import api from "../../../services/api";
 import { getResultLabel, translate, SENDINBLUE_TEMPLATES } from "../../../utils";
 import styled from "styled-components";
 import { toastr } from "react-redux-toastr";
+import ModalTailwind from "../../../components/modals/ModalTailwind";
 
 export default function ModalAffectations({ isOpen, onCancel, young }) {
   const FILTERS = ["SEARCH"];
   const [modal, setModal] = useState({ isOpen: false, message: "", onConfirm: () => {} });
-
+  const getDefaultQuery = () => {
+    return {
+      query: { bool: { must: { match_all: {} }, filter: [{ term: { "cohort.keyword": young.cohort } }, { term: { "status.keyword": "VALIDATED" } }] } },
+      track_total_hits: true,
+    };
+  };
   const handleAffectation = async (center) => {
     try {
       const session = await api.get(`/cohesion-center/${center._id}/cohort/${center.session}/session-phase1`);
@@ -38,216 +44,116 @@ export default function ModalAffectations({ isOpen, onCancel, young }) {
   };
 
   return (
-    <Modal centered isOpen={isOpen} toggle={onCancel} size="lg">
-      <ModalContainer>
-        <CloseSvg className="close-icon hover:cursor-pointer" height={10} width={10} onClick={onCancel} />
-        <div className="p-2 text-center w-full flex flex-col items-center min-h-[520px]">
-          <div className="mb-4">
-            <h3 className="mb-3">
-              Affecter {young.firstName} {young.lastName} à un centre
-            </h3>
-            <div className="mb-4 text-gray-500">Rechercher et selectionner un centre ci-dessous :</div>
-            <ReactiveBase url={`${apiURL}/es`} app="edit-cohesioncenter" headers={{ Authorization: `JWT ${api.getToken()}` }}>
-              <div style={{ flex: 2, position: "relative" }}>
-                <Filter>
-                  <DataSearch
-                    showIcon={false}
-                    placeholder="Rechercher par nom de centre, code postal, departement..."
-                    componentId="SEARCH"
-                    dataField={["name", "city", "zip", "code", "department"]}
-                    react={{ and: FILTERS.filter((e) => e !== "SEARCH") }}
-                    style={{ flex: 2 }}
-                    innerClass={{ input: "searchbox" }}
-                    autosuggest={false}
-                    queryFormat="and"
-                  />
-                </Filter>
-                <ResultTable>
-                  <ReactiveListComponent
-                    scrollOnChange={false}
-                    react={{ and: FILTERS }}
-                    paginationAt="bottom"
-                    size={3}
-                    renderResultStats={(e) => {
-                      return (
-                        <div>
-                          <BottomResultStats>{getResultLabel(e, 3)}</BottomResultStats>
-                        </div>
-                      );
-                    }}
-                    render={({ data }) => (
-                      <Table>
-                        <thead>
-                          <tr>
-                            <th>Centre</th>
-                            <th>Séjour</th>
-                            <th>Action</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {data.map((hit) => (
-                            <HitCenter
-                              key={hit._id}
-                              hit={hit}
-                              onSend={(center) =>
-                                setModal({
-                                  isOpen: true,
-                                  title: "Changement de centre",
-                                  message: (
-                                    <p>
-                                      Voulez-vous affecter <b>{young.firstName}</b> au centre <b>{center.name}</b> au séjour de <b>{center.session}</b>.
-                                    </p>
-                                  ),
-                                  onConfirm: () => handleAffectation(center),
-                                })
-                              }
-                            />
-                          ))}
-                        </tbody>
-                      </Table>
-                    )}
-                  />
-                </ResultTable>
-              </div>
-            </ReactiveBase>
-            <ModalConfirm
-              isOpen={modal.isOpen}
-              onConfirm={modal.onConfirm}
-              title={modal.title}
-              message={modal.message}
-              onCancel={() => setModal((prevState) => ({ ...prevState, isOpen: false }))}
+    <ModalTailwind centered isOpen={isOpen} onClose={onCancel} className="w-[512px] bg-white rounded-lg shadow-xl p-2">
+      <div className="mb-4">
+        <div className="mb-4 text-gray-900 text-md text-center font-medium">Sélectionnez votre centre</div>
+        <ReactiveBase url={`${apiURL}/es`} app="sessionphase1" headers={{ Authorization: `JWT ${api.getToken()}` }}>
+          <div className="flex items-center justify-center w-full">
+            <DataSearch
+              defaultQuery={getDefaultQuery}
+              showIcon={false}
+              placeholder="Rechercher par mots clés, ville, code postal..."
+              componentId="SEARCH"
+              dataField={["nameCentre", "cityCentre", "zipCentre", "codeCentre"]}
+              react={{ and: FILTERS.filter((e) => e !== "SEARCH") }}
+              style={{ marginRight: "1rem", flex: 1 }}
+              innerClass={{ input: "searchbox" }}
+              className="datasearch-searchfield shadow-sm w-2/3"
+              URLParams={true}
+              autosuggest={false}
             />
           </div>
-        </div>
-        <Footer>
-          <ModalButton onClick={onCancel}>Retour</ModalButton>
-        </Footer>
-      </ModalContainer>
-    </Modal>
+
+          <div className="reactive-result">
+            <ReactiveListComponent
+              defaultQuery={getDefaultQuery}
+              scrollOnChange={false}
+              react={{ and: FILTERS }}
+              paginationAt="bottom"
+              showTopResultStats={false}
+              size={3}
+              render={({ data }) => (
+                <div className="flex flex-col justify-center items-center gap-4 w-full">
+                  {data.map((hit) => (
+                    <HitCenter
+                      key={hit._id}
+                      hit={hit}
+                      onSend={(center) =>
+                        setModal({
+                          isOpen: true,
+                          title: "Changement de centre",
+                          message: (
+                            <p>
+                              Voulez-vous affecter <b>{young.firstName}</b> au centre <b>{center.name}</b> au séjour de <b>{center.session}</b>.
+                            </p>
+                          ),
+                          onConfirm: () => handleAffectation(center),
+                        })
+                      }
+                    />
+                  ))}
+                </div>
+              )}
+            />
+          </div>
+        </ReactiveBase>
+        <ModalConfirm
+          isOpen={modal.isOpen}
+          onConfirm={modal.onConfirm}
+          title={modal.title}
+          message={modal.message}
+          onCancel={() => setModal((prevState) => ({ ...prevState, isOpen: false }))}
+        />
+      </div>
+      <Footer>
+        <ModalButton onClick={onCancel}>Retour</ModalButton>
+      </Footer>
+    </ModalTailwind>
   );
 }
 
 const HitCenter = ({ hit, onSend }) => {
-  const [selectedSession, setSelectedSession] = useState(hit.cohorts[0]);
-  const options = hit.cohorts.filter((cohort) => cohort !== selectedSession);
-
-  const [sessions, setSessions] = useState();
-
-  useEffect(() => {
-    try {
-      (async () => {
-        const { data } = await api.get(`/cohesion-center/${hit._id}/session-phase1`);
-        setSessions(data);
-      })();
-    } catch (e) {
-      console.log(e);
-    }
-  }, [hit]);
-
-  if (!sessions) return null;
-
   return (
-    <tr>
-      <td>
-        <MultiLine>
-          <span className="font-bold text-black">{hit.name}</span>
-          <p>{`${hit.city || ""} • ${hit.department || ""}`}</p>
-        </MultiLine>
-      </td>
-      <td>
-        <ActionBox>
-          <UncontrolledDropdown disabled={!options.length} setActiveFromChild>
-            <DropdownToggle tag="button">
-              {selectedSession}
-              {!!options.length && <Chevron color={"#444"} />}
-            </DropdownToggle>
-            <DropdownMenu>
-              {options.map((cohort) => {
-                return (
-                  <DropdownItem key={cohort} className="dropdown-item" onClick={() => setSelectedSession(cohort)}>
-                    {cohort}
-                  </DropdownItem>
-                );
-              })}
-            </DropdownMenu>
-          </UncontrolledDropdown>
-        </ActionBox>
-      </td>
-      {sessions.find((session) => session.cohort === selectedSession)?.placesLeft <= 0 ? (
-        <td>
-          <div className="font-bold border-[1px] rounded-lg text-center p-1 bg-white text-[#555]">Complet</div>
-        </td>
-      ) : (
-        <td onClick={(e) => e.stopPropagation()}>
-          <PanelActionButton onClick={() => onSend({ ...hit, session: selectedSession })} title="Affecter à ce centre" />
-        </td>
-      )}
-    </tr>
+    <>
+      <hr />
+      <div className="flex flex-row gap-4 justify-center items-center w-full">
+        <div className="w-1/2">
+          <MultiLine>
+            <span className="font-bold text-black">{hit.nameCentre}</span>
+            <p>{`${hit.cityCentre || ""} • ${hit.department || ""}`}</p>
+          </MultiLine>
+        </div>
+        <div className="w-1/4">
+          <div key={hit.cohort} className={`rounded-full text-xs font-medium leading-5 px-3 py-1 w-fit border-[1px] border-[#0C7CFF] text-[#0C7CFF] bg-[#F9FCFF]`}>
+            {hit.cohort}
+          </div>
+        </div>
+        <div className="cursor-pointer" onClick={onSend}>
+          <RightArrow />
+        </div>
+      </div>
+    </>
   );
 };
 
-const ActionBox = styled.div`
-  .dropdown-menu {
-    max-width: 200px;
-    min-width: 200px;
-    a,
-    div {
-      white-space: nowrap;
-      font-size: 14px;
-      :hover {
-        color: inherit;
-      }
-    }
-  }
-  button {
-    background-color: #fff;
-    border: 1px solid #ced4da;
-    color: #495057;
-    display: inline-flex;
-    flex: 1;
-    justify-content: space-between;
-    align-items: center;
-    text-align: left;
-    border-radius: 0.5rem;
-    padding: 0 0 0 12px;
-    font-size: 12px;
-    font-weight: 700;
-    cursor: pointer;
-    outline: 0;
-    width: 100%;
-    max-width: 200px;
-    min-width: 200px;
-    min-height: 34px;
-    .edit-icon {
-      height: 17px;
-      margin-right: 10px;
-      path {
-        fill: ${({ color }) => `${color}`};
-      }
-    }
-    .down-icon {
-      margin-left: auto;
-      padding: 7px 15px;
-      margin-left: 15px;
-      svg {
-        height: 10px;
-      }
-      svg polygon {
-        fill: ${({ color }) => `${color}`};
-      }
-    }
-  }
-  .dropdown-item {
-    border-radius: 0;
-    background-color: transparent;
-    border: none;
-    color: #767676;
-    white-space: nowrap;
-    font-size: 14px;
-    padding: 5px 15px;
-    font-weight: 400;
-    :hover {
-      background-color: #f3f3f3;
-    }
-  }
-`;
+const RightArrow = () => {
+  return (
+    <svg width="42" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g filter="url(#filter0_d_3100_51832)">
+        <rect x="2" y="1" width="38" height="38" rx="19" fill="#2563EB" />
+        <path d="M18.5 14.1667L24.3333 20L18.5 25.8334" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+      </g>
+      <defs>
+        <filter id="filter0_d_3100_51832" x="0" y="0" width="42" height="42" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+          <feOffset dy="1" />
+          <feGaussianBlur stdDeviation="1" />
+          <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.05 0" />
+          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_3100_51832" />
+          <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_3100_51832" result="shape" />
+        </filter>
+      </defs>
+    </svg>
+  );
+};
