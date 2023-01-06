ede51e58ca5e28701ed5840c3a2df8cea0dc87d8
Seb Doncker <seb@dingorock.fr>
2023-01-06 14:40:39 +0100

    feat(admin/app/api): Droit à l'image - modification  (#2166)

---
+++ app/src/scenes/representants-legaux/components/AuthorizeBlock.js
@@ -0,0 +1,23 @@
+import RadioButton from "./RadioButton";
+import { booleanToString } from "../commons";
+import ErrorMessage from "../../inscription2023/components/ErrorMessage";
+import React from "react";
+
+export default function AuthorizeBlock({ title, value, onChange, children, className, error }) {
+  const options = [
+    { value: "true", label: "J'autorise" },
+    { value: "false", label: "Je n'autorise pas" },
+  ];
+
+  function onValueChange(e) {
+    onChange(e === "true");
+  }
+
+  return (
+    <div className={className}>
+      <RadioButton label={title} options={options} onChange={onValueChange} value={booleanToString(value)} />
+      {error && <ErrorMessage className="mb-2">{error}</ErrorMessage>}
+      <div>{children}</div>
+    </div>
+  );
+}
