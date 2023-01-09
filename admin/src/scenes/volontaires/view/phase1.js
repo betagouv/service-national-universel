b313d8cc615efd754cfe261ac65e169ec78befa5
Theret Guillaume <73123940+guiTheret@users.noreply.github.com>
2023-01-09 11:22:38 +0000

    Update phase1.js

--- admin/src/scenes/volontaires/view/phase1.js
+++ admin/src/scenes/volontaires/view/phase1.js
@@ -37,6 +37,7 @@ import DocumentPhase1 from "../components/DocumentPhase1";
 import ModalAffectations from "../components/ModalAffectation";
 import Select from "../../../components/Select2";
 import YoungHeader from "../../phase0/components/YoungHeader";
+import Field from "../components/Field.js";
 
 export default function Phase1(props) {
   const user = useSelector((state) => state.Auth.user);
@@ -189,7 +190,40 @@ export default function Phase1(props) {
     setModalPointagePresenceJDM({ isOpen: false, value: null });
     setModalPointageDepart({ isOpen: false, value: null });
   };
-
+  const EditTop = () => {
+    return (
+      <>
+        {!editing ? (
+          <button
+            className="flex items-center gap-2 rounded-full text-xs font-medium leading-5 cursor-pointer px-3 py-2 border-[1px] border-blue-100 text-blue-600 bg-blue-100 hover:border-blue-600 disabled:opacity-50 disabled:cursor-not-allowed"
+            onClick={() => setEditing(true)}
+            disabled={loading}>
+            <Pencil stroke="#2563EB" className="w-[12px] h-[12px]" />
+            Modifier
+          </button>
+        ) : (
+          <div className="flex items-center gap-2">
+            <button
+              className="flex items-center gap-2 rounded-full text-xs font-medium leading-5 cursor-pointer px-3 py-2 border-[1px] border-gray-100 text-gray-700 bg-gray-100 hover:border-gray-700 disabled:opacity-50 disabled:cursor-not-allowed"
+              onClick={() => {
+                setEditing(false);
+                setErrors({});
+              }}
+              disabled={loading}>
+              Annuler
+            </button>
+            <button
+              className="flex items-center gap-2 rounded-full text-xs font-medium leading-5 cursor-pointer px-3 py-2 border-[1px] border-blue-100 text-blue-600 bg-blue-100 hover:border-blue-600 disabled:opacity-50 disabled:cursor-not-allowed"
+              onClick={onSubmit}
+              disabled={loading}>
+              <Pencil stroke="#2563EB" className="w-[12px] h-[12px] mr-[6px]" />
+              Enregistrer les changements
+            </button>
+          </div>
+        )}
+      </>
+    );
+  };
   return (
     <>
       <YoungHeader young={props.young} tab="phase1" onChange={props.onChange} />
@@ -198,42 +232,19 @@ export default function Phase1(props) {
           <div className="mx-8 py-4">
             <div className="flex flex-row justify-between items-center">
               <div className="flex flex-row items-center justify-center">
-                <div className="text-lg font-medium">Séjour de cohésion</div>
-                <div className={`flex flex-row items-center justify-center rounded-full border-[1px] border-gray-300 px-2 py-0.5 ml-3`}>
-                  {young.statusPhase1 === "AFFECTED" && <div className="w-2 h-2 rounded-full bg-blue-500 shadow-sm" />}
-                  <div className="text-gray-500 text-xs ml-1">Affectée</div>
-                </div>
+                <div className="text-lg font-medium mr-2">Séjour de cohésion</div>
+                <Badge
+                  minify
+                  text={translatePhase1(young.statusPhase1)}
+                  // icon={young.statusPhase1 === "AFFECTED" && <div className="w-2 h-2 rounded-full bg-blue-500 shadow-sm mr-1" />}
+                  color={YOUNG_STATUS_COLORS[young.statusPhase1]}
+                />
               </div>
-              <>
-                {!editing ? (
-                  <button
-                    className="flex items-center gap-2 rounded-full text-xs font-medium leading-5 cursor-pointer px-3 py-2 border-[1px] border-blue-100 text-blue-600 bg-blue-100 hover:border-blue-600 disabled:opacity-50 disabled:cursor-not-allowed"
-                    onClick={() => setEditing(true)}
-                    disabled={loading}>
-                    <Pencil stroke="#2563EB" className="w-[12px] h-[12px]" />
-                    Modifier
-                  </button>
-                ) : (
-                  <div className="flex items-center gap-2">
-                    <button
-                      className="flex items-center gap-2 rounded-full text-xs font-medium leading-5 cursor-pointer px-3 py-2 border-[1px] border-gray-100 text-gray-700 bg-gray-100 hover:border-gray-700 disabled:opacity-50 disabled:cursor-not-allowed"
-                      onClick={() => {
-                        setEditing(false);
-                        setErrors({});
-                      }}
-                      disabled={loading}>
-                      Annuler
-                    </button>
-                    <button
-                      className="flex items-center gap-2 rounded-full text-xs font-medium leading-5 cursor-pointer px-3 py-2 border-[1px] border-blue-100 text-blue-600 bg-blue-100 hover:border-blue-600 disabled:opacity-50 disabled:cursor-not-allowed"
-                      onClick={onSubmit}
-                      disabled={loading}>
-                      <Pencil stroke="#2563EB" className="w-[12px] h-[12px] mr-[6px]" />
-                      Enregistrer les changements
-                    </button>
-                  </div>
-                )}
-              </>
+              <EditTop />
+            </div>
+            <div className="mt-3">
+              <div className="text-xs text-gray-900 font-medium">Présence</div>
+              <Field />
             </div>
           </div>
         </div>
