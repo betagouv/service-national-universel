318902413d6f58ded8bd2643aee4de4b90b275e4
Theret Guillaume <73123940+guiTheret@users.noreply.github.com>
2023-01-12 11:19:21 +0000

    Update point-de-rassemblement.js

--- api/src/controllers/planDeTransport/point-de-rassemblement.js
+++ api/src/controllers/planDeTransport/point-de-rassemblement.js
@@ -191,7 +191,7 @@ router.get("/:id", passport.authenticate("referent", { session: false, failWithE
   }
 });
 
-// get meetingPoint info with a single meetingPoint Id and Bus Id
+// get 1 meetingPoint info with meetingPoint Id and Bus Id as params
 router.get("/ligneToPoint/:pdrId/:busId", passport.authenticate("referent", { session: false, failWithError: true }), async (req, res) => {
   try {
     const { error: errorParams, value: valueParams } = Joi.object({ pdrId: Joi.string().required(), busId: Joi.string().required() }).validate(req.params, {
