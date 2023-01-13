e0ff6c4fcd822270ce7fe26cdaf331ad9bb5eed8
seb <seb@snarkfacgtory.fr>
2023-01-13 12:02:59 +0100

    Ajout du bouton Je donne mon avis

--- app/src/scenes/phase1/affected.js
+++ app/src/scenes/phase1/affected.js
@@ -166,6 +166,11 @@ export default function Affected() {
             </section>
           </div>
         </div>
+        <div className="flex justify-end pt-4 pb-8 pr-8">
+          <a href="https://jedonnemonavis.numerique.gouv.fr/Demarches/3504?&view-mode=formulaire-avis&nd_source=button&key=060c41afff346d1b228c2c02d891931f">
+            <img src="https://jedonnemonavis.numerique.gouv.fr/static/bouton-bleu.svg" alt="Je donne mon avis" className="h-[60px]" />
+          </a>
+        </div>
       </>
     </>
   );
