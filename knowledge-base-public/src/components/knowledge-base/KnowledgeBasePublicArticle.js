fe14feab181495eccda5dd7df5a8686e694f9288
Szilagyi Kinga <szilagyikinga42@gmail.com>
2023-01-13 16:20:49 +0100

    fix(bdc): hide updatedAt date and display updated at banner

--- knowledge-base-public/src/components/knowledge-base/KnowledgeBasePublicArticle.js
+++ knowledge-base-public/src/components/knowledge-base/KnowledgeBasePublicArticle.js
@@ -6,7 +6,7 @@ const KnowledgeBasePublicArticle = ({ item, isLoading }) => {
     <div className="wrapper mx-auto flex w-full flex-shrink flex-grow flex-col overflow-hidden bg-coolGray-100 print:bg-transparent  print:pb-12">
       {item?.updatedAt && (
         <span className="ml-auto mt-2 mb-4 flex flex-col items-end text-xs italic text-gray-400 print:mt-0 print:mb-2">
-          <em>Article mis à jour le {Intl.DateTimeFormat("fr-FR", { year: "numeric", month: "long", day: "numeric" }).format(new Date(item.updatedAt))}</em>
+          {/* <em>Article mis à jour le {Intl.DateTimeFormat("fr-FR", { year: "numeric", month: "long", day: "numeric" }).format(new Date(item.updatedAt))}</em> */}
           <button className="noprint mt-2 cursor-pointer border border-gray-300 bg-gray-100 font-normal text-black shadow-none" onClick={window.print}>
             🖨 Imprimer
           </button>
