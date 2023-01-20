27671ce940a2eddd47ba4700eb38872111d78411
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2023-01-20 16:06:57 +0100

    up

--- app/src/scenes/representants-legaux/desktop/consentement.js
+++ app/src/scenes/representants-legaux/desktop/consentement.js
@@ -17,7 +17,6 @@ import ErrorMessage from "../../inscription2023/components/ErrorMessage";
 import api from "../../../services/api";
 import { BorderButton, PlainButton } from "../components/Buttons";
 import plausibleEvent from "../../../services/plausible";
-import { regexPhoneFrenchCountries } from "../../../utils";
 import AuthorizeBlock from "../components/AuthorizeBlock";
 
 export default function Consentement({ step, parentId }) {
@@ -197,7 +196,7 @@ export default function Consentement({ step, parentId }) {
       validate("email", "invalid", !validator.isEmail(data.email));
     }
     if (validate("phone", "empty", validator.isEmpty(data.phone, { ignore_whitespace: true }))) {
-      validate("phone", "invalid", !validator.matches(data.phone, regexPhoneFrenchCountries));
+      validate("phone", "invalid", !validator.isMobilePhone(data.phone, ["fr-FR", "fr-GF", "fr-GP", "fr-MQ", "fr-RE"]));
     }
 
     // --- address
