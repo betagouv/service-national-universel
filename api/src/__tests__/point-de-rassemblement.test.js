440401bc7381836da898af677e15250056e17576
guiTheret <gtheret@sfr.fr>
2023-01-20 15:40:09 +0100

    Update point-de-rassemblement.test.js

--- api/src/__tests__/point-de-rassemblement.test.js
+++ api/src/__tests__/point-de-rassemblement.test.js
@@ -2,7 +2,13 @@ require("dotenv").config({ path: "./.env-testing" });
 const request = require("supertest");
 
 const getNewPointDeRassemblementFixture = require("./fixtures/PlanDeTransport/pointDeRassemblement");
-const { createPointDeRassemblementHelper } = require("./helpers/PlanDeTransport/pointDeRassemblement");
+const { getNewCohesionCenterFixture } = require("./fixtures/cohesionCenter");
+const { getNewSessionPhase1Fixture } = require("./fixtures/sessionPhase1");
+
+const { createPointDeRassemblementHelper, createPointDeRassemblementWithBus } = require("./helpers/PlanDeTransport/pointDeRassemblement");
+
+const { createCohesionCenter } = require("./helpers/cohesionCenter");
+const { createSessionPhase1 } = require("./helpers/sessionPhase1");
 
 const getNewYoungFixture = require("./fixtures/young");
 const getAppHelper = require("./helpers/app");
@@ -52,12 +58,46 @@ describe("Meeting point", () => {
     });
     it("should return 403 when youngs are still linked to point-de-rassemblement", async () => {
       const pointDeRassemblement = await createPointDeRassemblementHelper({ ...getNewPointDeRassemblementFixture() });
-      const young = await createYoungHelper({ ...getNewYoungFixture(), meetingPointId: pointDeRassemblement._id });
+      await createYoungHelper({ ...getNewYoungFixture(), meetingPointId: pointDeRassemblement._id });
       const res = await request(getAppHelper())
-        .put("/point-de-rassemblement/delete/cohort/" + pointDeRassemblement._id, { cohort: "Février 2023 - C" })
-        .send();
-      console.log(res.code);
+        .put("/point-de-rassemblement/delete/cohort/" + pointDeRassemblement._id)
+        .send({ cohort: "Février 2023 - C" });
       expect(res.status).toBe(403);
+    });
+  });
+  describe("GET /point-de-rassemblement/fullInfo/:pdrId/:busId", () => {
+    it("should return 403 when young try to fetch another pdr than his", async () => {
+      const pointDeRassemblemenYoung = await createPointDeRassemblementHelper({ ...getNewPointDeRassemblementFixture() });
+      const young = await createYoungHelper({ ...getNewYoungFixture(), meetingPointId: pointDeRassemblemenYoung._id });
+
+      const pointDeRassemblement = await createPointDeRassemblementHelper({ ...getNewPointDeRassemblementFixture() });
+      const passport = require("passport");
+      const previous = passport.user;
+      passport.user = young;
+
+      const res = await request(getAppHelper())
+        .get("/point-de-rassemblement/fullInfo/" + pointDeRassemblement._id + "/" + pointDeRassemblement._id)
+        .send();
+      expect(res.status).toBe(403);
+      passport.user = previous;
+    });
+    it("should return 403 when young try to fetch another bus than his", async () => {
+      const cohesionCenter = await createCohesionCenter(getNewCohesionCenterFixture());
+      const sessionPhase1 = await createSessionPhase1({ ...getNewSessionPhase1Fixture(), cohesionCenterId: cohesionCenter._id });
+
+      const resultYoung = await createPointDeRassemblementWithBus(getNewPointDeRassemblementFixture(), cohesionCenter._id, sessionPhase1._id);
+      const young = await createYoungHelper({ ...getNewYoungFixture(), meetingPointId: resultYoung.pdr._id, ligneId: resultYoung.bus._id });
+
+      const { pdr, bus } = await createPointDeRassemblementWithBus(getNewPointDeRassemblementFixture(), cohesionCenter._id, sessionPhase1._id);
+      const passport = require("passport");
+      const previous = passport.user;
+      passport.user = young;
+
+      const res = await request(getAppHelper())
+        .get("/point-de-rassemblement/fullInfo/" + pdr._id + "/" + bus._id)
+        .send();
+      expect(res.status).toBe(403);
+      passport.user = previous;
     });
   });
 });
