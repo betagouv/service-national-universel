c9428fbd90edb4709d885d72fd892c6756819b5b
guiTheret <gtheret@sfr.fr>
2023-01-23 11:01:08 +0100

    Update app.js

--- api/src/__tests__/helpers/app.js
+++ api/src/__tests__/helpers/app.js
@@ -10,7 +10,7 @@ function getAppHelper() {
   app.use(cookieParser());
   app.use("/referent", require("../../controllers/referent"));
   app.use("/young", require("../../controllers/young"));
-  app.use("/point-de-rassemblement", require("../../controllers/PlanDeTransport/point-de-rassemblement"));
+  app.use("/point-de-rassemblement", require("../../controllers/planDeTransport/point-de-rassemblement"));
   app.use("/mission", require("../../controllers/mission"));
   app.use("/program", require("../../controllers/program"));
   app.use("/application", require("../../controllers/application"));
