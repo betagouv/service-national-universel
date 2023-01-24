a8ee914678c8316a1f5b08da2b0ffa14d87930b6
naustra <lebihanp@hotmail.fr>
2023-01-24 16:42:10 +0000

    feat(admin): update open Affectation for ref / admin (#2204)

--- api/src/models/cohort.js
+++ api/src/models/cohort.js
@@ -39,10 +39,22 @@ const Schema = new mongoose.Schema({
         "Si true, les affectations sont 'révélées' au jeune. Sinon, le jeune doit avoir l'impression qu'il est toujours dans un état d'attente d'affectation même si il a été affecté.",
     },
   },
-  manualAffectionOpenForReferent: {
+  manualAffectionOpenForAdmin: {
+    type: Boolean,
+    documentation: {
+      description: "Si true, les admins peuvent manuellement affecter un jeune à un centre",
+    },
+  },
+  manualAffectionOpenForReferentRegion: {
     type: Boolean,
     documentation: {
       description: "Si true, les referents régionaux peuvent manuellement affecter un jeune à un centre",
+    },
+  },
+  manualAffectionOpenForReferentDepartment: {
+    type: Boolean,
+    documentation: {
+      description: "Si true, les referents départementaux peuvent manuellement affecter un jeune à un centre",
     },
   },
 
