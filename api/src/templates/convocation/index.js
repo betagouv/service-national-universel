259f92d1512c5db2cbf5a42a4c92ff7766beb675
Theret Guillaume <73123940+guiTheret@users.noreply.github.com>
2023-01-30 11:05:32 +0000

    Update index.js

--- api/src/templates/convocation/index.js
+++ api/src/templates/convocation/index.js
@@ -111,7 +111,7 @@ const renderLocalTransport = async (young) => {
   console.log(getBaseUrl());
   try {
     if (!["AFFECTED", "DONE"].includes(young.statusPhase1)) throw `young ${young.id} not affected`;
-    if (!young.sessionPhase1Id || (!young.meetingPointId && young.deplacementPhase1Autonomous !== "true")) throw `young ${young.id} unauthorized`;
+    if (!young.sessionPhase1Id) throw `young ${young.id} unauthorized`;
     const session = await SessionPhase1.findById(young.sessionPhase1Id);
     if (!session) throw `session ${young.sessionPhase1Id} not found for young ${young._id}`;
     const center = await CohesionCenterModel.findById(session.cohesionCenterId);
