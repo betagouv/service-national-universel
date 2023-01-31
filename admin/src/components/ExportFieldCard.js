b6b69e90f416e44b5d6bd5bfa8305d1baa958449
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2023-01-31 14:52:50 +0100

    better style modal

--- admin/src/components/ExportFieldCard.js
+++ admin/src/components/ExportFieldCard.js
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function ExportFieldCard({ category, selectedFields, setSelectedFields }) {
-  const [isOpen, setIsOpen] = useState(false);
-
+  const [isOpen, setIsOpen] = React.useState(false);
   return (
-    <div className="rounded-xl border-2 border-gray-100 px-3 py-2 hover:shadow-ninaButton cursor-pointer">
+    <div className="rounded-xl border-[1px] px-3 py-2.5 cursor-pointer hover:bg-gray-50 transition">
       <div
         onClick={() => {
           if (!selectedFields.includes(category.id)) {
@@ -15,20 +14,20 @@ export default function ExportFieldCard({ category, selectedFields, setSelectedF
             setSelectedFields(newValues);
           }
         }}>
-        <div className="flex justify-between w-full">
-          <div className="text-left text-lg w-3/4 font-medium mb-1 text-gray-600">{category.title}</div>
-          <div className="h-4">
-            <input readOnly type="checkbox" checked={selectedFields.includes(category.id)} />
-          </div>
+        <div className="flex items-center justify-between">
+          <p className="text-sm font-medium text-gray-600">{category.title}</p>
+          <input readOnly type="checkbox" checked={selectedFields.includes(category.id)} />
         </div>
-        <div className={`w-full text-gray-400 text-left h-${isOpen ? "auto" : 16} overflow-hidden`}>
+        <div className={`text-gray-400 h-${isOpen ? "auto" : 16} overflow-hidden`}>
           {category.desc.map((e) => (
-            <div key={e}>{e}</div>
+            <p key={e} className="text-xs">
+              {e}
+            </p>
           ))}
         </div>
       </div>
-      {category.desc.length > 3 && (
-        <button className="text-gray-500 text-center w-full hover:text-gray-800" onClick={() => setIsOpen(!isOpen)}>
+      {category.desc.length > 4 && (
+        <button className="text-gray-500 text-xs text-center hover:text-gray-700" onClick={() => setIsOpen(!isOpen)}>
           {!isOpen ? "Voir plus" : "Réduire"}
         </button>
       )}
