61eac2ece9dc23e3815b349d16b1acb898bc2e5a
Nicolas Bretécher <nicolas.bretecher@selego.co>
2023-02-01 12:50:33 +0100

    fix(lib/admin): retours sur historique volontaires (#2225)
    * wip
    
    * done

--- admin/src/components/views/Historic2.js
+++ admin/src/components/views/Historic2.js
@@ -1,44 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import { formatStringLongDate, translateModelFields, isIsoDate, translateHistory } from "../../utils";
 import { formatLongDateFR, translateAction } from "snu-lib";
 import FilterIcon from "../../assets/icons/Filter";
 import UserCard from "../UserCard";
-import MultiSelect from "../../scenes/dashboard/components/MultiSelect";
 import { HiOutlineArrowRight } from "react-icons/hi";
+
+import { Listbox, Transition } from "@headlessui/react";
+import { BsChevronDown, BsChevronUp } from "react-icons/bs";
+import { AiOutlineCheck } from "react-icons/ai";
 
 export default function Historic({ model, data, customFilterOptions, refName }) {
   const [query, setQuery] = useState("");
   const [isOpen, setIsOpen] = useState(false);
-  const [commonFilters, setCommonFilters] = useState({ op: [], user: [], path: [] });
-  const [customFilter, setCustomFilter] = useState({ label: "", values: null });
-  const activeFilters = getActiveFilters();
-  const filteredData = filterData();
-
-  function getActiveFilters() {
-    let filters = [];
-    for (const [key, value] of Object.entries(commonFilters)) {
-      if (value.length) filters.push({ [key]: value });
-    }
-    if (customFilter?.value) filters.push(customFilter.value);
-    return filters;
-  }
+  const [authors, setAuthors] = useState([]);
+  const [ops, setOps] = useState([]);
+  const [paths, setPaths] = useState([]);
+  const pathOptions = getOptions("path").map((e) => ({ label: translateModelFields(model, e), value: e }));
+  const opOptions = getOptions("op").map((e) => ({ label: translateAction(e), value: e }));
+  const userOptions = getOptions("author").map((e) => ({ label: e, value: e }));
+  const [customFilter, setCustomFilter] = useState(null);
+  const filteredData = data.filter((event) => filterEvent(event));
 
   function getOptions(key) {
     const arr = data?.map((e) => e[key]);
     return [...new Set(arr)];
   }
 
-  function filterData() {
-    let d = data;
-    if (activeFilters?.length) d = d.filter((e) => filterEvent(e, activeFilters));
-    if (query) d = d.filter((e) => searchEvent(e, query));
-    return d;
-  }
-
-  function filterEvent(event, filters) {
-    return filters.every((filter) => {
-      return Object.entries(filter).some(([key, value]) => value.includes(event[key]));
-    });
+  function filterEvent(event) {
+    if (paths.length) return paths.includes(event.path);
+    if (ops.length) return ops.includes(event.op);
+    if (authors.length) return authors.includes(event.author);
+    if (customFilter?.value?.length) {
+      return customFilter.value.some((value) => {
+        return Object.entries(value).every(([key, value]) => value.includes(event[key]));
+      });
+    }
+    if (query) return searchEvent(event, query);
+    return true;
   }
 
   function searchEvent(e, query) {
@@ -57,32 +55,17 @@ export default function Historic({ model, data, customFilterOptions, refName }) 
         <input onChange={(e) => setQuery(e.target.value)} value={query} className="border p-2 rounded-lg w-64 text-xs" placeholder="Rechercher..." />
         <button
           onClick={() => setIsOpen(!isOpen)}
-          className={`group py-2 px-3 rounded-lg flex items-center gap-2 ${isOpen ? "bg-gray-500 hover:bg-gray-100" : "bg-gray-100 hover:bg-gray-500"}`}>
+          className={`group py-2 px-3 rounded-lg flex items-center gap-2 ${isOpen ? "bg-gray-500 hover:bg-gray-100" : "bg-gray-100 hover:bg-gray-500"} transition`}>
           <FilterIcon className={isOpen ? "fill-gray-100 group-hover:fill-gray-500" : "fill-gray-500 group-hover:fill-gray-100"} />
           <p className={isOpen ? "text-gray-100 group-hover:text-gray-500" : "text-gray-500 group-hover:text-gray-100"}>Filtres</p>
         </button>
         {customFilterOptions && <CustomFilters customFilterOptions={customFilterOptions} customFilter={customFilter} setCustomFilter={setCustomFilter} />}
       </div>
       {isOpen && (
-        <div className="flex flex-wrap gap-4 p-4 bg-slate-50">
-          <MultiSelect
-            options={getOptions("path").map((e) => ({ label: translateModelFields(model, e), value: e }))}
-            value={commonFilters.path}
-            onChange={(path) => setCommonFilters((f) => ({ ...f, ...{ path } }))}
-            label="Donnée modifiée"
-          />
-          <MultiSelect
-            options={getOptions("op").map((e) => ({ label: translateAction(e), value: e }))}
-            value={commonFilters.op}
-            onChange={(op) => setCommonFilters((f) => ({ ...f, ...{ op } }))}
-            label="Type d'action"
-          />
-          <MultiSelect
-            options={getOptions("author").map((e) => ({ label: e, value: e }))}
-            value={commonFilters.author}
-            onChange={(author) => setCommonFilters((f) => ({ ...f, ...{ author } }))}
-            label="Auteur de la modification"
-          />
+        <div className="flex flex-wrap gap-4 p-4">
+          <MultiSelect options={pathOptions} selected={paths} onChange={setPaths} label="Donnée modifiée" />
+          <MultiSelect options={opOptions} selected={ops} onChange={setOps} label="Type d'action" />
+          <MultiSelect options={userOptions} selected={authors} onChange={setAuthors} label="Auteur de la modification" />
         </div>
       )}
       <table className="table-fixed w-full">
@@ -107,7 +90,7 @@ export default function Historic({ model, data, customFilterOptions, refName }) 
 }
 
 function FilterButton({ filter, setCustomFilter, customFilter }) {
-  const checked = filter.label === customFilter.label;
+  const checked = filter.label === customFilter?.label;
 
   function handleChange() {
     if (checked) return setCustomFilter({ label: "", value: null });
@@ -156,5 +139,52 @@ function Event({ e, index, model, refName, path }) {
         <UserCard user={e.user} />
       </td>
     </tr>
+  );
+}
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+function MultiSelect({ options, selected, onChange, label }) {
+  return (
+    <div className="min-w-72">
+      <Listbox value={selected} onChange={onChange} multiple>
+        {({ open }) => (
+          <div className="relative">
+            <Listbox.Button className="flex rounded-lg p-2 border bg-white items-center gap-3">
+              <p className="text-left text-sm text-gray-500 max-w-sm whitespace-nowrap overflow-hidden">
+                {label} : {selected?.map((e) => e.label).join(", ")}
+              </p>
+              <span className="pointer-events-none flex items-center pr-2">
+                {open ? <BsChevronUp className="h-4 w-4 text-gray-400" aria-hidden="true" /> : <BsChevronDown className="h-4 w-4 text-gray-400" aria-hidden="true" />}
+              </span>
+            </Listbox.Button>
+
+            <Transition show={open} as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
+              <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                {options.map((option) => (
+                  <Listbox.Option
+                    key={option.value}
+                    className={({ active }) => classNames(active ? "text-white bg-blue-600" : "text-gray-900", "relative cursor-default select-none py-2 pl-3 pr-9 list-none")}
+                    value={option.value}>
+                    {({ selected, active }) => (
+                      <>
+                        <span className={classNames(selected ? "font-semibold" : "font-normal", "block truncate")}>{option.label}</span>
+                        {selected ? (
+                          <span className={classNames(active ? "text-white" : "text-blue-600", "absolute inset-y-0 right-0 flex items-center pr-4")}>
+                            <AiOutlineCheck className="h-5 w-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </Transition>
+          </div>
+        )}
+      </Listbox>
+    </div>
   );
 }
