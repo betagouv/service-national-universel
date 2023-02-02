5069080080336d6306250c164640c7aacb1c78e1
Nicolas Bretécher <nicolas.bretecher@selego.co>
2023-02-02 16:58:19 +0100

    feat(admin): refonte liste structures (staging) (#2212)

--- admin/src/scenes/structure/index.js
+++ admin/src/scenes/structure/index.js
@@ -6,6 +6,7 @@ import { SentryRoute } from "../../sentry";
 import View from "./view";
 import Edit from "./edit";
 import List from "./list";
+import ListV2 from "./listV2";
 import Create from "./create";
 import { environment } from "../../config";
 
@@ -17,7 +18,7 @@ export default function Index() {
       <SentryRoute path="/structure/create" component={Create} />
       <SentryRoute path="/structure/:id/edit" component={environment === "production" ? Edit : View} />
       <SentryRoute path="/structure/:id" component={View} />
-      <SentryRoute path="/structure" component={List} />
+      <SentryRoute path="/structure" component={environment === "production" ? List : ListV2} />
     </Switch>
   );
 }
