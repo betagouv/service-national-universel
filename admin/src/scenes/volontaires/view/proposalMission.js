0498f26e3dcfe0b1aa5f9131dec255a7625ce291
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2023-02-02 13:12:33 +0100

    wip

--- admin/src/scenes/volontaires/view/proposalMission.js
+++ admin/src/scenes/volontaires/view/proposalMission.js
@@ -87,7 +87,7 @@ export default function ProposeMission({ young, onSend }) {
   return (
     <>
       <YoungHeader young={young} onChange={onSend} />
-      <div className="bg-white rounded-xl shadow-sm m-8 p-8 space-y-8 items-center">
+      <div className="bg-blue-50 rounded-xl shadow-sm m-8 p-8 space-y-8 items-center">
         <div className="grid grid-cols-9 border-b pb-8">
           <div className="w-9 h-9 rounded-full p-2 bg-gray-200 cursor-pointer hover:scale-105">
             <Link to={`/volontaire/${young._id}/phase2`}>
@@ -106,6 +106,7 @@ export default function ProposeMission({ young, onSend }) {
             Proposer une mission à {young.firstName} {young.lastName}
           </h1>
         </div>
+
         <ReactiveBase url={`${apiURL}/es`} app="mission" headers={{ Authorization: `JWT ${api.getToken()}` }}>
           <DataSearch
             showIcon={false}
@@ -146,67 +147,81 @@ export default function ProposeMission({ young, onSend }) {
 const HitMission = ({ mission, onSend }) => {
   console.log("🚀 ~ file: proposalMission.js:155 ~ HitMission ~ hit", mission);
   return (
-    <div className="bg-white shadow-sm rounded-xl p-4 my-8">
-      <div className="flex justify-between">
-        <Link className="flex basis-[35%] items-center" to={`/mission/${mission._d}`}>
-          {/* icon */}
-          <div className="flex items-center mr-4">
-            <IconDomain domain={mission?.isMilitaryPreparation === "true" ? "PREPARATION_MILITARY" : mission?.mainDomain} />
+    <div className="bg-white shadow-md rounded-xl p-4 my-8">
+      <div className="flex gap-4">
+        <IconDomain domain={mission?.isMilitaryPreparation === "true" ? "PREPARATION_MILITARY" : mission?.mainDomain} />
+        <div className="bg-red-100 w-full">
+          <div>{mission.structureName}</div>
+          <div className="bg-green-100 flex justify-between">
+            <div>{mission.name}</div>
+            <div>dates</div>
+            <p>places</p>
+            <button>bouton</button>
           </div>
-          {/* mission info */}
-          <div className="flex flex-col flex-1 justify-center">
-            <div className="uppercase text-gray-500 font-medium text-[11px] tracking-wider mb-1">{mission.structureName}</div>
-            <div className="text-[#242526] font-bold text-base mb-2">{mission.name}</div>
-            {/* tags */}
-            {mission.domains && (
-              <div className=" inline-flex flex-wrap">
-                {mission.domains.map((tag, index) => {
-                  return (
-                    <div
-                      key={index}
-                      className=" flex text-[11px] text-gray-600 rounded-full border-gray-200 border-[1px] justify-center items-center mb-2 mt-1 mr-1 px-3  py-0.5 font-medium ">
-                      {tag}
-                    </div>
-                  );
-                })}
-                {mission.isMilitaryPreparation === "true" ? (
-                  <div className="flex justify-center items-center bg-blue-900 text-white border-gray-200 border-[1px] rounded-full text-[11px] mb-2 mr-1 px-3 py-0.5 font-medium">
-                    Préparation militaire
-                  </div>
-                ) : null}
-              </div>
-            )}
-          </div>
-        </Link>
-        <div className="flex basis-[65%] justify-between items-center">
-          {/* date */}
-          <div className="flex flex-col basis-[30%] justify-center">
-            <div>
-              <span className="text-gray-500 mr-1 text-xs">Du</span>
-              <span className="text-[#242526] text-xs">{formatStringDateTimezoneUTC(mission.startAt)}</span>
-            </div>
-            <div>
-              <span className="text-gray-500 text-xs mr-1">Au</span>
-              <span className="text-[#242526] text-xs">{formatStringDateTimezoneUTC(mission.endAt)}</span>
-            </div>
-          </div>
-
-          {/* places disponibles */}
-          <div className="flex basis-[25%]">
-            {mission.placesLeft <= 0 ? (
-              <div className="font-medium text-xs text-gray-700"> {mission.placesLeft} place disponible</div>
-            ) : (
-              <div className="font-medium text-xs text-gray-700"> {mission.placesLeft} places disponibles</div>
-            )}
-          </div>
-          <div>
-            <button className="bg-blue-600 px-3 py-2 rounded-md text-sm text-blue-50 hover:brightness-110 active:brightness-125" onClick={onSend}>
-              Proposer la mission
-            </button>
-          </div>
+          <div>tags</div>
         </div>
       </div>
     </div>
+
+    //   </div>
+    //   <div className="flex justify-between">
+    //     <Link className="flex basis-[35%] items-center" to={`/mission/${mission._d}`}>
+    //       {/* icon */}
+    //       <div className="flex items-center mr-4"></div>
+    //       {/* mission info */}
+    //       <div className="flex flex-col flex-1 justify-center">
+    //         <div className="uppercase text-gray-500 font-medium text-[11px] tracking-wider mb-1">{mission.structureName}</div>
+    //         <div className="text-[#242526] font-bold text-base mb-2">{mission.name}</div>
+    //         {/* tags */}
+    //         {mission.domains && (
+    //           <div className=" inline-flex flex-wrap">
+    //             {mission.domains.map((tag, index) => {
+    //               return (
+    //                 <div
+    //                   key={index}
+    //                   className=" flex text-[11px] text-gray-600 rounded-full border-gray-200 border-[1px] justify-center items-center mb-2 mt-1 mr-1 px-3  py-0.5 font-medium ">
+    //                   {tag}
+    //                 </div>
+    //               );
+    //             })}
+    //             {mission.isMilitaryPreparation === "true" ? (
+    //               <div className="flex justify-center items-center bg-blue-900 text-white border-gray-200 border-[1px] rounded-full text-[11px] mb-2 mr-1 px-3 py-0.5 font-medium">
+    //                 Préparation militaire
+    //               </div>
+    //             ) : null}
+    //           </div>
+    //         )}
+    //       </div>
+    //     </Link>
+    //     <div className="flex basis-[65%] justify-between items-center">
+    //       {/* date */}
+    //       <div className="flex flex-col basis-[30%] justify-center">
+    //         <div>
+    //           <span className="text-gray-500 mr-1 text-xs">Du</span>
+    //           <span className="text-[#242526] text-xs">{formatStringDateTimezoneUTC(mission.startAt)}</span>
+    //         </div>
+    //         <div>
+    //           <span className="text-gray-500 text-xs mr-1">Au</span>
+    //           <span className="text-[#242526] text-xs">{formatStringDateTimezoneUTC(mission.endAt)}</span>
+    //         </div>
+    //       </div>
+
+    //       {/* places disponibles */}
+    //       <div className="flex basis-[25%]">
+    //         {mission.placesLeft <= 0 ? (
+    //           <div className="font-medium text-xs text-gray-700"> {mission.placesLeft} place disponible</div>
+    //         ) : (
+    //           <div className="font-medium text-xs text-gray-700"> {mission.placesLeft} places disponibles</div>
+    //         )}
+    //       </div>
+    //       <div>
+    //         <button className="bg-blue-600 px-3 py-2 rounded-md text-sm text-blue-50 hover:brightness-110 active:brightness-125" onClick={onSend}>
+    //           Proposer la mission
+    //         </button>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
