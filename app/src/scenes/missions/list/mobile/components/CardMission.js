9ee5403e58b7da8dde192b2296ca1e44170c96fc
Theret Guillaume <73123940+guiTheret@users.noreply.github.com>
2023-02-02 14:45:27 +0100

    feat(app): Ajout date "Trouvez une mission"

--- app/src/scenes/missions/list/mobile/components/CardMission.js
+++ app/src/scenes/missions/list/mobile/components/CardMission.js
@@ -3,11 +3,13 @@ import { Link } from "react-router-dom";
 import LocationMarker from "../../../../../assets/icons/LocationMarker";
 import IconDomain from "../../../components/IconDomain";
 import { getDistance } from "../../../../../utils";
+import dayjs from "dayjs";
+import Calendar from "../../../../../assets/icons/Calendar";
 import House from "../../../components/HouseIcon";
 
 export default function mission({ mission, youngLocation }) {
   return (
-    <Link to={`/mission/${mission._id}`} className="bg-white relative flex  justify-between shadow-nina rounded-xl p-3 border-[1px] border-[#ffffff] mb-4 z-10">
+    <Link to={`/mission/${mission._id}`} className="bg-white relative flex  justify-between shadow-nina rounded-xl p-3 border-[#ffffff] mb-4 z-10 pt-4 overflow-hidden ">
       <div className="flex flex-1">
         {/* icon */}
         <div className="flex items-center mr-3">
@@ -22,14 +24,23 @@ export default function mission({ mission, youngLocation }) {
             </div>
             <div className="text-gray-900 font-bold text-base">{mission?.name}</div>
           </div>
-          <div className="flex flex-1 items-center justify-between">
-            <div className="flex flex-1 items-center justify-start">
-              <div className="text-gray-500 text-xs font-medium">&nbsp;{mission?.placesLeft} places disponibles</div>
+          <div className="flex flex-1 items-center flex-row w-full mt-2">
+            {/* 
+            
+
+            */}
+            <div className="flex flex-1 flex-row text-xs gap-2 mx-2 text-gray-900 font-bold  items-center justify-start">
+              <Calendar width={10} heigth={16} className=" min-w-[14px] min-h-[20px] text-gray-400" />
+              <div className="flex flex-row gap-1 flex-wrap">
+                <div className="whitespace-nowrap">du {dayjs(mission.startAt).format("DD/MM/YYYY")}</div>
+                <div className="whitespace-nowrap">au {dayjs(mission.endAt).format("DD/MM/YYYY")}</div>
+              </div>
             </div>
+
             {youngLocation && mission.location ? (
               <div className="flex items-center justify-end space-x-2">
                 <LocationMarker className="text-gray-400" />
-                <div className="text-gray-800 text-base font-bold">
+                <div className="text-gray-800 text-xs font-bold">
                   à {getDistance(youngLocation.lat, youngLocation.lon, mission.location.lat, mission.location.lon).toFixed(1)} km
                 </div>
                 {mission?.hebergement === "true" ? (
@@ -51,6 +62,12 @@ export default function mission({ mission, youngLocation }) {
             ) : (
               <div />
             )}
+          </div>
+        </div>
+        {/* top-[-1px] to overlap parent div border */}
+        <div className="absolute right-[-1px] top-[-1px] bg-gray-200 rounded-bl-lg py-0.5 px-2 border-[1px] border-gray-200 overflow-hidden">
+          <div className="flex flex-1 items-center justify-start">
+            <div className="text-gray-500 text-xs font-medium">&nbsp;{mission?.placesLeft} places disponibles</div>
           </div>
         </div>
       </div>
