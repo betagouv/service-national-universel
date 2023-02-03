eb446818874ab45fec9c456e58ac4b389a9f3a67
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2023-02-03 17:00:24 +0100

    done

--- admin/src/components/views/Historic2.js
+++ admin/src/components/views/Historic2.js
@@ -67,7 +67,6 @@ export default function Historic({ model, data, customFilterOptions, refName }) 
         </div>
         {isOpen && (
           <div className="flex flex-wrap gap-4">
-            {/* <Combobox /> */}
             <MultiSelect options={pathOptions} selected={paths} onChange={setPaths} label="Donnée modifiée" />
             <MultiSelect options={opOptions} selected={ops} onChange={setOps} label="Type d'action" />
             <MultiSelect options={userOptions} selected={authors} onChange={setAuthors} label="Auteur de la modification" />
@@ -153,69 +152,8 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-function Combobox() {
-  const [query, setQuery] = useState("");
-  const [selectedPerson, setSelectedPerson] = useState(null);
-
-  const people = [
-    { id: 1, name: "Leslie Alexander" },
-    // More users...
-  ];
-
-  function classNames(...classes) {
-    return classes.filter(Boolean).join(" ");
-  }
-
-  const filteredPeople =
-    query === ""
-      ? people
-      : people.filter((person) => {
-          return person.name.toLowerCase().includes(query.toLowerCase());
-        });
-
-  return (
-    <Combobox as="div" value={selectedPerson} onChange={setSelectedPerson} multiple>
-      <Combobox.Label className="block text-sm font-medium text-gray-700">Assigned to</Combobox.Label>
-      <div className="relative mt-1">
-        <Combobox.Input
-          className="w-full rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
-          onChange={(event) => setQuery(event.target.value)}
-          displayValue={(person) => person?.name}
-        />
-        <Combobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
-          <BsChevronUp className="h-5 w-5 text-gray-400" aria-hidden="true" />
-        </Combobox.Button>
-
-        {filteredPeople.length > 0 && (
-          <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-            {filteredPeople.map((person) => (
-              <Combobox.Option
-                key={person.id}
-                value={person}
-                className={({ active }) => classNames("relative cursor-default select-none py-2 pl-3 pr-9", active ? "bg-indigo-600 text-white" : "text-gray-900")}>
-                {({ active, selected }) => (
-                  <>
-                    <span className={classNames("block truncate", selected && "font-semibold")}>{person.name}</span>
-
-                    {selected && (
-                      <span className={classNames("absolute inset-y-0 right-0 flex items-center pr-4", active ? "text-white" : "text-indigo-600")}>
-                        <AiOutlineCheck className="h-5 w-5" aria-hidden="true" />
-                      </span>
-                    )}
-                  </>
-                )}
-              </Combobox.Option>
-            ))}
-          </Combobox.Options>
-        )}
-      </div>
-    </Combobox>
-  );
-}
-
 function MultiSelect({ options, selected, onChange, label }) {
   const [query, setQuery] = useState("");
-  console.log("🚀 ~ file: Historic2.js:157 ~ MultiSelect ~ query", query);
   const filteredOptions = options.filter((option) => option.label.toLowerCase().includes(query.toLowerCase()));
 
   return (
@@ -238,10 +176,10 @@ function MultiSelect({ options, selected, onChange, label }) {
 
             <Transition show={open} as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
               <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                <input onChange={(e) => setQuery(e.target.value)} placeholder="Rechercher" />
+                <input onChange={(e) => setQuery(e.target.value)} placeholder="Rechercher..." className="w-full py-2 px-3 border-b text-xs" />
                 {filteredOptions.map((option) => (
                   <Listbox.Option
-                    key={option.label}
+                    key={option.value}
                     className={({ active }) => classNames(active ? "text-white bg-blue-600" : "text-gray-900", "relative cursor-default select-none py-2 pl-3 pr-9 list-none")}
                     value={option.value}>
                     {({ selected, active }) => (
