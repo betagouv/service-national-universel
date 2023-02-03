a80462cf3e30b3a079109c2b50dfda3b2449835f
Theret Guillaume <73123940+guiTheret@users.noreply.github.com>
2023-02-03 09:08:18 +0100

    feat(admin): Fiche volontaire : phase 2 - outils d’aide - mission personnalisée

--- admin/src/scenes/volontaires/components/Toolbox.js
+++ admin/src/scenes/volontaires/components/Toolbox.js
@@ -4,14 +4,16 @@ import { HiOutlineSearch, HiOutlineAdjustments } from "react-icons/hi";
 import Screwdriver from "../../../assets/icons/Screwdriver";
 import AdjustableWrench from "../../../assets/icons/AdjustableWrench";
 import ProposalMission from "../view/proposalMission";
-import CreateMission from "../view/createMission";
 import { useHistory } from "react-router-dom";
 import { supportURL } from "../../../config";
 
+import { COHESION_STAY_END } from "../../../utils";
+import ReactTooltip from "react-tooltip";
+
 export default function Toolbox({ young }) {
   const [openProposalMission, setOpenProposalMission] = React.useState(false);
-  const [openCreateMission, setOpenCreateMission] = React.useState(false);
   const history = useHistory();
+  const canApplyToPhase2 = ["DONE", "EXEMPTED"].includes(young.statusPhase1) && new Date() >= COHESION_STAY_END[young.cohort];
 
   return (
     <div className="flex flex-col">
@@ -35,7 +37,6 @@ export default function Toolbox({ young }) {
           <button
             className="group flex gap-1 rounded-[10px] border-[1px] py-2 items-center justify-center border-blue-600 hover:border-[#4881FF] bg-blue-600 hover:bg-[#4881FF]"
             onClick={() => {
-              setOpenCreateMission(false);
               setOpenProposalMission(!openProposalMission);
             }}>
             <HiOutlineSearch className="text-blue-300 w-5 h-5" />
@@ -79,24 +80,23 @@ export default function Toolbox({ young }) {
             </div>
           </div>
           <button
-            className="group flex gap-1 rounded-[10px] border-[1px] py-2 items-center justify-center border-blue-600 hover:border-[#4881FF] bg-blue-600 hover:bg-[#4881FF]"
-            onClick={() => {
-              setOpenProposalMission(false);
-              setOpenCreateMission(!openCreateMission);
-            }}>
+            data-tip=""
+            data-for="tooltip-custom"
+            className={`group flex gap-1 rounded-[10px] border-[1px] py-2 items-center justify-center border-blue-600  bg-blue-600 ${
+              canApplyToPhase2 ? "hover:border-[#4881FF] hover:bg-[#4881FF] " : "!cursor-not-allowed"
+            }`}
+            onClick={() => canApplyToPhase2 && history.push(`/volontaire/${young._id}/phase2/mission-personnalisé`)}>
             <HiOutlineAdjustments className="text-blue-300 w-5 h-5" />
-            <div className="text-blue-100 group-hover:text-white text-sm ">Créer une mission personnalisée</div>
+            <div className={`text-blue-100 text-sm ${canApplyToPhase2 && "group-hover:text-white"}`}>Créer une mission personnalisée</div>
           </button>
+          <ReactTooltip id="tooltip-custom" className="bg-white shadow-xl text-black !opacity-100" arrowColor="white" disable={false}>
+            <div className="text-[black]">Le jeune n&apos;est pas élibigle à la phase 2</div>
+          </ReactTooltip>
         </div>
       </div>
       {openProposalMission ? (
         <div className="bg-white rounded-xl px-2 pb-2.5">
           <ProposalMission young={young} onSend={() => history.go(0)} />{" "}
-        </div>
-      ) : null}
-      {openCreateMission ? (
-        <div className="bg-white rounded-xl">
-          <CreateMission young={young} onSend={() => history.go(0)} />{" "}
         </div>
       ) : null}
     </div>
