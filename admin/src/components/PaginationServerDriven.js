7ef7536ba9d3529da3037c102a040fe48dae1dab
Nicolas Bretécher <nicolas.bretecher@selego.co>
2023-02-08 18:14:44 +0100

    feat(admin,api): historique du plan de transports (#2128)

---
+++ admin/src/components/PaginationServerDriven.js
@@ -0,0 +1,71 @@
+/**
+ * Ce composant va avec le HistoricServerDriven.
+ * Il gère les données de pagination en provenance du serveur.
+ * Et il reprend la forme de la pagination des reactlist.
+ */
+import React from "react";
+
+const DEFAULT_DISPLAYED_PAGES = 4;
+
+export default function PaginationServerDriven({ pageCount, currentPage, count, itemsPerPage, itemsCount, className, changePage, displayedPages = DEFAULT_DISPLAYED_PAGES }) {
+  const lastPage = Math.min(pageCount - 1, Math.max(displayedPages, currentPage));
+  const firstPage = Math.max(lastPage - displayedPages + 1, 1);
+  const lastItem = currentPage * itemsPerPage + itemsCount;
+  const pages = [];
+  for (let i = firstPage; i <= lastPage; ++i) {
+    pages.push(<PageButton key={"page-" + i} page={i} changePage={changePage} active={currentPage === i} />);
+  }
+
+  function goToPrevious(e) {
+    e.preventDefault();
+    if (currentPage > 0) {
+      changePage && changePage(currentPage - 1);
+    }
+  }
+
+  function goToNext(e) {
+    e.preventDefault();
+    if (currentPage < pageCount) {
+      changePage && changePage(currentPage + 1);
+    }
+  }
+
+  return (
+    <div className={`flex items-center justify-end gap-2 ${className}`}>
+      <div className="text-[#242526] text-[12px]">
+        {currentPage * itemsPerPage + 1} - {currentPage * itemsPerPage + itemsCount} sur {count}
+      </div>
+      <a
+        href="#"
+        onClick={goToPrevious}
+        className={`flex items-center justify-center rounded-[3px] border-[1px] border-[transparent] bg-[#f7fafc] text-[#242526] py-[3px] px-[10px] ml-[5px] text-[12px] font-bold min-h-[30px] ${
+          currentPage > 0 ? "cursor-pointer" : "cursor-not-allowed"
+        }`}>
+        <img src={require("../assets/left.svg")} alt="icon left" />
+      </a>
+      <PageButton page={0} changePage={changePage} active={currentPage === 0} />
+      {pages}
+      <a
+        href="#"
+        onClick={goToNext}
+        className={`flex items-center justify-center rounded-[3px] border-[1px] border-[transparent] bg-[#f7fafc] text-[#242526] py-[3px] px-[10px] ml-[5px] text-[12px] font-bold min-h-[30px] ${
+          lastItem < count ? "cursor-pointer" : "cursor-not-allowed"
+        }`}>
+        <img src={require("../assets/right.svg")} alt="icon right" />
+      </a>
+    </div>
+  );
+}
+
+function PageButton({ page, changePage, active }) {
+  return (
+    <a
+      href="#"
+      onClick={() => changePage(page)}
+      className={`flex items-center justify-center rounded-[3px] border-[1px] border-[transparent] bg-[#f7fafc] text-[#242526] py-[3px] px-[10px] ml-[5px] text-[12px] cursor-pointer min-h-[30px] ${
+        active ? "font-bold" : "font-regular"
+      }`}>
+      {page + 1}
+    </a>
+  );
+}
---
+++ admin/src/components/views/HistoricServerDriven.js
@@ -0,0 +1,146 @@
+/**
+ * Cette version de l'historique considère que les données, la pagination et les filtres se font côté serveur.
+ */
+import React, { useState, useCallback } from "react";
+import { translateHistory, debounce } from "../../utils";
+import { formatLongDateFR, translateAction, translateBusPatchesField } from "snu-lib";
+import FilterIcon from "../../assets/icons/Filter";
+import UserCard from "../UserCard";
+import MultiSelect from "../../scenes/dashboard/components/MultiSelect";
+import { HiOutlineArrowRight } from "react-icons/hi";
+import PaginationServerDriven from "../PaginationServerDriven";
+import Loader from "../Loader";
+
+export default function HistoricServerDriven({ data, refName, path, pagination, changePage, filters, changeFilters, filterOptions, loading = false }) {
+  const [query, setQuery] = useState(filters?.query ? filters.query : "");
+  const [isOpen, setIsOpen] = useState(
+    filters && ((filters.op && filters.op.length > 0) || (filters.path && filters.path.length > 0) || (filters.author && filters.author.length > 0)),
+  );
+
+  const debouncedChangeFilter = useCallback(
+    debounce(async (value) => {
+      changeFilters({ ...filters, ...value });
+    }, 500),
+    [],
+  );
+
+  function changeQuery(value) {
+    setQuery(value);
+    debouncedChangeFilter({ query: value });
+  }
+
+  return (
+    <div className="w-full bg-white rounded-xl shadow-md text-slate-700">
+      <div className="w-full flex p-4 gap-4 justify-between items-center">
+        <div className="flex items-center gap-4 py-4">
+          <input onChange={(e) => changeQuery(e.target.value)} value={query} className="border p-2 rounded-lg w-64 text-xs" placeholder="Rechercher..." />
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className={`group py-2 px-3 rounded-lg flex items-center gap-2 ${isOpen ? "bg-gray-500 hover:bg-gray-100" : "bg-gray-100 hover:bg-gray-500"}`}>
+            <FilterIcon className={isOpen ? "fill-gray-100 group-hover:fill-gray-500" : "fill-gray-500 group-hover:fill-gray-100"} />
+            <p className={isOpen ? "text-gray-100 group-hover:text-gray-500" : "text-gray-500 group-hover:text-gray-100"}>Filtres</p>
+          </button>
+        </div>
+        {!loading && (
+          <PaginationServerDriven
+            pageCount={pagination.pageCount}
+            currentPage={pagination.page}
+            changePage={changePage}
+            count={pagination.count}
+            itemsPerPage={pagination.itemsPerPage}
+            itemsCount={data.length}
+            className="p-4"
+          />
+        )}
+      </div>
+      {isOpen && FilterDrawer({ filters, changeFilters, filterOptions })}
+      {loading ? (
+        <div className="pb-12">
+          <Loader />
+        </div>
+      ) : data.length === 0 ? (
+        <div className="italic p-4">Aucune donnée</div>
+      ) : (
+        <>
+          <table className="table-fixed w-full">
+            <thead>
+              <tr className="uppercase border-t border-t-slate-100">
+                {refName && <th className="font-normal px-4 py-3 text-xs text-gray-500">{refName}</th>}
+                <th className="font-normal px-4 py-3 text-xs text-gray-500">Action</th>
+                <th className="font-normal px-4 py-3 text-xs text-gray-500">Détails</th>
+                <th className="font-normal px-4 py-3 text-xs text-gray-500 w-16"></th>
+                <th className="font-normal px-4 py-3 text-xs text-gray-500"></th>
+                <th className="font-normal px-4 py-3 text-xs text-gray-500">Auteur</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((e, index) => (
+                <Event key={index} e={e} index={index} refName={refName} path={path} />
+              ))}
+            </tbody>
+          </table>
+          <hr className="border-t border-t-slate-100" />
+          <PaginationServerDriven
+            pageCount={pagination.pageCount}
+            currentPage={pagination.page}
+            changePage={changePage}
+            count={pagination.count}
+            itemsPerPage={pagination.itemsPerPage}
+            itemsCount={data.length}
+            className="p-4"
+          />
+        </>
+      )}
+    </div>
+  );
+}
+
+function Event({ e, index, refName, path }) {
+  return (
+    <tr key={index} className="border-t border-t-slate-100 hover:bg-slate-50 cursor-default">
+      {refName && (
+        <td className="px-4 py-3 cursor-pointer overflow-hidden">
+          <a href={`/${path}/${e.ref}`}>{e.refName}</a>
+        </td>
+      )}
+      <td className="px-4 py-3 overflow-hidden">
+        <p className="text-gray-400 truncate">
+          {translateAction(e.op)} • {formatLongDateFR(e.date)}
+        </p>
+        <p>{translateBusPatchesField(e.path)}</p>
+      </td>
+      <td className="px-4 py-3 truncate text-gray-400">{translateHistory(e.path, e.originalValue)}</td>
+      <td className="px-4 py-3">
+        <HiOutlineArrowRight />
+      </td>
+      <td className="px-4 py-3 truncate">{translateHistory(e.path, e.value)}</td>
+      <td className="px-4 py-3 overflow-hidden">
+        <UserCard user={e.user} />
+      </td>
+    </tr>
+  );
+}
+
+function FilterDrawer({ filters, changeFilters, filterOptions }) {
+  function getAuthorOptions() {
+    return filterOptions ? filterOptions.user.map((e) => ({ label: e.firstName + " " + e.lastName, value: e._id })) : [];
+  }
+
+  return (
+    <div className="flex flex-wrap gap-4 p-4 bg-slate-50">
+      <MultiSelect
+        options={filterOptions ? filterOptions.path.map((e) => ({ label: translateBusPatchesField(e) + " - " + e, value: e })) : []}
+        value={filters.path}
+        onChange={(path) => changeFilters((f) => ({ ...f, ...{ path } }))}
+        label="Donnée modifiée"
+      />
+      <MultiSelect
+        options={filterOptions ? filterOptions.op.map((e) => ({ label: translateAction(e), value: e })) : []}
+        value={filters.op}
+        onChange={(op) => changeFilters((f) => ({ ...f, ...{ op } }))}
+        label="Type d'action"
+      />
+      <MultiSelect options={getAuthorOptions()} value={filters.author} onChange={(author) => changeFilters((f) => ({ ...f, ...{ author } }))} label="Auteur de la modification" />
+    </div>
+  );
+}
