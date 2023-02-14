f2844300fe1e99f8dd3503b98fbe70e51d346e97
Nicolas Bretécher <nicolas.bretecher@selego.co>
2023-02-14 11:05:36 +0100

    feat(api/admin): refonte onglet notifications avec panneau de mail envoyé (#2240)
    Co-authored-by: Pierre LE BIHAN <lebihanp@hotmail.fr>
    Co-authored-by: hlecourt <50077368+hlecourt@users.noreply.github.com>

--- admin/src/scenes/volontaires/view/notifications.js
+++ admin/src/scenes/volontaires/view/notifications.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-import Emails from "../../../components/views/Emails";
+import Emails from "../../../components/views/Emails2";
 import YoungHeader from "../../phase0/components/YoungHeader";
 
 export default function Notifications({ young, onChange }) {
   return (
     <>
       <YoungHeader young={young} tab="notifications" onChange={onChange} />
-      <div className="p-[30px]">
+      <div className="m-8">
         <Emails email={young.email} />
       </div>
     </>
