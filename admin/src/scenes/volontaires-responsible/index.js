a43f6cd51cc6aed4469e311c41723867a0760b8e
Theret Guillaume <73123940+guiTheret@users.noreply.github.com>
2023-02-15 16:18:31 +0000

    clear volontaire-responsible list

--- admin/src/scenes/volontaires-responsible/index.js
+++ admin/src/scenes/volontaires-responsible/index.js
@@ -1,26 +1,15 @@
 import React from "react";
 import { Switch } from "react-router-dom";
-import { environment } from "../../config";
 import { SentryRoute } from "../../sentry";
 
 import ListV2 from "./listV2";
-import List from "./list";
 import View from "./view";
 
 export default function Index() {
   return (
     <Switch>
-      {environment === "production" ? (
-        <>
-          <SentryRoute path="/volontaire/:id" component={View} />
-          <SentryRoute path="/volontaire" exact component={List} />
-        </>
-      ) : (
-        <>
-          <SentryRoute path="/volontaire/list/:currentTab" component={ListV2} />
-          <SentryRoute path="/volontaire/:id/phase2/application/:applicationId" component={View} />
-        </>
-      )}
+      <SentryRoute path="/volontaire/list/:currentTab" component={ListV2} />
+      <SentryRoute path="/volontaire/:id/phase2/application/:applicationId" component={View} />
     </Switch>
   );
 }
