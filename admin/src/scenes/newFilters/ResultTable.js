3c005882cf11907b8095a65195907c55b475b26d
guiTheret <gtheret@sfr.fr>
2023-02-22 15:24:05 +0100

    Update ResultTable.js

--- admin/src/scenes/newFilters/ResultTable.js
+++ admin/src/scenes/newFilters/ResultTable.js
@@ -3,15 +3,14 @@ import React, { useEffect } from "react";
 import PaginationServerDriven from "../../components/PaginationServerDriven";
 export default function ResultTable({ render, currentCount, setPage, pagination = true, size, page, count }) {
   const pageCount = Math.ceil(count / size);
-  const currentPage = page;
   useEffect(() => {
-    setPage(1);
+    setPage(0);
   }, [count]);
   return (
     <div>
       {render}{" "}
       {pagination && (
-        <PaginationServerDriven pageCount={pageCount} currentPage={currentPage} changePage={setPage} count={count} itemsPerPage={size} itemsCount={currentCount} className="p-4" />
+        <PaginationServerDriven pageCount={pageCount} currentPage={page} changePage={setPage} count={count} itemsPerPage={size} itemsCount={currentCount} className="p-4" />
       )}
     </div>
   );
