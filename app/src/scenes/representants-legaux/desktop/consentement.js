9b896e080a1218f5ff41cf9c00d8ce76cc6b4601
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2023-02-24 16:26:19 +0100

    fix(admin): utilisation du regex pour les pages de consentement

--- app/src/scenes/representants-legaux/desktop/consentement.js
+++ app/src/scenes/representants-legaux/desktop/consentement.js
@@ -18,6 +18,7 @@ import api from "../../../services/api";
 import { BorderButton, PlainButton } from "../components/Buttons";
 import plausibleEvent from "../../../services/plausible";
 import AuthorizeBlock from "../components/AuthorizeBlock";
+import { regexPhoneFrenchCountries } from "../../../utils/";
 
 export default function Consentement({ step, parentId }) {
   const history = useHistory();
@@ -196,7 +197,7 @@ export default function Consentement({ step, parentId }) {
       validate("email", "invalid", !validator.isEmail(data.email));
     }
     if (validate("phone", "empty", validator.isEmpty(data.phone, { ignore_whitespace: true }))) {
-      validate("phone", "invalid", !validator.isMobilePhone(data.phone, ["fr-FR", "fr-GF", "fr-GP", "fr-MQ", "fr-RE"]));
+      validate("phone", "invalid", !validator.matches(data.phone, regexPhoneFrenchCountries));
     }
 
     // --- address
