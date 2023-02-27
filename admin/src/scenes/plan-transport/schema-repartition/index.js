6559b4059ed01ecb1ab1558e21b2d8636fb31821
Bryan BERGER <contact@weberger.net>
2023-02-27 12:54:06 +0100

    fix(admin): fix custom redirection on restricted child route

--- admin/src/scenes/plan-transport/schema-repartition/index.js
+++ admin/src/scenes/plan-transport/schema-repartition/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Switch } from "react-router-dom";
 import { ROLES } from "snu-lib";
 import { SentryRoute } from "../../../sentry";
@@ -9,11 +10,23 @@ export default function SchemaRepartitionIndex(props) {
   const region = props.match && props.match.params && props.match.params.region;
   const department = props.match && props.match.params && props.match.params.department;
 
+  const user = useSelector((state) => state.Auth.user);
+
+  const redirectUnauthorizedToObject = {
+    [`/schema-repartition/${user.region}`]: [ROLES.REFERENT_REGION],
+    [`/schema-repartition/${user.region}/${user.department}`]: ROLES.REFERENT_DEPARTMENT,
+  };
+
   return (
     <Switch>
       <SentryRoute path="/schema-repartition/:region/:department" component={() => <SchemaRepartition region={region} department={department} />} />
       <SentryRoute path="/schema-repartition/:region" component={() => <SchemaRepartition region={region} />} />
-      <RestrictedChildRoute path="/schema-repartition" component={() => <SchemaRepartition />} roles={[ROLES.ADMIN, ROLES.TRANSPORTER]} />
+      <RestrictedChildRoute
+        path="/schema-repartition"
+        component={() => <SchemaRepartition />}
+        allowedRoles={[ROLES.ADMIN, ROLES.TRANSPORTER]}
+        redirectUnauthorizedTo={redirectUnauthorizedToObject}
+      />
     </Switch>
   );
 }
--- admin/src/utils/components/RestrictedChildRoute.js
+++ admin/src/utils/components/RestrictedChildRoute.js
@@ -4,10 +4,35 @@ import { Redirect } from "react-router-dom";
 import { ROLES_LIST } from "snu-lib";
 import { SentryRoute } from "../../sentry";
 
-const RestrictedChildRoute = ({ component: Component, roles = ROLES_LIST, redirectTo = "/dashboard", ...rest }) => {
+const RestrictedChildRoute = ({ component: Component, allowedRoles = ROLES_LIST, redirectUnauthorizedTo = "/dashboard", ...rest }) => {
   const user = useSelector((state) => state.Auth.user);
 
-  if (!roles.includes(user.role)) {
+  let redirectTo = redirectUnauthorizedTo;
+
+  if (typeof redirectUnauthorizedTo === "object") {
+    const routeRule = Object.entries(redirectUnauthorizedTo).find(([, routeRuleValue]) => {
+      if (typeof routeRuleValue === "string" && routeRuleValue === user.role) {
+        return true;
+      }
+      if (typeof routeRuleValue !== "string" && routeRuleValue.includes(user.role)) {
+        return true;
+      }
+    });
+
+    if (!routeRule) {
+      redirectTo = redirectUnauthorizedTo.default || "/dashboard";
+      return;
+    }
+
+    const [routeRuleKey] = routeRule;
+    redirectTo = routeRuleKey;
+  }
+
+  if (!allowedRoles.includes(user.role)) {
+    return <Redirect to={redirectTo} />;
+  }
+
+  if (!allowedRoles.includes(user.role)) {
     return <Redirect to={redirectTo} />;
   }
   return <SentryRoute {...rest} render={(props) => <Component {...props} />} />;
