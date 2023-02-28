d7112396c95d1c058aa16cc647eb5e7851d76fa6
Bryan BERGER <contact@weberger.net>
2023-02-28 17:54:50 +0100

    fix(admin): fix schema repartition routing restrictions

--- admin/src/scenes/plan-transport/schema-repartition/index.js
+++ admin/src/scenes/plan-transport/schema-repartition/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Switch } from "react-router-dom";
+import { Redirect, Switch } from "react-router-dom";
 import { department2region, ROLES } from "snu-lib";
-import RestrictedChildRoute from "../../../utils/components/RestrictedChildRoute";
+import { SentryRoute } from "../../../sentry";
 import SchemaRepartition from "./SchemaRepartition";
 
 export default function SchemaRepartitionIndex(props) {
@@ -11,47 +11,52 @@ export default function SchemaRepartitionIndex(props) {
 
   const user = useSelector((state) => state.Auth.user);
 
-  const redirectUnauthorizedToObject = {
-    [`/schema-repartition/${user.region}`]: [ROLES.REFERENT_REGION],
-    [`/schema-repartition/${department2region[user.department[0]]}/${user.department[0]}`]: ROLES.REFERENT_DEPARTMENT,
+  const getRedirectionRoute = () => {
+    if (user.role === ROLES.REFERENT_DEPARTMENT) {
+      return `/schema-repartition/${department2region[user.department[0]]}/${user.department[0]}`;
+    }
+    if (user.role === ROLES.REFERENT_REGION) {
+      return `/schema-repartition/${user.region}`;
+    }
+    return "/dashboard";
   };
 
   return (
     <Switch>
-      <RestrictedChildRoute
+      <SentryRoute
         path="/schema-repartition/:region/:department"
-        component={() => <SchemaRepartition region={region} department={department} />}
-        restrictionRules={{
-          $or: [
-            [ROLES.ADMIN, ROLES.TRANSPORTER].includes(user.role),
-            {
-              $and: [ROLES.REFERENT_REGION === user.role, region === user.region],
-            },
-            {
-              $and: [ROLES.REFERENT_DEPARTMENT === user.role, user.department.includes(department), department2region[department] === region],
-            },
-          ],
+        component={() => {
+          const userDepartment = user.department.find((dep) => dep === department);
+
+          if (![ROLES.ADMIN, ROLES.TRANSPORTER, ROLES.REFERENT_REGION, ROLES.REFERENT_DEPARTMENT].includes(user.role)) {
+            return <Redirect to={getRedirectionRoute()} />;
+          }
+          if (
+            (ROLES.REFERENT_REGION === user.role && region !== user.region) ||
+            (ROLES.REFERENT_DEPARTMENT === user.role && !userDepartment && department2region[userDepartment] !== region)
+          ) {
+            return <Redirect to={getRedirectionRoute()} />;
+          }
+          return <SchemaRepartition region={region} department={department} />;
         }}
-        redirectUnauthorizedTo={redirectUnauthorizedToObject}
       />
-      <RestrictedChildRoute
+      <SentryRoute
         path="/schema-repartition/:region"
-        component={() => <SchemaRepartition region={region} />}
-        restrictionRules={{
-          $or: [
-            [ROLES.ADMIN, ROLES.TRANSPORTER].includes(user.role),
-            {
-              $and: [ROLES.REFERENT_REGION === user.role, region === user.region],
-            },
-          ],
+        component={() => {
+          if (![ROLES.ADMIN, ROLES.TRANSPORTER, ROLES.REFERENT_REGION].includes(user.role) || (ROLES.REFERENT_REGION === user.role && region !== user.region)) {
+            return <Redirect to={getRedirectionRoute()} />;
+          }
+          return <SchemaRepartition region={region} />;
         }}
-        redirectUnauthorizedTo={redirectUnauthorizedToObject}
       />
-      <RestrictedChildRoute
+      <SentryRoute
         path="/schema-repartition"
-        component={() => <SchemaRepartition />}
-        restrictionRules={[ROLES.ADMIN, ROLES.TRANSPORTER].includes(user.role)}
-        redirectUnauthorizedTo={redirectUnauthorizedToObject}
+        component={() => {
+          if (![ROLES.ADMIN, ROLES.TRANSPORTER].includes(user.role)) {
+            return <Redirect to={getRedirectionRoute()} />;
+          }
+          return <SchemaRepartition />;
+        }}
       />
     </Switch>
   );
