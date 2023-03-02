42dd5a99bd96392fe96cad0c019d20b0683db6ac
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2023-03-02 11:13:33 +0100

    wip

--- app/src/components/navbar/components/Menu.js
+++ app/src/components/navbar/components/Menu.js
@@ -8,6 +8,7 @@ import IconPhase3 from "../assets/IconPhase3";
 import IconHelp from "../assets/IconHelp";
 import LogoDiagoriente from "../assets/logoDiagoriente.svg";
 import Socials from "./Socials";
+import { permissionPhase1, permissionPhase2, permissionPhase3 } from "../../../utils";
 
 export default function Menu() {
   const young = useSelector((state) => state.Auth.young);
@@ -16,9 +17,9 @@ export default function Menu() {
     <nav className="my-3">
       <ul>
         <MenuItem to="/" icon={<IconHome />} text="Accueil" />
-        <MenuItem to="phase1" icon={<IconPhase1 />} text="Phase 1 - Séjour" status={young.statusPhase1} />
-        <MenuItem to="phase2" icon={<IconPhase2 />} text="Phase 2 - MIG" status={young.statusPhase2} />
-        <MenuItem to="phase3" icon={<IconPhase3 />} text="Phase 3 - Engagement" status={young.statusPhase3} />
+        <MenuItem to="phase1" icon={<IconPhase1 />} text="Phase 1 - Séjour" enabled={permissionPhase1(young)} status={young.statusPhase1} />
+        <MenuItem to="phase2" icon={<IconPhase2 />} text="Phase 2 - MIG" enabled={permissionPhase2(young)} status={young.statusPhase2} />
+        <MenuItem to="phase3" icon={<IconPhase3 />} text="Phase 3 - Engagement" enabled={permissionPhase3(young)} status={young.statusPhase3} />
         <div className="m-8" />
         <MenuItem to="/public-besoin-d-aide" icon={<IconHelp />} text="Besoin d'aide ?" />
         <Diagoriente />
@@ -28,30 +29,39 @@ export default function Menu() {
   );
 }
 
-function MenuItem({ to, icon, text, status }) {
-  const disabled = status ? !["DONE", "VALIDATED", "IN_PROGRESS"].includes(status) : false;
+function MenuItem({ to, enabled = true, icon, text, status }) {
+  const isActive = (to === "/" && location.pathname === "/") || (to !== "/" && location.pathname.includes(to));
 
   return (
     <li className="flex items-center">
-      {disabled ? (
-        <div className="mx-2 my-[1px] px-3 py-3 w-full rounded-md flex gap-3 text-[#526187] cursor-default items-center">
-          {icon}
-          <span>{text}</span>
-        </div>
-      ) : (
+      {enabled ? (
         <NavLink
           to={to}
           exact
-          className="mx-2 my-[2px] px-3 py-3 w-full rounded-md flex gap-3 text-[#D1DAEF] hover:bg-[#1B243D] hover:text-[#D1DAEF] items-center transition-colors duration-200"
+          className="mx-2 my-[2px] px-2 py-3 w-full rounded-md flex gap-4 text-[#D1DAEF] hover:bg-[#1B243D] hover:text-[#D1DAEF] items-center transition-colors duration-200"
           activeClassName="bg-[#344264] text-[#67A4FF] hover:bg-[#344264] hover:text-[#67A4FF]">
+          {icon && <div className={`${isActive ? "text-[#67A4FF]" : "text-[#7A90C3]"}`}>{icon}</div>}
+          {text && <span>{text}</span>}
+          {status && <StatusPill status={status} />}
+        </NavLink>
+      ) : (
+        <div className="mx-2 my-[1px] px-2 py-3 w-full rounded-md flex gap-4 text-[#526187] cursor-default items-center">
           {icon}
           <span>{text}</span>
-          {status === "IN_PROGRESS" && <span className="bg-[#2563EB] rounded-full text-xs text-[#D1DAEF] px-2 py-0.5 shadow-sm ml-auto">En cours</span>}
-          {status === "EXEMPTED" && <span className="bg-[#1E3A8A] rounded-full text-xs text-[#D1DAEF] px-2 py-0.5 shadow-sm ml-auto">Dispensé</span>}
-        </NavLink>
+        </div>
       )}
     </li>
   );
+}
+
+function StatusPill({ status }) {
+  if (status && status === "IN_PROGRESS") {
+    return <span className="bg-[#2563EB] rounded-full text-xs text-[#D1DAEF] px-2 py-0.5 shadow-sm ml-auto">En cours</span>;
+  }
+  if (status && status === "EXEMPTED") {
+    return <span className="bg-[#1E3A8A] rounded-full text-xs text-[#D1DAEF] px-2 py-0.5 shadow-sm ml-auto">Dispensé</span>;
+  }
+  return null;
 }
 
 function Diagoriente() {
--- app/src/components/navbar/components/Socials.js
+++ app/src/components/navbar/components/Socials.js
@@ -7,13 +7,13 @@ export default function Socials() {
   return (
     <div className="flex justify-end px-4 gap-6 items-center text-[#7A90C3]">
       <a href="https://www.facebook.com/snu.jemengage/" target="_blank" rel="noreferrer">
-        <IconFacebook className="hover:text-[#D1DAEF]" />
+        <IconFacebook className="hover:text-[#D1DAEF] transition-colors duration-200" />
       </a>
       <a href="https://twitter.com/snujemengage" target="_blank" rel="noreferrer">
-        <IconTwitter className="hover:text-[#D1DAEF]" />
+        <IconTwitter className="hover:text-[#D1DAEF] transition-colors duration-200" />
       </a>
       <a href="https://www.instagram.com/snujemengage/" target="_blank" rel="noreferrer">
-        <IconInstagram className="hover:text-[#D1DAEF]" />
+        <IconInstagram className="hover:text-[#D1DAEF] transition-colors duration-200" />
       </a>
     </div>
   );
