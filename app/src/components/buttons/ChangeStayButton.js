c4f0a6755a48e7a63ee614308c1facd7f4ffca8d
Bryan BERGER <contact@weberger.net>
2023-03-09 13:58:16 +0100

    fix(app): fix withdrawal button width in profile page

--- app/src/components/buttons/ChangeStayButton.js
+++ app/src/components/buttons/ChangeStayButton.js
@@ -7,7 +7,7 @@ const ChangeStayButton = () => {
   return (
     <Link
       to="/changer-de-sejour"
-      className="my-4 md:my-0 h-fit border-[1px] border-gray-300 text-gray-700 outline-none rounded-md font-medium text-sm block w-auto py-[10px] px-10 hover:text-gray-700">
+      className="my-2 md:my-0 h-fit border-[1px] border-gray-300 text-gray-700 outline-none rounded-md font-medium text-sm block w-auto py-[10px] px-10 hover:text-gray-700">
       Changer de séjour
     </Link>
   );
