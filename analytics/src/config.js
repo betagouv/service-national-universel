34987257054acc1fd8ec729a4c7b71e9428b4965
michaelgarcia75 <99188042+michaelgarcia75@users.noreply.github.com>
2023-03-10 16:11:14 +0100

    feat(analytics/api): add sentry & testroute sentry (#2351)

--- analytics/src/config.js
+++ analytics/src/config.js
@@ -4,10 +4,15 @@ const POSTGRESQL = process.env.POSTGRESQL;
 const SECRET_API_KEY = process.env.SECRET_API_KEY || "api-key";
 const JWT_SECRET = process.env.JWT_SECRET || "not-so-secret";
 
+const SENTRY_URL = process.env.SENTRY_URL || "";
+const SENTRY_TRACING_SAMPLE_RATE = process.env.SENTRY_TRACING_SAMPLE_RATE || 1;
+
 module.exports = {
   PORT,
   POSTGRESQL,
   ENVIRONMENT,
   SECRET_API_KEY,
   JWT_SECRET,
+  SENTRY_URL,
+  SENTRY_TRACING_SAMPLE_RATE,
 };
