6bd9289dfb3afe7f5246f013a36550efcfcf9b5c
Nicolas Bretécher <nicolas.bretecher@selego.co>
2023-03-14 09:24:41 +0100

    feat(app): Mise en production nouvelle navbar #2363

--- app/src/components/layout/navbar/components/NavigationMenu.js
+++ app/src/components/layout/navbar/components/NavigationMenu.js
@@ -24,7 +24,7 @@ export default function NavigationMenu({ onClose = () => {} }) {
         <MenuLink to="/phase1" icon={<IconPhase1 />} text="Phase 1 - Séjour" enabled={permissionPhase1(young)} status={young.statusPhase1} onClose={onClose} />
         <MenuLink to="/phase2" icon={<IconPhase2 />} text="Phase 2 - MIG" enabled={permissionPhase2(young)} status={young.statusPhase2} onClose={onClose} />
         <MenuGroup to="/phase3" icon={<IconPhase3 />} text="Phase 3 - Engagement" enabled={permissionPhase3(young)} status={young.statusPhase3} onClose={onClose}>
-          <MenuLink to="/phase3/les-programmes" text="Les programmes" onClose={onClose} />
+          <MenuLink to="/les-programmes" text="Les programmes" onClose={onClose} />
           <MenuLink to="/phase3/mission" text="Trouver une mission" onClose={onClose} />
           <MenuLink to="/phase3/valider" text="Valider ma phase 3" onClose={onClose} />
         </MenuGroup>
