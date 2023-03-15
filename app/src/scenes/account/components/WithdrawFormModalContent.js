22f7519481db1e81a4b755d3388a3de760af4ac9
szilagyikinga <szilagyikinga42@gmail.com>
2023-03-15 15:09:10 +0100

    fix: type and mobile design (#2373)

--- app/src/scenes/account/components/WithdrawFormModalContent.js
+++ app/src/scenes/account/components/WithdrawFormModalContent.js
@@ -2,12 +2,12 @@ import React from "react";
 import Danger from "../../../assets/icons/Danger";
 import { CancelButton } from "../../../components/buttons/SimpleButtons";
 import { PlainButton } from "../../phase1/components/Buttons";
-import { WITHRAWN_REASONS } from "../../../utils";
 import Field from "./Field";
 import ArrowLeft from "../../../assets/icons/ArrowLeft";
 import { Title, SubTitle } from "./elements";
 
 const WithdrawFormModalContent = ({
+  withdrawnReasons,
   onCancel,
   withdrawnReason,
   setWithdrawnReason,
@@ -21,30 +21,29 @@ const WithdrawFormModalContent = ({
 }) => {
   return (
     <>
-      <div className="flex w-full justify-center relative">
-        <button className="absolute left-0 w-9 h-9 flex justify-center items-center bg-[#E5E7EB] rounded-full" onClick={onBack}>
+      <div className="flex w-full md:justify-center relative">
+        <button className="absolute left-0 w-9 h-9 justify-center items-center bg-[#E5E7EB] rounded-full hidden md:flex" onClick={onBack}>
           <ArrowLeft />
         </button>
         <Danger />
       </div>
 
-      <Title className="mt-8">{title}</Title>
+      <Title className="mt-6 md:mt-8">{title}</Title>
       <SubTitle>{subTitle}</SubTitle>
-      <div className="w-full px-3">
+      <div className="w-full md:px-3">
         <Field
           className="w-full"
           type="select"
-          options={WITHRAWN_REASONS}
+          options={withdrawnReasons}
           name="withdrawnReason"
           label="Motif"
           value={withdrawnReason}
           onChange={setWithdrawnReason}
-          transformer={(v) => WITHRAWN_REASONS.find((r) => r.value === v)?.label}
+          transformer={(v) => withdrawnReasons.find((r) => r.value === v)?.label}
         />
         <Field
           className="w-full mt-4"
           type="textarea"
-          options={WITHRAWN_REASONS}
           name="withdrawnMessage"
           label="Précisez la raison de votre désistement ici (obligatoire)"
           value={withdrawnMessage}
@@ -52,11 +51,14 @@ const WithdrawFormModalContent = ({
         />
       </div>
 
-      <div className="flex mt-10 w-full gap-3">
-        <CancelButton className="flex-1" onClick={onCancel} />
+      <div className="flex flex-col md:flex-row mt-10 w-full gap-3">
+        <CancelButton className="flex-1 hidden md:block" onClick={onCancel} />
         <PlainButton disabled={!withdrawnReason || !withdrawnMessage} className="flex-1" onClick={onConfirm}>
           {confirmButtonName}
         </PlainButton>
+        <CancelButton className="flex-1 md:hidden" onClick={onBack}>
+          Retour
+        </CancelButton>
       </div>
     </>
   );
