e93874344a691b7682dd3cd348c5443e8e553ef7
Bryan BERGER <contact@weberger.net>
2023-03-15 17:56:37 +0100

    chore(app): mise à jour de la config eslint pour mettre en evidence le code mort (#2378)

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -7,12 +7,24 @@ module.exports = {
     es6: true,
     jest: true,
   },
-  extends: ["eslint:recommended", "plugin:import/recommended", "plugin:prettier/recommended"],
+  extends: ["eslint:recommended", "plugin:import/recommended", "plugin:prettier/recommended", "plugin:import/errors", "plugin:import/warnings"],
   plugins: ["prettier", "jest"],
   parserOptions: {
     ecmaVersion: 2021,
   },
   rules: {
     "no-unreachable": "warn",
+    "no-unused-vars": "warn",
+    "import/no-unused-modules": [
+      1,
+      {
+        unusedExports: true,
+        missingExports: true,
+        ignoreExports: ["./.eslintrc.js", "./jest.config.js"],
+      },
+    ],
+  },
+  settings: {
+    "import/extensions": [".js"],
   },
 };
