be539bec81f3c618f6f846c878d44ca80f55a665
seb <seb@snarkfacgtory.fr>
2023-03-19 02:59:37 +0100

    Ajout des composants pour faire les graphs des pages du Dashboard

---
+++ admin/src/scenes/dashboardV2/components/graphs/graph-commons.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+export const graphColors = {
+  1: ["#1E40AF"],
+  2: ["#60A5FA", "#1E40AF"],
+  3: ["#93C5FD", "#3B82F6", "#1E40AF"],
+  4: ["#93C5FD", "#60A5FA", "#2563EB", "#1E40AF"],
+  5: ["#93C5FD", "#60A5FA", "#3B82F6", "#2563EB", "#1E40AF"],
+  6: ["#93C5FD", "#60A5FA", "#3B82F6", "#2563EB", "#1D4ED8", "#1E40AF"],
+  7: ["#BFDBFE", "#93C5FD", "#60A5FA", "#3B82F6", "#2563EB", "#1D4ED8", "#1E40AF"],
+};
+
+export function Legend({ color, name, value = null, className = "" }) {
+  return (
+    <div className={`flex flex-row-reverse items-center ${className}`}>
+      <div className="text-xs text-gray-600 ml-2">{name}</div>
+      <div className={`flex items-center`}>
+        <div className={`rounded-full w-[10px] h-[10px]`} style={{ backgroundColor: color }}></div>
+        {value !== null && value !== undefined && <div className="font-medium text-lg text-gray-900 ml-2">{value}</div>}
+      </div>
+    </div>
+  );
+}
+
+export function Legends({ labels, values = null, className = "" }) {
+  const colors = graphColors[labels.length];
+  return (
+    <div className={`flex ${className}`}>
+      {labels.map((label, idx) => (
+        <Legend color={colors[idx]} name={label} value={values ? values[idx] : null} key={labels} className="mr-4 last:mr-0" />
+      ))}
+    </div>
+  );
+}
--- admin/src/scenes/dashboardV2/moderator-ref/subscenes/general/index.js
+++ admin/src/scenes/dashboardV2/moderator-ref/subscenes/general/index.js
@@ -1,56 +1,69 @@
 import React from "react";
 
-import { Doughnut } from "react-chartjs-2";
 // eslint-disable-next-line import/namespace
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import DashboardContainer from "../../../components/DashboardContainer";
+import FullDoughnut from "../../../components/graphs/FullDoughnut";
+import DemiDoughnut from "../../../components/graphs/DemiDoughnut";
+import { Legends } from "../../../components/graphs/graph-commons";
+import BarChart from "../../../components/graphs/BarChart";
+import RoundRatio from "../../../components/graphs/RoundRatio";
+import HorizontalBar from "../../../components/graphs/HorizontalBar";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
-
-export const data = {
-  labels: ["Occupées", "Disponibles"],
-  title: "Salles de réunion",
-  datasets: [
-    {
-      label: "# of Votes",
-      data: [12, 19],
-      backgroundColor: ["#1E40AF", "#3B82F6"],
-      borderWidth: 0,
-      with: 0,
-      height: 0,
-      padding: 0,
-    },
-  ],
-};
 
 export default function Index() {
   return (
     <DashboardContainer active="general" availableTab={["general", "engagement", "sejour", "inscription"]}>
-      <div className="w-[400px] bg-white mt-8">
-        <Doughnut
-          data={data}
-          options={{
-            rotation: -90,
-            circumference: 180,
-            cutout: "80%",
-            plugins: {
-              title: {
-                display: true,
-                text: "Custom Chart Title",
-                padding: {
-                  top: 10,
-                  bottom: 30,
-                },
-              },
-              legend: {
-                display: true,
-                labels: {
-                  usePointStyle: true,
-                  position: "bottom",
-                },
-              },
-            },
-          }}
+      <h4>Full Doughnut</h4>
+      <div className="bg-white m-8 p-8 grid grid-cols-2 gap-2">
+        <FullDoughnut title="Présence à l'arrivée" legendSide="left" labels={["Oui", "Non", "Non renseigné"]} values={[45, 23, 38]} />
+        <FullDoughnut title="Présence à l'arrivée" legendSide="bottom" labels={["Oui", "Non", "Non renseigné", "Décalé"]} values={[45, 23, 38, 12]} maxLegends={2} />
+        <FullDoughnut title="Présence à l'arrivée" legendSide="top" labels={["Oui", "Non", "Non renseigné"]} values={[45, 23, 38]} />
+        <FullDoughnut title="Présence à l'arrivée" legendSide="right" labels={["Oui", "Non", "Non renseigné", "Autre"]} values={[45, 23, 38, 25]} maxLegends={2} />
+      </div>
+
+      <h4>Demi Doughnut</h4>
+      <div className="bg-white m-8 px-8 pt-8">
+        <DemiDoughnut title="Places proposées" labels={["Occupées", "Disponibles"]} values={[40, 12]} />
+      </div>
+
+      <h4>Legends</h4>
+      <div className="bg-white m-8 p-8">
+        <Legends labels={["Première", "Seconde", "Troisième"]} values={[40, 12, 34]} className="mb-8" />
+        <Legends labels={["Première", "Seconde", "Troisième", "Sans valeur"]} />
+      </div>
+
+      <h4>Bar Chart</h4>
+      <div className="bg-white m-8 p-8 flex justify-between items-end">
+        <BarChart title="1. Solidarité" values={[16, 9]} unit="%" className="h-[100px]" />
+        <BarChart title="2. Test more" values={[16, 9, 25, 3]} unit="%" className="h-[200px] mt-8" />
+        <BarChart values={[16, 9, 25, 3]} noValue className="h-[200px] mt-8" />
+      </div>
+
+      <h4>Round Ratio</h4>
+      <div className="bg-white m-8 p-8 flex justify-around items-center">
+        <RoundRatio value={0.09} className="w-[64px] h-[64px]" />
+        <RoundRatio value={1} className="w-[128px] h-[128px]" />
+        <RoundRatio value={0.96} className="w-[128px] h-[128px]" />
+        <RoundRatio value={0.83} className="w-[64px] h-[64px]" />
+        <RoundRatio value={0.017} className="w-[64px] h-[64px]" />
+      </div>
+
+      <h4>Horizontal Bar</h4>
+      <div className="bg-white m-8 p-8">
+        <HorizontalBar
+          title="Objectif des inscriptions"
+          labels={["Sur la liste principale", "Sue liste complémentaire", "En attente de validation", "En attente de correction", "En cours"]}
+          values={[31527, 450, 901, 901, 3359]}
+          goal={45039}
+        />
+        <HorizontalBar
+          className="mt-8"
+          title="Objectif des inscriptions"
+          labels={["Sur la liste principale", "Sue liste complémentaire", "En attente de validation", "En attente de correction", "En cours"]}
+          values={[31527, 450, 901, 4901, 10359]}
+          goal={45039}
         />
       </div>
     </DashboardContainer>
