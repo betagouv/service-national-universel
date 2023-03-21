ead715fd8d2c028da5d4290f81438db2062fe716
guiTheret <gtheret@sfr.fr>
2023-03-21 17:14:30 +0100

    up

--- admin/src/scenes/dashboardV2/moderator-ref/subscenes/inscription/index.js
+++ admin/src/scenes/dashboardV2/moderator-ref/subscenes/inscription/index.js
@@ -1,8 +1,38 @@
 import React from "react";
 import ButtonPrimary from "../../../../../components/ui/buttons/ButtonPrimary";
 import DashboardContainer from "../../../components/DashboardContainer";
+import HorizontalBar from "../../../components/graphs/HorizontalBar";
 
+import { FilterDashBoard } from "../../../components/FilterDashBoard";
 export default function Index() {
+  const filterArray = [
+    {
+      name: "Région",
+      datafield: "region.keyword",
+      fullValue: "Toutes",
+      options: [
+        { key: "Ile de france", label: "Ile de france" },
+        { key: "Bretagne", label: "Bretagne" },
+        { key: "oui", label: "oui" },
+        { key: "oui oui", label: "oui oui" },
+      ],
+    },
+    {
+      name: "Département",
+      datafield: "departement.keyword",
+      fullValue: "Tous",
+      options: [
+        { key: "Ile de france", label: "Ile de france" },
+        { key: "Bretagne", label: "Bretagne" },
+      ],
+    },
+    {
+      name: "Date de début",
+      customComponent: (setFilter, filter) => <div>Custom component</div>,
+    },
+  ];
+
+  const [selectedFilters, setSelectedFilters] = React.useState({});
   return (
     <DashboardContainer
       active="inscription"
@@ -18,6 +48,15 @@ export default function Index() {
         </div>
       }>
       <div>Inscription</div>
+      <FilterDashBoard selectedFilters={selectedFilters} setSelectedFilters={setSelectedFilters} filterArray={filterArray} />
+      <div className="bg-white my-4 p-8 rounded-lg">
+        <HorizontalBar
+          title="Objectif des inscriptions"
+          labels={["Sur la liste principale", "Sue liste complémentaire", "En attente de validation", "En attente de correction", "En cours"]}
+          values={[31527, 450, 901, 901, 3359]}
+          goal={45039}
+        />
+      </div>
     </DashboardContainer>
   );
 }
