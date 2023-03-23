19764171f5554a6ab29cc962b0d2b3f71af7df44
guiTheret <gtheret@sfr.fr>
2023-03-23 16:35:04 +0100

    Update index.js

--- admin/src/scenes/dashboardV2/moderator-ref/subscenes/inscription/index.js
+++ admin/src/scenes/dashboardV2/moderator-ref/subscenes/inscription/index.js
@@ -10,12 +10,15 @@ import { YOUNG_STATUS, REFERENT_ROLES, departmentList, regionList, COHORTS, ES_N
 import { useSelector } from "react-redux";
 import StatutPhase from "../../../components/inscription/StatutPhase.js";
 
+import { BsChevronUp, BsChevronDown } from "react-icons/bs";
+
 export default function Index() {
   const user = useSelector((state) => state.Auth.user);
   const [inscriptionGoals, setInscriptionGoals] = useState();
 
   const [inscriptionDetailObject, setInscriptionDetailObject] = useState({});
-  const [inscriptionDetailObjectBottom, setInscriptionDetailObjectBottom] = useState({});
+  const [age, setAge] = useState({});
+  const [sexe, setSexe] = useState({});
 
   const filterArray = [
     {
@@ -51,6 +54,8 @@ export default function Index() {
     },
   ];
 
+  const [selectedDetail, setSelectedDetail] = useState("age");
+
   async function fetchInscriptionGoals() {
     const res = await getInscriptionGoals();
     setInscriptionGoals(res);
@@ -61,14 +66,20 @@ export default function Index() {
   }
 
   async function fetchDetailInscriptions() {
-    const res = await getDetailInscriptions(selectedFiltersBottom);
-    setInscriptionDetailObjectBottom(res);
+    const res = await getDetailInscriptions({ ...selectedFilters, ...selectedFiltersBottom });
+    // get all the years from the res object
+    const objectYears = {};
+    Object.keys(res.age).forEach((e) => {
+      const year = new Date(parseInt(e)).getFullYear();
+      if (!objectYears[year]) objectYears[year] = 1;
+      else objectYears[year] += 1;
+    });
+    setAge(objectYears);
+    setSexe(res.gender);
   }
 
   useEffect(() => {
     fetchInscriptionGoals();
-    fetchCurrentInscriptions();
-    fetchDetailInscriptions();
   }, []);
 
   useEffect(() => {
@@ -123,8 +134,8 @@ export default function Index() {
 
       <StatutPhase values={inscriptionDetailObject} />
 
-      <div className="min-w-[450px] w-[40%] bg-white rounded-lg my-4 py-6 px-8">
-        <div className="flex flex-row justify-between items-center">
+      <div className="min-w-[450px] w-[40%] bg-white rounded-lg my-4 py-6 px-8 flex items-center flex-col">
+        <div className="flex flex-row justify-between items-center w-full">
           <div className="text-base font-bold text-gray-900">En détail</div>
           <div className="w-fit">
             {filterArrayBottom.map((filter) => (
@@ -132,11 +143,60 @@ export default function Index() {
             ))}
           </div>
         </div>
-        <FullDoughnut title="Présence à l'arrivée" legendSide="right" labels={["Oui", "Non", "Non renseigné", "Autre"]} values={[45, 23, 38, 25]} maxLegends={2} />
+        {/* Filter to chose displayed graphs */}
+        <div className="py-10">
+          <FilterDetail selectedDetail={selectedDetail} setSelectedDetail={setSelectedDetail} />
+        </div>
+
+        <div className="flex flex-col justify-center items-center w-full gap-10">
+          <FullDoughnut
+            title="Âge"
+            legendSide="right"
+            labels={["Né en 2005", "Né en 2006", "Né en 2007", "Né en 2008"]}
+            values={[age["2005"], age["2006"], age["2007"], age["2008"]]}
+            maxLegends={2}
+          />
+          <FullDoughnut title="Sexe" legendSide="left" labels={["Garçons", "Filles"]} values={[sexe.male, sexe.female]} maxLegends={2} />
+        </div>
       </div>
     </DashboardContainer>
   );
 }
+
+const FilterDetail = ({ selectedDetail, setSelectedDetail }) => {
+  const optionsDetail = [
+    { key: "age", label: "Âge et sexe" },
+    { key: "class", label: "Classe et situation" },
+    { key: "situtaion", label: "Situations particulières" },
+    { key: "qpv", label: "Issus quartiers prioritaires et zones rurales" },
+  ];
+  const [showDetailFilter, setShowDetailFilter] = useState(false);
+  return (
+    <div className="relative">
+      <div className="flex flex-row gap-2 items-center p-2 cursor-pointer" onClick={() => setShowDetailFilter((open) => !open)}>
+        <div>{optionsDetail.find((e) => e.key === selectedDetail).label}</div>
+        {showDetailFilter && <BsChevronUp className="h-4 w-4 text-gray-900" aria-hidden="true" />}
+        {!showDetailFilter && <BsChevronDown className="h-4 w-4 text-gray-900" aria-hidden="true" />}
+      </div>
+
+      {showDetailFilter && (
+        <div className="absolute bg-white z-50 w-fit rounded-md shadow-sm">
+          {optionsDetail.map((option) => (
+            <div
+              key={option.key}
+              onClick={() => {
+                setShowDetailFilter(false);
+                setSelectedDetail(option.key);
+              }}
+              className="hover:bg-gray-100 p-2 cursor-pointer">
+              {option.label}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
 
 const getInscriptionGoals = async () => {
   let dataMerged = [];
@@ -166,7 +226,90 @@ function filterByRegionAndDepartement(e, filters, user) {
   return true;
 }
 
-async function getDetailInscriptions(filters) {}
+async function getDetailInscriptions(filters) {
+  const body = {
+    query: { bool: { must: { match_all: {} }, filter: [] } },
+    aggs: {
+      age: {
+        terms: {
+          field: "birthdateAt",
+          size: ES_NO_LIMIT,
+        },
+      },
+      gender: {
+        terms: {
+          field: "gender.keyword",
+          size: ES_NO_LIMIT,
+        },
+      },
+      grade: {
+        terms: {
+          field: "grade.keyword",
+          size: ES_NO_LIMIT,
+        },
+      },
+      situation: {
+        terms: {
+          field: "situation.keyword",
+          size: ES_NO_LIMIT,
+        },
+      },
+      handicap: {
+        terms: {
+          field: "handicap.keyword",
+          size: ES_NO_LIMIT,
+        },
+      },
+      ppsBeneficiary: {
+        terms: {
+          field: "ppsBeneficiary.keyword",
+          size: ES_NO_LIMIT,
+        },
+      },
+      qpv: {
+        terms: {
+          field: "qpv.keyword",
+          size: ES_NO_LIMIT,
+        },
+      },
+      isRegionRural: {
+        terms: {
+          field: "isRegionRural.keyword",
+          size: ES_NO_LIMIT,
+        },
+      },
+    },
+    size: 0,
+  };
+  if (filters?.cohort?.length) body.query.bool.filter.push({ terms: { "cohort.keyword": filters.cohort } });
+  if (filters?.academy?.length) body.query.bool.filter.push({ terms: { "academy.keyword": filters.academy } });
+  if (filters?.region?.length)
+    body.query.bool.filter.push({
+      bool: {
+        should: [
+          { bool: { must: [{ term: { "schooled.keyword": "true" } }, { terms: { "schoolRegion.keyword": filters.region } }] } },
+          { bool: { must: [{ term: { "schooled.keyword": "false" } }, { terms: { "region.keyword": filters.region } }] } },
+        ],
+      },
+    });
+  if (filters?.department?.length) body.query.bool.filter.push({ terms: { "department.keyword": filters.department } });
+  if (filters?.status?.length) body.query.bool.filter.push({ terms: { "status.keyword": filters.status } });
+  const { responses } = await api.esQuery("young", body);
+  if (!responses.length) return;
+  // get keys of aggregations
+  const keys = Object.keys(responses[0].aggregations);
+  const obj = {};
+  keys.forEach((key) => {
+    // for each key, get the buckets
+    obj[key] = {};
+    responses[0].aggregations[key].buckets.forEach((e) => {
+      // for each bucket, get the key and the doc_count
+      obj[key][e.key] = e.doc_count;
+    });
+  });
+
+  return obj;
+}
 async function getCurrentInscriptions(filters) {
   const body = {
     query: { bool: { must: { match_all: {} }, filter: [] } },
@@ -196,5 +339,6 @@ async function getCurrentInscriptions(filters) {
 
   const { responses } = await api.esQuery("young", body);
   if (!responses.length) return {};
+  console.log("responses", responses[0]);
   return api.getAggregations(responses[0]);
 }
