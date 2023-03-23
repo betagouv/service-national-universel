b70b649be55eaf6ac15b12aa2d5575e38c9baa47
Nicolas Bretécher <nicolas.bretecher@selego.co>
2023-03-23 16:47:15 +0100

    chore(app): Changement de wording dans le composant de vérification d'adresse (#2413)

--- app/src/scenes/inscription2023/components/Button.js
+++ app/src/scenes/inscription2023/components/Button.js
@@ -4,9 +4,12 @@ const hoveredStyle = "hover:bg-white hover:shadow-sm hover:!text-[#000091] hover
 const disabledStyle = "bg-[#E5E5E5] text-[#929292] cursor-not-allowed !border-[#E5E5E5]";
 const style = "bg-[#000091] border-[#000091] text-white";
 
-const Button = ({ onClick, children = "Continuer", disabled = false }) => {
+const Button = ({ onClick, children = "Continuer", disabled = false, className = "" }) => {
   return (
-    <button disabled={disabled} className={`flex items-center justify-center px-3 py-2 border  ${disabled ? disabledStyle : style} ${!disabled && hoveredStyle}`} onClick={onClick}>
+    <button
+      disabled={disabled}
+      className={`flex items-center justify-center px-3 py-2 border ${disabled ? disabledStyle : style} ${!disabled && hoveredStyle} ${className}`}
+      onClick={onClick}>
       {children}
     </button>
   );
