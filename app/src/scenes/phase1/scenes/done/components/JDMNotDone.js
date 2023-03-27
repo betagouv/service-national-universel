2152257d72f18c7b8a73810418a8c779d9556980
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2023-03-27 23:07:33 +0200

    up

--- app/src/scenes/phase1/scenes/done/components/JDMNotDone.js
+++ app/src/scenes/phase1/scenes/done/components/JDMNotDone.js
@@ -3,7 +3,7 @@ import Unlock from "../../../../../assets/icons/Unlock";
 
 export default function JDMNotDone() {
   return (
-    <div className="flex flex-col w-full lg:w-1/2 items-stretch mt-8 mb-16 md:mb-8 md:px-10 gap-3">
+    <div className="md:max-w-md mt-8 md:mt-0 mb-16 md:mb-8 px-6 md:px-10 space-y-3">
       <div className="flex justify-center">
         <Unlock className="saturate-0" />
       </div>
@@ -11,7 +11,7 @@ export default function JDMNotDone() {
         Réalisez votre JDC et <br />
         obtenez votre certificat !
       </p>
-      <p className="text-center text-gray-500">
+      <p className="text-center leading-relaxed text-xs text-gray-500">
         Vous devez réaliser votre JDC car <strong>vous n&apos;avez pas participé</strong> à la Journée défense et mémoire (JDM) lors de votre séjour de cohésion.
       </p>
     </div>
