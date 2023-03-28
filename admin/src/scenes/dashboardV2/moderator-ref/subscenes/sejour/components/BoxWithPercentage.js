b02169d696b9bf191ab2330db66a8ba2e721c23c
hlecourt <hlecourt99@gmail.com>
2023-03-28 12:28:50 +0200

    up

--- admin/src/scenes/dashboardV2/moderator-ref/subscenes/sejour/components/BoxWithPercentage.js
+++ admin/src/scenes/dashboardV2/moderator-ref/subscenes/sejour/components/BoxWithPercentage.js
@@ -2,7 +2,7 @@ import React from "react";
 import RoundRatio from "../../../../components/graphs/RoundRatio";
 
 export default function BoxWithPercentage({ total, title, number, subLabel }) {
-  const value = total ? number / total : 0;
+  const value = total ? (total - number) / total : 0;
   return (
     <div className="flex items-center justify-between gap-2 bg-white rounded-lg shadow-[0_8px_16px_rgba(0,0,0,0.05)] px-6 py-4 h-[102px]">
       <div className="flex flex-col gap-2">
