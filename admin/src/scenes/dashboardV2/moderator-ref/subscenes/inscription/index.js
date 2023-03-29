7e815407a67682ad96186a238369155e7aeabf41
Theret Guillaume <73123940+guiTheret@users.noreply.github.com>
2023-03-29 13:07:00 +0100

    Update index.js

--- admin/src/scenes/dashboardV2/moderator-ref/subscenes/inscription/index.js
+++ admin/src/scenes/dashboardV2/moderator-ref/subscenes/inscription/index.js
@@ -229,7 +229,6 @@ export default function Index() {
   useEffect(() => {
     fetchCurrentInscriptions();
     fetchDetailInscriptions();
-    updateLocalisationOptions();
   }, [selectedFilters]);
 
   useEffect(() => {
