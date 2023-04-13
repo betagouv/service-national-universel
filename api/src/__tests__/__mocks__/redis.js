8d78139f5f7fed1a2cf2a8e2fb154da299a24396
Pierre LE BIHAN <lebihanp@hotmail.fr>
2023-04-13 12:45:33 +0200

    mock redis

---
+++ api/src/__tests__/__mocks__/redis.js
@@ -0,0 +1,13 @@
+const redis = jest.createMockFromModule("redis");
+
+const data = {};
+
+const mockRedis = {
+  createClient: jest.fn(() => mockRedis),
+  connect: jest.fn(() => Promise.resolve()),
+  setEx: jest.fn((i, _, v) => (data[i] = v) && Promise.resolve()),
+  get: jest.fn((i) => Promise.resolve(data[i])),
+  disconnect: jest.fn(() => Promise.resolve()),
+};
+
+module.exports = mockRedis;
