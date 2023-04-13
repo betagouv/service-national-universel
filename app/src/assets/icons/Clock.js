13ebdd6695b36faed0856a59308a927c12753147
Bryan BERGER <contact@weberger.net>
2023-04-13 10:30:00 +0200

    fix(app): fix engagement recognition block responsiveness and fix some syntax errors

--- app/src/assets/icons/Clock.js
+++ app/src/assets/icons/Clock.js
@@ -7,9 +7,9 @@ const Clock = (props) => (
       <path
         d="M16 12.6667V16L18.5 18.5M23.5 16C23.5 20.1421 20.1421 23.5 16 23.5C11.8579 23.5 8.5 20.1421 8.5 16C8.5 11.8579 11.8579 8.5 16 8.5C20.1421 8.5 23.5 11.8579 23.5 16Z"
         stroke="#4B5563"
-        stroke-width="1.4"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </g>
   </svg>
