983cba67b2fd4d6fdfa96eddfd7431dcb93c0d66
hlecourt <hlecourt99@gmail.com>
2023-04-14 11:50:11 +0200

    chore(admin): disabled Juin 2023 on Schema de repartition

--- admin/src/scenes/plan-transport/util.js
+++ admin/src/scenes/plan-transport/util.js
@@ -26,7 +26,7 @@ export const cohortList = [
   { label: "Séjour du <b>19 Février au 3 Mars 2023</b>", value: "Février 2023 - C" },
   { label: "Séjour du <b>9 au 21 Avril 2023</b>", value: "Avril 2023 - A" },
   { label: "Séjour du <b>16 au 28 Avril 2023</b>", value: "Avril 2023 - B" },
-  { label: "Séjour du <b>11 au 23 Juin 2023</b>", value: "Juin 2023" },
+  // { label: "Séjour du <b>11 au 23 Juin 2023</b>", value: "Juin 2023" },
   { label: "Séjour du <b>4 au 16 Juillet 2023</b>", value: "Juillet 2023" },
 ];
 
