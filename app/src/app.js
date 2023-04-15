1e96ba9ef0454470a06fd31df995ecccd7ecaea3
hlecourt <50077368+hlecourt@users.noreply.github.com>
2023-04-15 11:14:04 +0200

    feat(app): modal warning bus prb (#2494)

--- app/src/app.js
+++ app/src/app.js
@@ -52,6 +52,9 @@ import { history, initSentry, SentryRoute } from "./sentry";
 import * as Sentry from "@sentry/react";
 import { cohortsInit } from "./utils/cohorts";
 import { getAvailableSessions } from "./services/cohort.service";
+import Modal from "./components/ui/modals/Modal";
+import ButtonLight from "./components/ui/buttons/ButtonLight";
+import Warning from "./assets/icons/Warning";
 
 initSentry();
 initApi();
@@ -133,8 +136,47 @@ export default function App() {
 const Espace = () => {
   const [isModalCGUOpen, setIsModalCGUOpen] = useState(false);
   const [isResumePhase1WithdrawnModalOpen, setIsResumePhase1WithdrawnModalOpen] = useState(false);
+  const [warningBusModalOpen, setWarningBusModalOpen] = useState(false);
 
   const young = useSelector((state) => state.Auth.young);
+
+  const busLignes = [
+    "6422a6ba2e94300602510423",
+    "6422a6bb2e94300602510507",
+    "6422a6be2e943006025107a2",
+    "6422a6bf2e94300602510827",
+    "6422a6bf2e94300602510845",
+    "6422a6bf2e94300602510876",
+    "6422a6c02e943006025109d5",
+    "6422a6c12e943006025109fe",
+    "6422a6c12e94300602510a71",
+    "6422a6c12e94300602510aab",
+    "6422a6c22e94300602510b3d",
+    "6422a6c22e94300602510b57",
+    "6422a6c32e94300602510beb",
+    "6422a6c32e94300602510c3d",
+    "6422a6c62e94300602510e72",
+    "6422a6c62e94300602510eb4",
+    "6422a6c62e94300602510ece",
+    "6422a6cb2e9430060251134c",
+    "6422a6cb2e943006025113ac",
+    "6422a6cb2e943006025113c6",
+    "6422a6cc2e94300602511416",
+    "6422a6ce2e94300602511644",
+    "6422a6ce2e9430060251169b",
+    "6422a6cf2e94300602511737",
+    "6422a6cf2e9430060251176b",
+    "6422a6d02e94300602511893",
+    "6422a6d12e943006025118bb",
+    "6422a6d32e94300602511b94",
+    "6422a6d42e94300602511bae",
+    "6422a6d72e94300602511ec6",
+    "6422a6d92e94300602511f93",
+    "6422a6dc2e94300602511fd0",
+    "6422a6dd2e94300602512108",
+    "6422a6de2e943006025121a3",
+    "6422a6df2e94300602512323",
+  ];
 
   const handleModalCGUConfirm = async () => {
     setIsModalCGUOpen(false);
@@ -150,6 +192,11 @@ const Espace = () => {
     if (young && young.acceptCGU !== "true") {
       setIsModalCGUOpen(true);
     }
+
+    if (young && busLignes.includes(young.ligneId)) {
+      setWarningBusModalOpen(true);
+    }
+
     if (location.pathname === "/" && young && young.acceptCGU === "true" && canYoungResumePhase1(young)) {
       getAvailableSessions(young).then((sessions) => {
         if (sessions.length) setIsResumePhase1WithdrawnModalOpen(true);
@@ -202,6 +249,7 @@ const Espace = () => {
       <Footer />
       <ModalCGU isOpen={isModalCGUOpen} onAccept={handleModalCGUConfirm} />
       <ModalResumePhase1ForWithdrawn isOpen={isResumePhase1WithdrawnModalOpen} onClose={() => setIsResumePhase1WithdrawnModalOpen(false)} />
+      <ModalBusWarning isOpen={warningBusModalOpen} onClose={() => setWarningBusModalOpen(false)} />
     </>
   );
 };
@@ -218,3 +266,28 @@ function ScrollToTop() {
 
   return null;
 }
+
+const ModalBusWarning = ({ isOpen, onClose }) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} className="w-[512px] bg-white rounded-xl p-6">
+      <Warning className="w-10 h-10 mx-auto text-gray-400" />
+      <p className="text-sm text-gray-500 leading-5 mt-4 mx-2">
+        Votre départ en séjour approche et les équipes encadrantes ont hâte de vous accueillir au sein de votre centre d&apos;affectation ! <br />
+        <br />
+        <strong>Cependant, en raison de difficultés de transport, il est possible que vos lieux et horaires de départ soient modifiés.</strong> <br />
+        <br />
+        Nous revenons vers vous dans la journée pour vous préciser votre nouvel horaire de départ. <br />
+        <br />
+        Nous mettons tout en oeuvre pour veiller à ce que le transport vers votre centre se déroule dans les meilleurs conditions.
+        <br />
+        <br />
+        Pour toute demande de renseignements, nous vous invitons à prendre contact avec le numéro de téléphone indiqué sur votre convocation ou via notre centre d&apos;aide.
+      </p>
+      <div className=" mt-12">
+        <ButtonLight className="w-full" onClick={onClose}>
+          Fermer
+        </ButtonLight>
+      </div>
+    </Modal>
+  );
+};
