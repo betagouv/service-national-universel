e75620b7ecdeee1222e8928f263504e047d1a9d9
hlecourt <hlecourt99@gmail.com>
2023-04-17 09:48:19 +0200

    chore(app): up

--- app/src/app.js
+++ app/src/app.js
@@ -138,62 +138,14 @@ const Espace = () => {
   const [isResumePhase1WithdrawnModalOpen, setIsResumePhase1WithdrawnModalOpen] = useState(false);
 
   // ! To clean after depart April B
-  const [warningBusModalOpen, setWarningBusModalOpen] = useState(false);
-  const [warningBusDepartLundiModalOpen, setWarningBusDepartLundiModalOpen] = useState(false);
-  const [warningBusNoChangeModalOpen, setWarningBusNoChangeModalOpen] = useState(false);
   const [warningBusDepartLundiGoodModalOpen, setWarningBusDepartLundiGoodModalOpen] = useState(false);
   // ! To clean after depart April B
 
   const young = useSelector((state) => state.Auth.young);
 
   // ! To clean after depart April B
-  const busLignes = [
-    "6422a6ba2e94300602510423",
-    "6422a6bb2e94300602510507",
-    "6422a6be2e943006025107a2",
-    "6422a6bf2e94300602510827",
-    "6422a6bf2e94300602510845",
-    "6422a6bf2e94300602510876",
-    "6422a6c02e943006025109d5",
-    "6422a6c12e943006025109fe",
-    "6422a6c12e94300602510a71",
-    "6422a6c12e94300602510aab",
-    "6422a6c22e94300602510b3d",
-    "6422a6c22e94300602510b57",
-    "6422a6c32e94300602510beb",
-    "6422a6c32e94300602510c3d",
-    "6422a6c62e94300602510eb4",
-    "6422a6c62e94300602510ece",
-    "6422a6cb2e9430060251134c",
-    "6422a6cb2e943006025113ac",
-    "6422a6cb2e943006025113c6",
-    "6422a6cc2e94300602511416",
-    "6422a6ce2e94300602511644",
-    "6422a6ce2e9430060251169b",
-    "6422a6cf2e94300602511737",
-    "6422a6cf2e9430060251176b",
-    "6422a6d02e94300602511893",
-    "6422a6d12e943006025118bb",
-    "6422a6d32e94300602511b94",
-    "6422a6d42e94300602511bae",
-    "6422a6d72e94300602511ec6",
-    "6422a6d92e94300602511f93",
-    "6422a6dc2e94300602511fd0",
-    "6422a6dd2e94300602512108",
-    "6422a6de2e943006025121a3",
-    "6422a6df2e94300602512323",
-  ];
-
-  const busLignesDepartLundi = [];
 
   const busDepartLundiGood = [
-    "6422a6d32e94300602511b7a",
-    "6422a6d82e94300602511f0a",
-    "6422a6c92e943006025111b2",
-    "6422a6c12e94300602510a27",
-    "6422a6c82e9430060251101b",
-    "6422a6d02e943006025117d6",
-    "6422a6df2e94300602512372",
     "6422a6bc2e94300602510605",
     "6422a6bd2e94300602510652",
     "6422a6bd2e943006025106a8",
@@ -204,15 +156,19 @@ const Espace = () => {
     "6422a6c42e94300602510ca5",
     "6422a6c42e94300602510cf9",
     "6422a6c62e94300602510e72",
+    "6422a6c82e9430060251101b",
     "6422a6c92e94300602511196",
+    "6422a6c92e943006025111b2",
     "6422a6c92e94300602511206",
     "6422a6ca2e94300602511298",
     "6422a6ca2e94300602511312",
+    "6422a6d02e943006025117d6",
+    "6422a6de2e94300602512250",
     "6422a6de2e9430060251228e",
     "6422a6df2e94300602512309",
+    "6422a6df2e94300602512372",
   ];
 
-  const busWithNoChangeAtTheEnd = ["6422a6bb2e943006025104df"];
   // ! To clean after depart April B
 
   const handleModalCGUConfirm = async () => {
@@ -231,16 +187,6 @@ const Espace = () => {
     }
 
     // ! To clean after depart April B
-    if (young && busLignes.includes(young.ligneId)) {
-      setWarningBusModalOpen(true);
-    }
-    if (young && busLignesDepartLundi.includes(young.ligneId)) {
-      setWarningBusDepartLundiModalOpen(true);
-    }
-    if (young && busWithNoChangeAtTheEnd.includes(young.ligneId)) {
-      setWarningBusNoChangeModalOpen(true);
-    }
-
     if (young && busDepartLundiGood.includes(young.ligneId)) {
       setWarningBusDepartLundiGoodModalOpen(true);
     }
@@ -298,9 +244,6 @@ const Espace = () => {
       <Footer />
       <ModalCGU isOpen={isModalCGUOpen} onAccept={handleModalCGUConfirm} />
       <ModalResumePhase1ForWithdrawn isOpen={isResumePhase1WithdrawnModalOpen} onClose={() => setIsResumePhase1WithdrawnModalOpen(false)} />
-      <ModalBusWarning isOpen={warningBusModalOpen} onClose={() => setWarningBusModalOpen(false)} />
-      <ModalBusWarningDepartLundi isOpen={warningBusDepartLundiModalOpen} onClose={() => setWarningBusDepartLundiModalOpen(false)} />
-      <ModalBusWarningNoChange isOpen={warningBusNoChangeModalOpen} onClose={() => setWarningBusNoChangeModalOpen(false)} />
       <ModalBusDepartLundiGood isOpen={warningBusDepartLundiGoodModalOpen} onClose={() => setWarningBusDepartLundiGoodModalOpen(false)} />
     </>
   );
@@ -320,51 +263,6 @@ function ScrollToTop() {
 }
 
 // ! To clean after depart April B
-const ModalBusWarning = ({ isOpen, onClose }) => {
-  return (
-    <Modal isOpen={isOpen} onClose={onClose} className="w-[512px] bg-white rounded-xl p-6">
-      <Warning className="w-10 h-10 mx-auto text-gray-400" />
-      <p className="text-sm text-gray-500 leading-5 mt-4 mx-2">
-        Vos informations de transport ont bien été mises à jour, vous pouvez dès à présent télécharger votre convocation. <br />
-        <br />
-        Veuillez nous excuser pour la gêne occasionnée. <br /> <br />
-        Les équipes du Service National Universel
-      </p>
-      <div className=" mt-12">
-        <ButtonLight className="w-full" onClick={onClose}>
-          Fermer
-        </ButtonLight>
-      </div>
-    </Modal>
-  );
-};
-
-const ModalBusWarningDepartLundi = ({ isOpen, onClose }) => {
-  return (
-    <Modal isOpen={isOpen} onClose={onClose} className="w-[512px] bg-white rounded-xl p-6">
-      <div className="flex flex-col gap-2">
-        <Warning className="w-10 h-10 mx-auto text-gray-400" />
-        <h4 className="flex text-center">Changement de date de votre départ en séjour</h4>
-      </div>
-      <p className="text-sm text-gray-500 leading-5 mt-4 mx-2">
-        Bonjour, <br />
-        <br />
-        Votre départ est confirmé pour lundi 17 avril 2023 : les éléments complémentaires (lieu de rassemblement et horaire) vous seront communiqués dans la journée. <br />
-        <br />
-        Nous vous prions de bien vouloir nous excuser pour la gêne occasionnée, nos équipes se mobilisent pour vous apporter une réponse rapide.
-        <br />
-        <br />
-        Cordialement, <br />
-        Les équipes du Service National Universel
-      </p>
-      <div className=" mt-12">
-        <ButtonLight className="w-full" onClick={onClose}>
-          Fermer
-        </ButtonLight>
-      </div>
-    </Modal>
-  );
-};
 
 const ModalBusDepartLundiGood = ({ isOpen, onClose }) => {
   return (
@@ -386,30 +284,6 @@ const ModalBusDepartLundiGood = ({ isOpen, onClose }) => {
         Les équipes du Service National Universel
       </p>
       <div className="mt-12">
-        <ButtonLight className="w-full" onClick={onClose}>
-          Fermer
-        </ButtonLight>
-      </div>
-    </Modal>
-  );
-};
-
-const ModalBusWarningNoChange = ({ isOpen, onClose }) => {
-  return (
-    <Modal isOpen={isOpen} onClose={onClose} className="w-[512px] bg-white rounded-xl p-6">
-      <div className="flex flex-col gap-2">
-        <Warning className="w-10 h-10 mx-auto text-gray-400" />
-        <h4 className="flex text-center">Information importante concernant votre transport</h4>
-      </div>
-      <p className="text-sm text-gray-500 leading-5 mt-4 mx-2">
-        Contrairement aux informations que vous avez reçues par mail et SMS aucun changement d&apos;horaire et de point de rassemblement n&apos;est prévu sur votre ligne de
-        transport. Les informations inscrites sur votre convocation sont à jour, vous pouvez dès à présent la télécharger de nouveau. <br />
-        <br />
-        En vous priant de bien vouloir nous excuser pour la gêne occasionnée. <br />
-        <br />
-        Votre départ en séjour approche et les équipes encadrantes ont hâte de vous accueillir au sein de votre centre d&apos;affectation !
-      </p>
-      <div className=" mt-12">
         <ButtonLight className="w-full" onClick={onClose}>
           Fermer
         </ButtonLight>
