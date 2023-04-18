e7283a378bde7641d717e7697f991748ae4ffa01
Nicolas Bretécher <nicolas.bretecher@selego.co>
2023-04-18 15:28:09 +0200

    feat(admin): Configurer le paramétrage dynamique de l'édition des PDR (#2490)

--- admin/src/scenes/pointDeRassemblement/components/ModalCreation.js
+++ admin/src/scenes/pointDeRassemblement/components/ModalCreation.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BsChevronDown, BsSearch } from "react-icons/bs";
 import { toastr } from "react-redux-toastr";
 import { useHistory } from "react-router-dom";
-import { ES_NO_LIMIT, ROLES, START_DATE_SESSION_PHASE1 } from "snu-lib";
+import { ES_NO_LIMIT, isPdrEditionOpen } from "snu-lib";
 import ModalTailwind from "../../../components/modals/ModalTailwind";
 import { capture } from "../../../sentry";
 import api from "../../../services/api";
@@ -12,11 +12,21 @@ import { useSelector } from "react-redux";
 export default function ModalCreation({ isOpen, onCancel, defaultPDR = null, editable = true }) {
   const user = useSelector((state) => state.Auth.user);
   const history = useHistory();
-  const availableCohorts = Object.keys(START_DATE_SESSION_PHASE1)
-    .reduce((acc, cohort) => {
-      return user.role === ROLES.ADMIN ? [...acc, cohort] : START_DATE_SESSION_PHASE1[cohort] > new Date() ? [...acc, cohort] : acc;
-    }, [])
-    .filter((cohort) => !defaultPDR || !defaultPDR.cohorts.includes(cohort));
+  const [availableCohorts, setAvailableCohorts] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await api.get("/cohort");
+        const filteredCohorts = data.filter((cohort) => isPdrEditionOpen(user, cohort) === true && !defaultPDR?.cohorts.includes(cohort.name));
+        const availableCohorts = filteredCohorts.map((cohort) => cohort.name);
+        setAvailableCohorts(availableCohorts);
+      } catch (err) {
+        capture(err);
+        toastr.error("Erreur", "Une erreur est survenue");
+      }
+    })();
+  }, []);
 
   const refSelect = React.useRef(null);
   const refInput = React.useRef(null);
