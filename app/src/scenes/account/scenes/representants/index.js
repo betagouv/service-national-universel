ab2fb1262d3f4472ffff9a6e5dbe8483b6f170be
Szilagyi Kinga <szilagyikinga42@gmail.com>
2023-04-27 14:27:05 +0200

    fix(app): display parent info on profil page

--- app/src/scenes/account/scenes/representants/index.js
+++ app/src/scenes/account/scenes/representants/index.js
@@ -37,7 +37,7 @@ const AccountRepresentantsPage = () => {
   const young = useSelector((state) => state.Auth.young);
   const dispatch = useDispatch();
 
-  const [formValues, setFormValues] = useState(getInitialFormValues());
+  const [formValues, setFormValues] = useState(getInitialFormValues(young));
 
   const [errors, setErrors] = useState(null);
   const [isSubmitting, setIsSubmitting] = useState(false);
