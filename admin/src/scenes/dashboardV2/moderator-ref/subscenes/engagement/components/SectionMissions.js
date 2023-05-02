0bed85eb961f96998439e2d0a5a88048da612163
seb <seb@snarkfacgtory.fr>
2023-05-02 08:51:37 +0200

    Changement des DatePicker

--- admin/src/scenes/dashboardV2/moderator-ref/subscenes/engagement/components/SectionMissions.js
+++ admin/src/scenes/dashboardV2/moderator-ref/subscenes/engagement/components/SectionMissions.js
@@ -1,11 +1,13 @@
 import Section from "../../../../components/ui/Section";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import { FilterComponent } from "../../../../components/FilterDashBoard";
 import MissionsProposedPlaces from "./MissionsProposedPlaces";
 import MissionsStatuts from "./MissionsStatuts";
 import MissionsDetail from "./MissionsDetail";
 import MissionsYoungPreferences from "./MissionsYoungPreferences";
-import DatePicker from "react-datepicker";
+import DatePicker from "../../../../../../components/ui/forms/DatePicker";
+import dayjs from "dayjs";
+import { Popover, Transition } from "@headlessui/react";
 
 export default function SectionMissions({ filters }) {
   const [missionFiltersArray, setMissionFiltersArray] = useState([]);
@@ -74,29 +76,66 @@ const DateFilterComponent = ({ filter, selectedFilters, setSelectedFilters }) =>
   const selectedFilterValue = selectedFilters[filter.id];
   const [visible, setVisible] = React.useState(false);
 
-  function onChange(value) {
-    setSelectedFilters({ ...selectedFilters, [filter.id]: value });
-  }
+  const formattedDate = selectedFilterValue ? dayjs(selectedFilterValue).format("DD/MM/YYYY") : "Choisir";
 
   return (
     <div className="relative w-fit">
       <div className={`border-[2px] p-0.5 ${visible ? "rounded-xl border-blue-600" : "border-transparent"}`}>
-        <div className="flex cursor-pointer flex-row items-center gap-1 rounded-md border-[1px] border-gray-200 bg-[#FFFFFF] p-2 hover:border-gray-300">
+        <div
+          className="flex cursor-pointer flex-row items-center gap-1 rounded-md border-[1px] border-gray-200 bg-[#FFFFFF] p-2 hover:border-gray-300"
+          onClick={() => setVisible(true)}>
           <div className="text-xs font-medium text-gray-700">{filter.name}</div>
-          <div className="w-[80px] rounded bg-gray-100 py-1 px-2 text-xs text-gray-500">
-            <DatePicker
-              locale="fr"
-              selected={selectedFilterValue}
-              onChange={onChange}
-              placeholderText={filter.noValue ? filter.noValue : "Choisir"}
-              className="w-full bg-[transparent]"
-              dateFormat="dd/MM/yyyy"
-              onCalendarOpen={() => setVisible(true)}
-              onCalendarClose={() => setVisible(false)}
-            />
-          </div>
+          <div className="w-[80px] rounded bg-gray-100 py-1 px-2 text-xs text-gray-500">{formattedDate}</div>
         </div>
       </div>
+      <Popover className="relative">
+        <DateDropdown filter={filter} visible={visible} setVisible={setVisible} selectedFilters={selectedFilters} setSelectedFilters={setSelectedFilters} />
+      </Popover>
     </div>
   );
 };
+
+function DateDropdown({ filter, visible, setVisible, selectedFilters, setSelectedFilters }) {
+  const ref = React.useRef(null);
+
+  React.useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setVisible(false);
+      }
+    };
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  }, []);
+
+  return (
+    <Transition
+      as={Fragment}
+      show={visible}
+      enter="transition ease-out duration-200"
+      enterFrom="opacity-0 translate-y-1"
+      enterTo="opacity-100 translate-y-0"
+      leave="transition ease-in duration-150"
+      leaveFrom="opacity-100 translate-y-0"
+      leaveTo="opacity-0 translate-y-1">
+      <Popover.Panel className={`absolute right-0 z-20 w-[305px] translate-y-[-9px]`}>
+        <div ref={ref} className="rounded-lg bg-white shadow-lg ">
+          <DatePicker
+            fromYear={2022}
+            toYear={2024}
+            value={selectedFilters[filter.id]}
+            onChange={(date) => {
+              setSelectedFilters({ ...selectedFilters, [filter.id]: date });
+            }}
+          />
+        </div>
+      </Popover.Panel>
+    </Transition>
+  );
+}
+
+/*
+
+ */
