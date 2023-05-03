f35ff96f3b97ea38776d983dceff28b16878f082
hlecourt <hlecourt99@gmail.com>
2023-05-03 15:48:59 +0200

    fix(admin): retour maquette dashboard general

--- admin/src/scenes/dashboardV2/components/ui/InfoMessage.js
+++ admin/src/scenes/dashboardV2/components/ui/InfoMessage.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function InfoMessage({ bg = "", Icon = null, message = "" }) {
   return (
     <div className={`flex items-center gap-4 rounded-lg ${bg} p-4 text-base leading-5 text-white`}>
-      <Icon className="h-7 w-7 text-white" />
+      <Icon className="h-10 w-10 text-white" />
       <span>{message}</span>
     </div>
   );
