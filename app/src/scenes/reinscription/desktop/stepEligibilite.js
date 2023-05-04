5ae24b7208dac6b56ac5a1930b8201c8b6b8f3f9
Szilagyi Kinga <szilagyikinga42@gmail.com>
2023-05-04 15:00:30 +0200

    fix(app): eligibility updates

--- app/src/scenes/reinscription/desktop/stepEligibilite.js
+++ app/src/scenes/reinscription/desktop/stepEligibilite.js
@@ -134,7 +134,7 @@ export default function StepEligibilite() {
     };
 
     try {
-      const updatedYoung = { ...young, updates };
+      const updatedYoung = { ...young, ...updates };
       const res = await api.post("/cohort-session/eligibility/2023", updatedYoung);
       if (!res.ok) {
         capture(res.code);
