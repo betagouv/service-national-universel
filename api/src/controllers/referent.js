844f1aff2630240b17734c1cff0e828c329aa6b1
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2023-05-05 16:12:33 +0200

    fix(api): get young if structure no longer exists

--- api/src/controllers/referent.js
+++ api/src/controllers/referent.js
@@ -910,7 +910,7 @@ router.get("/young/:id", passport.authenticate("referent", { session: false, fai
     let applications = [];
     for (let application of applicationsFromDb) {
       const structure = await StructureModel.findById(application.structureId);
-      applications.push({ ...application._doc, structure: serializeStructure(structure, req.user) });
+      applications.push({ ...application._doc, structure: structure ? serializeStructure(structure, req.user) : null });
     }
     return res.status(200).send({ ok: true, data: { ...data._doc, applications } });
   } catch (error) {
