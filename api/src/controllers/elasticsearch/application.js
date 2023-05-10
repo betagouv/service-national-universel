ad1000c074460c8beb6a2c1ccf39ccbaa98e4b58
hlecourt <hlecourt99@gmail.com>
2023-05-10 17:19:09 +0200

    up

--- api/src/controllers/elasticsearch/application.js
+++ api/src/controllers/elasticsearch/application.js
@@ -37,7 +37,7 @@ router.post("/by-mission/:id/:action(search|export)", passport.authenticate(["yo
     const { user, body } = req;
     // Configuration
     const searchFields = ["youngEmail", "youngFirstName", "youngLastName"];
-    const filterFields = ["status.keyword", "contractStatus.keyword", "filesType.keyword", "youngDepartment.keyword"];
+    const filterFields = ["status.keyword", "contractStatus.keyword", "filesType.keyword", "youngDepartment.keyword", "missionId.keyword"];
     const sortFields = [];
 
     // Body params validation
@@ -58,6 +58,67 @@ router.post("/by-mission/:id/:action(search|export)", passport.authenticate(["yo
     const contextFilters = [
       ...applicationContextFilters,
       { term: { "missionId.keyword": req.params.id } },
+      query.tab === "pending" ? { terms: { "status.keyword": ["WAITING_VALIDATION"] } } : null,
+      query.tab === "follow" ? { terms: { "status.keyword": ["IN_PROGRESS", "VALIDATED"] } } : null,
+    ].filter(Boolean);
+
+    // Build request body
+    const { hitsRequestBody, aggsRequestBody } = buildRequestBody({
+      searchFields,
+      filterFields,
+      queryFilters,
+      page,
+      sort,
+      contextFilters,
+    });
+
+    if (req.params.action === "export") {
+      const response = await allRecords("application", hitsRequestBody.query);
+      return res.status(200).send({ ok: true, data: response });
+    } else {
+      const response = await esClient.msearch({ index: "application", body: buildNdJson({ index: "application", type: "_doc" }, hitsRequestBody, aggsRequestBody) });
+      return res.status(200).send(response.body);
+    }
+  } catch (error) {
+    capture(error);
+    res.status(500).send({ ok: false, code: ERRORS.SERVER_ERROR });
+  }
+});
+
+router.post("/:action(search|export)", passport.authenticate(["young", "referent"], { session: false, failWithError: true }), async (req, res) => {
+  try {
+    const { user, body } = req;
+    // Configuration
+    const searchFields = ["youngEmail", "youngFirstName", "youngLastName"];
+    const filterFields = [
+      "status.keyword",
+      "contractStatus.keyword",
+      "filesType.keyword",
+      "youngDepartment.keyword",
+      "missionId.keyword",
+      "tutorName.keyword",
+      "missionName.keyword",
+    ];
+    const sortFields = [];
+
+    // Body params validation
+    const { queryFilters, page, sort, error } = joiElasticSearch({ filterFields, sortFields, body });
+    if (error) return res.status(400).send({ ok: false, code: ERRORS.INVALID_PARAMS });
+
+    const { error: errorQuery, value: query } = Joi.object({
+      tab: Joi.string().trim().valid("all", "follow", "pending"),
+    }).validate(req.query, { stripUnknown: true });
+    if (errorQuery) return res.status(400).send({ ok: false, code: ERRORS.INVALID_PARAMS });
+
+    const { applicationContextFilters, applicationContextError } = await buildApplicationContext(user);
+    if (applicationContextError) {
+      return res.status(applicationContextError.status).send(applicationContextError.body);
+    }
+
+    // Context filters
+    const contextFilters = [
+      ...applicationContextFilters,
+      // { term: { "missionId.keyword": req.params.id } },
       query.tab === "pending" ? { terms: { "status.keyword": ["WAITING_VALIDATION"] } } : null,
       query.tab === "follow" ? { terms: { "status.keyword": ["IN_PROGRESS", "VALIDATED"] } } : null,
     ].filter(Boolean);
