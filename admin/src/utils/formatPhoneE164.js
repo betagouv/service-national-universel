f39a3e8c0c98169f5e47f856cefbc9c087b2198a
Michael Garcia <mika_garcia21@hotmail.com>
2023-05-11 11:06:04 +0200

    fix bug admin export formatPhoneE164

--- admin/src/utils/formatPhoneE164.js
+++ admin/src/utils/formatPhoneE164.js
@@ -5,6 +5,9 @@ import { capture } from "../sentry";
 // ! Delete capture if no errors in months
 
 export const formatPhoneE164 = (phone, phoneZone) => {
+  if (!phone) {
+    return;
+  }
   if (typeof phone !== "string") {
     capture("Pb with phone format" + JSON.stringify(phone));
     return;
