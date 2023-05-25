107b13ceec7c25d378749de69f7252793c59e2ad
C2Chandelier <102954303+C2Chandelier@users.noreply.github.com>
2023-05-25 14:50:31 +0200

    design mission

---
+++ admin/src/assets/icons/Clock.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Clock = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+      <path
+        d="M8.00001 5V8L10.25 10.25M14.75 8C14.75 11.7279 11.7279 14.75 8.00001 14.75C4.27208 14.75 1.25 11.7279 1.25 8C1.25 4.27208 4.27208 1.25 8.00001 1.25C11.7279 1.25 14.75 4.27208 14.75 8Z"
+        stroke="white"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default Clock;
