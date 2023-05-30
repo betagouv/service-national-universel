0377e8e2a92fb652cbb0cda6fcdf40e5a6a1936c
C2Chandelier <102954303+C2Chandelier@users.noreply.github.com>
2023-05-30 12:28:29 +0200

    feat(admin): design page mission (couleur du statut et bandeau) (#2647)

---
+++ admin/src/assets/icons/InfoCircleMission.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const InfoCircle = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="none">
+      <path
+        d="M16.5 21H15V15H13.5M15 9H15.015M28.5 15C28.5 22.4558 22.4558 28.5 15 28.5C7.54416 28.5 1.5 22.4558 1.5 15C1.5 7.54416 7.54416 1.5 15 1.5C22.4558 1.5 28.5 7.54416 28.5 15Z"
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default InfoCircle;
