595a4721ebe0f56472adb161343e68e44c792dbc
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2023-05-31 18:13:47 +0200

    increase allocated memory for build

--- admin/src/build-only-on-clevercloud.js
+++ admin/src/build-only-on-clevercloud.js
@@ -3,12 +3,12 @@ const { execSync } = require("child_process");
 // Only on clever cloud.
 if (process.env.CC_DEPLOYMENT_ID) {
   if (process.env.STAGING) {
-    execSync("npm run build:staging", {
+    execSync("npm run build:staging --max_old_space_size=8192", {
       stdio: "inherit",
     });
   }
   if (process.env.PROD) {
-    execSync("npm run build", {
+    execSync("npm run build --max_old_space_size=8192", {
       stdio: "inherit",
     });
   }
