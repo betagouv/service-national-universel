df90ce90085eb7b578b5124a7086a013854bd2cc
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2023-06-01 16:54:42 +0200

    up

--- app/src/scenes/home/waitingList.jsx
+++ app/src/scenes/home/waitingList.jsx
@@ -2,22 +2,23 @@ import Img3 from "../../assets/homePhase2Desktop.png";
 import Img2 from "../../assets/homePhase2Mobile.png";
 import React from "react";
 import { useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
 import { translate } from "snu-lib";
-import Check from "../../assets/icons/Check";
 import plausibleEvent from "../../services/plausible";
 import { COHESION_STAY_LIMIT_DATE } from "../../utils";
+import { getCohort } from "../../utils/cohorts";
+import Clock from "../../assets/icons/Clock";
+import LinkInline from "../../components/ui/links/LinkInline";
 
 export default function WaitingList() {
   const young = useSelector((state) => state.Auth.young);
-  const history = useHistory();
+  const cohort = getCohort(young.cohort);
 
   return (
     <>
       {/* DESKTOP */}
       <div className="hidden lg:flex">
-        <div className="m-10 w-full">
-          <div className="flex items-center justify-between overflow-hidden rounded-xl bg-white shadow-sm">
+        <div className="m-8 w-full">
+          <div className="flex items-center justify-between overflow-hidden rounded-xl bg-white shadow-sm max-w-7xl mx-auto">
             <div className="flex w-1/2 flex-col gap-8 py-6 pl-10 pr-3">
               <div className="text-[44px] font-medium leading-tight tracking-tight text-gray-800">
                 <strong>{young.firstName},</strong> bienvenue sur votre compte volontaire.
@@ -27,12 +28,10 @@ export default function WaitingList() {
               </div>
 
               <hr className="text-gray-200" />
-              <div className="flex items-center gap-5">
-                <div className="flex h-8 w-8 items-center justify-center rounded-full bg-gray-100 p-2">
-                  <Check className="text-gray-600" />
-                </div>
-                <div className="flex-1 text-sm leading-5 text-[#6B7280]">
-                  Votre inscription au SNU est bien validée. Nous vous recontacterons dès qu’une place se libère dans les prochains jours.
+              <div className="flex gap-5">
+                <Clock className="text-gray-600 flex-1 rounded-full bg-gray-100 p-2" />
+                <div className="flex-1 text-sm leading-5 text-gray-500 space-y-6">
+                  <Content showLinks={cohort?.uselessInformation?.showChangeCohortButtonOnHomeWaitingList} />
                 </div>
               </div>
               <hr className="text-gray-200" />
@@ -61,12 +60,10 @@ export default function WaitingList() {
             </div>
 
             <hr className="mt-3 text-gray-200" />
-            <div className="flex items-center gap-2">
-              <div className="flex h-8 w-8 items-center justify-center rounded-full bg-gray-100 p-2">
-                <Check className="text-gray-600" />
-              </div>
-              <div className="flex-1 text-sm text-[#738297]">
-                Votre inscription au SNU est bien validée. Nous vous recontacterons dès qu’une place se libère dans les prochains jours.
+            <div className="flex gap-2 my-2">
+              <Clock className="text-gray-600 rounded-full bg-gray-100 p-2" />
+              <div className="flex-1 text-sm leading-5 text-gray-500 space-y-4">
+                <Content showLinks={cohort?.uselessInformation?.showChangeCohortButtonOnHomeWaitingList} />
               </div>
             </div>
             <hr className="text-gray-200" />
@@ -85,4 +82,23 @@ export default function WaitingList() {
       </div>
     </>
   );
+}
+
+function Content(showLinks) {
+  if (showLinks) {
+    return (
+      <>
+        <p>
+          Des places peuvent se libérer à tout moment. Si vous le souhaitez, vous pouvez donc être convoqué(e) dans les prochains jours et jusqu’à l’avant-veille du départ en
+          séjour. Pour cela, vous n’avez rien à faire : restez inscrit à ce séjour.
+        </p>
+        <p>
+          Par contre, si vous ne souhaitez pas recevoir une convocation tardive, vous pouvez choisir de vous{" "}
+          <LinkInline to="/changer-de-sejour">positionner sur un séjour à venir</LinkInline> ou bien{" "}
+          <LinkInline to="account/general?desistement=1">retirer votre candidature</LinkInline>.
+        </p>
+      </>
+    );
+  }
+  return <p>Votre inscription au SNU est bien validée. Nous vous recontacterons dès qu’une place se libère dans les prochains jours.</p>;
 }
