1bbe7911b099aca6a1573c635ae41369fc5a22b9
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2023-06-13 08:49:20 +0200

    fix(app): JDMA

--- app/src/components/JDMA.jsx
+++ app/src/components/JDMA.jsx
@@ -4,8 +4,7 @@ export default function JDMA({ id }) {
   return (
     <a
       href={`https://jedonnemonavis.numerique.gouv.fr/Demarches/${id}?&view-mode=formulaire-avis&nd_source=button&key=060c41afff346d1b228c2c02d891931f`}
-      className="relative w-36 h-16"
-      onClick={handleClick}>
+      className="relative w-36 h-16">
       <img src="https://jedonnemonavis.numerique.gouv.fr/static/bouton-blanc.svg" alt="Je donne mon avis" className="absolute h-16" />
       <img src="https://jedonnemonavis.numerique.gouv.fr/static/bouton-bleu.svg" alt="Je donne mon avis" className="absolute h-16 hover:hidden" />
     </a>
