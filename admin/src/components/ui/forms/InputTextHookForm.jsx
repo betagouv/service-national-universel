66fba12726a839e3a4d6e63bfa916c0e53781f55
Szilagyi Kinga <szilagyikinga42@gmail.com>
2023-06-15 16:18:12 +0200

    fix(admin): more accessible input forms

--- admin/src/components/ui/forms/InputTextHookForm.jsx
+++ admin/src/components/ui/forms/InputTextHookForm.jsx
@@ -6,9 +6,14 @@ export default function InputText({ register, name, label, disabled = false, err
       className={`flex min-h-[54px] w-full flex-col justify-center rounded-lg border-[1px] bg-white py-2 px-2.5 ${disabled ? "border-gray-200" : "border-gray-300"} ${
         error ? "border-red-500" : ""
       }`}>
-      {label && <p className={`text-xs leading-4 ${disabled ? "text-gray-400" : "text-gray-500"} `}>{label}</p>}
+      {label && (
+        <label htmlFor={name} className={`text-xs leading-4 ${disabled ? "text-gray-400" : "text-gray-500"} `}>
+          {label}
+        </label>
+      )}
       <div className="flex items-center gap-2">
         <input
+          id={name}
           className={`w-full bg-white text-sm ${disabled ? "text-gray-400" : "text-gray-900"} placeholder:text-gray-500`}
           {...register(name, validation)}
           disabled={disabled}
