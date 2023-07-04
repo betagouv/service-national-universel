bed07a6e0e2a08f7453dc15ac7c32fbc64395da4
Guillaume <guillaume@macmax.home>
2023-07-04 17:48:55 +0200

    fix mark

--- admin/src/components/ui/forms/NumberInput.jsx
+++ admin/src/components/ui/forms/NumberInput.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 export default function NumberInput({ days, label, onChange }) {
   const handleInputChange = (event) => {
-    event.persist();
     onChange(event.target.value);
   };
 
@@ -10,7 +9,7 @@ export default function NumberInput({ days, label, onChange }) {
     <div className={`flex min-h-[54px] w-full flex-col justify-center rounded-lg border-[1px] bg-white py-2 px-2.5 border-gray-300`}>
       {label && <p className={`text-xs leading-4 text-gray-500"`}>{label}</p>}
       <div className="flex items-center gap-2">
-        <input type="number" defaultValue={days || "préciser un nombre de jours"} onChange={handleInputChange} placeholder="Enter duration in days" />
+        <input type="number" defaultValue={days} onChange={handleInputChange} placeholder="Préciser un nombre de jours" />
       </div>
     </div>
   );
