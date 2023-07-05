c521171eb5f818b6cf8b6e38560d733a8459b6b1
Guillaume Pichon <73333478+GuiPich91@users.noreply.github.com>
2023-07-05 15:34:31 +0200

    feat(app): update young screen for JDM/JDC (#2780)

--- app/src/scenes/phase1/scenes/done/components/JDMDone.jsx
+++ app/src/scenes/phase1/scenes/done/components/JDMDone.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import Unlock from "../../../../../assets/icons/Unlock";
 import CensusDone from "./CensusDone";
 import CensusNotDone from "./CensusNotDone";
+import { isCohortNeedJdm } from "../../../../../utils/cohorts";
 
-export default function JDMDone() {
+export default function JDMDone({ cohort }) {
   const [isCensusDoneOpen, setIsCensusDoneOpen] = React.useState(false);
   const [isCensusNotDoneOpen, setIsCensusNotDoneOpen] = React.useState(false);
-
   return (
     <div className="mt-8 mb-16 space-y-3 md:mt-0 md:mb-8 md:px-10">
       <div className="flex justify-center">
         <Unlock />
       </div>
-      <p className="text-center text-lg font-bold">Vous avez participé à la JDM</p>
+      {isCohortNeedJdm(cohort) && <p className="text-center text-lg font-bold">Vous avez participé à la JDM</p>}
       <p className="text-center text-xl font-bold leading-7">
         Obtenez votre certificat <br /> de participation à la JDC !
       </p>
