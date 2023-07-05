87c7ea74e657ff957c723682e6b0b5d14b4130f8
Guillaume <guillaume@macmax.home>
2023-07-05 15:19:51 +0200

    update

--- app/src/scenes/phase1/scenes/done/components/JDMDone.jsx
+++ app/src/scenes/phase1/scenes/done/components/JDMDone.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import Unlock from "../../../../../assets/icons/Unlock";
 import CensusDone from "./CensusDone";
 import CensusNotDone from "./CensusNotDone";
+import {isCohortNeedJdm} from "../../../../../utils/cohorts";
 
 export default function JDMDone({ cohort }) {
   const [isCensusDoneOpen, setIsCensusDoneOpen] = React.useState(false);
   const [isCensusNotDoneOpen, setIsCensusNotDoneOpen] = React.useState(false);
-  const needTheJDMPresenceTrue = ["Février 2023 - C", "Avril 2023 - A", "Avril 2023 - B", "Février 2022", "2021", "2022", "2020"];
   return (
     <div className="mt-8 mb-16 space-y-3 md:mt-0 md:mb-8 md:px-10">
       <div className="flex justify-center">
         <Unlock />
       </div>
-      {needTheJDMPresenceTrue.includes(cohort) && <p className="text-center text-lg font-bold">Vous avez participé à la JDM</p>}
+      {isCohortNeedJdm(cohort) && <p className="text-center text-lg font-bold">Vous avez participé à la JDM</p>}
       <p className="text-center text-xl font-bold leading-7">
         Obtenez votre certificat <br /> de participation à la JDC !
       </p>
--- app/src/scenes/phase1/scenes/done/components/JDMNotDone.jsx
+++ app/src/scenes/phase1/scenes/done/components/JDMNotDone.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Unlock from "../../../../../assets/icons/Unlock";
+import {isCohortNeedJdm} from "../../../../../utils/cohorts"
 
-export default function JDMNotDone({ cohort }) {
-  const needTheJDMPresenceTrue = ["Février 2023 - C", "Avril 2023 - A", "Avril 2023 - B", "Février 2022", "2021", "2022", "2020"];
+export default function JDMNotDone({ cohort  }) {
   return (
     <div className="mt-8 mb-16 space-y-3 px-6 md:mt-0 md:mb-8 md:max-w-md md:px-10">
       <div className="flex justify-center">
@@ -12,7 +12,7 @@ export default function JDMNotDone({ cohort }) {
         Réalisez votre JDC et <br />
         obtenez votre certificat !
       </p>
-      {needTheJDMPresenceTrue.includes(cohort) && (
+      {isCohortNeedJdm(cohort) && (
         <p className="text-center text-xs leading-relaxed text-gray-500">
           Vous devez réaliser votre JDC car <strong>vous n&apos;avez pas participé</strong> à la Journée défense et mémoire (JDM) lors de votre séjour de cohésion.
         </p>
