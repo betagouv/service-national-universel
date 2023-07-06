216ecdd3344417add14d9034e4f8eb1b39c5d9c0
Szilagyi Kinga <szilagyikinga42@gmail.com>
2023-07-06 06:04:32 +0200

    fix(api):dsnj export departure date

--- api/src/crons/index.js
+++ api/src/crons/index.js
@@ -152,7 +152,7 @@ if (ENVIRONMENT === "production" && process.env.INSTANCE_NUMBER === "0") {
     youngPatches.handler();
   });
 
-  cron.schedule("30 03 * * *", () => {
+  cron.schedule("15 04 * * *", () => {
     dsnjExport.handler();
   });
 
