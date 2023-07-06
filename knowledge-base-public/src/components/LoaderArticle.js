535fe000381568c5f4ac5c03fe6f00e17b319710
Szilagyi Kinga <szilagyikinga42@gmail.com>
2023-07-06 10:21:03 +0200

    wip sub theme

--- knowledge-base-public/src/components/LoaderArticle.js
+++ knowledge-base-public/src/components/LoaderArticle.js
@@ -1,18 +1,12 @@
-const LoaderArticle = () => (
-  <div className="my-1 w-full shrink-0 grow-0 lg:my-4">
-    <article className="flex items-center overflow-hidden rounded-lg bg-white py-6 shadow-lg">
-      <div className="flex flex-grow flex-col">
-        <header className="relative flex items-center justify-between px-8 leading-tight">
-          <div className="h-2 w-full bg-gray-200">
-            <div className="animated-background" />
-          </div>
-        </header>
-      </div>
-      <svg xmlns="http://www.w3.org/2000/svg" className="mr-6 h-4 w-4 shrink-0 grow-0 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
-      </svg>
-    </article>
+import React from "react";
+
+const Article = ({ className = "" }) => (
+  <div className={`flex w-full max-w-[690px] flex-col overflow-hidden rounded-lg bg-white px-4 shadow-md ${className}`}>
+    <div className="flex h-[60px] w-full items-center justify-center pl-4 pr-9">
+      <div className="mr-4 h-8 w-8 rounded-md bg-gray-200" />
+      <div className="h-6 flex-1 rounded bg-gray-200" />
+    </div>
   </div>
 );
 
-export default LoaderArticle;
+export default Article;
