6efcbeb0bfd283c8dff1baf024ea2049527afd83
Guillaume <guillaume@macmax.home>
2023-07-07 16:14:00 +0200

    update

--- api/src/crons/deleteCNIAndSpecificAmenagementType.js
+++ api/src/crons/deleteCNIAndSpecificAmenagementType.js
@@ -3,10 +3,11 @@ require("../mongo");
 const { capture } = require("../sentry");
 const slack = require("../slack");
 const YoungModel = require("../models/young");
+const { deleteFile } = require("../utils/index");
 
 exports.handler = async () => {
-    let countTotal = 0; 
-    let countModified = 0; 
+  let countTotal = 0;
+  let countModified = 0;
 
   const where = {
     statusPhase1: { $in: ["DONE", "NOT_DONE", "WITHDRAWN"] },
@@ -21,16 +22,22 @@ exports.handler = async () => {
         return;
       } else {
         countTotal++;
-        young.set({ latestCNIFileExpirationDate: null, latestCNIFileCategory: "deleted", files: { cniFiles: [] } });
+        for (const file of young.files.cniFiles) {
+          await deleteFile(`/young/${young._id}/cniFiles/${file._id}`);
+        }
+        young.set({ latestCNIFileExpirationDate: null, latestCNIFileCategory: "deleted" });
         if (young.specificAmenagment === "true") {
           young.set({ specificAmenagmentType: "Contenu supprimé" });
         }
-        if(young.specificAmenagmentType ==="Contenu supprimé" || young.latestCNIFileCategory === "deleted"){
-            countModified++ 
+        if (young.specificAmenagmentType === "Contenu supprimé" || young.latestCNIFileCategory === "deleted") {
+          countModified++;
         }
         await young.save({ fromUser: { firstName: "Script de suppression du champ Nature de l'aménagement spécifique" } });
       }
-      await slack.success({ title: "Suppression des CNI et des aménagements spécifiques pour les jeunes des cohortes passées", text: `${countModified}/${countTotal} Jeunes modifiés` });
+      await slack.success({
+        title: "Suppression des CNI et des aménagements spécifiques pour les jeunes des cohortes passées",
+        text: `${countModified}/${countTotal} Jeunes modifiés`,
+      });
     } catch (e) {
       capture(e);
       await slack.error({ title: "WARNING: Problème suppression CNI && specificAmenagmentType", text: JSON.stringify(e) });
