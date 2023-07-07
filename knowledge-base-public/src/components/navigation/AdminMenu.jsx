801bba1a7f61ddd84344b7647e295d4ccd48f618
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2023-07-07 22:09:54 +0200

    add group behavior and transitions to flyout menus

--- knowledge-base-public/src/components/navigation/AdminMenu.jsx
+++ knowledge-base-public/src/components/navigation/AdminMenu.jsx
@@ -1,11 +1,11 @@
-import { useContext } from "react";
+import { Fragment, useContext } from "react";
 import useUser from "../../hooks/useUser";
 import SeeAsContext from "../../contexts/seeAs";
 import { useSWRConfig } from "swr";
 import { adminURL, snuApiUrl } from "../../config";
 import API from "../../services/api";
 import Link from "next/link";
-import { Popover } from "@headlessui/react";
+import { Popover, Transition } from "@headlessui/react";
 import { translateRoleBDC } from "../../utils/constants";
 import { HiOutlineLogout, HiOutlineUser } from "react-icons/hi";
 
@@ -49,37 +49,49 @@ export default function AdminMenu() {
         </Popover>
       )}
 
-      <Popover className="relative flex grow-0 justify-end md:flex-none">
-        <Popover.Button className="flex h-10 w-10 items-center justify-center gap-3 rounded-full border-none bg-blue-50 p-0 text-sm font-bold uppercase tracking-tight text-[#32257F]">
-          {user.firstName?.[0]}
-          {user.lastName?.[0]}
-        </Popover.Button>
+      <Popover.Group>
+        <Popover className="relative flex grow-0 justify-end md:flex-none">
+          <Popover.Button className="flex h-10 w-10 items-center justify-center gap-3 rounded-full border-none bg-blue-50 p-0 text-sm font-bold uppercase tracking-tight text-[#32257F]">
+            {user.firstName?.[0]}
+            {user.lastName?.[0]}
+          </Popover.Button>
 
-        <Popover.Panel className="absolute right-0 top-14 z-10 w-80 overflow-hidden rounded-md bg-white text-gray-800">
-          <nav>
-            <ul>
-              <li>
-                <Link href={adminURL} className="flex items-center justify-between p-4 text-sm font-medium transition-colors hover:bg-gray-100">
-                  Espace admin SNU
-                  <HiOutlineUser className="text-xl text-gray-400" />
-                </Link>
-              </li>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-200"
+            enterFrom="opacity-0 translate-y-1"
+            enterTo="opacity-100 translate-y-0"
+            leave="transition ease-in duration-150"
+            leaveFrom="opacity-100 translate-y-0"
+            leaveTo="opacity-0 translate-y-1"
+          >
+            <Popover.Panel className="absolute right-0 top-14 z-10 w-80 overflow-hidden rounded-md bg-white text-gray-800">
+              <nav>
+                <ul>
+                  <li>
+                    <Link href={adminURL} className="flex items-center justify-between p-4 text-sm font-medium transition-colors hover:bg-gray-100">
+                      Espace admin SNU
+                      <HiOutlineUser className="text-xl text-gray-400" />
+                    </Link>
+                  </li>
 
-              <hr />
+                  <hr />
 
-              <li>
-                <button
-                  onClick={onLogout}
-                  className="flex w-full items-center justify-between border border-none bg-transparent p-4 text-sm font-medium text-gray-800 shadow-none transition-colors hover:bg-gray-100"
-                >
-                  <span>Déconnexion</span>
-                  <HiOutlineLogout className="text-xl text-gray-400" />
-                </button>
-              </li>
-            </ul>
-          </nav>
-        </Popover.Panel>
-      </Popover>
+                  <li>
+                    <button
+                      onClick={onLogout}
+                      className="flex w-full items-center justify-between border border-none bg-transparent p-4 text-sm font-medium text-gray-800 shadow-none transition-colors hover:bg-gray-100"
+                    >
+                      <span>Déconnexion</span>
+                      <HiOutlineLogout className="text-xl text-gray-400" />
+                    </button>
+                  </li>
+                </ul>
+              </nav>
+            </Popover.Panel>
+          </Transition>
+        </Popover>
+      </Popover.Group>
     </>
   );
 }
--- knowledge-base-public/src/components/navigation/PublicMenu.jsx
+++ knowledge-base-public/src/components/navigation/PublicMenu.jsx
@@ -1,53 +1,66 @@
-import { Popover } from "@headlessui/react";
+import { Popover, Transition } from "@headlessui/react";
 import { useRouter } from "next/router";
 import { adminURL, appURL, baseDeConnaissanceURL } from "../../config";
 import Link from "next/link";
 import { HiOutlineExternalLink } from "react-icons/hi";
 import { FiMenu } from "react-icons/fi";
+import { Fragment } from "react";
 
 export default function PublicMenu() {
   const router = useRouter();
 
   return (
     <>
-      <Popover className="relative md:hidden">
-        <Popover.Button className="rounded-none border-none bg-transparent p-2 shadow-none">
-          <FiMenu className="text-2xl text-white" />
-        </Popover.Button>
+      <Popover.Group>
+        <Popover className="relative md:hidden">
+          <Popover.Button className="rounded-none border-none bg-transparent p-2 shadow-none">
+            <FiMenu className="text-2xl text-white" />
+          </Popover.Button>
 
-        <Popover.Panel className="absolute right-0 top-14 z-10 w-80 rounded-md bg-white text-gray-800">
-          <nav>
-            <ul>
-              <li>
-                <a href="https://www.snu.gouv.fr" className="flex items-center justify-between p-4 text-sm font-medium">
-                  Retour sur le site du SNU
-                  <HiOutlineExternalLink className="text-xl text-gray-400" />
-                </a>
-              </li>
-              <hr />
-              <li>
-                <Link
-                  href={`${adminURL}/auth?redirect=${baseDeConnaissanceURL}/base-de-connaissance/${router?.query?.slug || ""}`}
-                  className="flex items-center justify-between p-4 text-sm font-medium"
-                >
-                  Espace professionnel
-                  <HiOutlineExternalLink className="text-xl text-gray-400" />
-                </Link>
-              </li>
-              <hr />
-              <li>
-                <Link
-                  href={`${appURL}/auth?redirect=${baseDeConnaissanceURL}/base-de-connaissance/${router?.query?.slug || ""}`}
-                  className="flex items-center justify-between p-4 text-sm font-medium"
-                >
-                  Espace volontaire
-                  <HiOutlineExternalLink className="text-xl text-gray-400" />
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </Popover.Panel>
-      </Popover>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-200"
+            enterFrom="opacity-0 translate-y-1"
+            enterTo="opacity-100 translate-y-0"
+            leave="transition ease-in duration-150"
+            leaveFrom="opacity-100 translate-y-0"
+            leaveTo="opacity-0 translate-y-1"
+          >
+            <Popover.Panel className="absolute right-0 top-14 z-10 w-80 rounded-md bg-white text-gray-800">
+              <nav>
+                <ul>
+                  <li>
+                    <a href="https://www.snu.gouv.fr" className="flex items-center justify-between p-4 text-sm font-medium">
+                      Retour sur le site du SNU
+                      <HiOutlineExternalLink className="text-xl text-gray-400" />
+                    </a>
+                  </li>
+                  <hr />
+                  <li>
+                    <Link
+                      href={`${adminURL}/auth?redirect=${baseDeConnaissanceURL}/base-de-connaissance/${router?.query?.slug || ""}`}
+                      className="flex items-center justify-between p-4 text-sm font-medium"
+                    >
+                      Espace professionnel
+                      <HiOutlineExternalLink className="text-xl text-gray-400" />
+                    </Link>
+                  </li>
+                  <hr />
+                  <li>
+                    <Link
+                      href={`${appURL}/auth?redirect=${baseDeConnaissanceURL}/base-de-connaissance/${router?.query?.slug || ""}`}
+                      className="flex items-center justify-between p-4 text-sm font-medium"
+                    >
+                      Espace volontaire
+                      <HiOutlineExternalLink className="text-xl text-gray-400" />
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+            </Popover.Panel>
+          </Transition>
+        </Popover>
+      </Popover.Group>
 
       <nav className="hidden md:block">
         <ul className="flex items-center justify-end gap-6">
