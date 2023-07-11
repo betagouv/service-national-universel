af6cd8649ced71290bae37b3dc0cd1119bb6b33f
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2023-07-11 11:30:33 +0200

    almost done

--- knowledge-base-public/src/components/knowledge-base/KnowledgeBaseSearch.js
+++ knowledge-base-public/src/components/knowledge-base/KnowledgeBaseSearch.js
@@ -1,123 +1,154 @@
 import { Fragment, useRef } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
 import { useState } from 'react'
-import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
-import { Combobox } from '@headlessui/react'
 import useUser from '../../hooks/useUser'
 import API from '../../services/api'
 
+import { Dialog, Transition } from '@headlessui/react'
+import { XCircleIcon, MagnifyingGlassIcon, ArrowLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid'
+import { Combobox } from '@headlessui/react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
 export default function KnowledgeBaseSearch({ open, setOpen }) {
-    const { restriction } = useUser();
-    const [search, setSearch] = useState('')
-    const [isSearching, setIsSearching] = useState(false);
-    const [selectedPerson, setSelectedPerson] = useState(null)
-    const [items, setItems] = useState([]);
-    const searchTimeout = useRef(null);
+  const { restriction } = useUser();
+  const [query, setQuery] = useState("");
+  const [isSearching, setIsSearching] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null)
+  const [items, setItems] = useState([]);
+  const searchTimeout = useRef(null);
+  const router = useRouter();
 
-    function classNames(...classes) {
-        return classes.filter(Boolean).join(' ')
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+  }
+
+  const computeSearch = (e) => {
+    const search = e.target.value
+    setQuery(search);
+    if (search.length > 0 && !isSearching) setIsSearching(true);
+    if (!search.length) {
+      setIsSearching(false);
+      clearTimeout(searchTimeout.current);
+      setItems([]);
+      return;
     }
+    clearTimeout(searchTimeout.current);
+    searchTimeout.current = setTimeout(async () => {
+      setIsSearching(true);
+      const response = await API.get({ path: `/knowledge-base/${restriction}/search`, query: { search } });
+      setIsSearching(false);
+      if (response.ok) setItems(response.data);
+    }, 1000);
+  };
 
-    const computeSearch = (e) => {
-        const search = e.target.value
-        setSearch(search);
-        if (search.length > 0 && !isSearching) setIsSearching(true);
-        if (!search.length) {
-            setIsSearching(false);
-            setSearch("");
-            clearTimeout(searchTimeout.current);
-            setItems([]);
-            return;
-        }
-        clearTimeout(searchTimeout.current);
-        searchTimeout.current = setTimeout(async () => {
-            setIsSearching(true);
-            const response = await API.get({ path: `/knowledge-base/${restriction}/search`, query: { search } });
-            setIsSearching(false);
-            if (response.ok) {
-                setItems(response.data);
-            }
-        }, 1000);
-    };
+  const handleDelete = () => {
+    setSelectedItem(null);
+    setItems([]);
+  };
 
-    return (
-        <Transition.Root show={open} as={Fragment}>
-            <Dialog as="div" className="relative z-10" onClose={setOpen}>
-                <Transition.Child
-                    as={Fragment}
-                    enter="ease-out duration-300"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="ease-in duration-200"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                >
-                    <div className="fixed inset-0 bg-black bg-opacity-60 transition-opacity" />
-                </Transition.Child>
+  const handleSelect = (item) => {
+    setOpen(false);
+    setSelectedItem(item);
+    setItems([]);
+    router.push("/base-de-connaissance/" + item.slug);
+  };
 
-                <div className="fixed inset-0 z-10 overflow-y-auto">
-                    <div className="flex flex-col min-h-full items-end justify-start p-4 text-center sm:items-center sm:p-0 sm:pt-44">
-                        <Transition.Child
-                            as={Fragment}
-                            enter="ease-out duration-300"
-                            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                            enterTo="opacity-100 translate-y-0 sm:scale-100"
-                            leave="ease-in duration-200"
-                            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                        >
-                            <Dialog.Panel>
-                                <Combobox as="div" value={selectedPerson} onChange={setSelectedPerson}>
-                                    <div className="relative mt-2 w-full max-w-[40rem]">
-                                        <Combobox.Input
-                                            className="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-10 text-gray-800 shadow-sm sm:text-sm sm:leading-6"
-                                            onChange={computeSearch}
-                                            displayValue={(person) => person?.name}
-                                        />
-                                        <Combobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
-                                            <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                                        </Combobox.Button>
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={setOpen}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-60 transition-opacity" />
+        </Transition.Child>
 
-                                        {items.length > 0 && (
-                                            <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                                                {items.map((person) => (
-                                                    <Combobox.Option
-                                                        key={person.id}
-                                                        value={person}
-                                                        className={({ active }) =>
-                                                            classNames(
-                                                                'relative cursor-default select-none py-2 pl-3 pr-9',
-                                                                active ? 'bg-blue-600 text-white' : 'text-gray-900'
-                                                            )
-                                                        }
-                                                    >
-                                                        {({ active, selected }) => (
-                                                            <>
-                                                                <span className={classNames('block truncate', selected && 'font-semibold')}>{person.name}</span>
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex flex-col min-h-full items-end justify-start p-4 text-center sm:items-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="w-full md:w-[40rem] md:pt-44">
+                <Combobox as="div" value={selectedItem} onChange={handleSelect}>
+                  <div className="relative mt-2 w-full">
+                    <Combobox.Input
+                      className="w-full rounded-md border-0 bg-white p-3 px-10 text-gray-800 shadow-sm sm:text-sm sm:leading-6"
+                      onChange={computeSearch}
+                      displayValue={query}
+                      placeholder="Rechercher un article"
+                    />
 
-                                                                {selected && (
-                                                                    <span
-                                                                        className={classNames(
-                                                                            'absolute inset-y-0 right-0 flex items-center pr-4',
-                                                                            active ? 'text-white' : 'text-blue-600'
-                                                                        )}
-                                                                    >
-                                                                        <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                                                                    </span>
-                                                                )}
-                                                            </>
-                                                        )}
-                                                    </Combobox.Option>
-                                                ))}
-                                            </Combobox.Options>
-                                        )}
-                                    </div>
-                                </Combobox>
-                            </Dialog.Panel>
-                        </Transition.Child>
+                    <div className="absolute inset-y-0 left-0 flex items-center">
+                      {query ? (
+                        <button onClick={() => setOpen(false)} className="ml-3 reset">
+                          <ArrowLeftIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                        </button>
+                      ) : (
+                        <MagnifyingGlassIcon className="h-5 w5 px-3 text-gray-400" aria-hidden="true" />
+                      )}
                     </div>
-                </div>
-            </Dialog>
-        </Transition.Root>
-    )
+
+                    {query ? (<Combobox.Button onClick={handleDelete} className="absolute inset-y-0 right-0 items-center px-3 focus:outline-none bg-transparent border-none shadow-none">
+                      <XCircleIcon className="text-gray-400 h-5 w-5" aria-hidden="true" />
+                    </Combobox.Button>) : null}
+
+                    <div className="absolute z-10 mt-1 max-h-[800px] w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg sm:text-sm">
+                      {isSearching ? (
+                        <p className="animate-pulse p-3 text-left font-medium text-gray-500">Recherche en cours...</p>
+                      ) : (
+                        items.length > 0 ? (
+                          <Combobox.Options>
+                            {items.map((item) => (
+                              <Combobox.Option
+                                key={item._id}
+                                value={item}
+                                className={({ active }) =>
+                                  classNames(
+                                    'relative cursor-pointer select-none p-4 border-b-2 border-gray-100',
+                                    active ? 'bg-blue-600 text-white' : 'text-gray-900'
+                                  )
+                                }
+                              >
+                                {({ selected }) => (
+                                  <div className="flex justify-between">
+                                    <p className={classNames('block truncate', selected && 'font-semibold')}>{item.title}</p>
+                                    <ChevronRightIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                                  </div>
+                                )}
+                              </Combobox.Option>
+                            ))}
+                          </Combobox.Options>
+                        ) : (
+                          <>
+                            <p className="p-3 text-left text-gray-500 font-medium">Aucun résultat ne correspond à votre recherche.</p>
+                            <hr />
+                            <div className="p-3">
+                              <Link href="" className="text-blue-600 font-medium">
+                                Vous avez une question ? Contactez-nous
+                              </Link>
+                            </div>
+                          </>
+                        ))}
+                    </div>
+
+                  </div>
+                </Combobox>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  )
 }
