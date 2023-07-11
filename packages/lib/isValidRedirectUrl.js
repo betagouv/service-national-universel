ca6fb8e7249a073e31b165bb3d93f575d41dc6f0
Michael Garcia <mika_garcia21@hotmail.com>
2023-07-11 16:11:29 +0200

    fix-bug-redirect-url

--- packages/lib/isValidRedirectUrl.js
+++ packages/lib/isValidRedirectUrl.js
@@ -1,5 +1,5 @@
 function isValidRedirectUrl(url) {
-  const regex = /^((https:\/\/)?([\w-]+\.)*(snu\.gouv\.fr|beta-snu\.dev)\/?.*|^[^\/]+$|^\/.*)$/i;
+  const regex = /^(https:\/\/)([\w-]+\.)?(snu\.gouv\.fr|beta-snu\.dev)(\/.*)?(\?[\w-+%=&]*)?$|^\/?[\w-]+(\/[\w-]+)*(\?[\w-+%=&]*)?$/i;
   if (!url) return false;
   return regex.test(url);
 }
