b9b9ac2e687a286fe7864585038c3ceac1695077
Nicolas Bretécher <nicolas.bretecher@selego.co>
2023-07-12 16:10:00 +0200

    feat(kb): New search bar (staging only) (#2793)

--- knowledge-base-public/src/components/Header.jsx
+++ knowledge-base-public/src/components/Header.jsx
@@ -1,13 +1,26 @@
 import Link from "next/link";
 import Navigation from "./navigation/Navigation";
+import KnowledgeBaseSearch from "./knowledge-base/KnowledgeBaseSearch.js";
+import { useEffect, useState } from "react";
+import SearchShortcut from "./SearchShortcut";
 
 export default function Header() {
+  const [searchOpen, setSearchOpen] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener("keydown", function (event) {
+      if (event.key === "k" && (event.ctrlKey || event.metaKey)) {
+        setSearchOpen(!searchOpen);
+      }
+    });
+  }, []);
+
   return (
     <header className="flex items-center justify-between gap-4 border-b-2 border-white border-opacity-20 bg-[#32257F] p-4 print:hidden">
       <div className="flex items-center gap-4">
         <div className="flex-none">
           <Link href="/">
-            <img className="h-14 w-14 cursor-pointer" src="/assets/logo-snu.png" alt="Logo du SNU" />
+            <img className="h-14 w-14" src="/assets/logo-snu.png" alt="Logo du SNU" />
           </Link>
         </div>
 
@@ -23,12 +36,18 @@ export default function Header() {
       </div>
 
       <div className="flex items-center gap-4">
-        <button className="border-none bg-transparent p-2 shadow-none">
+        <button
+          onClick={() => setSearchOpen(true)}
+          className="flex items-center gap-3 rounded-md border-none bg-transparent px-2.5 py-2 shadow-none transition-colors hover:bg-black hover:bg-opacity-20"
+        >
+          <SearchShortcut />
           <img src="/assets/search.svg" />
         </button>
 
         <Navigation />
       </div>
+
+      <KnowledgeBaseSearch open={searchOpen} setOpen={setSearchOpen} />
     </header>
   );
 }
