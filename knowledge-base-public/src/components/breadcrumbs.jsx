d457e253a85e5bdaf292140fd2aabb2778c75872
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2023-07-15 17:51:44 +0200

    nowrap breadcrumb

--- knowledge-base-public/src/components/breadcrumbs.jsx
+++ knowledge-base-public/src/components/breadcrumbs.jsx
@@ -4,14 +4,14 @@ import Link from "next/link";
 const Breadcrumbs = ({ parents, path }) => {
   return (
     <nav aria-label="Breadcrumb" className="mt-4 text-sm leading-4 print:hidden">
-      <ol className="flex items-center">
+      <ol className="flex flex-wrap items-center">
         <li>
           <Link href={path} className="rounded py-1.5 pr-2">
             Accueil
           </Link>
         </li>
         {parents.map(({ _id, slug, title }) => (
-          <li key={_id} className="flex items-center gap-1">
+          <li key={_id} className="flex flex-nowrap items-center gap-1">
             <ChevronRightIcon className="h-4" />
             <Link href={`${path}/${slug}`} className="rounded px-2 py-1.5">
               {title}
