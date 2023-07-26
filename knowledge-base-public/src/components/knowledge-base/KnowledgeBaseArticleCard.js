14f9884103bee652d204dea46615780ec20df085
szilagyikinga <szilagyikinga42@gmail.com>
2023-07-26 10:00:14 +0200

    fix(dbc): bdc theme page and other fixes (#2834)

--- knowledge-base-public/src/components/knowledge-base/KnowledgeBaseArticleCard.js
+++ knowledge-base-public/src/components/knowledge-base/KnowledgeBaseArticleCard.js
@@ -7,7 +7,7 @@ const KnowledgeBaseArticleCard = ({ title, slug, path, className = "" }) => {
       className={`flex h-[60px] w-full max-w-[690px] flex-col justify-center overflow-hidden rounded-lg bg-white px-4 shadow-md ${className}`}
       href={`${path}/${slug}${path === "/base-de-connaissance" ? "?loadingType=article" : ""}`}
     >
-      <h3 className="text-sm font-medium text-gray-900">{title}</h3>
+      <h3 className="line-clamp-2 text-sm font-medium text-gray-900">{title}</h3>
     </Link>
   );
 };
