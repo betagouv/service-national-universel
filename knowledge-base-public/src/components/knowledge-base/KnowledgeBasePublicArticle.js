1e4f0fa3bee45bb900bb575827f4353cc8e1cc06
Guillaume <guillaume@macmax.home>
2023-07-27 09:07:33 +0200

    update

--- knowledge-base-public/src/components/knowledge-base/KnowledgeBasePublicArticle.js
+++ knowledge-base-public/src/components/knowledge-base/KnowledgeBasePublicArticle.js
@@ -1,5 +1,9 @@
+
 import TextEditor from "../TextEditor";
-import { useMemo } from "react";
+import Link from "next/link";
+import useSWR, { useSWRConfig } from "swr";
+import API from "../../services/api";
+import { useMemo, useEffect, useState } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import KnowledgeBasePublicNoAnswer from "./KnowledgeBasePublicNoAnswer";
@@ -8,43 +12,108 @@ import FeedbackComponent from "../FeedBack";
 import { HiPrinter } from "react-icons/hi";
 
 const KnowledgeBasePublicArticle = ({ item, isLoading }) => {
+  if (!item || isLoading) return <ArticleLoader />;
+
+  const { cache } = useSWRConfig();
+  const parentId = item.parentId;
+  const [siblingsData, setSiblingsData] = useState([]); // État pour stocker les données des frères et sœurs
   const group = useMemo(() => {
     return item?.group || item?.parents?.[0]?.group;
   }, [item]);
+
+  useEffect(() => {
+    const fetchSiblings = async () => {
+      try {
+        const response = await API.post({ path: `/knowledge-base/getSiblings`, body: { parentId } });
+        setSiblingsData(response.siblings);
+      } catch (error) {
+        console.error(error);
+        // Gérer les erreurs ici
+      }
+    };
+
+    fetchSiblings();
+  }, []);
 
   if (isLoading) return <ArticleLoader />;
   return (
     <div className="w-full bg-white">
       <section className="mx-auto flex max-w-[792px] flex-shrink flex-grow flex-col overflow-hidden px-4 text-gray-800 print:bg-transparent print:pb-12">
         <Breadcrumbs parents={item?.parents || []} path="/base-de-connaissance" />
-        <div className="flex flew-row justify-between pt-4 pb-2">
-          <div className="mr-4">
-            <h2 className="mb-2 text-[20px] font-bold print:mb-0 print:text-black">{group?.title}</h2>
-            <h1 className="mb-2 text-[20px] font-bold md:text-[24px] print:mb-0 print:text-black">{item?.title}</h1>
-            <h6 className="text-[18px] text-snu-purple-100 md:text-[18px] lg:text-xl print:text-black">{item?.description}</h6>
+        <div className="flex flex-col md:flex-row">
+          <div className="mr-24 mt-4 max-w-[200px]">
+            <ol className="">
+              {/* <li>
+                <Link href={`/base-de-connaissance/${item.parents[0].slug}`} className="rounded py-1.5 pr-2">
+                  {item.parents[0].title}
+                </Link>
+              </li>
+              {item.siblings[0].map(
+                ({ _id, slug, title, type, status }) =>
+                  type === "article" &&
+                  status === "PUBLISHED" && (
+                    <li key={_id} className={`flex flex-nowrap items-center gap-1 ${item._id === _id ? 'bg-gray-200' : ''}`}>
+                      <Link href={`/base-de-connaissance/${slug}`} className="rounded px-2 py-1.5 text-[12px]" onClick={() => cache.clear()}>
+                        {title}
+                      </Link>
+                    </li>
+                  )
+              )} */}
+              {item.parents.length > 2 && (
+                <>
+                  <li key={item._id} className="rounded py-1.5 pr-2">
+                    <Link href={`/base-de-connaissance/${item.parents[1].slug}`} className="rounded px-2 py-1.5 text-[12px]">
+                      {item.parents[1].title}
+                    </Link>
+                  </li>
+                  {Array.isArray(siblingsData) &&
+                    siblingsData.length > 0 &&
+                    siblingsData[0].map(
+                      ({ _id, slug, title, type, status }) =>
+                        type === "article" &&
+                        status === "PUBLISHED" && (
+                          <li key={_id} className={`flex flex-nowrap items-center gap-1 ${_id === item._id ? "bg-gray-200" : ""}`}>
+                            <Link href={`/base-de-connaissance/${slug}`} className="rounded px-2 py-1.5 text-[12px]" onClick={() => cache.clear()}>
+                              {title}
+                            </Link>
+                          </li>
+                        )
+                    )}
+                </>
+              )}
+            </ol>
           </div>
           <div>
-            {item?.updatedAt && (
-              <span className="mb-4 ml-auto mt-2 flex flex-col items-end text-xs italic text-gray-400 print:mb-2 print:mt-0">
-                {/* <em>Article mis à jour le {Intl.DateTimeFormat("fr-FR", { year: "numeric", month: "long", day: "numeric" }).format(new Date(item.updatedAt))}</em> */}
-                <button
-                  className="noprint mt-2 hidden cursor-pointer rounded-md border border-gray-300 bg-white px-4 py-3 font-normal text-black shadow-none md:block"
-                  onClick={window.print}
-                >
-                  <div className="flex justify-center">
-                    <HiPrinter className="w-[20px] text-[20px] text-[#374151]" />
-                  </div>
-                </button>
-              </span>
-            )}
+            <div className="flew-row flex justify-between pb-2 pt-4">
+              <div className="mr-4">
+                <h2 className="mb-2 text-[24px] font-bold print:mb-0 print:text-black">{group?.title}</h2>
+                <h1 className="mb-2 text-[24px] font-bold md:text-[30px] print:mb-0 print:text-black">{item?.title}</h1>
+                <h6 className="text-[18px] text-snu-purple-100 md:text-[18px] lg:text-xl print:text-black">{item?.description}</h6>
+              </div>
+              <div>
+                {item?.updatedAt && (
+                  <span className="mb-4 ml-auto mt-2 flex flex-col items-end text-xs italic text-gray-400 print:mb-2 print:mt-0">
+                    {/* <em>Article mis à jour le {Intl.DateTimeFormat("fr-FR", { year: "numeric", month: "long", day: "numeric" }).format(new Date(item.updatedAt))}</em> */}
+                    <button
+                      className="noprint mt-2 hidden cursor-pointer rounded-md border border-gray-300 bg-white px-4 py-3 font-normal text-black shadow-none md:block"
+                      onClick={window.print}
+                    >
+                      <div className="flex justify-center">
+                        <HiPrinter className="w-[20px] text-[20px] text-[#374151]" />
+                      </div>
+                    </button>
+                  </span>
+                )}
+              </div>
+            </div>
+            <hr className="mb-6 mt-4" />
+            <TextEditor readOnly content={item.content} _id={item._id} slug={item.slug} />
+            <ToastContainer />
+            <FeedbackComponent item={item} />
+            <hr className="mb-6 mt-4" />
+            <KnowledgeBasePublicNoAnswer />
           </div>
         </div>
-        <hr className="mb-6 mt-4" />
-        <TextEditor readOnly content={item.content} _id={item._id} slug={item.slug} />
-        <ToastContainer />
-        <FeedbackComponent item={item} />
-        <hr className="mb-6 mt-4" />
-        <KnowledgeBasePublicNoAnswer />
       </section>
     </div>
   );
