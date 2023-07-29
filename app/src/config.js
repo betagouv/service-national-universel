df502774a784fb668c968ab6cc372afdbe8c26a7
Szilagyi Kinga <szilagyikinga42@gmail.com>
2023-07-29 07:08:36 +0200

    fix(app): kb url on local and staging

--- app/src/config.js
+++ app/src/config.js
@@ -3,7 +3,8 @@ const environment = import.meta.env.MODE;
 let apiURL = "http://localhost:8080";
 let adminURL = "http://localhost:8082";
 let appURL = "http://localhost:8081";
-let supportURL = "http://localhost:8083";
+// knowledgeBase URL
+let supportURL = "http://localhost:8084";
 let maintenance = false;
 let SENTRY_URL = "https://c5165ba99b4f4f2d8f1d4c0b16a654db@sentry.selego.co/14";
 let SENTRY_TRACING_SAMPLE_RATE = 1.0;
@@ -14,7 +15,8 @@ if (environment === "staging") {
   apiURL = "https://api.beta-snu.dev";
   adminURL = "https://admin.beta-snu.dev";
   appURL = "https://moncompte.beta-snu.dev";
-  supportURL = "https://app-9266b532-ff6e-4a6a-aeeb-e6ff7bb67f60.cleverapps.io";
+  // knowledgeBase URL
+  supportURL = "https://support.beta-snu.dev";
   SENTRY_URL = "https://c5165ba99b4f4f2d8f1d4c0b16a654db@sentry.selego.co/14";
   SENTRY_TRACING_SAMPLE_RATE = 1.0;
   SENTRY_SESSION_SAMPLE_RATE = 0.5;
@@ -24,6 +26,7 @@ if (environment === "production") {
   apiURL = "https://api.snu.gouv.fr";
   adminURL = "https://admin.snu.gouv.fr";
   appURL = "https://moncompte.snu.gouv.fr";
+  // knowledgeBase URL
   supportURL = "https://support.snu.gouv.fr";
   SENTRY_URL = "https://d09670865360498e9567369808de4064@sentry.selego.co/13";
   SENTRY_TRACING_SAMPLE_RATE = 0.01;
