9e967c5242efc8941c1ec5f401112bed70b9ec88
Guillaume Pichon <73333478+GuiPich91@users.noreply.github.com>
2023-07-31 14:05:27 +0200

    Fix(BDC Front): fix cropped button on sectionPage  (#2844)

--- knowledge-base-public/src/components/BlueIcon.js
+++ knowledge-base-public/src/components/BlueIcon.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const BlueIcon = ({ icon, className }) => (
-  <span aria-hidden className={`material-icons flex h-9 w-9 items-center justify-center rounded-md bg-snu-purple-900 text-white ${className}`}>
+  <span aria-hidden className={`material-icons flex h-9 w-9 flex-none items-center justify-center rounded-md bg-snu-purple-900 text-white ${className}`}>
     {icon}
   </span>
 );
