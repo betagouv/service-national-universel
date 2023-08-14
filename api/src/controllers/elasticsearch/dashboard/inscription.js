4e794ec2feee5423b6e2d1f25166c2d3a0adaa0b
Guillaume <guillaume@macmax.home>
2023-08-14 14:49:06 +0200

    update

--- api/src/controllers/elasticsearch/dashboard/inscription.js
+++ api/src/controllers/elasticsearch/dashboard/inscription.js
@@ -7,3 +7,315 @@ const { ERRORS } = require("../../../utils");
 const { joiElasticSearch } = require("../utils");
 const { ROLES, ES_NO_LIMIT, YOUNG_STATUS_PHASE1 } = require("snu-lib");
 
+router.post("/inscriptionGoal", passport.authenticate(["referent"], { session: false, failWithError: true }), async (req, res) => {
+  try {
+    const body = {
+      query: { bool: { must: { match_all: {} } } },
+      size: ES_NO_LIMIT,
+    };
+    const responseInscription = await esClient.search({ index: "inscriptiongoal", body: body });
+    if (!responseInscription?.body) {
+      return res.status(404).send({ ok: false, code: ERRORS.INVALID_BODY });
+    }
+    return res.status(200).send(responseInscription.body);
+  } catch (error) {
+    capture(error);
+    res.status(500).send({ ok: false, code: ERRORS.SERVER_ERROR });
+  }
+});
+
+router.post("/youngBySchool", passport.authenticate(["referent"], { session: false, failWithError: true }), async (req, res) => {
+  try {
+    const filterFields = ["status", "cohort", "academy", "departement"];
+    const { queryFilters, error } = joiElasticSearch({ filterFields, body: req.body });
+    if (error) return res.status(400).send({ ok: false, code: ERRORS.INVALID_PARAMS });
+    const body = {
+      query: { bool: { must: { match_all: {} }, filter: [] } },
+      aggs: {
+        school: {
+          terms: { field: "schoolId.keyword", size: 500 },
+          aggs: { departments: { terms: { field: "department.keyword" } }, firstUser: { top_hits: { size: 1 } } },
+        },
+      },
+      size: 0,
+      track_total_hits: true,
+    };
+    if (queryFilters.region?.length) body.query.bool.filter.push({ terms: { "schoolRegion.keyword": queryFilters.region } });
+    if (queryFilters.department?.length) body.query.bool.filter.push({ terms: { "schoolDepartment.keyword": queryFilters.department } });
+    if (queryFilters.cohort?.length) body.query.bool.filter.push({ terms: { "cohort.keyword": queryFilters.cohort } });
+    if (queryFilters.academy?.length) body.query.bool.filter.push({ terms: { "academy.keyword": queryFilters.academy } });
+
+    const result = await esClient.search({ index: "young", body: body });
+    const response = result.body;
+    return res.status(200).send(response);
+  } catch (error) {
+    capture(error);
+    res.status(500).send({ ok: false, code: ERRORS.SERVER_ERROR });
+  }
+});
+
+router.post("/youngsReport", async (req, res) => {
+  try {
+    const { filters, department } = req.body;
+
+    const body = {
+      query: {
+        bool: {
+          must: { match_all: {} },
+          filter: [{ terms: { "cohort.keyword": filters?.cohort } }, { term: { "department.keyword": department } }],
+        },
+      },
+      aggs: { status: { terms: { field: "status.keyword" } } },
+      size: 0,
+    };
+
+    const responses = await esClient.search({ index: "young", body: body });
+    console.log(responses.body.aggregations.status);
+    if (responses.body && responses.body.hits && responses.body.hits.hits) {
+      return res.status(200).json(responses.body);
+    } else {
+      return res.status(404).send("No matching data found in Elasticsearch");
+    }
+  } catch (error) {
+    capture(error);
+    return res.status(500).send("Error fetching data from Elasticsearch");
+  }
+});
+
+router.post("/inscriptionInfo", passport.authenticate(["referent"], { session: false, failWithError: true }), async (req, res) => {
+  try {
+    const filterFields = ["status", "cohort", "academy", "departement"];
+    const { queryFilters, error } = joiElasticSearch({ filterFields, body: req.body });
+    if (error) return res.status(400).send({ ok: false, code: ERRORS.INVALID_PARAMS });
+    const body = {
+      query: { bool: { must: { match_all: {} }, filter: [] } },
+      aggs: {
+        age: {
+          terms: {
+            field: "birthdateAt",
+            size: ES_NO_LIMIT,
+          },
+        },
+        gender: {
+          terms: {
+            field: "gender.keyword",
+            size: ES_NO_LIMIT,
+          },
+        },
+        grade: {
+          terms: {
+            field: "grade.keyword",
+            size: ES_NO_LIMIT,
+          },
+        },
+        situation: {
+          terms: {
+            field: "situation.keyword",
+            size: ES_NO_LIMIT,
+          },
+        },
+        qpv: {
+          terms: {
+            field: "qpv.keyword",
+            size: ES_NO_LIMIT,
+          },
+        },
+        rural: {
+          terms: {
+            field: "isRegionRural.keyword",
+            size: ES_NO_LIMIT,
+          },
+        },
+        handicap: {
+          terms: {
+            field: "handicap.keyword",
+            size: ES_NO_LIMIT,
+          },
+        },
+        allergies: {
+          terms: {
+            field: "allergies.keyword",
+            size: ES_NO_LIMIT,
+          },
+        },
+        handicapInSameDepartment: {
+          terms: {
+            field: "handicapInSameDepartment.keyword",
+            size: ES_NO_LIMIT,
+          },
+        },
+        reducedMobilityAccess: {
+          terms: {
+            field: "reducedMobilityAccess.keyword",
+            size: ES_NO_LIMIT,
+          },
+        },
+        ppsBeneficiary: {
+          terms: {
+            field: "ppsBeneficiary.keyword",
+            size: ES_NO_LIMIT,
+          },
+        },
+        paiBeneficiary: {
+          terms: {
+            field: "paiBeneficiary.keyword",
+            size: ES_NO_LIMIT,
+          },
+        },
+        specificAmenagment: {
+          terms: {
+            field: "specificAmenagment.keyword",
+            size: ES_NO_LIMIT,
+          },
+        },
+      },
+      size: 0,
+    };
+    if (queryFilters?.cohort?.length) body.query.bool.filter.push({ terms: { "cohort.keyword": queryFilters.cohort } });
+    if (queryFilters?.academy?.length) body.query.bool.filter.push({ terms: { "academy.keyword": queryFilters.academy } });
+    if (queryFilters?.region?.length)
+      body.query.bool.filter.push({
+        bool: {
+          should: [
+            { bool: { must: [{ term: { "schooled.keyword": "true" } }, { terms: { "schoolRegion.keyword": queryFilters.region } }] } },
+            { bool: { must: [{ term: { "schooled.keyword": "false" } }, { terms: { "region.keyword": queryFilters.region } }] } },
+          ],
+        },
+      });
+    if (queryFilters?.department?.length) body.query.bool.filter.push({ terms: { "department.keyword": queryFilters.department } });
+    if (queryFilters?.status?.length) body.query.bool.filter.push({ terms: { "status.keyword": queryFilters.status } });
+
+    const result = await esClient.search({ index: "young", body: body });
+    const response = result.body;
+    return res.status(200).send(response);
+  } catch (error) {
+    capture(error);
+    res.status(500).send({ ok: false, code: ERRORS.SERVER_ERROR });
+  }
+});
+
+router.post("/getInAndOutCohort", passport.authenticate(["referent"], { session: false, failWithError: true }), async (req, res) => {
+  try {
+    const filterFields = ["status", "cohort", "academy", "departement"];
+    const { queryFilters, error } = joiElasticSearch({ filterFields, body: req.body });
+    if (error) return res.status(400).send({ ok: false, code: ERRORS.INVALID_PARAMS });
+    const cohortList = queryFilters?.cohort?.length ? queryFilters.cohort : COHORTS;
+
+    const aggs = cohortList.reduce((acc, cohort) => {
+      acc["in&" + cohort] = {
+        filter: {
+          bool: {
+            must: [{ term: { "cohort.keyword": cohort } }, { term: { "status.keyword": "VALIDATED" } }, { exists: { field: "originalCohort.keyword" } }],
+            must_not: [{ term: { "originalCohort.keyword": cohort } }],
+            filter: [],
+          },
+        },
+      };
+      acc["out&" + cohort] = {
+        filter: {
+          bool: {
+            must: [{ term: { "originalCohort.keyword": cohort } }, { term: { "status.keyword": "VALIDATED" } }, { exists: { field: "originalCohort.keyword" } }],
+            must_not: [{ term: { "cohort.keyword": cohort } }],
+            filter: [],
+          },
+        },
+      };
+      return acc;
+    }, {});
+
+    const body = {
+      query: { bool: { must: { match_all: {} }, filter: [] } },
+      aggs,
+      size: 0,
+    };
+
+    if (queryFilters?.academy?.length) body.query.bool.filter.push({ terms: { "academy.keyword": queryFilters.academy } });
+    if (queryFilters?.region?.length)
+      body.query.bool.filter.push({
+        bool: {
+          should: [
+            { bool: { must: [{ term: { "schooled.keyword": "true" } }, { terms: { "schoolRegion.keyword": queryFilters.region } }] } },
+            { bool: { must: [{ term: { "schooled.keyword": "false" } }, { terms: { "region.keyword": queryFilters.region } }] } },
+          ],
+        },
+      });
+    if (queryFilters?.department?.length) body.query.bool.filter.push({ terms: { "department.keyword": queryFilters.department } });
+
+    const result = await esClient.search({ index: "young", body: body });
+    const response = result.body;
+    return res.status(200).send(response);
+  } catch (error) {
+    capture(error);
+    res.status(500).send({ ok: false, code: ERRORS.SERVER_ERROR });
+  }
+});
+
+router.post("/youngForInscription", passport.authenticate(["referent"], { session: false, failWithError: true }), async (req, res) => {
+  try {
+    const filterFields = ["statusPhase1", "statusPhase2", "statusPhase3", "status", "cohort", "academy", "departement"];
+    const { queryFilters, error } = joiElasticSearch({ filterFields, body: req.body });
+    if (error) return res.status(400).send({ ok: false, code: ERRORS.INVALID_PARAMS });
+    const body = {
+      query: {
+        bool: {
+          must: { match_all: {} },
+          filter: [
+            queryFilters?.cohort?.length ? { terms: { "cohort.keyword": queryFilters.cohort } } : null,
+            queryFilters?.academy?.length ? { terms: { "academy.keyword": queryFilters.academy } } : null,
+            queryFilters?.department?.length ? { terms: { "department.keyword": queryFilters.department } } : null,
+          ].filter(Boolean),
+        },
+      },
+      aggs: {
+        status: {
+          terms: {
+            field: "status.keyword",
+            size: ES_NO_LIMIT,
+          },
+          aggs: {
+            statusPhase1: {
+              terms: {
+                field: "statusPhase1.keyword",
+                size: ES_NO_LIMIT,
+              },
+            },
+            statusPhase2: {
+              terms: {
+                field: "statusPhase2.keyword",
+                size: ES_NO_LIMIT,
+              },
+            },
+            statusPhase3: {
+              terms: {
+                field: "statusPhase3.keyword",
+                size: ES_NO_LIMIT,
+              },
+            },
+          },
+        },
+      },
+      size: 0,
+    };
+
+    if (queryFilters?.region?.length)
+      body.query.bool.filter.push({
+        bool: {
+          should: [
+            { bool: { must: [{ term: { "schooled.keyword": "true" } }, { terms: { "schoolRegion.keyword": queryFilters.region } }] } },
+            { bool: { must: [{ term: { "schooled.keyword": "false" } }, { terms: { "region.keyword": queryFilters.region } }] } },
+          ],
+        },
+      });
+    const responseYoung = await esClient.search({ index: "young", body: body });
+    if (!responseYoung?.body) {
+      return res.status(404).send({ ok: false, code: ERRORS.INVALID_BODY });
+    }
+    const youngCenter = responseYoung.body;
+    return res.status(200).send(youngCenter);
+  } catch (error) {
+    capture(error);
+    res.status(500).send({ ok: false, code: ERRORS.SERVER_ERROR });
+  }
+});
+
+module.exports = router;
