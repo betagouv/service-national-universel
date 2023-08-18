282dea6540f0a2ba9fb7190a00fb5477a0f28ff4
Michael Garcia <mika_garcia21@hotmail.com>
2023-08-18 15:40:30 +0200

    up

--- api/src/rateLimiters.js
+++ api/src/rateLimiters.js
@@ -1,37 +1,37 @@
 class RateLimiter {
-    constructor(limitPerSecond = 10) {
-      this.limit = limitPerSecond;
-      this.calls = 0;
-      this.queue = [];
-      setInterval(() => this._release(), 1000);
-    }
-  
-    private _release() {
-      this.calls = 0;
-      while (this.calls < this.limit && this.queue.length) {
-        const nextCall = this.queue.shift();
-        this.calls += 1;
-        nextCall();
-      }
-    }
-  
-    async call(fn) {
-      return new Promise((resolve, reject) => {
-        const tryCall = () => {
-          if (this.calls < this.limit) {
-            this.calls += 1;
-            resolve(fn());
-          } else {
-            this.queue.push(tryCall);
-          }
-        };
-        tryCall();
-      });
+  constructor(limitPerSecond = 10) {
+    this.limit = limitPerSecond;
+    this.calls = 0;
+    this.queue = [];
+    setInterval(() => this._release(), 1000);
+  }
+
+  _release() {
+    this.calls = 0;
+    while (this.calls < this.limit && this.queue.length) {
+      const nextCall = this.queue.shift();
+      this.calls += 1;
+      nextCall();
     }
   }
-  
-  const rateLimiterContactSIB = new RateLimiter(10);
-  
-  module.exports = {
-    rateLimiterContactSIB,
-  };
+
+  async call(fn) {
+    return new Promise((resolve, reject) => {
+      const tryCall = () => {
+        if (this.calls < this.limit) {
+          this.calls += 1;
+          resolve(fn());
+        } else {
+          this.queue.push(tryCall);
+        }
+      };
+      tryCall();
+    });
+  }
+}
+
+const rateLimiterContactSIB = new RateLimiter(10);
+
+module.exports = {
+  rateLimiterContactSIB,
+};
