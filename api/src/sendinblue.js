354324bed02a1b3fa1427646ab393a1fbb648ac6
Michael Garcia <mika_garcia21@hotmail.com>
2023-08-18 15:30:41 +0200

    upup

--- api/src/sendinblue.js
+++ api/src/sendinblue.js
@@ -4,6 +4,7 @@ const queryString = require("querystring");
 const { SENDINBLUEKEY, ENVIRONMENT } = require("./config");
 const { capture } = require("./sentry");
 const { SENDINBLUE_TEMPLATES, YOUNG_STATUS } = require("snu-lib");
+const { rateLimiterContactSIB } = require("./rateLimiters");
 
 const SENDER_NAME = "Service National Universel";
 const SENDER_NAME_SMS = "SNU";
@@ -234,6 +235,8 @@ async function updateContact(id, { attributes, emailBlacklisted, smsBlacklisted,
 }
 
 async function sync(obj, type, { force } = { force: false }) {
+  const res = await rateLimiterContactSIB.call(() => console.log("TEST"));
+
   if (ENVIRONMENT !== "production" && !force) return console.log("no sync sendinblue");
   try {
     const user = JSON.parse(JSON.stringify(obj));
@@ -292,7 +295,7 @@ async function sync(obj, type, { force } = { force: false }) {
 
 async function syncContact(email, attributes, listIds) {
   try {
-    const res = await createContact({ email, attributes, listIds, updateEnabled: true });
+    const res = await rateLimiterContactSIB.call(() => createContact({ email, attributes, listIds, updateEnabled: true }));
     if (!res || res?.code) throw new Error(JSON.stringify({ res, email, attributes, listIds }));
     return;
   } catch (e) {
