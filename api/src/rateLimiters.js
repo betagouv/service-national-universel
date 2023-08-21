26cdae7f17a8d108efc707bdb03163619a63e093
Michael Garcia <mika_garcia21@hotmail.com>
2023-08-21 10:49:57 +0200

    up

--- api/src/rateLimiters.js
+++ api/src/rateLimiters.js
@@ -3,7 +3,20 @@ class RateLimiter {
     this.limit = limitPerSecond;
     this.calls = 0;
     this.queue = [];
-    setInterval(() => this._release(), 1000);
+    this.interval = null;
+  }
+
+  _startInterval() {
+    if (!this.interval) {
+      this.interval = setInterval(() => this._release(), 1000);
+    }
+  }
+
+  _stopInterval() {
+    if (this.interval && !this.hasPendingTasks()) {
+      clearInterval(this.interval);
+      this.interval = null;
+    }
   }
 
   _release() {
@@ -13,6 +26,7 @@ class RateLimiter {
       this.calls += 1;
       nextCall();
     }
+    this._stopInterval(); // Check if interval should be stopped after processing the queue
   }
 
   async call(fn) {
@@ -23,10 +37,23 @@ class RateLimiter {
           resolve(fn());
         } else {
           this.queue.push(tryCall);
+          this._startInterval(); // Start the interval when there are tasks in the queue
         }
       };
       tryCall();
     });
+  }
+
+  hasPendingTasks() {
+    return this.queue.length > 0 || this.calls > 0;
+  }
+
+  stop() {
+    // Not needed anymore, but keep it for manual stopping if required
+    if (this.interval) {
+      clearInterval(this.interval);
+      this.interval = null;
+    }
   }
 }
 
