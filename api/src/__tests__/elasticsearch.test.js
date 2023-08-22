d111e165a350245a65df46538768e74d67791b59
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2023-08-22 18:18:20 +0200

    update tests

--- api/src/__tests__/elasticsearch.test.js
+++ api/src/__tests__/elasticsearch.test.js
@@ -1,6 +1,9 @@
 require("dotenv").config({ path: "./.env-testing" });
 const request = require("supertest");
 const getAppHelper = require("./helpers/app");
+const { createMissionHelper } = require("./helpers/mission");
+const { createMissionApiHelper } = require("./helpers/missionapi");
+const getNewMissionFixture = require("./fixtures/mission");
 const { dbConnect, dbClose } = require("./helpers/db");
 
 jest.setTimeout(10_000);
@@ -13,8 +16,21 @@ afterAll(dbClose);
 describe("Elasticsearch", () => {
   describe("POST /elasticsearch/missionapi/search", () => {
     it("Should return 200", async () => {
-      const body = { filters: { distance: 10, location: { lat: 48.856614, lon: 2.3522219 } }, page: 0, size: 20, sort: "geo" };
+      const mission = await createMissionApiHelper(getNewMissionFixture({ domain: "education" }));
+      const body = { filters: { domain: mission.domain, distance: 50, location: { lat: 48.856614, lon: 2.3522219 } }, page: 0, size: 20, sort: "geo" };
       const response = await request(getAppHelper()).post("/elasticsearch/missionapi/search").send(body);
+      expect(response.status).toBe(200);
+      expect(response.body).toBeDefined();
+      expect(response.body.ok).toBe(true);
+      expect(response.body.data).toBeDefined();
+    });
+  });
+  describe("POST /elasticsearch/mission/young/search", () => {
+    it("Should return 200", async () => {
+      const mission = await createMissionHelper(getNewMissionFixture({ domains: ["CITIZENSHIP"] }));
+      const body = { filters: { domains: mission.domains, distance: 50, location: { lat: 48.856614, lon: 2.3522219 } }, page: 0, size: 20, sort: "geo" };
+      console.log("🚀 ~ file: elasticsearch.test.js:31 ~ it ~ mission:", mission)
+      const response = await request(getAppHelper()).post("/elasticsearch/mission/young/search").send(body);
       expect(response.status).toBe(200);
       expect(response.body).toBeDefined();
       expect(response.body.ok).toBe(true);
---
+++ api/src/__tests__/helpers/missionapi.js
@@ -0,0 +1,55 @@
+const MissionObject = require("../../models/missionapi");
+
+async function getMissionsApiHelper(params = {}) {
+  return await MissionObject.find(params);
+}
+
+async function getMissionApiByIdHelper(missionId) {
+  return await MissionObject.findById(missionId);
+}
+
+async function deleteMissionApiByIdHelper(missionId) {
+  const mission = await getMissionApiByIdHelper(missionId);
+  await mission.remove();
+}
+
+async function createMissionApiHelper(mission) {
+  return await MissionObject.create(mission);
+}
+
+function expectMissionToEqual(mission, expectedMission) {
+  // Need to parse the objects because attributes types changed
+  // Deep equal failed on Date which became string
+  const missionParsed = JSON.parse(JSON.stringify(mission));
+  const expectedMissionParsed = JSON.parse(JSON.stringify(expectedMission));
+  expect(missionParsed.name).toEqual(expectedMissionParsed.name);
+  expect(missionParsed.startAt).toEqual(expectedMissionParsed.startAt);
+  expect(missionParsed.endAt).toEqual(expectedMissionParsed.endAt);
+  expect(missionParsed.placesTotal).toEqual(expectedMissionParsed.placesTotal);
+  expect(missionParsed.placesLeft).toEqual(expectedMissionParsed.placesLeft);
+  expect(missionParsed.actions).toEqual(expectedMissionParsed.actions);
+  expect(missionParsed.description).toEqual(expectedMissionParsed.description);
+  expect(missionParsed.justifications).toEqual(expectedMissionParsed.justifications);
+  expect(missionParsed.contraintes).toEqual(expectedMissionParsed.contraintes);
+  expect(missionParsed.structureName).toEqual(expectedMissionParsed.structureName);
+  expect(missionParsed.address).toEqual(expectedMissionParsed.address);
+  expect(missionParsed.zip).toEqual(expectedMissionParsed.zip);
+  expect(missionParsed.city).toEqual(expectedMissionParsed.city);
+  expect(missionParsed.department).toEqual(expectedMissionParsed.department);
+  expect(missionParsed.region).toEqual(expectedMissionParsed.region);
+  expect(missionParsed.country).toEqual(expectedMissionParsed.country);
+  expect(missionParsed.remote).toEqual(expectedMissionParsed.remote);
+  expect(missionParsed.location.lat).toEqual(expectedMissionParsed.location.lat);
+  expect(missionParsed.location.lon).toEqual(expectedMissionParsed.location.lon);
+}
+
+const notExisitingMissionId = "123a49ba503040e4d2153944";
+
+module.exports = {
+  getMissionsApiHelper,
+  getMissionApiByIdHelper,
+  deleteMissionApiByIdHelper,
+  createMissionApiHelper,
+  expectMissionToEqual,
+  notExisitingMissionId,
+};
