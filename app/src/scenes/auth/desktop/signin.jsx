e3dd13f36f09f3c794368bab10bd71424832d08f
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2023-08-25 11:47:15 +0200

    chore(app): update signin text

--- app/src/scenes/auth/desktop/signin.jsx
+++ app/src/scenes/auth/desktop/signin.jsx
@@ -110,7 +110,7 @@ export default function Signin() {
         <div className="mt-3 text-[#E5E5E5] space-y-3">
           <div className="mt-3 mb-2 text-center text-xl font-bold text-[#161616]">Vous n&apos;êtes pas encore inscrit(e) ?</div>
           <p className="text-center text-base text-[#161616] my-3">
-            Les inscriptions sont actuellement uniquement ouvertes aux volontaires âgés de 15 à 17 ans et scolarisés en seconde en Nouvelle-Calédonie ou à Wallis-et-Futuna.
+            Les inscriptions sont actuellement uniquement ouvertes aux volontaires âgés de 15 à 17 ans et scolarisés ou résidant en Nouvelle-Calédonie ou à Wallis-et-Futuna.
             <br />
           </p>
           <Link
