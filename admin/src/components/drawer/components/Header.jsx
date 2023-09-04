8786d7ecbb13e00000d14df8dc2c8e3a38ee262c
hlecourt <hlecourt99@gmail.com>
2023-09-04 16:54:59 +0200

    chore(admin): detail sidebar
    Icon, comportement + env

--- admin/src/components/drawer/components/Header.jsx
+++ admin/src/components/drawer/components/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
 import logo_snu from "../../../assets/logo-snu.png";
 import Separator from "./Separator";
+import { environment } from "../../../config";
 
 export default function Header({ open, setOpen }) {
   //save the current state of the sidebar in the local storage
@@ -11,7 +12,8 @@ export default function Header({ open, setOpen }) {
   };
   return (
     <>
-      <button className={`group flex flex-row h-[116px] !pt-8 !pb-7 !pl-4 hover:bg-[#1B1F42] items-center ${open && "!pr-2"}`} onClick={onChangeOpen}>
+      <button className={`group relative flex flex-row h-[116px] !pt-8 !pb-7 !pl-4 hover:bg-[#1B1F42] items-center ${open && "!pr-2"}`} onClick={onChangeOpen}>
+        <Badge open={open} />
         <img className="w-[56px] h-[56px]" src={logo_snu} alt="logo_snu" />
         {open ? (
           <p className="ml-3 text-white uppercase text-sm font-medium text-left leading-[17px] w-full">
@@ -25,3 +27,15 @@ export default function Header({ open, setOpen }) {
     </>
   );
 }
+
+const Badge = ({ open }) => {
+  if (environment === "production") return <></>;
+  return (
+    <div
+      className={`absolute top-[5px] ${open ? "right-[10px]" : "right-1/2 transform translate-x-1/2"} h-[18px] border-[1px] ${
+        environment === "development" ? "bg-blue-500" : "bg-red-500"
+      } border-white rounded-full text-white text-[10px] align-middle px-1 uppercase`}>
+      {environment === "development" ? "Dev" : "Test"}
+    </div>
+  );
+};
