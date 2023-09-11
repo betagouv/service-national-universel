607af576631726ddcd0f6fff4a45ffe34229c354
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2023-09-11 10:40:08 +0200

    done

--- knowledge-base-public/src/pages/base-de-connaissance/sitemap.js
+++ knowledge-base-public/src/pages/base-de-connaissance/sitemap.js
@@ -8,46 +8,61 @@ import useUser from "../../hooks/useUser";
 
 const SiteMap = ({ items }) => {
   const { restriction } = useUser();
-  const filteredItems = restriction ? items.filter((item) => item.allowedRoles.includes(restriction)) : items;
 
   return (
-    <Wrapper home>
+    <Wrapper>
       <Head>
-        <title>SNU - Base de connaissance</title>
+        <title>SNU - Base de connaissance - Plan du site</title>
       </Head>
       <div className="bg-white">
-        <div className="max-w-6xl mx-auto p-4">
-        <Breadcrumbs parents={[{ _id: 1, slug: "sitemap", title: "Plan du site" }]} path="/base-de-connaissance" />
+        <div className="max-w-6xl mx-auto p-4 mb-8">
+          <Breadcrumbs parents={[{ _id: 1, slug: "sitemap", title: "Plan du site" }]} path="/base-de-connaissance" />
           <h1 className="text-3xl font-bold leading-9 py-8">Plan du site</h1>
           <nav>
-            <ul className="list-disc ml-4 space-y-6 text-gray-600 underline">
+            <ul className="list-disc ml-4 space-y-6">
               <li>
-                <Link href="/">Accueil</Link>
+                <Link href="/" className="text-gray-600 underline hover:text-blue-800">
+                  Accueil
+                </Link>
               </li>
 
-              {filteredItems.map((item) => (
-                <>
-                  <li key={item.slug}>
-                    <Link href={"/base-de-connaissance/" + item.slug}>{item.title}</Link>
-                  </li>
-                  <ul className="list-disc ml-6 space-y-6">
-                    {item.children.map((child) => (
-                      <li key={child.slug}>
-                        <Link href={"/base-de-connaissance/" + child.slug}>{child.title}</Link>
-                      </li>
-                    ))}
-                  </ul>
-                </>
-              ))}
+              {items
+                .filter((item) => (restriction ? item.allowedRoles.includes(restriction) : item))
+                .map((item) => (
+                  <>
+                    <li key={item.slug} className="space-x-2">
+                      <Link href={"/base-de-connaissance/" + item.slug} className="text-gray-600 underline underline-offset-2 hover:text-blue-800">
+                        {item.title}
+                      </Link>
+                    </li>
+                    <ul className="list-disc ml-6 space-y-6">
+                      {item.children
+                        .filter((child) => (restriction ? child.allowedRoles.includes(restriction) : child))
+                        .map((child) => (
+                          <li key={child.slug} className="space-x-2">
+                            <Link href={"/base-de-connaissance/" + child.slug} className="text-gray-600 underline underline-offset-2 hover:text-blue-800">
+                              {child.title}
+                            </Link>
+                          </li>
+                        ))}
+                    </ul>
+                  </>
+                ))}
 
               <li>
-                <a href="https://snu.gouv.fr">Retour sur le ste du SNU</a>
+                <a href="https://snu.gouv.fr" rel="noopener noreferrer" target="_blank" className="text-gray-600 underline underline-offset-2 hover:text-blue-800">
+                  Retour sur le site du SNU
+                </a>
               </li>
               <li>
-                <a href={appURL}>Espace volontaire</a>
+                <a href={appURL} rel="noopener noreferrer" target="_blank" className="text-gray-600 underline underline-offset-2 hover:text-blue-800">
+                  Espace volontaire
+                </a>
               </li>
               <li>
-                <a href={adminURL}>Espace professionnel</a>
+                <a href={adminURL} rel="noopener noreferrer" target="_blank" className="text-gray-600 underline underline-offset-2 hover:text-blue-800">
+                  Espace professionnel
+                </a>
               </li>
             </ul>
           </nav>
@@ -59,7 +74,8 @@ const SiteMap = ({ items }) => {
 
 export async function getStaticProps() {
   const res = await API.get({ path: "/knowledge-base/sitemap" });
-  // Why not query DB here directly?
+  // TODO: implement direct DB querying from Next app instead of Support API:
+  // https://github.com/vercel/next.js/blob/canary/examples/with-mongodb/lib/mongodb.ts
   if (!res.ok) {
     return {
       notFound: true,
