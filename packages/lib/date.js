8578ba62860d008d9ee4330e568220712f38524c
flawa <hugopozzoli@gmail.com>
2023-09-11 19:04:43 +0200

    fix(snu-lib): rebase

--- packages/lib/date.js
+++ packages/lib/date.js
@@ -166,16 +166,6 @@ function calculateAge(birthDate, otherDate) {
   return years;
 }
 
-const formatDateForPostGre = (date) => {
-  if (!date) return "-";
-  const d = new Date(date);
-  const year = d.getFullYear();
-  const month = (d.getMonth() + 1).toString().padStart(2, "0");
-  const day = d.getDate().toString().padStart(2, "0");
-
-  return `${year}-${month}-${day}`;
-};
-
 export {
   MONTHS,
   formatDay,
