cd33ada07dbc11865d87bcf8104cb6e2264f6024
flawa <hugopozzoli@gmail.com>
2023-09-13 11:48:23 +0200

    fix(snu-lib): formatDateForPostGre duplication

--- packages/lib/date.js
+++ packages/lib/date.js
@@ -86,16 +86,6 @@ const formatStringDateTimezoneUTC = (date) => {
   });
 };
 
-const formatDateForPostGre = (date) => {
-  if (!date) return "-";
-  const d = new Date(date);
-  const year = d.getFullYear();
-  const month = (d.getMonth() + 1).toString().padStart(2, "0");
-  const day = d.getDate().toString().padStart(2, "0");
-
-  return `${year}-${month}-${day}`;
-};
-
 const formatStringDateWithDayTimezoneUTC = (date) => {
   if (!date) return "-";
   const d = new Date(date);
