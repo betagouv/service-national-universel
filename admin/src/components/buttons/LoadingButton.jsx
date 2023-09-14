d95e0c72fb661a737dbfe6e49691186898e4e014
Michael Garcia <mika_garcia21@hotmail.com>
2023-09-14 17:50:33 +0200

    upup

--- admin/src/components/buttons/LoadingButton.jsx
+++ admin/src/components/buttons/LoadingButton.jsx
@@ -41,7 +41,7 @@ const VioletButtonHeader = styled(Button)`
   cursor: pointer;
   ${({ disabled, color }) => (!disabled && !color ? `:hover {background: ${colors.darkPurple} !important;}` : null)}
   ${({ color }) => (color ? `background-color: ${color} !important;}` : `background-color: ${colors.purple} !important;`)}
-    &:hover {
+  &:hover {
     box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.5);
   }
 `;
