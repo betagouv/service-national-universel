f9146df4e98a48a75e61cd72d2d6b9b77d44913a
flawa <hugopozzoli@gmail.com>
2023-09-18 14:42:15 +0200

    feat(admin/api): dashboard volontaires equivalence mig

---
+++ admin/src/scenes/dashboardV2/moderator-ref/subscenes/engagement/components/VolontairesEquivalenceMig.jsx
@@ -0,0 +1,152 @@
+import React, { useEffect, useState } from "react";
+import { translateEquivalenceStatus } from "snu-lib";
+import queryString from "query-string";
+import api from "@/services/api";
+import DashboardBox from "@/scenes/dashboardV2/components/ui/DashboardBox";
+import StatusTable from "@/scenes/dashboardV2/components/ui/StatusTable";
+import { FullDoughnut } from "@/scenes/dashboardV2/components/graphs";
+import { LoadingDoughnut } from "@/scenes/dashboardV2/components/ui/loading";
+
+export default function VolontairesEquivalenceMig({ filters }) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [statuses, setStatuses] = useState(null);
+  const [types, setTypes] = useState(null);
+  const [selected, setSelected] = useState("ALL");
+  const [filtered, setFiltered] = useState(null);
+
+  async function loadData() {
+    setError(null);
+    setLoading(true);
+    try {
+      const result = await api.post(`/dashboard/engagement/volontaires-equivalence-mig`, { filters });
+      if (result.ok) {
+        setStatuses(
+          result.data.statuses.map((status) => ({
+            label: translateEquivalenceStatus(status.status),
+            status: status.status,
+            nb: status.value,
+            percentage: Math.round(status.percentage * 100),
+            url: `/volontaire?${queryString.stringify({
+              status: filters.status,
+              status_equivalence: status.status,
+            })}`,
+          })),
+        );
+        setTypes(result.data.types.map((type) => ({ ...type, url: `/volontaire?${queryString.stringify({ status: filters.status, type_equivalence: type.label })}` })));
+      } else {
+        console.log("error : ", result);
+        setError("Erreur: impossible de charger les données.");
+      }
+    } catch (err) {
+      console.log("unable to load volontaires equivalence mig: ", err);
+      setError("Erreur: impossible de charger les données.");
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    loadData();
+  }, [filters]);
+
+  useEffect(() => {
+    if (!statuses || !types) return;
+    const status = statuses.find((s) => s.status === selected);
+
+    setFiltered({
+      options: [{ label: "Tous", value: "ALL" }, ...statuses.map((s) => ({ label: s.label, value: s.status }))],
+      types:
+        selected === "ALL"
+          ? types
+          : types
+              .filter((t) => !!t.statuses[selected])
+              .map((t) => ({
+                ...t,
+                value: t.statuses[selected],
+                percentage: t.statuses[selected] / status.nb,
+                subTypes: t.subTypes
+                  .filter((st) => !!st.statuses[selected])
+                  .map((st) => ({
+                    ...st,
+                    value: st.statuses[selected],
+                    percentage: st.statuses[selected] / status.nb,
+                    url: `/volontaire?${queryString.stringify({
+                      status: filters.status,
+                      status_equivalence: selected,
+                      type_equivalence: t.label,
+                      sub_type_equivalence: st.label,
+                    })}`,
+                  })),
+                url: `/volontaire?${queryString.stringify({
+                  status: filters.status,
+                  status_equivalence: selected,
+                  type_equivalence: t.label,
+                })}`,
+              })),
+    });
+  }, [filters, selected, statuses, types]);
+
+  const getInfo = (type) => {
+    if (!type.subTypes?.length) return null;
+
+    return (
+      <div className="p-8">
+        <div className="mb-4 text-base font-bold text-gray-900">{type.label}</div>
+        <StatusTable className="grow" statuses={type.subTypes.map((t) => ({ ...t, status: t.label, nb: t.value, percentage: Math.round(t.percentage * 100) }))} nocols />
+      </div>
+    );
+  };
+
+  return (
+    <DashboardBox
+      title="Demandes d'équivalence de MIG"
+      headerChildren={
+        filtered &&
+        selected && (
+          <select value={selected} onChange={(e) => setSelected(e.target.value)}>
+            {filtered.options.map((o) => (
+              <option key={o.value} value={o.value}>
+                {o.label}
+              </option>
+            ))}
+          </select>
+        )
+      }
+      childrenClassName="pt-4">
+      {loading && (
+        <div className="flex items-stretch justify-center">
+          <StatusTable loading={loading} nocols colWidth="w-100" className="basis-2/4" />
+          <div className="mx-6 flex items-stretch basis-1/4 ">
+            <div className="w-[1px] bg-gray-300"></div>
+          </div>
+          <div className="flex basis-1/4">
+            <LoadingDoughnut />
+          </div>
+        </div>
+      )}
+      {error && <div className="flex items-center justify-center p-8 text-center text-sm font-medium text-red-600">{error}</div>}
+      {!loading && !error && (
+        <div className="flex items-stretch justify-stretch">
+          <StatusTable className="grow" statuses={statuses.map((s) => ({ ...s, status: s.label }))} loading={loading} nocols colWidth="w-100" />
+          <div className="mx-12 shrink-0 flex items-stretch">
+            <div className="w-[1px] bg-gray-300"></div>
+          </div>
+          <div>
+            <FullDoughnut
+              legendSide="right"
+              maxLegends={3}
+              labels={filtered.types.map((type) => type.label)}
+              values={filtered.types.map((type) => Math.round(type.percentage * 100))}
+              valueSuffix="%"
+              legendUrls={filtered.types.map((type) => type.url)}
+              tooltips={filtered.types.map((type) => type.value)}
+              className="justify-center"
+              legendInfoPanels={filtered.types.map(getInfo)}
+            />
+          </div>
+        </div>
+      )}
+    </DashboardBox>
+  );
+}
--- api/src/controllers/dashboard/engagement.js
+++ api/src/controllers/dashboard/engagement.js
@@ -159,18 +159,6 @@ router.post("/volontaires-statuts-divers", passport.authenticate("referent", { s
       }
     }
 
-    // missions equivalences
-    const youngIds = youngs.map((young) => young._id.toString());
-    const equivalences = await MissionEquivalenceModel.aggregate([
-      { $match: { youngId: { $in: youngIds } } },
-      {
-        $group: {
-          _id: `$status`,
-          count: { $sum: 1 },
-        },
-      },
-    ]);
-
     // --- format data
     let data = [];
     for (const status of Object.keys(phase2)) {
@@ -179,9 +167,98 @@ router.post("/volontaires-statuts-divers", passport.authenticate("referent", { s
     for (const status of Object.keys(contract)) {
       data.push({ category: "contract", status, value: contract[status], percentage: totalContract ? contract[status] / totalContract : 0 });
     }
-    const totalEquivalences = equivalences.reduce((acc, eq) => acc + eq.count, 0);
-    for (const status of equivalences) {
-      data.push({ category: "equivalence", status: status._id, value: status.count, percentage: totalEquivalences ? status.count / totalEquivalences : 0 });
+
+    // --- result
+    return res.status(200).send({ ok: true, data });
+  } catch (error) {
+    capture(error);
+    res.status(500).send({ ok: false, code: ERRORS.SERVER_ERROR });
+  }
+});
+
+router.post("/volontaires-equivalence-mig", passport.authenticate("referent", { session: false, failWithError: true }), async (req, res) => {
+  try {
+    // --- test body
+    const { error, value } = Joi.object({
+      filters: filtersJoi,
+    }).validate(req.body);
+    if (error) {
+      return res.status(400).send({ ok: false, code: ERRORS.INVALID_BODY });
+    }
+    const { filters } = value;
+
+    // --- get data
+    // TODO: optimization
+    const youngs = await YoungModel.find(computeYoungFilter(filters), { phase2ApplicationStatus: 1, statusPhase2Contract: 1 });
+
+    const youngIds = youngs.map((young) => young._id.toString());
+    const missions = await MissionEquivalenceModel.find({ youngId: { $in: youngIds } });
+
+    let total = 0;
+    let statuses = {};
+    let types = {};
+    let subTypes = {};
+    for (const mission of missions) {
+      ++total;
+
+      if (statuses[mission.status] === undefined) {
+        statuses[mission.status] = 0;
+      }
+      ++statuses[mission.status];
+
+      if (types[mission.type] === undefined) {
+        types[mission.type] = {
+          statuses: {
+            [mission.status]: 0,
+          },
+          count: 0,
+        };
+      }
+      if (types[mission.type].statuses[mission.status] === undefined) {
+        types[mission.type].statuses[mission.status] = 0;
+      }
+      ++types[mission.type].count;
+      ++types[mission.type].statuses[mission.status];
+
+      if (mission.sousType) {
+        if (subTypes[mission.sousType] === undefined) {
+          subTypes[mission.sousType] = {
+            statuses: {
+              [mission.status]: 0,
+            },
+            type: mission.type,
+            count: 0,
+          };
+        }
+        if (subTypes[mission.sousType].statuses[mission.status] === undefined) {
+          subTypes[mission.sousType].statuses[mission.status] = 0;
+        }
+        ++subTypes[mission.sousType].count;
+        ++subTypes[mission.sousType].statuses[mission.status];
+      }
+    }
+
+    const data = { statuses: [], types: [], subTypes: [] };
+    for (const status of Object.keys(statuses)) {
+      data.statuses.push({ status, value: statuses[status], percentage: total ? statuses[status] / total : 0 });
+    }
+    for (const subType of Object.keys(subTypes)) {
+      data.subTypes.push({
+        label: subType,
+        value: subTypes[subType].count,
+        percentage: total ? subTypes[subType].count / total : 0,
+        type: subTypes[subType].type,
+        statuses: subTypes[subType].statuses,
+      });
+    }
+    for (const type of Object.keys(types)) {
+      data.types.push({
+        label: type,
+        value: types[type].count,
+        percentage: total ? types[type].count / total : 0,
+        subTypes: data.subTypes.filter((subType) => subType.type === type),
+        statuses: types[type].statuses,
+      });
     }
 
     // --- result
