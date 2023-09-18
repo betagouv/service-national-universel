7930af6afa1bcaabbf45eba10dbb309e23baa60a
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2023-09-18 13:01:48 +0200

    consentement

--- app/src/scenes/preinscription/steps/stepProfil.jsx
+++ app/src/scenes/preinscription/steps/stepProfil.jsx
@@ -70,8 +70,7 @@ export default function StepProfil() {
     setError(errors);
     if (!Object.keys(errors).length) {
       setData({ ...data, email: trimmedEmail, step: PREINSCRIPTION_STEPS.CONFIRM });
-      plausibleEvent("Phase0/CTA preinscription - infos persos");
-      history.push("/preinscription/confirm");
+      plausibleEvent("Phase0/CTA preinscription - infos persos", { callback: () => history.push("/preinscription/confirm") });
     }
   };
 
