b3974c6424f49a983f016bc9452f1d8be6dd7c80
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2023-09-20 12:54:05 +0200

    add screen for IN_PROGRESS status

---
+++ app/src/scenes/inscription2023/InscriptionClosed.jsx
@@ -0,0 +1,16 @@
+import DSFRContainer from "@/components/dsfr/layout/DSFRContainer";
+import DSFRLayout from "@/components/dsfr/layout/DSFRLayout";
+import React from "react";
+import { useSelector } from "react-redux";
+import { translateCohort } from "snu-lib";
+
+export default function InscriptionClosed() {
+  const young = useSelector((state) => state.Auth.young);
+  return (
+    <DSFRLayout title="Inscription au séjour de cohésion">
+      <DSFRContainer title="Vous n'avez pas complété votre dossier d'inscription à temps.">
+        <p className="mb-16">Les inscriptions pour le séjour du {translateCohort(young?.cohort)} sont clôturées. Vous ne pourrez donc pas participer au séjour.</p>
+      </DSFRContainer>
+    </DSFRLayout>
+  );
+}
