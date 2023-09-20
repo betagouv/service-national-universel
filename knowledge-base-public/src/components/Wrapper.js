31d47bac4887421836bdc01c1f0cefa31f479b9b
Guillaume <guillaume@macmax.home>
2023-09-20 14:33:38 +0200

    fix

--- knowledge-base-public/src/components/Wrapper.js
+++ knowledge-base-public/src/components/Wrapper.js
@@ -9,7 +9,7 @@ import Footer from "./Footer";
 const Wrapper = ({ home, children }) => {
   const { user } = useUser();
   const { setSeeAs, seeAs } = useContext(SeeAsContext);
-  const withSeeAs = ["admin", "referent_department", "referent_region"].includes(user?.role);
+  const withSeeAs = ["admin", "referent_department", "referent_region", "head_center", "structure", "visitor", "dsnj"].includes(user?.role);
   const withSeeAsPublicAndYoung = ["public", "young"].includes(seeAs);
 
   return (
@@ -20,7 +20,7 @@ const Wrapper = ({ home, children }) => {
           <AiOutlineInfoCircle className="text-blue-500 text-xl flex-none" />
           <p className="text-sm text-blue-800">
             Vous visualisez la base de connaissance en tant que {translateRoleBDC[seeAs]}.{" "}
-            <button onClick={() => setSeeAs("admin")} className="noprint text-sm text-blue-800 underline reset">
+            <button onClick={() => setSeeAs(user.role)} className="noprint text-sm text-blue-800 underline reset">
               Rétablir la vue par défaut.
             </button>
           </p>
