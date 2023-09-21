d8abc6e84dcfcad0937c4f54b086332f5c96717e
Guillaume <guillaume@macmax.home>
2023-09-21 11:30:26 +0200

    update

--- knowledge-base-public/src/components/Wrapper.js
+++ knowledge-base-public/src/components/Wrapper.js
@@ -20,7 +20,7 @@ const Wrapper = ({ home, children }) => {
           <AiOutlineInfoCircle className="text-blue-500 text-xl flex-none" />
           <p className="text-sm text-blue-800">
             Vous visualisez la base de connaissance en tant que {translateRoleBDC[seeAs]}.{" "}
-            <button onClick={() => setSeeAs(user.role)} className="noprint text-sm text-blue-800 underline reset">
+            <button onClick={() => setSeeAs("admin")} className="noprint text-sm text-blue-800 underline reset">
               Rétablir la vue par défaut.
             </button>
           </p>
