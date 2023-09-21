5bed726758729a64b81e246c6fafb18e19a7b543
hlecourt <hlecourt99@gmail.com>
2023-09-21 14:44:53 +0200

    up

--- admin/src/scenes/dashboardV2/moderator-ref/subscenes/engagement/index.jsx
+++ admin/src/scenes/dashboardV2/moderator-ref/subscenes/engagement/index.jsx
@@ -107,7 +107,7 @@ export default function Index() {
 
   return (
     <DashboardContainer
-      availableTab={["general", "engagement", "sejour", "inscription", "analytics"]}
+      availableTab={["general", "engagement", "sejour", "inscription"]}
       active="engagement"
       navChildren={
         <div className="flex items-center gap-2">
