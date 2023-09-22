d2ac21167a853de5c036345a694a3952cd1897d1
Raphaël Huchet <raph@selego.co>
2023-09-22 14:24:22 +0200

    chore: up engagement

--- api/src/controllers/elasticsearch/dashboard/index.js
+++ api/src/controllers/elasticsearch/dashboard/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 router.use("/general", require("./general"));
 router.use("/sejour", require("./sejour"));
 router.use("/inscription", require("./inscription"));
+router.use("/engagement", require("./engagement"));
 
 module.exports = router;
