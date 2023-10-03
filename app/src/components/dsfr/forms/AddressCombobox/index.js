471047ea7acb938eeca8e93698ba8da7993b6de3
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2023-10-03 15:52:13 +0200

    up

--- app/src/components/dsfr/forms/AddressCombobox/index.js
+++ app/src/components/dsfr/forms/AddressCombobox/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import AddressDisplay from "./components/AddressDisplay";
-import AddressSearch from "./components/AddressSearch";
+import AddressDisplay from "./AddressDisplay";
+import AddressSearch from "./AddressSearch";
 
 export default function AddressCombobox({ data, setData }) {
   if (data?.city) return <AddressDisplay data={data} setData={setData} />;
