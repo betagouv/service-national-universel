e2f32730fb09772c52e7e16246153b997ff0824f
C2Chandelier <102954303+C2Chandelier@users.noreply.github.com>
2023-10-12 16:19:04 +0200

    up

--- admin/src/scenes/dashboardV2/head-center/subscenes/general/index.jsx
+++ admin/src/scenes/dashboardV2/head-center/subscenes/general/index.jsx
@@ -1,16 +1,8 @@
-import React, { useEffect } from "react";
-import api from "@/services/api";
+import React from "react";
+
 import DashboardContainer from "../../../components/DashboardContainer";
 
 export default function Index() {
-  useEffect(() => {
-    const updateStats = async () => {
-      const response = await api.post("/elasticsearch/dashboard/general/todo");
-      const s = response.data;
-      console.log(s);
-    };
-    updateStats();
-  }, []);
   return (
     <DashboardContainer active="general" availableTab={["general", "sejour"]}>
       <div>Général</div>
