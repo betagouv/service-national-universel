756b2c497dcd9286afbcff13f7ad1a2eb091adf5
Tangi Mendès <tangimds@gmail.com>
2023-10-20 10:05:42 +0200

    Update TabSchool.jsx

--- admin/src/scenes/dashboardV2/components/inscription/TabSchool.jsx
+++ admin/src/scenes/dashboardV2/components/inscription/TabSchool.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import { HiOutlineChevronLeft, HiOutlineChevronRight, HiOutlineExternalLink } from "react-icons/hi";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { ROLES } from "snu-lib";
 import api from "../../../../services/api";
 import { getNewLink, replaceSpaces } from "../../../../utils";
 import queryString from "query-string";
