51c7c566d58e5b41d186232362ed1f12cf965ed5
Tangi Mendès <tangimds@gmail.com>
2023-10-20 12:41:30 +0200

    Update index.jsx

--- admin/src/scenes/dashboardV2/visitor/subscenes/inscription/index.jsx
+++ admin/src/scenes/dashboardV2/visitor/subscenes/inscription/index.jsx
@@ -3,7 +3,7 @@ import General from "@/scenes/dashboardV2/components/inscription/General";
 
 export default function Index() {
   const [selectedFilters, setSelectedFilters] = useState({
-    cohort: ["Février 2023 - C", "Avril 2023 - A", "Avril 2023 - B", "Juin 2023", "Juillet 2023", "Octobre 2023 - NC"],
+    cohort: [],
   });
 
   return (
