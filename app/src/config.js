78506e851c05db081926f6a608054f19d67745e6
Pierre LE BIHAN <lebihanp@hotmail.fr>
2023-10-24 16:15:38 +0300

    up

--- app/src/config.js
+++ app/src/config.js
@@ -1,13 +1,13 @@
 const environment = import.meta.env.MODE;
-let apiURL = import.meta.env.API_URL ?? "http://localhost:8080";
-let appURL = import.meta.env.APP_URL ?? "http://localhost:8081";
-let adminURL = import.meta.env.ADMIN_URL ?? "http://localhost:8082";
-let supportURL = import.meta.env.SUPPORT_URL ?? "http://localhost:8083";
+let apiURL = import.meta.env.VITE_API_URL ?? "http://localhost:8080";
+let appURL = import.meta.env.VITE_APP_URL ?? "http://localhost:8081";
+let adminURL = import.meta.env.VITE_ADMIN_URL ?? "http://localhost:8082";
+let supportURL = import.meta.env.VITE_SUPPORT_URL ?? "http://localhost:8083";
 let maintenance = import.meta.env.VITE_MAINTENANCE === "true";
-let SENTRY_URL = import.meta.env.SENTRY_URL;
-let SENTRY_TRACING_SAMPLE_RATE = import.meta.env.SENTRY_TRACING_SAMPLE_RATE ?? 1.0;
-let SENTRY_SESSION_SAMPLE_RATE = import.meta.env.SENTRY_SESSION_SAMPLE_RATE ?? 1.0;
-let SENTRY_ON_ERROR_SAMPLE_RATE = import.meta.env.SENTRY_ON_ERROR_SAMPLE_RATE ?? 1.0;
+let SENTRY_URL = import.meta.env.VITE_SENTRY_URL;
+let SENTRY_TRACING_SAMPLE_RATE = import.meta.env.VITE_SENTRY_TRACING_SAMPLE_RATE ?? 1.0;
+let SENTRY_SESSION_SAMPLE_RATE = import.meta.env.VITE_SENTRY_SESSION_SAMPLE_RATE ?? 1.0;
+let SENTRY_ON_ERROR_SAMPLE_RATE = import.meta.env.VITE_SENTRY_ON_ERROR_SAMPLE_RATE ?? 1.0;
 
 let franceConnectUrl = import.meta.env.FRANCE_CONNECT_URL ?? "https://fcp.integ01.dev-franceconnect.fr/api/v1";
 
