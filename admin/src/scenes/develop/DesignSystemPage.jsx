75f47b40f7d6a0786e2e6660cb157ba6fbadf8c5
hlecourt <hlecourt99@gmail.com>
2023-10-26 11:35:16 +0200

    up

--- admin/src/scenes/develop/DesignSystemPage.jsx
+++ admin/src/scenes/develop/DesignSystemPage.jsx
@@ -1,9 +1,22 @@
 import React from "react";
-import { Page, Header, Subheader, Container } from "@snu/ds/admin";
+import { Page, Header, Subheader, Container, InputText } from "@snu/ds/admin";
 import { HiOutlineCommandLine } from "react-icons/hi2";
 import ButtonPrimary from "@/components/ui/buttons/ButtonPrimary";
 
 export default function DesignSystemPage() {
+  const [values, setValues] = React.useState({
+    input1: "",
+    input2: "",
+    input3: "",
+    input4: "",
+    input5: "",
+  });
+  const error = "Ceci est une erreur";
+
+  const handleChange = (event) => {
+    event.persist();
+    setValues((prev) => ({ ...prev, [event.target.name]: event.target.value }));
+  };
   return (
     <Page>
       <Header
@@ -17,6 +30,15 @@ export default function DesignSystemPage() {
       <Subheader title="Code, preview, test, build and ship." />
       <Container title="Hello...">
         <div className="text-ds-red">...World!</div>
+      </Container>
+      <Container title="Champs simples (InputText)">
+        <div className="grid grid-cols-2 gap-4 w-full">
+          <InputText placeholder="Input text normal" disabled={false} active={false} name="input1" value={values.input1} onChange={handleChange} readOnly={false} />
+          <InputText placeholder="Input text disabled" disabled={true} active={true} name="input2" value={values.input2} onChange={handleChange} readOnly={false} />
+          <InputText placeholder="Input text actif" disabled={false} active={true} name="input3" value={values.input3} onChange={handleChange} readOnly={false} />
+          <InputText placeholder="Input text readOnly" disabled={false} active={false} name="input4" value={values.input4} onChange={handleChange} readOnly={true} />
+          <InputText placeholder="Input text error" disabled={false} active={false} name="input5" value={values.input5} onChange={handleChange} readOnly={false} error={error} />
+        </div>
       </Container>
     </Page>
   );
---
+++ packages/ds/src/admin/form/InputText.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { HiOutlineExclamation } from "react-icons/hi";
+import { classNames } from "../utils";
+
+type OwnProps = {
+  name: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+  disabled?: boolean;
+  active?: boolean;
+  readOnly?: boolean;
+  error?: string;
+};
+
+export default function InputText({
+  name,
+  value,
+  onChange,
+  placeholder,
+  disabled,
+  active,
+  readOnly,
+  error,
+}: OwnProps) {
+  return (
+    <div className="flex flex-col gap-2">
+      <div
+        className={classNames(
+          readOnly || disabled
+            ? "cursor-none outline-none"
+            : "cursor-text focus-within:outline focus-within:outline-blue-500",
+          disabled ? "bg-gray-50" : "bg-white",
+          !readOnly && !disabled && active
+            ? "border-blue-500"
+            : "border-gray-300",
+          error && !readOnly && !disabled && "border-red-500",
+          "flex items-center justify-between h-[54px] w-full rounded-md border-[1px]  px-[13px] py-[9px] focus-within:outline-2 focus-within:outline-offset-2"
+        )}
+      >
+        <input
+          type="text"
+          name={name}
+          id={name}
+          className="flex flex-1 h-full py-2 font-normal leading-5 text-gray-900 text-sm placeholder:text-gray-500 disabled:text-gray-500 disabled:bg-gray-50 read-only:cursor-none"
+          placeholder={placeholder}
+          disabled={disabled}
+          value={value}
+          readOnly={readOnly}
+          onChange={(e) => onChange(e)}
+        />
+        {error && !readOnly && !disabled && (
+          <div className="flex items-center justify-center w-5 h-5">
+            <HiOutlineExclamation className="text-red-500 w-5 h-5" />
+          </div>
+        )}
+      </div>
+      {error && !readOnly && !disabled && (
+        <p className="text-sm leading-5 text-red-500">{error}</p>
+      )}
+    </div>
+  );
+}
