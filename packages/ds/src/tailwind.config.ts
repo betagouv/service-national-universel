19005b29a04dde68de48adbcc409cc4573372fb6
flawa <hugopozzoli@gmail.com>
2023-10-26 12:02:49 +0200

    feat(ds/admin): template & tailwind reload fix

--- packages/ds/src/tailwind.config.ts
+++ packages/ds/src/tailwind.config.ts
@@ -4,7 +4,13 @@ export default {
   theme: {
     extend: {
       colors: {
-        "ds-red": "#FFC107",
+        "ds-gray": {
+          400: "#9CA3AF",
+          900: "#111827",
+        },
+      },
+      boxShadow: {
+        container: "0px 0px 8px 0px rgba(0, 0, 0, 0.08)",
       },
     },
   },
