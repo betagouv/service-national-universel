57de0019b49d597d32921b60645756b8ce067397
Pierre LE BIHAN <lebihanp@hotmail.fr>
2023-10-27 13:05:20 +0300

    chore(lib): Force deploy

--- packages/lib/index.js
+++ packages/lib/index.js
@@ -1,7 +1,6 @@
 import { WITHRAWN_REASONS, YOUNG_STATUS, YOUNG_STATUS_PHASE1 } from "./constants";
 import translation from "./translation";
 import { ROLES } from "./roles";
-
 import sanitizeHtml from "sanitize-html";
 
 const isInRuralArea = (v) => {
