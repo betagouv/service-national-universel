58e2230f7ecbf5a02815547e9e9bef4df9c7a2db
Hugo Pozzoli <hugo.pozzoli.snu@gmail.com>
2023-10-31 16:03:41 +0100

    feat(ds/admin): classes empty layout (#3163)

---
+++ admin/src/scenes/classe/list.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { Page, Header, Container, Button } from "@snu/ds/admin";
+import { HiPlus } from "react-icons/hi";
+import ClasseIcon from "@/components/drawer/icons/Classe";
+
+export default function list() {
+  const [classes, setClasses] = useState(undefined);
+
+  return (
+    <Page>
+      <Header
+        title="Liste de mes classes"
+        breadcrumb={[{ title: <ClasseIcon className="scale-[65%]" /> }, { title: "Mes classes" }]}
+        actions={[
+          <Button key="empty" title={`(Voir template ${classes ? "vide" : "liste"})`} type="secondary" onClick={() => setClasses(classes ? undefined : [])} />,
+          <Button key="list" leftIcon={<ClasseIcon />} title="Créer une classe" className="ml-4" />,
+        ]}
+      />
+      {!classes && (
+        <Container className="!p-8">
+          <div className="py-6 bg-gray-50">
+            <div className="flex items-center justify-center h-[136px] mb-4 text-lg text-gray-500 text-center">Vous n’avez pas encore créé de classe engagée</div>
+            <div className="flex items-start justify-center h-[136px]">
+              <Button type="wired" leftIcon={<HiPlus />} title="Créer une première classe engagée" />
+            </div>
+          </div>
+        </Container>
+      )}
+    </Page>
+  );
+}
