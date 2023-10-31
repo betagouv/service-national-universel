9b5bb0b804b6d91d2b919cd62fa3327395e4ded4
Hugo Pozzoli <hugo.pozzoli.snu@gmail.com>
2023-10-31 16:02:02 +0100

    feat(ds/admin): modal component (#3157)

--- packages/ds/src/admin/ui/DropdownButton.tsx
+++ packages/ds/src/admin/ui/DropdownButton.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import { HiChevronDown } from "react-icons/hi";
 import Button from "./Button";
-import Badge from "../badge/Badge";
+import Badge from "./Badge";
 import { classNames } from "@/utils";
 
 type OptionGroupItem = {
