9d47538b018aadb05135680fe5a1b4bb43a25b6c
Hugo Pozzoli <hugo.pozzoli.snu@gmail.com>
2023-10-31 16:21:39 +0100

    feat(ds/admin): create class form, Label ds component & other fixs (#3164)

---
+++ admin/src/scenes/classe/create.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Page, Header, Container, Button, Label, InputText } from "@snu/ds/admin";
+import ClasseIcon from "@/components/drawer/icons/Classe";
+
+export default function create() {
+  const actions = [
+    <a key="cancel" href="/mes-classes" className="mr-2">
+      <Button title="Annuler" type="secondary" disabled />
+    </a>,
+    <Button key="create" leftIcon={<ClasseIcon />} title="Créer cette classe" disabled />,
+  ];
+
+  return (
+    <Page>
+      <Header
+        title="Création d’une classe engagée"
+        breadcrumb={[{ title: <ClasseIcon className="scale-[65%]" /> }, { title: "Mes classes", href: "/mes-classes" }, { title: "Créer une classe" }]}
+        actions={actions}
+      />
+      <Container title="Informations générales">
+        <div className="flex items-stretch justify-stretch">
+          <div className="flex-1">
+            <Label title="Cohorte" tooltip="This is a test and need to be replaced." />
+            <InputText value="CLE 23-24" disabled />
+          </div>
+          <div className="mx-14 w-[1px] bg-gray-200 shrink-0">&nbsp;</div>
+          <div className="flex-1">
+            <Label title="Numéro d’identification" tooltip="This is a test and need to be replaced." />
+            <div className="flex items-center justify-between gap-3">
+              <InputText className="flex-1" value="75IDF_11/10/2024_" disabled />
+              <InputText className="flex-1" placeholder="ABCDE" />
+            </div>
+          </div>
+        </div>
+      </Container>
+      <Container title="Référent de classe">
+        <div className="flex items-stretch justify-stretch">
+          <div className="flex-1">
+            <Label title="Nouveau référent de classe ..." />
+            <InputText className="mb-3" label="Nom" placeholder="Préciser" />
+            <InputText className="mb-3" label="Prénom" placeholder="Préciser" />
+            <InputText label="Adresse email" type="email" placeholder="Préciser" />
+          </div>
+          <div className="mx-14 w-[1px] bg-gray-200 shrink-0">&nbsp;</div>
+          <div className="flex-1">
+            <Label title="... ou référent de classe existant" />
+          </div>
+        </div>
+      </Container>
+      <div className="flex items-center justify-end">{actions}</div>
+    </Page>
+  );
+}
