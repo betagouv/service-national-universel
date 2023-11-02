c77a3a90aaf37af8acfc327f09a34b25c801bb9a
hlecourt <hlecourt99@gmail.com>
2023-11-02 12:20:44 +0100

    up

--- admin/src/scenes/phase0/view.jsx
+++ admin/src/scenes/phase0/view.jsx
@@ -312,15 +312,8 @@ export default function VolontairePhase0View({ young, onChange, globalMode }) {
 }
 
 function FooterPending({ young, requests, sending, onDeletePending, onSendPending, footerClass }) {
-  const [sentRequestsCount, setSentRequestsCount] = useState(0);
-  const [pendingRequestsCount, setPendingRequestsCount] = useState(0);
-
-  useEffect(() => {
-    const sent = requests.filter((r) => r.status === "SENT" || r.status === "REMINDED").length;
-    const pending = requests.filter((r) => r.status === "PENDING").length;
-    setSentRequestsCount(sent);
-    setPendingRequestsCount(pending);
-  }, [requests]);
+  const sentRequestsCount = requests.filter((r) => r.status === "SENT" || r.status === "REMINDED")?.length || 0;
+  const pendingRequestsCount = requests.filter((r) => r.status === "PENDING")?.length || 0;
 
   return (
     <div className={`fixed bottom-0 right-0 flex bg-white py-[20px] px-[42px] shadow-[0px_-16px_16px_-3px_rgba(0,0,0,0.05)] ${footerClass}`}>
