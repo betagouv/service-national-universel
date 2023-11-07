c2ad4250d028f93875133753ef661d8cdde6b60c
flawa <hugopozzoli@gmail.com>
2023-11-07 09:46:01 +0100

    feat(api): cron tester

---
+++ api/src/crons/__tester__.js
@@ -0,0 +1,12 @@
+// To test run:
+// node ./src/crons/__tester__.js --cron patch/young
+
+(async () => {
+  await require('../env-manager')()
+
+  switch (process.argv[3]) {
+    case "patch/young":
+      require('./patch/young').manualHandler("2023-08-17", "2023-08-18");
+      break;
+  }
+})();
