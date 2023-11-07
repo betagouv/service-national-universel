f4876f469a8ccce8e2c3deba873cd77860c7a454
hlecourt <hlecourt99@gmail.com>
2023-11-07 14:26:10 +0100

    up

--- admin/src/scenes/dashboardV2/components/inscription/General.jsx
+++ admin/src/scenes/dashboardV2/components/inscription/General.jsx
@@ -24,6 +24,7 @@ export default function General({ selectedFilters, setSelectedFilters }) {
 
   const [inscriptionDetailObject, setInscriptionDetailObject] = useState({});
   const [totalInscriptions, setTotalInscriptions] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const [filterArray, setFilterArray] = useState([]);
   const [departmentOptions, setDepartmentOptions] = useState([]);
@@ -36,6 +37,7 @@ export default function General({ selectedFilters, setSelectedFilters }) {
     //regex to get all cohort 2024
     const cohortsFilters = getCohortNameList(cohorts).filter((e) => e.match(/2024/));
     setSelectedFilters({ cohort: cohortsFilters });
+    setIsLoading(false);
   }, []);
 
   useEffect(() => {
@@ -108,6 +110,9 @@ export default function General({ selectedFilters, setSelectedFilters }) {
         .reduce((acc, current) => acc + (current.max && !isNaN(Number(current.max)) ? Number(current.max) : 0), 0),
     [inscriptionGoals, selectedFilters.cohort, selectedFilters.department, selectedFilters.region, selectedFilters.academy],
   );
+
+  if (isLoading) return null;
+
   return (
     <div className="flex flex-col gap-8 ">
       <FilterDashBoard selectedFilters={selectedFilters} setSelectedFilters={setSelectedFilters} filterArray={filterArray} />
