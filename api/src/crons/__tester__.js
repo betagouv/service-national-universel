5dc61250aedd16b98a5c656eb23490533bfda8f8
Pierre LE BIHAN <lebihanp@hotmail.fr>
2023-11-09 12:15:14 +0200

    fix(api): Fix __tester__

--- api/src/crons/__tester__.js
+++ api/src/crons/__tester__.js
@@ -8,13 +8,13 @@
 
   switch (process.argv[2]) {
     case "patch/young":
-      require("./patch/young").manualHandler("2023-11-08", "2023-11-09");
+      await require("./patch/young").manualHandler("2023-11-08", "2023-11-09");
       break;
     case "refresh-materialized-views":
-      require("./patch/refresh-materialized-views").handler();
+      await require("./patch/refresh-materialized-views").handler();
       break;
     case "patch/structure":
-      require("./patch/structure").manualHandler("2023-11-08", "2023-11-09");
+      await require("./patch/structure").manualHandler("2023-11-08", "2023-11-09");
       break;
   }
   process.exit(0);
