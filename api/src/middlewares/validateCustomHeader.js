be5e6547d698ee2c810b970b1e25751aefd74d69
hlecourt <hlecourt99@gmail.com>
2023-11-09 14:28:50 +0100

    up

--- api/src/middlewares/validateCustomHeader.js
+++ api/src/middlewares/validateCustomHeader.js
@@ -3,14 +3,15 @@ const Joi = require("joi");
 const validateCustomHeader = (req, res, next) => {
   // Define the schema for the header
   const schema = Joi.object({
-    "x-user-timezone": Joi.number().required().default(0),
+    "x-user-timezone": Joi.number().required(),
   });
 
   const { error } = schema.validate(req.headers, { allowUnknown: true });
   if (error) {
-    req.headers["x-user-timezone"] = 0;
+    if (error.details[0].context.key === "x-user-timezone") {
+      req.headers["x-user-timezone"] = 0;
+    }
   }
-  console.log("req.headers", req.headers);
   next();
 };
 module.exports = validateCustomHeader;
