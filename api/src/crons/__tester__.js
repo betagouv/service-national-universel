d89041acd480abf2441c5f028988868f8d8ae629
Michael Garcia <mika_garcia21@hotmail.com>
2023-11-09 11:09:40 +0100

    fix(api): update tester

--- api/src/crons/__tester__.js
+++ api/src/crons/__tester__.js
@@ -4,14 +4,17 @@
 // You need to run in local apps and target the right database (Prod usually)
 
 (async () => {
-  await require('../env-manager')()
+  await require("../env-manager")();
 
   switch (process.argv[2]) {
     case "patch/young":
-      require('./patch/young').manualHandler("2023-11-06", "2023-11-08");
+      require("./patch/young").manualHandler("2023-11-08", "2023-11-09");
       break;
     case "refresh-materialized-views":
-      require('./patch/refresh-materialized-views').handler();
+      require("./patch/refresh-materialized-views").handler();
+      break;
+    case "patch/structure":
+      require("./patch/structure").manualHandler("2023-11-08", "2023-11-09");
       break;
   }
   process.exit(0);
