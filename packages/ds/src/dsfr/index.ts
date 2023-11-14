01023aa1f4d1642f1720d1182cd59cc5cf33eb01
Marin <kalasasta@gmail.com>
2023-11-14 14:21:58 +0100

    feat(1394): Removed UI from DSFRContainer

--- packages/ds/src/dsfr/index.ts
+++ packages/ds/src/dsfr/index.ts
@@ -5,7 +5,8 @@ import InputPhone from "./form/InputPhone";
 
 // Layout
 import Container from "./layout/Container";
+import PaddedContainer from "./layout/PaddedContainer";
 import Page from "./layout/Page";
 import Section from "./layout/Section";
 
-export { InputPhone, Container, Page, Section };
+export { InputPhone, Container, PaddedContainer, Page, Section };
