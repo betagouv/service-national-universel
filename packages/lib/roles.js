1a3965ea52681ef2e212c409816c36f2e5e5ae5a
Hugo Pozzoli <hugo.pozzoli.snu@gmail.com>
2023-11-16 15:42:24 +0100

    feat(api): 1407 - put etablissement (#3266)

--- packages/lib/roles.js
+++ packages/lib/roles.js
@@ -829,6 +829,10 @@ function canWriteClasse(actor) {
   return actor.role === ROLES.ADMINISTRATEUR_CLE && actor.subRole === SUB_ROLES.referent_etablissement;
 }
 
+function canUpdateEtablissement(actor) {
+  return actor.role === ROLES.ADMINISTRATEUR_CLE && actor.subRole === SUB_ROLES.referent_etablissement;
+}
+
 export {
   ROLES,
   SUB_ROLES,
@@ -956,4 +960,5 @@ export {
   canUpdateMyself,
   canInviteCoordinateur,
   canWriteClasse,
+  canUpdateEtablissement,
 };
