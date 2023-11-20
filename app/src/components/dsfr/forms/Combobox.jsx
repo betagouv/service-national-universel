a1d0c6b4a5a6e31a51b4b65a3876900522d70a51
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2023-11-20 12:42:09 +0100

    up

--- app/src/components/dsfr/forms/Combobox.jsx
+++ app/src/components/dsfr/forms/Combobox.jsx
@@ -1,14 +1,10 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { RiSearchLine } from "react-icons/ri";
-import { toastr } from "react-redux-toastr";
 import ErrorMessage from "@/components/dsfr/forms/ErrorMessage";
-import { debounce } from "@/utils";
 
-export default function AsyncCombobox({ label, hint = "Aucun résultat.", getOptions, value, onChange, errorMessage }) {
+export default function Combobox({ label, hint = "Aucun résultat.", options, value, loading = false, onChangeQuery, onChange, errorMessage }) {
   const [open, setOpen] = useState(false);
-  const [query, setQuery] = useState(value);
-  const [options, setOptions] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [query, setQuery] = useState();
   const dropdownRef = useRef(null);
 
   useEffect(() => {
@@ -31,36 +27,16 @@ export default function AsyncCombobox({ label, hint = "Aucun résultat.", getOpt
     return () => document.removeEventListener("keydown", handleKeyDown);
   }, []);
 
-  const debouncedOptions = useCallback(
-    debounce(async (query) => {
-      try {
-        setLoading(true);
-        const options = await getOptions(query);
-        if (options?.length) {
-          setOptions(options);
-        }
-      } catch (e) {
-        toastr.error("Erreur", "Une erreur est survenue lors de la recherche", { timeOut: 10_000 });
-      } finally {
-        setLoading(false);
-      }
-    }, 300),
-    [],
-  );
-
   function handleFocus() {
     setQuery("");
     onChange(null);
     setOpen(true);
   }
 
-  const handleChangeQuery = async (e) => {
-    setOptions([]);
+  const handleChange = (e) => {
     const query = e.target.value;
     setQuery(query);
-    if (query.length > 1) {
-      debouncedOptions(query);
-    }
+    onChangeQuery(query);
   };
 
   const handleSelect = (option) => {
@@ -77,7 +53,7 @@ export default function AsyncCombobox({ label, hint = "Aucun résultat.", getOpt
           <input
             type="text"
             value={query || value?.label || ""}
-            onChange={(e) => handleChangeQuery(e)}
+            onChange={handleChange}
             onFocus={handleFocus}
             className="w-[100%] border-b-2 border-gray-800 bg-[#EEEEEE] rounded-tl rounded-tr px-3 py-2 pr-5"
           />
--- app/src/scenes/inscription2023/components/ShoolInFrance.jsx
+++ app/src/scenes/inscription2023/components/ShoolInFrance.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import AsyncCombobox from "@/components/dsfr/forms/AsyncCombobox";
+import React, { useCallback, useEffect, useState } from "react";
+import Combobox from "@/components/dsfr/forms/Combobox";
 import CreatableSelect from "../../../components/CreatableSelect";
 import Input from "./Input";
 import AddressForm from "@/components/dsfr/forms/AddressForm";
@@ -7,10 +7,14 @@ import GhostButton from "../../../components/dsfr/ui/buttons/GhostButton";
 import { FiChevronLeft } from "react-icons/fi";
 import { getAddressOptions } from "@/services/api-adresse";
 import { getCities, getSchools } from "../utils";
+import { debounce } from "@/utils";
+import { toastr } from "react-redux-toastr";
 
 export default function SchoolInFrance({ school, onSelectSchool, errors, corrections = null }) {
-  const [city, setCity] = useState(school?.city);
-  const [schools, setSchools] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [city, setCity] = useState(school ? { label: school.city + " - " + school.departmentName, value: [school.city, school.departmentName] } : null);
+  const [cityOptions, setCityOptions] = useState([]);
+  const [schoolOptions, setSchoolOptions] = useState([]);
   const [manualFilling, setManualFilling] = useState(school?.fullName && !school?.id);
   const [manualSchool, setManualSchool] = useState(school ?? {});
 
@@ -23,8 +27,49 @@ export default function SchoolInFrance({ school, onSelectSchool, errors, correct
     setCity(city);
     if (!city?.value.length === 2) return;
     const schools = await getSchools(city);
-    setSchools(schools);
+    setSchoolOptions(schools);
   }
+
+  const debouncedOptions = useCallback(
+    debounce(async (query) => {
+      try {
+        setLoading(true);
+        const options = await getCities(query);
+        if (options?.length) {
+          setCityOptions(options);
+        }
+      } catch (e) {
+        toastr.error("Erreur", "Une erreur est survenue lors de la recherche", { timeOut: 10_000 });
+      } finally {
+        setLoading(false);
+      }
+    }, 300),
+    [],
+  );
+
+  const handleChangeQuery = async (query) => {
+    setCityOptions([]);
+    if (query.length > 1) {
+      debouncedOptions(query);
+    }
+  };
+
+  useEffect(() => {
+    async function fetchSchools() {
+      try {
+        setLoading(true);
+        const schools = await getSchools(city);
+        setSchoolOptions(schools);
+      } catch (e) {
+        toastr.error("Erreur", "Une erreur est survenue lors de la recherche", { timeOut: 10_000 });
+      } finally {
+        setLoading(false);
+      }
+    }
+    if (city?.value.length === 2 && !loading && !schoolOptions.length) {
+      fetchSchools();
+    }
+  }, [city, loading, schoolOptions]);
 
   return manualFilling ? (
     <>
@@ -62,16 +107,25 @@ export default function SchoolInFrance({ school, onSelectSchool, errors, correct
     </>
   ) : (
     <>
-      <AsyncCombobox label="Rechercher une commune" hint="Aucune commune trouvée." getOptions={getCities} value={city} onChange={handleChangeCity} error={errors.city} />
+      <Combobox
+        label="Rechercher une commune"
+        value={city}
+        onChange={handleChangeCity}
+        options={cityOptions}
+        onChangeQuery={handleChangeQuery}
+        loading={loading}
+        hint="Aucune commune trouvée."
+        error={errors.city}
+      />
       <CreatableSelect
         label="Nom de l'établissement"
         value={school?.fullName && `${school.fullName} - ${school.adresse}`}
-        options={schools
+        options={schoolOptions
           .map((e) => `${e.fullName}${e.adresse ? ` - ${e.adresse}` : ""}`)
           .sort()
           .map((c) => ({ value: c, label: c }))}
         onChange={(value) => {
-          onSelectSchool(schools.find((e) => `${e.fullName}${e.adresse ? ` - ${e.adresse}` : ""}` === value));
+          onSelectSchool(schoolOptions.find((e) => `${e.fullName}${e.adresse ? ` - ${e.adresse}` : ""}` === value));
         }}
         placeholder="Sélectionnez un établissement"
         onCreateOption={(value) => {
