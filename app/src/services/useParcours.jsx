0b8fbdfce1bcbdbf17d298c14edf31ece5450750
Guillaume Pichon <73333478+GuiPich91@users.noreply.github.com>
2023-11-21 09:55:09 +0100

    feat(app): 1395 - New step Done for CLE parcours (#3268)

---
+++ app/src/services/useParcours.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import gfm from 'remark-gfm';
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation, useHistory } from "react-router-dom";
+import parcoursConfig from "../utils/parcoursConfig";
+import { YOUNG_SOURCE } from "snu-lib";
+import { setYoung } from "../redux/auth/actions";
+import { toastr } from "react-redux-toastr";
+
+export const useParcours = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const { pathname } = useLocation();
+  const young = useSelector((state) => state.Auth.young);
+
+  const replacePlaceholders = (text, placeholders) => {
+    return Object.entries(placeholders).reduce((acc, [key, value]) => {
+      return acc.replace(new RegExp(`{${key}}`, "g"), value);
+    }, text);
+  };
+
+  const getStepConfig = (source, stepName) => {
+    const sourceConfig = parcoursConfig[source] || parcoursConfig[YOUNG_SOURCE.VOLONTAIRE];
+    const stepConfig = sourceConfig[stepName];
+    if (!stepConfig) return null;
+
+    // Remplacer les placeholders dans les textes de la configuration de l'étape
+    const replacedStepConfig = { ...stepConfig };
+    Object.keys(replacedStepConfig).forEach((key) => {
+      if (typeof replacedStepConfig[key] === "string") {
+        const textWithPlaceholders = replacePlaceholders(replacedStepConfig[key], { firstName: young.firstName, email: young.email });
+
+        // Utiliser ReactMarkdown pour transformer le Markdown en éléments React
+        replacedStepConfig[key] = <ReactMarkdown remarkPlugins={[gfm]}>{textWithPlaceholders}</ReactMarkdown>;
+      }
+    });
+
+    return replacedStepConfig;
+  };
+
+  const stepPreinscriptionDoneConfig = getStepConfig(young.source, "stepPreinscriptionDone");
+
+  return {
+    stepPreinscriptionDoneConfig,
+  };
+};
+
+export default useParcours;
