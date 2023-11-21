c88fbd53dea5ac4d06d0f57943e018ee5d58bbec
Nicolas Bretécher <nicolas.bretecher@selego.co>
2023-11-21 08:46:55 +0100

    fix(app): 1382 - Amélio saisie établissement (retours PO) (#3277)

--- app/src/scenes/inscription2023/components/ShoolInFrance.jsx
+++ app/src/scenes/inscription2023/components/ShoolInFrance.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AsyncCombobox from "@/components/dsfr/forms/AsyncCombobox";
 import CreatableSelect from "../../../components/CreatableSelect";
 import Input from "./Input";
@@ -7,12 +7,26 @@ import GhostButton from "../../../components/dsfr/ui/buttons/GhostButton";
 import { FiChevronLeft } from "react-icons/fi";
 import { getAddressOptions } from "@/services/api-adresse";
 import { getCities, getSchools } from "../utils";
+import { toastr } from "react-redux-toastr";
 
 export default function SchoolInFrance({ school, onSelectSchool, errors, corrections = null }) {
-  const [city, setCity] = useState(school?.city);
-  const [schools, setSchools] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [city, setCity] = useState(school ? { label: school.city + " - " + school.departmentName, value: [school.city, school.departmentName] } : null);
+  const [schoolOptions, setSchoolOptions] = useState([]);
   const [manualFilling, setManualFilling] = useState(school?.fullName && !school?.id);
   const [manualSchool, setManualSchool] = useState(school ?? {});
+
+  async function fetchSchools(city) {
+    try {
+      setLoading(true);
+      const schools = await getSchools(city);
+      setSchoolOptions(schools);
+    } catch (e) {
+      toastr.error("Erreur", "Une erreur est survenue lors de la recherche", { timeOut: 10_000 });
+    } finally {
+      setLoading(false);
+    }
+  }
 
   async function handleChangeCity(city) {
     if (!city) {
@@ -22,9 +36,14 @@ export default function SchoolInFrance({ school, onSelectSchool, errors, correct
     onSelectSchool(undefined);
     setCity(city);
     if (!city?.value.length === 2) return;
-    const schools = await getSchools(city);
-    setSchools(schools);
+    await fetchSchools(city);
   }
+
+  useEffect(() => {
+    if (city?.value.length === 2 && !loading && !schoolOptions.length) {
+      fetchSchools(city);
+    }
+  }, [city, loading, schoolOptions]);
 
   return manualFilling ? (
     <>
@@ -66,12 +85,12 @@ export default function SchoolInFrance({ school, onSelectSchool, errors, correct
       <CreatableSelect
         label="Nom de l'établissement"
         value={school?.fullName && `${school.fullName} - ${school.adresse}`}
-        options={schools
+        options={schoolOptions
           .map((e) => `${e.fullName}${e.adresse ? ` - ${e.adresse}` : ""}`)
           .sort()
           .map((c) => ({ value: c, label: c }))}
         onChange={(value) => {
-          onSelectSchool(schools.find((e) => `${e.fullName}${e.adresse ? ` - ${e.adresse}` : ""}` === value));
+          onSelectSchool(schoolOptions.find((e) => `${e.fullName}${e.adresse ? ` - ${e.adresse}` : ""}` === value));
         }}
         placeholder="Sélectionnez un établissement"
         onCreateOption={(value) => {
