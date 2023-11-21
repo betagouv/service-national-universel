e7b495bf8c42b913fe257481b700f8ac80f1b96a
Tangi Mendès <tangimds@gmail.com>
2023-11-21 15:25:54 +0100

    clean

--- admin/src/services/api-adresse.js
+++ admin/src/services/api-adresse.js
@@ -1,5 +1,4 @@
 import { capture } from "../sentry";
-import { departmentLookUp } from "snu-lib/region-and-departments";
 
 // https://adresse.data.gouv.fr/api-doc/adresse
 // Filtres possibles : postcode, citycode (INSEE), type, limit, autocomplete
@@ -66,55 +65,4 @@ const putLocation = async (city, zip) => {
   }
 };
 
-function formatOption(option) {
-  const { department, region } = getDepartmentAndRegionFromContext(option.properties.context);
-  return {
-    addressVerified: "true",
-    address: option.properties.type !== "municipality" ? option.properties.name : "",
-    coordinatesAccuracyLevel: option.properties.type,
-    zip: option.properties.postcode,
-    city: option.properties.city,
-    cityCode: option.properties.citycode,
-    department,
-    region,
-    location: { lat: option.geometry.coordinates[1], lon: option.geometry.coordinates[0] },
-  };
-}
-
-function getDepartmentAndRegionFromContext(context) {
-  // Context has the department number as first element, department name as an optional second element and region as the last element.
-  // Examples:
-  // ['50', ' Manche', ' Normandie']
-  // ['988', ' Nouvelle-Calédonie']
-  const arr = context.split(",");
-  const departmentNumber = arr[0].trim();
-  const department = departmentLookUp[departmentNumber];
-  const region = arr[arr.length - 1].trim();
-  return { department, region };
-}
-
-async function getAddressOptions(query, signal) {
-  // Call BAN API
-  const res = await apiAdress(query, { limit: 10 }, { signal });
-  if (!res) return [[], null];
-  if (res.error) return [null, res.error];
-  if (!res.features?.length) return [[], null];
-
-  // Format and group options
-  const formattedOptions = res.features.map((option) => formatOption(option));
-
-  const housenumbers = formattedOptions.filter((option) => option.coordinatesAccuracyLevel === "housenumber");
-  const streets = formattedOptions.filter((option) => option.coordinatesAccuracyLevel === "street");
-  const localities = formattedOptions.filter((option) => option.coordinatesAccuracyLevel === "locality");
-  const municipalities = formattedOptions.filter((option) => option.coordinatesAccuracyLevel === "municipality");
-
-  const options = [];
-  if (housenumbers.length > 0) options.push({ label: "Numéro", options: housenumbers });
-  if (streets.length > 0) options.push({ label: "Voie", options: streets });
-  if (localities.length > 0) options.push({ label: "Lieu-dit", options: localities });
-  if (municipalities.length > 0) options.push({ label: "Commune", options: municipalities });
-
-  return [options, null];
-}
-
-export { apiAdress, putLocation, getAddressOptions };
+export { apiAdress, putLocation };
