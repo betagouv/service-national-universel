e665ca9b3f291937e7339a5e3ddee953458680a9
Tangi Mendès <tangimds@gmail.com>
2023-11-23 10:33:24 +0100

    form in email

--- admin/src/scenes/signup/email.jsx
+++ admin/src/scenes/signup/email.jsx
@@ -21,7 +21,8 @@ export default function email({ user }) {
   const urlParams = new URLSearchParams(window.location.search);
   const invitationToken = urlParams.get("token");
 
-  const submit = async () => {
+  const submit = async (e) => {
+    e.preventDefault();
     try {
       const { ok, data, code, message } = await api.put(`/cle/referent-signup/request-confirmation-email`, { email, confirmEmail, invitationToken });
       if (!ok) return toastr.error(message || translate(code));
@@ -37,51 +38,55 @@ export default function email({ user }) {
       <div className="m-auto max-w-[587px]">
         <Stepper currentStep={2} stepCount={5} title="Création d’un compte : adresse email" nextTitle="Code d'activation" />
       </div>
-      <Container className="flex flex-col gap-8">
-        <div className="flex items-start justify-between">
-          <h1 className="text-xl font-bold">Renseignez l'adresse email de votre etablissement</h1>
-          <i className={fr.cx("fr-icon-question-fill", "text-[var(--background-action-high-blue-france)]")}></i>
-        </div>
-        <hr className="p-1" />
-        <div className="flex gap-6">
-          <div className="w-full">
-            <Input
-              label="Adresse email"
-              state="default"
-              stateRelatedMessage="Email invalide"
-              nativeInputProps={{
-                placeholder: "exemple@mail.com",
-                type: "email",
-                value: email,
-                onChange: (e) => setEmail(e.target.value),
-              }}
-            />
+      <form onSubmit={submit}>
+        <Container className="flex flex-col gap-8">
+          <div className="flex items-start justify-between">
+            <h1 className="text-xl font-bold">Renseignez l'adresse email de votre etablissement</h1>
+            <i className={fr.cx("fr-icon-question-fill", "text-[var(--background-action-high-blue-france)]")}></i>
           </div>
-          <div className="w-full">
-            <Input
-              label="Confirmez votre adresse email"
-              state="default"
-              stateRelatedMessage="Email invalide"
-              nativeInputProps={{
-                placeholder: "exemple@mail.com",
-                type: "email",
-                value: confirmEmail,
-                onChange: (e) => setConfirmEmail(e.target.value),
-              }}
-            />
+          <hr className="p-1" />
+          <div className="flex gap-6">
+            <div className="w-full">
+              <Input
+                label="Adresse email"
+                state="default"
+                stateRelatedMessage="Email invalide"
+                nativeInputProps={{
+                  placeholder: "exemple@mail.com",
+                  type: "email",
+                  value: email,
+                  onChange: (e) => setEmail(e.target.value),
+                  required: true,
+                }}
+              />
+            </div>
+            <div className="w-full">
+              <Input
+                label="Confirmez votre adresse email"
+                state="default"
+                stateRelatedMessage="Email invalide"
+                nativeInputProps={{
+                  placeholder: "exemple@mail.com",
+                  type: "email",
+                  value: confirmEmail,
+                  onChange: (e) => setConfirmEmail(e.target.value),
+                  required: true,
+                }}
+              />
+            </div>
           </div>
-        </div>
-        <div>
-          <Alert description="Il doit s'agir d'une adresse email académique." severity="info" small />
-        </div>
-        <div>
-          <Alert description="Nous allons vous envoyer un code pour activer votre adresse email renseignée ci-dessus." severity="info" small />
-        </div>
-        <hr className="p-1" />
-        <div className="flex justify-end">
-          <Button onClick={submit}>Continuer</Button>
-        </div>
-      </Container>
+          <div>
+            <Alert description="Il doit s'agir d'une adresse email académique." severity="info" small />
+          </div>
+          <div>
+            <Alert description="Nous allons vous envoyer un code pour activer votre adresse email renseignée ci-dessus." severity="info" small />
+          </div>
+          <hr className="p-1" />
+          <div className="flex justify-end">
+            <Button type="submit">Continuer</Button>
+          </div>
+        </Container>
+      </form>
     </Section>
   );
 }
