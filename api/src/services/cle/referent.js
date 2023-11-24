f045490ec2112eb0dd68bed3237bbe6155190e4d
C2Chandelier <102954303+C2Chandelier@users.noreply.github.com>
2023-11-24 16:55:16 +0100

    feat(admin/api): 1502 - ajout du back pour les établissement CLE (#3281)

--- api/src/services/cle/referent.js
+++ api/src/services/cle/referent.js
@@ -1,44 +1,51 @@
 const crypto = require("crypto");
 const { SENDINBLUE_TEMPLATES } = require("snu-lib");
-
+const { ERRORS } = require("../../utils");
 const { sendTemplate } = require("../../sendinblue");
 const config = require("../../config");
 const ReferentModel = require("../../models/referent");
 const { inSevenDays } = require("../../utils");
+const { capture } = require("../../sentry");
 
-const findOrCreateReferent = async (referent, { etablissement, role, subRole, session }) => {
-  // Return if already exists
-  if (referent._id) return referent;
+const findOrCreateReferent = async (referent, { etablissement, role, subRole }) => {
+  try {
+    // Return if already exists
+    if (referent._id) return referent;
 
-  // Create referent
-  if (!referent.email || !referent.firstName || !referent.lastName) throw new Error("Missing referent email or firstName or lastName");
-  const invitationToken = crypto.randomBytes(20).toString("hex");
-  referent = await ReferentModel.create({
-    ...referent,
-    role,
-    subRole,
-    invitationToken,
-    invitationExpires: inSevenDays(),
-    department: etablissement.department,
-    region: etablissement.region,
-  }, { session });
+    // Create referent
+    if (!referent.email || !referent.firstName || !referent.lastName) throw new Error("Missing referent email or firstName or lastName");
+    const invitationToken = crypto.randomBytes(20).toString("hex");
+    referent = await ReferentModel.create({
+      ...referent,
+      role,
+      subRole,
+      invitationToken,
+      invitationExpires: inSevenDays(),
+      department: etablissement.department,
+      region: etablissement.region,
+    });
 
-  return referent;
-}
+    return referent;
+  } catch (e) {
+    if (e.code === 11000) return ERRORS.USER_ALREADY_REGISTERED;
+    capture(e);
+  }
+};
 
-const inviteReferent = async (referent, { role, user }) => {
+const inviteReferent = async (referent, { role, user }, etablissement) => {
   // Send invite
   const cta = `${config.ADMIN_URL}/auth/signup/invite?token=${referent.invitationToken}`;
   const fromName = `${user.firstName} ${user.lastName}`;
   const toName = `${referent.firstName} ${referent.lastName}`;
+  const name_school = `${etablissement.name}`;
 
   await sendTemplate(SENDINBLUE_TEMPLATES.invitationReferent[role], {
     emailTo: [{ name: `${referent.firstName} ${referent.lastName}`, email: referent.email }],
-    params: { cta, fromName, toName },
+    params: { cta, fromName, toName, name_school },
   });
-}
+};
 
 module.exports = {
   findOrCreateReferent,
   inviteReferent,
-}
+};
