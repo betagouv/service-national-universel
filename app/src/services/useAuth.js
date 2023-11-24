1deddda7ba0d5a377b22a59df5ca3586de98b04d
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2023-11-24 17:54:24 +0100

    Co-authored-by: Guillaume Pichon <GuiPich91@users.noreply.github.com>

--- app/src/services/useAuth.js
+++ app/src/services/useAuth.js
@@ -3,6 +3,7 @@ import { useLocation, useHistory } from "react-router-dom";
 import { setYoung } from "../redux/auth/actions";
 import { toastr } from "react-redux-toastr";
 import { logoutYoung } from "./young.service";
+import { YOUNG_SOURCE } from "snu-lib";
 
 export const useAuth = () => {
   const dispatch = useDispatch();
@@ -25,12 +26,17 @@ export const useAuth = () => {
 
   const loginOrLogout = () => (isLoggedIn ? logout() : login());
 
+  const isCLEFromURL = new URLSearchParams(window.location.search).get("parcours")?.toUpperCase() === YOUNG_SOURCE.CLE;
+  const isCLEFromDB = young?.source === YOUNG_SOURCE.CLE;
+  const isCLE = isCLEFromURL || isCLEFromDB;
+
   return {
     young,
     logout,
     login,
     isLoggedIn,
     loginOrLogout,
+    isCLE,
   };
 };
 
