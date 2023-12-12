05933b8d04fbda1ec874f0e151c3000831f0b9da
pitiscarf <dev@rthebaud.fr>
2023-12-12 17:39:46 +0100

    Add ci terraform stack & github action

---
+++ admin/docker-env.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+for i in $(env | grep DOCKER_ENV_)
+do
+    key=$(echo $i | cut -d '=' -f 1)
+    value=$(echo $i | cut -d '=' -f 2-)
+    echo $key=$value
+    # sed All files
+    find /service-national-universel/admin/build -type f -exec sed -i "s|${key}|${value}|g" '{}' +
+
+    # sed JS and CSS only
+    # find /service-national-universel/admin/build -type f \( -name '*.js' -o -name '*.css' \) -exec sed -i "s|${key}|${value}|g" '{}' +
+done
---
+++ terraform/environments/ci/variables.tf
@@ -0,0 +1,20 @@
+variable "environments" {
+  type = map(object({
+    domain  = string
+    dns_zone  = string
+    secret_revision  = number
+    image_tag = string
+    apps = map(object({
+      name                  = string
+      subdomain             = string
+      cpu_limit             = optional(number, 256)
+      memory_limit          = optional(number, 256)
+      min_scale             = optional(number, 1)
+      max_scale             = optional(number, 1)
+      timeout               = optional(number, 60)
+      max_concurrency       = optional(number, 50)
+      environment_variables = optional(map(string), {})
+    }))
+  }))
+  nullable = false
+}
