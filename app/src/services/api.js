72e2f901c1924402730f35284c1b5a652fdfa241
Pierre LE BIHAN <lebihanp@hotmail.fr>
2023-12-13 16:09:53 +0000

    test

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -2,6 +2,7 @@ import fetchRetry from "fetch-retry";
 import { apiURL } from "../config";
 import { createFormDataForFileUpload } from "snu-lib";
 import { capture } from "../sentry";
+
 import { ERRORS } from "snu-lib/errors";
 
 let fetch = window.fetch;
