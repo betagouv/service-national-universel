1fdd9803ff1b03ca78945d2d06fd5bd35692c1ba
pitiscarf <dev@rthebaud.fr>
2023-12-16 17:55:46 +0100

    add project_id & secret_id fot CI

--- terraform/environments/ci/imports.tf
+++ terraform/environments/ci/imports.tf
@@ -1,8 +1,8 @@
 import {
   to = scaleway_account_project.main
-  id = "<project_id>"
+  id = "1b29c5d9-9723-400a-aa8b-0c85ae3567f7"
 }
 import {
   to = scaleway_secret.ci
-  id = "<secret_id>"
+  id = "fr-par/44d2c577-fd0d-4f32-b104-210bf39da6cd"
 }
