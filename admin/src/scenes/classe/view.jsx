b60d0d2c6029c0268287dfbf2e999780d69e2ae5
Tangi Mendès <tangimds@gmail.com>
2023-12-20 11:09:28 +0100

    display dates

--- admin/src/scenes/classe/view.jsx
+++ admin/src/scenes/classe/view.jsx
@@ -28,6 +28,7 @@ import { MdContentCopy } from "react-icons/md";
 import Loader from "@/components/Loader";
 import { IoWarningOutline } from "react-icons/io5";
 import { MdOutlineDangerous } from "react-icons/md";
+import dayjs from "dayjs";
 
 export default function view() {
   const [classe, setClasse] = useState({});
@@ -207,12 +208,23 @@ export default function view() {
               placeholder={"Choisissez une cohorte"}
               options={cohorts?.map((c) => ({ value: c.name, label: c.name }))}
               closeMenuOnSelect={true}
-              value={classe?.cohort ? { value: classe?.cohort, label: translate(classe?.cohort) } : null}
+              value={classe?.cohort ? { value: classe?.cohort, label: classe?.cohort } : null}
               onChange={(options) => {
                 setClasse({ ...classe, cohort: options.value });
               }}
               error={errors.cohort}
             />
+            <div className="flex flex-col gap-2 rounded-lg bg-gray-100 px-3 py-2 mb-3">
+              <p className="text-left text-sm  text-gray-800">Dates</p>
+              <div className="flex items-center">
+                <p className="text-left text-xs text-gray-500 flex-1">
+                  Début : <strong>{classe?.cohort ? dayjs(cohorts.find((c) => c.name === classe?.cohort)?.dateStart).format("DD/MM/YYYY") : ""}</strong>
+                </p>
+                <p className="text-left text-xs text-gray-500 flex-1">
+                  Fin : <strong>{classe?.cohort ? dayjs(cohorts.find((c) => c.name === classe?.cohort)?.dateEnd).format("DD/MM/YYYY") : ""}</strong>
+                </p>
+              </div>
+            </div>
             <Label title="Numéro d’identification" />
             <div className="flex items-center justify-between gap-3 mb-3">
               <InputText className="flex-1" value={classe.uniqueKey} disabled />
