2681784bcce7c68e437814909b87737fe6808234
michaelgarcia75 <99188042+michaelgarcia75@users.noreply.github.com>
2023-12-20 11:48:41 +0100

    feat(analytics): 1601 - création de log_etablissements (#3400)

---
+++ analytics/src/models/log-etablissements.model.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require("sequelize");
+const { db } = require("../services/databases/postgresql.service");
+
+const OBJ = db.define(
+  "log_etablissements",
+  {
+    evenement_valeur: DataTypes.TEXT,
+    evenement_type: DataTypes.TEXT,
+    etablissement_id: DataTypes.TEXT,
+    etablissement_name: DataTypes.TEXT,
+    etablissement_departement: DataTypes.TEXT,
+    etablissement_region: DataTypes.TEXT,
+    date: DataTypes.DATE,
+    raw_data: DataTypes.JSONB,
+  },
+  {
+    defaultScope: {
+      attributes: { exclude: ["createdAt", "updatedAt", "raw_data"] },
+    },
+  },
+);
+
+module.exports = OBJ;
