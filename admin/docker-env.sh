5faf8ea70bed9fd4cf0fa5006fb4459ceb69276f
pitiscarf <dev@rthebaud.fr>
2023-12-21 16:09:15 +0100

    call process explicitly (without turbo)

--- admin/docker-env.sh
+++ admin/docker-env.sh
@@ -5,7 +5,12 @@
 # Link: https://import-meta-env.org/guide/getting-started/introduction.html#guide
 
 # Create the sed command to substituate in one pass every environment variables prefixed by DOCKER_ENV_
-command=$(env | grep DOCKER_ENV_ | sed 's/^\(.*\)=\(.*\)$/-e ###s|\1|\2|g###/g' | xargs echo sed -i | sed 's/###/\"/g')
-echo $command
-# Apply the substitution only on the index-*.js file
-eval $command $(find /service-national-universel/admin/build/assets -name "index-*.js" -type f | head -n 1)
+
+file=$(find /service-national-universel/admin/build/assets -name "index-*.js" -type f | head -n 1)
+for i in $(env | grep DOCKER_ENV_)
+do
+    key=$(echo $i | cut -d '=' -f 1)
+    value=$(echo $i | cut -d '=' -f 2-)
+    echo $key=$value
+    sed -i "s|${key}|${value}|g" $file
+done
