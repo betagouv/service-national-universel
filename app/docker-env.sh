11ca80deef43418386767f9d542ea9962033cf04
pitiscarf <dev@rthebaud.fr>
2023-12-22 11:44:29 +0100

    activate 1 pass substitution

--- app/docker-env.sh
+++ app/docker-env.sh
@@ -4,11 +4,9 @@
 # Use import-meta-env to substitute at runtime
 # Link: https://import-meta-env.org/guide/getting-started/introduction.html#guide
 
-file=$(find /service-national-universel/app/build/assets -name "index-*.js" -type f | head -n 1)
-for i in $(env | grep DOCKER_ENV_)
-do
-    key=$(echo $i | cut -d '=' -f 1)
-    value=$(echo $i | cut -d '=' -f 2-)
-    echo $key=$value
-    sed -i "s|${key}|${value}|g" $file
-done
+# Use \x07 character as delimiter
+delim=$(echo -ne "\x07")
+# Create the sed command to substituate in one pass every environment variables prefixed by DOCKER_ENV_
+command=$(env | grep DOCKER_ENV_ | sed "s/^\(.*\)=\(.*\)$/-e ###s$delim\1$delim\2${delim}g###/g" | xargs echo sed -i.bak | sed 's/###/\"/g')
+# Apply the substitution only on the index-*.js file
+eval $command $(find /service-national-universel/app/build/assets -name "index-*.js" -type f | head -n 1)
