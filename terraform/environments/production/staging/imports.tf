cfb9daca7a16ae9b2747384163d722bef74fa102
rthebaud <dev@rthebaud.fr>
2023-12-22 11:31:55 +0100

    Terraform - separate staging & production state (#3421)
    * split production & staging state
    
    * DO not use remote_state to avoid locking
    
    * format tf files
    
    * Update README
    
    * remove unused tf outputs

---
+++ terraform/environments/production/staging/imports.tf
@@ -0,0 +1,5 @@
+# Secrets
+import {
+  to = scaleway_secret.staging
+  id = "fr-par/f050e65a-b996-402d-ad16-389b52172927"
+}
--- terraform/environments/production/staging/main.tf
+++ terraform/environments/production/staging/main.tf
@@ -6,7 +6,9 @@ terraform {
   }
   required_version = ">= 0.13"
 
-  backend "pg" {}
+  backend "pg" {
+    schema_name = "apps_staging"
+  }
 }
 
 provider "scaleway" {
@@ -20,91 +22,47 @@ variable "image_tag" {
 }
 
 locals {
-  organization_id = "db949d19-5fe0-4def-a89b-f801aad2d050" # Selego
-  env             = "ci"
-  domain          = "beta-snu.dev"
-  api_hostname    = "api.${local.env}.${local.domain}"
-  admin_hostname  = "admin.${local.env}.${local.domain}"
-  app_hostname    = "moncompte.${local.env}.${local.domain}"
-  secrets         = jsondecode(base64decode(data.scaleway_secret_version.main.data))
+  project_id     = "a0c93450-f68c-4768-8fe8-6e07a1644530"
+  domain         = "beta-snu.dev"
+  api_hostname   = "api.${local.domain}"
+  admin_hostname = "admin.${local.domain}"
+  app_hostname   = "moncompte.${local.domain}"
+  secrets        = jsondecode(base64decode(data.scaleway_secret_version.staging.data))
 }
 
 # Project
-resource "scaleway_account_project" "main" {
-  name        = "snu-${local.env}"
-  description = "SNU project for '${local.env}'"
-}
-
-# Secrets
-resource "scaleway_secret" "ci" {
-  name        = "snu-${local.env}"
-  project_id  = scaleway_account_project.main.id
-  description = "Secrets for environment '${local.env}'"
-}
-data "scaleway_secret_version" "main" {
-  secret_id = scaleway_secret.ci.id
-  revision  = "latest_enabled"
-}
-
-# Logs
-resource "scaleway_cockpit" "main" {
-  project_id = scaleway_account_project.main.id
+data "scaleway_account_project" "main" {
+  project_id = local.project_id
 }
 
 # Registry
-resource "scaleway_registry_namespace" "main" {
-  project_id  = scaleway_account_project.main.id
-  name        = "snu-${local.env}"
-  description = "SNU registry for ${local.env}"
-  is_public   = false
+data "scaleway_registry_namespace" "main" {
+  name = "snu-production"
 }
 
-# Application user
-resource "scaleway_iam_application" "main" {
-  name        = "snu-deploy-${local.env}"
-  description = "Application allowed to deploy apps in '${local.env}'"
+# Secrets
+resource "scaleway_secret" "staging" {
+  name        = "snu-staging"
+  project_id  = data.scaleway_account_project.main.id
+  description = "Secrets for environment 'staging'"
 }
-
-# Deploy policy
-resource "scaleway_iam_policy" "deploy" {
-  name           = "snu-deploy-${local.env}-policy"
-  description    = "Allow to deploy apps in '${local.env}'"
-  application_id = scaleway_iam_application.main.id
-  rule {
-    project_ids = [scaleway_account_project.main.id]
-    permission_set_names = [
-      "AllProductsFullAccess",
-    ]
-  }
-  rule {
-    organization_id = local.organization_id
-    permission_set_names = [
-      "ProjectReadOnly",
-      "IAMReadOnly"
-    ]
-  }
+data "scaleway_secret_version" "staging" {
+  secret_id = scaleway_secret.staging.id
+  revision  = "latest_enabled"
 }
 
 # Containers namespace
-resource "scaleway_container_namespace" "main" {
-  project_id  = scaleway_account_project.main.id
-  name        = "snu-${local.env}"
-  description = "SNU container namespace for environment '${local.env}'"
+resource "scaleway_container_namespace" "staging" {
+  project_id  = data.scaleway_account_project.main.id
+  name        = "snu-staging"
+  description = "SNU container namespace for environment 'staging'"
 }
 
 # Containers
-
-# DNS zone
-resource "scaleway_domain_zone" "main" {
-  project_id = scaleway_account_project.main.id
-  domain     = local.domain
-  subdomain  = local.env
-}
-
 resource "scaleway_container" "api" {
-  name            = "${local.env}-api"
-  namespace_id    = scaleway_container_namespace.main.id
-  registry_image  = "${scaleway_registry_namespace.main.endpoint}/api:${var.image_tag}"
+  name            = "staging-api"
+  namespace_id    = scaleway_container_namespace.staging.id
+  registry_image  = "${data.scaleway_registry_namespace.main.endpoint}/api:${var.image_tag}"
   port            = 8080
   cpu_limit       = 768
   memory_limit    = 1024
@@ -167,14 +125,6 @@ resource "scaleway_container" "api" {
   }
 }
 
-resource "scaleway_domain_record" "api" {
-  dns_zone = scaleway_domain_zone.main.id
-  name     = "api"
-  type     = "CNAME"
-  data     = "${scaleway_container.api.domain_name}."
-  ttl      = 3600
-}
-
 resource "scaleway_container_domain" "api" {
   container_id = scaleway_container.api.id
   hostname     = local.api_hostname
@@ -183,9 +133,9 @@ resource "scaleway_container_domain" "api" {
 
 
 resource "scaleway_container" "admin" {
-  name            = "${local.env}-admin"
-  namespace_id    = scaleway_container_namespace.main.id
-  registry_image  = "${scaleway_registry_namespace.main.endpoint}/admin:${var.image_tag}"
+  name            = "staging-admin"
+  namespace_id    = scaleway_container_namespace.staging.id
+  registry_image  = "${data.scaleway_registry_namespace.main.endpoint}/admin:${var.image_tag}"
   port            = 8080
   cpu_limit       = 256
   memory_limit    = 256
@@ -198,14 +148,14 @@ resource "scaleway_container" "admin" {
   deploy          = true
 
   environment_variables = {
-    "APP_NAME"                  = "admin"
-    "CLE"                       = "true"
-    "STAGING"                   = "true"
-    "DOCKER_ENV_VITE_ADMIN_URL" = "https://${local.admin_hostname}"
-    "DOCKER_ENV_VITE_API_URL"   = "https://${local.api_hostname}"
-    "DOCKER_ENV_VITE_APP_URL"   = "https://${local.app_hostname}"
-    "DOCKER_ENV_VITE_SENTRY_SESSION_SAMPLE_RATE" : "0.1"
-    "DOCKER_ENV_VITE_SUPPORT_URL" : "https://support.beta-snu.dev"
+    "APP_NAME"                                   = "admin"
+    "CLE"                                        = "true"
+    "STAGING"                                    = "true"
+    "DOCKER_ENV_VITE_ADMIN_URL"                  = "https://${local.admin_hostname}"
+    "DOCKER_ENV_VITE_API_URL"                    = "https://${local.api_hostname}"
+    "DOCKER_ENV_VITE_APP_URL"                    = "https://${local.app_hostname}"
+    "DOCKER_ENV_VITE_SENTRY_SESSION_SAMPLE_RATE" = "0.1"
+    "DOCKER_ENV_VITE_SUPPORT_URL"                = "https://support.beta-snu.dev"
   }
 
   secret_environment_variables = {
@@ -215,23 +165,15 @@ resource "scaleway_container" "admin" {
   }
 }
 
-resource "scaleway_domain_record" "admin" {
-  dns_zone = scaleway_domain_zone.main.id
-  name     = "admin"
-  type     = "CNAME"
-  data     = "${scaleway_container.admin.domain_name}."
-  ttl      = 3600
-}
-
 resource "scaleway_container_domain" "admin" {
   container_id = scaleway_container.admin.id
   hostname     = local.admin_hostname
 }
 
 resource "scaleway_container" "app" {
-  name            = "${local.env}-app"
-  namespace_id    = scaleway_container_namespace.main.id
-  registry_image  = "${scaleway_registry_namespace.main.endpoint}/app:${var.image_tag}"
+  name            = "staging-app"
+  namespace_id    = scaleway_container_namespace.staging.id
+  registry_image  = "${data.scaleway_registry_namespace.main.endpoint}/app:${var.image_tag}"
   port            = 8080
   cpu_limit       = 256
   memory_limit    = 256
@@ -244,15 +186,15 @@ resource "scaleway_container" "app" {
   deploy          = true
 
   environment_variables = {
-    "APP_NAME"                  = "app"
-    "CLE"                       = "true"
-    "STAGING"                   = "true"
-    "DOCKER_ENV_VITE_ADMIN_URL" = "https://${local.admin_hostname}"
-    "DOCKER_ENV_VITE_API_URL"   = "https://${local.api_hostname}"
-    "DOCKER_ENV_VITE_APP_URL"   = "https://${local.app_hostname}"
-    "DOCKER_ENV_VITE_SENTRY_SESSION_SAMPLE_RATE" : "0.1"
-    "DOCKER_ENV_VITE_SUPPORT_URL" : "https://support.beta-snu.dev"
-    "FOLDER_APP" = "app"
+    "APP_NAME"                                   = "app"
+    "CLE"                                        = "true"
+    "STAGING"                                    = "true"
+    "DOCKER_ENV_VITE_ADMIN_URL"                  = "https://${local.admin_hostname}"
+    "DOCKER_ENV_VITE_API_URL"                    = "https://${local.api_hostname}"
+    "DOCKER_ENV_VITE_APP_URL"                    = "https://${local.app_hostname}"
+    "DOCKER_ENV_VITE_SENTRY_SESSION_SAMPLE_RATE" = "0.1"
+    "DOCKER_ENV_VITE_SUPPORT_URL"                = "https://support.beta-snu.dev"
+    "FOLDER_APP"                                 = "app"
   }
 
   secret_environment_variables = {
@@ -261,29 +203,11 @@ resource "scaleway_container" "app" {
   }
 }
 
-resource "scaleway_domain_record" "app" {
-  dns_zone = scaleway_domain_zone.main.id
-  name     = "moncompte"
-  type     = "CNAME"
-  data     = "${scaleway_container.app.domain_name}."
-  ttl      = 3600
-}
-
 resource "scaleway_container_domain" "app" {
   container_id = scaleway_container.app.id
   hostname     = local.app_hostname
 }
 
-
-output "project_id" {
-  value = scaleway_account_project.main.id
-}
-output "secret_id" {
-  value = scaleway_secret.ci.id
-}
-output "registry_endpoint" {
-  value = scaleway_registry_namespace.main.endpoint
-}
 output "api_endpoint" {
   value = "https://${local.api_hostname}"
 }
