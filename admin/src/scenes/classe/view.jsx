ae9b6c34d2ccc0278da48f59c337b60281229742
Nicolas Bretécher <nicolas.bretecher@selego.co>
2024-01-02 11:20:44 +0100

    refactor(admin): Use cohorts data from redux instead of fetching API (#3430)

--- admin/src/scenes/classe/view.jsx
+++ admin/src/scenes/classe/view.jsx
@@ -33,7 +33,7 @@ import { MdOutlineDangerous } from "react-icons/md";
 import plausibleEvent from "@/services/plausible";
 import dayjs from "dayjs";
 
-export default function view() {
+export default function View() {
   const [classe, setClasse] = useState({});
   const [url, setUrl] = useState("");
   const [studentStatus, setStudentStatus] = useState([]);
@@ -44,8 +44,8 @@ export default function view() {
   const user = useSelector((state) => state.Auth.user);
   const [edit, setEdit] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-  const [canEditCohort, setCanEditCohort] = useState(false);
-  const [cohorts, setCohorts] = useState([]);
+  const canEditCohort = useState([ROLES.ADMIN, ROLES.REFERENT_DEPARTMENT, ROLES.REFERENT_REGION].includes(user?.role));
+  const cohorts = useSelector((state) => state.Cohorts).filter((c) => c.type === COHORT_TYPE.CLE);
 
   const history = useHistory();
 
@@ -97,23 +97,6 @@ export default function view() {
   useEffect(() => {
     getClasse();
   }, [edit]);
-
-  useEffect(() => {
-    if (!user) return;
-    setCanEditCohort([ROLES.ADMIN, ROLES.REFERENT_DEPARTMENT, ROLES.REFERENT_REGION].includes(user.role));
-  }, [user]);
-
-  useEffect(() => {
-    (async () => {
-      try {
-        const responseCohorts = await api.get(`/cohort?type=${COHORT_TYPE.CLE}`);
-        setCohorts(responseCohorts.data);
-      } catch (e) {
-        capture(e);
-        setCohorts([]);
-      }
-    })();
-  }, []);
 
   const sendInfo = async () => {
     try {
@@ -177,17 +160,15 @@ export default function view() {
     history.push(`/volontaire/create?classeId=${classe._id}`);
   };
 
-  const actionList = edit
-    ? [
-        <div className="flex items-center justify-end ml-6">
-          <Button key="cancel" type="cancel" title="Annuler" onClick={onCancel} disabled={isLoading} />
-          <Button key="validate" type="primary" title="Valider" className={"!h-8 ml-2"} onClick={sendInfo} disabled={isLoading} />
-        </div>,
-      ]
-    : [ROLES.ADMINISTRATEUR_CLE, ROLES.REFERENT_CLASSE, ROLES.ADMIN, ROLES.REFERENT_DEPARTMENT, ROLES.REFERENT_REGION].includes(user.role) &&
-        classe?.status !== STATUS_CLASSE.WITHDRAWN
-      ? [<Button key="change" type="change" leftIcon={<HiOutlinePencil size={16} />} title="Modifier" onClick={() => setEdit(!edit)} disabled={isLoading} />]
-      : null;
+  const actionList = edit ? (
+    <div className="flex items-center justify-end ml-6">
+      <Button key="cancel" type="cancel" title="Annuler" onClick={onCancel} disabled={isLoading} />
+      <Button key="validate" type="primary" title="Valider" className={"!h-8 ml-2"} onClick={sendInfo} disabled={isLoading} />
+    </div>
+  ) : [ROLES.ADMINISTRATEUR_CLE, ROLES.REFERENT_CLASSE, ROLES.ADMIN, ROLES.REFERENT_DEPARTMENT, ROLES.REFERENT_REGION].includes(user.role) &&
+    classe?.status !== STATUS_CLASSE.WITHDRAWN ? (
+    [<Button key="change" type="change" leftIcon={<HiOutlinePencil size={16} />} title="Modifier" onClick={() => setEdit(!edit)} disabled={isLoading} />]
+  ) : null;
 
   if (!classe) return <Loader />;
 
