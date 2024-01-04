ad917fd41eeda22dc8abfa5479832b2ea19a062b
C2Chandelier <charlelie.chandelier@epitech.eu>
2024-01-04 14:28:10 +0100

    up

--- admin/src/scenes/volontaire-cle/list.jsx
+++ admin/src/scenes/volontaire-cle/list.jsx
@@ -17,7 +17,7 @@ export default function list() {
   const [sessionsPhase1, setSessionsPhase1] = useState(null);
   const [bus, setBus] = useState(null);
   const [classes, setClasses] = useState(null);
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(null);
   const pageId = "youngCle-list";
   const [selectedFilters, setSelectedFilters] = useState({});
   const [isExportOpen, setIsExportOpen] = useState(false);
@@ -60,92 +60,108 @@ export default function list() {
       <Header
         title="Liste de mes élèves"
         breadcrumb={[{ title: <StudentIcon className="scale-[65%]" /> }, { title: "Mes élèves" }]}
-        actions={[<Button title="Exporter" type="primary" onClick={() => setIsExportOpen(true)} />]}
+        actions={data && [<Button title="Exporter" type="primary" onClick={() => setIsExportOpen(true)} />]}
       />
-      <ModalExport
-        isOpen={isExportOpen}
-        setIsOpen={setIsExportOpen}
-        route="/elasticsearch/cle/young/export"
-        transform={(data, values) => transformVolontairesCLE(data, values)}
-        exportFields={youngCleExportFields}
-        exportTitle="volontaires"
-        showTotalHits={true}
-        selectedFilters={selectedFilters}
-      />
-      <Container className="!p-0">
-        <div className="mb-8 flex flex-col rounded-xl bg-white py-4">
-          <div className="flex items-stretch justify-between  bg-white px-4 pt-2">
-            <Filters
-              pageId={pageId}
-              route="/elasticsearch/cle/young/search?needClasseInfo=true"
-              setData={(value) => setData(value)}
-              filters={filterArray}
-              searchPlaceholder="Rechercher par mots clés, ville, code postal..."
-              selectedFilters={selectedFilters}
-              setSelectedFilters={setSelectedFilters}
-              paramData={paramData}
-              setParamData={setParamData}
-              size={size}
-            />
-            <SortOption
-              sortOptions={[
-                { label: "Nom (A > Z)", field: "lastName.keyword", order: "asc" },
-                { label: "Nom (Z > A)", field: "lastName.keyword", order: "desc" },
-                { label: "Prénom (A > Z)", field: "firstName.keyword", order: "asc" },
-                { label: "Prénom (Z > A)", field: "firstName.keyword", order: "desc" },
-                { label: "Date de création (récent > ancien)", field: "createdAt", order: "desc" },
-                { label: "Date de création (ancien > récent)", field: "createdAt", order: "asc" },
-              ]}
-              selectedFilters={selectedFilters}
-              paramData={paramData}
-              setParamData={setParamData}
-            />
+      {!data && (
+        <Container className="!p-8">
+          <div className="py-6 bg-gray-50">
+            <div className="flex items-center justify-center h-[136px] mb-4 text-lg text-gray-500 text-center">Vous n’avez pas encore créé de classe engagée</div>
+            <div className="flex items-start justify-center h-[136px]">
+              <Link to="/classes/create">
+                <Button type="wired" leftIcon={<HiPlus />} title="Créer une première classe engagée" />
+              </Link>
+            </div>
           </div>
-          <div className="mt-2 flex flex-row flex-wrap items-center px-4">
-            <Save selectedFilters={selectedFilters} filterArray={filterArray} page={paramData?.page} pageId={pageId} />
-            <SelectedFilters
-              filterArray={filterArray}
-              selectedFilters={selectedFilters}
-              setSelectedFilters={setSelectedFilters}
-              paramData={paramData}
-              setParamData={setParamData}
-            />
-          </div>
-
-          <ResultTable
-            paramData={paramData}
-            setParamData={setParamData}
-            currentEntryOnPage={data?.length}
-            size={size}
-            setSize={setSize}
-            render={
-              <table className="mt-6 mb-2 flex w-full flex-col divide-y table-auto divide-gray-100 border-gray-100">
-                <thead>
-                  <tr className="flex items-center py-3 px-4 text-xs font-[500] leading-5 uppercase text-gray-500 bg-gray-50 cursor-default">
-                    <span className="w-[35%]">Élèves</span>
-                    <span className="w-[20%]">Cohortes</span>
-                    <span className="w-[20%]">Classes</span>
-                    <span className="w-[20%]">Statuts</span>
-                    <span className="w-[5%]">Actions</span>
-                  </tr>
-                </thead>
-                <tbody>
-                  {data.map((hit) => (
-                    <Hit key={hit._id} hit={hit} />
-                  ))}
-                </tbody>
-                <tr className="flex items-center py-3 px-4 text-xs uppercase text-gray-400 bg-gray-50">
-                  <span className="w-[35%]">Élèves</span>
-                  <span className="w-[20%]">Cohortes</span>
-                  <span className="w-[20%]">Classes</span>
-                  <span className="w-[20%]">Statuts</span>
-                  <span className="w-[5%]">Actions</span>
-                </tr>
-              </table>
-            }
+        </Container>
+      )}
+      {data && (
+        <>
+          <ModalExport
+            isOpen={isExportOpen}
+            setIsOpen={setIsExportOpen}
+            route="/elasticsearch/cle/young/export"
+            transform={(data, values) => transformVolontairesCLE(data, values)}
+            exportFields={youngCleExportFields}
+            exportTitle="volontaires"
+            showTotalHits={true}
+            selectedFilters={selectedFilters}
           />
-        </div>
-      </Container>
+          <Container className="!p-0">
+            <div className="mb-8 flex flex-col rounded-xl bg-white py-4">
+              <div className="flex items-stretch justify-between  bg-white px-4 pt-2">
+                <Filters
+                  pageId={pageId}
+                  route="/elasticsearch/cle/young/search?needClasseInfo=true"
+                  setData={(value) => setData(value)}
+                  filters={filterArray}
+                  searchPlaceholder="Rechercher par mots clés, ville, code postal..."
+                  selectedFilters={selectedFilters}
+                  setSelectedFilters={setSelectedFilters}
+                  paramData={paramData}
+                  setParamData={setParamData}
+                  size={size}
+                />
+                <SortOption
+                  sortOptions={[
+                    { label: "Nom (A > Z)", field: "lastName.keyword", order: "asc" },
+                    { label: "Nom (Z > A)", field: "lastName.keyword", order: "desc" },
+                    { label: "Prénom (A > Z)", field: "firstName.keyword", order: "asc" },
+                    { label: "Prénom (Z > A)", field: "firstName.keyword", order: "desc" },
+                    { label: "Date de création (récent > ancien)", field: "createdAt", order: "desc" },
+                    { label: "Date de création (ancien > récent)", field: "createdAt", order: "asc" },
+                  ]}
+                  selectedFilters={selectedFilters}
+                  paramData={paramData}
+                  setParamData={setParamData}
+                />
+              </div>
+              <div className="mt-2 flex flex-row flex-wrap items-center px-4">
+                <Save selectedFilters={selectedFilters} filterArray={filterArray} page={paramData?.page} pageId={pageId} />
+                <SelectedFilters
+                  filterArray={filterArray}
+                  selectedFilters={selectedFilters}
+                  setSelectedFilters={setSelectedFilters}
+                  paramData={paramData}
+                  setParamData={setParamData}
+                />
+              </div>
+
+              <ResultTable
+                paramData={paramData}
+                setParamData={setParamData}
+                currentEntryOnPage={data?.length}
+                size={size}
+                setSize={setSize}
+                render={
+                  <table className="mt-6 mb-2 flex w-full flex-col divide-y table-auto divide-gray-100 border-gray-100">
+                    <thead>
+                      <tr className="flex items-center py-3 px-4 text-xs font-[500] leading-5 uppercase text-gray-500 bg-gray-50 cursor-default">
+                        <span className="w-[35%]">Élèves</span>
+                        <span className="w-[20%]">Cohortes</span>
+                        <span className="w-[20%]">Classes</span>
+                        <span className="w-[20%]">Statuts</span>
+                        <span className="w-[5%]">Actions</span>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {data.map((hit) => (
+                        <Hit key={hit._id} hit={hit} />
+                      ))}
+                    </tbody>
+                    <tr className="flex items-center py-3 px-4 text-xs uppercase text-gray-400 bg-gray-50">
+                      <span className="w-[35%]">Élèves</span>
+                      <span className="w-[20%]">Cohortes</span>
+                      <span className="w-[20%]">Classes</span>
+                      <span className="w-[20%]">Statuts</span>
+                      <span className="w-[5%]">Actions</span>
+                    </tr>
+                  </table>
+                }
+              />
+            </div>
+          </Container>
+        </>
+      )}
     </Page>
   );
 }
