780d7dd2992b392f450b41562340020693444c3b
michaelgarcia75 <99188042+michaelgarcia75@users.noreply.github.com>
2024-01-11 12:20:31 +0100

    fix(api): 1520: remediation token duration (#3468)

--- api/src/cookie-options.js
+++ api/src/cookie-options.js
@@ -1,9 +1,9 @@
 const config = require("./config");
 
 //!COOKIE need to be in milliseconds
-const COOKIE_SIGNIN_MAX_AGE = 60 * 60 * 2 * 1000; // 2h
-const COOKIE_SNUPPORT_MAX_AGE = 60 * 60 * 2 * 1000; //2h
-const COOKIE_TRUST_TOKEN_JWT_MAX_AGE = 60 * 60 * 24 * 30 * 1000; // 1 mois
+const COOKIE_SIGNIN_MAX_AGE_MS = 60 * 60 * 2 * 1000; // 2h
+const COOKIE_SNUPPORT_MAX_AGE_MS = 60 * 60 * 2 * 1000; //2h
+const COOKIE_TRUST_TOKEN_JWT_MAX_AGE_MS = 60 * 60 * 24 * 30 * 1000; // 1 mois
 //!COOKIE need to be in milliseconds
 
 function cookieOptions(maxAge) {
@@ -19,7 +19,7 @@ function cookieOptions(maxAge) {
 
 module.exports = {
   cookieOptions,
-  COOKIE_SIGNIN_MAX_AGE,
-  COOKIE_SNUPPORT_MAX_AGE,
-  COOKIE_TRUST_TOKEN_JWT_MAX_AGE,
+  COOKIE_SIGNIN_MAX_AGE_MS,
+  COOKIE_SNUPPORT_MAX_AGE_MS,
+  COOKIE_TRUST_TOKEN_JWT_MAX_AGE_MS,
 };
--- api/src/jwt-options.js
+++ api/src/jwt-options.js
@@ -1,6 +1,6 @@
 //!TOKEN need to be in seconds
-const JWT_SIGNIN_MAX_AGE = 60 * 60 * 2; // 2h
-const JWT_TRUST_TOKEN_MAX_AGE = 60 * 60 * 24 * 30; // 1 mois
+const JWT_SIGNIN_MAX_AGE_S = 60 * 60 * 2; // 2h
+const JWT_TRUST_TOKEN_MAX_AGE_S = 60 * 60 * 24 * 30; // 1 mois
 //!TOKEN need to be in seconds
 
 // ! If you upgrade this, all jwt will be invalid
@@ -11,8 +11,8 @@ const checkJwtSigninVersion = (token) => token.__v === JWT_SIGNIN_VERSION;
 const checkJwtTrustTokenVersion = (token) => token.__v === JWT_TRUST_TOKEN_VERSION;
 
 module.exports = {
-  JWT_SIGNIN_MAX_AGE,
-  JWT_TRUST_TOKEN_MAX_AGE,
+  JWT_SIGNIN_MAX_AGE_S,
+  JWT_TRUST_TOKEN_MAX_AGE_S,
   JWT_SIGNIN_VERSION,
   JWT_TRUST_TOKEN_VERSION,
   checkJwtSigninVersion,
