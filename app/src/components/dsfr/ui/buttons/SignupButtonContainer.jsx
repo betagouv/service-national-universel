9a081ca611ad8a8fffe1c96142427cd04e92b489
Marin <kalasasta@gmail.com>
2024-01-18 17:31:31 +0100

    feat(1184): Signup Button Container is using react dsfr.

--- app/src/components/dsfr/ui/buttons/SignupButtonContainer.jsx
+++ app/src/components/dsfr/ui/buttons/SignupButtonContainer.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { FiChevronLeft } from "react-icons/fi";
 import PrimaryButton from "./PrimaryButton";
 import SecondaryButton from "./SecondaryButton";
+import { Button } from "@snu/ds/dsfr";
+import { fr } from "@codegouvfr/react-dsfr";
 
 export default function SignupButtonContainer({
   onClickNext,
@@ -17,15 +19,19 @@ export default function SignupButtonContainer({
       <hr className="mb-8 md:block" />
       <div className={`flex ${!collapsePrevious && "flex-col-reverse md:flex-row"} justify-center gap-2 md:justify-end items-center`}>
         {onClickPrevious && (
-          <SecondaryButton className={`${collapsePrevious ? "w-auto" : "w-full md:w-auto"} `} onClick={onClickPrevious}>
-            {onClickNext && collapsePrevious ? <FiChevronLeft className="block md:hidden" /> : <p className="block w-64 md:hidden">{labelPrevious}</p>}
+          <Button
+            priority="secondary"
+            iconId={onClickNext && collapsePrevious && fr.cx("fr-icon-arrow-left-s-line")}
+            className={`${collapsePrevious ? "w-auto" : "w-full md:w-auto"} `}
+            onClick={onClickPrevious}>
+            <p className="block w-64 md:hidden">{labelPrevious}</p>
             <p className="hidden md:block">{labelPrevious}</p>
-          </SecondaryButton>
+          </Button>
         )}
         {onClickNext && (
-          <PrimaryButton onClick={onClickNext} disabled={disabled}>
+          <Button onClick={onClickNext} disabled={disabled}>
             {labelNext}
-          </PrimaryButton>
+          </Button>
         )}
       </div>
       {text && <p className="md:text-right text-sm mt-3">{text}</p>}
