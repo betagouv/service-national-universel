01131bfd698b06837d274eb37a659261a59ec996
Michael Garcia <mika_garcia21@hotmail.com>
2024-01-22 15:00:33 +0100

    gregregre

--- api/src/controllers/application.js
+++ api/src/controllers/application.js
@@ -742,6 +742,7 @@ router.post(
               host: "127.0.0.1",
               port: 3310,
               timeout: 30000,
+              socket: "/run/clamd/socket.tcl",
             },
           });
           const { isInfected } = await clamscan.isInfected(tempFilePath);
