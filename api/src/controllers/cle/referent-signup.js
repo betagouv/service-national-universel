5c532518f21f88959eff2681153c87b6092f279e
C2Chandelier <charlelie.chandelier@epitech.eu>
2024-01-26 15:37:00 +0100

    up

--- api/src/controllers/cle/referent-signup.js
+++ api/src/controllers/cle/referent-signup.js
@@ -30,7 +30,7 @@ router.get("/token/:token", async (req, res) => {
       return res.status(400).send({ ok: false, code: ERRORS.INVALID_BODY });
     }
 
-    const referent = await ReferentModel.findOne({ invitationToken: value.token });
+    const referent = await ReferentModel.findOne({ invitationToken: value.token, invitationExpires: { $gt: Date.now() } });
     if (!referent) return res.status(404).send({ ok: false, code: ERRORS.NOT_FOUND });
 
     let etablissement;
@@ -170,6 +170,7 @@ router.post("/confirm-signup", async (req, res) => {
         region: ramsesSchool.region,
         department: ramsesSchool.departmentName,
         invitationToken: null,
+        invitationExpires: null,
         acceptCGU: true,
       });
       await referent.save({ fromUser: referent });
@@ -186,7 +187,7 @@ router.post("/confirm-signup", async (req, res) => {
       }
       if (!etablissement) return res.status(404).send({ ok: false, code: ERRORS.NOT_FOUND, message: "Vous n'êtes lié à aucun établissement" });
 
-      referent.set({ invitationToken: null, acceptCGU: true, region: etablissement.region, department: etablissement.department });
+      referent.set({ invitationToken: null, invitationExpires: null, acceptCGU: true, region: etablissement.region, department: etablissement.department });
       await referent.save({ fromUser: referent });
     }
 
