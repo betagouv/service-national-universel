551179b410184bf646d888285e635c33ddfa3939
Marin <kalasasta@gmail.com>
2024-01-31 13:45:37 +0100

    feat(app): 1783 - New navbar Phase 2.

--- app/src/components/layout/navbar/components/NavigationMenu.jsx
+++ app/src/components/layout/navbar/components/NavigationMenu.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { YOUNG_STATUS_PHASE3, FEATURES_NAME, isFeatureEnabled } from "snu-lib";
+import { YOUNG_STATUS_PHASE2, YOUNG_STATUS_PHASE3, FEATURES_NAME, isFeatureEnabled } from "snu-lib";
 import { environment, supportURL } from "../../../../config";
 import { permissionPhase1, permissionPhase2, permissionPhase3 } from "../../../../utils";
 import plausibleEvent from "@/services/plausible";
@@ -33,7 +33,10 @@ export default function NavigationMenu({ onClose = () => {} }) {
       <ul>
         <MenuLink to="/" icon={<IconHome />} text="Accueil" onClose={onClose} />
         <MenuLink to="/phase1" icon={<IconPhase1 />} text="Phase 1 - Séjour" enabled={permissionPhase1(young)} status={young.statusPhase1} onClose={onClose} />
-        <MenuLink to="/phase2" icon={<IconPhase2 />} text="Phase 2 - Engagement" enabled={permissionPhase2(young)} status={young.statusPhase2} onClose={onClose} />
+        <MenuGroup to="/phase2" icon={<IconPhase2 />} text="Phase 2 - Engagement" enabled={permissionPhase2(young)} status={young.statusPhase2} onClose={onClose}>
+          <MenuLink to="/phase2" text="Mission d’intérêt général" onClose={onClose} />
+          <MenuLink to="/phase3" text="Autres engagements" onClose={onClose} />
+        </MenuGroup>
         {hasAccessToPhase3(young) && (
           <MenuGroup to="/phase3" icon={<IconPhase3 />} text="Phase 3" enabled={permissionPhase3(young)} status={young.statusPhase3} onClose={onClose}>
             <MenuLink to="/les-programmes" text="Les programmes" onClose={onClose} />
