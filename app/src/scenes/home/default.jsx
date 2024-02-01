f92d027427413c0f270a78c069e1fec2fe740c67
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2024-02-01 15:41:46 +0100

    add links

--- app/src/scenes/home/default.jsx
+++ app/src/scenes/home/default.jsx
@@ -4,6 +4,7 @@ import { PHASE_STATUS_COLOR, translatePhase1, translatePhase2 } from "../../util
 import Badge from "../../components/Badge";
 import Img2 from "../../assets/homePhase2Desktop.png";
 import Img3 from "../../assets/homePhase2Mobile.png";
+import { Link } from "react-router-dom";
 
 export default function HomeDefault() {
   const young = useSelector((state) => state.Auth.young);
@@ -21,15 +22,19 @@ export default function HomeDefault() {
 
         <p className="text-xl font-bold leading-loose text-gray-700">Votre parcours</p>
 
-        <p className="leading-loose">
-          <span className="mr-2">1. Un séjour de cohésion</span>
-          <Badge text={translatePhase1(young.statusPhase1)} color={PHASE_STATUS_COLOR[young.statusPhase1]} />
-        </p>
+        <Link to="/phase1">
+          <p className="leading-loose">
+            <span className="mr-2">1. Un séjour de cohésion</span>
+            <Badge text={translatePhase1(young.statusPhase1)} color={PHASE_STATUS_COLOR[young.statusPhase1]} />
+          </p>
+        </Link>
 
-        <p className="leading-loose">
-          <span className="mr-2">2. Une phase d&apos;engagement</span>
-          <Badge text={translatePhase2(young.statusPhase2)} color={PHASE_STATUS_COLOR[young.statusPhase2]} />
-        </p>
+        <Link to="/phase2">
+          <p className="leading-loose">
+            <span className="mr-2">2. Une phase d&apos;engagement</span>
+            <Badge text={translatePhase2(young.statusPhase2)} color={PHASE_STATUS_COLOR[young.statusPhase2]} />
+          </p>
+        </Link>
       </div>
       <img className="flex-1 hidden xl:block" src={Img2} />
     </main>
