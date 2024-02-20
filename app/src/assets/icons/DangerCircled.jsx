3ac60e932256f2a3efd356decdf68515f2d672b1
Marin <kalasasta@gmail.com>
2024-02-20 11:49:47 +0100

    feat(app): Using react Icons on DangerCircled icon.

--- app/src/assets/icons/DangerCircled.jsx
+++ app/src/assets/icons/DangerCircled.jsx
@@ -1,20 +1,10 @@
 import * as React from "react";
+import { HiExclamation } from "react-icons/hi";
 
 const DangerCircled = () => (
-  <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="24" cy="24" r="24" fill="#FEF3C7" />
-    <path
-      d="M22.2675 16C23.0373 14.6667 24.9618 14.6667 25.7316 16L32.6598 28C33.4296 29.3333 32.4673 31 30.9277 31H17.0713C15.5317 31 14.5695 29.3333 15.3393 28L22.2675 16Z"
-      fill="#D97706"
-    />
-    <path
-      d="M23.9995 21V23M23.9995 27H24.0095M17.0713 31H30.9277C32.4673 31 33.4296 29.3333 32.6598 28L25.7316 16C24.9618 14.6667 23.0373 14.6667 22.2675 16L15.3393 28C14.5695 29.3333 15.5317 31 17.0713 31Z"
-      stroke="#111827"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-  </svg>
+  <span className="bg-amber-100 rounded-full p-2">
+    <HiExclamation background-color="black" stroke-color="black" stroke-width="1" size={32} className="fill-amber-600 stroke-black" />
+  </span>
 );
 
 export default DangerCircled;
