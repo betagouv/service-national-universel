583c2104e7a9308b5b56b0772622002285ded826
Marin <kalasasta@gmail.com>
2024-02-20 11:52:02 +0100

    feat(app): Using react Icons on DangerCircled icon.

--- app/src/assets/icons/DangerCircled.jsx
+++ app/src/assets/icons/DangerCircled.jsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 import { HiExclamation } from "react-icons/hi";
 
-const DangerCircled = () => (
+const DangerCircled = ({ size = 32 }) => (
   <span className="bg-amber-100 rounded-full p-2">
-    <HiExclamation background-color="black" stroke-color="black" stroke-width="1" size={32} className="fill-amber-600 stroke-black" />
+    <HiExclamation background-color="black" stroke-color="black" stroke-width="1" size={size} className="fill-amber-600 stroke-black" />
   </span>
 );
 
