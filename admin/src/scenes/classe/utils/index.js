3dcbf0795a59815f0ff934f2d8742376f860a10a
hlecourt <50077368+hlecourt@users.noreply.github.com>
2024-02-28 16:52:56 +0100

    fix(admin): Visibilité affectation CLE pour ref. dep. (#3623)

--- admin/src/scenes/classe/utils/index.js
+++ admin/src/scenes/classe/utils/index.js
@@ -40,20 +40,19 @@ export function getRights(user, classe, cohort) {
     canEdit:
       [ROLES.ADMINISTRATEUR_CLE, ROLES.REFERENT_CLASSE, ROLES.ADMIN, ROLES.REFERENT_DEPARTMENT, ROLES.REFERENT_REGION].includes(user.role) &&
       classe?.status !== STATUS_CLASSE.WITHDRAWN,
-    canEditCohort:
-      [ROLES.ADMIN, ROLES.REFERENT_DEPARTMENT].includes(user?.role) || (user?.role === ROLES.REFERENT_REGION && (cohort ? cohort.cleUpdateCohortForReferentRegion : true)),
+    canEditCohort: [ROLES.ADMIN].includes(user?.role) || (user?.role === ROLES.REFERENT_REGION && (cohort ? cohort.cleUpdateCohortForReferentRegion : true)),
     canEditCenter: user?.role === ROLES.ADMIN || (user?.role === ROLES.REFERENT_REGION && (cohort ? cohort.cleUpdateCentersForReferentRegion : true)),
     canEditPDR: user?.role === ROLES.ADMIN,
     showCohort:
-      [ROLES.ADMIN, ROLES.REFERENT_REGION].includes(user?.role) ||
+      [ROLES.ADMIN, ROLES.REFERENT_REGION, ROLES.REFERENT_DEPARTMENT].includes(user?.role) ||
       (user?.role === ROLES.ADMINISTRATEUR_CLE && cohort?.cleDisplayCohortsForAdminCLE) ||
       (user?.role === ROLES.REFERENT_CLASSE && cohort?.cleDisplayCohortsForReferentClasse),
     showCenter:
-      [ROLES.ADMIN, ROLES.REFERENT_REGION].includes(user?.role) ||
+      [ROLES.ADMIN, ROLES.REFERENT_REGION, ROLES.REFERENT_DEPARTMENT].includes(user?.role) ||
       (user?.role === ROLES.ADMINISTRATEUR_CLE && cohort?.cleDisplayCentersForAdminCLE) ||
       (user?.role === ROLES.REFERENT_CLASSE && cohort?.cleDisplayCentersForReferentClasse),
     showPDR:
-      user?.role === ROLES.ADMIN ||
+      [ROLES.ADMIN, ROLES.REFERENT_REGION, ROLES.REFERENT_DEPARTMENT].includes(user?.role) ||
       (user?.role === ROLES.ADMINISTRATEUR_CLE && cohort?.cleDisplayPDRForAdminCLE) ||
       (user?.role === ROLES.REFERENT_CLASSE && cohort?.cleDisplayPDRForReferentClasse),
   };
