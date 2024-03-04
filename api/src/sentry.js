8d5d968b04444fd33c875f2e027616fc7f0b3cf0
rthebaud <dev@rthebaud.fr>
2024-03-04 13:26:35 +0100

    feat(api): 1949 - CRON job (#3624)
    * feat(api): 1949 - Create cron service
    
    * Activate in production

--- api/src/sentry.js
+++ api/src/sentry.js
@@ -25,51 +25,55 @@ addGlobalEventProcessor((event) => {
   return event;
 });
 
-function initSentry(app) {
+function initSentry() {
+  init({
+    enabled: Boolean(SENTRY_URL),
+    dsn: SENTRY_URL,
+    environment: "api",
+    normalizeDepth: 16,
+    integrations: [
+      new ExtraErrorData({ depth: 16 }),
+      new RewriteFrames({ root: process.cwd() }),
+      new NodeIntegrations.Http({ tracing: true }),
+      new NodeIntegrations.Modules(),
+      new ProfilingIntegration(),
+      ...autoDiscoverNodePerformanceMonitoringIntegrations(),
+    ],
+    tracesSampleRate: Number(SENTRY_TRACING_SAMPLE_RATE) || 0.01,
+    profilesSampleRate: Number(SENTRY_PROFILE_SAMPLE_RATE) || 0.1, // Percent of Transactions profiled
+    ignoreErrors: [
+      /^No error$/,
+      /__show__deepen/,
+      /_avast_submit/,
+      /Access is denied/,
+      /anonymous function: captureException/,
+      /Blocked a frame with origin/,
+      /can't redefine non-configurable property "userAgent"/,
+      /change_ua/,
+      /console is not defined/,
+      /cordova/,
+      /DataCloneError/,
+      /Error: AccessDeny/,
+      /event is not defined/,
+      /feedConf/,
+      /ibFindAllVideos/,
+      /myGloFrameList/,
+      /SecurityError/,
+      /MyIPhoneApp/,
+      /snapchat.com/,
+      /vid_mate_check is not defined/,
+      /win\.document\.body/,
+      /window\._sharedData\.entry_data/,
+      /window\.regainData/,
+      /ztePageScrollModule/,
+    ],
+  });
+}
+
+function initSentryMiddlewares(app) {
   if (ENVIRONMENT !== "development") {
     // Evite le spam sentry en local
-    init({
-      enabled: Boolean(SENTRY_URL),
-      dsn: SENTRY_URL,
-      environment: "api",
-      normalizeDepth: 16,
-      integrations: [
-        new ExtraErrorData({ depth: 16 }),
-        new RewriteFrames({ root: process.cwd() }),
-        new NodeIntegrations.Http({ tracing: true }),
-        new NodeIntegrations.Modules(),
-        new ProfilingIntegration(),
-        ...autoDiscoverNodePerformanceMonitoringIntegrations(),
-      ],
-      tracesSampleRate: Number(SENTRY_TRACING_SAMPLE_RATE) || 0.01,
-      profilesSampleRate: Number(SENTRY_PROFILE_SAMPLE_RATE) || 0.1, // Percent of Transactions profiled
-      ignoreErrors: [
-        /^No error$/,
-        /__show__deepen/,
-        /_avast_submit/,
-        /Access is denied/,
-        /anonymous function: captureException/,
-        /Blocked a frame with origin/,
-        /can't redefine non-configurable property "userAgent"/,
-        /change_ua/,
-        /console is not defined/,
-        /cordova/,
-        /DataCloneError/,
-        /Error: AccessDeny/,
-        /event is not defined/,
-        /feedConf/,
-        /ibFindAllVideos/,
-        /myGloFrameList/,
-        /SecurityError/,
-        /MyIPhoneApp/,
-        /snapchat.com/,
-        /vid_mate_check is not defined/,
-        /win\.document\.body/,
-        /window\._sharedData\.entry_data/,
-        /window\.regainData/,
-        /ztePageScrollModule/,
-      ],
-    });
+    initSentry();
   }
 
   // The request handler must be the first middleware on the app
@@ -115,6 +119,7 @@ function captureMessage(mess, contexte) {
 
 module.exports = {
   initSentry,
+  initSentryMiddlewares,
   capture,
   captureMessage,
 };
