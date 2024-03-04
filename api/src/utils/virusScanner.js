548cef7dd736ee7ce13388615981bd73ba567666
pitiscarf <dev@rthebaud.fr>
2024-03-04 10:27:41 +0100

    fix error handling

--- api/src/utils/virusScanner.js
+++ api/src/utils/virusScanner.js
@@ -23,22 +23,22 @@ async function scanFile(tempFilePath, name, userId="anonymous") {
       headers["X-Auth-Token"] = API_ANTIVIRUS_TOKEN
 
       const response = await fetch(url, { method: 'POST', body: formData, headers });
-
-      if (response.status != 200) {
-        throw new Error(ERRORS.FILE_SCAN_DOWN);
-      }
-
-      const { infected } = await response.json()
-
-      if (infected) {
-        capture(`File ${name} of user(${userId}) is infected`);
-      }
-
-      return { infected };
     } catch (error) {
       capture(error);
       throw new Error(ERRORS.FILE_SCAN_DOWN);
     }
+
+    if (response.status != 200) {
+      throw new Error(ERRORS.FILE_SCAN_DOWN);
+    }
+
+    const { infected } = await response.json()
+
+    if (infected) {
+      capture(`File ${name} of user(${userId}) is infected`);
+    }
+
+    return { infected };
   };
   return await timeout(scan(), TIMEOUT_ANTIVIRUS_SERVICE);
 }
