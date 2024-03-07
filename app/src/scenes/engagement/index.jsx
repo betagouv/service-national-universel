21ec4d4089ab1a6738a43714ce18a9739ca8655c
Guillaume <guillaume@macmax.home>
2024-03-07 10:32:56 +0100

    feat(app): 1725 - breadcrumb add on App

--- app/src/scenes/engagement/index.jsx
+++ app/src/scenes/engagement/index.jsx
@@ -5,6 +5,7 @@ import { RiArrowLeftSLine } from "react-icons/ri";
 
 import api from "../../services/api";
 import Loader from "../../components/Loader";
+import Breadcrumb from "@/components/layout/navbar/components/BreadCrumb";
 import EngagementCard from "@/scenes/preinscription/components/EngagementCard";
 
 export default function Index() {
@@ -18,22 +19,25 @@ export default function Index() {
   }, []);
   if (!programs) return <Loader />;
   return (
-    <div className="pt-2 mb-4 pb-4 sm:px-4 md:px-16 md:pb-6 md:pt-6 md:mx-6 md:mt-10 rounded-lg bg-white">
-      <Link to="/autres-engagements">
-        <button className="mt-4 mb-6 flex py-2 px-2 border rounded-lg text-xs leading-4 font-medium">
-          <RiArrowLeftSLine className="mr-2 text-lg" />
-          Retour
-        </button>
-      </Link>
-      <div className="mb-8">
-        <h2 className="text-gray-900 text-2xl md:text-3xl font-bold mb-2">Tous les autres programmes d&apos;engagement</h2>
-        <p className="text-gray-600 text-lg">Rejoignez plus de 100 000 jeunes français déjà engagés dans de grandes causes</p>
+    <>
+      <Breadcrumb />
+      <div className="pt-2 mb-4 pb-4 sm:px-4 md:px-16 md:pb-6 md:pt-6 md:mx-6 md:mt-10 rounded-lg bg-white">
+        <Link to="/autres-engagements">
+          <button className="mt-4 mb-6 flex py-2 px-2 border rounded-lg text-xs leading-4 font-medium">
+            <RiArrowLeftSLine className="mr-2 text-lg" />
+            Retour
+          </button>
+        </Link>
+        <div className="mb-8">
+          <h2 className="text-gray-900 text-2xl md:text-3xl font-bold mb-2">Tous les autres programmes d&apos;engagement</h2>
+          <p className="text-gray-600 text-lg">Rejoignez plus de 100 000 jeunes français déjà engagés dans de grandes causes</p>
+        </div>
+        <div className="flex gap-8 overflow-x-auto md:grid md:grid-cols-3">
+          {programs.map((program, index) => (
+            <EngagementCard program={program} key={index} />
+          ))}
+        </div>
       </div>
-      <div className="flex gap-8 overflow-x-auto md:grid md:grid-cols-3">
-        {programs.map((program, index) => (
-          <EngagementCard program={program} key={index} />
-        ))}
-      </div>
-    </div>
+    </>
   );
 }
--- app/src/scenes/phase3/missions.jsx
+++ app/src/scenes/phase3/missions.jsx
@@ -13,6 +13,7 @@ import Pagination from "../../components/nav/Pagination";
 import { Link } from "react-router-dom";
 import { HeroContainer } from "@/components/Content";
 import { RiArrowLeftSLine } from "react-icons/ri";
+import Breadcrumb from "@/components/layout/navbar/components/BreadCrumb";
 
 export default function MissionsComponent() {
   const young = useSelector((state) => state.Auth.young);
@@ -54,6 +55,7 @@ export default function MissionsComponent() {
 
   return (
     <HeroContainer>
+      <Breadcrumb />
       <div className="pt-2 pb-4 sm:px-4 md:px-16 md:pb-6 md:pt-6 md:mx-6 md:mt-10 rounded-lg bg-white">
         <Link to="/autres-engagements">
           <button className="mt-4 mb-6 flex py-2 px-2 border rounded-lg text-xs leading-4 font-medium">
