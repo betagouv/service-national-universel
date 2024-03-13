13cd8675d86c6326c64ffb3066f5d6354803de9a
pitiscarf <dev@rthebaud.fr>
2024-03-13 22:48:42 +0100

    Admin & nginx config

---
+++ admin/docker_start.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -ex
+
+cd /usr/share/nginx/html
+
+mv index.html index.html.template
+
+sed '/<noscript> You need to enable JavaScript to run this app. <\/noscript>/a\
+<script> \
+    globalThis.runtime_env = { \
+        "API_URL":"$API_URL", \
+        "APP_URL":"$APP_URL", \
+        "ADMIN_URL":"$ADMIN_URL", \
+        "SUPPORT_URL":"$SUPPORT_URL", \
+        "MAINTENANCE":"$MAINTENANCE", \
+        "SENTRY_URL":"$SENTRY_URL", \
+        "SENTRY_TRACING_SAMPLE_RATE":"$SENTRY_TRACING_SAMPLE_RATE", \
+        "SENTRY_SESSION_SAMPLE_RATE":"$SENTRY_SESSION_SAMPLE_RATE", \
+        "SENTRY_ON_ERROR_SAMPLE_RATE":"$SENTRY_ON_ERROR_SAMPLE_RATE" \
+    }; \
+</script> \
+' index.html.template \
+| envsubst > index.html
+
+cd -
+
+# Start nginx
+exec nginx -g "daemon off;"
--- terraform/environments/ci/custom/main.tf
+++ terraform/environments/ci/custom/main.tf
@@ -164,7 +164,7 @@ resource "scaleway_container" "admin" {
   name            = "${local.env}-admin"
   namespace_id    = scaleway_container_namespace.main.id
   registry_image  = "${data.scaleway_registry_namespace.main.endpoint}/admin:${var.admin_image_tag}"
-  port            = 8080
+  port            = 80
   cpu_limit       = 256
   memory_limit    = 256
   min_scale       = 1
@@ -179,18 +179,18 @@ resource "scaleway_container" "admin" {
     "APP_NAME"                  = "admin"
     "CLE"                       = "true"
     "STAGING"                   = "true"
-    "DOCKER_ENV_VITE_ADMIN_URL" = "https://${local.admin_hostname}"
-    "DOCKER_ENV_VITE_API_URL"   = "https://${local.api_hostname}"
-    "DOCKER_ENV_VITE_APP_URL"   = "https://${local.app_hostname}"
-    "DOCKER_ENV_VITE_SENTRY_SESSION_SAMPLE_RATE" = 0.1
-    "DOCKER_ENV_VITE_SENTRY_TRACING_SAMPLE_RATE" = 0.1
-    "DOCKER_ENV_VITE_SUPPORT_URL" = "https://support.beta-snu.dev"
+    "ADMIN_URL"                  = "https://${local.admin_hostname}"
+    "API_URL"                    = "https://${local.api_hostname}"
+    "APP_URL"                    = "https://${local.app_hostname}"
+    "SENTRY_SESSION_SAMPLE_RATE" = 0.1
+    "SENTRY_TRACING_SAMPLE_RATE" = 0.1
+    "SUPPORT_URL"                = "https://support.beta-snu.dev"
   }
 
   secret_environment_variables = {
-    "DOCKER_ENV_VITE_SENTRY_URL"            = local.secrets.SENTRY_URL
-    "SENTRY_AUTH_TOKEN"                     = local.secrets.SENTRY_AUTH_TOKEN
-    "DOCKER_ENV_VITE_USERBACK_ACCESS_TOKEN" = local.secrets.USERBACK_ACCESS_TOKEN
+    "SENTRY_URL"            = local.secrets.SENTRY_URL
+    "SENTRY_AUTH_TOKEN"     = local.secrets.SENTRY_AUTH_TOKEN
+    "VITE_USERBACK_ACCESS_TOKEN" = local.secrets.USERBACK_ACCESS_TOKEN
   }
 }
 
