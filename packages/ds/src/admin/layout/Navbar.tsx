61f6c05b6cb40f7e3ebd778c00939c4b0bcf34fe
C2Chandelier <charlelie.chandelier@gmail.com>
2024-03-15 17:20:52 +0100

    chore(admin): Retour design PDT (#3668)

--- packages/ds/src/admin/layout/Navbar.tsx
+++ packages/ds/src/admin/layout/Navbar.tsx
@@ -13,17 +13,17 @@ type OwnProps = {
 export default function Navbar({ tab, button }: OwnProps) {
   return (
     <div className="flex justify-between border-b border-gray-200 mb-8">
-      <div className="flex gap-4 mt-3">
+      <div className="flex gap-2 mt-3">
         {tab.map((tabItem, index) => (
           <div
             key={`tab-${index}`}
-            className={`flex gap-2 text-sm leading-5 font-medium text-gray-500 cursor-pointer
+            className={`flex gap-1.5 text-sm leading-5 font-medium text-gray-500 cursor-pointer pb-3.5
             ${tabItem.isActive && "border-b-2 border-blue-600 !text-blue-600"}
             `}
             onClick={tabItem.onClick}
           >
             {tabItem.leftIcon}
-            <span className="truncate">{tabItem.title}</span>
+            <span className="truncate mr-3">{tabItem.title}</span>
           </div>
         ))}
       </div>
