50d85396c1c1519ac54011df9f42783b10fa2a1f
Guillaume <guillaume@macmax.home>
2024-03-19 16:58:35 +0100

    up

--- api/src/crons/reminderWaitingCorrection.js
+++ api/src/crons/reminderWaitingCorrection.js
@@ -10,14 +10,22 @@ exports.handler = async () => {
     const now = new Date();
     now.setUTCHours(0, 0, 0, 0);
 
-    const threeDaysBefore = new Date(now);
-    threeDaysBefore.setDate(threeDaysBefore.getDate() - 3);
+    const threeDaysAgo = new Date(now);
+    threeDaysAgo.setDate(now.getDate() - 3);
 
-    const sevenDaysBefore = new Date(now);
-    sevenDaysBefore.setDate(sevenDaysBefore.getDate() - 7);
+    const sevenDaysAgo = new Date(now);
+    sevenDaysAgo.setDate(now.getDate() - 7);
 
     const cursor = await YoungModel.find({
-      "correctionRequests.status": "SENT",
+      correctionRequests: {
+        $elemMatch: {
+          status: "SENT",
+          sentAt: {
+            $gte: sevenDaysAgo,
+            $lte: threeDaysAgo,
+          },
+        },
+      },
       status: YOUNG_STATUS.WAITING_CORRECTION,
     }).cursor();
 
@@ -27,10 +35,8 @@ exports.handler = async () => {
       try {
         for (const request of young.correctionRequests) {
           const sentAt = new Date(request.sentAt);
-          if (
-            (sentAt >= threeDaysBefore && sentAt < new Date(threeDaysBefore).setDate(threeDaysBefore.getDate() + 1)) ||
-            (sentAt >= sevenDaysBefore && sentAt < new Date(sevenDaysBefore).setDate(sevenDaysBefore.getDate() + 1))
-          ) {
+          // On compare les jours sans les heures pour faire au plus juste
+          if (sentAt.toISOString().split("T")[0] === threeDaysAgo.toISOString().split("T")[0] || sentAt.toISOString().split("T")[0] === sevenDaysAgo.toISOString().split("T")[0]) {
             countNotice++;
             await sendTemplate(SENDINBLUE_TEMPLATES.young.PHASE_1_WAITING_CORRECTION_REMINDER, {
               emailTo: [{ name: `${young.firstName} ${young.lastName}`, email: young.email }],
