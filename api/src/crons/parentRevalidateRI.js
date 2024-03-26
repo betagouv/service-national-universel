b550d7ca81b068cbddeaca162286d3bb9a37ccb4
Marin <kalasasta@gmail.com>
2024-03-26 11:18:05 +0100

    fix(api): 2064 - Prevent sending RI reminders to Withdrawn or refused youngs.

--- api/src/crons/parentRevalidateRI.js
+++ api/src/crons/parentRevalidateRI.js
@@ -3,11 +3,12 @@ const YoungModel = require("../models/young");
 const CohortModel = require("../models/cohort");
 const { sendTemplate } = require("../sendinblue");
 const slack = require("../slack");
-const { SENDINBLUE_TEMPLATES, REGLEMENT_INTERIEUR_VERSION } = require("snu-lib");
+const { YOUNG_STATUS, SENDINBLUE_TEMPLATES, REGLEMENT_INTERIEUR_VERSION } = require("snu-lib");
 const { APP_URL } = require("../config");
 
 exports.handler = async () => {
   try {
+    const { VALIDATED, WAITING_LIST, WAITING_VALIDATION, WAITING_CORRECTION, REINSCRIPTION, IN_PROGRESS } = YOUNG_STATUS;
     let countNotice = 0;
     const newRiDate = new Date(REGLEMENT_INTERIEUR_VERSION);
     const cohortCollection = await CohortModel.find({
@@ -18,16 +19,20 @@ exports.handler = async () => {
       cohort: cohorts,
       parent1ValidationDate: { $lt: newRiDate },
     }).cursor();
+
     await cursor.eachAsync(async function (young) {
-      countNotice++;
-      await sendTemplate(SENDINBLUE_TEMPLATES.parent.PARENT1_REVALIDATE_RI, {
-        emailTo: [{ name: `${young.parent1FirstName} ${young.parent1LastName}`, email: young.parent1Email }],
-        params: {
-          cta: `${APP_URL}/representants-legaux/ri-consentement?token=${young.parent1Inscription2023Token}`,
-          youngFirstName: young.firstName,
-          youngName: young.lastName,
-        },
-      });
+      const { status } = young;
+      if ([VALIDATED, WAITING_LIST, WAITING_VALIDATION, WAITING_CORRECTION, REINSCRIPTION, IN_PROGRESS].includes(status)) {
+        countNotice++;
+        await sendTemplate(SENDINBLUE_TEMPLATES.parent.PARENT1_REVALIDATE_RI, {
+          emailTo: [{ name: `${young.parent1FirstName} ${young.parent1LastName}`, email: young.parent1Email }],
+          params: {
+            cta: `${APP_URL}/representants-legaux/ri-consentement?token=${young.parent1Inscription2023Token}`,
+            youngFirstName: young.firstName,
+            youngName: young.lastName,
+          },
+        });
+      }
     });
     slack.success({ title: "1 week notice pending parent RI validation", text: `${countNotice} parent has been noticed !` });
   } catch (e) {
