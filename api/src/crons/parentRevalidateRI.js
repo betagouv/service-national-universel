cb45b4bab06ac9b719946085481f88c89c763388
Marin <kalasasta@gmail.com>
2024-03-26 11:42:31 +0100

    fix(api): 2064 - filtering youngs on status inside cursor.

--- api/src/crons/parentRevalidateRI.js
+++ api/src/crons/parentRevalidateRI.js
@@ -8,7 +8,7 @@ const { APP_URL } = require("../config");
 
 exports.handler = async () => {
   try {
-    const { VALIDATED, WAITING_LIST, WAITING_VALIDATION, WAITING_CORRECTION, REINSCRIPTION, IN_PROGRESS } = YOUNG_STATUS;
+    const { ABANDONED, WITHDRAWN, REFUSED, NOT_ELIGIBLE } = YOUNG_STATUS;
     let countNotice = 0;
     const newRiDate = new Date(REGLEMENT_INTERIEUR_VERSION);
     const cohortCollection = await CohortModel.find({
@@ -17,22 +17,20 @@ exports.handler = async () => {
     const cohorts = cohortCollection.map(({ name }) => name);
     const cursor = await YoungModel.find({
       cohort: cohorts,
+      status: { $nin: [ABANDONED, WITHDRAWN, REFUSED, NOT_ELIGIBLE] },
       parent1ValidationDate: { $lt: newRiDate },
     }).cursor();
 
     await cursor.eachAsync(async function (young) {
-      const { status } = young;
-      if ([VALIDATED, WAITING_LIST, WAITING_VALIDATION, WAITING_CORRECTION, REINSCRIPTION, IN_PROGRESS].includes(status)) {
-        countNotice++;
-        await sendTemplate(SENDINBLUE_TEMPLATES.parent.PARENT1_REVALIDATE_RI, {
-          emailTo: [{ name: `${young.parent1FirstName} ${young.parent1LastName}`, email: young.parent1Email }],
-          params: {
-            cta: `${APP_URL}/representants-legaux/ri-consentement?token=${young.parent1Inscription2023Token}`,
-            youngFirstName: young.firstName,
-            youngName: young.lastName,
-          },
-        });
-      }
+      countNotice++;
+      await sendTemplate(SENDINBLUE_TEMPLATES.parent.PARENT1_REVALIDATE_RI, {
+        emailTo: [{ name: `${young.parent1FirstName} ${young.parent1LastName}`, email: young.parent1Email }],
+        params: {
+          cta: `${APP_URL}/representants-legaux/ri-consentement?token=${young.parent1Inscription2023Token}`,
+          youngFirstName: young.firstName,
+          youngName: young.lastName,
+        },
+      });
     });
     slack.success({ title: "1 week notice pending parent RI validation", text: `${countNotice} parent has been noticed !` });
   } catch (e) {
