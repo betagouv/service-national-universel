e7dff7010ccfae2a67c3efb6d4086f441c20839e
pitiscarf <dev@rthebaud.fr>
2024-03-27 10:54:32 +0100

    instantiate browser at app boot

--- pdf/index.js
+++ pdf/index.js
@@ -1,33 +1,44 @@
-require("dotenv").config({ path: "./.env" });
+(async function () {
+  require("dotenv").config({ path: "./.env" });
 
-const express = require("express");
-const puppeteer = require("puppeteer");
-const bodyParser = require("body-parser");
+  const express = require("express");
+  const puppeteer = require("puppeteer");
+  const bodyParser = require("body-parser");
 
-const { initSentry, capture } = require("./sentry");
+  const { initSentry, capture } = require("./sentry");
 
-require("events").EventEmitter.defaultMaxListeners = 30; // Fix warning node
+  require("events").EventEmitter.defaultMaxListeners = 30; // Fix warning node
 
-const fs = require("fs");
+  const fs = require("fs");
 
-const { PORT: port, GENERATE_LOCALLY } = require("./config.js");
+  const { PORT: port, GENERATE_LOCALLY } = require("./config.js");
 
-const app = express();
+  const initBrowser = async () => {
+    try {
+      return await puppeteer.launch({ headless: "new" });
+    } catch (error) {
+      capture(error);
+      throw error;
+    }
+  };
 
-const registerSentryErrorHandler = initSentry(app);
+  const browser = await initBrowser();
 
-app.use(bodyParser.json());
+  const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+  const registerSentryErrorHandler = initSentry(app);
 
-const DEFAULT_OPTIONS = {
-  format: "A4",
-  margin: 0,
-};
+  app.use(bodyParser.json());
 
-const renderFromHtml = async (html, options) => {
-  try {
-    const { browser, page } = await getBrowserAndPage(options);
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  const DEFAULT_OPTIONS = {
+    format: "A4",
+    margin: 0,
+  };
+
+  const renderFromHtml = async (html, options) => {
+    const page = await getPage(options);
     await page.setContent(html, options?.navigation ?? {});
     const pdfOptions = options ?? {};
     const buffer = await page.pdf({
@@ -35,65 +46,57 @@ const renderFromHtml = async (html, options) => {
       ...pdfOptions,
     });
 
-    await browser.close();
+    await page.close();
 
     return buffer;
-  } catch (error) {
-    console.log(error);
-    capture(error);
-  }
-};
+  };
 
-const getBrowserAndPage = async (options) => {
-  try {
-    const browser = await puppeteer.launch(
-      options?.launch ?? { headless: "new" }
-    );
+  const getPage = async (options) => {
     const page = await browser.newPage();
 
     if (options?.emulateMedia) {
       await page.emulateMediaType(options.emulateMedia);
     }
 
-    return { browser, page };
-  } catch (error) {
-    console.log(error);
-    capture(error);
-  }
-};
+    return page;
+  };
 
-app.use(express.static(__dirname + "/public"));
+  app.use(express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
 
-app.post("/render", async (req, res) => {
-  try {
-    const random = Math.random();
-    console.time("RENDERING " + random);
-    const buffer = await renderFromHtml(req.body.html, req.body.options || {});
-    if (!buffer)
-      throw new Error("No buffer returned : " + JSON.stringify(req.body));
-    if (GENERATE_LOCALLY)
-      fs.writeFileSync(
-        `generated/${new Date().toISOString()}_test.pdf`,
-        buffer
+  app.post("/render", async (req, res) => {
+    try {
+      const random = Math.random();
+      console.time("RENDERING " + random);
+      const buffer = await renderFromHtml(
+        req.body.html,
+        req.body.options || {}
       );
-    res.contentType("application/pdf");
-    res.setHeader("Content-Dispositon", 'inline; filename="test.pdf"');
-    res.set("Cache-Control", "public, max-age=1");
-    res.send(buffer);
-    console.timeEnd("RENDERING " + random);
-  } catch (error) {
-    console.log(error);
-    capture(error);
-    res.status(500).send({ ok: false, error });
-  }
-});
+      if (!buffer)
+        throw new Error("No buffer returned : " + JSON.stringify(req.body));
+      if (GENERATE_LOCALLY)
+        fs.writeFileSync(
+          `generated/${new Date().toISOString()}_test.pdf`,
+          buffer
+        );
+      res.contentType("application/pdf");
+      res.setHeader("Content-Dispositon", 'inline; filename="test.pdf"');
+      res.set("Cache-Control", "public, max-age=1");
+      res.send(buffer);
+      console.timeEnd("RENDERING " + random);
+    } catch (error) {
+      console.log(error);
+      capture(error);
+      res.status(500).send({ ok: false, error });
+    }
+  });
 
-registerSentryErrorHandler();
+  registerSentryErrorHandler();
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+})();
