b70ce1574d947657a648be91869bdaec321fc18d
Marin <kalasasta@gmail.com>
2024-03-28 12:53:13 +0100

    feat(lib): Made AddressSearch Dsfr compliant.

--- app/src/components/dsfr/forms/AddressForm/AddressSearch.jsx
+++ app/src/components/dsfr/forms/AddressForm/AddressSearch.jsx
@@ -4,6 +4,7 @@ import { RiSearchLine } from "react-icons/ri";
 import ErrorMessage from "@/components/dsfr/forms/ErrorMessage";
 import useAddress from "@/services/useAddress";
 import { useDebounce } from "@uidotdev/usehooks";
+import { Input } from "@snu/ds/dsfr";
 
 export default function AddressSearch({ updateData, label, error }) {
   const [query, setQuery] = useState("");
@@ -41,30 +42,26 @@ export default function AddressSearch({ updateData, label, error }) {
 
   return (
     <div ref={dropdownRef}>
-      <label className="flex flex-col gap-1">
-        {label}
-        <span className="text-[#666666] text-xs mb-1">Si l'adresse est introuvable, sélectionnez uniquement une commune ou un code postal.</span>
-        <div className="relative">
-          <input type="text" value={query} onChange={handleChangeQuery} className="w-[100%] border-b-2 border-gray-800 bg-[#EEEEEE] rounded-tl rounded-tr px-3 py-2 pr-5" />
-          <span className="material-icons absolute right-5 mt-[12px] text-lg">
-            <RiSearchLine />
-          </span>
-        </div>
-      </label>
+      <div className="relative">
+        <Input label={label} hintText="Si l'adresse est introuvable, sélectionnez uniquement une commune ou un code postal." value={query} onChange={handleChangeQuery} />
+        <span className="material-icons absolute right-5 mt-[12px] text-lg">
+          <RiSearchLine />
+        </span>
+      </div>
 
       <div className="relative">
         {query?.trim().length > 2 && (
-          <div className="bg-white border flex flex-col absolute z-10 -top-1 w-full shadow">
+          <div className="bg-white border flex flex-col absolute z-10 -top-6 w-full shadow">
             {isPending ? (
-              <p className="animate-pulse p-3 text-gray-800 text-center">Chargement</p>
+              <span className="animate-pulse p-3 text-gray-800 text-center">Chargement</span>
             ) : isError ? (
-              <p className="p-3 text-red-500 text-center">Erreur lors de la recherche</p>
+              <span className="p-3 text-red-500 text-center">Erreur lors de la recherche</span>
             ) : results.length ? (
               <AddressDropdown options={results} handleSelect={handleSelect} />
             ) : (
-              <p className="p-3 text-gray-800 text-center">
+              <span className="p-3 text-gray-800 text-center">
                 L'adresse ne s'affiche pas ? Renseignez une <strong>commune</strong> ou un <strong>code postal</strong>.
-              </p>
+              </span>
             )}
           </div>
         )}
