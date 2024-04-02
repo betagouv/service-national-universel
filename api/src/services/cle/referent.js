877f2d475551cda7c1badfc147376e1ad98b1af0
C2Chandelier <charlelie.chandelier@epitech.eu>
2024-04-02 15:40:00 +0200

    up

--- api/src/services/cle/referent.js
+++ api/src/services/cle/referent.js
@@ -82,7 +82,7 @@ const ACADEMIQUE_DOMAINS = [
 ];
 
 const validateEmailAcademique = (email) => {
-  if (process.env.STAGING === "true" || process.env.TESTING === "true" || process.env.NODE_ENV === "test") return true;
+  if (["development", "staging"].includes(config.ENVIRONMENT)) return true;
   const domain = email.split("@")[1];
   if (!domain) return false;
   return ACADEMIQUE_DOMAINS.includes(domain);
