1681f39c63350d5b2f11eb749219b556c1408685
Marin <kalasasta@gmail.com>
2024-04-09 18:17:48 +0200

    feat(app): 2138 - Fixed double underline in "reglement interieur"

--- app/src/scenes/inscription2023/steps/stepConsentements.jsx
+++ app/src/scenes/inscription2023/steps/stepConsentements.jsx
@@ -89,11 +89,7 @@ export default function StepConsentements() {
                 </>
               )}
               et m&apos;engage à en respecter le{" "}
-              <a
-                href="https://cni-bucket-prod.cellar-c2.services.clever-cloud.com/file/SNU-reglement-interieur-2024.pdf"
-                target="_blank"
-                rel="noreferrer"
-                className="underline hover:underline">
+              <a href="https://cni-bucket-prod.cellar-c2.services.clever-cloud.com/file/SNU-reglement-interieur-2024.pdf" target="_blank" rel="noreferrer">
                 règlement intérieur
               </a>
               .
