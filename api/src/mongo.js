8a5255a1ce31c6325b23eb58869a13710ff12a88
Michael Garcia <mika_garcia21@hotmail.com>
2024-04-18 16:34:17 +0200

    bug ssl mongo

--- api/src/mongo.js
+++ api/src/mongo.js
@@ -18,7 +18,7 @@ async function initDB() {
     maxPoolSize: 30,
     minPoolSize: 5,
     waitQueueTimeoutMS: 30_000,
-    tls: true, // Enable TLS
+    // tls: true, // Enable TLS
   };
 
   if (ENVIRONMENT === "production") {
