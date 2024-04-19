d0352956d83521f0199199d87503af82103715ae
C2Chandelier <charlelie.chandelier@epitech.eu>
2024-04-19 13:41:15 +0200

    up

--- admin/src/scenes/dsnj-export/components/ExportBox.jsx
+++ admin/src/scenes/dsnj-export/components/ExportBox.jsx
@@ -8,9 +8,62 @@ import { Button } from "@snu/ds/admin";
 import { PlainButton } from "../../plan-transport/components/Buttons";
 import Download from "@/assets/icons/Download";
 import Pencil from "@/assets/icons/Pencil";
+import Loader from "@/components/Loader";
 
 import DatePicker from "./DatePicker";
 import { ROLES } from "snu-lib";
+
+export default function ExportBox({ title, availableFrom, availableUntil, setModalConfirm, onDownload, isDownloading = false }) {
+  const user = useSelector((state) => state.Auth.user);
+  const now = dayjs();
+  const exportAvailableFrom = dayjs(availableFrom);
+  const exportAvailableUntil = dayjs(availableUntil);
+  const isExportAvailable =
+    now.isAfter(exportAvailableFrom) || (now.isSame(exportAvailableFrom, "day") && now.isBefore(exportAvailableUntil)) || now.isSame(exportAvailableUntil, "day");
+
+  const generateText = () => {
+    switch (true) {
+      case now.isBefore(exportAvailableFrom):
+        return (
+          <p className="text-sm leading-5 font-normal text-gray-500">
+            Disponible à partir du : <span className="text-sm leading-5 font-bold text-gray-900">{exportAvailableFrom.format("YYYY-MM-DD")}</span>
+          </p>
+        );
+      case now.isAfter(exportAvailableFrom) || (now.isSame(exportAvailableFrom, "day") && now.isBefore(exportAvailableUntil)) || now.isSame(exportAvailableUntil, "day"):
+        return (
+          <div className="text-sm leading-5 font-normal text-gray-500 flex flex-col gap-2">
+            <p>
+              Générée le : <span className="text-sm leading-5 font-bold text-gray-900">{exportAvailableFrom.format("YYYY-MM-DD")}</span>
+            </p>
+            <p>
+              Disponible jusqu'au : <span className="text-sm leading-5 font-bold text-gray-900">{exportAvailableUntil.format("YYYY-MM-DD")}</span>
+            </p>
+          </div>
+        );
+      case now.isAfter(exportAvailableUntil):
+        return (
+          <p className="text-sm leading-5 font-normal text-gray-500">
+            Indisponible depuis le : <span className="text-sm leading-5 font-bold text-gray-900">{exportAvailableUntil.format("YYYY-MM-DD")}</span>
+          </p>
+        );
+    }
+  };
+
+  return (
+    <div className="flex flex-1 flex-col rounded-lg bg-white p-6 shadow text-gray-900">
+      <h1 className="text-lg leading-7 font-bold mb-8">{title}</h1>
+      {generateText()}
+      {user.role === ROLES.ADMIN && (
+        <Button title="Modifier la date" type="tertiary" leftIcon={<HiOutlinePencil />} className="w-full mb-4 mt-6 mx-auto" onClick={() => setModalConfirm(true)} />
+      )}
+      {isExportAvailable ? (
+        <Button title={isDownloading ? <Loader /> : "Exporter"} className="w-full mx-auto" disabled={isDownloading} onClick={onDownload} />
+      ) : (
+        <Button title="Bientôt disponible" disabled className="w-full mx-auto" />
+      )}
+    </div>
+  );
+}
 
 const BoxContent = ({ availableFrom, availableUntil, onChangeDate, onDownload, editable, isLoading }) => {
   const [isDatePickerOpen, setDatePickerOpen] = useState(false);
@@ -54,49 +107,3 @@ const BoxContent = ({ availableFrom, availableUntil, onChangeDate, onDownload, e
     </>
   );
 };
-
-export default function ExportBox({ title, availableFrom, availableUntil, onChangeDate, onDownload, editable = true, isDownloading = false }) {
-  const user = useSelector((state) => state.Auth.user);
-
-  const generateText = () => {
-    const now = dayjs();
-    const exportAvailableFrom = dayjs(availableFrom);
-    const exportAvailableUntil = dayjs(availableUntil);
-    console.log("availableFrom", availableFrom);
-    console.log("availableUntil", availableUntil);
-
-    switch (true) {
-      case now.isBefore(exportAvailableFrom):
-        return (
-          <p className="text-sm leading-5 font-normal text-gray-500">
-            Disponible à partir du : <span className="text-sm leading-5 font-bold text-gray-900">{availableFrom.format("YYYY-MM-DD")}</span>
-          </p>
-        );
-      case now.isAfter(exportAvailableFrom) && now.isBefore(exportAvailableUntil):
-        return (
-          <p className="text-sm leading-5 font-normal text-gray-500">
-            Générée le : <span className="text-sm leading-5 font-bold text-gray-900">{availableFrom.format("YYYY-MM-DD")}</span>
-            Disponible jusqu'au : <span className="text-sm leading-5 font-bold text-gray-900">{availableUntil.format("YYYY-MM-DD")}</span>
-          </p>
-        );
-      case now.isAfter(exportAvailableUntil):
-        return (
-          <p className="text-sm leading-5 font-normal text-gray-500">
-            Indisponible depuis le : <span className="text-sm leading-5 font-bold text-gray-900">{availableUntil.format("YYYY-MM-DD")}</span>
-          </p>
-        );
-    }
-  };
-
-  const formatDate = (date) => {
-    return new Date(date).toLocaleDateString("fr-FR", { day: "2-digit", month: "2-digit", year: "2-digit" });
-  };
-
-  return (
-    <div className="flex flex-1 flex-col rounded-lg bg-white p-6 shadow text-gray-900">
-      <h1 className="text-lg leading-7 font-bold ">{title}</h1>
-      {generateText()}
-      {user.role === ROLES.ADMIN && <Button title="Modifier la date" type="tertiary" leftIcon={<HiOutlinePencil />} />}
-    </div>
-  );
-}
--- admin/src/scenes/dsnj-export/index.jsx
+++ admin/src/scenes/dsnj-export/index.jsx
@@ -3,13 +3,13 @@ import { toastr } from "react-redux-toastr";
 import * as FileSaver from "file-saver";
 import { useSelector } from "react-redux";
 import { FaMagnifyingGlass } from "react-icons/fa6";
-import { HiOutlineChartSquareBar } from "react-icons/hi";
+import { HiOutlineChartSquareBar, HiOutlineCalendar } from "react-icons/hi";
 
 import { ROLES, translate } from "snu-lib";
-import dayjs from "@/utils/dayjs.utils";
+import dayjs from "dayjs";
 import api from "@/services/api";
 import useDocumentTitle from "@/hooks/useDocumentTitle";
-import { Select, Page, Header } from "@snu/ds/admin";
+import { Select, Page, Header, ModalConfirmation } from "@snu/ds/admin";
 import { NewGetCohortSelectOptions } from "@/services/cohort.service";
 
 import ExportBox from "./components/ExportBox";
@@ -20,22 +20,24 @@ const DSNJExport = () => {
   const user = useSelector((state) => state.Auth.user);
   const cohortList = useSelector((state) => state.Cohorts);
   const [currentCohort, setCurrentCohort] = useState(cohortList[0]);
-  const cohortWithExportDates = addFieldExportAvailableUntil(cohortList);
+  const cohortAddField = addFieldExportAvailableUntil(currentCohort);
   const [cohorts, setCohorts] = useState([]);
   const [isLDownloadingByKey, setDownloadingByKey] = useState({});
   const [isSelectMenuOpen, setIsSelectMenuOpen] = useState(false);
   const cohortOptions = NewGetCohortSelectOptions(cohortList);
+  const [isModalConfirmOpen, setIsModalConfirmOpen] = useState(false);
   useDocumentTitle("Export DSNJ");
+
+  console.log(currentCohort);
 
   function addFieldExportAvailableUntil(cohort) {
     // export available until 1 month after the cohort
-    const exportsAvailableUntil = new Date(cohort.dateEnd);
-    exportsAvailableUntil.setMonth(exportsAvailableUntil.getMonth() + 1);
+    const exportsAvailableUntil = dayjs(cohort.dateEnd).add(1, "month").toISOString();
     return {
       ...cohort,
       dsnjExportDates: {
         ...cohort.dsnjExportDates,
-        exportsAvailableUntil: exportsAvailableUntil.toISOString(),
+        exportsAvailableUntil: exportsAvailableUntil,
       },
     };
   }
@@ -52,8 +54,9 @@ const DSNJExport = () => {
     setCohorts(updatedCohorts);
   };
 
-  const download = (key) => async () => {
-    try {
+  const handleDownload = (key) => async () => {
+    console.log("test");
+    /*     try {
       setDownloadingByKey({ ...isLDownloadingByKey, [key]: true });
       const file = await api.get(`/cohort/${currentCohort._id}/export/${key}`);
       FileSaver.saveAs(new Blob([new Uint8Array(file.data.data)], { type: file.mimeType }), file.fileName);
@@ -61,7 +64,11 @@ const DSNJExport = () => {
       toastr.error("Oups, une erreur est survenue pendant le téléchagement", "");
     } finally {
       setDownloadingByKey({ ...isLDownloadingByKey, [key]: false });
-    }
+    } */
+  };
+
+  const handleConfirm = () => {
+    setIsModalConfirmOpen(false);
   };
 
   return (
@@ -80,6 +87,7 @@ const DSNJExport = () => {
                 maxMenuHeight={520}
                 className="w-[500px]"
                 disabled={user.role === ROLES.HEAD_CENTER}
+                closeMenuOnSelect={true}
                 onMenuOpen={() => setIsSelectMenuOpen(true)}
                 onMenuClose={() => setIsSelectMenuOpen(false)}
                 onChange={(e) => {
@@ -90,35 +98,62 @@ const DSNJExport = () => {
             </>
           }
         />
+
         <div className="flex gap-4">
           <ExportBox
             editable={user.role === ROLES.ADMIN}
             title="Liste des centres"
-            availableFrom={cohortWithExportDates?.dsnjExportDates[exportDateKeys[0]]}
-            availableUntil={cohortWithExportDates?.exportsAvailableUntil}
+            availableFrom={cohortAddField?.dsnjExportDates[exportDateKeys[0]]}
+            availableUntil={cohortAddField?.dsnjExportDates?.exportsAvailableUntil}
+            setModalConfirm={setIsModalConfirmOpen}
             onChangeDate={updateExportDate(exportDateKeys[0])}
-            onDownload={download(exportDateKeys[0])}
+            onDownload={handleDownload(exportDateKeys[0])}
             isDownloading={!!isLDownloadingByKey[exportDateKeys[0]]}
           />
           <ExportBox
             editable={user.role === ROLES.ADMIN}
             title="Liste des volontaires affectés et sur liste complémentaire"
-            availableFrom={cohortWithExportDates?.dsnjExportDates[exportDateKeys[1]]}
-            availableUntil={cohortWithExportDates?.exportsAvailableUntil}
+            availableFrom={cohortAddField?.dsnjExportDates[exportDateKeys[1]]}
+            availableUntil={cohortAddField?.dsnjExportDates?.exportsAvailableUntil}
+            setModalConfirm={setIsModalConfirmOpen}
             onChangeDate={updateExportDate(exportDateKeys[1])}
-            onDownload={download(exportDateKeys[1])}
+            onDownload={handleDownload(exportDateKeys[1])}
             isDownloading={!!isLDownloadingByKey[exportDateKeys[1]]}
           />
           <ExportBox
             editable={user.role === ROLES.ADMIN}
             title="Liste des volontaires après le séjour"
-            availableFrom={cohortWithExportDates?.dsnjExportDates[exportDateKeys[2]]}
-            availableUntil={cohortWithExportDates?.exportsAvailableUntil}
+            availableFrom={cohortAddField?.dsnjExportDates[exportDateKeys[2]]}
+            availableUntil={cohortAddField?.dsnjExportDates?.exportsAvailableUntil}
+            setModalConfirm={setIsModalConfirmOpen}
             onChangeDate={updateExportDate(exportDateKeys[2])}
-            onDownload={download(exportDateKeys[2])}
+            onDownload={handleDownload(exportDateKeys[2])}
             isDownloading={!!isLDownloadingByKey[exportDateKeys[2]]}
           />
         </div>
+        <ModalConfirmation
+          isOpen={isModalConfirmOpen}
+          onClose={() => {
+            setIsModalConfirmOpen(false);
+          }}
+          className="md:max-w-[700px]"
+          icon={
+            <div className="bg-gray-100 rounded-full p-2.5">
+              <HiOutlineCalendar size={24} />
+            </div>
+          }
+          title="Modification de la date de mise à disponibilité de l’export DSNJ"
+          text={
+            <p className="text-red-600 text-left mt-4">
+              <span className="font-bold">Attention : </span>un nouvel export sera généré le <span className="font-bold">12/06/2024</span> avec les informations mises à jour.
+              L’ancien export ne sera plus téléchargeable par la DSNJ : <span className="underline">pensez à les prévenir qu’ils doivent à nouveau télécharger cet export !</span>
+            </p>
+          }
+          actions={[
+            { title: "Annuler", isCancel: true },
+            { title: "Valider", onclick: () => handleConfirm() },
+          ]}
+        />
       </Page>
     </>
   );
