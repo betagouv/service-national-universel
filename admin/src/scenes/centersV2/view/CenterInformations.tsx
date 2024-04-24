3c11f3a05ba177a5b39ccee433ed94bd55825a72
achorein <anselme@chorein.fr>
2024-04-24 16:16:08 +0200

    cleanup

--- admin/src/scenes/centersV2/view/CenterInformations.tsx
+++ admin/src/scenes/centersV2/view/CenterInformations.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { BiHandicap } from "react-icons/bi";
+// @ts-expect-error lib non ts
 import { useDebounce } from "@uidotdev/usehooks";
 
 import ReactTooltip from "react-tooltip";
