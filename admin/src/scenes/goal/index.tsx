63cf1a4e522b5d482a14b96d7eb94bd899897d20
Anselme Chorein <achorein@users.noreply.github.com>
2024-04-25 09:25:22 +0200

    feat(admin): 2253 - Objectifs - Mise à jour sélecteur de cohort (#3800)

--- admin/src/scenes/goal/index.tsx
+++ admin/src/scenes/goal/index.tsx
@@ -1,47 +1,41 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { toastr } from "react-redux-toastr";
 import { Col, Row } from "reactstrap";
 import styled from "styled-components";
-import useDocumentTitle from "../../hooks/useDocumentTitle";
 
-import CrossSVG from "../../assets/cross.svg";
-import PlusSVG from "../../assets/plus.svg";
-import Loader from "../../components/Loader";
-import LoadingButton from "../../components/buttons/LoadingButton";
-import api from "../../services/api";
-import { department2region, departmentList, region2department } from "../../utils";
+import useDocumentTitle from "@/hooks/useDocumentTitle";
+import api from "@/services/api";
+import { department2region, departmentList, region2department } from "@/utils";
 
-import { getCohortSelectOptions } from "@/services/cohort.service";
-import { useSelector } from "react-redux";
-import Select from "../plan-transport/components/Select";
+import Loader from "@/components/Loader";
+import LoadingButton from "@/components/buttons/LoadingButton";
+import SelectCohort from "@/components/cohorts/SelectCohort";
+
+import ToggleBloc from "./ToggleBloc";
+
 export default function Goal() {
   useDocumentTitle("Objectifs");
 
-  const [inscriptionGoals, setInscriptionGoals] = useState();
+  const [inscriptionGoals, setInscriptionGoals] = useState<{ department: string; region: string; max: null | number }[]>();
   const [loading, setLoading] = useState(false);
   const [blocsOpened, setBlocsOpened] = useState([]);
   const [cohort, setCohort] = useState(null);
-  const [cohortList, setCohortList] = useState([]);
-  const cohorts = useSelector((state) => state.Cohorts);
 
   useEffect(() => {
-    //regex to match cohort witch not include CLE
-    const cohortList = getCohortSelectOptions(cohorts).filter((c) => !c.label.includes("CLE"));
-    setCohortList(cohortList);
     if (!cohort) setCohort("Février 2024 - C");
   }, []);
 
   const getInscriptionGoals = async () => {
     if (!cohort) return;
     const { data, ok } = await api.get(`/inscription-goal/${cohort}`);
-    if (!ok) return toastr.error("Impossible de charger les objectifs d'inscription");
+    if (!ok) return toastr.error("Impossible de charger les objectifs d'inscription", "");
     setInscriptionGoals(departmentList.map((d) => data.find((e) => e.department === d) || { department: d, region: department2region[d], max: null }));
   };
   useEffect(() => {
     getInscriptionGoals();
   }, [cohort]);
 
-  if (!inscriptionGoals || !cohortList.length || !cohort) return <Loader />;
+  if (!inscriptionGoals || !cohort) return <Loader />;
 
   return (
     <>
@@ -59,11 +53,11 @@ export default function Goal() {
             }}>
             Enregistrer
           </LoadingButton>
-          <Select
-            options={cohortList}
-            value={cohort}
-            onChange={(e) => {
-              setCohort(e);
+          <SelectCohort
+            cohort={cohort}
+            filterFn={(c) => !c.name.includes("CLE")}
+            onChange={(cohortName) => {
+              setCohort(cohortName);
             }}
           />
         </div>
@@ -101,7 +95,7 @@ export default function Goal() {
                             <Col md={8}>
                               <input
                                 onChange={(e) => {
-                                  const val = e.target.value && !isNaN(Number(e.target.value)) ? Number(e.target.value) : "";
+                                  const val = e.target.value && !isNaN(Number(e.target.value)) ? Number(e.target.value) : null;
                                   setInscriptionGoals(inscriptionGoals.map((ig) => (ig.department === department ? { ...ig, max: val } : ig)));
                                 }}
                                 value={inscriptionGoals.find((ig) => ig.department === department)?.max || ""}
@@ -131,55 +125,6 @@ const Departement = styled.div`
   color: #666;
 `;
 
-const ToggleBloc = ({ children, title, borderBottom, borderRight, borderLeft, disabled, onClick, visible }) => {
-  return (
-    <Row
-      style={{
-        borderBottom: borderBottom ? "2px solid #f4f5f7" : 0,
-        borderRight: borderRight ? "2px solid #f4f5f7" : 0,
-        borderLeft: borderLeft ? "2px solid #f4f5f7" : 0,
-        backgroundColor: disabled ? "#f9f9f9" : "transparent",
-      }}>
-      <Wrapper>
-        <div onClick={onClick} style={{ display: "flex", alignItems: "center", justifyContent: "space-between", cursor: "pointer" }}>
-          <Legend>{title}</Legend>
-          <div>
-            <Icon src={visible ? CrossSVG : PlusSVG} />
-          </div>
-        </div>
-        <div style={{ display: visible ? "block" : "none" }}>{children}</div>
-      </Wrapper>
-    </Row>
-  );
-};
-
-const Wrapper = styled.div`
-  padding: 2rem;
-  margin-bottom: 1rem;
-  border-radius: 5px;
-  filter: drop-shadow(0px 2px 4px rgba(0, 0, 0, 0.05));
-  background-color: white;
-  width: 100%;
-  .detail {
-    display: flex;
-    align-items: flex-start;
-    font-size: 14px;
-    text-align: left;
-    margin-top: 1rem;
-    &-title {
-      color: #798399;
-    }
-    &-text {
-      color: rgba(26, 32, 44);
-    }
-  }
-  p {
-    font-size: 13px;
-    color: #798399;
-    margin-top: 1rem;
-  }
-`;
-
 const Title = styled.div`
   color: rgb(38, 42, 62);
   font-weight: 700;
@@ -192,16 +137,4 @@ const Header = styled.div`
   margin: 25px 0;
   align-items: flex-start;
   /* justify-content: space-between; */
-`;
-
-const Legend = styled.div`
-  color: rgb(38, 42, 62);
-  font-size: 1.3rem;
-  font-weight: 500;
-`;
-
-const Icon = styled.img`
-  height: 18px;
-  font-size: 18px;
-  cursor: pointer;
 `;
