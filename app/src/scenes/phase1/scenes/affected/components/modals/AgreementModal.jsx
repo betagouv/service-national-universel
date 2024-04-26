bb5e5ab0c79674c04aba2529dfdfd68249fa8f27
Nicolas Bretécher <nicolas.bretecher@selego.co>
2024-04-26 10:40:11 +0200

    feat(app): 2283 - CLE - Add confirmation step for affected users (#3806)

--- app/src/scenes/phase1/scenes/affected/components/modals/AgreementModal.jsx
+++ app/src/scenes/phase1/scenes/affected/components/modals/AgreementModal.jsx
@@ -6,7 +6,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { toastr } from "react-redux-toastr";
 import { Link } from "react-router-dom";
 import { Modal } from "reactstrap";
-import { transportDatesToString } from "snu-lib";
+import { isCle, transportDatesToString } from "snu-lib";
 import Close from "@/components/layout/navbar/assets/Close";
 import { ModalContainer } from "@/components/modals/Modal";
 
@@ -29,7 +29,7 @@ export function AgreementModal({ isOpen, setIsOpen, departureDate, returnDate })
     }
   };
   return (
-    <Modal centered isOpen={isOpen} toggle={() => setIsOpen(false)} size="xl">
+    <Modal centered isOpen={isOpen} toggle={() => setIsOpen(false)} size="lg">
       <ModalContainer>
         <button onClick={() => setIsOpen(false)}>
           <Close className="close-icon" height={10} width={10} />
@@ -38,7 +38,7 @@ export function AgreementModal({ isOpen, setIsOpen, departureDate, returnDate })
           <h1 className="pb-1 text-center text-xl text-gray-900">Confirmez votre participation au séjour</h1>
           <p className="pb-4 text-center text-base text-gray-500">Vous devez confirmer votre participation au séjour avant votre départ.</p>
           <div className="mb-2 flex flex-shrink flex-col flex-wrap justify-center gap-6 p-2 md:justify-start lg:flex-row">
-            <div className="flex flex-col rounded-2xl border-[1px] border-blue-600 py-5 px-5 shadow-sm md:max-w-screen-1/2 lg:w-1/3">
+            <div className="flex flex-col rounded-2xl border-[1px] border-blue-600 py-5 px-5 shadow-sm w-80">
               <h1 className="pb-4 text-xl font-bold leading-7">Je confirme</h1>
               <div className="text-sm text-gray-600">
                 <p className="pb-2">
@@ -55,18 +55,20 @@ export function AgreementModal({ isOpen, setIsOpen, departureDate, returnDate })
                 Valider
               </button>
             </div>
-            <div className="flex flex-col rounded-2xl border-[1px] border-gray-100 py-5 px-5 shadow-sm md:max-w-screen-1/2 lg:w-1/3">
-              <h1 className="pb-4 text-xl font-bold leading-7">J&apos;ai changé d&apos;avis</h1>
-              <p className="pb-3 text-sm text-gray-600">Les dates ne me conviennent plus ({transportDatesToString(departureDate, returnDate)})</p>
-              <Link to="/changer-de-sejour" className="whitespace-nowrap pb-4 text-sm text-blue-600 hover:underline hover:underline-offset-2">
-                Changer de séjour &gt;
-              </Link>
-              <p className="pb-3 text-sm text-gray-600">Je ne souhaite plus participer au SNU</p>
-              <Link to="account/general?desistement=1" className="whitespace-nowrap text-sm text-blue-600 hover:underline hover:underline-offset-2">
-                Me désister &gt;
-              </Link>
-            </div>
-          </div>{" "}
+            {!isCle(young) && (
+              <div className="flex flex-col rounded-2xl border-[1px] border-gray-100 py-5 px-5 shadow-sm w-80">
+                <h1 className="pb-4 text-xl font-bold leading-7">J&apos;ai changé d&apos;avis</h1>
+                <p className="pb-3 text-sm text-gray-600">Les dates ne me conviennent plus ({transportDatesToString(departureDate, returnDate)})</p>
+                <Link to="/changer-de-sejour" className="whitespace-nowrap pb-4 text-sm text-blue-600 hover:underline hover:underline-offset-2">
+                  Changer de séjour &gt;
+                </Link>
+                <p className="pb-3 text-sm text-gray-600">Je ne souhaite plus participer au SNU</p>
+                <Link to="account/general?desistement=1" className="whitespace-nowrap text-sm text-blue-600 hover:underline hover:underline-offset-2">
+                  Me désister &gt;
+                </Link>
+              </div>
+            )}
+          </div>
         </div>
       </ModalContainer>
     </Modal>
