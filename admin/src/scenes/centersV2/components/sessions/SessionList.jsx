2bdba99cfeda80dcd3e80fc761cdf8ec69375cbe
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2024-05-02 13:30:24 +0200

    fix specific dates

--- admin/src/scenes/centersV2/components/sessions/SessionList.jsx
+++ admin/src/scenes/centersV2/components/sessions/SessionList.jsx
@@ -138,6 +138,25 @@ export default function SessionList({ center, sessions, user, focusedSession, on
     }
   };
 
+  const getSpecificDate = () => {
+    if (editingBottom && editInfoSession?.hasSpecificDate) {
+      return {
+        from: editInfoSession?.dateStart || focusedCohortData.dateStart,
+        to: editInfoSession?.dateEnd || focusedCohortData.dateEnd,
+      };
+    }
+    if (!editingBottom && focusedSession?.hasSpecificDate) {
+      return {
+        from: focusedSession.dateStart,
+        to: focusedSession.dateEnd,
+      };
+    }
+    return {
+      from: undefined,
+      to: undefined,
+    };
+  };
+
   if (!sessions || sessions.length === 0) return null;
   return (
     <div className="flex flex-col gap-4 mx-8">
@@ -250,12 +269,9 @@ export default function SessionList({ center, sessions, user, focusedSession, on
                           ) : null
                         }
                         readOnly={!editingBottom || !canPutSpecificDateOnSessionPhase1(user)}
-                        value={editInfoSession.hasSpecificDate}
+                        value={editingBottom ? editInfoSession.hasSpecificDate : focusedSession.hasSpecificDate}
                         onChange={() => setEditInfoSession({ ...editInfoSession, hasSpecificDate: !editInfoSession.hasSpecificDate })}
-                        range={{
-                          from: editInfoSession?.dateStart || undefined,
-                          to: editInfoSession?.dateEnd || undefined,
-                        }}
+                        range={getSpecificDate()}
                         onChangeRange={(range) => {
                           setEditInfoSession({
                             ...editInfoSession,
