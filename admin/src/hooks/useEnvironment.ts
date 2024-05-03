9a6d4aab5b4d1ede4f9f112f75aabf891d2d60a1
achorein <anselme@chorein.fr>
2024-05-03 09:35:43 +0200

    cleanup

--- admin/src/hooks/useEnvironment.ts
+++ admin/src/hooks/useEnvironment.ts
@@ -1,14 +1,8 @@
 const useEnvironment = () => {
   return {
-    get isProduction() {
-      return window.location.hostname.includes("snu.gouv.fr");
-    },
-    get isStaging() {
-      return window.location.hostname.includes(".ci.") || window.location.hostname.includes("scw.cloud");
-    },
-    get isDevelopment() {
-      return window.location.hostname.includes("localhost");
-    },
+    isProduction: window.location.hostname.includes("snu.gouv.fr"),
+    isStaging: window.location.hostname.includes(".ci.") || window.location.hostname.includes("scw.cloud"),
+    isDevelopment: window.location.hostname.includes("localhost"),
   };
 };
 
