96f8ba5aa091176221118ca85207ad52d32cb089
achorein <anselme@chorein.fr>
2024-05-03 12:58:37 +0200

    disable export button when expired

--- admin/src/scenes/dsnj-export/components/ExportBox.tsx
+++ admin/src/scenes/dsnj-export/components/ExportBox.tsx
@@ -23,17 +23,14 @@ export default function ExportBox({ title, availableFrom, availableUntil, onClic
   const now = dayjs().startOf("day");
   const exportAvailableFrom = availableFrom ? dayjs(availableFrom).startOf("day") : null;
   const exportAvailableUntil = dayjs(availableUntil).startOf("day");
-  const isExportAvailable = availableFrom
-    ? now.isAfter(exportAvailableFrom) || (now.isSame(exportAvailableFrom, "day") && now.isBefore(exportAvailableUntil)) || now.isSame(exportAvailableUntil, "day")
-    : false;
 
   const isBefore = now.isBefore(exportAvailableFrom);
   const isAvailable = now.isAfter(exportAvailableFrom) || (now.isSame(exportAvailableFrom, "day") && now.isBefore(exportAvailableUntil)) || now.isSame(exportAvailableUntil, "day");
   const isExpired = now.isAfter(exportAvailableUntil);
 
   return (
-    <div className="flex flex-1 flex-col rounded-xl bg-white p-6 shadow text-gray-900 h-[20em]">
-      <h1 className="flex-grow h-1/3 text-lg leading-7 font-bold">{title}</h1>
+    <div className="flex flex-1 flex-col rounded-xl bg-white p-6 shadow text-gray-900 h-[21em]">
+      <h1 className="h-20 text-lg leading-7 font-bold">{title}</h1>
       <div className="flex-grow h-1/5">
         {isBefore && (
           <p className="text-sm leading-5 font-normal text-gray-500">
@@ -56,12 +53,12 @@ export default function ExportBox({ title, availableFrom, availableUntil, onClic
           </p>
         )}
       </div>
-      <div className="flex-grow h-2/5 mt-3">
-        {user.role === ROLES.ADMIN && <Button title="Modifier la date" type="tertiary" leftIcon={<HiOutlinePencil />} className="w-full mb-4 mx-auto" onClick={onClick} />}
-        {isExportAvailable ? (
+      <div className="h-20">
+        {user.role === ROLES.ADMIN && <Button title="Modifier la date" type="tertiary" leftIcon={<HiOutlinePencil />} className="w-full mb-2 mx-auto" onClick={onClick} />}
+        {isAvailable && !isExpired ? (
           <Button title="Exporter" loading={isDownloading} className="w-full mx-auto" disabled={isDownloading} onClick={onDownload} />
         ) : (
-          <Button title="Bientôt disponible" disabled className="w-full mx-auto" />
+          <Button title={isExpired ? "Indisponible" : "Bientôt disponible"} disabled className="w-full mx-auto" />
         )}
       </div>
     </div>
