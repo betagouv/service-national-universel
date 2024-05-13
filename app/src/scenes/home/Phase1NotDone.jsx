e9656b02b32d57d3bf8a7232c582b3f978188ff2
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2024-05-13 11:29:56 +0200

    linting

--- app/src/scenes/home/Phase1NotDone.jsx
+++ app/src/scenes/home/Phase1NotDone.jsx
@@ -8,12 +8,12 @@ import Withdrawal from "../account/scenes/general/components/Withdrawal";
 import Engagement from "./components/Engagement";
 
 export default function Phase1NotDone() {
-  const young=useSelector((state) => state.Auth.young);
+  const young = useSelector((state) => state.Auth.young);
 
   return (
     <div className="flex w-full lg:w-fit flex-col-reverse lg:flex-row bg-white lg:rounded-xl lg:m-10 bg-white lg:shadow-md">
-      {wasYoungExcluded(young)? (
-        <div className="space-y-10 p-4 lg:m16">
+      {wasYoungExcluded(young) ? (
+        <div className="space-y-10 p-4 lg:m-16">
           <p className="max-w-4xl text-5xl font-medium leading-tight tracking-tight text-gray-900">
             <strong>{young.firstName}, </strong>
             votre séjour de cohésion n&apos;a pas été validé pour motif d&apos;exclusion.
@@ -25,7 +25,7 @@ export default function Phase1NotDone() {
           </p>
           <Engagement />
         </div>
-      ):(
+      ) : (
         <>
           <div className="sm:px-4 sm:pb-4 lg:px-0 lg:pb-0 lg:my-16 lg:ml-16  ">
             <p className="text-3xl lg:text-5xl font-medium leading-tight tracking-tight text-gray-800">
@@ -37,28 +37,25 @@ export default function Phase1NotDone() {
               </div>
             </div>
 
-            {!isCle(young)&&(
+            {!isCle(young) && (
               <>
                 <div className="flex w-full lg:w-fit flex-col items-stretch">
                   <button
                     className="mt-4 rounded-[10px] border-[1px] border-blue-600 bg-blue-600 py-2.5 px-3 text-sm font-medium leading-5 text-white hover:bg-white hover:!text-blue-600"
-                    onClick={() => window.location.href="/changer-de-sejour"}>
+                    onClick={() => (window.location.href = "/changer-de-sejour")}>
                     Choisir un nouveau séjour
                   </button>
                 </div>
 
-                {
-                  youngCanWithdraw(young)?
-                    <div className="flex w-full lg:w-fit flex-col items-stretch">
-                      <span className="pt-4 [&>button]:text-blue-600"  >
-                        <Withdrawal young={young} />
-                      </span>
-                    </div>
-                    :null
-                }
+                {youngCanWithdraw(young) ? (
+                  <div className="flex w-full lg:w-fit flex-col items-stretch">
+                    <span className="pt-4 [&>button]:text-blue-600">
+                      <Withdrawal young={young} />
+                    </span>
+                  </div>
+                ) : null}
               </>
             )}
-
           </div>
           <div className="hidden flex-none xl:block">
             <img className="object-" src={Img3} alt="" />
