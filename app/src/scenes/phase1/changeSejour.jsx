48d76686a75959ed843ff2b3b5d9b2da18874c5d
Guillaume <guillaume@macmax.home>
2024-05-13 15:29:24 +0200

    up

--- app/src/scenes/phase1/changeSejour.jsx
+++ app/src/scenes/phase1/changeSejour.jsx
@@ -69,10 +69,14 @@ export default function ChangeSejour() {
           toastr.error("Il n'y a pas de département pour ce jeune");
           throw new Error("Department is undefined");
         }
+        if (newSejour === "à venir") {
+          setmodalConfirmControlOk(true);
+          return;
+        }
         const res = await api.get(`/inscription-goal/${newSejour}/department/${young.department}/reached`);
         if (!res.ok) throw new Error(res);
         const isGoalReached = res.data;
-        if (!isGoalReached || newSejour === "à venir") {
+        if (!isGoalReached) {
           setmodalConfirmControlOk(true);
         } else {
           setmodalConfirmGoalReached(true);
