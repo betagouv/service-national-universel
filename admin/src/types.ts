b585eab10b150156d178b6f8e1f450fe0d6218fb
Nicolas Bretécher <nicolas.bretecher@selego.co>
2024-05-16 11:22:44 +0200

    feat(api, admin): 2227 - Ajout de l'email académique lors de la création de la session (#3857)

--- admin/src/types.ts
+++ admin/src/types.ts
@@ -9,6 +9,42 @@ export type User = {
   subRole?: string;
 };
 
+export type Center = {
+  academy: string;
+  address: string;
+  addressVerified: string;
+  centerDesignation: string;
+  city: string;
+  code: string;
+  code2022: string;
+  cohorts: string[];
+  complement: string;
+  createdAt: string;
+  department: string;
+  domain: string;
+  name: string;
+  placesTotal: number;
+  pmr: string;
+  region: string;
+  sessionStatus: string[];
+  typology: string;
+  updatedAt: string;
+  waitingList: string[];
+  zip: string;
+  __v: number;
+  _id: string;
+};
+
+export type Session = {
+  _id: string;
+  cohort: string;
+  placesTotal: number;
+  placesLeft: number;
+  dateStart: string | null;
+  dateEnd: string | null;
+  sanitaryContactEmail: string;
+};
+
 export type Cohort = {
   name: string;
   type: string;
