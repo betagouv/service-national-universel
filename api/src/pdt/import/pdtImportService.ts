814613e43d05ebfad06aaba4ad6e9638f96329ce
achorein <anselme@chorein.fr>
2024-05-16 11:13:04 +0200

    fix import

--- api/src/pdt/import/pdtImportService.ts
+++ api/src/pdt/import/pdtImportService.ts
@@ -6,7 +6,7 @@ import { PDT_IMPORT_ERRORS, departmentLookUp } from "snu-lib";
 import { CohesionCenterModel, PointDeRassemblementModel, SessionPhase1Model, CleClasseModel } from "../../models";
 import { ERRORS } from "../../utils";
 
-import { isValidBoolean, isValidDate, isValidDepartment, isValidNumber, isValidTime } from "./utils";
+import { isValidBoolean, isValidDate, isValidDepartment, isValidNumber, isValidTime } from "./pdtImportUtils";
 
 export interface PdtErrors {
   [key: string]: { line: number; error: string; extra?: string }[];
