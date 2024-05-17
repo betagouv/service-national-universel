3082bd78c88e270f5c0a9e9727adf2163d263eff
Johannbr <johannbr@gmail.com>
2024-05-17 16:28:33 +0200

    feat(admin,api): 2177 - some testing

--- packages/lib/src/plandetransport/ligneService.ts
+++ packages/lib/src/plandetransport/ligneService.ts
@@ -6,13 +6,13 @@ export const isTeamLeaderOrSupervisorEditable = (actor, cohort: CohortDto) => {
     case ROLES.ADMIN:
       return true;
     case ROLES.TRANSPORTER:
-      return cohort?.busEditionOpenForTransporter;
+      return !!cohort?.busEditionOpenForTransporter;
     case ROLES.REFERENT_REGION:
-      return cohort?.informationsConvoyage?.editionOpenForReferentRegion;
+      return !!cohort?.informationsConvoyage?.editionOpenForReferentRegion;
     case ROLES.REFERENT_DEPARTMENT:
-      return cohort?.informationsConvoyage?.editionOpenForReferentDepartment;
+      return !!cohort?.informationsConvoyage?.editionOpenForReferentDepartment;
     case ROLES.HEAD_CENTER:
-      return cohort?.informationsConvoyage?.editionOpenForHeadOfCenter;
+      return !!cohort?.informationsConvoyage?.editionOpenForHeadOfCenter;
     default:
       return false;
   }
