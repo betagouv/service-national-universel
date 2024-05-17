5d158e5529923985e99e65ea5ded6fd701da783e
Johannbr <johannbr@gmail.com>
2024-05-17 16:32:31 +0200

    feat(admin,api): 2177 - tooltip

--- admin/src/scenes/settings/InformationsConvoyage.tsx
+++ admin/src/scenes/settings/InformationsConvoyage.tsx
@@ -21,7 +21,7 @@ export const InformationsConvoyage = ({ disabled, informationsConvoyageData, han
     <div className="flex flex-col gap-3">
       <div className="flex items-center gap-2">
         <p className="text-xs  font-medium text-gray-900">Remplissage des informations de convoyage</p>
-        <MdInfoOutline data-tip data-for="remplissage_PDR" className="h-5 w-5 cursor-pointer text-gray-400" />
+        <MdInfoOutline data-tip data-for="remplissage_info_convoyage" className="h-5 w-5 cursor-pointer text-gray-400" />
         <ReactTooltip id="remplissage_info_convoyage" type="light" place="top" effect="solid" className="custom-tooltip-radius !opacity-100 !shadow-md">
           <p className=" w-[275px] list-outside !px-2 !py-1.5 text-left text-xs text-gray-600">
             Ouverture ou fermerture pour les utilisateurs de la possibilité de modifier les informations de convoyage.
