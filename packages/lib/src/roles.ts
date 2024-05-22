1586f2f1fb0feb8d46ffc34deae0e8ddab9bcd56
C2Chandelier <charlelie.chandelier@epitech.eu>
2024-05-22 12:08:19 +0200

    up

--- packages/lib/src/roles.ts
+++ packages/lib/src/roles.ts
@@ -104,12 +104,7 @@ function canInviteUser(actorRole, targetRole) {
 
 const canDeleteStructure = (actor, target) => isAdmin(actor) || referentInSameGeography(actor, target);
 
-const canDeleteYoung = (actor, target) => {
-  // un référent peut supprimer un volontaire, mais un volontaire peut se supprimer uniquement si il est HTS
-  if (actor._id.toString() === target._id.toString()) {
-    if (target.source === "CLE") return false;
-    return true;
-  }
+const canDeleteYoung = (actor) => {
   return isAdmin(actor);
 };
 
