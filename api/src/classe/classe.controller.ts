809444ec862aa8b3d6e85563c94d246eec34087d
hlecourt <hlecourt99@gmail.com>
2024-05-22 18:25:01 +0200

    Revert "fix(admin): 2286 - export classes mongo (#3896)"
    This reverts commit c2e39a4afbf35577f7016f810639aca4f230fc13.

--- api/src/classe/classe.controller.ts
+++ api/src/classe/classe.controller.ts
@@ -1,17 +1,12 @@
 import passport from "passport";
 import express, { Response } from "express";
 import Joi from "joi";
-
-import { ROLES, canDownloadYoungDocuments, YOUNG_STATUS, STATUS_CLASSE } from "snu-lib";
-
 import { capture } from "../sentry";
 import { ERRORS, isReferent } from "../utils";
-import { CleClasseModel } from "../models";
+import { canDownloadYoungDocuments } from "snu-lib";
+import { generateConvocationsByClasseId } from "./classe.service";
 
 import { UserRequest } from "../controllers/request";
-
-import { generateConvocationsByClasseId } from "./classe.service";
-import { findCohesionCentersForClasses, findPdrsForClasses, getYoungsGroupByClasses, findLigneInfoForClasses, findReferentInfoForClasses } from "./export/classeExportService";
 
 const router = express.Router();
 router.post(
@@ -39,65 +34,6 @@ router.post(
         "cache-control": "public, max-age=1",
       });
       res.send(convocations);
-    } catch (error) {
-      capture(error);
-      return res.status(500).send({ ok: false, code: error.message });
-    }
-  },
-);
-
-router.post(
-  "/export",
-  passport.authenticate(["young", "referent"], {
-    session: false,
-    failWithError: true,
-  }),
-  async (req: UserRequest, res: Response) => {
-    try {
-      const { error, value } = Joi.object({ cohort: Joi.array().min(1).items(Joi.string()).required() })
-        .unknown()
-        .validate(req.body, { stripUnknown: true });
-      if (error) {
-        return res.status(400).send({ ok: false, code: ERRORS.INVALID_PARAMS });
-      }
-      if (![ROLES.ADMIN, ROLES.REFERENT_REGION].includes(req.user.role)) return res.status(403).send({ ok: false, code: ERRORS.OPERATION_UNAUTHORIZED });
-
-      const allClasses = await CleClasseModel.find({
-        cohort: value.cohort,
-        status: { $in: [STATUS_CLASSE.INSCRIPTION_IN_PROGRESS, STATUS_CLASSE.INSCRIPTION_TO_CHECK, STATUS_CLASSE.VALIDATED] },
-      })
-        .populate({
-          path: "etablissement",
-        })
-        .lean();
-
-      // uniquement les classes de la région du référent
-      const classes = allClasses.filter((classe) => req.user.role !== ROLES.REFERENT_REGION || classe.etablissement?.region === req.user.region);
-
-      // const etablissements = await findEtablissementsForClasses(classes);
-      const centres = await findCohesionCentersForClasses(classes);
-      const pdrs = await findPdrsForClasses(classes);
-      const youngs = await getYoungsGroupByClasses(classes);
-      const lignesBus = await findLigneInfoForClasses(classes);
-      const referents = await findReferentInfoForClasses(classes);
-
-      for (let classe of classes) {
-        // populate
-        classe.cohesionCenter = centres?.find((e) => classe.cohesionCenterId === e._id.toString());
-        classe.pointDeRassemblement = pdrs?.find((e) => classe.pointDeRassemblementId === e._id.toString());
-        classe.ligne = lignesBus.find((e) => classe.ligneId === e._id.toString());
-        classe.referentClasse = referents?.filter((e) => classe.referentClasseIds.includes(e._id.toString()));
-
-        // calcul des effectifs
-        const classeYoungs = youngs[classe._id];
-        classe.studentInProgress = classeYoungs?.filter((student) => student.status === YOUNG_STATUS.IN_PROGRESS || student.status === YOUNG_STATUS.WAITING_CORRECTION).length || 0;
-        classe.studentWaiting = classeYoungs?.filter((student) => student.status === YOUNG_STATUS.WAITING_VALIDATION).length || 0;
-        classe.studentValidated = classeYoungs?.filter((student) => student.status === YOUNG_STATUS.VALIDATED).length || 0;
-        classe.studentAbandoned = classeYoungs?.filter((student) => student.status === YOUNG_STATUS.ABANDONED).length || 0;
-        classe.studentNotAutorized = classeYoungs?.filter((student) => student.status === YOUNG_STATUS.NOT_AUTORISED).length || 0;
-        classe.studentWithdrawn = classeYoungs?.filter((student) => student.status === YOUNG_STATUS.WITHDRAWN).length || 0;
-      }
-      res.send({ ok: true, data: classes });
     } catch (error) {
       capture(error);
       return res.status(500).send({ ok: false, code: error.message });
