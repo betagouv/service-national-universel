b884a8e873e8af3a637977eb92406d0a66256df7
Pierre LE BIHAN <lebihanp@hotmail.fr>
2024-05-23 18:58:45 +0200

    Ajout de documentation

---
+++ api/config/README.md
@@ -0,0 +1,10 @@
+Copy local-development.example.js to local-development.js
+
+If you want to set up var envs, write them at the end of the file.
+
+You need to have those var defined in your .env file:
+GITGUARDIAN_API_KEY=
+SCW_ACCESS_KEY=
+SCW_SECRET_KEY=
+SCW_DEFAULT_PROJECT_ID=
+DEV=true
---
+++ api/config/local-development.example.js
@@ -0,0 +1,10 @@
+// ! Copy to local-development.js & Edit local configuration
+// * Add SCW_ACCESS_KEY and SCW_SECRET_KEY at the beginning
+
+const asyncSecretConfig = require("../src/secrets-manager");
+
+module.exports = {
+  SECRET_NAME: "snu-ci",
+  ...asyncSecretConfig(),
+  ENABLE_SENDINBLUE: false,
+};
