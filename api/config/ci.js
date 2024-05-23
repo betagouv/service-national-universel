a2c030c85b3b8578da9d08e6fb28bd8957e16d4f
Pierre LE BIHAN <lebihanp@hotmail.fr>
2024-05-23 17:54:25 +0200

    Move config one folder up

---
+++ api/config/ci.js
@@ -0,0 +1,12 @@
+const asyncSecretConfig = require("../src/secrets-manager");
+
+module.exports = {
+  ENVIRONMENT: "ci",
+  SECRET_NAME: "snu-ci",
+  SECRET_REVISION: "12",
+  RELEASE: process.env.RELEASE,
+  API_URL: "https://api.ci.beta-snu.dev",
+  APP_URL: "https://moncompte.ci.beta-snu.dev",
+  ADMIN_URL: "https://admin.ci.beta-snu.dev",
+  ...asyncSecretConfig(),
+};
