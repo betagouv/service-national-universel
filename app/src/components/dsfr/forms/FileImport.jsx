2319ed7754471d2cc8f95526f09bd33225239f6a
Guillaume <guillaume@macmax.home>
2024-05-29 15:34:57 +0200

    feat(api, app): 2269 - Convert heic image to png and update resize image

--- app/src/components/dsfr/forms/FileImport.jsx
+++ app/src/components/dsfr/forms/FileImport.jsx
@@ -1,26 +1,45 @@
 import React from "react";
-import { resizeImage } from "../../../services/file.service";
+import { convertImage, resizeImage } from "../../../services/file.service";
 
 export default function FileImport({ id, file, setFile, setError = () => {}, onChange }) {
   async function handleChange(e) {
     if (!e.target.files.length) return;
-    const image = await resizeImage(e.target.files[0]);
-    setFile(image);
-    setError({});
-    onChange && onChange();
+
+    let image = e.target.files[0];
+    console.log("Selected file:", image);
+
+    if (!["application/pdf", "image/jpeg", "image/png", "image/jpg", "image/heif", "image/heic"].includes(image.type)) {
+      setError({ message: "Format de fichier non supporté." });
+      return;
+    }
+
+    try {
+      if (["image/heif", "image/heic"].includes(image.type)) {
+        console.log("Converting HEIC/HEIF to PNG");
+        image = await convertImage(image, "PNG");
+      }
+      if (image.size > 1_000_000) {
+        console.log("Resizing image");
+        image = await resizeImage(image);
+      }
+      console.log("Final image:", image);
+      setFile(image);
+      setError({});
+      onChange && onChange();
+    } catch (error) {
+      setError({ message: error.message });
+      console.error("Error processing the file:", error);
+    }
   }
 
   return (
     <>
       <div className="my-4 text-sm text-gray-500">Formats supportés : jpg, png, pdf. Pour les PDF, taille maximum : 5 Mo.</div>
-      <input type="file" id={id} name={id} accept=".png, .jpg, .jpeg, .pdf" onChange={handleChange} className="hidden" />
+      <input type="file" id={id} name={id} accept=".png, .jpg, .jpeg, .pdf, .heif, .heic" onChange={handleChange} className="hidden" />
       <div className="my-4 flex w-full">
         <div>
-          <label htmlFor={id} className="cursor-pointer rounded bg-[#EEEEEE] py-2 px-3 text-sm text-gray-600">
-            Parcourir...
-          </label>
+          <label htmlFor={id} className="cursor-pointer rounded bg-[#EEEEEE] py-2 px-3 text-sm text-gray-600">Importer un fichier</label>
         </div>
-        {file ? <p className="ml-4 mt-2 text-sm text-gray-800">{file.name}</p> : <p className="ml-4 mt-2 text-sm text-gray-800">Aucun fichier sélectionné.</p>}
       </div>
     </>
   );
--- app/src/services/file.service.js
+++ app/src/services/file.service.js
@@ -1,27 +1,78 @@
+//
 import { readAndCompressImage } from "browser-image-resizer";
+import heic2any from "heic2any";
 import { environment } from "../config";
 
 export async function resizeImage(file, config = {}) {
   if (!["image/jpeg", "image/png"].includes(file.type)) return file;
 
+  let quality = 0.8;
+  let maxWidth = 1000;
+  let maxHeight = 1000;
+
   const defaultConfig = {
-    quality: 0.8,
-    maxWidth: 1000,
-    maxHeight: 1000,
+    mimeType: "image/png",
     debug: environment !== "production" ? true : false,
   };
   config = { ...defaultConfig, ...config };
 
   let image = file;
+  console.log("Original size:", image.size);
 
-  if (image.size > 1000000) {
-    image = await readAndCompressImage(file, config);
-    image.name = file.name;
+  while (image.size > 100000 && quality > 0) {
+    try {
+      config = {
+        ...config,
+        quality,
+        maxWidth,
+        maxHeight,
+      };
+      image = await readAndCompressImage(file, config);
+      console.log(`Compressed size at quality ${quality}:`, image.size);
+      quality -= 0.1;
+      maxWidth -= 100;
+      maxHeight -= 100;
+    } catch (error) {
+      console.error("Error during compression:", error);
+      throw new Error("Failed to compress image");
+    }
+
+    if (image.size <= 100000) break;
   }
 
-  if (image.size > 1000000) {
-    image = await resizeImage(image);
+  if (image.size > 100000) {
+    console.warn("Image size is still larger than 100KB after maximum compression attempts.");
   }
 
+  image.name = file.name;
   return image;
+}
+
+export async function convertImage(file, format = "PNG") {
+  format = format.toUpperCase();
+  if (!["JPEG", "PNG"].includes(format)) {
+    throw new Error("Output format needs to be one of [JPEG, PNG]");
+  }
+
+  if (["image/heif", "image/heic"].includes(file.type)) {
+    try {
+      console.log("Starting conversion of HEIC to PNG");
+      const arrayBuffer = await file.arrayBuffer();
+      console.log("ArrayBuffer obtained:", arrayBuffer);
+
+      const outputBlob = await heic2any({
+        blob: new Blob([arrayBuffer], { type: file.type }),
+        toType: `image/${format.toLowerCase()}`,
+      });
+      console.log("Conversion successful:", outputBlob);
+
+      const image = new File([outputBlob], `${file.name.split(".")[0]}.${format.toLowerCase()}`, { type: outputBlob.type });
+      return image;
+    } catch (error) {
+      console.error("Error converting HEIC to PNG:", error);
+      throw new Error("Failed to convert HEIC to PNG");
+    }
+  }
+
+  return file;
 }
