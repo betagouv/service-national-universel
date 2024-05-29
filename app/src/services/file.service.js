260d5cc9808934b4096e36f44cfac1d5a8100560
Guillaume <guillaume@macmax.home>
2024-05-29 17:44:35 +0200

    up

--- app/src/services/file.service.js
+++ app/src/services/file.service.js
@@ -1,57 +1,34 @@
-//
 import { readAndCompressImage } from "browser-image-resizer";
 import heic2any from "heic2any";
 import { environment } from "../config";
 
 export async function resizeImage(file, config = {}) {
   if (!["image/jpeg", "image/png"].includes(file.type)) return file;
-
-  let quality = 0.8;
-  let maxWidth = 1000;
-  let maxHeight = 1000;
-
   const defaultConfig = {
+    quality: 0.8,
+    maxWidth: 1000,
+    maxHeight: 1000,
     mimeType: "image/png",
     debug: environment !== "production" ? true : false,
   };
   config = { ...defaultConfig, ...config };
-
   let image = file;
-  console.log("Original size:", image.size);
-
-  while (image.size > 100000 && quality > 0) {
-    try {
-      config = {
-        ...config,
-        quality,
-        maxWidth,
-        maxHeight,
-      };
-      image = await readAndCompressImage(file, config);
-      console.log(`Compressed size at quality ${quality}:`, image.size);
-      quality -= 0.1;
-      maxWidth -= 100;
-      maxHeight -= 100;
-    } catch (error) {
-      console.error("Error during compression:", error);
-      throw new Error("Failed to compress image");
-    }
-
-    if (image.size <= 100000) break;
+  console.log(image.size);
+  if (image.size > 1000000) {
+    image = await readAndCompressImage(file, config);
+    image.name = file.name;
+  }
+  if (image.size > 1000000) {
+    image = await resizeImage(image);
   }
 
-  if (image.size > 100000) {
-    console.warn("Image size is still larger than 100KB after maximum compression attempts.");
-  }
-
-  image.name = file.name;
   return image;
 }
 
 export async function convertImage(file, format = "PNG") {
   format = format.toUpperCase();
   if (!["JPEG", "PNG"].includes(format)) {
-    throw new Error("Output format needs to be one of [JPEG, PNG]");
+    throw new Error("output format needs to be one of [JPEG, PNG]");
   }
 
   if (["image/heif", "image/heic"].includes(file.type)) {
