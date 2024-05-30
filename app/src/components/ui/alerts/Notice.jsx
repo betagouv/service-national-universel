2d609c3a0bdcf36a7d67822ddceda3588918b194
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2024-05-30 18:10:30 +0200

    yo

--- app/src/components/ui/alerts/Notice.jsx
+++ app/src/components/ui/alerts/Notice.jsx
@@ -3,9 +3,11 @@ import { RiInformationFill } from "react-icons/ri";
 
 export default function Notice({ children }) {
   return (
-    <div className="m-[1rem] md:m-[2rem] p-4 flex items-center border-2 border-yellow-500 bg-white rounded-md text-yellow-600">
-      <RiInformationFill className="inline-block mr-4 h-6 w-6 flex-none" />
-      {children}
+    <div className="m-[1rem] md:m-[2rem] p-4 md:flex items-center border-2 border-yellow-500 bg-white rounded-md text-yellow-600">
+      <div className="mb-4 text-center">
+        <RiInformationFill className="inline-block mr-4 h-6 w-6 flex-none" />
+      </div>
+      <div>{children}</div>
     </div>
   );
 }
