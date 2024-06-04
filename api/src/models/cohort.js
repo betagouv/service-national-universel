385601ebc37a0876fe6571876f6be6ea6a66395f
Marin <kalasasta@gmail.com>
2024-06-04 12:37:25 +0200

    feat(lib, api): Better handling of timezone offsets.

--- api/src/models/cohort.js
+++ api/src/models/cohort.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const { isAfter, isWithinInterval } = require("date-fns");
-const { COHORT_TYPE, COHORT_TYPE_LIST } = require("snu-lib");
+const { COHORT_TYPE, COHORT_TYPE_LIST, getDateTimeByTimeZoneOffset } = require("snu-lib");
 const patchHistory = require("mongoose-patch-history").default;
 
 const MODELNAME = "cohort";
@@ -322,25 +322,40 @@ Schema.virtual("fromUser").set(function (fromUser) {
   }
 });
 
-Schema.virtual("isInstructionOpen").get(function () {
+Schema.methods.getIsInstructionOpen = function (timeZoneOffset) {
+  const now = getDateTimeByTimeZoneOffset(timeZoneOffset);
   const end = this.instructionEndDate;
-  if (!end || isAfter(new Date(), end)) return false;
+  if (!end || isAfter(now, end)) return false;
   return true;
+};
+
+Schema.virtual("isInstructionOpen").get(function () {
+  return this.getIsInstructionOpen();
 });
 
-Schema.virtual("isInscriptionOpen").get(function () {
+Schema.methods.getIsInscriptionOpen = function (timeZoneOffset) {
+  const now = getDateTimeByTimeZoneOffset(timeZoneOffset);
   const start = this.inscriptionStartDate;
   const end = this.inscriptionEndDate;
   if (!start || !end || isAfter(start, end)) return false;
-  return isWithinInterval(new Date(), { start, end });
+  return isWithinInterval(now, { start, end });
+};
+
+Schema.virtual("isInscriptionOpen").get(function () {
+  return this.getIsInscriptionOpen();
 });
 
-Schema.virtual("isReInscriptionOpen").get(function () {
+Schema.methods.getIsReInscriptionOpen = function (timeZoneOffset) {
+  const now = getDateTimeByTimeZoneOffset(timeZoneOffset);
   const start = this.reInscriptionStartDate;
   const end = this.reInscriptionEndDate;
-  if (!start || !end) return this.isInscriptionOpen;
+  if (!start || !end) return this.getIsInscriptionOpen(timeZoneOffset);
   if (isAfter(start, end)) return false;
-  return isWithinInterval(new Date(), { start, end });
+  return isWithinInterval(now, { start, end });
+};
+
+Schema.virtual("isReInscriptionOpen").get(function () {
+  return this.getIsReInscriptionOpen();
 });
 
 Schema.pre("save", function (next) {
