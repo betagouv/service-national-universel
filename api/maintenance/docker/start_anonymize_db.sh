bb9c6e8fcadb4edd80747d98f26eccb73342d35a
pitiscarf <romain.thebaud@beta.gouv.fr>
2024-06-07 18:17:28 +0200

    change working dir

--- api/maintenance/docker/start_anonymize_db.sh
+++ api/maintenance/docker/start_anonymize_db.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-script_dir="$(dirname $0)/../scripts"
+cd "$(dirname $0)/../scripts"
 
 set -o pipefail
 set -e
 
-src_db_uri=$($script_dir/get_secrets.sh snu-production | jq -r ".MONGO_URL")
-dest_db_uri=$($script_dir/get_secrets.sh snu-staging | jq -r ".MONGO_URL")
+src_db_uri=$(./get_secrets.sh snu-production | jq -r ".MONGO_URL")
+dest_db_uri=$(./get_secrets.sh snu-staging | jq -r ".MONGO_URL")
 
-$script_dir/anonymize_db.sh $src_db_uri $dest_db_uri
+./anonymize_db.sh $src_db_uri $dest_db_uri
