faa7259baa997190d5b314483330d2787619d175
pitiscarf <romain.thebaud@beta.gouv.fr>
2024-06-07 17:14:50 +0200

    fix_typo

--- api/maintenance/docker/start_anonymize_db.sh
+++ api/maintenance/docker/start_anonymize_db.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-script_dir=$(dirname $0)
+script_dir="$(dirname $0)/../scripts"
 
 set -o pipefail
 set -e
 
-src_db_uri=$($script_dir/../scripts/get_secrets.sh snu-production | jq -r ".MONGO_URL")
-dest_db_uri=$($script_dir/../scripts/get_secrets.sh snu-staging | jq -r ".MONGO_URL")
+src_db_uri=$($script_dir/get_secrets.sh snu-production | jq -r ".MONGO_URL")
+dest_db_uri=$($script_dir/get_secrets.sh snu-staging | jq -r ".MONGO_URL")
 
-$script_dir/../scripts/anonymise_db.sh $src_db_uri $dest_db_uri
+$script_dir/anonymize_db.sh $src_db_uri $dest_db_uri
