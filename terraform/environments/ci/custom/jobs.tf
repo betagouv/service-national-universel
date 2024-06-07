2a4acd51d2260584aeae475bb04363f8918bdabf
pitiscarf <dev@rthebaud.fr>
2024-06-07 12:14:31 +0200

    Job created & Dockerfile updated

---
+++ terraform/environments/ci/custom/jobs.tf
@@ -0,0 +1,16 @@
+resource scaleway_job_definition main {
+  project_id  = local.project_id
+  name = "anonymize_db"
+  description = "Dump source database, then reimport anonymized collections in destination database"
+  cpu_limit = 1024
+  memory_limit = 4096
+  image_uri = "${data.scaleway_registry_namespace.main.endpoint}/api:${var.api_image_tag}"
+  command = "./api/maintenance/docker/start_anonymize_db.sh"
+  timeout = "2h"
+
+  env = {
+    "NODE_ENV"       = "production"
+    "SCW_PROJECT_ID" = local.project_id
+    "SCW_SECRET_KEY" = local.secrets.SCW_SECRET_KEY
+  }
+}
