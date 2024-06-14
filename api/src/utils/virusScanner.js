6223d1afe680f2b2702761838a855e71b849e9db
rthebaud <romain.thebaud@beta.gouv.fr>
2024-06-14 16:57:35 +0200

    feat(api): add ENABLE_ANTIVIRUS configuration (#3993)

--- api/src/utils/virusScanner.js
+++ api/src/utils/virusScanner.js
@@ -2,32 +2,35 @@
 const config = require("config");
 const { capture } = require("../sentry");
 const fetch = require("node-fetch");
-const FormData = require('form-data');
-const { createReadStream } = require('fs');
+const FormData = require("form-data");
+const { createReadStream } = require("fs");
 const { timeout } = require("../utils");
 const { ERRORS } = require("./index");
 
 const TIMEOUT_ANTIVIRUS_SERVICE = 20000;
 
-async function scanFile(tempFilePath, name, userId="anonymous") {
+async function scanFile(tempFilePath, name, userId = "anonymous") {
+  if (!config.get("ENABLE_ANTIVIRUS")) {
+    return { infected: false };
+  }
 
   const scan = async () => {
     const stream = createReadStream(tempFilePath);
     const url = `${config.API_ANTIVIRUS_ENDPOINT}/scan`;
 
     const formData = new FormData();
-    formData.append('file', stream);
+    formData.append("file", stream);
 
-    const headers = formData.getHeaders()
-    headers["X-Auth-Token"] = config.API_ANTIVIRUS_TOKEN
+    const headers = formData.getHeaders();
+    headers["X-Auth-Token"] = config.API_ANTIVIRUS_TOKEN;
 
-    const response = await fetch(url, { method: 'POST', body: formData, headers });
+    const response = await fetch(url, { method: "POST", body: formData, headers });
 
     if (response.status != 200) {
       throw new Error(ERRORS.FILE_SCAN_BAD_RESPONSE);
     }
 
-    const { infected } = await response.json()
+    const { infected } = await response.json();
 
     if (infected) {
       capture(`File ${name} of user(${userId}) is infected`);
