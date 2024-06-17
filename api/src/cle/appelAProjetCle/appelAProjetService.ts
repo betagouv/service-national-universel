cc6c07a57eabfe525e517c3ec7aa0496cd950a3a
Johann <Johannbr@users.noreply.github.com>
2024-06-17 17:02:34 +0200

    feat(api): 2598 - brancher et exporter ds et annuaire etablissement (#3994)
    * feat(api): 2598 - CLE 24-25 : Brancher et exporter les données de DS et annuaire des établissements
    
    * feat(api): 2598 - CLE 24-25 : Brancher et exporter les données de DS et annuaire des établissements WIP
    
    * feat(api): 2598 - CLE 24-25 : Brancher et exporter les données de DS et annuaire des établissements
    
    * feat(api): 2598 - CLE 24-25 : Brancher et exporter les données de DS et annuaire des établissements WIP
    
    * feat(api): 2598 - CLE 24-25 : Brancher et exporter les données de DS et annuaire des établissements WIP
    
    * feat(api): 2598 - CLE 24-25 : remove @ path resolution for api project
    
    * feat(api): 2598 - CLE 24-25 : DSquery test
    
    * feat(api): 2598 - CLE 24-25 : optim import

---
+++ api/src/cle/appelAProjetCle/appelAProjetService.ts
@@ -0,0 +1,26 @@
+import { getClassesAndEtablissementsFromAppelAProjets } from "../../providers/demarcheSimplifiee/demarcheSimplifieeProvider";
+import { IReferent } from "../../models/referentType";
+import { IEtablissement } from "../../models/cle/etablissementType";
+import { IClasse } from "../../models/cle/classeType";
+
+export const syncAppelAProjet = async () => {
+  const appelAProjets = await getClassesAndEtablissementsFromAppelAProjets();
+  const referentsToCreate: IReferent[] = [];
+  const referentsToLog: IReferent[] = [];
+  const etablissementsToCreate: IEtablissement[] = [];
+  const etablissementsToUpdate: IEtablissement[] = [];
+  const classesToCreate: IClasse[] = [];
+  const classesToUpdate: IClasse[] = [];
+  for (const appelAProjet of appelAProjets) {
+    // if referent exists, update it
+    // if not, create referent
+    //---------------
+    // get complemenary data from api-education
+    // if etablissement exists, update it
+    // if not, create etablissement
+    //---------------
+    // if classe exists, update it
+    // if not, create classe
+  }
+  return appelAProjets;
+};
