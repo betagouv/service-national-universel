023d93f2a6b37d00910de3e31452679cdd837251
C2Chandelier <charlelie.chandelier@epitech.eu>
2024-06-18 15:22:59 +0200

    up

--- api/src/cle/etablissement/etablissementController.ts
+++ api/src/cle/etablissement/etablissementController.ts
@@ -77,7 +77,6 @@ router.get("/:id", passport.authenticate("referent", { session: false, failWithE
 
 router.put("/:id", passport.authenticate("referent", { session: false, failWithError: true }), async (req: UserRequest, res: Response) => {
   try {
-    console.log("LAAAAAAA");
     const { error, value } = Joi.object({
       id: Joi.string().required(),
       name: Joi.string().required(),
@@ -99,7 +98,6 @@ router.put("/:id", passport.authenticate("referent", { session: false, failWithE
       .unknown()
       .validate({ ...req.params, ...req.body }, { stripUnknown: true });
     if (error) {
-      console.log("error", error);
       capture(error);
       return res.status(400).send({ ok: false, code: ERRORS.INVALID_BODY });
     }
