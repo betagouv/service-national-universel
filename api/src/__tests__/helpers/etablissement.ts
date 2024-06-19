43e4fa2ee7d391bbc6bbd5bf16ddd8bb98fdb664
C2Chandelier <charlelie.chandelier@gmail.com>
2024-06-19 11:11:43 +0200

    Update api/src/__tests__/helpers/etablissement.ts
    Co-authored-by: Anselme Chorein <achorein@users.noreply.github.com>

--- api/src/__tests__/helpers/etablissement.ts
+++ api/src/__tests__/helpers/etablissement.ts
@@ -3,6 +3,6 @@ import { CleEtablissementModel } from "../../models";
 async function createEtablissement(etablissement) {
   const etablissementCreated = await CleEtablissementModel.create(etablissement);
   return etablissementCreated;
-}; 
+}
 
 export { createEtablissement };
