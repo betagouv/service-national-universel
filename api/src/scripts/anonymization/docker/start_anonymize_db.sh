896285c25e59f53e8c7c493e81c1725b22acdb42
pitiscarf <romain.thebaud@beta.gouv.fr>
2024-06-20 22:14:36 +0200

    reactivate anonymization

--- api/src/scripts/anonymization/docker/start_anonymize_db.sh
+++ api/src/scripts/anonymization/docker/start_anonymize_db.sh
@@ -8,6 +8,6 @@ set -e
 src_db_uri=$(../get_secrets.sh snu-production | jq -r ".MONGO_URL")
 dest_db_uri=$(../get_secrets.sh snu-staging | jq -r ".MONGO_URL")
 
-#./anonymize_db.sh $src_db_uri $dest_db_uri
+./anonymize_db.sh $src_db_uri $dest_db_uri
 
 NODE_ENV=staging node ../reindex_es_all_models/index.js
