02d15e31392e4906d4bcea93064c692b92fe98db
Johann <Johannbr@users.noreply.github.com>
2024-07-01 17:52:26 +0200

    feat(api): add migration repository tools (#4043)
    * feat(api): add migration repository tools
    
    * feat(api): setup to use in docker container /app as a workdir
    
    * feat(api): remove unlock and add migration script
    
    * feat(api): use mongoose in script and add feature flagService
    
    * feat(api): mock isFeatureAvailable
    
    * feat(api): migration - updating the migration configuration, adding new migration scripts, modifying the package.json file
    
    * feat(api): migration - config to allow mongoose connection for down script

---
+++ api/src/models/featureFlagType.ts
@@ -0,0 +1,16 @@
+import { Document } from "mongoose";
+
+export type FeatureFlagDocument = FeatureFlagType & Document;
+
+export interface FeatureFlagType {
+  name: string;
+  description: string;
+  enabled?: boolean;
+  date: { from: Date; to: Date };
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export enum FeatureFlagName {
+  SYNC_APPEL_A_PROJET_CLE = "SYNC_APPEL_A_PROJET_CLE",
+}
