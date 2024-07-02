3bc37010217086897edf3cc958e5154334f944fd
Guillaume <guillaumepichon91@gmail.com>
2024-07-02 11:46:43 +0200

    fix(BDC): 2887 - Redirect to ContactForm admin

--- knowledge-base-public/src/components/knowledge-base/KnowledgeBasePublicNoAnswer.js
+++ knowledge-base-public/src/components/knowledge-base/KnowledgeBasePublicNoAnswer.js
@@ -1,9 +1,24 @@
-import { appURL } from "../../config";
-
+import { appURL, adminURL } from "../../config";
+import useUser from "../../hooks/useUser";
+import { useEffect, useState } from "react";
 const KnowledgeBasePublicNoAnswer = () => {
+  const { restriction } = useUser();
+  const [userRole, setUserRole] = useState("public");
+  let url;
+  if (userRole === "young" || userRole === "public") {
+    url = appURL;
+  } else if (userRole !== null && userRole !== "public" && userRole !== "young") {
+    url = adminURL;
+  }
+  useEffect(() => {
+    if (restriction) {
+      setUserRole(restriction);
+    } else setUserRole(null);
+  }, [restriction]);
+
   return (
     <div className="mb-16 mt-4 flex flex-row items-center justify-start">
-      <a href={appURL + "/besoin-d-aide"} className="flex flex-row items-center justify-center text-[16px] leading-6 text-snu-purple-200" target="_blank" rel="noopener noreferrer">
+      <a href={url + "/besoin-d-aide"} className="flex flex-row items-center justify-center text-[16px] leading-6 text-snu-purple-200" target="_blank" rel="noopener noreferrer">
         <p className="text-blue-600">Je n&apos;ai pas trouvé de réponse à ma question&nbsp;→</p>
       </a>
     </div>
