9fd389ae8161078199d565c476ee44a078aa7d9e
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2024-07-02 18:00:37 +0200

    up

--- app/src/scenes/phase2/Home/components/Programs.jsx
+++ app/src/scenes/phase2/Home/components/Programs.jsx
@@ -66,7 +66,7 @@ export function Programs() {
                 <div className="aspect-square rounded-full mx-auto overflow-hidden">
                   <img src={image} alt={program.name} className="h-full w-full object-cover" />
                 </div>
-                <p className="text-center font-bold mt-2 text-sm">{program.name}</p>
+                <p className="text-center font-bold mt-2 text-sm line-clamp-3">{program.name}</p>
               </a>
             </div>
           );
