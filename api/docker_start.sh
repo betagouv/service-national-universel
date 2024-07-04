2c23b2a236485a50ddbaa7e941101baad72d7283
rthebaud <romain.thebaud@beta.gouv.fr>
2024-07-04 10:39:18 +0200

    fix(api): Disable pm2 stderr (#4062)

--- api/docker_start.sh
+++ api/docker_start.sh
@@ -8,7 +8,7 @@ if [[ $PM2_SLACK_URL == "" ]]
 then
   echo "PM2_SLACK_URL is not defined"
 else
-  pm2 --no-daemon multiset "pm2-slack:slack_url $PM2_SLACK_URL pm2-slack:servername PM2-ERROR-API"
+  pm2 --no-daemon multiset "pm2-slack:slack_url $PM2_SLACK_URL pm2-slack:servername PM2-ERROR-API pm2-slack:error false"
 fi
 
 
