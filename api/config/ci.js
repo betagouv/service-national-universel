235aea255ca1ef544f59c93a24e2819bf8fcd31a
rthebaud <romain.thebaud@beta.gouv.fr>
2024-07-10 15:59:04 +0200

    chore(api): update secrets revision (#4086)

--- api/config/ci.js
+++ api/config/ci.js
@@ -6,7 +6,7 @@ if (!secretKey) {
   throw new Error("SCW_SECRET_KEY is required to get configuration secrets");
 }
 
-const REVISION = 4;
+const REVISION = 5;
 const secrets = getSecrets(secretKey, CI_PROJECT_ID, "snu-ci", REVISION);
 
 module.exports = {
