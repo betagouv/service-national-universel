17b6b97775a069ca12fc06cd52d5893ce6d58a8f
C2Chandelier <charlelie.chandelier@epitech.eu>
2024-07-12 18:35:03 +0200

    relou

--- api/src/emails/cle/classe.js
+++ api/src/emails/cle/classe.js
@@ -52,40 +52,6 @@ module.exports = (emailsEmitter) => {
     }
   });
 
-  // Classe infos completed
-  emailsEmitter.on(SENDINBLUE_TEMPLATES.CLE.CLASSE_INFOS_COMPLETED, async (classe) => {
-    try {
-      const etablissement = await EtablissementModel.findById(classe.etablissementId);
-      if (!etablissement) throw new Error("Etablissement not found");
-
-      // Admins
-      const referents = await ReferentModel.find({ _id: { $in: [...classe.referentClasseIds, ...etablissement.referentEtablissementIds, ...etablissement.coordinateurIds] } });
-      if (!referents?.length) throw new Error("Referents not found");
-      await sendTemplate(SENDINBLUE_TEMPLATES.CLE.CLASSE_INFOS_COMPLETED, {
-        emailTo: referents.map((referent) => ({ email: referent.email, name: `${referent.firstName} ${referent.lastName}` })),
-        params: {
-          class_name: classe.name,
-          class_code: classe.uniqueKeyAndId,
-          classUrl: `${config.APP_URL}/je-rejoins-ma-classe-engagee?id=${classe._id.toString()}`,
-        },
-      });
-
-      // Referents departementaux et régionaux
-      const refsDepReg = [...(await getReferentDep(etablissement.departement)), ...(await getReferentReg(etablissement.region))];
-
-      await sendTemplate(SENDINBLUE_TEMPLATES.CLE.CLASSE_INFOS_COMPLETED_DEP_REG, {
-        emailTo: refsDepReg.map((referent) => ({ email: referent.email, name: `${referent.firstName} ${referent.lastName}` })),
-        params: {
-          class_name: classe.name,
-          class_code: classe.uniqueKeyAndId,
-          cta: `${config.ADMIN_URL}/classes/${classe._id.toString()}`,
-        },
-      });
-    } catch (error) {
-      capture(error);
-    }
-  });
-
   //classe verified
   emailsEmitter.on(SENDINBLUE_TEMPLATES.CLE.CLASSE_VERIFIED, async (classe) => {
     try {
