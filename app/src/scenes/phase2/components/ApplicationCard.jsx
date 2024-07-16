7dc629b8461ebb834da89be9ba164f0d002345b2
Nicolas Bretécher <nicolas.bretecher@selego.co>
2024-07-16 15:51:24 +0200

    feat(app): 2809 - [Refonte phase 2] Home statut "Validée" (#4073)

--- app/src/scenes/phase2/components/ApplicationCard.jsx
+++ app/src/scenes/phase2/components/ApplicationCard.jsx
@@ -9,10 +9,10 @@ export default function ApplicationCard({ application }) {
   return (
     <Link
       to={`/phase2/mission/${application.missionId}`}
-      className="bg-white rounded-xl border p-3 w-72 md:w-96 h-48 flex flex-col justify-between flex-none snap-always snap-center">
+      className="bg-white rounded-xl border p-3 w-72 md:w-96 only:w-full only:md:w-96  h-48 flex flex-col justify-between flex-none snap-always snap-center shadow-sm">
       <p className={`text-xs rounded-full px-2 py-1 w-fit ${statusColors[application.status]}`}>{translateApplication(application.status)}</p>
 
-      <p className="text-xs leading-5 text-gray-400">{application.mission.structureName}</p>
+      <p className="text-xs leading-5 text-gray-500">{application.mission.structureName}</p>
 
       <div className="flex items-center gap-4">
         <IconDomain domain={application.mission?.isMilitaryPreparation === "true" ? "PREPARATION_MILITARY" : application.mission?.mainDomain} />
@@ -20,10 +20,10 @@ export default function ApplicationCard({ application }) {
       </div>
 
       <div className="flex justify-between items-end">
-        <p className="text-xs leading-5 text-gray-400 line-clamp-1">
+        <p className="text-xs leading-5 text-gray-500 line-clamp-1">
           {application.mission.placesLeft} place{application.mission.placesLeft > 1 ? "s" : ""} disponible{application.mission.placesLeft > 1 ? "s" : ""}
         </p>
-        <p className="text-xs leading-5 text-gray-400 line-clamp-1">
+        <p className="text-xs leading-5 text-gray-500 line-clamp-1">
           <HiLocationMarker className="inline-block text-gray-300 mr-1 text-base align-text-top" />
           {application.mission.city}
         </p>
