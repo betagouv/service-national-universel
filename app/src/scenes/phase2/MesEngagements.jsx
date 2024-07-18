853056a368f2ff63618f01ff564569576f6fd302
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2024-07-18 10:14:46 +0200

    up

--- app/src/scenes/phase2/MesEngagements.jsx
+++ app/src/scenes/phase2/MesEngagements.jsx
@@ -1,30 +1,34 @@
-import React, { useState } from "react";
+import React from "react";
+import { NavLink } from "react-router-dom";
 import TableauDeBord from "./views/TableauDeBord";
 import Preferences from "../preferences";
 
 export default function View() {
-  const [currentTab, setCurrentTab] = useState(0);
+  const params = new URLSearchParams(window.location.search);
   return (
     <div className="p-1 md:p-8 bg-white flex flex-col">
       <h1 className="mt-6 mb-6 mx-auto text-center font-bold text-4xl md:text-5xl max-w-xl leading-tight md:leading-tight">Mes engagements</h1>
-      <span className="p-8 flex justify-center ">
-        <span
-          onClick={() => setCurrentTab(0)}
-          className={`pr-1 pl-1 pb-2 cursor-pointer hover:text-blue-600 hover:border-b-2 border-blue-600 text-gray-400 cursor-pointer ${
-            currentTab === 0 && "!text-blue-600 border-b-2"
-          }`}>
+      <span className="p-8 flex justify-center gap-4">
+        <NavLink
+          to="?tab=dashboard"
+          isActive={() => {
+            return params.get("tab") !== "settings";
+          }}
+          className="pr-1 pl-1 pb-2 hover:text-blue-600 hover:border-b-2 border-blue-600 text-gray-400"
+          activeClassName="!text-blue-600 border-b-2">
           Tableau de bord
-        </span>
-        <span
-          onClick={() => setCurrentTab(1)}
-          className={`pr-1 pl-1 ml-4 pb-2 cursor-pointer hover:text-blue-600 hover:border-b-2 border-blue-600 text-gray-400 cursor-pointer ${
-            currentTab === 1 && "text-blue-600 border-b-2"
-          }`}>
+        </NavLink>
+        <NavLink
+          to="?tab=settings"
+          isActive={() => {
+            params.get("tab") === "settings";
+          }}
+          className="pr-1 pl-1 pb-2 hover:text-blue-600 hover:border-b-2 border-blue-600 text-gray-400"
+          activeClassName="!text-blue-600 border-b-2">
           Mes préférences
-        </span>
+        </NavLink>
       </span>
-      {currentTab === 0 && <TableauDeBord />}
-      {currentTab === 1 && <Preferences />}
+      {params.get("tab") === "settings" ? <Preferences /> : <TableauDeBord />}
     </div>
   );
 }
