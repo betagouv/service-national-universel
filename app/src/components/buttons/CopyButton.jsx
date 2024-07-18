f549efbfe18908f41ba310a80374c2ad2dfb7118
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2024-07-19 01:38:35 +0200

    up

--- app/src/components/buttons/CopyButton.jsx
+++ app/src/components/buttons/CopyButton.jsx
@@ -7,13 +7,13 @@ export default function CopyButton({ string }) {
   const [copied, setCopied] = useState(false);
   return (
     <button
-      className="flex cursor-pointer items-center justify-center hover:scale-105"
+      className="flex items-center justify-center p-1"
       onClick={() => {
         copyToClipboard(string);
         setCopied(true);
         setTimeout(() => setCopied(false), 3000);
       }}>
-      {copied ? <HiCheckCircle className="text-green-500" /> : <MdOutlineContentCopy className="text-xl text-gray-500" />}
+      {copied ? <HiCheckCircle className="text-xl text-green-500" /> : <MdOutlineContentCopy className="text-xl text-gray-500" />}
     </button>
   );
 }
