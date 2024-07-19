918395d1fbbf9572d26560b7ee6da3def19cf8cd
C2Chandelier <charlelie.chandelier@epitech.eu>
2024-07-19 16:08:18 +0200

    up

--- api/migrations/20240711125727-delete_empty_classe.js
+++ api/migrations/20240711125727-delete_empty_classe.js
@@ -2,7 +2,7 @@ const ClasseModel = require("../src/models/cle/classe");
 const YoungModel = require("../src/models/young");
 
 module.exports = {
-  async up(db, client) {
+  async up() {
     const emptyClasses = await ClasseModel.find({ schoolYear: "2023-2024", seatsTaken: 0 });
 
     const emptyClassIds = emptyClasses.map((classe) => classe._id);
@@ -13,32 +13,9 @@ module.exports = {
 
     const classesToDelete = emptyClasses.filter((classe) => !classesWithYoungs.includes(classe._id.toString()));
 
-    if (classesToDelete.length === 0) {
-      return;
-    }
-
-    const backupCollection = db.collection("deleted_classe_backup");
-    await backupCollection.insertMany(classesToDelete);
-
-    await ClasseModel.deleteMany({ _id: { $in: classesToDelete.map((classe) => classe._id) } });
-
-    const backupCount = await backupCollection.countDocuments({});
-    if (backupCount === 0) {
-      await backupCollection.drop();
-    }
-  },
-
-  async down(db, client) {
-    const backupCollection = db.collection("deleted_classe_backup");
-    const deletedClasses = await backupCollection.find({}).toArray();
-
-    if (deletedClasses.length > 0) {
-      await db.collection("classes").insertMany(deletedClasses);
-    }
-
-    const backupCount = await backupCollection.countDocuments({});
-    if (backupCount === 0) {
-      await backupCollection.drop();
+    if (classesToDelete.length > 0) {
+      const now = new Date();
+      await ClasseModel.updateMany({ _id: { $in: classesToDelete.map((classe) => classe._id) } }, { $set: { deletedAt: now } });
     }
   },
 };
