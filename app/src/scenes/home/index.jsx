aa2d4a7b61b235a27bc54eb9cdd3260d1315447f
Johannbr <johannbr@gmail.com>
2024-08-15 17:42:10 +0200

    chore(app): getCohort instead of getCohortNames()

--- app/src/scenes/home/index.jsx
+++ app/src/scenes/home/index.jsx
@@ -2,7 +2,7 @@ import { React, useEffect, useState } from "react";
 import { Redirect } from "react-router-dom";
 import useDocumentTitle from "../../hooks/useDocumentTitle";
 import { YOUNG_STATUS, YOUNG_STATUS_PHASE1, YOUNG_STATUS_PHASE2, hasAccessToReinscription } from "../../utils";
-import { cohortAssignmentAnnouncementsIsOpenForYoung, getCohort } from "../../utils/cohorts";
+import { cohortAssignmentAnnouncementsIsOpenForYoung, cohortsInit, getCohort } from "../../utils/cohorts";
 import Affected from "./Affected";
 import FutureCohort from "./FutureCohort";
 import InscriptionClosedCLE from "./InscriptionClosedCLE";
@@ -47,8 +47,9 @@ export default function Home() {
     }
   };
 
-  useEffect(() => {
+  useEffect(async () => {
     fetchReInscriptionOpen();
+    await cohortsInit();
   }, []);
 
   if (reinscriptionOpenLoading) return <Loader />;
@@ -94,7 +95,7 @@ export default function Home() {
       return <HomePhase2 />;
     }
 
-    if (![YOUNG_STATUS_PHASE1.DONE, YOUNG_STATUS_PHASE1.EXEMPTED].includes(young.statusPhase1)) {
+    if (!!getCohort(young.cohort) && ![YOUNG_STATUS_PHASE1.DONE, YOUNG_STATUS_PHASE1.EXEMPTED].includes(young.statusPhase1)) {
       // they are in the new cohort, we display the inscription step
       const isCohortInstructionOpen = new Date() < new Date(cohort.instructionEndDate);
       if (isCLE && [YOUNG_STATUS.WAITING_VALIDATION, YOUNG_STATUS.WAITING_CORRECTION].includes(young.status) && !isCohortInstructionOpen) {
