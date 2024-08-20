f3e019344cd7b2b04b4d2cb871d52f4b1f4482b2
Pierre LE BIHAN <lebihanp@hotmail.fr>
2024-08-20 18:20:40 +0200

    up

--- api/src/passport.js
+++ api/src/passport.js
@@ -38,8 +38,8 @@ async function validateUser(Model, jwtPayload, done, role) {
 
     const user = await Model.findById(value._id);
     if (user) {
-      const passwordMatch = !value.passwordChangedAt || user.passwordChangedAt === value.passwordChangedAt;
-      const logoutMatch = !value.lastLogoutAt || user.lastLogoutAt === value.lastLogoutAt;
+      const passwordMatch = user.passwordChangedAt?.getTime() === value.passwordChangedAt?.getTime();
+      const logoutMatch = user.lastLogoutAt?.getTime() === value.lastLogoutAt?.getTime();
 
       if (passwordMatch && logoutMatch && (!role || user.role === role)) {
         return done(null, user);
