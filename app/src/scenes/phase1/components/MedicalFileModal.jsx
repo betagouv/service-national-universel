900b32e76a74dc8f1e7cd62ad0c855dc8c905bab
Nicolas Bretécher <nicolas.bretecher@selego.co>
2024-08-27 10:37:12 +0200

    fix(app): 2960 - Wording étape fiche sanitaire (#4226)

--- app/src/scenes/phase1/components/MedicalFileModal.jsx
+++ app/src/scenes/phase1/components/MedicalFileModal.jsx
@@ -96,7 +96,7 @@ const MedicalFileModal = ({ isOpen, onClose, onClick, title = "Téléchargez vot
                 <span className="flex h-8 w-8 bg-white shrink-0 items-center justify-center rounded-full border border-gray-300 text-sm text-gray-700">3</span>
               </div>
               <div>
-                <p className="bg-[#DBEAFE] w-fit rounded-sm px-1.5 text-xs text-[#1E40AF]">Avant le départ</p>
+                <p className="bg-[#DBEAFE] w-fit rounded-sm px-1.5 text-xs text-[#1E40AF] font-medium">Avant le départ</p>
                 <p className="text-sm text-gray-800 font-bold">Envoyer l'ensemble des documents par e-mail</p>
                 <p className="text-xs text-gray-700 leading-relaxed">
                   à{" "}
@@ -111,10 +111,10 @@ const MedicalFileModal = ({ isOpen, onClose, onClick, title = "Téléchargez vot
                 <span className="flex h-8 w-8 bg-white shrink-0 items-center justify-center rounded-full border border-gray-300 text-sm text-gray-700">4</span>
               </div>
               <div>
-                <p className="bg-[#DBEAFE] w-fit rounded-sm px-1.5 text-xs text-[#1E40AF]">Le jour du départ</p>
+                <p className="bg-[#DBEAFE] w-fit rounded-sm px-1.5 text-xs text-[#1E40AF] font-medium">Le jour du départ</p>
                 <p className="text-sm text-gray-800 font-bold">Remettre l’ensemble des documents en mains propres</p>
                 <p className="text-xs text-gray-600 leading-relaxed">
-                  à l’équipe d’encadrement à l’arrivée au séjour, dans une enveloppe portant la mention <i>“À l’attention du référent sanitaire, Pli Confidentiel”</i>.
+                  à l’équipe d’encadrement, dans une enveloppe portant la mention <i>“À l’attention du référent sanitaire, Pli Confidentiel”</i>.
                 </p>
               </div>
             </li>
@@ -125,7 +125,7 @@ const MedicalFileModal = ({ isOpen, onClose, onClick, title = "Téléchargez vot
               <span className="flex h-8 w-8 bg-white shrink-0 items-center justify-center rounded-full border border-gray-300 text-sm text-gray-700">3</span>
             </div>
             <div>
-              <p className="bg-[#DBEAFE] w-fit rounded-sm px-1.5 text-xs text-[#1E40AF]">Le jour du départ</p>
+              <p className="bg-[#DBEAFE] w-fit rounded-sm px-1.5 text-xs text-[#1E40AF] font-medium">Le jour du départ</p>
               <p className="text-sm text-gray-800 font-bold">Remettre l’ensemble des documents en mains propres</p>
               <p className="text-xs text-gray-600 leading-relaxed">
                 à l’équipe d’encadrement à l’arrivée au séjour, dans une enveloppe portant la mention <i>“À l’attention du référent sanitaire, Pli Confidentiel”</i>.
