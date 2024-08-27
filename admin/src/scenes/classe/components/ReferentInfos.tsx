dcc6d670222dc441f852a1e4905fc74ca06da753
C2Chandelier <charlelie.chandelier@epitech.eu>
2024-08-27 11:18:58 +0200

    remove tooltip

--- admin/src/scenes/classe/components/ReferentInfos.tsx
+++ admin/src/scenes/classe/components/ReferentInfos.tsx
@@ -37,13 +37,6 @@ export default function ReferentInfos({ classe, setClasse, editRef, setEditRef, 
     <Container title="Référent de classe" actions={containerActionList({ edit: editRef, setEdit: setEditRef, canEdit: rights.canEditRef })}>
       <div className="flex items-stretch justify-stretch">
         <div className="flex-1">
-          {editRef && (
-            <Label
-              title="Nom et Prénom"
-              name="estimatedSeats"
-              tooltip="Le nom et prénom ne sera pas pris en compte si le compte existe déjà sur la plateforme. Le référent de classe pourra le modifier lors de son inscription."
-            />
-          )}
           <InputText
             name="refName"
             className="mb-3"
