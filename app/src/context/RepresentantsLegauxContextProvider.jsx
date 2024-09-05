0c135407298b01247cbc10ec064bfa7282c7b339
Nicolas Bretécher <nicolas.bretecher@selego.co>
2024-09-05 09:47:40 +0200

    fix(app): 3197 - Réparer les pages Consentements (volontaires HTS) et Représentants légaux (HTS et CLE) (#4269)

--- app/src/context/RepresentantsLegauxContextProvider.jsx
+++ app/src/context/RepresentantsLegauxContextProvider.jsx
@@ -3,14 +3,20 @@ import { Redirect } from "react-router-dom";
 import queryString from "query-string";
 import api from "../services/api";
 import { capture } from "../sentry";
+import Loader from "@/components/Loader";
+import { fetchClass } from "@/services/classe.service";
+import { fetchCohort } from "@/utils/cohorts";
 
 export const RepresentantsLegauxContext = createContext();
 
 const RepresentantsLegauxContextProvider = ({ children, parentId }) => {
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const params = queryString.parse(location.search);
   const { token } = params;
   const [young, setYoung] = useState(null);
+  const [classe, setClasse] = useState(null);
+  const [cohort, setCohort] = useState(null);
 
   useEffect(() => {
     async function getYoungFromToken() {
@@ -19,16 +25,28 @@ const RepresentantsLegauxContextProvider = ({ children, parentId }) => {
         const { ok, data } = await api.get(`/representants-legaux/young?token=${token}&parent=${parentId}`);
         if (!ok) return setError(true);
         setYoung(data);
+
+        if (data.cohortId) {
+          const { ok, data: cohort } = await fetchCohort(data.cohortId);
+          if (ok) setCohort(cohort);
+        }
+
+        if (data.classeId) {
+          const classe = await fetchClass(data.classeId);
+          if (ok) setClasse(classe);
+        }
       } catch (e) {
         if (e.code === "INVALID_BODY" || e.code === "OPERATION_UNAUTHORIZED") setError(true);
         capture(e);
       }
+      setLoading(false);
     }
     getYoungFromToken();
   }, [token, parentId]);
 
+  if (loading) return <Loader />;
   if (error) return <Redirect to="/representants-legaux/token-invalide" />;
-  return <RepresentantsLegauxContext.Provider value={{ young, token }}>{children}</RepresentantsLegauxContext.Provider>;
+  return <RepresentantsLegauxContext.Provider value={{ young, token, classe, cohort }}>{children}</RepresentantsLegauxContext.Provider>;
 };
 
 export default RepresentantsLegauxContextProvider;
