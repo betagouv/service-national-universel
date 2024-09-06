4e908bd3c0c1c9ef16f2280d1d912a1747fac0e5
hlecourt <hlecourt99@gmail.com>
2024-09-06 14:57:50 +0200

    up

--- api/src/crons/classesStatusUpdate.js
+++ api/src/crons/classesStatusUpdate.js
@@ -1,8 +1,8 @@
-const config = require("config");
+// const config = require("../../config");
 const slack = require("../slack");
 const { capture } = require("../sentry");
 const { CohortModel, ClasseModel, YoungModel } = require("../models");
-const ClasseStateManager = require("../cle/classe/stateManager");
+const ClasseStateManager = require("../cle/classe/stateManager").default;
 
 exports.handler = async () => {
   try {
@@ -10,8 +10,9 @@ exports.handler = async () => {
     let totalOpen = 0;
     const resultClosed = [];
     let totalClosed = 0;
+
     //For now this logic (perf wise) is ok but we need to monitor after the first 2025 cohort opening
-    const cursor = CohortModel.find({ type: "CLE", inscriptionEndDate: { $gte: new Date() } }).cursor();
+    const cursor = CohortModel.find({ type: "CLE" }).cursor(); //dateStart: { $gte: new Date() }
 
     await cursor.addCursorFlag("noCursorTimeout", true).eachAsync(async (cohort) => {
       // Get inscription date
@@ -22,35 +23,27 @@ exports.handler = async () => {
       const isInscriptionClosed = now >= inscriptionEndDate || now <= inscriptionStartDate;
 
       if (isInscriptionOpen) {
-        //we fetch all the classes of the cohort because we need to update them or recompute them (prevent from edge case)
-        const classes = await ClasseModel.find({ cohortId: cohort._id.toString() }).select("status");
-        //if some classe are in status CLOSED or ASSIGNED, we need to update them
-        const needUpdate = classes.some((c) => ["CLOSED", "ASSIGNED"].includes(c.status));
-        if (needUpdate) {
-          for (const classe of classes) {
-            await ClasseStateManager.compute(classe._id.toString(), { fromUser: { firstName: "Ouverture automatique des inscriptions " + cohort.name } }, { YoungModel });
-          }
-          resultOpen.push(cohort.name);
-          totalOpen += classes.length;
-        }
+        const { result, total } = await updatesClassesStatus({
+          statusList: ["CLOSED", "ASSIGNED"],
+          cohort,
+          fromUser: { firstName: "Ouverture automatique des inscriptions " + cohort.name },
+        });
+        resultOpen.push(...result);
+        totalOpen = total;
       }
 
       if (isInscriptionClosed) {
-        //we fetch all the classes of the cohort because we need to update them or recompute them (prevent from edge case)
-        const classes = await ClasseModel.find({ cohortId: cohort._id.toString() }).select("status");
-        //if some classe are in status OPEN, we need to update them
-        const needUpdate = classes.some((c) => c.status === "OPEN");
-        if (needUpdate) {
-          for (const classe of classes) {
-            await ClasseStateManager.compute(classe._id.toString(), { fromUser: { firstName: "Fermeture automatique des inscriptions " + cohort.name } }, { YoungModel });
-          }
-          resultClosed.push(cohort.name);
-          totalClosed += classes.length;
-        }
+        const { result, total } = await updatesClassesStatus({
+          statusList: ["OPEN"],
+          cohort,
+          fromUser: { firstName: "Fermeture automatique des inscriptions " + cohort.name },
+        });
+        resultClosed.push(...result);
+        totalClosed = total;
       }
     });
 
-    slack.success({
+    await slack.success({
       title: "ClassesStatusUpdate",
       text: `Open: ${totalOpen} classes updated for cohorts: ${resultOpen.join(", ")}. Closed: ${totalClosed} classes updated for cohorts: ${resultClosed.join(", ")}`,
     });
@@ -58,5 +51,23 @@ exports.handler = async () => {
     capture(e);
     slack.error({ title: "ClassesStatusUpdate", text: JSON.stringify(e) });
     throw e;
+  }
+};
+
+const updatesClassesStatus = async ({ statusList, cohort, fromUser }) => {
+  const result = [];
+  let total = 0;
+  //we fetch all the classes of the cohort because we need to update them or recompute them (prevent from edge case)
+  const classes = await ClasseModel.find({ cohortId: cohort._id.toString(), status: { $ne: "WITHDRAWN" } }).select({ status: 1 });
+  //if some classe are in status CLOSED or ASSIGNED, we need to update them
+  const needUpdate = classes.some((c) => statusList.includes(c.status));
+  if (needUpdate) {
+    for (const classe of classes) {
+      await ClasseStateManager.compute(classe._id.toString(), fromUser, { YoungModel });
+    }
+    result.push(cohort.name);
+    total += classes.length;
+
+    return { result, total };
   }
 };
