f28a2eb078db906a578f676e598e12e097828b5d
Guillaume <guillaumepichon91@gmail.com>
2024-09-06 16:46:45 +0200

    update onboarding Date

--- app/src/scenes/cle/MyClass.jsx
+++ app/src/scenes/cle/MyClass.jsx
@@ -4,8 +4,20 @@ import { fr } from "date-fns/locale";
 import React from "react";
 
 export default function MyClass({ classe }) {
-  const formatDate = (dateString) => {
-    return format(new Date(dateString), "dd MMMM yyyy", { locale: fr });
+  const formatDate = (dateString, dateFormat) => {
+    return format(new Date(dateString), dateFormat, { locale: fr });
+  };
+
+  const formatDateRange = (dateStart, dateEnd) => {
+    const startMonth = formatDate(dateStart, "MMMM yyyy");
+    const endMonth = formatDate(dateEnd, "MMMM yyyy");
+
+    // Si le mois et l'année sont les mêmes
+    if (startMonth === endMonth) {
+      return `Du ${formatDate(dateStart, "dd")} au ${formatDate(dateEnd, "dd MMMM yyyy")}`;
+    }
+    // Si les mois sont différents
+    return `Du ${formatDate(dateStart, "dd MMMM yyyy")} au ${formatDate(dateEnd, "dd MMMM yyyy")}`;
   };
 
   let fields = [
@@ -26,7 +38,7 @@ export default function MyClass({ classe }) {
   if (classe?.cohortDetails?.dateStart && classe?.cohortDetails?.dateEnd) {
     fields.push({
       label: "Date de séjour",
-      value: `Du ${formatDate(classe?.cohortDetails?.dateStart)} au ${formatDate(classe?.cohortDetails?.dateEnd)}`,
+      value: formatDateRange(classe?.cohortDetails?.dateStart, classe?.cohortDetails?.dateEnd),
     });
   }
 
