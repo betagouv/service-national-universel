b61413d8a6be160bad32f12b5be47b03b438d532
C2Chandelier <charlelie.chandelier@epitech.eu>
2024-09-09 16:44:59 +0200

    up

--- api/migrations/20240906151116-fix_youngs_status_equivalence.js
+++ api/migrations/20240906151116-fix_youngs_status_equivalence.js
@@ -6,25 +6,21 @@ module.exports = {
     const youngs = await YoungModel.find({
       statusPhase1: { $in: ["DONE", "EXEMPTED"] },
       status_equivalence: { $in: ["WAITING_VERIFICATION", "WAITING_CORRECTION"] },
+    }).select({ _id: 1 });
+    logger.info(`Found ${youngs.length} youngs to update.`);
+    const youngIds = youngs.map(({ _id }) => _id);
+    const validatedEquivalences = await MissionEquivalenceModel.find({
+      youngId: { $in: youngIds },
+      status: "VALIDATED",
     });
-
-    let modifiedYoungs = [];
-
-    for (const young of youngs) {
-      const validatedEquivalence = await MissionEquivalenceModel.findOne({
-        youngId: young._id,
-        status: "VALIDATED",
-      });
-
-      if (validatedEquivalence) {
-        young.set("status_equivalence", "VALIDATED");
-        await young.save();
-        modifiedYoungs.push(young);
-      }
-    }
-    if (modifiedYoungs.length > 0) {
-      logger.info(`Updated ${modifiedYoungs.length} youngs status equivalence`);
-      await db.collection("youngs_modified").insertMany(modifiedYoungs);
-    }
+    const validatedYoungIds = validatedEquivalences.map(({ youngId }) => youngId);
+    await YoungModel.updateMany(
+      {
+        _id: { $in: validatedYoungIds },
+      },
+      {
+        $set: { status_equivalence: "VALIDATED" },
+      },
+    );
   },
 };
