e98d3c323397ecda115a856411cfb8290c5b9036
Guillaume <guillaumepichon91@gmail.com>
2024-09-13 16:25:15 +0200

    fix: getCohortPeriod with DateFns

--- packages/lib/src/sessions.ts
+++ packages/lib/src/sessions.ts
@@ -3,6 +3,8 @@ import { YOUNG_STATUS, YOUNG_STATUS_PHASE1 } from "./constants/constants";
 import { isCle } from "./young";
 import { getZonedDate } from "./utils/date";
 import { EtablissementDto } from "./dto";
+import { format } from "date-fns";
+import { fr } from "date-fns/locale";
 
 const COHORTS_WITH_JDM_COUNT = ["2019", "2020", "2021", "2022", "Février 2022", "Juin 2022", "Juillet 2022", "Février 2023 - C", "Avril 2023 - B", "Avril 2023 - A", "Juin 2023"];
 
@@ -24,24 +26,31 @@ const getCohortYear = (cohort) => cohort?.dateStart?.slice(0, 4);
 const getCohortPeriod = (cohort, withBold = false) => {
   if (!cohort.dateStart || !cohort.dateEnd) return cohort.name || cohort;
   if (cohort.name === "à venir") return "à venir";
-  const startDate = getZonedDate(cohort.dateStart);
-  const endDate = getZonedDate(cohort.dateEnd);
 
-  const endDateformatOptions = { year: "numeric", month: "long", day: "numeric" };
-  const startDateformatOptions: Partial<typeof endDateformatOptions> = { day: "numeric" };
-  if (startDate.getMonth() !== endDate.getMonth()) {
-    startDateformatOptions.month = "long";
+  // Fonction pour formater les dates avec la localisation française
+  const formatDate = (dateString, dateFormat) => {
+    return format(new Date(dateString), dateFormat, { locale: fr });
+  };
+
+  // On simplifie le rendu
+  const startMonthYear = formatDate(cohort.dateStart, "MMMM yyyy");
+  const endMonthYear = formatDate(cohort.dateEnd, "MMMM yyyy");
+
+  let formattedPeriod;
+  if (startMonthYear === endMonthYear) {
+    // Si même mois et même année, on affiche juste les jours avec le mois et l'année à la fin
+    formattedPeriod = `du ${formatDate(cohort.dateStart, "dd")} au ${formatDate(cohort.dateEnd, "dd MMMM yyyy")}`;
+  } else {
+    // Si les mois ou les années sont différents, on affiche les deux dates complètes
+    formattedPeriod = `du ${formatDate(cohort.dateStart, "dd MMMM yyyy")} au ${formatDate(cohort.dateEnd, "dd MMMM yyyy")}`;
   }
-  if (startDate.getFullYear() !== endDate.getFullYear()) {
-    startDateformatOptions.year = "numeric";
-  }
-  const formattedStart = new Intl.DateTimeFormat("fr-FR", startDateformatOptions as any).format(startDate);
-  const formattedEnd = new Intl.DateTimeFormat("fr-FR", endDateformatOptions as any).format(endDate);
 
-  if (withBold) return `du <b>${formattedStart} au ${formattedEnd}</b>`;
+  // Si `withBold` est activé, on retourne le texte avec des balises <b>
+  if (withBold) return `du <b>${formattedPeriod}</b>`;
 
-  return `du ${formattedStart} au ${formattedEnd}`;
+  return formattedPeriod;
 };
+
 
 const formatShortCohortPeriod = (cohort) => {
   if (!cohort.dateStart || !cohort.dateEnd) return cohort.name || cohort;
