1df8288bc7e9eaf254654f5f28acb015f8891ae7
Michael Garcia <mika_garcia21@hotmail.com>
2024-09-13 14:40:40 +0200

    chore(api): mise a jour version secret manager

--- api/config/ci.js
+++ api/config/ci.js
@@ -6,7 +6,7 @@ if (!secretKey) {
   throw new Error("SCW_SECRET_KEY is required to get configuration secrets");
 }
 
-const REVISION = 7;
+const REVISION = 8;
 const secrets = getSecrets(secretKey, CI_PROJECT_ID, "snu-ci", REVISION);
 
 module.exports = {
