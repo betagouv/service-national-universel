3fbc6842e5982d1c2eafb06e533a6c88214c3ab3
Eric SANCHEZ <eric.cmsfr@gmail.com>
2024-09-16 16:09:04 +0200

    feat(api): 3240 - migrate - create CLE 2025 cohort and add verified classes without cohort

---
+++ api/migrations/20240913131927-create-cohorte-cle-2025-globale.js
@@ -0,0 +1,55 @@
+const { STATUS_CLASSE } = require("snu-lib");
+const { CohortModel, ClasseModel } = require("../src/models");
+const { logger } = require("../src/logger");
+
+const cohortName = "CLE 2025";
+
+module.exports = {
+  async up() {
+    try {
+      const classesVerifiedAndNoCohort = await ClasseModel.find({ $or: [{ cohortId: { $exists: false } }, { cohortId: null }], status: STATUS_CLASSE.VERIFIED });
+      const cohort = new CohortModel({
+        snuId: cohortName,
+        name: cohortName,
+        eligibility: {
+          zones: ["A", "B", "C"],
+          schoolLevels: ["4eme", "3eme", "2ndePro", "2ndeGT", "1erePro", "1ereGT", "TermPro", "TermGT", "CAP", "1ereCAP", "2ndeCAP", "Autre", "NOT_SCOLARISE"],
+          bornAfter: new Date("2010-01-01"),
+          bornBefore: new Date("2007-12-31"),
+        },
+        inscriptionStartDate: new Date("2024-09-18"),
+        inscriptionEndDate: new Date("2024-11-30"),
+        instructionEndDate: new Date("2024-11-30"),
+        buffer: 1,
+        event: "Event Name",
+      });
+
+      const cohortCle2025Saved = await cohort.save();
+      logger.info(`Cohorte ${cohortName} is created with success (id: ${cohortCle2025Saved._id})`);
+
+      const updateResult = await ClasseModel.updateMany({ _id: { $in: classesVerifiedAndNoCohort.map((classe) => classe._id) } }, { $set: { cohortId: cohortCle2025Saved._id } });
+      logger.info(`${updateResult.modifiedCount} classes added to cohort ${cohortName}`);
+    } catch (error) {
+      logger.error(`Error while migration up: ${error.message}`);
+      throw error;
+    }
+  },
+
+  async down() {
+    try {
+      const cohortCle2025ToDelete = await CohortModel.findOne({ name: cohortName });
+
+      if (cohortCle2025ToDelete === null) {
+        logger.info(`Cohorte ${cohortName} not found`);
+        return;
+      }
+
+      const updateResult = await ClasseModel.updateMany({ cohortId: cohortCle2025ToDelete._id }, { $set: { cohortId: null } });
+      logger.info(`${updateResult.modifiedCount} Classes removed from cohort ${cohortName}`);
+      await CohortModel.deleteOne({ name: cohortName });
+      logger.info(`Cohorte ${cohortName} is deleted with success`);
+    } catch (error) {
+      logger.error(`Error while migration down: ${error.message}`);
+    }
+  },
+};
