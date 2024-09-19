00be44972c1526b74bd1f0626fb9829dd8345c84
pitiscarf <romain.thebaud@beta.gouv.fr>
2024-09-19 09:26:26 +0200

    test job in custom env

--- api/src/scripts/reindex_es_all_models/docker/start_reindex_es.sh
+++ api/src/scripts/reindex_es_all_models/docker/start_reindex_es.sh
@@ -5,9 +5,11 @@ cd "$(dirname $0)/.."
 set -o pipefail
 set -e
 
-if [ -z $GROUP_NAME -o $GROUP_NAME === "none"]; then
+if [[ -z $GROUP_NAME || $GROUP_NAME == "none" ]]; then
     echo "Usage $0 <centre|cle|useful|all>"
     exit 1
 fi
 
-npx -y ts-node ../reindex_es_all_models/index.js $GROUP_NAME
+ls
+
+node ./index.js $GROUP_NAME
--- terraform/environments/ci/custom/main.tf
+++ terraform/environments/ci/custom/main.tf
@@ -167,6 +167,25 @@ resource "scaleway_container" "app" {
   }
 }
 
+
+resource scaleway_job_definition reindex_es {
+  project_id  = local.project_id
+  name = "reindex_es"
+  description = "Reindex models in Elasticsearch"
+  cpu_limit = 2048
+  memory_limit = 4096
+  image_uri = "${scaleway_registry_namespace.main.endpoint}/api:${var.api_image_tag}"
+  command = "./api/src/scripts/reindex_es_all_models/docker/start_reindex_db.sh"
+  timeout = "2h"
+
+  env = {
+    "SCW_PROJECT_ID" = local.project_id
+    "SCW_SECRET_KEY" = local.secrets.SCW_SECRET_KEY
+    "GROUP_NAME" = "useful"
+    "NODE_ENV" = "ci"
+  }
+}
+
 output "api_endpoint" {
   value = "https://${local.api_hostname}"
 }
