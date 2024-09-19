5087209832f9686520c5014cd96f05e047216b12
pitiscarf <romain.thebaud@beta.gouv.fr>
2024-09-19 09:35:30 +0200

    fix custom stack

--- terraform/environments/ci/custom/main.tf
+++ terraform/environments/ci/custom/main.tf
@@ -174,7 +174,7 @@ resource scaleway_job_definition reindex_es {
   description = "Reindex models in Elasticsearch"
   cpu_limit = 2048
   memory_limit = 4096
-  image_uri = "${scaleway_registry_namespace.main.endpoint}/api:${var.api_image_tag}"
+  image_uri = "${data.scaleway_registry_namespace.main.endpoint}/api:${var.api_image_tag}"
   command = "./api/src/scripts/reindex_es_all_models/docker/start_reindex_db.sh"
   timeout = "2h"
 
