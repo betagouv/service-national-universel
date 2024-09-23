e6173e606e451663b30709b554a23ddf149c3022
Guillaume <guillaumepichon91@gmail.com>
2024-09-23 17:05:17 +0200

    feat(app, api): 3238 - Change wording in convocation

--- app/src/scenes/phase1/scenes/affected/components/TodoBackpack.jsx
+++ app/src/scenes/phase1/scenes/affected/components/TodoBackpack.jsx
@@ -82,7 +82,7 @@ export default function TodoBackpack({ lunchBreak, data }) {
           <div className="flex items-baseline gap-4">
             <input type="checkbox" name="collation" id="collation" checked={todo.collation} onChange={handleCheck} />
             <label htmlFor="collation">
-              Une <strong>collation ou un déjeuner froid</strong> pour le repas.
+              Un <strong>repas froid</strong> pour le repas.
             </label>
           </div>
         )}
