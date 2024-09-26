1894c795e9970d1234fb2fb1f357032dd6bf071d
Nicolas Bretécher <nicolas.bretecher@selego.co>
2024-09-26 09:35:00 +0200

    feat(app): 3072 - Amélioration du tracking pour l'API Engagement (#4359)

--- app/src/scenes/preinscription/steps/stepConfirm.jsx
+++ app/src/scenes/preinscription/steps/stepConfirm.jsx
@@ -12,7 +12,6 @@ import api from "../../../services/api";
 import plausibleEvent from "../../../services/plausible";
 import dayjs from "dayjs";
 import DSFRContainer from "../../../components/dsfr/layout/DSFRContainer";
-import ProgressBar from "../components/ProgressBar";
 import { environment, supportURL } from "@/config";
 import InfoMessage from "../components/InfoMessage";
 
@@ -118,6 +117,7 @@ export default function StepConfirm() {
           await cohortsInit();
           dispatch(setYoung(user));
           removePersistedData();
+
           history.push(isEmailValidationEnabled ? "/preinscription/email-validation" : "/preinscription/done");
         }
       }
