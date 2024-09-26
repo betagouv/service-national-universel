f703e57a3e2018cc3bcf5678e6289da77d74efa8
C2Chandelier <charlelie.chandelier@gmail.com>
2024-09-26 09:56:55 +0200

    fix(api): reduction du temps de reponse de l'export des classes (#4370)

--- admin/src/scenes/classe/utils/index.ts
+++ admin/src/scenes/classe/utils/index.ts
@@ -190,8 +190,6 @@ interface ReferentClasse extends ReferentType {
 }
 
 export interface ClasseExport extends ClasseType {
-  nb_classe: number;
-  nb_young: number;
   referents: ReferentClasse[];
   referentEtablissement: {
     firstName: string;
@@ -237,7 +235,7 @@ export function exportExcelSheet(classes: ClasseExport[], type: typeExport) {
       studentInProgress: c.studentInProgress ?? 0,
       studentWaiting: c.studentWaiting ?? 0,
       studentWithdrawn: c.studentWithdrawn ?? 0,
-      dossierOuvert: `${((c.studentWaiting ?? 0 + c.studentInProgress ?? 0 + c.studentValidated ?? 0) / c.totalSeats) * 100} %`,
+      dossierOuvert: `${Math.round(((c.studentWaiting ?? 0 + c.studentInProgress ?? 0 + c.studentValidated ?? 0) / c.totalSeats) * 100)} %`,
       academy: c.academy,
       region: c.region,
       department: c.department,
