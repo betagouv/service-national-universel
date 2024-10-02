a5a17489f67532fa25fca85df229a7d7e99021ac
C2Chandelier <charlelie.chandelier@epitech.eu>
2024-10-02 12:02:59 +0200

    up

--- api/migrations/20241002091833-rattrappage-youngsInClasse-wrongCohort.js
+++ api/migrations/20241002091833-rattrappage-youngsInClasse-wrongCohort.js
@@ -10,7 +10,15 @@ module.exports = {
       const youngs = await YoungModel.find({ classeId: classe._id });
       for (const young of youngs) {
         if (young.cohortId !== classe.cohortId) {
-          young.set({ cohortId: classe.cohortId, cohort: classe.cohort });
+          const originalCohortId = young.cohortId;
+          const originalCohortName = young.cohort;
+          young.set({
+            cohortId: classe.cohortId,
+            cohort: classe.cohort,
+            originalCohort: originalCohortName,
+            originalCohortId: originalCohortId,
+            cohortChangeReason: `Import SI-SNU`,
+          });
           await young.save({ fromUser: { firstName: "Rattrappage CLE des young avec une cohorte différente de sa classe" } });
           youngUpdated.push(young._id);
           logger.info(`Young ${young._id} updated with cohortId ${classe.cohortId} and cohort ${classe.cohort}`);
