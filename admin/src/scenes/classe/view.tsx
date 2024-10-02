53498437f880c1fa34002cdb55b5ed2d71469a47
Eric SANCHEZ <eric.cmsfr@gmail.com>
2024-10-02 21:37:41 +0200

    feat(api, admin): 3283 - Add manual inscription rights according to dynamic CLE settings

--- admin/src/scenes/classe/view.tsx
+++ admin/src/scenes/classe/view.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import dayjs from "dayjs";
@@ -7,7 +7,7 @@ import { toastr } from "react-redux-toastr";
 import { Page, Header, Badge } from "@snu/ds/admin";
 import { capture } from "@/sentry";
 import api from "@/services/api";
-import { translate, YOUNG_STATUS, STATUS_CLASSE, translateStatusClasse, COHORT_TYPE, FUNCTIONAL_ERRORS, LIMIT_DATE_ESTIMATED_SEATS, ClassesRoutes } from "snu-lib";
+import { translate, YOUNG_STATUS, STATUS_CLASSE, translateStatusClasse, COHORT_TYPE, FUNCTIONAL_ERRORS, LIMIT_DATE_ESTIMATED_SEATS, ClassesRoutes, ROLES } from "snu-lib";
 import { appURL } from "@/config";
 import Loader from "@/components/Loader";
 import { AuthState } from "@/redux/auth/reducer";
@@ -224,6 +224,23 @@ export default function View() {
     setErrors({});
   };
 
+  const canPerformManualInscriptionActions = useMemo(() => {
+    if (!cohort) return false;
+
+    switch (user.role) {
+      case ROLES.REFERENT_DEPARTMENT:
+        return cohort.inscriptionOpenForReferentDepartment === true;
+      case ROLES.REFERENT_REGION:
+        return cohort.inscriptionOpenForReferentRegion === true;
+      case ROLES.REFERENT_CLASSE:
+        return cohort.inscriptionOpenForReferentClasse === true;
+      case ROLES.ADMINISTRATEUR_CLE:
+        return cohort.inscriptionOpenForAdministrateurCle === true;
+      default:
+        return false;
+    }
+  }, [user.role, cohort]);
+
   if (!classe) return <Loader />;
 
   return (
@@ -239,7 +256,7 @@ export default function View() {
           },
           { title: "Fiche de la classe" },
         ]}
-        actions={getHeaderActionList({ user, classe, setClasse, isLoading, setIsLoading, url, id, studentStatus })}
+        actions={getHeaderActionList({ user, classe, setClasse, isLoading, setIsLoading, url, id, studentStatus, canPerformManualInscriptionActions })}
       />
       <GeneralInfos
         classe={classe}
