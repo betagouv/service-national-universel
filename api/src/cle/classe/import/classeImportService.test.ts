6fff5771b375f59952719c1365304c75d2b4b59f
C2Chandelier <charlelie.chandelier@epitech.eu>
2024-10-10 15:52:37 +0200

    fix-test

--- api/src/cle/classe/import/classeImportService.test.ts
+++ api/src/cle/classe/import/classeImportService.test.ts
@@ -85,7 +85,7 @@ describe("importClasseCohort", () => {
     const result = await importClasseCohort(filePath, importKey, ClasseImportType.NEXT_CLASSE_COHORT);
 
     expect(getFile).toHaveBeenCalledWith(filePath);
-    expect(readCSVBuffer).toHaveBeenCalledWith(Buffer.from("some mock data"), true);
+    expect(readCSVBuffer).toHaveBeenCalledWith(Buffer.from("some mock data"));
     expect(mapClassesCohortsForSept2024).toHaveBeenCalledWith(mockCSV, ClasseImportType.NEXT_CLASSE_COHORT);
     expect(addCohortToClasseByCohortSnuId).toHaveBeenCalledTimes(2);
     expect(addCohortToClasseByCohortSnuId).toHaveBeenCalledWith({ classeId: "1", cohortCode: "IDF_101" }, importKey, ClasseImportType.NEXT_CLASSE_COHORT);
