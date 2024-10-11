475b59f094833568ab3e50b32d6a2577c9a4caa1
C2Chandelier <charlelie.chandelier@epitech.eu>
2024-10-11 14:54:28 +0200

    up

--- api/migrations/20241011121242-rattrappage_PDR_Academie.js
+++ api/migrations/20241011121242-rattrappage_PDR_Academie.js
@@ -1,11 +1,11 @@
-const { PointDeRassemblementModel } = require("../src/models");
+const { PointDeRassemblementModel, PlanTransportModel } = require("../src/models");
 const { departmentToAcademy } = require("snu-lib");
 const { logger } = require("../src/logger");
 
 module.exports = {
   async up(db, client) {
-    const PointDeRassemblements = await PointDeRassemblementModel.find({ academie: { $exists: false } });
-    for (const pointDeRassemblement of PointDeRassemblements) {
+    const pointDeRassemblements = await PointDeRassemblementModel.find({ academie: { $exists: false } });
+    for (const pointDeRassemblement of pointDeRassemblements) {
       let academie = departmentToAcademy[pointDeRassemblement.department];
       if (!academie) {
         logger.error(`No academy found for department ${pointDeRassemblement.department}`);
@@ -14,6 +14,21 @@ module.exports = {
       }
       pointDeRassemblement.set({ academie });
       await pointDeRassemblement.save({ fromUser: { firstName: "Migration mapper department to academy for pointDeRassemblement" } });
+    }
+
+    const planDeTransports = await PlanTransportModel.find({ cohort: "Toussaint 2024" });
+    for (const planDeTransport of planDeTransports) {
+      const pointDeRassemblements = planDeTransport.pointDeRassemblements;
+      for (const pointDeRassemblement of pointDeRassemblements) {
+        let academie = departmentToAcademy[pointDeRassemblement.department];
+        if (!academie) {
+          logger.error(`No academy found for department ${pointDeRassemblement.department}`);
+          academie = "NO_ACADEMY";
+          continue;
+        }
+        pointDeRassemblement.academie = academie;
+      }
+      await planDeTransport.save({ fromUser: { firstName: "Migration mapper department to academy for pointDeRassemblement in planDeTransport" } });
     }
   },
 };
