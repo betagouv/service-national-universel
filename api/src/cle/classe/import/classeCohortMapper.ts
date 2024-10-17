a7db0f6b98205ecc96ef0f7ac461cfd7dad506b3
C2Chandelier <charlelie.chandelier@gmail.com>
2024-10-17 15:37:37 +0200

    feat(api): Modification su service de l'import des classes pour coller au nouveau besoin (#4452)

--- api/src/cle/classe/import/classeCohortMapper.ts
+++ api/src/cle/classe/import/classeCohortMapper.ts
@@ -6,6 +6,7 @@ export const mapClassesCohortsForSept2024 = (classesChortes: ClasseCohortCSV[], 
       const classeCohorteMapped: ClasseCohortMapped = {
         classeId: classeCohorte["Identifiant de la classe engagée"],
         cohortCode: classeCohorte["Session formule"],
+        classeTotalSeats: classeCohorte["Effectif de jeunes concernés"],
         centerCode: classeCohorte["Désignation du centre"],
         pdrCode: classeCohorte["Code point de rassemblement initial"],
         sessionCode: `${classeCohorte["Session : Code de la session"]}_${classeCohorte["Désignation du centre"]}`,
