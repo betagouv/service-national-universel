dcfeda586061b7370ebc9124c430d41dc08f3ef4
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2024-10-29 14:01:44 +0100

    improve migration

--- api/migrations/20241023142732-add-cohort-status.js
+++ api/migrations/20241023142732-add-cohort-status.js
@@ -1,11 +1,20 @@
 const { COHORT_STATUS } = require("snu-lib");
 const { CohortModel } = require("../src/models");
+const archivedCohortNames = ["2019", "2020", "2021"];
 
 module.exports = {
   async up() {
-    const archivedCohortNames = ["2019", "2020", "2021"];
-    await CohortModel.updateMany({ name: { $in: archivedCohortNames } }, { $set: { status: COHORT_STATUS.ARCHIVED } });
-    await CohortModel.updateMany({ name: { $nin: archivedCohortNames } }, { $set: { status: COHORT_STATUS.PUBLISHED } });
+    const cohortsToArchive = await CohortModel.find({ name: { $in: archivedCohortNames } });
+    for (const cohort of cohortsToArchive) {
+      cohort.set({ status: COHORT_STATUS.ARCHIVED });
+      await cohort.save({ fromUser: { firstName: "Add status migration" } });
+    }
+
+    const cohortsToPublish = await CohortModel.find({ name: { $nin: archivedCohortNames } });
+    for (const cohort of cohortsToPublish) {
+      cohort.set({ status: COHORT_STATUS.PUBLISHED });
+      await cohort.save({ fromUser: { firstName: "Add status migration" } });
+    }
   },
 
   async down() {
