57daab70b09f429b3bcdfbc6b9dd9876a1a89b4b
hlecourt <50077368+hlecourt@users.noreply.github.com>
2024-10-31 15:04:00 +0100

    feat(api,admin,app): 3229 - POC Régionalisation de l’éligibilité front (#4290)
    Co-authored-by: Anselme Chorein <achorein@users.noreply.github.com>
    Co-authored-by: Anselme <anselme@chorein.fr>

---
+++ admin/src/scenes/settings/tabs/EligibilityTab.tsx
@@ -0,0 +1,225 @@
+import { Select } from "@snu/ds/admin";
+import React, { useEffect, useState } from "react";
+import { CohortDto, GRADES, region2department, translate, translateGrade } from "snu-lib";
+import DatePickerInput from "@/components/ui/forms/dateForm/DatePickerInput";
+import ButtonPrimary from "@/components/ui/buttons/ButtonPrimary";
+import { BiLoaderAlt } from "react-icons/bi";
+import { toastr } from "react-redux-toastr";
+import { capture } from "@/sentry";
+import api from "@/services/api";
+import { IoWarningOutline } from "react-icons/io5";
+import ReactTooltip from "react-tooltip";
+
+type EligibilityTabsProps = {
+  cohort?: CohortDto;
+  readOnly?: boolean;
+  getCohort: () => void;
+};
+
+type EligibilityForm = {
+  zones: string[];
+  schoolLevels: string[];
+  bornAfter: Date | null;
+  bornBefore: Date | null;
+};
+
+type ErrorForm = {
+  zones?: string;
+  schoolLevels?: string;
+  bornAfter?: string;
+  bornBefore?: string;
+  interval?: string;
+};
+
+region2department["Étranger"] = ["Etranger"]; // Add Etranger to the list of regions because eligibility can be set to Etranger
+
+export default function EligibilityTab({ cohort, readOnly, getCohort }: EligibilityTabsProps) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<ErrorForm>({});
+  const [formValues, setFormValues] = useState<EligibilityForm>({
+    zones: [],
+    schoolLevels: [],
+    bornAfter: null,
+    bornBefore: null,
+  });
+
+  useEffect(() => {
+    setFormValues({
+      zones: cohort?.eligibility?.zones || [],
+      schoolLevels: cohort?.eligibility?.schoolLevels || [],
+      bornAfter: cohort?.eligibility?.bornAfter || null,
+      bornBefore: cohort?.eligibility?.bornBefore || null,
+    });
+  }, [cohort]);
+
+  const onSubmit = async () => {
+    try {
+      const err: ErrorForm = {};
+      if (!formValues.bornAfter) err.bornAfter = "La date est obligatoire";
+      if (!formValues.bornBefore) err.bornBefore = "La date est obligatoire";
+      if (formValues.bornAfter && formValues.bornBefore && formValues.bornAfter > formValues.bornBefore) {
+        err.interval = "L'interval de date n'est pas valide";
+      }
+
+      if (formValues.zones.length === 0) err.zones = "Veuillez sélectionner au moins un département";
+      if (formValues.schoolLevels.length === 0) err.schoolLevels = "Veuillez sélectionner au moins un niveau scolaire";
+
+      setError(err);
+      if (Object.values(err).length > 0) {
+        return toastr.warning("Information", "Veuillez corriger les erreurs dans le formulaire");
+      }
+
+      setIsLoading(true);
+      const { ok, code } = await api.put(`/cohort/${cohort?._id?.toString()}/eligibility`, formValues);
+      if (!ok) {
+        toastr.error("Oups, une erreur est survenue lors de la mise à jour de l'élégibilité", translate(code));
+      }
+      toastr.success("Information", "La session a bien été mise à jour");
+    } catch (e) {
+      capture(e);
+      toastr.error("Oups, une erreur est survenue lors de la mise à jour de la session", "");
+    } finally {
+      await getCohort();
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex w-full flex-col gap-8">
+      <div className="flex flex-col gap-8 rounded-xl bg-white px-8 pb-12 pt-8 shadow-[0_8px_16px_0_rgba(0,0,0,0.05)]">
+        <div className="flex w-full flex-col gap-8">
+          <p className="text-lg font-medium leading-5 text-gray-900">Éligibilités par dates de naissance</p>
+          <div className="flex w-full gap-4">
+            <DatePickerInput
+              className={""}
+              mode="single"
+              label="Né après le"
+              placeholder="JJ/MM/AAAA"
+              isTime={false}
+              // @ts-ignore
+              value={formValues.bornAfter}
+              onChange={(e) => setFormValues({ ...formValues, bornAfter: e })}
+              error={error?.bornAfter || ""}
+              readOnly={false}
+              disabled={isLoading || readOnly}
+            />
+            <DatePickerInput
+              className={""}
+              mode="single"
+              label="Né avant le"
+              placeholder="JJ/MM/AAAA"
+              isTime={false}
+              // @ts-ignore
+              value={formValues.bornBefore}
+              onChange={(e) => setFormValues({ ...formValues, bornBefore: e })}
+              error={error.bornBefore || ""}
+              readOnly={false}
+              disabled={isLoading || readOnly}
+            />
+          </div>
+        </div>
+        {error?.interval && <div className="text-[#EF4444]">{error?.interval}</div>}
+      </div>
+      <div className="flex flex-col gap-8 rounded-xl bg-white px-8 pb-12 pt-8 shadow-[0_8px_16px_0_rgba(0,0,0,0.05)]">
+        <div className="flex w-full flex-col gap-8">
+          <p className="text-lg font-medium leading-5 text-gray-900">Éligibilités par situations scolaire</p>
+          <div className="flex row w-full gap-4 justify-center">
+            {Object.keys(GRADES).map((grade, index) => (
+              <div key={grade + "grade" + index} className="flex h-6 items-center">
+                <input
+                  id={grade}
+                  name={grade}
+                  type="checkbox"
+                  disabled={isLoading || readOnly}
+                  checked={formValues.schoolLevels.includes(grade)}
+                  className="h-4 w-4 rounded border-gray-300 text-blue-600 cursor-pointer"
+                  onChange={(e) => {
+                    const newValues = e.target.checked ? [...formValues.schoolLevels, grade] : formValues.schoolLevels.filter((g) => g !== grade);
+                    setFormValues((prev: EligibilityForm) => ({ ...prev, schoolLevels: newValues }));
+                  }}
+                />
+                <label htmlFor={grade} className="mt-1.5 ml-2 text-sm cursor-pointer whitespace-nowrap">
+                  {translateGrade(grade)}
+                </label>
+              </div>
+            ))}
+          </div>
+          {error.schoolLevels && <div className="text-[#EF4444]">{error.schoolLevels}</div>}
+        </div>
+      </div>
+      <div className="flex flex-col gap-8 rounded-xl bg-white px-8 pb-12 pt-8 shadow-[0_8px_16px_0_rgba(0,0,0,0.05)]">
+        <div className="flex w-full flex-col gap-8">
+          <p className="text-lg font-medium leading-5 text-gray-900">Éligibilités par départements</p>
+
+          <div className="grid grid-cols-2 gap-10">
+            {Object.keys(region2department).map((region, index) => {
+              const departmentsSelected = formValues?.zones?.filter((dep) => region2department[region].includes(dep));
+              const isAllSelected = departmentsSelected.length === region2department[region].length;
+
+              return (
+                <div key={region + "eligibility" + index} className="flex flex-col gap-0">
+                  <div className="flex justify-between">
+                    <div className="flex items-center gap-2">
+                      <label className="text-sm text-gray-500 mt-1.5 ml-2">{region}</label>
+                      {!departmentsSelected.length && (
+                        <>
+                          <IoWarningOutline className="h-4 w-4 text-amber-500 cursor-pointer" data-tip data-for={region + "tooltip"} />
+                          <ReactTooltip id={region + "tooltip"} type="light" place="top" effect="solid" className="custom-tooltip-radius !opacity-100 !shadow-md">
+                            <p className=" w-[275px] list-outside !px-2 !py-1.5 text-left text-xs text-gray-600">Aucun département n'a été sélectionné dans cette région</p>
+                          </ReactTooltip>
+                        </>
+                      )}
+                    </div>
+                    <div className="flex items-center gap-2">
+                      <label htmlFor={region + "selectAll"} className="mt-1.5 ml-2 text-sm cursor-pointer whitespace-nowrap">
+                        Sélectionner tout
+                      </label>
+                      <input
+                        id={region + "selectAll"}
+                        name={region + "selectAll"}
+                        type="checkbox"
+                        checked={isAllSelected}
+                        disabled={isLoading || readOnly}
+                        className="h-4 w-4 rounded border-gray-300 text-blue-600 cursor-pointer"
+                        onChange={(e) => {
+                          const newValues = e.target.checked
+                            ? [...new Set([...formValues.zones, ...region2department[region]])]
+                            : formValues.zones.filter((dep) => !region2department[region].includes(dep));
+                          setFormValues((prev: EligibilityForm) => ({ ...prev, zones: newValues }));
+                        }}
+                      />
+                    </div>
+                  </div>
+                  <Select
+                    isMulti={true}
+                    isClearable={true}
+                    placeholder="Sélectionner un ou plusieurs départements"
+                    disabled={isLoading || readOnly}
+                    options={region2department[region].map((dep) => ({ label: dep, value: dep }))}
+                    value={departmentsSelected.map((dep) => ({ label: dep, value: dep }))}
+                    onChange={(data) => {
+                      const newValues = data.map((option) => option.value);
+                      setFormValues((prev: EligibilityForm) => ({
+                        ...prev,
+                        zones: [...prev.zones.filter((dep) => !region2department[region].includes(dep)), ...newValues],
+                      }));
+                    }}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        {error?.zones && <div className="text-[#EF4444]">{error.zones}</div>}
+      </div>
+      {!readOnly && (
+        <div className="flex items-center justify-center gap-3 ">
+          <ButtonPrimary disabled={isLoading} className="h-[50px] w-[300px]" onClick={onSubmit}>
+            {isLoading && <BiLoaderAlt className="h-4 w-4 animate-spin" />}
+            Enregistrer
+          </ButtonPrimary>
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ packages/ds/src/admin/layout/NavbarControlled.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import cx from "classnames";
+
+type NavbarControlledProps = {
+  tabs: Array<{
+    id: string;
+    title: React.ReactNode | string;
+    label?: string;
+    leftIcon?: React.ReactNode;
+  }>;
+  active?: string;
+  onTabChange?: (id: string) => void;
+  button?: Array<React.ReactNode>;
+};
+
+export default function NavbarControlled({
+  tabs,
+  active,
+  onTabChange,
+  button,
+}: NavbarControlledProps) {
+  return (
+    <div className="flex justify-between border-b border-gray-200 mb-8">
+      <div className="flex gap-2 mt-3">
+        {tabs.map((tabItem, index) => (
+          <div
+            key={`tab-${index}`}
+            className={cx(
+              "flex gap-1.5 text-sm leading-5 font-medium text-gray-500 cursor-pointer pb-3.5",
+              {
+                "border-b-2 border-blue-600 !text-blue-600":
+                  tabItem.id === active,
+              },
+            )}
+            onClick={() => onTabChange?.(tabItem.id)}
+          >
+            <div>
+              <span
+                className={cx("text-[10px] leading-4 ml-8", {
+                  "text-gray-400": tabItem.id !== active,
+                })}
+              >
+                {tabItem.label}
+              </span>
+              <div className="flex gap-2">
+                {tabItem.leftIcon}
+                <span className="truncate mr-3">{tabItem.title}</span>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      {button && (
+        <div className="flex gap-4 pb-3">
+          {button?.map((buttonItem, index) => (
+            <div key={`button-${index}`}>{buttonItem}</div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
