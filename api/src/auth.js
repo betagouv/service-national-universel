d4539f974043749f94181c3918e619a1c53639e0
Michael Garcia <mika_garcia21@hotmail.com>
2024-11-05 11:43:08 +0100

    up

--- api/src/auth.js
+++ api/src/auth.js
@@ -33,11 +33,8 @@ const {
 
 const { serializeYoung, serializeReferent } = require("./utils/serializer");
 const { validateFirstName } = require("./utils/validator");
-
 const { getFilteredSessions } = require("./utils/cohort");
-
 const { ClasseModel, EtablissementModel, CohortModel } = require("./models");
-
 const { getFeatureFlagsAvailable } = require("./featureFlag/featureFlagService");
 
 class Auth {
