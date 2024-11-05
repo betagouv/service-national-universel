f0a632337bd219c108c995f0f876a205e9087d3e
Michael Garcia <mika_garcia21@hotmail.com>
2024-11-05 11:55:34 +0100

    up

--- api/src/auth.js
+++ api/src/auth.js
@@ -32,9 +32,7 @@ const {
 } = require("snu-lib");
 
 const { serializeYoung, serializeReferent } = require("./utils/serializer");
-
 const { validateFirstName } = require("./utils/validator");
-
 const { getFilteredSessions } = require("./utils/cohort");
 const { ClasseModel, EtablissementModel, CohortModel } = require("./models");
 const { getFeatureFlagsAvailable } = require("./featureFlag/featureFlagService");
