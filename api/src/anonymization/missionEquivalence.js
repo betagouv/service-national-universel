c580227b9448e00606680e865ca08804a808ea74
michaelgarcia75 <99188042+michaelgarcia75@users.noreply.github.com>
2024-11-13 14:41:41 +0100

    feat(api): anonymise newfields not specified by default (#4351)

--- api/src/anonymization/missionEquivalence.js
+++ api/src/anonymization/missionEquivalence.js
@@ -1,12 +1,42 @@
 const { generateRandomName, generateRandomEmail, generateAddress, starify } = require("../utils/anonymise");
+const { anonymizeNonDeclaredFields } = require("../utils/anonymise-model-fields");
 
-function anonymize(item) {
+function anonymize(itemToAnonymize) {
+  const whitelist = [
+    "_id.$oid",
+    "youngId",
+    "status",
+    "type",
+    "desc",
+    "sousType",
+    "structureName",
+    "address",
+    "zip",
+    "city",
+    "startDate",
+    "endDate",
+    "frequency",
+    "frequency.nombre",
+    "frequency.duree",
+    "frequency.frequence",
+    "missionDuration",
+    "contactFullName",
+    "contactEmail",
+    "files",
+    "message",
+    "createdAt",
+    "updatedAt",
+    "__v",
+  ];
+  const item = anonymizeNonDeclaredFields(itemToAnonymize, whitelist);
+
   item.message && (item.message = starify(item.message));
   item.address && (item.address = generateAddress());
   item.contactEmail && (item.contactEmail = generateRandomEmail());
   item.contactFullName && (item.contactFullName = generateRandomName() + generateRandomName());
   item.structureName && (item.structureName = generateRandomName());
   item.files && (item.files = []);
+
   return item;
 }
 
