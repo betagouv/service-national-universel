dd39e513b1c8952adc64567ed099cc982a6d140a
michaelgarcia75 <99188042+michaelgarcia75@users.noreply.github.com>
2024-11-13 14:41:41 +0100

    feat(api): anonymise newfields not specified by default (#4351)

--- api/src/anonymization/meetingPoint.js
+++ api/src/anonymization/meetingPoint.js
@@ -1,8 +1,38 @@
 const { generateAddress, starify } = require("../utils/anonymise");
+const { anonymizeNonDeclaredFields } = require("../utils/anonymise-model-fields");
 
-function anonymize(item) {
+function anonymize(itemToAnonymize) {
+  const whitelist = [
+    "_id.$oid",
+    "isValid",
+    "cohort",
+    "cohortId",
+    "busId",
+    "busExcelId",
+    "centerId",
+    "centerCode",
+    "departureAddress",
+    "departureZip",
+    "departureCity",
+    "departureDepartment",
+    "departureRegion",
+    "hideDepartmentInConvocation",
+    "departureAt",
+    "departureAtString",
+    "realDepartureAtString",
+    "returnAt",
+    "returnAtString",
+    "realReturnAtString",
+    "createdAt",
+    "updatedAt",
+    "deletedAt",
+    "__v",
+  ];
+  const item = anonymizeNonDeclaredFields(itemToAnonymize, whitelist);
+
   item.departureAddress && (item.departureAddress = generateAddress());
   item.centerCode && (item.centerCode = starify(item.centerCode));
+
   return item;
 }
 
--- api/src/anonymization/sessionPhase1.js
+++ api/src/anonymization/sessionPhase1.js
@@ -1,6 +1,52 @@
 const { starify } = require("../utils/anonymise");
+const { anonymizeNonDeclaredFields } = require("../utils/anonymise-model-fields");
 
-function anonymize(item) {
+function anonymize(itemToAnonymize) {
+  const whitelist = [
+    "_id.$oid",
+    "cohesionCenterId",
+    "cohort",
+    "cohortId",
+    "department",
+    "region",
+    "codeCentre",
+    "nameCentre",
+    "zipCentre",
+    "cityCentre",
+    "headCenterId",
+    "team",
+    "team.firstName",
+    "team.lastName",
+    "team.role",
+    "team.email",
+    "team.phone",
+    "waitingList",
+    "placesTotal",
+    "placesLeft",
+    "timeScheduleFiles",
+    "timeScheduleFiles._id",
+    "timeScheduleFiles.name",
+    "timeScheduleFiles.uploadedAt",
+    "timeScheduleFiles.size",
+    "timeScheduleFiles.mimetype",
+    "hasTimeSchedule",
+    "pedagoProjectFiles",
+    "pedagoProjectFiles._id",
+    "pedagoProjectFiles.name",
+    "pedagoProjectFiles.uploadedAt",
+    "pedagoProjectFiles.size",
+    "pedagoProjectFiles.mimetype",
+    "hasPedagoProject",
+    "dateStart",
+    "dateEnd",
+    "status",
+    "sanitaryContactEmail",
+    "createdAt",
+    "updatedAt",
+    "__v",
+  ];
+  const item = anonymizeNonDeclaredFields(itemToAnonymize, whitelist);
+
   if (!["VALIDATED", "WAITING_VALIDATION"].includes(item.status)) item.status = "WAITING_VALIDATION";
   item.team &&
     (item.team = item.team.map((member) => {
@@ -10,6 +56,7 @@ function anonymize(item) {
       member.phone && (member.phone = starify(member.phone));
       return member;
     }));
+
   return item;
 }
 
