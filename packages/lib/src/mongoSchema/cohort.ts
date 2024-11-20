865445babcd670cbec5d600a55450fe8ffe67e72
Pierre LE BIHAN <lebihanp@hotmail.fr>
2024-11-20 16:02:57 +0100

    feat(cohort): ajout d'une valeur par défaut de 8 jours pour la validation du séjour

--- packages/lib/src/mongoSchema/cohort.ts
+++ packages/lib/src/mongoSchema/cohort.ts
@@ -329,6 +329,7 @@ export const CohortSchema = {
     documentation: {
       description: "Nombre de jours nécessaire pour valider le séjour",
     },
+    default: 8,
   },
 
   daysToValidateForTerminalGrade: {
