450e691d4963080638cea7b9ea36b671e832e7d8
Pierre LE BIHAN <lebihanp@hotmail.fr>
2024-11-20 15:13:49 +0100

    feat(cron): amélioration de la gestion des jeunes avec utilisation de cursor pour le traitement asynchrone

--- api/src/crons/autoValidatePhase1.ts
+++ api/src/crons/autoValidatePhase1.ts
@@ -4,7 +4,7 @@ import { updateStatusPhase1 } from "../utils";
 import { capture } from "../sentry";
 import slack from "../slack";
 import { CohortModel, LigneBusModel, YoungModel } from "../models";
-import { getDepartureDate } from "snu-lib";
+import { getDepartureDate, YOUNG_STATUS, YOUNG_STATUS_PHASE1 } from "snu-lib";
 
 export const handler = async (): Promise<void> => {
   try {
@@ -17,22 +17,22 @@ export const handler = async (): Promise<void> => {
     if (!onGoingCohorts.length) return;
 
     for (const cohort of onGoingCohorts) {
-      const youngs = await YoungModel.find({ cohortId: cohort._id, status: YOUNG_STATUS.VALIDATED, statusPhase1: YOUNG_STATUS_PHASE1.AFFECTED });
+      const cursor = await YoungModel.find({ cohortId: cohort._id, status: YOUNG_STATUS.VALIDATED, statusPhase1: YOUNG_STATUS_PHASE1.AFFECTED }).cursor();
 
       let nbYoungs = 0;
-      for (const young of youngs) {
+      await cursor.eachAsync(async (young) => {
         const bus = await LigneBusModel.findById(young.ligneId);
         const sessionPhase1 = await CohortModel.findById(young.sessionPhase1Id);
         const dateStart = getDepartureDate(young, sessionPhase1, cohort, { bus });
 
-        if (differenceInDays(now, dateStart) !== cohort.daysToValidate) continue;
+        if (differenceInDays(now, dateStart) !== cohort.daysToValidate) return;
 
         const validationDateWithDays = addDays(new Date(dateStart), cohort.daysToValidate).toISOString();
         const modified = await updateStatusPhase1(young, validationDateWithDays, {
           firstName: `[CRON] Autovalidation de la phase 1 après ${cohort.daysToValidate} jours après le départ`,
         });
         if (modified) nbYoungs++;
-      }
+      });
 
       if (nbYoungs > 0) {
         slack.success({
