d9ae3b764c4bd57df1abf2ce8ba08869d0c3aae0
pitiscarf <romain.thebaud@beta.gouv.fr>
2024-11-21 20:19:26 +0100

    docker_start.sh for apiv2

---
+++ apiv2/docker_start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+if [[ $ENVIRONMENT != "production" && $ENVIRONMENT != "staging" && $ENVIRONMENT != "ci" ]]
+then
+  RUN_TASKS=true PORT=8087 node apiv2/dist/mainJob.js &
+  exec node apiv2/dist/main.js
+else
+  if [[ $RUN_TASKS == "true" ]]
+  then
+    exec node apiv2/dist/mainJob.js
+  else
+    exec node apiv2/dist/main.js
+  fi
+fi
