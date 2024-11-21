e2a5fc2cf4d84c54298613e1be45d5ab973b680a
C2Chandelier <charlelie.chandelier@epitech.eu>
2024-11-21 16:43:14 +0100

    up

--- admin/src/scenes/centersV2/view/CenterInformations.tsx
+++ admin/src/scenes/centersV2/view/CenterInformations.tsx
@@ -24,7 +24,7 @@ export default function Details({ center }: { center: CohesionCenterType }) {
         <Title>{center.name}</Title>
         {center?.deletedAt && <Badge title="Archivé" status="WAITING_CORRECTION" />}
       </div>
-      <div className="flex flex-col gap-8 rounded-lg bg-white px-8 pt-8 pb-12">
+      <div className="flex flex-col gap-8 !rounded-lg bg-white p-8">
         <div className="flex items-center justify-between">
           <div className="text-lg font-medium leading-6 text-gray-900">Informations générales</div>
           <div className="flex items-center text-xs leading-4 font-medium text-gray-500">
@@ -36,10 +36,10 @@ export default function Details({ center }: { center: CohesionCenterType }) {
           </div>
         </div>
         <div className="flex text-sm leading-5 font-normal text-gray-900">
-          <div className="flex w-[45%] flex-col gap-4">
-            <div className="flex flex-col gap-4">
+          <div className="flex w-[45%] flex-col gap-5">
+            <div className="flex flex-col gap-1.5">
               <div className="font-bold ">Désignations</div>
-              <div className="flex flex-col gap-1 bg-gray-50 pl-3 py-2">
+              <div className="flex flex-col gap-1 bg-gray-50 rounded-md shadow-sm pl-3 py-2">
                 <p>
                   <span className="text-gray-500 mr-2">Nom du Centre : </span>
                   {center.name && formatNameAndAddress(center.name)}
@@ -62,7 +62,7 @@ export default function Details({ center }: { center: CohesionCenterType }) {
                 </p>
               </div>
             </div>
-            <div className="flex flex-col gap-1">
+            <div className="flex flex-col gap-1.5">
               <div className="flex">
                 <div className="font-bold">Capacité maximale d'accueil</div>
                 <HiOutlineInformationCircle data-tip="" data-for="tooltip-capacite" className="ml-2 mt-0.5 text-gray-400" size={20} />
@@ -70,34 +70,34 @@ export default function Details({ center }: { center: CohesionCenterType }) {
                   <div className="text-gray-700 text-xs font-[400] w-[260px] text-center mb-1">Les modifications se font uniquement dans le module de répartition SI SNU.</div>
                 </ReactTooltip>
               </div>
-              <p className="flex items-center pl-3 bg-gray-50 h-10">{center.placesTotal}</p>
+              <p className="flex items-center pl-3 bg-gray-50 rounded-md shadow-sm h-10">{center.placesTotal}</p>
             </div>
-            <div className="flex flex-col gap-1">
+            <div className="flex flex-col gap-1.5">
               <p className="font-bold">Accessibilité PMR</p>
-              <p className="flex items-center pl-3 bg-gray-50 h-10">{translate(center.pmr)}</p>
+              <p className="flex items-center pl-3 bg-gray-50 rounded-md shadow-sm h-10">{translate(center.pmr)}</p>
             </div>
           </div>
           <div className="flex w-[10%] items-center justify-center">
-            <div className="h-4/5 w-[1px] border-r-[1px] border-gray-300"></div>
+            <div className="h-full w-[1px] border-r-[1px] border-gray-300"></div>
           </div>
           <div className="flex w-[45%] flex-col  justify-between">
-            <div className="flex flex-col gap-4">
-              <div className="flex flex-col gap-1">
+            <div className="flex flex-col gap-5">
+              <div className="flex flex-col gap-1.5">
                 <div className="font-bold">Gestionnaire ou propriétaire</div>
                 <p
-                  className={cx("flex items-center pl-3 bg-gray-50 h-10 ", {
+                  className={cx("flex items-center pl-3 bg-gray-50 rounded-md shadow-sm h-10 ", {
                     "text-gray-500 italic": !center.complement || center.complement === "",
                   })}>
                   {center.complement && center.complement !== "" ? center.complement : "Non renseigné"}
                 </p>
               </div>
-              <div className="flex flex-col gap-4">
+              <div className="flex flex-col gap-1.5">
                 <div className="font-bold">Adresse</div>
-                <div className="flex flex-col gap-1 bg-gray-50 pl-3 py-2">
+                <div className="flex flex-col gap-1 bg-gray-50 rounded-md shadow-sm pl-3 py-2">
                   <p>{center.address && formatNameAndAddress(center.address)}</p>
                   <p>{center.zip + " " + (center.city && formatNameAndAddress(center.city))}</p>
                 </div>
-                <div className="flex flex-col gap-1 bg-gray-50 pl-3 py-2">
+                <div className="flex flex-col gap-1 bg-gray-50 rounded-md shadow-sm pl-3 py-2">
                   <p>
                     <span className="text-gray-500 mr-2">Département : </span>
                     {center.department}
