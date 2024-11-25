e2bd7df5ea875fae435fee8642d3478dc56e5ba5
Michael Garcia <mika_garcia21@hotmail.com>
2024-11-25 15:02:46 +0100

    up

--- admin/src/components/ui/forms/DatePicker.tsx
+++ admin/src/components/ui/forms/DatePicker.tsx
@@ -21,33 +21,33 @@ type DatePickerProps = {
 };
 
 export default function DatePicker({ value, onChange, disabled, fromYear, toYear, mode = "single" }: DatePickerProps) {
-  const defaultMonth = React.useMemo(() => {
-    if (mode === "single") {
-      if (value && typeof value !== "string" && !(value instanceof Date)) {
-        return dayjs().toDate();
-      }
-      return value ? dayjs(value).toDate() : dayjs().toDate();
-    } else {
-      if (value && typeof value === "object" && "from" in value && value.from) {
-        return dayjs(value.from).toDate();
-      }
-      return dayjs().toDate();
-    }
-  }, [value, mode]);
-
-  const selected = React.useMemo(() => {
-    if (mode === "single") {
-      if (value && typeof value !== "string" && !(value instanceof Date)) {
-        return undefined;
-      }
-      return value ? dayjs(value).toDate() : undefined;
-    } else {
-      return {
-        from: value && typeof value === "object" && "from" in value && value.from ? dayjs(value.from).toDate() : undefined,
-        to: value && typeof value === "object" && "to" in value && value.to ? dayjs(value.to).toDate() : undefined,
-      };
-    }
-  }, [value, mode]);
+  const defaultMonth =
+    mode === "single"
+      ? value
+        ? dayjs(typeof value === "string" || value instanceof Date ? value : new Date()).toDate()
+        : dayjs(new Date()).toDate()
+      : // @ts-expect-error from
+        value?.from
+        ? // @ts-expect-error from
+          dayjs(value?.from).toDate()
+        : dayjs(new Date()).toDate();
+  const selected =
+    mode === "single"
+      ? value
+        ? dayjs(typeof value === "string" || value instanceof Date ? value : new Date()).toDate()
+        : undefined
+      : {
+          // @ts-expect-error from
+          from: value?.from
+            ? // @ts-expect-error from
+              dayjs(value?.from).toDate()
+            : undefined,
+          // @ts-expect-error to
+          to: value?.to
+            ? // @ts-expect-error to
+              dayjs(value?.to).toDate()
+            : undefined,
+        };
 
   return (
     <DayPicker
@@ -62,18 +62,10 @@ export default function DatePicker({ value, onChange, disabled, fromYear, toYear
       toYear={toYear}
       selected={selected}
       onSelect={(date) => {
-        if (mode === "range") {
-          onChange(
-            date
-              ? {
-                  from: date.from ? dayjs(date.from).toDate() : undefined,
-                  to: date.to ? dayjs(date.to).toDate() : undefined,
-                }
-              : undefined,
-          );
-        } else {
-          onChange(date ? dayjs(date).toDate() : undefined);
-        }
+        // @ts-expect-error toUtc
+        if (mode === "range") return onChange({ from: date?.from ? dayjs(date.from).toUtc().toDate() : undefined, to: date?.to ? dayjs(date.to).toUtc().toDate() : undefined });
+        // @ts-expect-error toUtc
+        onChange(date ? dayjs(date).toUtc().toDate() : undefined);
       }}
     />
   );
