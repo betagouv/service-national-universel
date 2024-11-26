cf4a80217a9fce11cb7539523d91eb7b97f21d52
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2024-11-26 15:42:48 +0100

    up

--- app/src/scenes/changeSejour/scenes/NoMatchingDate.tsx
+++ app/src/scenes/changeSejour/scenes/NoMatchingDate.tsx
@@ -1,25 +1,19 @@
-import React, { useEffect, useState } from "react";
-import { Link, useHistory } from "react-router-dom";
-import { HiArrowLeft } from "react-icons/hi";
+import React, { useState } from "react";
 import NoSejourSection from "../components/NoSejourSection";
 import CurrentSejourNotice from "../components/CurrentSejourNotice";
+import Container from "@/components/layout/Container";
+import FullscreenModal from "@/components/modals/FullscreenModal";
 
 export default function NoMatchingDate() {
-  const history = useHistory();
-
+  const [open, setOpen] = useState(false);
   return (
-    <div className="flex flex-col justify-center items-center bg-white pb-12 px-4 md:px-[8rem]">
-      <div className="w-full flex items-center justify-between py-4">
-        <button onClick={() => history.push("/changer-de-sejour/")} className="flex items-center gap-1 mr-2">
-          <HiArrowLeft className="text-xl text-gray-500" />
-        </button>
-        <h1 className="text-2xl font-bold text-center">Aucune date ne me convient</h1>
-        <div></div>
-      </div>
+    <Container title="Aucune date ne me convient" backlink="/changer-de-sejour/">
       <CurrentSejourNotice />
-      <hr />
       <p className="mt-4 text-sm leading-5 text-[#6B7280] font-normal">Faites votre choix</p>
       <NoSejourSection />
-    </div>
+      <FullscreenModal isOpen={open} setOpen={() => setOpen(false)} title="Êtes-vous sûr de vouloir vous désister ?">
+        yo
+      </FullscreenModal>
+    </Container>
   );
 }
--- app/src/scenes/changeSejour/scenes/PrevenirSejour.tsx
+++ app/src/scenes/changeSejour/scenes/PrevenirSejour.tsx
@@ -1,43 +1,19 @@
-import React, { useState, Fragment } from "react";
-import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from "@headlessui/react";
-// import { CheckIcon } from '@heroicons/react/24/outline'
-import useAuth from "@/services/useAuth";
-import { WITHRAWN_REASONS, YOUNG_STATUS_PHASE1 } from "snu-lib";
-import ReasonMotifSection from "../components/ReasonMotifSection";
+import React, { useState } from "react";
+import ReasonForm from "../components/ReasonForm";
 import CurrentSejourNotice from "../components/CurrentSejourNotice";
 import Container from "@/components/layout/Container";
 import FullscreenModal from "@/components/modals/FullscreenModal";
 
 export default function PrevenirSejour() {
-  const { young } = useAuth();
+  const [open, setOpen] = useState(false);
   const [withdrawnMessage, setWithdrawnMessage] = useState("");
   const [withdrawnReason, setWithdrawnReason] = useState("");
-  const filteredWithdrawnReasons = WITHRAWN_REASONS.filter(
-    (r) =>
-      (!r.phase2Only || young.statusPhase1 === YOUNG_STATUS_PHASE1.DONE || young.statusPhase1 === YOUNG_STATUS_PHASE1.EXEMPTED) &&
-      (!r.cohortOnly || r.cohortOnly.includes(young.cohort)),
-  );
-
-  const [open, setOpen] = useState(false);
 
   return (
-    <Container title="Être alerté lors de l'ouverture des inscriptions sur les prochains séjours" backlink="/changer-de-sejour/no-date">
+    <Container title="M'alerter lors de l'ouverture des prochaines inscriptions" backlink="/changer-de-sejour/no-date">
       <CurrentSejourNotice />
       <p className="mt-4 mb-6 text-sm leading-5 text-[#6B7280] font-normal">Vous serez alerté(e) par e-mail lors de l'ouverture des futures inscriptions.</p>
-
-      <ReasonMotifSection
-        filteredWithdrawnReasons={filteredWithdrawnReasons}
-        withdrawnReason={withdrawnReason}
-        setWithdrawnReason={setWithdrawnReason}
-        withdrawnMessage={withdrawnMessage}
-        setWithdrawnMessage={setWithdrawnMessage}
-      />
-
-      <button
-        onClick={() => setOpen(true)}
-        className="w-full mt-4 rounded-md border-[1px] bg-blue-600 py-2.5 px-3 text-sm leading-5 text-white transition duration-300 ease-in-out hover:bg-blue-800">
-        Envoyer ma demande
-      </button>
+      <ReasonForm reason={withdrawnReason} setReason={setWithdrawnReason} message={withdrawnMessage} setMessage={setWithdrawnMessage} onSubmit={() => setOpen(true)} />
       <FullscreenModal isOpen={open} setOpen={() => setOpen(false)} title="Êtes-vous sûr de vouloir changer de séjour ?">
         wesh
       </FullscreenModal>
