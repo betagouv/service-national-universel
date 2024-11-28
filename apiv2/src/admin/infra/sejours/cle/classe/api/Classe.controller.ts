07aa869fa20e3ec38e2c12a86039f2093ff69295
Johann <Johannbr@users.noreply.github.com>
2024-11-28 14:25:52 +0100

    feat(admin): call to apiv2 (#4567)
    Co-authored-by: Anselme <anselme@chorein.fr>

--- apiv2/src/admin/infra/sejours/cle/classe/api/Classe.controller.ts
+++ apiv2/src/admin/infra/sejours/cle/classe/api/Classe.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Put, UseGuards } from "@nestjs/common";
+import { Controller, Get, Param, Post, UseGuards } from "@nestjs/common";
 import { ClasseModel } from "../../../../../core/sejours/cle/classe/Classe.model";
 import { ClasseService } from "../../../../../core/sejours/cle/classe/Classe.service";
 import { VerifierClasse } from "../../../../../core/sejours/cle/classe/useCase/VerifierClasse";
@@ -19,7 +19,7 @@ export class ClasseController {
         return this.classeService.findAll();
     }
 
-    @Put(":id/verify")
+    @Post(":id/verify")
     @UseGuards(ClasseAdminCleGuard)
     verify(@Param("id") id: string): Promise<ClasseModel> {
         return this.verifierClasse.execute(id);
