8e12b68ec14a024b8820bc5c0cedb2c9e5686874
Nicolas Bretécher <nicolas.bretecher@selego.co>
2024-11-29 12:05:42 +0100

    fix(app): 3731 - Ne pas planter en cas d'échec au fetch du nombre de tickets (#4576)

--- app/src/components/layout/navbar/components/UserMenu.jsx
+++ app/src/components/layout/navbar/components/UserMenu.jsx
@@ -5,14 +5,15 @@ import { setYoung } from "../../../../redux/auth/actions";
 import API from "../../../../services/api";
 import MenuLink from "./MenuLink";
 import MenuButton from "./MenuButton";
-import { permissionPhase2 } from "../../../../utils";
+import useTickets from "../useTickets";
 import { toastr } from "react-redux-toastr";
 
-export default function UserMenu({ onClose, ticketsInfo }) {
+export default function UserMenu({ onClose }) {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.Auth.young);
   const history = useHistory();
   const [isLoggingOut, setIsLoggingOut] = useState(false);
+  const { data: ticketsInfo } = useTickets();
 
   async function logout() {
     try {
@@ -32,7 +33,7 @@ export default function UserMenu({ onClose, ticketsInfo }) {
       <div className="mb-6 flex gap-3">
         <div className="relative">
           <p className="flex h-9 w-9 items-center justify-center rounded-full bg-[#344264] text-center capitalize text-[#768BAC]">{user.firstName[0] + user.lastName[0]}</p>
-          {ticketsInfo.newStatusCount > 0 && (
+          {ticketsInfo?.newStatusCount > 0 && (
             <span className="absolute top-[0px] right-[1px] w-2.5 h-2.5 bg-blue-600 rounded-full text-white border-[1px] border-[#212B44] text-xs flex items-center justify-center"></span>
           )}
         </div>
@@ -43,7 +44,7 @@ export default function UserMenu({ onClose, ticketsInfo }) {
       </div>
       <ul>
         <MenuLink onClose={onClose} to="/account" text="Mon Profil" />
-        {ticketsInfo.hasMessage == true && <MenuLink onClose={onClose} ticketCount={ticketsInfo.newStatusCount} to="/echanges" text="Mes échanges" />}
+        {ticketsInfo?.hasMessage == true && <MenuLink onClose={onClose} ticketCount={ticketsInfo.newStatusCount} to="/echanges" text="Mes échanges" />}
         <MenuButton disabled={isLoggingOut} onClick={logout} text="Déconnexion" />
       </ul>
     </nav>
