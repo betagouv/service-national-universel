1f5bd2497c8e42cb76f2b8df2a6b48abab2504f2
C2Chandelier <charlelie.chandelier@gmail.com>
2024-12-02 17:54:45 +0100

    feat(api,admin): 3589 - Ajout de nouveaux champ dans le suivi de la classe et dans les exports (#4575)
    Co-authored-by: C2Chandelier <charlelie.chandelier@epitech.eu>

--- admin/src/scenes/classe/components/StatsInfos.tsx
+++ admin/src/scenes/classe/components/StatsInfos.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ReactTooltip from "react-tooltip";
+import { HiOutlineInformationCircle } from "react-icons/hi";
+
 import { Container, Button } from "@snu/ds/admin";
 import { ROLES, YOUNG_STATUS, ClassesRoutes } from "snu-lib";
 import { User } from "@/types";
@@ -14,6 +17,11 @@ interface Props {
 export default function StatsInfos({ classe, user, studentStatus, validatedYoung }: Props) {
   const isUserCLE = [ROLES.ADMINISTRATEUR_CLE, ROLES.REFERENT_CLASSE].includes(user.role);
   const isUserAdminOrReferent = [ROLES.ADMIN, ROLES.REFERENT_REGION, ROLES.REFERENT_DEPARTMENT].includes(user.role);
+  const openFiles =
+    (studentStatus[YOUNG_STATUS.VALIDATED] || 0) +
+    (studentStatus[YOUNG_STATUS.IN_PROGRESS] || 0) +
+    (studentStatus[YOUNG_STATUS.WAITING_VALIDATION] || 0) +
+    (studentStatus[YOUNG_STATUS.WAITING_CORRECTION] || 0);
 
   const linkPath = isUserCLE ? "/mes-eleves" : "/inscription";
   const showButton = (isUserCLE && classe.schoolYear === "2024-2025") || isUserAdminOrReferent;
@@ -27,20 +35,33 @@ export default function StatsInfos({ classe, user, studentStatus, validatedYoung
           </Link>
         ),
       ]}>
-      <div className="flex justify-between" style={{ pointerEvents: "none" }}>
+      <div className="flex justify-between">
         <table className="flex-1">
           <tbody>
-            <tr className="border-b border-gray-200">
+            <tr className="border-b border-gray-200 cursor-default">
               <td className="font-bold pr-4 py-2">Élèves prévus :</td>
               <td className="px-4 font-bold text-lg text-center py-2">{classe.totalSeats || 0}</td>
               <td className="text-gray-500 text-center py-2">Élèves</td>
             </tr>
-            <tr className="border-b border-gray-200">
+            <tr className="border-b border-gray-200 cursor-default">
+              <td className="font-bold pr-4 py-2 flex">
+                <p>Dossiers ouverts : </p>
+                <HiOutlineInformationCircle data-tip data-for="tooltip-openfile" className="mb-1 ml-2 text-gray-400 cursor-pointer" size={20} />
+                <ReactTooltip id="tooltip-openfile" type="light" place="top" effect="solid" className="custom-tooltip-radius rounded-md !opacity-100 !shadow-md !z-50">
+                  <div className="text-gray-700 text-xs font-[400] w-[260px] text-center mb-1">
+                    =élèves validés + inscription en cours + en attente de validation + en attente de correction
+                  </div>
+                </ReactTooltip>
+              </td>
+              <td className="px-4 font-bold text-lg text-center py-2">{openFiles}</td>
+              <td className="text-gray-500 text-center py-2">Dossiers</td>
+            </tr>
+            <tr className="border-b border-gray-200 cursor-default">
               <td className="font-bold pr-4 py-2">Élèves inscrits :</td>
               <td className="px-4 font-bold text-lg text-center py-2">{validatedYoung}</td>
               <td className="text-gray-500 text-center py-2">({Math.round((validatedYoung * 100) / classe.totalSeats || 0)}%)</td>
             </tr>
-            <tr>
+            <tr className="cursor-default">
               <td className="font-bold pr-4 py-2">Places libres :</td>
               <td className="px-4 font-bold text-lg text-center py-2">{classe.totalSeats - validatedYoung < 0 ? 0 : classe.totalSeats - validatedYoung}</td>
               <td className="text-gray-500 text-center py-2"> ({Math.round(100 - (validatedYoung * 100) / classe.totalSeats)}%)</td>
@@ -50,19 +71,23 @@ export default function StatsInfos({ classe, user, studentStatus, validatedYoung
         <div className="mx-8 w-[1px] bg-gray-200">&nbsp;</div>
         <table className="flex-1">
           <tbody>
-            <tr>
+            <tr className="cursor-default">
+              <td className="font-bold text-lg text-right">{studentStatus.affectedStudents || 0}</td>
+              <td className="px-4 flex-1">Élèves affectés</td>
+            </tr>
+            <tr className="cursor-default">
               <td className="font-bold text-lg text-right">{studentStatus[YOUNG_STATUS.VALIDATED] || 0}</td>
               <td className="px-4 flex-1">Élèves validés</td>
             </tr>
-            <tr>
+            <tr className="cursor-default">
               <td className="font-bold text-lg text-right">{studentStatus[YOUNG_STATUS.IN_PROGRESS] || 0}</td>
               <td className="px-4 flex-1">Élèves en cours d'inscription</td>
             </tr>
-            <tr>
+            <tr className="cursor-default">
               <td className="font-bold text-lg text-right">{studentStatus[YOUNG_STATUS.WAITING_VALIDATION] || 0}</td>
               <td className="px-4 flex-1">Élèves en attente de validation</td>
             </tr>
-            <tr>
+            <tr className="cursor-default">
               <td className="font-bold text-lg text-right">{studentStatus[YOUNG_STATUS.WAITING_CORRECTION] || 0}</td>
               <td className="px-4 flex-1">Élèves en attente de correction</td>
             </tr>
@@ -71,19 +96,19 @@ export default function StatsInfos({ classe, user, studentStatus, validatedYoung
         <div className="mx-8 w-[1px] bg-gray-200">&nbsp;</div>
         <table className="flex-1">
           <tbody>
-            <tr>
+            <tr className="cursor-default">
               <td className="font-bold text-lg text-right">{studentStatus[YOUNG_STATUS.ABANDONED] || 0}</td>
               <td className="px-4 flex-1">Inscriptions abandonnées</td>
             </tr>
-            <tr className="">
+            <tr className="cursor-default">
               <td className="font-bold text-lg text-right">{studentStatus[YOUNG_STATUS.NOT_AUTORISED] || 0}</td>
               <td className="px-4 flex-1">Élèves non autorisés</td>
             </tr>
-            <tr>
+            <tr className="cursor-default">
               <td className="font-bold text-lg text-right">{studentStatus[YOUNG_STATUS.WITHDRAWN] || 0}</td>
               <td className="px-4 flex-1">Élèves désistés</td>
             </tr>
-            <tr>
+            <tr className="cursor-default">
               <td className="font-bold text-lg text-right">{studentStatus[YOUNG_STATUS.REFUSED] || 0}</td>
               <td className="px-4 flex-1">Élèves refusés</td>
             </tr>
