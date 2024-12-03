d8b895e5225091236df3406bd16dc7fa44962e26
Pierre LE BIHAN <lebihanp@hotmail.fr>
2024-12-03 10:05:41 +0100

    Refactor inscriptionGoal.ts: Update cohort options for new inscription goal fixture

--- api/src/__tests__/fixtures/inscriptionGoal.ts
+++ api/src/__tests__/fixtures/inscriptionGoal.ts
@@ -10,7 +10,7 @@ export default function getNewInscriptionGoalFixture(object: Partial<Inscription
     region: department2region[department],
     max: faker.number.int({ min: 11, max: 123 }),
     cohortId: new ObjectId().toString(),
-    cohort: faker.lorem.words(),
+    cohort: faker.helpers.arrayElement(["Février 2023 - C", "Février 2024 - A"]),
     ...object,
   };
 }
