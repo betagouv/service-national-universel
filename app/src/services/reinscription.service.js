9b9fb955d57a818ffd94529698b0c3a1feec668a
Guillaume Pichon <73333478+GuiPich91@users.noreply.github.com>
2024-12-06 10:16:42 +0100

    refactor(app): 2633 - Refacto HomePage App (#4033)
    Co-authored-by: Guillaume <guillaume@MacMax.local>
    Co-authored-by: Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>

---
+++ app/src/services/reinscription.service.js
@@ -0,0 +1,12 @@
+import { apiURL } from "@/config";
+
+export async function fetchReInscriptionOpen() {
+  return fetch(`${apiURL}/cohort-session/isReInscriptionOpen`, {
+    headers: { "x-user-timezone": new Date().getTimezoneOffset() },
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (!res.ok) throw new Error(res.code);
+      return res.data;
+    });
+}
