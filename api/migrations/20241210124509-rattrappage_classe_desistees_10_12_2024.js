f8927465bf78faff6c6c4ae85e4cb9b09e0a4471
C2Chandelier <charlelie.chandelier@gmail.com>
2024-12-10 14:25:19 +0100

    feat(api): 3746 - migration pour dé-désistée des classes  (#4599)
    Co-authored-by: C2Chandelier <charlelie.chandelier@epitech.eu>

---
+++ api/migrations/20241210124509-rattrappage_classe_desistees_10_12_2024.js
@@ -0,0 +1,42 @@
+const { logger } = require("../src/logger");
+const { ClasseModel, YoungModel } = require("../src/models");
+
+module.exports = {
+  async up(db, client) {
+    const classeIds = ["668ea356c269e600463e7a74", "668ea34fc269e600463e7841", "668ea2eec269e600463e5200"];
+
+    const classes = await ClasseModel.find({ _id: { $in: classeIds } });
+
+    logger.info(`Found ${classes.length} classes...`);
+
+    for (const classe of classes) {
+      const youngs = await YoungModel.find({ classeId: classe._id });
+      logger.info(`Found ${youngs.length} youngs in class ${classe.name}...`);
+
+      for (const young of youngs) {
+        young.set({ status: "VALIDATED" });
+        await young.save({ fromUser: { firstName: "rattrapage des jeunes CLE désistés" } });
+      }
+
+      classe.set({ status: "CLOSED" });
+
+      await classe.save({ fromUser: { firstName: "rattrapage des classes désistés" } });
+    }
+  },
+
+  async down(db, client) {
+    const classeIds = ["668ea356c269e600463e7a74", "668ea34fc269e600463e7841", "668ea2eec269e600463e5200"];
+
+    const classes = await ClasseModel.find({ _id: { $in: classeIds } });
+
+    for (const classe of classes) {
+      const youngs = await YoungModel.find({ classeId: classe._id });
+      for (const young of youngs) {
+        young.set({ status: "ABANDONED" });
+        await young.save({ fromUser: { firstName: "rattrapage des jeunes CLE désistés" } });
+      }
+      classe.set({ status: "WITHDRAWN" });
+      await classe.save({ fromUser: { firstName: "rattrapage des classes désistés" } });
+    }
+  },
+};
