7454f7bef8509e0bd9c0f76ee2e07d61d3333fbb
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2024-12-11 09:24:57 +0100

    improve responsive behavior

--- app/src/components/layout/HomeContainer.tsx
+++ app/src/components/layout/HomeContainer.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function HomeContainer({ title, img, children }: { title: React.ReactNode; img: string; children: React.ReactNode }) {
   return (
-    <div className="max-w-7xl mx-auto bg-white flex flex-col md:flex-row-reverse md:justify-between gap-2 p-[1rem] md:p-[4rem]">
-      <div>
-        <img src={img} width="auto" height="auto" className="px-12 md:px-0 mx-auto" />
+    <div className="max-w-7xl mx-auto bg-white flex flex-col xl:flex-row-reverse xl:justify-between gap-2 p-[1rem] md:p-[4rem]">
+      <div className="w-64 md:w-96 mx-auto flex-none md:hidden xl:block">
+        <img src={img} width="auto" height="auto" className="md:px-0 mx-auto" />
       </div>
       <div className="max-w-2xl">
         <h1 className="font-bold text-3xl md:text-5xl leading-10 md:leading-tight mt-2">{title}</h1>
