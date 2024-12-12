e2e0b0951b31efddf5d7a148943749575fa8a6a7
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2024-12-12 16:36:58 +0100

    up

--- app/src/scenes/changeSejour/components/NoSejourSection.tsx
+++ app/src/scenes/changeSejour/components/NoSejourSection.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { HiArrowRight } from "react-icons/hi";
+import useAuth from "@/services/useAuth";
+import { YOUNG_STATUS } from "snu-lib";
 
 const NoSejourSection = () => {
+  const { young } = useAuth();
   return (
     <section className="mt-6 rounded-md border">
       <Link to="/changer-de-sejour/prevenir-sejour" className="flex p-3 gap-3 justify-between items-center w-full">
@@ -12,13 +15,15 @@ const NoSejourSection = () => {
         </div>
         <HiArrowRight className="text-blue-500 flex-none" />
       </Link>
-      <Link to="/changer-de-sejour/se-desister" className="flex p-3 gap-6 justify-between items-center w-full border-t border-gray-300">
-        <div>
-          <p className="text-sm leading-5 font-medium">Se désister</p>
-          <p className="mt-1 text-sm leading-5 font-normal text-gray-500">Vous ne souhaitez plus participer au séjour</p>
-        </div>
-        <HiArrowRight className="text-blue-500 flex-none" />
-      </Link>
+      {[YOUNG_STATUS.ABANDONED, YOUNG_STATUS.WITHDRAWN].includes(young.status) ? null : (
+        <Link to="/changer-de-sejour/se-desister" className="flex p-3 gap-6 justify-between items-center w-full border-t border-gray-300">
+          <div>
+            <p className="text-sm leading-5 font-medium">Se désister</p>
+            <p className="mt-1 text-sm leading-5 font-normal text-gray-500">Vous ne souhaitez plus participer au séjour</p>
+          </div>
+          <HiArrowRight className="text-blue-500 flex-none" />
+        </Link>
+      )}
     </section>
   );
 };
