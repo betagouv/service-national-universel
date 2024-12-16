2d6794980139d9df3cf5e5757e625514f04f7487
Nicolas Bretécher <95406348+nicobret@users.noreply.github.com>
2024-12-16 16:27:13 +0100

    retours qa

--- app/src/scenes/home/components/SejourNotice.tsx
+++ app/src/scenes/home/components/SejourNotice.tsx
@@ -1,18 +1,21 @@
 import usePermissions from "@/hooks/usePermissions";
+import useAuth from "@/services/useAuth";
 import React from "react";
 import { HiArrowRight } from "react-icons/hi";
 import { Link } from "react-router-dom";
-import { CohortType, getCohortPeriod } from "snu-lib";
+import { CohortType, getCohortPeriod, YOUNG_STATUS } from "snu-lib";
 
 export default function SejourNotice({ cohort }: { cohort: CohortType }) {
+  const { young, isCLE } = useAuth();
   const { canModifyInscription } = usePermissions();
 
   return (
     <section id="changer-de-sejour" className="mt-4 border rounded-md p-4">
       <p>
-        Vous êtes positionné(e) sur le séjour <strong>{getCohortPeriod(cohort)}</strong>.
+        Vous êtes {young.status === YOUNG_STATUS.WAITING_LIST ? "positionné(e) sur" : "inscrit(e) sur liste complémentaire pour"} le séjour{" "}
+        <strong>{getCohortPeriod(cohort)}</strong>.
       </p>
-      {canModifyInscription && (
+      {canModifyInscription && !isCLE && (
         <Link to="/changer-de-sejour">
           <p className="mt-2 text-sm text-blue-600">
             Changer de séjour
