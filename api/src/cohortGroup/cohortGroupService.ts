a3cb06695a16fa14b867dc33cf0630f3b105b5d5
Nicolas Bretécher <nicolas.bretecher@selego.co>
2024-12-18 14:58:13 +0100

    fix(api): 3794 - Fix reinscription eligibility (#4621)

--- api/src/cohortGroup/cohortGroupService.ts
+++ api/src/cohortGroup/cohortGroupService.ts
@@ -1,6 +1,11 @@
 import { COHORT_TYPE, YOUNG_SOURCE, YoungType } from "snu-lib";
 import { CohortGroupDocument, CohortGroupModel, CohortModel } from "../models";
 
+type Query = {
+  type: string;
+  year?: { $gte: number } | { $gt: number };
+};
+
 export async function getCohortGroupsForYoung(young: YoungType): Promise<CohortGroupDocument[]> {
   const cohort = await CohortModel.findById(young.cohortId);
   if (!cohort) throw new Error("Cohort not found");
@@ -8,11 +13,12 @@ export async function getCohortGroupsForYoung(young: YoungType): Promise<CohortG
   const cohortGroup = await CohortGroupModel.findById(cohort.cohortGroupId);
   if (!cohortGroup) throw new Error("Cohort group not found");
 
-  const query = {
-    type: COHORT_TYPE.VOLONTAIRE,
-    // On propose aux CLE désistés de se réinscrire sur l'année en cours, mais pas aux HTS.
-    year: young.source === YOUNG_SOURCE.CLE ? { $gte: cohortGroup.year } : { $gt: cohortGroup.year },
-  };
+  const query: Query = { type: COHORT_TYPE.VOLONTAIRE };
+
+  // On propose aux CLE désistés de se réinscrire sur l'année en cours, mais pas aux HTS.
+  if (cohortGroup.year) {
+    query.year = young.source === YOUNG_SOURCE.CLE ? { $gte: cohortGroup.year } : { $gt: cohortGroup.year };
+  }
 
   return await CohortGroupModel.find(query).sort({ year: 1, name: 1 });
 }
