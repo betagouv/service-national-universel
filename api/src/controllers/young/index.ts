3b337f9f5801a528983d86e149fb883d2743c2bb
C2Chandelier <charlelie.chandelier@epitech.eu>
2025-01-07 12:39:11 +0100

    good template

--- api/src/controllers/young/index.ts
+++ api/src/controllers/young/index.ts
@@ -61,6 +61,9 @@ import {
   ContractType,
   CohortType,
   SUB_ROLES,
+  ReferentType,
+  WITHRAWN_REASONS,
+  formatDateFRTimezoneUTC,
 } from "snu-lib";
 import { getFilteredSessionsForChangementSejour } from "../../cohort/cohortService";
 import { anonymizeApplicationsFromYoungId } from "../../services/application";
@@ -74,7 +77,6 @@ import { FileTypeResult } from "file-type";
 import { requestValidatorMiddleware } from "../../middlewares/requestValidatorMiddleware";
 import { authMiddleware } from "../../middlewares/authMiddleware";
 import { accessControlMiddleware } from "../../middlewares/accessControlMiddleware";
-import { ReferentType } from "../../../../packages/lib/.rollup.cache/Users/charlelie/projects/service-national-universel/packages/lib/dist/mongoSchema/referent";
 
 const router = express.Router();
 const YoungAuth = new AuthObject(YoungModel);
@@ -924,6 +926,7 @@ router.put("/withdraw", passport.authenticate("young", { session: false, failWit
     }
 
     const { withdrawnMessage, withdrawnReason } = value;
+    const oldStatusPhase1 = young.statusPhase1;
 
     young.set({
       status: YOUNG_STATUS.WITHDRAWN,
@@ -947,49 +950,53 @@ router.put("/withdraw", passport.authenticate("young", { session: false, failWit
 
     // We notify the ref dep and the young
     try {
+      const youngFullName = young.firstName + " " + young.lastName;
       const referents: ReferentDocument[] = await ReferentModel.find({ role: ROLES.REFERENT_DEPARTMENT, department: young.department });
       const SUB_ROLES_PRIORITY = [SUB_ROLES.manager_department, SUB_ROLES.assistant_manager_department, SUB_ROLES.secretariat, SUB_ROLES.manager_phase2];
-      let selectedReferent: ReferentDocument | null = null;
-      for (const subRole of SUB_ROLES_PRIORITY) {
-        selectedReferent = referents.find((ref) => ref.subRole === subRole) || null;
-        if (selectedReferent) break;
-      }
+      let selectedReferent: ReferentDocument | undefined = referents.find((referent) => referent.subRole && SUB_ROLES_PRIORITY.includes(referent.subRole));
       if (!selectedReferent && referents.length > 0) {
         selectedReferent = referents[0];
       }
       if (selectedReferent) {
-        await sendTemplate(SENDINBLUE_TEMPLATES.referent.YOUNG_WITHDRAWN_CLE, {
+        await sendTemplate(SENDINBLUE_TEMPLATES.referent.YOUNG_WITHDRAWN_NOTIFICATION, {
           emailTo: [{ name: `${selectedReferent.firstName} ${selectedReferent.lastName}`, email: selectedReferent.email }],
-          params: { youngFirstName: young.firstName, youngLastName: young.lastName, raisondesistement: withdrawnReason },
+          params: { student_name: youngFullName, message: WITHRAWN_REASONS.find((r) => r.value === withdrawnReason)?.label || "" },
         });
       }
       // If they are CLE, we notify the class referent.
       if (cohort?.type === YOUNG_SOURCE.CLE) {
         const classe = await ClasseModel.findById(young.classeId);
         const referent = await ReferentModel.findById(classe?.referentClasseIds[0]);
+        const datecohorte = `du ${formatDateFRTimezoneUTC(cohort.dateStart)} au ${formatDateFRTimezoneUTC(cohort.dateEnd)}`;
         if (referent) {
           await sendTemplate(SENDINBLUE_TEMPLATES.referent.YOUNG_WITHDRAWN_CLE, {
             emailTo: [{ name: `${referent.firstName} ${referent.lastName}`, email: referent.email }],
-            params: { youngFirstName: young.firstName, youngLastName: young.lastName, raisondesistement: withdrawnReason },
+            params: {
+              youngFirstName: young.firstName,
+              youngLastName: young.lastName,
+              datecohorte,
+              raisondesistement: WITHRAWN_REASONS.find((r) => r.value === withdrawnReason)?.label || "",
+            },
           });
         }
       }
 
       // If young affected, we notify the head center
-      if (young.statusPhase1 === YOUNG_STATUS_PHASE1.AFFECTED && young.sessionPhase1Id != null) {
+      if (oldStatusPhase1 === YOUNG_STATUS_PHASE1.AFFECTED && young.sessionPhase1Id != null) {
         const session = await SessionPhase1Model.findById(young.sessionPhase1Id);
         const headCenter = await ReferentModel.findById(session?.headCenterId);
+
         if (headCenter) {
-          await sendTemplate(SENDINBLUE_TEMPLATES.referent.YOUNG_WITHDRAWN_CLE, {
+          await sendTemplate(SENDINBLUE_TEMPLATES.headCenter.YOUNG_WITHDRAWN, {
             emailTo: [{ name: `${headCenter.firstName} ${headCenter.lastName}`, email: headCenter.email }],
-            params: { youngFirstName: young.firstName, youngLastName: young.lastName, raisondesistement: withdrawnReason },
+            params: { contact_name: youngFullName, message: WITHRAWN_REASONS.find((r) => r.value === withdrawnReason)?.label || "" },
           });
         }
       }
 
       await sendTemplate(SENDINBLUE_TEMPLATES.young.WITHDRAWN, {
         emailTo: [{ name: `${young.firstName} ${young.lastName}`, email: young.email }],
-        params: { raisondesistement: withdrawnReason },
+        params: { message: WITHRAWN_REASONS.find((r) => r.value === withdrawnReason)?.label || "" },
       });
     } catch (e) {
       capture(e);
