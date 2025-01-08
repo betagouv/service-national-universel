d466be8f129376d99b6e1963a17b9fa684d4aadc
Romain THEBAUD <romain.thebaud@beta.gouv.fr>
2025-01-08 07:24:23 +0100

    fix imports

--- devops/anonymization/docker_start.sh
+++ devops/anonymization/docker_start.sh
@@ -6,5 +6,6 @@ set -o pipefail
 set -e
 
 
+node -e 'require("bson"); require("uuid")'
 node -e 'require("../../api/src/anonymization/young.js")'
 # ./anonymize_db.sh $SOURCE_DATABASE_URI $TARGET_DATABASE_URI
