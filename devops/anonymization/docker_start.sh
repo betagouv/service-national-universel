1ac4eb7d890cb2eefedd3b945c31477ab81de688
Romain THEBAUD <romain.thebaud@beta.gouv.fr>
2025-01-08 08:05:31 +0100

    requirements ok

--- devops/anonymization/docker_start.sh
+++ devops/anonymization/docker_start.sh
@@ -5,7 +5,4 @@ cd "$(dirname $0)"
 set -o pipefail
 set -e
 
-
-node -e 'require("bson"); require("uuid")'
-node -e 'require("../../api/src/anonymization/young.js")'
-# ./anonymize_db.sh $SOURCE_DATABASE_URI $TARGET_DATABASE_URI
+./anonymize_db.sh $SOURCE_DATABASE_URI $TARGET_DATABASE_URI
