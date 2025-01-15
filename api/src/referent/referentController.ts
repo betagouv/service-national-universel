c5e8929305faae5b387a6a47937a8c94f01737ff
C2Chandelier <charlelie.chandelier@epitech.eu>
2025-01-15 12:40:38 +0100

    first

--- api/src/referent/referentController.ts
+++ api/src/referent/referentController.ts
@@ -107,8 +107,9 @@ import {
   getDepartmentForInscriptionGoal,
   isAdmin,
   isReferentReg,
-  isReferentDep,
   canValidateYoungToLP,
+  WITHRAWN_REASONS,
+  formatDateFRTimezoneUTC,
 } from "snu-lib";
 import { getFilteredSessions, getAllSessions, getFilteredSessionsForCLE } from "../utils/cohort";
 import scanFile from "../utils/virusScanner";
@@ -675,6 +676,12 @@ router.put("/young/:id", passport.authenticate("referent", { session: false, fai
       }
     }
 
+    if (newYoung.status === YOUNG_STATUS.WITHDRAWN && young.status !== YOUNG_STATUS.WITHDRAWN) {
+      const { withdrawnReason } = newYoung;
+      await handleNotifForWithdrawn(young, cohort, withdrawnReason);
+      newYoung.statusPhase1 = young.statusPhase1 === YOUNG_STATUS_PHASE1.AFFECTED ? YOUNG_STATUS_PHASE1.WAITING_AFFECTATION : young.statusPhase1;
+    }
+
     young.set(newYoung);
     await young.save({ fromUser: req.user });
 
@@ -706,6 +713,64 @@ router.put("/young/:id", passport.authenticate("referent", { session: false, fai
     }
   }
 });
+
+async function handleNotifForWithdrawn(young, cohort, withdrawnReason) {
+  const oldStatusPhase1 = young.statusPhase1;
+
+  // We notify the ref dep and the young
+  try {
+    const youngFullName = young.firstName + " " + young.lastName;
+    const referents: ReferentDocument[] = await ReferentModel.find({ role: ROLES.REFERENT_DEPARTMENT, department: young.department });
+    const SUB_ROLES_PRIORITY = [SUB_ROLES.manager_department, SUB_ROLES.assistant_manager_department, SUB_ROLES.secretariat, SUB_ROLES.manager_phase2];
+    let selectedReferent: ReferentDocument | undefined = referents.find((referent) => referent.subRole && SUB_ROLES_PRIORITY.includes(referent.subRole));
+    if (!selectedReferent && referents.length > 0) {
+      selectedReferent = referents[0];
+    }
+    if (selectedReferent) {
+      await sendTemplate(SENDINBLUE_TEMPLATES.referent.YOUNG_WITHDRAWN_NOTIFICATION, {
+        emailTo: [{ name: `${selectedReferent.firstName} ${selectedReferent.lastName}`, email: selectedReferent.email }],
+        params: { student_name: youngFullName, message: WITHRAWN_REASONS.find((r) => r.value === withdrawnReason)?.label || "" },
+      });
+    }
+    // If they are CLE, we notify the class referent.
+    if (cohort?.type === YOUNG_SOURCE.CLE) {
+      const classe = await ClasseModel.findById(young.classeId);
+      const referent = await ReferentModel.findById(classe?.referentClasseIds[0]);
+      const datecohorte = `du ${formatDateFRTimezoneUTC(cohort.dateStart)} au ${formatDateFRTimezoneUTC(cohort.dateEnd)}`;
+      if (referent) {
+        await sendTemplate(SENDINBLUE_TEMPLATES.referent.YOUNG_WITHDRAWN_CLE, {
+          emailTo: [{ name: `${referent.firstName} ${referent.lastName}`, email: referent.email }],
+          params: {
+            youngFirstName: young.firstName,
+            youngLastName: young.lastName,
+            datecohorte,
+            raisondesistement: WITHRAWN_REASONS.find((r) => r.value === withdrawnReason)?.label || "",
+          },
+        });
+      }
+    }
+
+    // If young affected, we notify the head center
+    if (oldStatusPhase1 === YOUNG_STATUS_PHASE1.AFFECTED && young.sessionPhase1Id != null) {
+      const session = await SessionPhase1Model.findById(young.sessionPhase1Id);
+      const headCenter = await ReferentModel.findById(session?.headCenterId);
+
+      if (headCenter) {
+        await sendTemplate(SENDINBLUE_TEMPLATES.headCenter.YOUNG_WITHDRAWN, {
+          emailTo: [{ name: `${headCenter.firstName} ${headCenter.lastName}`, email: headCenter.email }],
+          params: { contact_name: youngFullName, message: WITHRAWN_REASONS.find((r) => r.value === withdrawnReason)?.label || "" },
+        });
+      }
+    }
+
+    await sendTemplate(SENDINBLUE_TEMPLATES.young.WITHDRAWN, {
+      emailTo: [{ name: `${young.firstName} ${young.lastName}`, email: young.email }],
+      params: { message: WITHRAWN_REASONS.find((r) => r.value === withdrawnReason)?.label || "" },
+    });
+  } catch (e) {
+    capture(e);
+  }
+}
 
 /**
  * Permet de valider plusieurs jeunes CLE à la fois (pas d'objectifs pour les CLE).
