84075292c93dbbd5db06a6d078fbf18acd8d2b17
Romain THEBAUD <romain.thebaud@beta.gouv.fr>
2025-01-20 17:47:22 +0100

    Dockerfile clevercloud

--- devops/ci/build-application-docker.js
+++ devops/ci/build-application-docker.js
@@ -1,24 +1,15 @@
-const process = require("node:process");
-const { readFile } = require("node:fs/promises");
-const { resolve } = require("node:path");
 const UserInput = require("../scripts/lib/user-input");
 const {
   childProcess,
   childProcessStdin,
   parseRegistryEndpoint,
-  parseEnvFile,
 } = require("../scripts/lib/utils");
 
-async function loadEnvFile(path) {
-  const contents = await readFile(path, { encoding: "utf8" });
-  return parseEnvFile(contents);
-}
 
 async function main() {
   const input = new UserInput(`Build application docker image`)
     .arg("image", "Tag of the image (ex: rg.fr-par.scw.cloud/snu-ci:latest)")
-    .opt("args-file", "Path to envfile with build arguments", { shortcut: "a" })
-    .opt("secrets-file", "Path to envfile with secrets", { shortcut: "s" })
+    .arg("secrets-file", "Path to envfile with secrets")
     .optBool("push", "Push image on registry", {
       default: false,
     })
@@ -43,28 +34,10 @@ async function main() {
     ".",
   ];
 
-  const buildArgs = input.argsFile ? await loadEnvFile(input.argsFile) : {};
-  buildArgs["RELEASE"] = tagName;
+  args.push("--secret");
+  args.push(`id=BUILD_SECRETS,src=${input.secretsFile}`);
 
-  for (const key in buildArgs) {
-    const value = buildArgs[key];
-    args.push("--build-arg");
-    args.push(`${key}=${value}`);
-  }
-
-  const env = {
-    ...process.env,
-  };
-  const secrets = input.secretsFile ? await loadEnvFile(input.secretsFile) : {};
-
-  for (const key in secrets) {
-    const value = secrets[key];
-    args.push("--secret");
-    args.push(`id=${key}`);
-    env[key] = value;
-  }
-
-  await childProcess("docker", args, { env });
+  await childProcess("docker", args);
 
   if (input.login) {
     await childProcessStdin(
