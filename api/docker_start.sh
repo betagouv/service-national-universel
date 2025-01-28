1a7585842538ba06eee52c72928232a60b6d3513
rthebaud <romain.thebaud@beta.gouv.fr>
2025-01-28 11:53:35 +0100

    chore(api): Drop rsyslog & Drain logs directly in CC (#4702)

--- api/docker_start.sh
+++ api/docker_start.sh
@@ -2,16 +2,6 @@
 
 set -e
 
-touch /var/log/api.error.log /var/log/api.output.log
-
-if [[ $LOGS_ES_ENDPOINT != "" ]]
-then
-  LOGS_ES_HOST=$(echo $LOGS_ES_ENDPOINT | sed 's#https://.*:.*@\(.*\)#\1#g') \
-  LOGS_ES_USER=$(echo $LOGS_ES_ENDPOINT | sed 's#https://\(.*\):.*@.*#\1#g') \
-  LOGS_ES_PASSWORD=$(echo $LOGS_ES_ENDPOINT | sed 's#https://.*:\(.*\)@.*#\1#g') \
-  rsyslogd -f api/docker_rsyslog.conf
-fi
-
 if [[ $ENVIRONMENT != "production" && $ENVIRONMENT != "staging" && $ENVIRONMENT != "ci" ]]
 then
   export RUN_API_AND_TASKS=true
