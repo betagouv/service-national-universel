bf9012d0b5e020cca841ec9a08238b7df3ed15d8
Nicolas Bretécher <nicolas.bretecher@selego.co>
2025-02-04 09:45:25 +0100

    refactor(api, admin): 211 - Listes de volontaires - optimiser la récupération des labels de filtres (#4709)

---
+++ api/src/filterLabel/filterLabelService.ts
@@ -0,0 +1,16 @@
+import { getClasseLabelMap, getEtablissementLabelMap, getLigneLabelMap, getSessionLabelMap } from "./filterLabelRepository";
+
+export const listTypes = { INSCRIPTION: "inscription-list", VOLONTAIRES: "young-list", VOLONTAIRES_CLE: "youngCle-list" };
+
+const labelMaps = {
+  [listTypes.VOLONTAIRES]: [getSessionLabelMap, getLigneLabelMap, getClasseLabelMap, getEtablissementLabelMap],
+  [listTypes.VOLONTAIRES_CLE]: [getSessionLabelMap, getLigneLabelMap, getClasseLabelMap],
+  [listTypes.INSCRIPTION]: [getClasseLabelMap, getEtablissementLabelMap],
+};
+
+export async function getLabels(listType: string): Promise<{ [key: string]: string }> {
+  const labelMap = labelMaps[listType];
+  if (!labelMap) throw new Error("Invalid list type");
+  const labels = await Promise.all(labelMap.map((fn) => fn()));
+  return labels.reduce((acc, curr) => ({ ...acc, ...curr }), {});
+}
--- packages/lib/src/routes/index.ts
+++ packages/lib/src/routes/index.ts
@@ -38,5 +38,6 @@ export type { PreinscriptionRoutes } from "./preinscription";
 export type { AffectationRoutes } from "./phase1/affectation";
 export type { ReferentielRoutes } from "./referentiel";
 export type { ReferentRoutes } from "./iam/referent";
+export type { FilterLabelRoutes } from "./filterLabel";
 
 export * from "./phase1";
