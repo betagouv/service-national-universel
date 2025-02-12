175db4519c3e038d3dedcb3ac437cb1c2c2df5b5
Anselme <anselme@chorein.fr>
2025-02-12 14:53:11 +0100

    feat(admin): Couleur du menu différente en fonction de l'environnement

--- admin/src/hooks/useEnvironment.ts
+++ admin/src/hooks/useEnvironment.ts
@@ -1,11 +1,15 @@
 import { environment } from "@/config";
 
 const useEnvironment = () => {
-  return {
+  const environmentInfo = {
     isProduction: environment == "production",
-    isStaging: !["development", "production"].includes(environment),
+    isPrepoduction: environment === "staging",
+    isCi: environment === "ci",
     isDevelopment: environment === "development",
+    isCustom: false,
   };
+  environmentInfo.isCustom = !environmentInfo.isProduction && !environmentInfo.isPrepoduction && !environmentInfo.isCi && !environmentInfo.isDevelopment;
+  return environmentInfo;
 };
 
 export default useEnvironment;
