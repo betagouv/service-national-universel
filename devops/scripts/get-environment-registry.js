c1ab5c982c27f31c27e48fa27b86c1a3ecc55e57
rthebaud <romain.thebaud@beta.gouv.fr>
2025-02-17 16:50:44 +0100

    chore(misc): Use static docker registries (#4746)

--- devops/scripts/get-environment-registry.js
+++ devops/scripts/get-environment-registry.js
@@ -5,25 +5,11 @@ const { EnvConfig } = require("./lib/config");
 async function main() {
   const input = new UserInput("Get environment registry from environment name")
     .arg("environment-name", "Environment name")
-    .env("SCW_SECRET_KEY", "Scaleway secret key")
-    .env("SCW_ORGANIZATION_ID", "Scaleway organization identifier")
     .validate();
-
-  const scaleway = new ScalewayClient(
-    input.SCW_SECRET_KEY,
-    input.SCW_ORGANIZATION_ID
-  );
 
   const config = new EnvConfig(input.environmentName);
 
-  const project = await scaleway.findProject(config.projectName());
-
-  const namespace = await scaleway.findOrThrow(RESOURCE.ContainerNamespace, {
-    project_id: project.id,
-    name: config.containerNamespace(),
-  });
-
-  console.log(namespace.registry_endpoint);
+  console.log(config.dockerRegistry());
 }
 
 if (require.main === module) {
