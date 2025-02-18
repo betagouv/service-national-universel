800910ab37b4605e46ac6e22157f737549ea953a
Nicolas Bretécher <nicolas.bretecher@proton.me>
2025-02-18 11:47:17 +0100

    up

---
+++ api/migrations/20250218103949-recreate-missing-sessions-fev-2022.js
@@ -0,0 +1,79 @@
+import { YOUNG_STATUS } from "snu-lib";
+import { CohesionCenterModel, SessionPhase1Model, YoungModel, CohortModel } from "../../../models";
+
+const { ObjectId } = require("mongoose").Types;
+
+const data = [
+  {
+    sessionId: "61e0440cc6470a07ce47e011",
+    centerId: "609bebc40c1cc9a888ae907a",
+    cohortName: "Février 2022",
+  },
+  {
+    sessionId: "61e041c4d6d38907c8e6004a",
+    centerId: "63bec528b6afd806283c0578",
+    cohortName: "Février 2022",
+  },
+];
+
+function createSessionPhase1({ sessionId, center, cohort, placesTaken }) {
+  // Les volontaires ont déjà un sessionId : on n'y touche pas, on recrée les sessions manquantes et on rajoute le centerId aux jeunes.
+  const placesTotal = center.placesTotal || 0;
+  return new SessionPhase1Model({
+    _id: new ObjectId(sessionId),
+    cohesionCenterId: center.id,
+    cohort: cohort.name,
+    headCenterId: center.id,
+    placesTotal: center.placesTotal,
+    placesLeft: Math.floor(placesTotal - placesTaken),
+    status: "VALIDATED",
+    cityCentre: center.city,
+    codeCentre: center.code,
+    department: center.department,
+    nameCentre: center.name,
+    region: center.region,
+    zipCentre: center.zip,
+    hasTimeSchedule: false,
+    timeScheduleFiles: [],
+    hasPedagoProject: false,
+    pedagoProjectFiles: [],
+    cohortId: cohort.id,
+  });
+}
+
+module.exports = {
+  async up() {
+    for await (const session of data) {
+      const cohort = await CohortModel.findOne({ name: session.cohortName });
+      if (!cohort) {
+        console.log(`❌ Cohort not found: ${session.cohortName}`);
+        continue;
+      }
+      const center = await CohesionCenterModel.findById(session.centerId);
+      if (!center) {
+        console.log(`❌ Cohesion center not found: ${session.centerId}`);
+        continue;
+      }
+      const placesTaken = await YoungModel.countDocuments({ sessionPhase1Id: session.sessionId, status: YOUNG_STATUS.VALIDATED });
+      const sessionToCreate = createSessionPhase1({
+        sessionId: session.sessionId,
+        center,
+        cohort,
+        placesTaken,
+      });
+
+      const res = await sessionToCreate.save();
+      console.log(`✅ Session created: ${res._id}`);
+
+      console.log(`✅ Updating youngs for session ${session.sessionId}`);
+      await YoungModel.updateMany({ sessionPhase1Id: session.sessionId }, { cohesionCenterId: center.id });
+    }
+  },
+
+  async down() {
+    for await (const session of data) {
+      await SessionPhase1Model.deleteOne({ _id: session.sessionId });
+      await YoungModel.updateMany({ sessionPhase1Id: session.sessionId }, { $unset: { cohesionCenterId: "" } });
+    }
+  },
+};
