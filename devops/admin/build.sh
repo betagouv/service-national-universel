4028a2548799d65f729875d39e5eb50eaad18b17
Romain THEBAUD <romain.thebaud@beta.gouv.fr>
2025-02-19 11:01:31 +0100

    fix build

--- devops/admin/build.sh
+++ devops/admin/build.sh
@@ -7,11 +7,11 @@ cd "$(dirname $0)/../.."
 echo $VITE_RELEASE
 echo $VITE_ENVIRONMENT
 
-turbo_version=$(cat /tmp/package-lock.json | grep turbo | head -n 1 | sed 's/"turbo": "\(.*\)"/\1/g')
+turbo_version=$(cat package-lock.json | grep turbo | head -n 1 | sed 's/"turbo": "\(.*\)"/\1/g')
 npm install --global "turbo@$turbo_version"
 
 turbo prune admin
 cp tsconfig.front.json out
 cd out
 npm ci --no-audit --no-fund
-npm run build
+turbo run build
