5a757f5608794e292bf0fb82b48d16fcd6a9f1d8
Romain THEBAUD <romain.thebaud@beta.gouv.fr>
2025-02-21 20:23:00 +0100

    debug

--- devops/admin/build.sh
+++ devops/admin/build.sh
@@ -37,7 +37,7 @@ if [[ $CC_DEPLOYMENT_ID != "" ]]; then
     # CC_NODE_BUILD_TOOL=custom
     # CC_CUSTOM_BUILD_TOOL="VITE_RELEASE=$CC_COMMIT_ID devops/admin/build.sh"
     # CC_RUN_COMMAND="nginx -g 'daemon off;' -c nginx.conf"
-    mv devops/admin/nginx.conf devops/admin/package.json .
+    mv devops/admin/nginx.conf devops/admin/index.js devops/admin/package.json .
     rm -rf $(ls -A | grep -v "build\|nginx.conf\|package.json")
     npm install
 fi
