4ff6374b9013e5e582c50060af9d080c4a5dc957
Romain THEBAUD <romain.thebaud@beta.gouv.fr>
2025-02-28 17:13:58 +0100

    fix nginx config path

--- devops/build/all/start-nginx.sh
+++ devops/build/all/start-nginx.sh
@@ -2,12 +2,11 @@
 
 waitForUpstream() {
     until curl --output /dev/null --silent --head $1; do
-        printf '.'
-        sleep 5
+        sleep 2
     done
 }
 
 waitForUpstream "http://127.0.0.1:3000" # api
 waitForUpstream "http://127.0.0.1:3006" # apiv2
 
-exec nginx -c nginx.conf
+exec nginx -c $(dirname $0)/nginx.conf
