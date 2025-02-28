49eabbae16caf5bf3e73d8962447511a3f804eea
Romain THEBAUD <romain.thebaud@beta.gouv.fr>
2025-02-28 18:01:02 +0100

    nginx proxy_temp_path directive

--- devops/build/build-all.sh
+++ devops/build/build-all.sh
@@ -50,6 +50,7 @@ mkdir -p $destination/apiv2/
 mv out/apiv2/{dist/*,node_modules} $destination/apiv2/
 
 envsubst '$APP_HOME $PORT' < devops/build/all/nginx.conf > $destination/nginx.conf
+mkdir -p $destination/nginx/proxy
 cp devops/build/all/{package.json,ecosystem.config.js,start-nginx.sh} $destination
 
 rm -Rf out
