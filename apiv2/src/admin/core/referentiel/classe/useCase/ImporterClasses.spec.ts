fd9246b3c35b4ccbafb4b693875f8103c44a1df6
Anselme <anselme@chorein.fr>
2025-03-07 16:27:35 +0100

    update test

--- apiv2/src/admin/core/referentiel/classe/useCase/ImporterClasses.spec.ts
+++ apiv2/src/admin/core/referentiel/classe/useCase/ImporterClasses.spec.ts
@@ -77,7 +77,8 @@ describe("ImporterClasses", () => {
         ];
 
         mockFileGateway.downloadFile.mockResolvedValue({ Body: Buffer.from("") });
-        mockFileGateway.parseXLS.mockResolvedValue(mockXlsxData);
+        mockFileGateway.parseXLS.mockResolvedValueOnce(mockXlsxData); // import
+        mockFileGateway.parseXLS.mockResolvedValueOnce([]); // desist
         mockFileGateway.generateExcel.mockResolvedValue(Buffer.from(""));
         mockFileGateway.uploadFile.mockResolvedValue({ Key: "report.xlsx" });
 
@@ -132,7 +133,8 @@ describe("ImporterClasses", () => {
         ];
 
         mockFileGateway.downloadFile.mockResolvedValue({ Body: Buffer.from("") });
-        mockFileGateway.parseXLS.mockResolvedValue(mockXlsxData);
+        mockFileGateway.parseXLS.mockResolvedValueOnce(mockXlsxData); // import
+        mockFileGateway.parseXLS.mockResolvedValueOnce([]); // desist
         mockFileGateway.generateExcel.mockResolvedValue(Buffer.from(""));
         mockFileGateway.uploadFile.mockResolvedValue({ Key: "report.xlsx" });
 
@@ -162,7 +164,8 @@ describe("ImporterClasses", () => {
         ];
 
         mockFileGateway.downloadFile.mockResolvedValue({ Body: Buffer.from("") });
-        mockFileGateway.parseXLS.mockResolvedValue(mockXlsxData);
+        mockFileGateway.parseXLS.mockResolvedValueOnce(mockXlsxData); // import
+        mockFileGateway.parseXLS.mockResolvedValueOnce([]); // desist
         mockFileGateway.generateExcel.mockResolvedValue(Buffer.from(""));
         mockFileGateway.uploadFile.mockResolvedValue({ Key: "report.xlsx" });
 
@@ -200,6 +203,43 @@ describe("ImporterClasses", () => {
                 originalSessionNom: "Old Session",
                 sessionChangeReason: "Import SI-SNU",
             }),
+        ]);
+    });
+
+    it("should handle duplicate import and desist classe error", async () => {
+        const mockXlsxData = [
+            {
+                "Session formule": "2024",
+                "Identifiant de la classe engagée": "CLASS-001",
+            },
+        ];
+
+        mockFileGateway.downloadFile.mockResolvedValue({ Body: Buffer.from("") });
+        mockFileGateway.parseXLS.mockResolvedValueOnce(mockXlsxData); // import
+        mockFileGateway.parseXLS.mockResolvedValueOnce(mockXlsxData); // desist
+        mockFileGateway.generateExcel.mockResolvedValue(Buffer.from(""));
+        mockFileGateway.uploadFile.mockResolvedValue({ Key: "report.xlsx" });
+
+        mockClasseGateway.findById.mockResolvedValue({
+            id: "CLASS-001",
+        });
+
+        const result = await useCase.execute({
+            fileKey: "test.xlsx",
+            folderPath: "/test",
+            auteur: { email: "test@test.com", nom: "Test", prenom: "User" },
+            fileName: "",
+            fileLineCount: 0,
+            type: ReferentielTaskType.IMPORT_CLASSES,
+        });
+
+        expect(result).toEqual([
+            {
+                classeId: "CLASS-001",
+                cohortCode: "2024",
+                error: "Classe existe dans les 2 onglets",
+                sessionCode: "undefined_undefined",
+            },
         ]);
     });
 });
