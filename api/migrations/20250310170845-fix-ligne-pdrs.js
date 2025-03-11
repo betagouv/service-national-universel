d8ceffb856053f66bb4ac30fe9ca7128779eca04
Anselme <anselme@chorein.fr>
2025-03-11 09:43:09 +0100

    fix(api,admin): 309 - Incohérences des PDRs sur une ligne de bus

---
+++ api/migrations/20250310170845-fix-ligne-pdrs.js
@@ -0,0 +1,56 @@
+const { LigneBusModel } = require("../src/models");
+
+module.exports = {
+  async up(db, client) {
+    const lignesIncoherentes = await LigneBusModel.aggregate([
+      {
+        $addFields: {
+          convertedId: {
+            $toString: "$_id",
+          },
+        },
+      },
+      {
+        $lookup: {
+          as: "etapesByLigne",
+          from: "lignetopoints",
+          foreignField: "lineId",
+          localField: "convertedId",
+        },
+      },
+      {
+        $addFields: {
+          nbEtapes: { $size: "$etapesByLigne" },
+          nbPdrs: { $size: "$meetingPointsIds" },
+          id: "$convertedId",
+        },
+      },
+      {
+        $match: {
+          nbEtapes: { $gt: 1 },
+          nbPdrs: 1,
+        },
+      },
+      {
+        $project: {
+          id: 1,
+          nbPdrs: 1,
+          nbEtapes: 1,
+          meetingPointsIds: 1,
+          etapesByLigne: 1,
+        },
+      },
+    ]);
+    for (const ligneIncoherente of lignesIncoherentes) {
+      const pdrsEtapes = ligneIncoherente.etapesByLigne.map((etape) => etape.meetingPointId);
+      if (ligneIncoherente.nbEtapes > ligneIncoherente.nbPdrs) {
+        console.log("Ligne ", ligneIncoherente.id, "modifications pdrs", ligneIncoherente.meetingPointsIds, "=>", pdrsEtapes);
+        await LigneBusModel.updateOne({ _id: ligneIncoherente.id }, { $set: { meetingPointsIds: pdrsEtapes } }, { fromUser: { firstName: "Corrections PDRs", lastName: "" } });
+      } else {
+        console.log("Ligne ", ligneIncoherente.id, "n'a pas plus d'étapes que de pdrs", ligneIncoherente.meetingPointsIds, ligneIncoherente.nbPdrs);
+      }
+    }
+  },
+
+  async down(db, client) {},
+};
