adf85d30880d9fd3498e72524ceb4fc3d27e95be
Eric SANCHEZ <eric.cmsfr@gmail.com>
2025-03-20 15:39:40 +0100

    Fix erreur type CampagneSpecifiqueForm/Hook

--- admin/src/scenes/planMarketing/campagne/CampagneSpecifiqueHook.ts
+++ admin/src/scenes/planMarketing/campagne/CampagneSpecifiqueHook.ts
@@ -8,27 +8,41 @@ import { CampagneSpecifiqueMapper } from "./mapper/CampagneSpecifiqueMapper";
 const CAMPAGNE_SPECIFIQUE_QUERY_KEY = "campagnes-specifiques";
 const DEFAULT_SORT = "DESC";
 
+interface SaveCampagneParams {
+  id?: string;
+  payload: CampagneSpecifiqueFormData & { generic: false };
+  onSuccess?: (success: boolean) => void;
+}
+
 export const useCampagneSpecifique = ({ sessionId }: { sessionId: string }) => {
   const queryClient = useQueryClient();
 
   const { mutate: saveCampagne } = useMutation({
-    mutationFn: ({ id, payload }: { id?: string; payload: CampagneSpecifiqueFormData & { generic: false } }) => {
+    mutationFn: ({ id, payload }: SaveCampagneParams) => {
       if (id) {
         return PlanMarketingService.update(id, CampagneSpecifiqueMapper.toUpdatePayload({ ...payload, id }));
       }
       return PlanMarketingService.create(CampagneSpecifiqueMapper.toCreatePayload(payload));
     },
-    onSuccess: () => {
+    onSuccess: (_, variables) => {
       // Force la récupération des campagnes spécifiques
       // pour mettre à jour la liste des campagnes spécifiques avec la référence à la campagne générique
       queryClient.invalidateQueries({ queryKey: [CAMPAGNE_SPECIFIQUE_QUERY_KEY, DEFAULT_SORT, sessionId] });
       toastr.clean();
       toastr.success("Succès", "Campagne sauvegardée avec succès", { timeOut: 5000 });
+
+      if (variables.onSuccess) {
+        variables.onSuccess(true);
+      }
     },
-    onError: (error: HttpError) => {
+    onError: (error: HttpError, variables) => {
       if (error?.statusCode === 422) {
         toastr.clean();
         toastr.error("Erreur", translateMarketing(error.message) || "Une erreur est survenue lors de la sauvegarde de la campagne", { timeOut: 5000 });
+      }
+
+      if (variables.onSuccess) {
+        variables.onSuccess(false);
       }
     },
   });
