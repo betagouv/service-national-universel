9fd9dea6eaab65fe119395fe764ea145a2b19ef8
Johann <Johannbr@users.noreply.github.com>
2025-03-25 09:57:48 +0100

    feat(apiv2,admin): 289-288-316 - plan marketing lot 2-b (#4840)
    Co-authored-by: Eric SANCHEZ <eric.cmsfr@gmail.com>
    Co-authored-by: Sanchez Eric <eric.sanchez@uppertech.fr>
    Co-authored-by: Guillaume Pichon <73333478+GuiPich91@users.noreply.github.com>

--- apiv2/src/plan-marketing/infra/api/Campagne.validation.ts
+++ apiv2/src/plan-marketing/infra/api/Campagne.validation.ts
@@ -1,12 +1,8 @@
-import { CampagneModel, CreateCampagneModel } from "@plan-marketing/core/Campagne.model";
+import { CreateCampagneGeneriqueModel, CampagneGeneriqueModel, CreateCampagneSpecifiqueModelWithRef, CreateCampagneSpecifiqueModelWithoutRef } from "@plan-marketing/core/Campagne.model";
 import { CampagneJeuneType, DestinataireListeDiffusion } from "snu-lib";
-import { IsArray, IsBoolean, IsEnum, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
+import { IsArray, IsBoolean, IsEnum, IsNotEmpty, IsOptional, IsString, IsNumber } from "class-validator";
 
-export class CreateCampagneDto implements CreateCampagneModel {
-    @IsString()
-    @IsOptional()
-    campagneGeneriqueId?: string;
-
+class BaseCampagneDto {
     @IsString()
     @IsNotEmpty()
     nom: string;
@@ -27,10 +23,6 @@ export class CreateCampagneDto implements CreateCampagneModel {
     @IsNotEmpty()
     listeDiffusionId: string;
 
-    @IsBoolean()
-    @IsNotEmpty()
-    generic: boolean;
-
     @IsNotEmpty()
     @IsArray()
     destinataires: DestinataireListeDiffusion[];
@@ -40,8 +32,53 @@ export class CreateCampagneDto implements CreateCampagneModel {
     type: CampagneJeuneType;
 }
 
-export class UpdateCampagneDto extends CreateCampagneDto implements CampagneModel {
+export class CreateCampagneGeneriqueDto extends BaseCampagneDto implements CreateCampagneGeneriqueModel {
+    @IsBoolean()
+    @IsNotEmpty()
+    generic: true = true;
+}
+
+export class CreateCampagneSpecifiqueWithoutRefDto extends BaseCampagneDto implements CreateCampagneSpecifiqueModelWithoutRef {
+    @IsBoolean()
+    @IsNotEmpty()
+    generic: false = false;
+
+    @IsString()
+    @IsNotEmpty()
+    cohortId: string;
+}
+
+export class CreateCampagneSpecifiqueWithRefDto implements CreateCampagneSpecifiqueModelWithRef {
+    @IsBoolean()
+    @IsNotEmpty()
+    generic: false = false;
+
+    @IsString()
+    @IsNotEmpty()
+    cohortId: string;
+
+    @IsString()
+    @IsNotEmpty()
+    campagneGeneriqueId: string;
+}
+
+export class UpdateCampagneGeneriqueDto extends CreateCampagneGeneriqueDto implements CampagneGeneriqueModel {
     @IsString()
     @IsNotEmpty()
     id: string;
 }
+
+export class UpdateCampagneSpecifiqueWithoutRefDto extends CreateCampagneSpecifiqueWithoutRefDto {
+    @IsString()
+    @IsNotEmpty()
+    id: string;
+}
+
+export class UpdateCampagneSpecifiqueWithRefDto extends CreateCampagneSpecifiqueWithRefDto {
+    @IsString()
+    @IsNotEmpty()
+    id: string;
+}
+
+export type CreateCampagneDto = CreateCampagneGeneriqueDto | CreateCampagneSpecifiqueWithoutRefDto | CreateCampagneSpecifiqueWithRefDto;
+export type UpdateCampagneDto = UpdateCampagneGeneriqueDto | UpdateCampagneSpecifiqueWithoutRefDto | UpdateCampagneSpecifiqueWithRefDto;
