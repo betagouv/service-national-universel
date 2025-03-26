d40ad8b1534b5305ab10f61621d9d9d4d10e1431
Nicolas Bretécher <nicolas.bretecher@proton.me>
2025-03-26 16:22:52 +0100

    up

--- admin/src/components/views/Historic.jsx
+++ admin/src/components/views/Historic.jsx
@@ -2,102 +2,106 @@ import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { formatStringLongDate, translate, ROLES } from "../../utils";
 import Loader from "../../components/Loader";
-import { HiOutlineChevronUp, HiOutlineChevronDown, HiArrowRight } from "react-icons/hi";
+import { HiOutlineChevronDown, HiArrowRight } from "react-icons/hi";
 import { useToggle } from "@uidotdev/usehooks";
 import { filterResult, getFieldName, translator } from "./lib/patchesUtils";
 import { isIsoDate } from "snu-lib";
 import { usePatches } from "./lib/usePatches";
+import ReactTooltip from "react-tooltip";
 
 export default function Historic({ model, value }) {
   const { data: patches, isPending } = usePatches(model, value);
-  const [hideSync, setHideSync] = useToggle(true);
-  const [filter, setFilter] = useState("");
-  const filteredPatches = patches?.filter((hit) => filterResult(model, hit, filter, hideSync));
+  const [simpleMode, setSimpleMode] = useToggle(true);
+  const [search, setSearch] = useState("");
+  const filteredPatches = patches?.filter((hit) => filterResult(model, hit, search, simpleMode));
   const user = useSelector((state) => state.Auth.user);
 
   return isPending ? (
     <Loader />
   ) : (
-    <div className="flex w-full flex-col gap-3">
-      {filteredPatches.length === 0 ? <div className="p-1 italic">Aucune donnée</div> : null}
+    <>
       {user?.role === ROLES.ADMIN ? (
-        <div className="grid grid-cols-2 gap-4">
-          <input onChange={(e) => setFilter(e.target.value)} value={filter} className="rounded-lg bg-white p-2 shadow-sm" placeholder="Rechercher..." />
-          {model === "mission" && (
-            <div className="mt-2">
-              <input id="filter-modifs" type="checkbox" checked={hideSync} onChange={setHideSync} />
-              <label htmlFor="filter-modifs" className="ml-2 text-coolGray-500">
-                Masquer les synchronisations sans modification et les données source.
-              </label>
+        <>
+          <input onChange={(e) => setSearch(e.target.value)} value={search} className="w-full rounded-lg bg-white p-3 shadow-md" placeholder="Rechercher..." />
+          {model === "mission" ? (
+            <div className="w-fit">
+              <ReactTooltip id="filter-modifs" className="bg-white shadow-xl" arrowColor="white">
+                <p className="text-gray-800">Masquer les synchronisations sans modification et les données source, traduire les noms des champs.</p>
+              </ReactTooltip>
+              <div data-tip data-for="filter-modifs" className="p-3">
+                <input id="filter-modifs" type="checkbox" checked={simpleMode} onChange={setSimpleMode} />
+                <label htmlFor="filter-modifs" className="m-0 ml-2 text-coolGray-500">
+                  Mode simplifié
+                </label>
+              </div>
             </div>
+          ) : (
+            <div className="p-3"></div>
           )}
-        </div>
+        </>
       ) : null}
-      {filteredPatches.map((hit) => (
-        <Hit model={model} key={hit._id} hit={hit} hideSync={hideSync} />
-      ))}
-    </div>
+      <div className="grid gap-3">
+        {filteredPatches.length === 0 ? <p>Aucune donnée.</p> : filteredPatches.map((hit) => <Hit model={model} key={hit._id} hit={hit} simpleMode={simpleMode} />)}
+      </div>
+    </>
   );
 }
 
-const Hit = ({ hit, model, hideSync }) => {
-  const [viewDetails, setViewDetails] = useState(true);
+const Hit = ({ hit, model, simpleMode }) => {
+  const [viewModifs, setViewModifs] = useToggle(true);
   const user = useSelector((state) => state.Auth.user);
-  const filteredOps = hideSync ? hit.ops.filter((op) => !op.path.includes("/jvaRawData") && !op.path.includes("/lastSyncAt")) : hit.ops;
+  const modifications = simpleMode ? hit.ops.filter((op) => !op.path.includes("/jvaRawData") && !op.path.includes("/lastSyncAt")) : hit.ops;
 
   return (
     <div className="rounded-lg bg-white shadow-md">
-      <button className="w-full flex items-center justify-between border-b p-3" onClick={() => setViewDetails((e) => !e)}>
-        <div>
-          <span className="font-bold">
+      <div className="border-b p-3 flex justify-between">
+        <p>
+          <strong>
             {hit.user && hit.user.email ? (
               hit.user.role ? (
                 // * Referent
-                <a href={`/user/${hit.user._id}`} target="_blank" rel="noreferrer" className="text-snu-purple-300 hover:text-snu-purple-300 hover:underline">
+                <a href={`/user/${hit.user._id}`} target="_blank" rel="noreferrer" className="text-blue-600 underline-offset-2 hover:text-blue-600 hover:underline">
                   {`${hit.user.firstName} ${hit.user.lastName} (${translate(hit.user.role)})`}
                 </a>
               ) : (
                 // * Young
-                <a href={`/volontaire/${hit.user._id}`} target="_blank" rel="noreferrer" className="text-snu-purple-300 hover:text-snu-purple-300 hover:underline">
+                <a href={`/volontaire/${hit.user._id}`} target="_blank" rel="noreferrer" className="text-blue-600 underline-offset-2 hover:text-blue-600 hover:underline">
                   {`${hit.user.firstName} ${hit.user.lastName} (Volontaire)`}
                 </a>
               )
             ) : hit.user && hit.user.firstName ? (
               // * Scripts / Cron
               user?.role === ROLES.ADMIN ? (
-                [hit.user.firstName, hit.user.lastName].join(" ")
+                hit.user.firstName
               ) : (
                 "Modification automatique"
               )
             ) : (
               "Acteur non renseigné"
             )}
-          </span>
+          </strong>
           ,&nbsp;{formatStringLongDate(hit.date)}
-        </div>
+        </p>
+        <button className="text-gray-500" onClick={setViewModifs}>
+          {modifications.length} modification{hit.ops.length > 1 ? "s" : ""}
+          <HiOutlineChevronDown className={`inline-block ml-2 transition-all duration-300 ease-in-out ${viewModifs && "rotate-180"}`} />
+        </button>
+      </div>
 
-        <div className="flex items-center gap-2 text-coolGray-500">
-          <span className="italic">
-            {hit.ops.length} action{hit.ops.length > 1 ? "s" : ""}
-          </span>
-          {viewDetails ? <HiOutlineChevronUp /> : <HiOutlineChevronDown />}
-        </div>
-      </button>
-
-      {viewDetails
-        ? filteredOps?.map((e, i) => {
+      {viewModifs
+        ? modifications.map((e, i) => {
             const originalValue = translator(JSON.stringify(e.path)?.replace(/"/g, ""), JSON.stringify(e.originalValue)?.replace(/"/g, ""));
             const value = translator(JSON.stringify(e.path)?.replace(/"/g, ""), JSON.stringify(e.value)?.replace(/"/g, ""));
             return (
-              <div className="flex justify-between border-b border-[#f3f3f3] p-3" key={`${hit.date}-${i}`}>
-                <div className="flex-1 ">{getFieldName(model, e.path)}&nbsp;:</div>
-                <div className="flex-1 text-center">
+              <div className="grid grid-cols-12 border-b border-gray-100 p-3" key={`${hit.date}-${i}`}>
+                <div className="col-span-3">{simpleMode ? getFieldName(model, e.path) : e.path}&nbsp;:</div>
+                <div className="col-span-4">
                   {(isIsoDate(originalValue) ? formatStringLongDate(originalValue) : originalValue) || <span className="italic text-coolGray-400">Vide</span>}
                 </div>
-                <div className="text-center">
-                  <HiArrowRight />
+                <div>
+                  <HiArrowRight className="mx-3 my-1" />
                 </div>
-                <div className="flex-1 text-center">{(isIsoDate(value) ? formatStringLongDate(value) : value) || <span className="italic text-coolGray-400">Vide</span>}</div>
+                <div className="col-span-4">{(isIsoDate(value) ? formatStringLongDate(value) : value) || <span className="italic text-coolGray-400">Vide</span>}</div>
               </div>
             );
           })
