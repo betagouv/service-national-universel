774a11efa2cb58547e5ac1840a507d3a42f04bdd
Nicolas Bretécher <nicolas.bretecher@proton.me>
2025-03-31 10:44:43 +0200

    up

--- api/src/services/permission/permissionService.ts
+++ api/src/services/permission/permissionService.ts
@@ -20,28 +20,28 @@ export class PermissionService implements IPermissionService {
   check(action: string, cohort?: CohortType): boolean {
     // if (this.user.subRole === "god") return true;
     const permission = this.permissions[action];
-    if (permission?.value) {
-      return permission.value;
-    }
-    if (permission?.startAt && permission?.endAt) {
-      if (!cohort) throw new Error(`Cohort is required for permission ${action}`);
-      const start = new Date(cohort![permission.startAt]);
-      const end = new Date(cohort![permission.endAt]);
-      const now = new Date();
-      if (isWithinInterval(now, { start, end })) {
-        return true;
-      }
+    if (!permission) {
       return false;
     }
-    if (permission?.endAt) {
-      if (!cohort) throw new Error(`Cohort is required for permission ${action}`);
-      if (new Date() > cohort[permission.endAt]) {
+    if (permission.value) {
+      return permission.value;
+    }
+    if (permission.setting || permission.startAt || permission.endAt) {
+      if (!cohort) {
+        throw new Error(`Cohort is required for permission ${action}`);
+      }
+      if (permission.setting) {
+        return cohort[permission.setting] as boolean;
+      }
+      if (permission.startAt && permission.endAt) {
+        const start = new Date(cohort[permission.startAt]);
+        const end = new Date(cohort[permission.endAt]);
+        const now = new Date();
+        if (isWithinInterval(now, { start, end })) {
+          return true;
+        }
         return false;
       }
-    }
-    if (permission?.setting) {
-      if (!cohort) throw new Error(`Cohort is required for permission ${action}`);
-      return cohort[permission.setting] as boolean;
     }
     return false;
   }
