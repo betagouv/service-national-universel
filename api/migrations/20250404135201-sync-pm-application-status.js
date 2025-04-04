e55e2dd9ea66f93f81b4e9734a64759406f3ef3e
Nicolas Bretécher <nicolas.bretecher@proton.me>
2025-04-04 16:00:15 +0200

    add migration

---
+++ api/migrations/20250404135201-sync-pm-application-status.js
@@ -0,0 +1,36 @@
+const { APPLICATION_STATUS } = require("snu-lib");
+const { ApplicationModel } = require("../src/models");
+
+const aggregation = [
+  { $match: { status: APPLICATION_STATUS.WAITING_VERIFICATION } },
+  { $addFields: { youngIdToObj: { $toObjectId: "$youngId" } } },
+  {
+    $lookup: {
+      from: "youngs",
+      localField: "youngIdToObj",
+      foreignField: "_id",
+      as: "youngDetails",
+    },
+  },
+  { $unwind: "$youngDetails" },
+  { $match: { "youngDetails.statusMilitaryPreparationFiles": "VALIDATED" } },
+  { $project: { _id: 1 } },
+];
+
+module.exports = {
+  async up(db, client) {
+    const applicationsToUpdate = await db.aggregate(aggregation).toArray();
+    const ids = applicationsToUpdate.map((application) => application._id);
+    await ApplicationModel.updateMany(
+      { _id: { $in: ids } },
+      { $set: { status: APPLICATION_STATUS.WAITING_VALIDATION } },
+      { fromUser: { firstName: "Migration de synchronisation des candidatures PM" } },
+    );
+  },
+
+  async down(db, client) {
+    // TODO write the statements to rollback your migration (if possible)
+    // Example:
+    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
+  },
+};
