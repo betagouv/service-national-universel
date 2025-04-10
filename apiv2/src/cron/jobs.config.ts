4ada3307debba44aad4e8c8d067f310983408b8e
Johann <Johannbr@users.noreply.github.com>
2025-04-10 14:37:56 +0200

    feat(apiv2): 625 - ajouter cron apiv2 (#4912)

---
+++ apiv2/src/cron/jobs.config.ts
@@ -0,0 +1,19 @@
+import { JobsOptions } from "bullmq";
+
+export interface CronJob {
+    name: CronJobName;
+    pattern: string;
+    data?: Record<string, unknown>;
+    opts?: JobsOptions;
+}
+
+export enum CronJobName {
+    ENVOYER_CAMPAGNES_PROGRAMMEES = "envoyer-campagnes-programmees",
+}
+
+export const cronJobs: CronJob[] = [
+    // {
+    //     name: CronJobName.ENVOYER_CAMPAGNES_PROGRAMMEES,
+    //     pattern: "0 8 * * *",
+    // },
+];
