af3a6577bd50098977a18f37773cc0786810cef4
Guillaume Pichon <73333478+GuiPich91@users.noreply.github.com>
2025-04-10 16:00:34 +0200

    feat(admin): 462 - Rendre l’export des contacts de convocation accessible à la DG SNU (#4908)

--- admin/src/scenes/settings/operations/actions/InscriptionsSection.tsx
+++ admin/src/scenes/settings/operations/actions/InscriptionsSection.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Collapsable } from "@snu/ds/admin";
 import { CohortDto } from "snu-lib";
-import ExportContactConvocation from "./Inscription/ExportContactSimulation/ExportContactConvocation";
 import BasculeJeuneValides from "./Inscription/BasculeJeune/BasculeJeuneValides";
 import BasculeJeuneNonValides from "./Inscription/BasculeJeune/BasculeJeuneNonValides";
 
@@ -14,7 +13,6 @@ export default function InscriptionsSection({ session }: InscriptionSectionProps
     <Collapsable title="Inscriptions">
       <BasculeJeuneNonValides session={session} />
       <BasculeJeuneValides session={session} />
-      <ExportContactConvocation session={session} />
     </Collapsable>
   );
 }
