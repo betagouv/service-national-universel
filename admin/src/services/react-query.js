62cab154e4cd95c2c501249566b86c05a6bb4800
Nicolas Bretécher <nicolas.bretecher@proton.me>
2025-04-30 10:49:41 +0200

    up

--- admin/src/services/react-query.js
+++ admin/src/services/react-query.js
@@ -1,6 +1,7 @@
 import { capture } from "@/sentry";
 import { QueryCache, QueryClient } from "@tanstack/react-query";
 import { toastr } from "react-redux-toastr";
+import { ERRORS } from "snu-lib";
 
 export const queryClient = new QueryClient({
   queryCache: new QueryCache({
@@ -8,6 +9,10 @@ export const queryClient = new QueryClient({
       // https://tkdodo.eu/blog/breaking-react-querys-api-on-purpose#defining-on-demand-messages
       if (query.meta?.errorMessage) {
         toastr.error("Erreur", query.meta.errorMessage);
+      }
+      // Filter out 401, 404 and 403 errors
+      if (error.message === ERRORS.NOT_FOUND || error.message === ERRORS.UNAUTHORIZED || error.message === ERRORS.FORBIDDEN) {
+        return;
       }
       capture(error);
     },
