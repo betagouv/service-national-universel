c39133e2fc7052c6128721bbca53e4f4a405ff3f
Nicolas Bretécher <nicolas.bretecher@proton.me>
2025-04-30 13:37:45 +0200

    app

--- admin/src/services/react-query.ts
+++ admin/src/services/react-query.ts
@@ -1,9 +1,7 @@
 import { capture } from "@/sentry";
 import { QueryCache, QueryClient } from "@tanstack/react-query";
 import { toastr } from "react-redux-toastr";
-import { ERRORS } from "snu-lib";
-
-const FILTERED_ERRORS = [ERRORS.NOT_FOUND, ERRORS.OPERATION_UNAUTHORIZED, ERRORS.OPERATION_NOT_ALLOWED];
+import { ERROR_FILTER } from "snu-lib";
 
 export const queryClient = new QueryClient({
   queryCache: new QueryCache({
@@ -12,7 +10,7 @@ export const queryClient = new QueryClient({
       if (query.meta?.errorMessage) {
         toastr.error("Erreur", query.meta.errorMessage as string);
       }
-      if (!FILTERED_ERRORS.includes(error.message as (typeof FILTERED_ERRORS)[number])) {
+      if (!ERROR_FILTER.includes(error.message as (typeof ERROR_FILTER)[number])) {
         capture(error);
       }
     },
