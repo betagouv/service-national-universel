bc13ee5de4bdc540eafcef64fa57bafcbf60eb84
Philippe de Mangou <philippe.demangou@gmail.com>
2025-05-02 17:32:44 +0200

    fix(app): mise à jour du message d'éligibilité pour les volontaires résidant dans certaines régions ou départements d'Outremer (#4986)
    Co-authored-by: Philippe de MANGOU <philippe.demangou@gmail.com>

--- app/src/scenes/preinscription/steps/stepEligibilite.jsx
+++ app/src/scenes/preinscription/steps/stepEligibilite.jsx
@@ -202,8 +202,7 @@ export default function StepEligibilite() {
           <div className="pl-12">
             <p className="m-0 text-xl font-bold">Message Important</p>
             <p className="m-0 text-sm sm:mr-4 md:mr-0">
-              Les inscriptions sont ouvertes pour les volontaires résidant en Corse et dans une région ou un département d’Outremer, les sessions organisées pour les volontaires
-              des autres régions sont complètes.
+              Les inscriptions sont ouvertes pour les volontaires résidant dans certaines régions ou certains départements d’Outremer, les sessions organisées pour les volontaires des autres régions sont complètes.
             </p>
           </div>
         </Container>
