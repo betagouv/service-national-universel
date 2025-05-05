57602f52834e1432507b8f40fa1aefea732af5a7
Nicolas Bretécher <nicolas.bretecher@selego.co>
2025-05-05 14:55:36 +0200

    chore(admin): Ne pas capturer sur Sentry les erreurs non pertinentes (#4979)

--- admin/src/services/react-query.ts
+++ admin/src/services/react-query.ts
@@ -1,15 +1,18 @@
 import { capture } from "@/sentry";
 import { QueryCache, QueryClient } from "@tanstack/react-query";
 import { toastr } from "react-redux-toastr";
+import { ERROR_FILTER } from "snu-lib";
 
 export const queryClient = new QueryClient({
   queryCache: new QueryCache({
     onError: (error, query) => {
       // https://tkdodo.eu/blog/breaking-react-querys-api-on-purpose#defining-on-demand-messages
       if (query.meta?.errorMessage) {
-        toastr.error("Erreur", query.meta.errorMessage);
+        toastr.error("Erreur", query.meta.errorMessage as string);
       }
-      capture(error);
+      if (!ERROR_FILTER.includes(error.message as (typeof ERROR_FILTER)[number])) {
+        capture(error);
+      }
     },
   }),
 });
--- app/src/services/react-query.ts
+++ app/src/services/react-query.ts
@@ -1,6 +1,7 @@
 import { capture } from "@/sentry";
 import { Query, QueryCache, QueryClient } from "@tanstack/react-query";
 import { toastr } from "react-redux-toastr";
+import { ERROR_FILTER } from "snu-lib";
 
 export const queryClient = new QueryClient({
   queryCache: new QueryCache({
@@ -8,6 +9,9 @@ export const queryClient = new QueryClient({
       if (error.name === "AuthenticationError") {
         console.log("Unauthorized, redirecting to auth");
         window.location.href = "/auth?disconnected=1";
+        return;
+      }
+      if (ERROR_FILTER.includes(error.message as (typeof ERROR_FILTER)[number])) {
         return;
       }
       capture(error);
@@ -19,8 +23,10 @@ export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
+      refetchOnMount: false,
       retry(failureCount, error) {
-        if (error.name === "AuthenticationError") {
+        // On ne réessaie pas en cas d'erreur 401, 403 ou 404
+        if (error.name === "AuthenticationError" || ERROR_FILTER.includes(error.message as (typeof ERROR_FILTER)[number])) {
           return false;
         }
         return failureCount < 3;
