6792fc747e92026021a97545bd572d48790a3551
C2Chandelier <charlelie.chandelier@epitech.eu>
2025-05-15 17:09:08 +0200

    up

--- admin/src/components/UserCard.tsx
+++ admin/src/components/UserCard.tsx
@@ -5,11 +5,11 @@ import UserInfo from "./UserInfo";
 import LinkWrapper from "./LinkWrapper";
 
 interface Props {
-  user: UserSaved;
+  user?: UserSaved;
 }
 
 export default function UserCard({ user }: Props) {
-  function getAvatar(user) {
+  function getAvatar(user: UserSaved | undefined) {
     if (!user) return "??";
     const firstName = user.impersonatedBy ? user.impersonatedBy.firstName : user?.firstName;
     const lastName = user.impersonatedBy ? user.impersonatedBy.lastName : user?.lastName;
@@ -18,15 +18,14 @@ export default function UserCard({ user }: Props) {
     if (firstName && !lastName) return "🤖";
     return "??";
   }
-  function getLink(user) {
+  function getLink(user: UserSaved | undefined) {
+    if (!user) return null;
     if (Object.values(ROLES).includes(user?.role)) return `/user/${user._id}`;
     if (user?.role === "Volontaire") return `/volontaire/${user._id}`;
     return null;
   }
 
   const hasLink = getLink(user) !== null;
-
-  if (!user) return null;
 
   return (
     <div className="flex items-center gap-2">
@@ -38,24 +37,26 @@ export default function UserCard({ user }: Props) {
         {getAvatar(user)}
       </div>
 
-      <div className="flex w-10/12 flex-col leading-5">
-        {user.impersonatedBy ? (
-          <>
-            <LinkWrapper link={getLink(user.impersonatedBy)} hasLink={getLink(user.impersonatedBy) !== null}>
-              <UserInfo user={user.impersonatedBy} />
-            </LinkWrapper>
+      {user && (
+        <div className="flex w-10/12 flex-col leading-5">
+          {user.impersonatedBy ? (
+            <>
+              <LinkWrapper link={getLink(user.impersonatedBy)} hasLink={getLink(user.impersonatedBy) !== null}>
+                <UserInfo user={user.impersonatedBy} />
+              </LinkWrapper>
 
+              <LinkWrapper link={getLink(user)} hasLink={hasLink}>
+                <p className="w-full truncate capitalize text-gray-500 text-xs decoration-2 underline-offset-2">En tant que :</p>
+                <UserInfo user={user} />
+              </LinkWrapper>
+            </>
+          ) : (
             <LinkWrapper link={getLink(user)} hasLink={hasLink}>
-              <p className="w-full truncate capitalize text-gray-500 text-xs decoration-2 underline-offset-2">En tant que :</p>
               <UserInfo user={user} />
             </LinkWrapper>
-          </>
-        ) : (
-          <LinkWrapper link={getLink(user)} hasLink={hasLink}>
-            <UserInfo user={user} />
-          </LinkWrapper>
-        )}
-      </div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
