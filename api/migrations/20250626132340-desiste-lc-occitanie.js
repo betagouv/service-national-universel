c04a4e04fd8cb58ee90257fb2ee236af5bfa6a5a
Eric SANCHEZ <eric.cmsfr@gmail.com>
2025-06-26 16:54:08 +0200

    fix(api): migration pour désister les LC d’Occitanie

---
+++ api/migrations/20250626132340-desiste-lc-occitanie.js
@@ -0,0 +1,33 @@
+const { logger } = require("../src/logger");
+const { YOUNG_STATUS } = require("snu-lib");
+const { YoungModel } = require("../src/models");
+
+const cohortName = "2025 HTS 04 - Juillet";
+const region = "Occitanie";
+const reasonCode = "other";
+const reasonMessage = "TODO: En attente de la réponse de Mathilde";
+
+const fromUser = { firstName: `Désaffectation ${cohortName} ${region}` };
+
+module.exports = {
+  async up() {
+    const jeunes = await YoungModel.find({
+      cohort: cohortName,
+      region,
+      status: YOUNG_STATUS.WAITING_LIST,
+    });
+    logger.info(`Désaffectation ${cohortName}: ${jeunes.length} jeunes à desister`);
+    for (const jeune of jeunes) {
+      logger.info(`- désistement ${jeune.id} ${jeune.firstName} ${jeune.lastName}`);
+      jeune.set({
+        status: YOUNG_STATUS.WITHDRAWN,
+        lastStatusAt: Date.now(),
+        withdrawnReason: reasonCode,
+        withdrawnMessage: reasonMessage,
+      });
+      await jeune.save({ fromUser });
+    }
+  },
+
+  async down() {},
+};
