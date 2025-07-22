206e3f80d7b4eb1c5c46d0b20a5e97caa5fdf23d
C2Chandelier <charlelie.chandelier@epitech.eu>
2025-07-22 12:02:42 +0200

    up

--- app/src/scenes/home/EnAttente.tsx
+++ app/src/scenes/home/EnAttente.tsx
@@ -1,30 +1,20 @@
 import React from "react";
-import { YOUNG_STATUS } from "../../utils";
-import useCohort from "@/services/useCohort";
 import useAuth from "@/services/useAuth";
 import HomeContainer from "@/components/layout/HomeContainer";
 import HomeHeader from "@/components/layout/HomeHeader";
 import hero from "../../assets/hero/home.png";
 import JDMA from "@/components/JDMA";
-import CorrectionRequests from "./components/CorrectionRequests";
-import StatusNotice from "./components/StatusNotice";
-import SejourNotice from "./components/SejourNotice";
+import EngagementPrograms from "../preinscription/components/EngagementPrograms";
 
 export default function EnAttente() {
-  const { young, isCLE } = useAuth();
-  const title = `${young.firstName}, bienvenue sur votre compte ${isCLE ? "élève" : "volontaire"}`;
-  const { cohort } = useCohort();
+  const { young } = useAuth();
 
   return (
     <HomeContainer>
-      <HomeHeader title={title} img={hero}>
-        <div className="grid grid-cols-1 gap-8">
-          <SejourNotice cohort={cohort} />
-          <StatusNotice status={young.status} />
-        </div>
-      </HomeHeader>
-
-      {young.status === YOUNG_STATUS.WAITING_CORRECTION && <CorrectionRequests />}
+      <HomeHeader title={`${young.firstName}, vous n’avez pas réalisé le séjour de cohésion.`} img={hero}></HomeHeader>
+      <br />
+      <hr />
+      <EngagementPrograms />
 
       <div className="mt-12 flex justify-end">
         <JDMA id={3154} />
