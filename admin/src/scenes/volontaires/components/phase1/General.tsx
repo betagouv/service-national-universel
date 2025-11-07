916c07e435f4f5e31a8dae37eacece92ea9a278d
Johannbr <johannbr@gmail.com>
2025-11-07 15:29:59 +0100

    feat(admin): 1054 - decommissionnement de la liste des volontaires

--- admin/src/scenes/volontaires/components/phase1/General.tsx
+++ admin/src/scenes/volontaires/components/phase1/General.tsx
@@ -5,7 +5,6 @@ import { Container, Button } from "@snu/ds/admin";
 import { ROLES } from "snu-lib";
 
 import Phase1ConfirmationFormBlock from "./Phase1ConfirmationFormBlock";
-import Phase1PresenceFormBlock from "./Phase1PresenceFormBlock";
 
 export default function General({ young, setYoung, values, setValues, isCheckIsOpen, user }) {
   const [editing, setEditing] = useState(false);
@@ -29,16 +28,6 @@ export default function General({ young, setYoung, values, setValues, isCheckIsO
     <Container title="Informations générales" actions={containerActionList(editing, setEditing, canEdit)}>
       <div className="flex justify-between">
         <Phase1ConfirmationFormBlock young={young} setYoung={setYoung} editing={editing} values={values} setValues={setValues} setLoading={setLoading} />
-        <div className="mx-14 w-[1px] bg-gray-200 shrink-0">&nbsp;</div>
-        <Phase1PresenceFormBlock
-          young={young}
-          setYoung={setYoung}
-          editing={editing}
-          values={values}
-          setValues={setValues}
-          setLoading={setLoading}
-          isYoungCheckinOpen={isCheckIsOpen}
-        />
       </div>
     </Container>
   );
