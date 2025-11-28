551ba80be7d409360957d58f48a842229263b265
Johannbr <johannbr@gmail.com>
2025-11-28 14:35:08 +0100

    chore(devops): 1151 - securisation des wf github

--- devops/build/build-all.sh
+++ devops/build/build-all.sh
@@ -18,16 +18,16 @@ fi
 cd "$(dirname $0)/../.."
 
 turbo_version=$(cat package-lock.json | grep turbo | head -n 1 | sed 's/"turbo": "\(.*\)"/\1/g')
-npm install --global "turbo@$turbo_version"
+npm install --global --ignore-scripts "turbo@$turbo_version"
 
 rm -Rf out
 turbo prune app admin api apiv2
 cp tsconfig.front.json out
 cp -r patches out
 cd out
-npm ci --no-audit --no-fund
+npm ci --no-audit --no-fund --ignore-scripts
 turbo run build
-npm ci --omit dev --workspace api --workspace apiv2 --include-workspace-root
+npm ci --omit dev --workspace api --workspace apiv2 --include-workspace-root --ignore-scripts
 cd ..
 
 rm -Rf $destination
