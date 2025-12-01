8cd504aa7a6d160112ce77eb1ca0745bb5277322
Johann Brancourt <Johannbr@users.noreply.github.com>
2025-12-01 16:22:00 +0100

    feat(admin): 1055 - decommissionnement des séjours CLE (#5218)

--- admin/src/scenes/classe/components/SejourInfos.tsx
+++ admin/src/scenes/classe/components/SejourInfos.tsx
@@ -33,7 +33,7 @@ export default function SejourInfos({ classe, setClasse, editStay, setEditStay, 
         </div>,
       ];
     } else if (canEdit) {
-      return [<Button key="change" type="modify" leftIcon={<HiOutlinePencil size={16} />} title="Modifier" onClick={() => setEdit(!edit)} disabled={isLoading} />];
+      return [];
     } else {
       return [];
     }
@@ -94,101 +94,12 @@ export default function SejourInfos({ classe, setClasse, editStay, setEditStay, 
                   <InputText name="centerDepartment" className="flex-1" label="Département" value={classe.cohesionCenter.department || ""} disabled />
                   <InputText name="centerRegion" className="flex-1" label="Région" value={classe.cohesionCenter.region || ""} disabled />
                 </div>
-                {![ROLES.REFERENT_CLASSE, ROLES.ADMINISTRATEUR_CLE].includes(user.role) && (
-                  <Link to={`/centre/` + classe.cohesionCenter._id} className="w-full">
-                    <Button type="tertiary" title="Voir le centre" className="w-full max-w-none" />
-                  </Link>
-                )}
-              </>
-            )}
-          </div>
-        )}
-        {rights.showCenter && rights.showPDR && <div className="mx-14 w-[1px] bg-gray-200 shrink-0">&nbsp;</div>}
-        {rights.showPDR && (
-          <div className="flex-1">
-            <Label
-              title="Point de rassemblement"
-              name="pdr"
-              tooltip={
-                ![ROLES.REFERENT_CLASSE, ROLES.ADMINISTRATEUR_CLE].includes(user.role)
-                  ? "Le point de rassemblement est automatiquement pré rempli avec l'adresse de l'établissement, vous pouvez le modifier si besoin"
-                  : null
-              }
-            />
-            <Select
-              isAsync
-              className="mb-3"
-              placeholder={![ROLES.REFERENT_CLASSE, ROLES.ADMINISTRATEUR_CLE].includes(user.role) ? "Choisissez un point de rassemblement existant" : "Non renseigné"}
-              loadOptions={(q) => searchPointDeRassemblements({ q, cohort: classe.cohort })}
-              defaultOptions={() =>
-                searchPointDeRassemblements({
-                  q: classe.etablissement?.name,
-                  cohort: classe.cohort,
-                })
-              }
-              noOptionsMessage={"Aucun point de rassemblement ne correspond à cette recherche"}
-              isClearable={true}
-              closeMenuOnSelect={true}
-              value={
-                classe.pointDeRassemblement?.name && classe.pointDeRassemblement?.department
-                  ? { value: classe.pointDeRassemblement._id, label: `${classe.pointDeRassemblement.name}, ${classe.pointDeRassemblement.department}` }
-                  : null
-              }
-              onChange={(option) =>
-                setClasse({
-                  ...classe,
-                  pointDeRassemblement: option?.pointDeRassemblement,
-                  pointDeRassemblementId: option?._id,
-                })
-              }
-              error={errors.pointDeRassemblement}
-              isActive={editStay && rights.canEditPDR}
-              readOnly={!editStay || !rights.canEditPDR}
-              disabled={!rights.canEditPDR || classe?.cohort === "CLE 23-24"}
-            />
-            {classe.pointDeRassemblement && (
-              <>
-                <InputText name="pdrAddress" className="mb-3" label="Numéro et nom de la voie" value={classe.pointDeRassemblement?.address} disabled />
-                <div className="flex items-center justify-between gap-3 mb-3">
-                  <InputText name="pdrZip" className="flex-1" label="Code Postal" value={classe.pointDeRassemblement?.zip} disabled />
-                  <InputText name="pdrCity" className="flex-1" label="Ville" value={classe.pointDeRassemblement?.city} disabled />
-                </div>
-                <div className="flex items-center justify-between gap-3 mb-3">
-                  <InputText name="pdrDepartment" className="flex-1" label="Département" value={classe.pointDeRassemblement?.department} disabled />
-                  <InputText name="pdrRegion" className="flex-1" label="Région" value={classe.pointDeRassemblement?.region} disabled />
-                </div>
-                {![ROLES.REFERENT_CLASSE, ROLES.ADMINISTRATEUR_CLE].includes(user.role) && (
-                  <Link to={`/point-de-rassemblement/` + classe.pointDeRassemblement._id} className="w-full">
-                    <Button type="tertiary" title="Voir le point de rassemblement" className="w-full max-w-none" />
-                  </Link>
-                )}
-              </>
-            )}
-            {infoBus && (
-              <div className="mt-3">
-                <Label title="Transport" name="ligneBus" />
-                <InputText name="busNumber" className="mb-3" label="Numéro de transport" value={infoBus.busId} disabled />
-                <Label title="Aller" name="Aller" />
-                <div className="flex gap-3">
-                  <InputText name="busDepartDate" className="mb-3" label="Date&nbsp;de&nbsp;départ" value={infoBus.departureDate} disabled />
-                  <InputText name="busPdrHour" className="mb-3" label="Heure&nbsp;de&nbsp;convocation" value={infoBus.meetingHour} disabled />
-                  <InputText name="busDepartHour" className="mb-3" label="Heure&nbsp;de&nbsp;départ" value={infoBus.departureHour} disabled />
-                </div>
-                <Label title="Retour" name="Retour" />
-                <div className="flex gap-3 w-full">
-                  <InputText name="busRetrunDate" className="mb-3 w-1/2" label="Date&nbsp;de&nbsp;retour" value={infoBus.returnDate} disabled />
-                  <InputText name="busReturnHour" className="mb-3 w-1/2" label="Heure&nbsp;de&nbsp;retour" value={infoBus.returnHour} disabled />
-                </div>
 
-                {![ROLES.REFERENT_CLASSE, ROLES.ADMINISTRATEUR_CLE].includes(user.role) && (
-                  <Link to={`/ligne-de-bus/` + classe.ligneId} className="w-full">
-                    <Button type="tertiary" title="Voir la ligne de bus" className="w-full max-w-none" />
-                  </Link>
-                )}
-              </div>
+              </>
             )}
           </div>
         )}
+
       </div>
     </Container>
   );
