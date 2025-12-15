4f25c500782c7909c1060d8baa87794546d2c340
Johann Brancourt <Johannbr@users.noreply.github.com>
2025-12-15 16:00:54 +0100

    feat(admin): 1057 - décommissionnement lot 5 (#5229)

--- admin/src/app.tsx
+++ admin/src/app.tsx
@@ -68,13 +68,7 @@ const Content = lazy(() => import("./scenes/content"));
 const DevelopAssetsPresentationPage = lazy(() => import("./scenes/develop/AssetsPresentationPage"));
 // @ts-ignore
 const DesignSystemPage = lazy(() => import("./scenes/develop/DesignSystemPage"));
-// @ts-ignore
-const DSNJExport = lazy(() => import("./scenes/dsnj-export"));
-// @ts-ignore
-const INJEPExport = lazy(() => import("./scenes/injep-export"));
 
-// @ts-ignore
-const Goal = lazy(() => import("./scenes/goal"));
 // @ts-ignore
 const Inscription = lazy(() => import("./scenes/inscription"));
 // @ts-ignore
@@ -312,14 +306,11 @@ const Home = () => {
                   <RestrictedRoute path="/inscription" component={Inscription} />
                   <RestrictedRoute path="/user" component={Utilisateur} />
                   <RestrictedRoute path="/contenu" component={Content} />
-                  <RestrictedRoute path="/objectifs" component={Goal} roles={[ROLES.ADMIN]} />
                   <RestrictedRoute path="/centre" component={Center} />
 
                   <RestrictedRoute path="/association" component={Association} />
                   <RestrictedRoute path="/besoin-d-aide" component={SupportCenter} />
                   <RestrictedRoute path="/equipe" component={Team} />
-                  <RestrictedRoute path="/dsnj-export" component={DSNJExport} />
-                  <RestrictedRoute path="/injep-export" component={INJEPExport} />
                   <RestrictedRoute path="/import-si-snu" component={ImportSiSnu} />
                   {[ROLES.ADMIN].includes(user?.role) && SUB_ROLE_GOD === user?.subRole ? <RestrictedRoute path="/plan-marketing/:tab?" component={PlanMarketing} /> : null}
 
--- admin/src/scenes/team/components/InfoDepartment.jsx
+++ admin/src/scenes/team/components/InfoDepartment.jsx
@@ -1,19 +1,15 @@
 import React, { useEffect, useState } from "react";
-import CardServiceDepartemental from "./card/CardServiceDepartemental";
-import CardContacts from "./card/CardContacts";
 import api from "../../../services/api";
 import CardRepresentant from "./card/CardRepresentant";
 
 export default function InfoDepartement({ department }) {
   const [servicesDep, setServicesDep] = useState();
-  const [contacts, setContacts] = useState();
   const [representant, setRepresentant] = useState();
 
   const getService = async () => {
     const { data, ok } = await api.get(`/department-service/${department}`);
     if (!ok) return;
     setServicesDep(data);
-    setContacts(data.contacts);
     setRepresentant(data.representantEtat || null);
   };
 
@@ -25,9 +21,7 @@ export default function InfoDepartement({ department }) {
 
   return (
     <div className="flex-stretch flex flex-row flex-wrap gap-y-4">
-      {department ? <CardServiceDepartemental servicesDep={servicesDep} department={department} getService={getService} /> : null}
       {department ? <CardRepresentant representant={representant} getService={getService} department={department} idServiceDep={servicesDep._id} /> : null}
-      {contacts ? <CardContacts contacts={contacts} idServiceDep={servicesDep._id} getService={getService} /> : null}
     </div>
   );
 }
